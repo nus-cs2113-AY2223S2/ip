441309e9c22780bbbd888a41d62cad15f7db58b2
HiIAmTzeKean <ngtzekean600@gmail.com>
2023-01-09 22:30:11 +0800

    feat: Add greet and exit message
    DUKE needs to have an upgrade.
    
    DUKE upgraded with a greet and exit message on program run.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
 public class Duke {
+    /**
+     * Main program that runs the Duke program.
+     * Greets users and exits.
+     * @param args
+     */
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String linebreak = "________________________________________\n";
+        System.out.println(logo + linebreak);
+
+        // Greet user
+        String greeting = "Hello! I'm Duke\nWhat can I do for you?\n";
+        System.out.println(greeting + linebreak);
+        
+        // Exit the program
+        String exit = "Bye. Hope to see you again soon!\n";
+        System.out.println(exit + linebreak);
     }
 }
