165017a20cf0c6adcaa12fe26d989ca395f54f8c
Ming En <mingen8216@gmail.com>
2023-01-12 12:16:58 +0800

    week 2

---
+++ src/main/java/Greetings.java
@@ -0,0 +1,13 @@
+public class Greetings {
+    public static void introduction() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void goodbye() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
