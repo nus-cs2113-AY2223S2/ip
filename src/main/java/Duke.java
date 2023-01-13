abf52074a7126534a43f2121ce4923c44d9580d4
T-Wan-Lin <tanwanlin.c2021>
2023-01-13 17:38:39 +0800

    Increment Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,6 +9,5 @@ public class Duke {
         System.out.println("Hello! I'm Duke\n");
         System.out.println("What can I do for you?\n");
         System.out.println("Bye. Hope to see you again soon!\n");
-
     }
 }
