5888e2018c7bd062a508478b7451604819ca854c
geeeetyx <e0725682@u.nus.edu>
2023-01-13 17:10:11 +0800

    feat: new files

---
+++ src/main/java/Week2/helloworld.java
@@ -0,0 +1,9 @@
+package Week2;
+
+public class helloworld {
+
+    public static void main(String[] args) {
+        // generate some simple output
+        System.out.println("Hello, World!");
+    }
+}
