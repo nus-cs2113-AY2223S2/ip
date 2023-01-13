25fb5d6fd5a588d66110c5d1fcbf4ac934c7275e
Sherlock-YH <yh1259891725@gmail.com>
2023-01-13 23:48:31 +0800

    Modify Level-0 Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
+        String Horizontal_line = "____________________________________________________________";
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("---------------------------------\n");
+        System.out.println(Horizontal_line+"\n");
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
-        System.out.println("---------------------------------\n");
+        System.out.println(Horizontal_line+"\n");
         System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
