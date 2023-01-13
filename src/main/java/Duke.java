6e17cd83cdb13551381541fa0703918e38791274
MattLiuZJ <matthew.liu.zhenjie@gmail.com>
2023-01-13 17:32:53 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
+    public static void dashSeperator() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void sayGreeting() {
+        dashSeperator();
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        dashSeperator();
+        System.out.println("Bye. Hope to see you again soon!");
+        dashSeperator();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //System.out.println("Hello from\n" + logo);
+        sayGreeting();
     }
 }
