04d57909cff66897f774e33d2ce4ffd8aeef8538
EangJS <eugeneang654871@gmail.com>
2023-01-13 22:57:26 +0800

    Add Increment: Level-0 (Duke Greetings)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,22 @@
 public class Duke {
+
+    /**
+     * Prints out the greeting message to the user
+     */
+    public static void greetUser(){
+        String greetMessage = "Hello! I'm Duke\n"
+                + "What can I do for you?";
+        String exitMessage = "Bye. Hope to see you again soon!";
+        System.out.println(greetMessage);
+    }
+
+    /**
+     * Prints out the exit message to the user
+     */
+    public static void exitProgram(){
+        String exitMessage = "Bye. Hope to see you again soon!";
+        System.out.println(exitMessage);
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +24,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greetUser();
+        exitProgram();
     }
 }
