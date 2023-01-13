14704b54dfcbd84ee3e8bc779a9d9e70d269c124
HiIAmTzeKean <ngtzekean600@gmail.com>
2023-01-13 17:34:19 +0800

    refactor: encapsulate greet and bye into functions
    Code readability in main function call. In preparation for
    Level 1 commit

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -1,32 +1,51 @@
 package Duke;
+
+import java.util.Scanner;
+
 public class Duke {
     static String LINEBREAK = "________________________________________\n";
+    static Scanner sc = new Scanner(System.in);
     /**
      * Main program that runs the Duke program.
      * Greets users and exits.
      * @param args
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-
-        printMessage(logo);
-
-        // Greet user
-        printMessage("Hello! I'm Duke\nWhat can I do for you?");
-        
-        // Exit the program
-        printMessage("Bye. Hope to see you again soon!");
+        greet();
+        bye();
     }
     /**
      * Prints message followed by a linebreak
      * @param message Output message to print
      */
-    private static void printMessage(String message){
+    private static void printMessage(String message) {
         System.out.println(message);
         System.out.println(LINEBREAK);
+    }
+    /**
+     * Gets the a message from console that user inputs
+     * @return String
+     */
+    private static String getMessage() {
+        return sc.nextLine();
+    }
+    /**
+     * Prints greet message to user
+     */
+    private static void greet() {
+        String logo = " ____        _        \n"
+        + "|  _ \\ _   _| | _____ \n"
+        + "| | | | | | | |/ / _ \\\n"
+        + "| |_| | |_| |   <  __/\n"
+        + "|____/ \\__,_|_|\\_\\___|\n";
+
+        printMessage(logo);
+        printMessage("Hello! I'm Duke\nWhat can I do for you?");
+    }
+    /**
+     * Prints bye messgae to user
+     */
+    private static void bye() {
+        printMessage("Bye. Hope to see you again soon!");
     }
 }
