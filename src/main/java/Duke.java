7e4f591dab51ced5e9cfaf64cdb6abebd2e24101
natashatanyt <natashatan.yt@gmail.com>
2023-01-13 21:24:07 +0800

    Add Level-0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
 public class Duke {
+    public static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    public static String HorizontalRule = "__________________________________________________";
+
+
+    public static void StartUp(){
+        System.out.println(logo);
+        System.out.println(HorizontalRule);
+        System.out.println("Hello! I'm Duke. \nWhat can I do for you?");
+        System.out.println(HorizontalRule);
+    }
+
+    public static void ShutDown(){
+        System.out.println(HorizontalRule);
+        System.out.println("Bye! Hope to see you again soon!");
+        System.out.println(HorizontalRule);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        StartUp();
+        ShutDown();
     }
 }
