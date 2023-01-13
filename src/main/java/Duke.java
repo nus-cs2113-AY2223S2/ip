b456b23508275e5a60e649bf244015a710305c14
ArtemiszenN <artemiszenn@gmail.com>
2023-01-14 00:45:09 +0800

    Add greet functionality: level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
+        String line = "____________________________________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|";
+        System.out.println(line+'\n'+logo+'\n'+line);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n"+line);
+        System.out.println("Bye. Hope to see you again soon!\n"+line);
     }
 }
