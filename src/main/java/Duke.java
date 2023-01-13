071d3a4a60c37d87d382993399e508d0c28e5ee3
Vu Van Dung <me@joulev.dev>
2023-01-13 16:54:28 +0800

    Implement an initial skeletal version of the program (level 0)
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+class IO {
+    /**
+     * Prints a string with a space in front. This space is used for indentation.
+     * Though optional, that indentation looks kinda nice.
+     * 
+     * @param s The string to be printed.
+     */
+    public static void println(String s) {
+        System.out.println(" " + s);
+    }
+
+    /**
+     * Prints a line of underscores, served as a divider between sections. Like
+     * the HTML's {@code hr} tag.
+     */
+    public static void print_line() {
+        System.out.println("____________________________________________________________");
+    }
+}
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        IO.print_line();
+        IO.println("Hello! I'm Duke");
+        IO.println("What can I do for you?");
+        IO.print_line();
+        IO.println("Bye. Hope to see you again soon!");
+        IO.print_line();
     }
 }
