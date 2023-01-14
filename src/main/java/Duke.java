023a94febf73b4e9bc638e28613375cadf87a7ac
jaredoong <jaredoong@gmail.com>
2023-01-14 16:29:15 +0800

    Update Duke.java
    Update base template provided to include welcome and goodbye message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,24 @@
 public class Duke {
+
+    public static void welcomeMessage() {
+        separator();
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+    }
+
+    public static void endingMessage() {
+        separator();
+        System.out.println(" Bye. Hope to see you again soon!");
+    }
+
+    public static void separator() {
+        System.out.println("____________________________________________________________");
+    }
+
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        welcomeMessage();
+        endingMessage();
+        separator();
     }
 }
