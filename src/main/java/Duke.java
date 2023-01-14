f7981589707194244a59fd910bd36e612acfe6e4
L0Z1K <qortmddbs731@naver.com>
2023-01-14 15:16:05 +0800

    feat: Complete 'level-0:Greet'
    I changed my chatbot name to Orca, which is my favorite animal.
    I added a simple greeting message.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " _______  ______    _______  _______ \n"
+                + "|       ||    _ |  |       ||   _   | \n"
+                + "|   _   ||   | ||  |       ||  |_|  | \n"
+                + "|  | |  ||   |_||_ |       ||       | \n"
+                + "|  |_|  ||    __  ||      _||       | \n"
+                + "|       ||   |  | ||     |_ |   _   | \n"
+                + "|_______||___|  |_||_______||__| |__| \n";
+
+        System.out.println("--------------------------------------------------");
+        System.out.println(logo);
+        System.out.println("Hello! I'm Orca, your assistant chatbot.");
+        System.out.println("What can I do for you?");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Bye! See you later!");
+        System.out.println("-----------------------------------");
     }
 }
