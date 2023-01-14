14551c5632a8d93eb3472d6b6842ceb157409edf
chongyongrui <yrchong2000@gmail.com>
2023-01-14 22:51:43 +0800

    add greeting and bye message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,20 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+        String horizontalBar = "-----------------------------------------\n";
         System.out.println("Hello from\n" + logo);
+
+        //to greet user
+        System.out.println(horizontalBar +
+                "Hello! I'm Duke! \n" +
+                "What can I do for you? \n" +
+                horizontalBar);
+
+        //to say bye
+        System.out.println(horizontalBar +
+                "Bye! Hope to see you again soon!\n" +
+                horizontalBar);
+
     }
 }
