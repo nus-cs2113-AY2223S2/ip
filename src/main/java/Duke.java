a227980113409000015d63bb6eeed6147880f38e
Manoj364 <theerazor@outlook.com>
2023-01-14 20:13:11 +0800

    Add increment: Level-0 greeting and exit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("_________________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(" ");
+        System.out.println("_________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(" ");
+        System.out.println("_________________________________________");
     }
 }
