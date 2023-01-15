1ee7646a757d98ec4302d3d39b000e64d849d87b
choongzhanhong <choongorilla@gmail.com>
2023-01-15 18:37:35 +0800

    .gitignore: add *.class Duke.java: add level-0 greet and exit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,27 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String intro = "Hello! I'm Duke\n"
+                + "What can I do for you?";
+
+        String outro = "Bye. Hope to see you again soon!";
+
+        // Greet message
+        printHLine();
+        System.out.println(intro);
+        printHLine();
+
+        // Exit message
+        System.out.println(outro);
+        printHLine();
+
+    }
+
+    /**
+     * Prints a horizontal line.
+     */
+    public static void printHLine() {
+        System.out.println("================================");
     }
 }
