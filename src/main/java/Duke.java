504c0e8bb78ed2089f73ae724ec4cfc7e6d96346
Vignesh-30 <30vignesh@gmail.com>
2023-01-15 13:48:19 +0800

    Level 0. Greet added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
+
+    public static void Greet() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?\n");
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Greet();
     }
 }
