03a12fe7112253293d222f7b362463b1d620ef1d
Vignesh-30 <30vignesh@gmail.com>
2023-01-15 13:59:59 +0800

    Seperated Level 0 into Greet and Bye methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,13 +4,17 @@ public class Duke {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
-
         System.out.println("____________________________________________________________");
+    }
+
+    public static void  Bye() {
+      //  System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!\n");
         System.out.println("____________________________________________________________");
     }
 
     public static void main(String[] args) {
         Greet();
+        Bye();
     }
 }
