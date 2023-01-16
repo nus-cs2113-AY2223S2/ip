7e288808389ea8bf139c952b3c499730c7608fe7
ChubbsBunns <troskproductions@gmail.com>
2023-01-16 13:51:09 +0800

    Removed entering name component

--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -7,10 +7,8 @@ public class Greet {
         System.out.println("Hey person! My name is Duke, that is D U K E Duke");
         System.out.println("How may I be of your assistance today my friend?");
         System.out.println("__________________________________________");
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter your name");
-        String userName = myObj.nextLine();
-        System.out.println("Hello " + userName + "! Aside from greeting I am not able to do much yet, so please check back soon!");
+
+        System.out.println("Hello! Aside from greeting I am not able to do much yet, so please check back soon!");
         System.out.println(("Bye bye!"));
     }
 }
