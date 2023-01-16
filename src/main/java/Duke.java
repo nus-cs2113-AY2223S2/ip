941fd2664f019d89a4174407f0a0e5abb99c6d69
JuneNita <junenita@gmail.com>
2023-01-17 07:59:54 +0800

    [ip1] add greet() in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
 public class Duke {
-    public static void main(String[] args) {
+    public static void printDuke() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "\n");
+    }
+
+    // Greets the users and exits
+    public static void greet() {
+        String breakLine = "------------------------------------------\n";
+        System.out.println(breakLine +
+                "Hello! I'm Duke\n" +
+                "What can I do for you?\n" +
+                breakLine +
+                "Bye. Hope to see you again soon!\n" +
+                breakLine);
+    }
+    public static void main(String[] args) {
+        printDuke();
+        greet();
     }
 }
