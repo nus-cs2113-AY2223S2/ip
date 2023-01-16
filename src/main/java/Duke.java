ee8ce06c21d8e600876a8f7602b3e261a387fec7
nichyjt <nicholas.yek@gmail.com>
2023-01-16 14:08:25 +0800

    Add Greet, Exit and PrintBorder functions for skeletal control flow

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,19 @@
 public class Duke {
+
+    public static void PrintBorder(){
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void Exit(){
+        System.out.println("Bye. Hope to see you again soon!");
+        PrintBorder();
+    }
+    public static void Greet(){
+        PrintBorder();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        PrintBorder();
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +21,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Greet();
+        Exit();
     }
 }
