e87c439d0ef99df6f5ff1e3310053f99a2bc2d3e
jacob-stein1 <jmstein@bu.edu>
2023-01-16 15:46:01 +0800

    Implement Duke level-0: greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String greeting = "____________________________________________________________\nHello! I'm Duke\nWhat can I do for you?";
+        String exit = "____________________________________________________________\nBye. Hope to see you again soon!\n____________________________________________________________";
+        System.out.println(greeting);
+        System.out.println(exit);
     }
 }
