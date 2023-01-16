04be2c16c6e310481be7782f6b7978c75471d307
Jin Xuan Ow Yong <owyongjinxuan@gmail.com>
2023-01-17 01:29:59 +0800

    Add greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
 public class Duke {
+    private static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+
     private static void printLogo() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,9 +10,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printLine();
+    }
+
+    private static void greet() {
+        System.out.println("Hello! I'm Duke\n"
+                         + "What can I do for you?");
+        printLine();
+    }
+
+    private static void exit() {
+        System.out.println("Bye. How to see you again soon!");
+        printLine();
     }
 
     public static void main(String[] args) {
         printLogo();
+        greet();
+        exit();
    }
 }
