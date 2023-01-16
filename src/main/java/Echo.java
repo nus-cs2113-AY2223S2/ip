98932183bc50fa5f4a997d549fc93608882a1fd6
ChubbsBunns <troskproductions@gmail.com>
2023-01-16 14:51:18 +0800

    Changed the default personality to a Pikachu and implemented level 1 requirements

---
+++ src/main/java/Echo.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Echo {
+    public void Echo()
+    {
+
+        String surprisedPikachuFaceLogo = "⣿⣿⣿⣿⣿⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿\n"
+                + "⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿\n"
+                + "⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿\n"
+                + "⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿\n"
+                + "⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣴⣿⣿⣿⣿\n"
+                + "⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿ \n"
+                + "⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠈⠉⠀⠀⠤⠄⠀⠀⠀⠉⠁⠀⠀⠀⠀⢿⣿⣿⣿ \n"
+                + "⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿ \n"
+                + "⣿⣿⣿⣿⣿⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿ \n"
+                + "⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿ \n"
+                + "⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿\n";
+
+
+        System.out.println("PIKA is happy you're back, let me echo some stuff back at you PIKAPI");
+        while(true)
+        {
+            System.out.println("______________________________________________");
+            Scanner myObj = new Scanner(System.in);
+            String inputString = myObj.nextLine();
+            System.out.println("______________________________________________");
+            if (inputString.equals("bye"))
+            {
+                System.out.println("    *suprised Pikachu face* \n");
+                System.out.println(surprisedPikachuFaceLogo + "\n");
+                System.out.println("Pikapi is surprised to see you go, see you soon fwen\n");
+                System.exit((0));
+            }
+            else
+            {
+                System.out.println(inputString);
+            }
+        }
+    }
+}
