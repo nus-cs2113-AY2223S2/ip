486f3ce257d5ed4ca545dc8062faacd1c508fcf6
Zijin <e0772699@u.nus.edu>
2023-01-16 14:51:03 +0800

    Added level 0 increment

---
+++ src/main/java/Level0.java
@@ -0,0 +1,6 @@
+public class Level0 {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Duke \nWhat can I do for you?");
+        System.out.println("\nBye. Hope to see you again soon!");
+    }
+}
