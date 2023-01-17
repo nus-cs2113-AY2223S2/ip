a27b738b829d8d975fb08e4ba6c5b60cc70b67dc
namsengi11 <namsengi11@gmail.com>
2023-01-18 00:00:55 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+
+        String greeting = " Hello! I'm Duke\n" +
+                " What can I do for you?";
+        String exiting = " Bye. Hope to see you again soon!";
+        String lineBreak = "____________________________________________________________";
+        System.out.println(lineBreak);
+        System.out.println(greeting);
+        System.out.println(lineBreak);
+        System.out.println(exiting);
+        System.out.println(lineBreak);
     }
 }
