0b4a165983070bcfd4d784ce31aff57e7f52c880
Ju-Can <ju.can.0860@gmail.com>
2023-01-17 22:33:20 +0800

    Lvl 0 Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,19 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________\n";
+        String greet = line +
+                "Hello! I'm Duke\n" +
+                "What can I do for you?\n" +
+                line +
+                "Bye. Hope to see your again soon!\n" +
+                line;
+
+        System.out.println(greet);
     }
 }
