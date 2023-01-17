5e12c21f82152988d919ff6c1c5966da1bb73c7f
Wilson Oh <e0773510@u.nus.edu>
2023-01-17 22:44:53 +0800

    Format files according to the specified coding standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,74 +4,72 @@ import java.util.stream.IntStream;
 
 public class Duke {
 
-  private ArrayList<Task> tasks = new ArrayList<>();
+    private ArrayList<Task> tasks = new ArrayList<>();
 
-  /**
-   * Variadic print function wrapping System.out.println, printing the
-   * lines out surrounded by 2 dividers and indented
-   *
-   * @param lines The text to be printed out. Each argument denotes a piece of
-   *              text to be printed out
-   *              on a new line
-   */
-  public void printMessage(String... lines) {
-    System.out.println("\t────────────────────────────────────────────────────────────");
-    for (String line : lines) {
-      System.out.println("\t" + line);
-    }
-    System.out.println("\t────────────────────────────────────────────────────────────");
-  }
-
-  public String[] getTasksFormatted() {
-    return IntStream.rangeClosed(1, tasks.size()).mapToObj(idx -> String.format("%d: %s", idx, tasks.get(idx - 1)))
-        .toArray(String[]::new);
-  }
-
-  public void addTask(String taskName) {
-    tasks.add(new Task(taskName));
-  }
-
-  public void run() {
-    printMessage("Hello! I'm Duke", "What can I do for you?");
-    // try-with-resources to close the scanner automatically, preventing resource
-    // leaks
-    try (Scanner sc = new Scanner(System.in)) {
-      while (true) {
-        System.out.print("> ");
-        String line = sc.nextLine();
-        String[] tokens = line.split(" ");
-        String command = tokens[0];
-        switch (command) {
-          case "bye":
-            printMessage("Bye. Hope to see you again soon!");
-            return;
-          case "list":
-            printMessage(getTasksFormatted());
-            break;
-          case "mark": {
-            int idx = Integer.parseInt(tokens[1]) - 1;
-            Task task = tasks.get(idx);
-            task.markDone();
-            printMessage("Nice! I've marked this task as done:", "  " + task.toString());
-            break;
-          }
-          case "unmark": {
-            int idx = Integer.parseInt(tokens[1]) - 1;
-            Task task = tasks.get(idx);
-            task.unMarkDone();
-            printMessage("OK, I've marked this task as not done yet:", "  " + task.toString());
-            break;
-          }
-          default:
-            addTask(line);
-            printMessage("added: " + line);
+    /**
+     * Variadic print function wrapping System.out.println, printing the
+     * lines out surrounded by 2 dividers and indented
+     *
+     * @param lines The text to be printed out. Each argument denotes a piece of
+     *              text to be printed out
+     *              on a new line
+     */
+    public void printMessage(String... lines) {
+        System.out.println("\t────────────────────────────────────────────────────────────");
+        for (String line : lines) {
+            System.out.println("\t" + line);
         }
-      }
+        System.out.println("\t────────────────────────────────────────────────────────────");
     }
-  }
 
-  public static void main(String[] args) {
-    Duke duke = new Duke();
-    duke.run();
-  }
+    public String[] getTasksFormatted() {
+        return IntStream.rangeClosed(1, tasks.size())
+            .mapToObj(idx -> String.format("%d: %s", idx, tasks.get(idx - 1)))
+            .toArray(String[] ::new);
+    }
+
+    public void addTask(String taskName) { tasks.add(new Task(taskName)); }
+
+    public void run() {
+        printMessage("Hello! I'm Duke", "What can I do for you?");
+        // try-with-resources to close the scanner automatically, preventing resource leaks
+        try (Scanner sc = new Scanner(System.in)) {
+            while (true) {
+                System.out.print("> ");
+                String line = sc.nextLine();
+                String[] tokens = line.split(" ");
+                String command = tokens[0];
+                switch (command) {
+                case "bye":
+                    printMessage("Bye. Hope to see you again soon!");
+                    return;
+                case "list":
+                    printMessage(getTasksFormatted());
+                    break;
+                case "mark": {
+                    int idx = Integer.parseInt(tokens[1]) - 1;
+                    Task task = tasks.get(idx);
+                    task.markDone();
+                    printMessage("Nice! I've marked this task as done:", "  " + task.toString());
+                    break;
+                }
+                case "unmark": {
+                    int idx = Integer.parseInt(tokens[1]) - 1;
+                    Task task = tasks.get(idx);
+                    task.unMarkDone();
+                    printMessage("OK, I've marked this task as not done yet:", "  " + task.toString());
+                    break;
+                }
+                default:
+                    addTask(line);
+                    printMessage("added: " + line);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Duke duke = new Duke();
+        duke.run();
+    }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,23 +1,19 @@
 public class Task {
 
-  private String name;
-  boolean done;
+    private String name;
+    boolean done;
 
-  Task(String taskName) {
-    this.name = taskName;
-    this.done = false;
-  }
+    Task(String taskName) {
+        this.name = taskName;
+        this.done = false;
+    }
 
-  public void markDone() {
-    this.done = true;
-  }
+    public void markDone() { this.done = true; }
 
-  public void unMarkDone() {
-    this.done = false;
-  }
+    public void unMarkDone() { this.done = false; }
 
-  @Override
-  public String toString() {
-    return String.format("[%s] %s", this.done ? "X" : " ", this.name);
-  }
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.done ? "X" : " ", this.name);
+    }
 }
