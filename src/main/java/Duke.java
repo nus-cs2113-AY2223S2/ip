ef614ca9bab6a22080794061e40fb6dc6e5aeff2
ltzehan <ltzehan13@gmail.com>
2023-01-17 11:08:58 +0800

    feat: Implemented Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
 public class Duke {
+
+    private static final String GREET_STR = "Hello! I'm Duke";
+    private static final String PROMPT_STR = "What can I do for you?";
+    private static final String EXIT_STR = "Bye. Hope to see you again soon!";
+
+    // Overloaded default for printDivider
+    private static void printDivider() {
+        printDivider(60);
+    }
+
+    private static void printDivider(int width) {
+        System.out.println("_".repeat(width));
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printDivider();
+        System.out.println(GREET_STR);
+        System.out.println(PROMPT_STR);
+        printDivider(40);
+        System.out.println(EXIT_STR);
     }
 }
