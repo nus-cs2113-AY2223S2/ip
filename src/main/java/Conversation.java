3e3587c9f1704021a69fc7c4c27b8020034e0213
Rayleigh47 <yanming8870@outlook.com>
2023-01-17 14:32:45 +0800

    Added Conversation.java Updated Duke.java

---
+++ src/main/java/Conversation.java
@@ -0,0 +1,17 @@
+public class Conversation {
+    public static void greeting() {
+        System.out.println("Hello! I'm Duke");
+    }
+    public static void question() {
+        System.out.println("What can I do for you?");
+    }
+    public static void farewell() {
+        System.out.println("Bye. Hope to see you again soon!");        
+    }
+    public static void lines() {
+        System.out.println("____________________________________________________________");
+    }
+    public static void gap() {
+        System.out.println("");
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +7,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Conversation.lines();
+        Conversation.greeting();
+        Conversation.question();
+        //Conversation.gap();
+        Conversation.lines();
+        Conversation.farewell();
+        //Conversation.gap();
+        Conversation.lines();
     }
 }
