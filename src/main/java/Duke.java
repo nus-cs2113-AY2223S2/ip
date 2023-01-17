5430300a4164f2557c0e4b762144302caafacca2
irving11119 <irving.deboer@gmail.com>
2023-01-17 11:45:40 +0800

    Update level 0 increments to match coding standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
-        String linebreak = "____________________________________________________________\n";
-        System.out.println(linebreak + "Hello I'm Duke!\nWhat can I do for you?\n" + linebreak + "Bye. Hope to see you again soon!\n" + linebreak);
+        String lineBreak = "____________________________________________________________\n";
+        System.out.println(lineBreak + "Hello I'm Duke!\nWhat can I do for you?\n"
+                + lineBreak + "Bye. Hope to see you again soon!\n" + lineBreak);
     }
 }
