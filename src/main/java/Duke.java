188fa9825812d815ff640b3c94f7a72ae61b7218
Wilson Oh <e0773510@u.nus.edu>
2023-01-17 21:22:23 +0800

    Add printMessage helper function for pretty printing Duke's output

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,21 @@
 public class Duke {
+  /**
+   * Variadic print function wrapping System.out.println, printing the
+   * lines out surrounded by 2 dividers and indented
+   *
+   * @param lines The text to be printed out. Each argument denotes a piece of
+   *              text to be printed out
+   *              on a new line
+   */
+  public static void printMessage(String... lines) {
+    System.out.println("\t────────────────────────────────────────────────────────────");
+    for (String line : lines) {
+      System.out.println("\t" + line);
+    }
+    System.out.println("\t────────────────────────────────────────────────────────────");
+  }
+
   public static void main(String[] args) {
-    System.out.println("Hello! I'm Duke");
-    System.out.println("What can I do for you?");
-    System.out.println("Bye. Hope to see you again soon!");
+    printMessage("Hello! I'm Duke", "What can I do for you?", "Bye. Hope to see you again soon!");
   }
 }
