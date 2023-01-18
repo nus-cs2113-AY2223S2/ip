96caceef83300a5f1d1a095d8deb6a96a65414a6
lil1n <lilinongsc@gmail.com>
2023-01-18 16:17:01 +0800

    src/main/java/Duke.java: add level 0 Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
+    public static void greet() {
+        String line = "____________________________________________________________";
+        String greeting = " Hello! I'm Duke\n"
+                + " What can I do for you?";
+        System.out.println(line);
+        System.out.println(greeting);
+        System.out.println(line);
+    }
+    public static void bye() {
+        String line = "____________________________________________________________";
+        String goodbye = " Bye. Hope to see you again soon!";
+        System.out.println(goodbye);
+        System.out.print(line);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        bye();
     }
 }
