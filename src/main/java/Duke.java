6e1c149241eaae90af1bd1137a9f90cc4a970d30
SSzeWen <seetszewen@gmail.com>
2023-01-18 13:55:34 +0800

    Level 0. Greet. Duke greets the user and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Greeting and Exiting instantly
+        String greet = "Hello! I'm Duke\nWhat can I do for you?";
+        String exit = "Bye. Hope to see you again soon!\n";
+        String line = "____________________________________________________________\n";
+        System.out.println(line + greet);
+        System.out.print(line + exit + line);
     }
 }
