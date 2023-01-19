ad8315444044f44d78f5f5b82ac541837f27f626
YatPang <yatpangleong08@gmail.com>
2023-01-19 16:29:00 +0800

    Add List, Add

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,8 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         String input;
+        String[] lst = new String[100];
+        int index = 0;
         while (true) {
             Scanner sc = new Scanner(System.in);
             input = sc.nextLine();
@@ -11,9 +13,17 @@ public class Duke {
                 // terminate
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
-            } else {
+            } else if (input.equals("list")) {
+                for (int i = 0; i < index; i++){
+                    System.out.println(i + 1 +". " + lst[i] );
+                }
+            }
+
+            else {
                 // echo
-                System.out.println(input);
+                System.out.println("added: "+ input);
+                lst[index] = input;
+                index += 1;
             }
         }
     }
