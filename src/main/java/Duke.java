064e7f7a1c10c2826bea4ab5c916e8c574993ca7
kdh3799 <379914588@qq.com>
2023-01-19 16:51:43 +0800

    Commit Week2 Level 0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,15 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String hellomsg = " Hello! I'm Duke\n" +
+                " What can I do for you?\n";
+        String byemsg = " Bye. Hope to see you again soon!\n";
+        String horizontalLine = "____________________________________________________________";
+
         System.out.println("Hello from\n" + logo);
+        System.out.println(horizontalLine);
+        System.out.println(hellomsg);
+        System.out.println(horizontalLine);
+        System.out.println(byemsg);
     }
 }
