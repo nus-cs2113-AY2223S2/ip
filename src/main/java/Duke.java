62aadef236a57176a39397a4898574274cebc97f
Leo Zheng Rui Darren <leo.darren@gmail.com>
2023-01-19 13:13:08 +0800

    Update variable names

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String Greeting = " Hello! I'm Duke\n";
-        String Prompt = " What can I do for you?\n";
-        String Farewell = " Bye. Hope to see you again soon!\n";
+        String greeting = " Hello! I'm Duke\n";
+        String prompt = " What can I do for you?\n";
+        String farewell = " Bye. Hope to see you again soon!\n";
         String line = "____________________________________________________________\n";
         System.out.println(line + Greeting + Prompt + line + Farewell + line);
     }
