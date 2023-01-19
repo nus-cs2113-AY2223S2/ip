a5b0d72cbcc60f420b8a4b29def57d825e9b3a8e
YatPang <yatpangleong08@gmail.com>
2023-01-19 16:12:26 +0800

    Add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        String input;
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                // terminate
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            } else {
+                // echo
+                System.out.println(input);
+            }
+        }
     }
 }
