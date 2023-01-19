57a252774b648caa9618562f3d921822bb0a7579
jared-ee <eejared01@gmail.com>
2023-01-19 14:41:32 +0800

    IP Week 2/Increment level-0 by Ee Jia En Jared, A0234035R

---
+++ src/main/java/level0.java
@@ -0,0 +1,7 @@
+public class level0 {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
