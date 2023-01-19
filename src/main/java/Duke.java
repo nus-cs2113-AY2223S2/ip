58ae93a9944fa586d63f688c565407a23e4aa908
TayJiunYuan <tayjy2000@gmail.com>
2023-01-19 22:42:28 +0800

    Welcome Message + Exit Message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,15 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizontalLine = "――――――――――――――――――――――――――――――――――――――――――";
+
+        String greetingLine = "Well hello there!\n" +
+                              "What can I do for you today?\n"; 
+
+        String exitLine = "Thank you! See you again soon :) \n";
+                            
+                              
+
+        System.out.println(greetingLine + horizontalLine);
+        System.out.println(exitLine + horizontalLine);
     }
 }
