faad7c5c12bebba9c354c0dfc47527b5a75cba79
Leo Zheng Rui Darren <leo.darren@gmail.com>
2023-01-19 13:03:16 +0800

    Add greet and exit messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String Greeting = " Hello! I'm Duke\n";
+        String Prompt = " What can I do for you?\n";
+        String Farewell = " Bye. Hope to see you again soon!\n";
+        String line = "____________________________________________________________\n";
+        System.out.println(line + Greeting + Prompt + line + Farewell + line);
     }
 }
