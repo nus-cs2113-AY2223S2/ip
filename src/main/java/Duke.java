64820df7ed59a13f5c940cd41afedf1c476d2d9c
Yc-Michael <e0725397@u.nues.edu>
2023-01-19 15:45:23 +0800

    Add Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,6 +15,7 @@ public class Duke {
         System.out.println(line);
         Scanner in = new Scanner(System.in);
         ArrayList<String> arr = new ArrayList<String>();
+
         String input;
         while(in.hasNext()){
             input = in.nextLine();
@@ -25,12 +26,37 @@ public class Duke {
             if(input.equals("list")){
                 System.out.println(line);
                 for(int i = 0;i<arr.size();i++){
-                    System.out.print(i+1+". ");
+                    System.out.print(i+1+".");
+
                     System.out.println(arr.get(i));
                 }
 
-            } else{
-                arr.add(input);
+            }else if(input.contains("unmark")){
+                System.out.println(line);
+                System.out.println("OK, I've marked this task as not done yet:");
+                Integer index = new Integer(0);
+                String [] IndexArr = input.split(" ",2);
+                index = Integer.parseInt(IndexArr[1])-1;
+                String name = arr.get((int)index);
+                arr.remove((int)index);
+                String [] NameArr = name.split(" ",2);
+                arr.add(index,"[ ] "+NameArr[1]);
+                System.out.println(arr.get(index));
+            }else if(input.contains("mark")){
+                Integer index = new Integer(0);
+                String [] IndexArr = input.split(" ",2);
+                System.out.println(line);
+                System.out.println("Nice! I've marked this task as done:");
+                index = Integer.parseInt(IndexArr[1])-1;
+                String name = arr.get((int)index);
+                Task tasking = new Task(name,true);
+                arr.remove((int)index);
+                String [] NameArr = name.split(" ",3);
+                arr.add(index,tasking.getStatusIcon()+NameArr[2]);
+                System.out.println(arr.get(index));
+            }else{
+
+                arr.add("[ ] "+input);
                 System.out.println(line);
                 System.out.println("added: "+input);
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+    protected String name;
+    protected boolean isDone;
+
+    public Task(String name, boolean isDone) {
+        this.name = name;
+        this.isDone = isDone;
+    }
+
+    public Task() {
+    }
+
+    public String getStatusIcon(){
+        return(isDone?"[X] ":" ");
+    }
+
+
+}
