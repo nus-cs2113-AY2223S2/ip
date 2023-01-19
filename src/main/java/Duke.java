5298354645a43b4d498245180b0f7e6f413c5ed4
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2023-01-19 10:40:19 +0800

    Level 0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
     public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+
+        String welcome = " Hello! I'm Duke\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________";
+        System.out.println(welcome);
+
+        String goodbye = " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(goodbye);
     }
 }
