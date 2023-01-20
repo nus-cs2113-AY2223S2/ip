a12b12551ebd196f3310f3d196c2868b6fdf937c
ArtemiszenN <artemiszenn@gmail.com>
2023-01-21 02:35:16 +0800

    Add list and add functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
@@ -9,16 +10,25 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|";
         System.out.println(line+'\n'+logo+'\n'+line);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?\n"+line);
-        //System.out.println("Bye. Hope to see you again soon!\n"+line);
         Scanner in = new Scanner(System.in);
         String command="";
+        ArrayList<String>textList = new ArrayList<String>();
         while(true){
             command = in.nextLine();
             if(command.equals("bye")){
                 System.out.println(line+'\n'+"Bye. Hope to see you again soon!"+'\n'+line);
                 return;
             }
-            System.out.println(line+'\n'+command+'\n'+line);
+            if(command.equals("list")){
+                System.out.println(line);
+                for(int i=0;i<textList.size();i++){
+                    System.out.println(Integer.toString(i+1)+". "+textList.get(i));
+                }
+                System.out.println(line);
+            }else{
+                textList.add(command);
+                System.out.println(line+'\n'+"added: "+command+'\n'+line);
+            }
         }
     }
 }
