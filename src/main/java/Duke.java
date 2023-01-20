c50ca35b259c9c1e012ce8b6289d9a0fc0bf29a4
Thiolk <thiolengkiat@gmail.com>
2023-01-20 21:54:34 +0800

    Implement addToList method and update checkInput method
    The current bot can only echo user inputs
    
    Current level requires bot to store user's inputs
    
    addToList method allows the bot to store user inputs in an ArrayList
    
    ArrayList was chosen as the list can be resized easily
    
    checkInputs method currently only checked for 2 kinds of inputs
    
    The current level requires to bot to be able to perform 3 actions
    
    if else statements have been added to check for the 3 kinds of inputs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,28 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void printHorizontalLine() {
         System.out.println("____________________________________________________________");
     }
 
-    public static void checkInput(String input) {
+    public static int checkInput(String input) {
         if (input.equals("bye")) {
-            return;
+            return 0;
+        } else if (input.equals("list")) {
+            return 1;
+        } else {
+            return 2;
         }
-        System.out.println(input);
+    }
+    public static void addToList(String input, ArrayList<String> listOfInputs) {
+        System.out.println("added: "+input);
+        listOfInputs.add(input);
         printHorizontalLine();
     }
 
+    public static void listInputs() {
+
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -24,11 +35,21 @@ public class Duke {
         System.out.println("What can I do for you?");
         printHorizontalLine();
         String line;
+        ArrayList<String> listOfInputs = new ArrayList<>();
         do {
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
             printHorizontalLine();
-            checkInput(line);
+            int instruction = checkInput(line);
+            if (instruction == 1) {
+                addToList(line, listOfInputs);
+            }
+            else if (instruction == 2) {
+                listInputs();
+            }
+            else {
+                break;
+            }
         } while (!line.equals("bye"));
         System.out.println("Bye. Hope to see you again soon!");
         printHorizontalLine();
