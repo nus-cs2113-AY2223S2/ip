c9aa4f9f4bc96accb55c0f3c8096c36d3478313f
EthanYidong <ethanyidong@gmail.com>
2023-01-20 12:45:10 +0800

    Add greeting and exit messages

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,13 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String divider = "____________________________________________________________";
         System.out.println("Hello from\n" + logo);
+        System.out.println(divider);
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+        System.out.println(divider);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(divider);
     }
 }
