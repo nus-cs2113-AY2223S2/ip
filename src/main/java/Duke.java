8569267a4bf253fa0ecacff758607355807c1dba
Vignesh-30 <30vignesh@gmail.com>
2023-01-20 21:42:10 +0800

    Level 1 added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
     static String line = "\t____________________________________________________________";
+    static String command_Bye = "bye";
 
     public static void Greet() {
         System.out.println(line);
@@ -8,13 +11,23 @@ public class Duke {
         System.out.println(line);
     }
 
-    public static void  Bye() {
+    public static void Bye() {
+        System.out.println(line);
         System.out.println("\tBye. Hope to see you again soon!\n");
         System.out.println(line);
     }
 
     public static void main(String[] args) {
         Greet();
+        Scanner in = new Scanner(System.in);
+        String user_command = new String();
+        user_command = in.nextLine();
+        while (!user_command.equals(command_Bye)) {
+            System.out.println(line);
+            System.out.println("\t" + user_command);
+            System.out.println(line);
+            user_command = in.nextLine();
+        }
         Bye();
     }
 }
