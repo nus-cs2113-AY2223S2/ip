06f2a89fef15ac5fd2096772cfd6ba363f77c8c9
geeeetyx <e0725682@u.nus.edu>
2023-01-20 15:03:55 +0800

    fix: deleted unwanted files

--- src/main/java/Week2/helloworld.java
+++
@@ -1,9 +0,0 @@
-package Week2;
-
-public class helloworld {
-
-    public static void main(String[] args) {
-        // generate some simple output
-        System.out.println("Hello, World!");
-    }
-}
