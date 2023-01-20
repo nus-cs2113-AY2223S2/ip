f7a9097d597c51184c1acafa71a66f715e582360
ArtemiszenN <artemiszenn@gmail.com>
2023-01-21 03:16:32 +0800

    Add task class Add mark as done functionality Add personality matrix

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,31 +3,104 @@ import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|";
+        String invalidCommandPrinter = "One hour of lifespan has been deducted, in accordance with our Terms and Services.";
+        String validCommandPrinter = "Command acknowledged. Reducing user lifespan by 30 minutes.";
+        String logo = "    // | |     //   ) )  // | |  \\\\ / / \\\\    / / //   ) ) \n"
+                + "   //__| |    //___/ /  //__| |   \\  /   \\\\  / / ((        \n"
+                + "  / ___  |   / ___ (   / ___  |   / /     \\\\/ /    \\\\      \n"
+                + " //    | |  //   | |  //    | |  / /\\\\     / /       ) )   \n"
+                + "//     | | //    | | //     | | / /  \\\\   / / ((___ / /    ";
         System.out.println(line+'\n'+logo+'\n'+line);
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n"+line);
+        System.out.println("Welcome to Araxys Systems, the only system powered by LifeForce™");
+        System.out.println("How may I help you today?");
+        System.out.println(line);
         Scanner in = new Scanner(System.in);
         String command="";
-        ArrayList<String>textList = new ArrayList<String>();
+        ArrayList<Task>taskList = new ArrayList<Task>();
         while(true){
             command = in.nextLine();
+            command = command.trim();
             if(command.equals("bye")){
-                System.out.println(line+'\n'+"Bye. Hope to see you again soon!"+'\n'+line);
+                System.out.println(line+'\n'+"Goodbye. To reach customer service, just look outside your window."+'\n'+line);
                 return;
             }
             if(command.equals("list")){
                 System.out.println(line);
-                for(int i=0;i<textList.size();i++){
-                    System.out.println(Integer.toString(i+1)+". "+textList.get(i));
+                System.out.println(validCommandPrinter);
+                for(int i=0;i<taskList.size();i++){
+                    System.out.print(Integer.toString(i+1)+":["+taskList.get(i).getStatusIcon()+"]///////");
+                    System.out.println(taskList.get(i).getDescription());
                 }
                 System.out.println(line);
             }else{
-                textList.add(command);
-                System.out.println(line+'\n'+"added: "+command+'\n'+line);
+                String[] commandList = command.split(" ");
+                if(commandList[0].equals("mark")){
+                    if (commandList.length!=2){
+                        System.out.println(line);
+                        System.out.println("Please only input \"mark\" followed by an integer.");
+                        System.out.println(invalidCommandPrinter);
+                        System.out.println(line);
+                        continue;
+                    }
+                    int index;
+                    try{
+                        index = Integer.parseInt(commandList[1]);
+                    } catch (NumberFormatException numberFormatException){
+                        System.out.println(line+'\n'+"Sorry, index is not numeric."+'\n'+line);
+                        System.out.println(invalidCommandPrinter);
+                        continue;
+                    }
+                    index--;
+                    if(index<0||index>=taskList.size()){
+                        System.out.println(line+'\n'+"Sorry, index is invalid."+'\n'+line);
+                        System.out.println(invalidCommandPrinter);
+                    }else if(taskList.get(index).isDone()){
+                        System.out.println(line+'\n'+"The task specified is already marked."+'\n'+line);
+                        System.out.println(invalidCommandPrinter);
+                    }else{
+                        taskList.get(index).setDone(true);
+                        System.out.println(line);
+                        System.out.println(validCommandPrinter);
+                        System.out.println("Task has been marked as: completed");
+                        System.out.println("[X] "+taskList.get(index).getDescription());
+                        System.out.println(line);
+                    }
+                }else if(commandList[0].equals("unmark")){
+                    if (commandList.length!=2){
+                        System.out.println(line);
+                        System.out.println("Please only input \"unmark\" followed by an integer.");
+                        System.out.println(invalidCommandPrinter);
+                        System.out.println(line);
+                        continue;
+                    }
+                    int index;
+                    try{
+                        index = Integer.parseInt(commandList[1]);
+                    } catch (NumberFormatException numberFormatException){
+                        System.out.println(line+'\n'+"Sorry, index is not numeric."+'\n'+line);
+                        System.out.println(invalidCommandPrinter);
+                        continue;
+                    }
+                    index--;
+                    if(index<0||index>=taskList.size()){
+                        System.out.println(line+'\n'+"Sorry, index is invalid."+'\n'+line);
+                        System.out.println(invalidCommandPrinter);
+                    }else if(!taskList.get(index).isDone()){
+                        System.out.println(line+'\n'+"The task specified is already unmarked."+'\n'+line);
+                        System.out.println(invalidCommandPrinter);
+                    }else{
+                        taskList.get(index).setDone(false);
+                        System.out.println(line);
+                        System.out.println(validCommandPrinter);
+                        System.out.println("Task has been marked as: not completed");
+                        System.out.println("[ ] "+taskList.get(index).getDescription());
+                        System.out.println(line);
+                    }
+                }else{
+                    taskList.add(new Task(command));
+                    System.out.println(validCommandPrinter);
+                    System.out.println(line+'\n'+"New task has been added: "+command+'\n'+line);
+                }
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone() ? "X" : " "); // mark done task with X
+    }
+
+    //...
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+}
