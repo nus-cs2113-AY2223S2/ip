3468fd0e75f82d2a87cb0641f21140f127cbd141
Varnika Srivastava <>
2023-01-20 22:25:05 +0800

    Add Increment: Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner in = new Scanner(System.in);
+        printLine();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        printLine();
+        String line;
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            printLine();
+            System.out.println(line);
+            printLine();
+            line = in.nextLine();
+        }
+        printLine();
         System.out.println("Bye. Hope to see you again soon!");
-
+        printLine();
     }
 }
