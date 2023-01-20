4070a94e5b8804a0d60f86fa6804d10438159d9a
L0Z1K <qortmddbs731@naver.com>
2023-01-20 21:51:02 +0800

    Feat: Mark whether task is done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,6 +18,7 @@ public class Duke {
 
         // Echoes user input until "bye" is entered
         String[] tasks = new String[100];
+        boolean[] done = new boolean[100];
         String userInput = "";
         while (true) {
             Scanner in = new Scanner(System.in);
@@ -26,14 +27,30 @@ public class Duke {
                 break;
             } else if (userInput.equals("list")) {
                 System.out.println("    --------------------------------------------------");
+                System.out.println("    Here are the tasks in your list:");
                 for (int i = 0; i < 100; i++) {
+                    char doneChar = done[i] ? 'X' : ' ';
                     if (tasks[i] != null) {
-                        System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                        System.out.println("    " + (i + 1) + ".[" + doneChar + "] " + tasks[i]);
                     } else {
                         break;
                     }
                 }
                 System.out.println("    --------------------------------------------------");
+            } else if (userInput.startsWith("mark")) {
+                int taskNo = Integer.parseInt(userInput.substring(5));
+                System.out.println("    --------------------------------------------------");
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println("      [X] " + tasks[taskNo - 1]);
+                System.out.println("    --------------------------------------------------");
+                done[taskNo - 1] = true;
+            } else if (userInput.startsWith("unmark")) {
+                int taskNo = Integer.parseInt(userInput.substring(7));
+                System.out.println("    --------------------------------------------------");
+                System.out.println("    I've marked this task as not done yet:");
+                System.out.println("      [ ] " + tasks[taskNo - 1]);
+                System.out.println("    --------------------------------------------------");
+                done[taskNo - 1] = false;
             } else {
                 System.out.println("    --------------------------------------------------");
                 System.out.println("     added: " + userInput);
@@ -41,6 +58,7 @@ public class Duke {
                 for (int i = 0; i < 100; i++) {
                     if (tasks[i] == null) {
                         tasks[i] = userInput;
+                        done[i] = false;
                         break;
                     }
                 }
