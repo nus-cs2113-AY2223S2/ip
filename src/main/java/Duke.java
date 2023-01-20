d57960576409ad637cac3b71541d65d8e5849d08
irving11119 <irving.deboer@gmail.com>
2023-01-20 22:37:32 +0800

    Add Level-1 increments to skeletal code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String line = "";
+
         String lineBreak = "____________________________________________________________\n";
-        System.out.println(lineBreak + "Hello I'm Duke!\nWhat can I do for you?\n"
-                + lineBreak + "Bye. Hope to see you again soon!\n" + lineBreak);
+        System.out.println(lineBreak + "Hello I'm Duke!\nWhat can I do for you?\n" + lineBreak);
+
+        while (!line.equals("bye")){
+            line = in.nextLine();
+            System.out.println(line + "\n" + lineBreak);
+        }
+
+        System.out.println("Bye. Hope to see you again soon!\n" + lineBreak);
     }
 }
