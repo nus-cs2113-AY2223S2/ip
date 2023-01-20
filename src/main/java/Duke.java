314c0fffdd688dd81efbd24fb3bb42ca5c7dca36
geeeetyx <e0725682@u.nus.edu>
2023-01-20 14:56:02 +0800

    Revert "feat: increment of level one"
    This reverts commit 4c8d1067be5e9225a88aa29bc6f9cc15b4f421db.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,26 +7,14 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        System.out.println("    ____________________________________");
-        System.out.println("    Hello! I'm Duke");
-        System.out.println("    What can I do for you?");
-        System.out.println("    ____________________________________");
 
-        Scanner userInput = new Scanner(System.in);
-        while (true) {
-            String text = userInput.nextLine();
-            if ("bye".equalsIgnoreCase(text)) {
-                break;
-            }
-            System.out.println("    ____________________________________");
-            System.out.println("    " + text);
-            System.out.println("    ____________________________________");
-        }
+        //level-0
+        System.out.println("____________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?\n");
 
-        System.out.println("    ____________________________________");
-        System.out.println("    Bye. Hope to see you again soon!");
-        System.out.println("    ____________________________________");
-
-
+        System.out.println("____________________________________");
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("____________________________________");
     }
 }
