da03ebdb89f20ba91b0c7811281bb5b39d1ba621
jaredoong <jaredoong@gmail.com>
2023-01-20 21:33:32 +0800

    Add echo functionality to Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,49 @@
+import java.util.Scanner;
 public class Duke {
+
+    public static boolean isCompleted = false;
 
     public static void welcomeMessage() {
         separator();
-        System.out.println(" Hello! I'm Duke");
-        System.out.println(" What can I do for you?");
+        formatMessage("Hello! I'm Duke");
+        formatMessage("What can I do for you?");
+        separator();
     }
 
     public static void endingMessage() {
         separator();
-        System.out.println(" Bye. Hope to see you again soon!");
+        formatMessage("Bye. Hope to see you again soon!");
+        separator();
     }
 
     public static void separator() {
         System.out.println("____________________________________________________________");
     }
 
+    public static void formatMessage(String message) {
+        String outputMessage = String.format("| %-57s|", message);
+        System.out.println(outputMessage);
+    }
+
+    public static void checkInput(String inputMessage) {
+        if (inputMessage.equalsIgnoreCase("bye")) {
+            isCompleted = true;
+            endingMessage();
+        } else {
+            separator();
+            formatMessage(inputMessage);
+            separator();
+        }
+    }
 
     public static void main(String[] args) {
         welcomeMessage();
-        endingMessage();
-        separator();
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        do {
+            line = in.nextLine();
+            checkInput(line);
+        } while (!isCompleted);
     }
 }
