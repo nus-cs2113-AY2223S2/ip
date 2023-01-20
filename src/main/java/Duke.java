b1148b6035e642bd40773257c099386d36368360
L0Z1K <qortmddbs731@naver.com>
2023-01-20 21:44:05 +0800

    Feat: Add list for storing todo and show them

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,18 +17,35 @@ public class Duke {
         System.out.println("    --------------------------------------------------\n");
 
         // Echoes user input until "bye" is entered
+        String[] tasks = new String[100];
         String userInput = "";
         while (true) {
             Scanner in = new Scanner(System.in);
             userInput = in.nextLine();
             if (userInput.equals("bye")) {
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println("    --------------------------------------------------");
+                for (int i = 0; i < 100; i++) {
+                    if (tasks[i] != null) {
+                        System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                    } else {
+                        break;
+                    }
+                }
+                System.out.println("    --------------------------------------------------");
+            } else {
+                System.out.println("    --------------------------------------------------");
+                System.out.println("     added: " + userInput);
+                System.out.println("    --------------------------------------------------\n");
+                for (int i = 0; i < 100; i++) {
+                    if (tasks[i] == null) {
+                        tasks[i] = userInput;
+                        break;
+                    }
+                }
             }
-            System.out.println("    --------------------------------------------------");
-            System.out.println("    " + userInput);
-            System.out.println("    --------------------------------------------------\n");
         }
-
         System.out.println("    -----------------------------------");
         System.out.println("    Bye! See you later!");
         System.out.println("    -----------------------------------");
