430b8897c92743eb1059210c3d6e8719d7523d4d
Vignesh-30 <30vignesh@gmail.com>
2023-01-20 21:30:32 +0800

    Added indent and improved lines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,16 @@
 public class Duke {
+    static String line = "\t____________________________________________________________";
 
     public static void Greet() {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?\n");
-        System.out.println("____________________________________________________________");
+        System.out.println(line);
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?\n");
+        System.out.println(line);
     }
 
     public static void  Bye() {
-      //  System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("____________________________________________________________");
+        System.out.println("\tBye. Hope to see you again soon!\n");
+        System.out.println(line);
     }
 
     public static void main(String[] args) {
