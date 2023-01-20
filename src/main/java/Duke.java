6b8c509d6fc9a66c37435a0382945c60d6cc0dfb
anthea-pr0g <antheayip02@gmail.com>
2023-01-20 23:18:08 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -11,10 +12,46 @@ public class Duke {
         String greeting="____________________________________________________________\n"
                 +" Hello! I'm Duke\n"
                 +" What can I do for you?\n"
-                +"____________________________________________________________\n"
-                +" Bye. Hope to see you again soon!\n"
-                +"____________________________________________________________\n";
+                + "____________________________________________________________\n";
 
         System.out.println(greeting);
+        Scanner in=new Scanner(System.in);
+        String[] list= new String[100];
+        int i=0;
+        while(true)
+        {
+            String line=in.nextLine();
+            if(line.equalsIgnoreCase("bye"))
+            {
+                break;
+            }
+            list[i]=line;
+
+            if(line.equalsIgnoreCase("list"))
+            {
+                System.out.println("____________________________________________________________");
+                for(int m=0;m<i;m+=1)
+                {
+                    int index=m+1;
+                    System.out.println(index + ". " + list[m] );
+                }
+                System.out.println( "____________________________________________________________");
+
+            }
+            else {
+                i += 1;
+
+                System.out.println("____________________________________________________________\n" + "added: " + line);
+                System.out.println("____________________________________________________________\n");
+            }
+
+        }
+
+
+            System.out.println("____________________________________________________________\n"
+                    +" Bye. Hope to see you again soon!\n"
+                    +"____________________________________________________________\n");
+
+
     }
 }
