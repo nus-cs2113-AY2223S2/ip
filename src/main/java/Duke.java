d519c132482b1fd6e73f10d9e16e01f2783ed6b4
Varnika Srivastava <>
2023-01-20 22:39:54 +0800

    Add Increment: Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,12 +12,24 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         printLine();
+        String[] tasks = new String[100];
+        int counter = 0;
         String line;
         line = in.nextLine();
         while (!line.equals("bye")) {
-            printLine();
-            System.out.println(line);
-            printLine();
+            if (!line.equals("list")) {
+                tasks[counter] = line;
+                counter++;
+                printLine();
+                System.out.println("added: " + line);
+                printLine();
+            } else {
+                printLine();
+                for (int i = 0; i < counter; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+                printLine();
+            }
             line = in.nextLine();
         }
         printLine();
