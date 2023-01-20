f0c77283c284bb2c0411f107880d70243741ad56
Thiolk <thiolengkiat@gmail.com>
2023-01-20 21:08:23 +0800

    Add horizontal line method and implement in level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
 public class Duke {
+    public static void printHorizontalLine() {
+        System.out.println("____________________________________________________________");
+        return;
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,8 +11,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printHorizontalLine();Line();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        printHorizontalLine();
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
