875214dc0ce0e5295afe4aba0c63d1e588e5a8d3
tliangac <tliangac@connect.ust.hk>
2023-01-20 14:58:25 +0800

    Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ public class Duke {
     public static void main(String[] args) {
         String lineBreak = "-----------------";
         System.out.println(lineBreak + '\n' + "Hello! I'm Duke" + '\n' + "What can I do for you?" + '\n');
-        System.out.println(lineBreak + '\n' + "Bye. Hope to see you again soon!" + '\n' + '\n' + lineBreak);
+        System.out.println(lineBreak + '\n' + "Bye. Hope to see you again soon!" + '\n' + lineBreak);
 
     }
 }
