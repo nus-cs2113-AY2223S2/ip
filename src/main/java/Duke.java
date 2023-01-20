3aa24f2eefabf3ade9382bbdeba14da91846a11c
mitch malvin <e0773911@u.nus.edu>
2023-01-21 00:19:19 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String line = "___________________________________________"
+        System.out.println(line);
+        System.out.println("Hello I'm Duke, your personal chatbot.");
+        System.out.println("Is there anything I can do for you");
+        System.out.println(line);
+        System.out.println("Bye! Do let me know if you need any further assistance");
     }
 }
