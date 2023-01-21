ca7b584b5b406e4980add84e49a2249a06d8be6a
irving11119 <irving.deboer@gmail.com>
2023-01-21 20:34:57 +0800

    Add multiple increments for week 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,32 +5,60 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         String line;
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int indexCount = 0;
+        boolean isRunning = true;
         String lineBreak = "____________________________________________________________\n";
         System.out.println(lineBreak + "Hello I'm Duke!\nWhat can I do for you?\n" +
                 "Input your tasks and I'll keep track of them!\n" + lineBreak);
 
         do {
-            System.out.print("Enter Your Task Here: ");
+            System.out.print("Enter Your Task/Command Here: ");
             line = in.nextLine();
             System.out.println(lineBreak);
-            if (line.equals("")){
-                System.out.println("Please input a valid task!\n" + lineBreak);
-            } else if (line.equals("bye")){
-                break;
+
+            if (line.equals("bye")) {
+                System.out.println("Aww you're going? Hope to see you again soon!\n" + lineBreak);
+                isRunning = false;
             } else {
-                tasks[indexCount++] = line;
+                String command = line.contains(" ") ? line.split(" ")[0] : line;
+                switch (command) {
+                case "":
+                    System.out.println("Please input a task!\n" + lineBreak);
+                    break;
 
-                for (int i = 0; i < indexCount; i++){
-                    System.out.println(i+1 + ". " + tasks[i]);
+                case "list":
+                    for (int i = 0; i < indexCount; i++) {
+                        String check = (tasks[i].isDone()) ? "X" : " ";
+                        System.out.println(i + 1 + ". [" + check + "] " + tasks[i].getTaskName());
+                    }
+                    System.out.println(lineBreak);
+                    break;
+
+                case "mark":
+                    String done = line.split(" ")[1];
+                    int index = Integer.parseInt(done) - 1;
+                    tasks[index].setDone(true);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  [X] " + tasks[index].getTaskName() + "\n" + lineBreak);
+                    break;
+
+                case "unmark":
+                    String undone = line.split(" ")[1];
+                    int index1 = Integer.parseInt(undone) - 1;
+                    tasks[index1].setDone(false);
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  [ ] " + tasks[index1].getTaskName() + "\n" + lineBreak);
+                    break;
+
+                default:
+                    Task newTask = new Task(line);
+                    tasks[indexCount++] = newTask;
+                    System.out.println("\"" + line + "\" has been added to the list\n" + lineBreak);
                 }
-
-                System.out.println(lineBreak);
             }
 
-        } while (true);
+        } while (isRunning);
 
-        System.out.println("Aww you're going? Hope to see you again soon!\n" + lineBreak);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String taskName;
+    private boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+}
