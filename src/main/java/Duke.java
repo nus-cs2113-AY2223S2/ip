c4588b1ac9e0ae61976947e1eaec64c4c3302f58
JeraldChen <jeraldchenjunhan@gmail.com>
2023-01-21 16:22:04 +0800

    Completed Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
     public static void main(String[] args) {
         String line;
         String line2;
+        String[] textList = new String[100];
+        int textListCount = 0;
         Scanner in = new Scanner(System.in);
+
         System.out.println("______________________________________________________________");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -18,8 +22,15 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("______________________________________________________________");
                 return;
-            } else {
-                System.out.println(line);
+            } else if (line2.equals("list")) { //if input is list
+                for (int i = 0; i < textListCount; ++i) {
+                    System.out.println(i+1 + ". " + textList[i]);
+                }
+                System.out.println("______________________________________________________________");
+            } else { //if input is any other text
+                textList[textListCount] = line;
+                textListCount++;
+                System.out.println("added: " + line);
                 System.out.println("______________________________________________________________");
             }
         }
