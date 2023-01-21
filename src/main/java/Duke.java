17c1e4ffa7298dce20f8c6132bf73c419fbfc589
Ng-YZ <e0772909@u.nus.edu>
2023-01-21 20:04:04 +0800

    Level 0 Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
+
+    public static void greet() {
+        String line = "____________________";
+        System.out.println(line);
+        System.out.println("Hello! i'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+        //System.out.println("Hello from\n" + logo);
+        greet();
     }
 }
