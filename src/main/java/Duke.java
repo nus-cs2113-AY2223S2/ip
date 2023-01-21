5bb404b066e9fbd7de324f3784141a983b2be011
Mustafa Anis Hussain <mustafahussain793@gmail.com>
2023-01-21 10:31:55 +0800

    Add greetings and echoes commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("\u2500".repeat(50));
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println("\u2500".repeat(50));
+
+        Scanner myObj = new Scanner(System.in);
+
+        String bye = "bye";
+        boolean isSame = false;
+
+        while (!isSame) {
+            String task = myObj.nextLine();
+            if (task.equals(bye)){
+                System.out.println("\u2500".repeat(50));
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("\u2500".repeat(50));
+                isSame = true;
+            }
+            else {
+                System.out.println("\u2500".repeat(50));
+                System.out.println(task); // Output user input
+                System.out.println("\u2500".repeat(50));
+            }
+        }
     }
 }
