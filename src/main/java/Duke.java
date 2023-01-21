ce5997e658ea45737ef156358bc5488051be7bc1
JuneNita <junenita@gmail.com>
2023-01-21 12:31:15 +0800

    Add greet, echo, exit in Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void printDuke() {
         String logo = " ____        _        \n"
@@ -8,18 +10,41 @@ public class Duke {
         System.out.println("Hello from\n" + logo + "\n");
     }
 
-    // Greets the users and exits
-    public static void greet() {
-        String breakLine = "------------------------------------------\n";
-        System.out.println(breakLine +
-                "Hello! I'm Duke\n" +
-                "What can I do for you?\n" +
-                breakLine +
-                "Bye. Hope to see you again soon!\n" +
-                breakLine);
+    public static String breakLine() {
+        return "------------------------------------------\n";
     }
+
+    // Greet
+    public static void greet() {
+        System.out.println(breakLine() + "Hello! I'm Lolo :)\n" + "What can I do for you?\n" + breakLine());
+    }
+
+    // echo string
+    public static void echo(String text) {
+        System.out.print(breakLine() + text + '\n' + breakLine());
+    }
+
+    // exit
+    public static void exit() {
+        System.out.print(breakLine() + "Ba-bye. Hope to see you again soon :)\n" + breakLine());
+    }
+
+    // read input
+    public static String readInput() {
+        System.out.print(">> ");
+        Scanner in = new Scanner(System.in);
+        return in.nextLine();
+    }
+
     public static void main(String[] args) {
         printDuke();
         greet();
+        String input = readInput();
+        while (!input.equals("bye")) {
+            echo(input);
+            input = readInput();
+        }
+        exit();
     }
+
 }
