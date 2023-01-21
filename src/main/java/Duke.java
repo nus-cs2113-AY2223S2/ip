aeb1d8dfe79107408b42d284d213e0b9943ef58e
Jin Xuan Ow Yong <owyongjinxuan@gmail.com>
2023-01-21 17:29:25 +0800

    Implement echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
 public class Duke {
+    // commands
+    static final String COMMAND_EXIT = "bye";
+
     private static void printLine() {
         System.out.println("____________________________________________________________");
     }
@@ -15,7 +20,7 @@ public class Duke {
 
     private static void greet() {
         System.out.println("Hello! I'm Duke\n"
-                         + "What can I do for you?");
+                + "What can I do for you?");
         printLine();
     }
 
@@ -24,9 +29,37 @@ public class Duke {
         printLine();
     }
 
+    private static void echo(String s) {
+        System.out.println(s);
+        printLine();
+    }
+
+    private static int processInput(String s) {
+        Scanner input = new Scanner(s);
+        String command = input.next();
+        if (command.equals(COMMAND_EXIT)) {
+            return -1;
+        }
+        echo(s);
+        return 0;
+    }
+
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         printLogo();
         greet();
-        exit();
-   }
+
+        String input;
+        do {
+            input = scan.nextLine();
+            printLine();
+            int state = processInput(input);
+            if (state == -1) {
+                exit();
+                break;
+            }
+        } while (true);
+
+        scan.close();
+    }
 }
