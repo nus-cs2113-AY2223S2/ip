7395932517da7604704acce364b6cff3f3f3c05b
pinyoko573 <pinyoko573@gmail.com>
2023-01-21 13:35:45 +0800

    Add Add, List functionality
    Store text entered by user and display back

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     static final String line = "____________________________________________________________";
@@ -10,16 +11,22 @@ public class Duke {
         System.out.println(line);
 
         Scanner in = new Scanner(System.in);
+        ArrayList<String> items = new ArrayList<String>();
         while (true) {
             String input = in.nextLine();
 
             System.out.println(line);
-            if (input.equals("bye")) {
+            if (input.equals("list")) {
+                for (int i = 0; i < items.size(); i++) {
+                    System.out.println((i+1)+". "+items.get(i));
+                }
+            } else if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
             } else {
-                System.out.println(input);
+                items.add(input);
+                System.out.println("added: "+input);
             }
             System.out.println(line);
         }
