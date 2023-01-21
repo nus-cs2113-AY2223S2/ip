b08b19d4848c106f7030f1d10f4227d84b2ecf8f
nicholas132000 <beefnlemon@gmail.com>
2023-01-21 12:47:01 +0800

    Add "Add, List"

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -5,9 +5,22 @@ public class Echo {
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
+        String[] tasks = new String[100];
+        int count = 0;
         while (!(line.equals("Bye") || line.equals("bye"))) {
-            System.out.println(line);
-            line = in.nextLine();
+            if (!line.equals("list")) {
+                System.out.println("added: " + line);
+                tasks[count] = line;
+                ++count;
+                line = in.nextLine();
+            } else {
+                int index = 1;
+                for (int i = 0; i < count; ++i) {
+                    System.out.println(index + ": " + tasks[i]);
+                    ++index;
+                }
+                line = in.nextLine();
+            }
         }
         System.out.println("Bye. Hope to see you again!\n");
     }
