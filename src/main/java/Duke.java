2892f17b403f960c92451d2dfc847b7831e167eb
pinyoko573 <pinyoko573@gmail.com>
2023-01-21 13:17:00 +0800

    Add Greet, Echo, Exit functionality
    Duke greets the user, echos commands and exits when user types bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
+    static final String line = "____________________________________________________________";
     public static void main(String[] args) {
-        String msg = "____________________________________________________________\n"
-                + " Hello! I'm Duke\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
-        System.out.println(msg);
+        // Greet the user
+        System.out.println(line);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String input = in.nextLine();
+
+            System.out.println(line);
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            } else {
+                System.out.println(input);
+            }
+            System.out.println(line);
+        }
+        in.close();
     }
 }
