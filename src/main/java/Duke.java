24bd7dd8fd72907f47170ffdc485f8e412616989
Thomas Jiralerspong <thomasjiralerspong@gmail.com>
2023-01-21 17:05:31 +0800

    Add printing intro and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printIntro();
+        printExit();
+
+    }
+
+    public static void printIntro() {
+        printMessage("Hello! I'm Duke\nWhat can I do for you?");
+    }
+    public static void printExit() {
+        printMessage("Bye. Hope to see you again soon!");
+    }
+    public static void printMessage(String message) {
+        System.out.println(message);
+        printHorizontalLine();
+    }
+    public static void printHorizontalLine() {
+        System.out.println("------------------------------------------");
     }
 }
