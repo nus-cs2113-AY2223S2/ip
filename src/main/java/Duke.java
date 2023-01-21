b47d4e44966927192006d7daf09b1ab9d86c18df
sansders <sean_tay1999@hotmail.com>
2023-01-21 15:07:17 +0800

    Add echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    static final String HORIZONTAL_LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String userInput;
+
+        System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(HORIZONTAL_LINE);
+
+        userInput = scanner.nextLine();
+
+        while (!userInput.equals("bye")) {
+            System.out.println(HORIZONTAL_LINE);
+            System.out.println(userInput);
+            System.out.println(HORIZONTAL_LINE);
+            System.out.println();
+            userInput = scanner.nextLine();
+        }
+
+        exit();
+
+    }
+
+    public static void exit() {
+        System.out.println(HORIZONTAL_LINE);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(HORIZONTAL_LINE);
     }
 }
