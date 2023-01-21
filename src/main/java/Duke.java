713dc745154597d0d4651f61596ea414cc6804e3
pinyoko573 <pinyoko573@gmail.com>
2023-01-21 14:32:48 +0800

    Add Mark as Done functionality and Customized Chatbot
    Add ability to mark tasks as done/not done and customized chatbot to be unique

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,27 +6,48 @@ public class Duke {
     public static void main(String[] args) {
         // Greet the user
         System.out.println(line);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("Hello! I'm your personal shopping checklist.");
+        System.out.println("What items do you need to buy today?");
         System.out.println(line);
 
         Scanner in = new Scanner(System.in);
-        ArrayList<String> items = new ArrayList<String>();
+        ArrayList<Item> items = new ArrayList<Item>();
         while (true) {
-            String input = in.nextLine();
+            try {
+                String input = in.nextLine();
 
-            System.out.println(line);
-            if (input.equals("list")) {
-                for (int i = 0; i < items.size(); i++) {
-                    System.out.println((i+1)+". "+items.get(i));
-                }
-            } else if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
-                break;
-            } else {
-                items.add(input);
-                System.out.println("added: "+input);
+                if (input.equals("list")) {
+                    System.out.println("Here are the items in your shopping list:");
+                    for (int i = 0; i < items.size(); i++) {
+                        Item item = items.get(i);
+                        System.out.println((i+1)+". ["+item.getStatus()+"] "+item.getDescription());
+                    }
+                } else if (input.split(" ")[0].equals("mark")) {
+                    int num = Integer.parseInt(input.split(" ")[1]);
+                    Item item = items.get(num-1);
+                    item.setStatus(true);
+
+                    System.out.println("Good job! I've marked this item as done:");
+                    System.out.println("["+item.getStatus()+"] "+item.getDescription());
+                } else if (input.split(" ")[0].equals("unmark")) {
+                    int num = Integer.parseInt(input.split(" ")[1]);
+                    Item item = items.get(num-1);
+                    item.setStatus(false);
+
+                    System.out.println("OK, I've marked this item as not done yet:");
+                    System.out.println("["+item.getStatus()+"] "+item.getDescription());
+                } else if (input.equals("bye")) {
+                    System.out.println("Bye. Thanks for using me!");
+                    System.out.println(line);
+                    break;
+                } else {
+                    Item newItem = new Item(input);
+                    items.add(newItem);
+                    System.out.println(input+" has been added to your shopping list.");
+                }
+            } catch(Exception e) {
+                System.out.println("Please input a valid command.");
             }
             System.out.println(line);
         }
---
+++ src/main/java/Item.java
@@ -0,0 +1,25 @@
+public class Item {
+    private String description;
+    private boolean isDone;
+
+    public Item (String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return (this.isDone ? "X" : " "); // mark done task with X
+    }
+
+    public void setStatus(boolean isDone) {
+        this.isDone = isDone;
+    }
+}
