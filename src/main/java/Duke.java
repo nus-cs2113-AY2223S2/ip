b7bebad3cf065e7d4f4805b574ca58b2c090673e
darrenangwx <darrenawx@gmail.com>
2023-01-22 01:49:49 +0800

    Add echo function
    The bot now echos what the user types.
    If 'bye' is typed, the bot will exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,6 +11,17 @@ public class Duke {
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+
+
+        Scanner in = new Scanner(System.in);
+        String line = "";
+        while(!line.equals("bye")) {
+            //Take in user input and print out user input.
+            line = in.nextLine();
+            System.out.println(line);
+        }
         System.out.println("Bye. Hope to see you again soon!");
+
+
     }
 }
