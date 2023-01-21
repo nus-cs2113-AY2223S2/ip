c1df4161b7ffe8d3f86b96e8591c457fc45b8c25
denzelcjy <denzel_cjy@hotmail.com>
2023-01-21 18:33:50 +0800

    Add Duke Level 1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void startDuke() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Duke");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void endDuke() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,11 +21,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("    ____________________________________________________________");
-        System.out.println("     Hello! I'm Duke");
-        System.out.println("     What can I do for you?");
-        System.out.println("    ____________________________________________________________");
-        System.out.println("     Bye. Hope to see you again soon!");
-        System.out.println("    ____________________________________________________________");
+        startDuke();
+        Scanner input = new Scanner(System.in);
+        String nextInput = input.nextLine();
+        while (!nextInput.equals("bye")) {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     " + nextInput);
+            System.out.println("    ____________________________________________________________");
+            nextInput = input.nextLine();
+        }
+        endDuke();
     }
+
 }
