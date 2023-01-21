0e0dd712166884f368e81de97fb5649db768e178
YatPang <yatpangleong08@gmail.com>
2023-01-21 18:03:06 +0800

    Add Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,55 @@
 import java.util.Scanner;
-
 public class Duke {
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         String input;
-        String[] lst = new String[100];
-        int index = 0;
+        Task[] tasks = new Task[100];
+        int lstID = 1;
         while (true) {
             Scanner sc = new Scanner(System.in);
             input = sc.nextLine();
-            if (input.equals("bye")) {
+            String[] inputs = input.split(" ");
+            if (inputs[0].equals("bye")) {
                 // terminate
                 System.out.println("Bye. Hope to see you again soon!");
                 return;
             } else if (input.equals("list")) {
-                for (int i = 0; i < index; i++){
-                    System.out.println(i + 1 +". " + lst[i] );
+                lstID = 1;
+                System.out.println("Here are the tasks in your list:");
+                for (Task tsk : tasks) {
+                    if (tsk==null){break;}
+                    System.out.println(lstID + ".["  + tsk.getStatusIcon() + "] "+ tsk.description);
+                    lstID += 1;
                 }
+                lstID = 1;
+            } else if(inputs[0].equals("unmark")){
+                if (Integer.parseInt(inputs[1]) >= lstID) {
+                    System.out.println("Error 404: Item not found!");
+                    continue;
+                }
+                tasks[Integer.parseInt(inputs[1]) - 1].markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                Task tsk = tasks[Integer.parseInt(inputs[1]) - 1];
+                System.out.println("[" + tsk.getStatusIcon() + "] " + tsk.description);
+            } else if(inputs[0].equals("mark")){
+                if (Integer.parseInt(inputs[1]) >= lstID) {
+                    System.out.println("Error 404: Item not found!");
+                    continue;
+                }
+                tasks[Integer.parseInt(inputs[1]) - 1].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                Task tsk = tasks[Integer.parseInt(inputs[1]) - 1];
+                System.out.println("[" + tsk.getStatusIcon() + "] " + tsk.description);
+            } else if(inputs[0].equals("add") && inputs[1].equals("task")){
+                Task tsk = new Task(inputs[2]);
+                tasks[lstID-1] = tsk;
+                lstID += 1;
+                System.out.println("Task added!");
             }
-
             else {
                 // echo
-                System.out.println("added: "+ input);
-                lst[index] = input;
-                index += 1;
+                System.out.println(input);
             }
         }
     }
