cfa934dcb9ee2b73bef684a0ea366eb3bc7f54ba
sansders <sean_tay1999@hotmail.com>
2023-01-21 15:07:55 +0800

    Add list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,32 +6,60 @@ public class Duke {
 
     public static void main(String[] args) {
 
+        // Variables needed
         Scanner scanner = new Scanner(System.in);
-
         String userInput;
+        String[] listOfInputs = new String[100];
+        int inputCounter = 0;
+        // Start of the program
+        greet();
 
+        // Get first input
+        userInput = scanner.nextLine();
+
+        // Loop for next inputs
+        while (!userInput.equals("bye")) {
+
+            // Print list upon user request
+            if (userInput.equals("list")) {
+                printList(listOfInputs, inputCounter);
+            }
+
+            // Add other user inputs into list
+            else {
+                System.out.println(HORIZONTAL_LINE);
+                System.out.println("added: " + userInput);
+                listOfInputs[inputCounter] = userInput;
+                inputCounter++;
+            }
+
+            // Print trailing horizontal line and take in next input
+            System.out.println(HORIZONTAL_LINE + "\n");
+            userInput = scanner.nextLine();
+
+        }
+
+        // Exit program
+        exit();
+
+    }
+
+    public static void greet() {
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println(HORIZONTAL_LINE);
-
-        userInput = scanner.nextLine();
-
-        while (!userInput.equals("bye")) {
-            System.out.println(HORIZONTAL_LINE);
-            System.out.println(userInput);
-            System.out.println(HORIZONTAL_LINE);
-            System.out.println();
-            userInput = scanner.nextLine();
-        }
-
-        exit();
-
     }
 
     public static void exit() {
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void printList(String[] listOfInputs, int inputCounter) {
+        for (int i = 0; i < inputCounter; i++) {
+            System.out.println(i + 1 + ". " + listOfInputs[i]);
+        }
     }
 }
