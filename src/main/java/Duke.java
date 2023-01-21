c666271d5697255b73d1863dd83e896a3a7d385e
JeraldChen <jeraldchenjunhan@gmail.com>
2023-01-21 21:52:40 +0800

    Completed Level-3: Mark as Done and added Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ public class Duke {
     public static void main(String[] args) {
         String line;
         String line2;
-        String[] textList = new String[100];
+        Task[] textList = new Task[100]; //store in array of Tasks to track both description and mark status
         int textListCount = 0;
         Scanner in = new Scanner(System.in);
 
@@ -17,19 +17,35 @@ public class Duke {
         while(true) {
             line = in.nextLine();
             line2 = line.toLowerCase();
+            Task t = new Task(line);
             System.out.println("______________________________________________________________");
             if (line2.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("______________________________________________________________");
                 return;
             } else if (line2.equals("list")) { //if input is list
+                System.out.println("Here are the tasks in your list: ");
                 for (int i = 0; i < textListCount; ++i) {
-                    System.out.println(i+1 + ". " + textList[i]);
+                    String listLine = i+1 + ". " + "[" + textList[i].getStatusIcon() + "] " + textList[i].description; //display x or blank space depending if element is marked or unmarked
+                    System.out.println(listLine);
                 }
                 System.out.println("______________________________________________________________");
-            } else { //if input is any other text
-                textList[textListCount] = line;
-                textListCount++;
+            } else if (line2.contains("mark") && !line2.contains("unmark")) { //mark is in unmark so need to check like this to avoid unmark being detected as mark
+                int indexOfTask = Integer.parseInt(line2.substring(5)) - 1; //find index of the task number starting from 0
+                textList[indexOfTask].markAsDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("  [" + textList[indexOfTask].getStatusIcon() + "] " + textList[indexOfTask].description);
+                System.out.println("______________________________________________________________");
+            } else if (line2.contains("unmark")) {
+                int indexOfTask = Integer.parseInt(line2.substring(7)) - 1;
+                textList[indexOfTask].unmarkAsDone();
+                System.out.println("OK, I've marked this task as not done yet: ");
+                System.out.println("  [" + textList[indexOfTask].getStatusIcon() + "] " + textList[indexOfTask].description);
+                System.out.println("______________________________________________________________");
+            }
+            else { //if input is any other text
+                textList[textListCount] = t; //add all input except for list, bye, mark and unmark to array of tasks
+                textListCount++; //counter to help input tasks into correct index in tasks array
                 System.out.println("added: " + line);
                 System.out.println("______________________________________________________________");
             }
