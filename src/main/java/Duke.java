e55841970b4a984d487f64e46b90b6e4c0946acd
EangJS <eugeneang654871@gmail.com>
2023-01-21 17:41:58 +0800

    Add Increment: Level-1 (Greet, Echo, Exit)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
 
     /**
@@ -5,8 +6,8 @@ public class Duke {
      */
     public static void greetUser(){
         String greetMessage = "Hello! I'm Duke\n"
-                + "What can I do for you?";
-        String exitMessage = "Bye. Hope to see you again soon!";
+                + "I will echo whatever you type\n"
+                + "Type <bye> to exit";
         System.out.println(greetMessage);
     }
 
@@ -14,17 +15,38 @@ public class Duke {
      * Prints out the exit message to the user
      */
     public static void exitProgram(){
-        String exitMessage = "Bye. Hope to see you again soon!";
+        String byeEmoji = new String(Character.toChars(0x1F44B));
+        String exitMessage = "Bye. Hope to see you again soon!" + byeEmoji;
         System.out.println(exitMessage);
     }
+
+    /**
+     * Prints out the echoed message to user
+     */
+    public static void echoUserInput(String userInput){
+        System.out.println(userInput);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        Scanner input = new Scanner(System.in);
+        String logo = " .----------------.  .----------------.  .----------------.  .----------------. \n" +
+                "| .--------------. || .--------------. || .--------------. || .--------------. |\n" +
+                "| |  ________    | || | _____  _____ | || |  ___  ____   | || |  _________   | |\n" +
+                "| | |_   ___ `.  | || ||_   _||_   _|| || | |_  ||_  _|  | || | |_   ___  |  | |\n" +
+                "| |   | |   `. \\ | || |  | |    | |  | || |   | |_/ /    | || |   | |_  \\_|  | |\n" +
+                "| |   | |    | | | || |  | '    ' |  | || |   |  __'.    | || |   |  _|  _   | |\n" +
+                "| |  _| |___.' / | || |   \\ `--' /   | || |  _| |  \\ \\_  | || |  _| |___/ |  | |\n" +
+                "| | |________.'  | || |    `.__.'    | || | |____||____| | || | |_________|  | |\n" +
+                "| |              | || |              | || |              | || |              | |\n" +
+                "| '--------------' || '--------------' || '--------------' || '--------------' |\n" +
+                " '----------------'  '----------------'  '----------------'  '----------------' ";
         System.out.println("Hello from\n" + logo);
         greetUser();
+        String userInput = "";
+        while(!userInput.equals("bye")){
+            userInput = input.nextLine();
+            echoUserInput(userInput);
+        }
         exitProgram();
     }
 }
