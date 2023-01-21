aebec3bbff38e23552ff1912227d5d262d6f2347
irving11119 <irving.deboer@gmail.com>
2023-01-21 12:07:46 +0800

    Add Level 2 increments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,16 +3,34 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String line = "";
+        String line;
 
+        String[] tasks = new String[100];
+        int indexCount = 0;
         String lineBreak = "____________________________________________________________\n";
-        System.out.println(lineBreak + "Hello I'm Duke!\nWhat can I do for you?\n" + lineBreak);
+        System.out.println(lineBreak + "Hello I'm Duke!\nWhat can I do for you?\n" +
+                "Input your tasks and I'll keep track of them!\n" + lineBreak);
 
-        while (!line.equals("bye")){
+        do {
+            System.out.print("Enter Your Task Here: ");
             line = in.nextLine();
-            System.out.println(line + "\n" + lineBreak);
-        }
+            System.out.println(lineBreak);
+            if (line.equals("")){
+                System.out.println("Please input a valid task!\n" + lineBreak);
+            } else if (line.equals("bye")){
+                break;
+            } else {
+                tasks[indexCount++] = line;
 
-        System.out.println("Bye. Hope to see you again soon!\n" + lineBreak);
+                for (int i = 0; i < indexCount; i++){
+                    System.out.println(i+1 + ". " + tasks[i]);
+                }
+
+                System.out.println(lineBreak);
+            }
+
+        } while (true);
+
+        System.out.println("Aww you're going? Hope to see you again soon!\n" + lineBreak);
     }
 }
