d0122cf405405e43e1e951b1249b38530cb7d73a
Thomas Jiralerspong <thomasjiralerspong@gmail.com>
2023-01-22 23:10:29 +0800

    Add ability to store data and display back to user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,48 @@
 import java.util.Scanner;
 public class Duke {
+    private static String[] storedData = new String[100];
+    private static int currentStoredDataIndex = 0;
+    private static final String indent = "      ";
     public static void main(String[] args) {
         printIntro();
         mainLoop();
         printExit();
 
     }
+    public static void addToList(String input) {
+        storedData[currentStoredDataIndex] = input;
+        currentStoredDataIndex ++;
+        printMessage("added: " + input);
+    }
     public static  void mainLoop() {
         Scanner in = new Scanner(System.in);
         String currentInput = in.nextLine();
         while (!currentInput.equals("bye")) {
-            printMessage(currentInput);
+            switch (currentInput) {
+                case "list":
+                    printMessage(getFormattedList());
+                    break;
+                default:
+                    addToList(currentInput);
+                    break;
+            }
             currentInput = in.nextLine();
         }
     }
+
+    public static String getFormattedListItem(String item, int number) {
+        return Integer.toString(number) + ". " + item;
+    }
+    public static String[] getFormattedList() {
+        String[] formattedList = new String[currentStoredDataIndex];
+        for (int i = 0; i < currentStoredDataIndex; i ++) {
+            formattedList[i] = getFormattedListItem(storedData[i], i + 1 );
+        }
+        return formattedList;
+    }
     public static void printIntro() {
-        printMessage("Hello! I'm Tom");
-        printMessage("What can I do for you?");
+        String[] intro = {"Hello! I'm Tom", "What can I do for you?"};
+        printMessage(intro);
     }
     public static void printExit() {
         printMessage("Bye. Hope to see you again soon!");
@@ -28,8 +54,17 @@ public class Duke {
         printSeparator();
     }
 
+    public static void printMessage(String[] message) {
+        printSeparator();
+        for (String line : message) {
+            printIndent();
+            System.out.print(line + "\n");
+        }
+        printSeparator();
+    }
+
     public static void printIndent() {
-        System.out.print("      ");
+        System.out.print(indent);
     }
     public static void printSeparator() {
         System.out.print("   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
