0387699f38e59f5468c667a2e7f7506e92ad76d1
PoobalanAatmikaLakshmi <88182404+PoobalanAatmikaLakshmi@users.noreply.github.com>
2023-01-22 16:09:04 +0800

    add Level1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,9 +8,22 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello I am Duke \n" + logo);
-        System.out.println("What can I help with?");
-        System.out.println("Bye see you again!");
+        System.out.println("Hello I am Baymax you personal life assistant \n" + logo);
+        System.out.println("What can I do for you today?");
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("Bye see you again!");
+                break;
+            }
+            System.out.println(line);
+
+
+        }
+
 
     }
 }
