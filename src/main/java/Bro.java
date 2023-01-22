81bc71548510eebbf03b4101af8586c4c5bf7a7b
kairuler <khairul.aizat000@gmail.com>
2023-01-22 21:12:13 +0800

    Added Greet, Echo, Exit

--- src/main/java/Bro.java
+++ src/main/java/Bro.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
 public class Bro {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
-                + " Sup. I'm Bro.\n"
-                + " What you want?\n"
-                + "____________________________________________________________\n"
-                + " Bye bye.\n"
-                + "____________________________________________________________\n";
+        String horizontalLine = "\n───────────────────────────────────────────────────────────────\n";
+        String logo = horizontalLine + " Sup. I'm Bro.\n" + " What do you want?" + horizontalLine;
         System.out.println(logo);
+
+        // User Input
+        String line, reply = "";
+        Scanner in = new Scanner(System.in);
+        boolean endInput = false;
+        while (!endInput) {
+            line = in.nextLine();
+            switch (line) {
+            case "list":
+                reply = " list";
+                break;
+            case "blah":
+                reply = " blah";
+                break;
+            case "bye":
+                reply = " Bye bye.";
+                endInput = true;
+                break;
+            default:
+                reply = " I don't understand.";
+            }
+            System.out.println(horizontalLine + reply + horizontalLine);
+        }
     }
 }
