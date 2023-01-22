5d7f0dd780dcbe9df5f06be956bdd31745e11f0b
Manoj364 <theerazor@outlook.com>
2023-01-22 18:40:57 +0800

    Add echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,15 +7,30 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
 
-        System.out.println("_________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println(" ");
-        System.out.println("_________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(" ");
-        System.out.println("_________________________________________");
+        System.out.println("    Hello from\n" + logo);
+        System.out.println("    _________________________________________");
+
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        System.out.println("     ");
+        System.out.println("    _________________________________________");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (!line.equalsIgnoreCase("bye")) {
+            System.out.println("    _________________________________________");
+            System.out.println("    " + line);
+            System.out.println(" ");
+            System.out.println("    _________________________________________");
+            in = new Scanner(System.in);
+            line = in.nextLine();
+        }
+
+
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("     ");
+        System.out.println("    _________________________________________");
     }
 }
