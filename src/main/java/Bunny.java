54636ad2de3f1bcb833d1ff43408f54af11d344f
EthanYidong <ethanyidong@gmail.com>
2023-01-22 23:25:49 +0800

    Implement marking and unmarking TODOs as done
    Override toString() for Todo class
    Add markAsDone and markAsNotDone  to Todo class
    
    Add mark and unmark handlers to REPL

--- src/main/java/Bunny.java
+++ src/main/java/Bunny.java
@@ -1,3 +1,4 @@
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -18,9 +19,29 @@ public class Bunny {
                 } else {
                     ArrayList<String> messageLines = new ArrayList<>();
                     for(int i = 0; i < bunny.numTodos(); i++) {
-                        messageLines.add((i + 1) + ". " + bunny.getTodo(i).getName());
+                        messageLines.add((i + 1) + ". " + bunny.getTodo(i));
                     }
                     bunny.printMessage(messageLines);
+                }
+            } else if (input.startsWith("mark")) {
+                try {
+                    int markIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                    bunny.getTodo(markIndex).markAsDone();
+                    bunny.printMessage("Nice! I've marked this task as done:\n\t" + bunny.getTodo(markIndex));
+                } catch (IndexOutOfBoundsException e) {
+                    bunny.printMessage("To mark a TODO as done, provide a valid TODO number like so: \"mark 1\"");
+                } catch (NumberFormatException e) {
+                    bunny.printMessage("To mark a TODO as done, provide a valid TODO number like so: \"mark 1\"");
+                }
+            } else if (input.startsWith("unmark")) {
+                try {
+                    int unmarkIndex = Integer.parseInt(input.split(" ")[1]) - 1;
+                    bunny.getTodo(unmarkIndex).markAsNotDone();
+                    bunny.printMessage("Nice! I've marked this task as not done yet:\n\t" + bunny.getTodo(unmarkIndex));
+                } catch (IndexOutOfBoundsException e) {
+                    bunny.printMessage("To unmark a TODO as done, provide a valid TODO number like so: \"unmark 1\"");
+                } catch (NumberFormatException e) {
+                    bunny.printMessage("To unmark a TODO as done, provide a valid TODO number like so: \"unmark 1\"");
                 }
             } else {
                 bunny.addTodo(new Todo(input));
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,8 +1,10 @@
 public class Todo {
     private String name;
+    private boolean done;
 
     public Todo(String name) {
         this.name = name;
+        this.done = false;
     }
 
     public String getName() {
@@ -11,5 +13,22 @@ public class Todo {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void markAsDone() {
+        this.done = true;
+    }
+
+    public void markAsNotDone() {
+        this.done = false;
+    }
+
+    @Override
+    public String toString() {
+        char marker = ' ';
+        if(this.done) {
+            marker = 'X';
+        }
+        return "[" + marker + "] " + this.name;
     }
 }
