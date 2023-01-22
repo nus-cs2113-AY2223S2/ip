0c5f62e97fb21d0961dbf16affb53dcfe1dd53c7
Manoj364 <theerazor@outlook.com>
2023-01-22 19:30:27 +0800

    Add ability to store text and display stored text

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,14 +16,28 @@ public class Duke {
         System.out.println("     ");
         System.out.println("    _________________________________________");
 
+        String[] list = new String[100];
         String line;
+        int counter = 0;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         while (!line.equalsIgnoreCase("bye")) {
-            System.out.println("    _________________________________________");
-            System.out.println("    " + line);
-            System.out.println(" ");
-            System.out.println("    _________________________________________");
+            if (!line.equalsIgnoreCase("list")) {
+                list[counter] = line;
+                counter++;
+                System.out.println("    _________________________________________");
+                System.out.println("    " + "added: " + line);
+                System.out.println(" ");
+                System.out.println("    _________________________________________");
+            }
+            if (line.equalsIgnoreCase("list")) {
+                System.out.println("    _________________________________________");
+                for (int i = 0; i < counter; ++i) {
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
+                System.out.println(" ");
+                System.out.println("    _________________________________________");
+            }
             in = new Scanner(System.in);
             line = in.nextLine();
         }
