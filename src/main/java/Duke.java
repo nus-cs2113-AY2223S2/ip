2dd826345794f2719c55d9ec01bb49e550199672
Tan Jian Wei <jianwei0911@gmail.com>
2023-01-22 11:44:59 +0800

    Touched up Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
+        
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
