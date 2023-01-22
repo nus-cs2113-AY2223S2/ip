a77a1c696292ec99db2f2ff7d459d354a6e84b66
jeraldgau <jerald.gau87@gmail.com>
2023-01-22 20:56:09 +0800

    Implement Increment Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Duke");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+         */
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t Hello! I'm Vivy");
+        System.out.println("\t What can I do for you?");
+        System.out.println("\t____________________________________________________________");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while(line.equals("bye") == false) {
+            System.out.println("\t____________________________________________________________");
+            System.out.println("\t " + line);
+            System.out.println("\t____________________________________________________________");
+            line = in.nextLine();
+        }
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t Bye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
     }
 }
