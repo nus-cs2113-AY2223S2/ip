43d7f9fb82a35c4c85b7e53576b1ec4089d086f8
woowenjun99 <woowenjun99@gmail.com>
2023-01-22 22:48:29 +0800

    Add in the functionality to store the data and list out

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,27 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static String[] texts = new String[100];
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        int counter = 0;
 
         while (true) {
             String line = in.nextLine();
-            System.out.println(line);
-            if (line.equals("bye")) {
-                break;
+            if (line.equals("list")) {
+                for (int i = 0; i < counter; ++i) {
+                    System.out.printf("%d: %s\n", i + 1, texts[i]);
+                }
+            } else {
+                texts[counter] = line;
+                counter += 1;
+                if (line.equals("bye")) {
+                    System.out.println(line);
+                    break;
+                }
+                System.out.println("added: " + line);
             }
         }
 
