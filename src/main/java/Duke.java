51752d49e2cfd08004a3843e111f7b719bdc6510
NivethaUdayakumar <nivethaus4@gmail.com>
2023-01-22 15:39:13 +0800

    Increment level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -16,17 +19,28 @@ public class Duke {
 
         String line;
         Scanner in = new Scanner(System.in);
-        while(true) {
+        ArrayList<String> tasks = new ArrayList<>();
+        while (true) {
             line = in.nextLine();
             if ("bye".equalsIgnoreCase(line)) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if ("list".equalsIgnoreCase(line)) {
+                System.out.println("____________________________________________________________");
+                int index = 1;
+                for (String item : tasks) {
+                    System.out.println(index + ". " + item);
+                    index++;
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks.add(line);
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + line);
+                System.out.println("____________________________________________________________");
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(line);
-            System.out.println("____________________________________________________________");
         }
     }
 }
