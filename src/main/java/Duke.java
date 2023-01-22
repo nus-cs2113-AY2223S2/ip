5f78505af2336b9cd8777a72df06375805e06da0
woowenjun99 <woowenjun99@gmail.com>
2023-01-23 00:00:57 +0800

    Add in a Task Class and Complete Task 3 functionality of marking and unmarking class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static String[] texts = new String[100];
+    private static final Task[] tasks = new Task[100];
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -10,12 +10,28 @@ public class Duke {
 
         while (true) {
             String line = in.nextLine();
+
             if (line.equals("list")) {
                 for (int i = 0; i < counter; ++i) {
-                    System.out.printf("%d: %s\n", i + 1, texts[i]);
+                    boolean isDone = tasks[i].isDone();
+                    String symbol = isDone ? "X" : " ";
+                    System.out.printf("%d:[%s] %s\n", i + 1, symbol, tasks[i].getTaskName());
                 }
             } else {
-                texts[counter] = line;
+                if (line.contains("mark")) {
+                    String[] array = line.split(" ");
+                    int index = Integer.parseInt(array[1]) - 1;
+                    boolean toMark = (array[0].equals("mark")) ? true : false;
+                    String remark = toMark ? "Nice! I've marked this task as done:" : "OK, I've marked this task as not done yet:";
+                    tasks[index].setDone(toMark);
+                    boolean isDone = tasks[index].isDone();
+                    String symbol = isDone ? "X" : " ";
+                    System.out.printf("%s\n[%s] %s\n", remark, symbol, tasks[index].getTaskName());
+                    continue;
+                }
+
+                Task newTask = new Task(line);
+                tasks[counter] = newTask;
                 counter += 1;
                 if (line.equals("bye")) {
                     System.out.println(line);
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    private boolean isDone;
+    private String taskName;
+
+    public Task(String taskName) {
+        this.isDone = false;
+        this.taskName = taskName;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+}
