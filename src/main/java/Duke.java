c85598c355fb3e245fc64e0aa0905e99baf64dbf
Thomas Jiralerspong <thomasjiralerspong@gmail.com>
2023-01-22 22:43:40 +0800

    Add main loop which repeats what user says

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,37 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
         printIntro();
+        mainLoop();
         printExit();
 
     }
-
+    public static  void mainLoop() {
+        Scanner in = new Scanner(System.in);
+        String currentInput = in.nextLine();
+        while (!currentInput.equals("bye")) {
+            printMessage(currentInput);
+            currentInput = in.nextLine();
+        }
+    }
     public static void printIntro() {
-        printMessage("Hello! I'm Duke\nWhat can I do for you?");
+        printMessage("Hello! I'm Tom");
+        printMessage("What can I do for you?");
     }
     public static void printExit() {
         printMessage("Bye. Hope to see you again soon!");
     }
     public static void printMessage(String message) {
-        System.out.println(message);
-        printHorizontalLine();
+        printSeparator();
+        printIndent();
+        System.out.print(message + "\n");
+        printSeparator();
     }
-    public static void printHorizontalLine() {
-        System.out.println("------------------------------------------");
+
+    public static void printIndent() {
+        System.out.print("      ");
+    }
+    public static void printSeparator() {
+        System.out.print("   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
     }
 }
