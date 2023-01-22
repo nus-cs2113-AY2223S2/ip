b55697de1bc1e5610e32bf4497fcab7d5b9088ec
woowenjun99 <woowenjun99@gmail.com>
2023-01-22 20:49:13 +0800

    Add in functionality where bot will continue to repeat user input until command bye is given

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
 
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+
+        while (true) {
+            String line = in.nextLine();
+            System.out.println(line);
+            if (line.equals("bye")) {
+                break;
+            }
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
+        in.close();
     }
 }
