fb189ba35dcd81b8a8b1751492945ade0f1c15da
jeraldgau <jerald.gau87@gmail.com>
2023-01-22 22:15:59 +0800

    Implemented Increment Level-3: Mark as done, and implemented Extention: A-Classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        // Greeting message
         System.out.println("\t____________________________________________________________");
         System.out.println("\t Hello! I'm Vivy");
         System.out.println("\t What can I do for you?");
@@ -10,21 +11,43 @@ public class Duke {
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
-        while(line.equals("bye") == false) {
-            // List out all the tasks added
-            if (line.equals("list")) {
+        while(!line.equals("bye")) {
+            String[] words = line.split(" ");
+            if (line.equals("list")) { // List out all the tasks added
                 System.out.println("\t____________________________________________________________");
-                for(int i = 0; i < taskCount; i++) {
-                    System.out.println("\t " + (i + 1) + ". " + tasks[i]);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("\t " + (i + 1) + "." + "[" + tasks[i].getStatusIcon() + "] "
+                            + tasks[i].getDescription());
                 }
                 System.out.println("\t____________________________________________________________");
-            } else {
-                tasks[taskCount] = line;
-                taskCount++;
+            } else if (words[0].equals("unmark") && (words.length == 2)) { // Mark a task as not done
+                int taskNumber = Integer.parseInt(words[1]);
+                tasks[taskNumber - 1].markAsNotDone();
+                // Printing out marked as not done message
                 System.out.println("\t____________________________________________________________");
-                System.out.println("\t added: " + line);
+                System.out.println("Understood. I've marked this task as not done yet:");
+                System.out.println("[" + tasks[taskNumber - 1].getStatusIcon() + "] " +
+                        tasks[taskNumber - 1].getDescription());
+                System.out.println("\t____________________________________________________________");
+            } else if (words[0].equals("mark") && (words.length == 2)) { // Mark a task as done
+                int taskNumber = Integer.parseInt(words[1]);
+                tasks[taskNumber - 1].markAsDone();
+                // Printing out marked as done message
+                System.out.println("\t____________________________________________________________");
+                System.out.println("Understood. I've marked this task as done:");
+                System.out.println("[" + tasks[taskNumber - 1].getStatusIcon() + "] " +
+                        tasks[taskNumber - 1].getDescription());
+                System.out.println("\t____________________________________________________________");
+            } else { // Adding a task to the list
+                Task currentTask = new Task(line);
+                tasks[taskCount] = currentTask;
+                taskCount++;
+                // Printing out added task message
+                System.out.println("\t____________________________________________________________");
+                System.out.println("\t added: " + currentTask.getDescription());
                 System.out.println("\t____________________________________________________________");
             }
             line = in.nextLine();
---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        if(isDone) {
+            // Mark done task with X
+            return "X";
+        }
+        return " ";
+    }
+}
