98b34d5e78cba254841f02160933e3a445180d5f
EthanYidong <ethanyidong@gmail.com>
2023-01-22 22:48:16 +0800

    Implement user command echoing
    Wrote a basic REPL which takes input and prints it out
    Scans for input equalling “bye” to signal a quit

--- src/main/java/Bunny.java
+++ src/main/java/Bunny.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Bunny {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         BunnySession bunny = new BunnySession();
-        bunny.printMessage("Hello! I'm Bunny\nWhat can I do for you?");
+
+        bunny.printMessage("Hello! I'm Bunny.\nWhat can I do for you?");
+
+        while(true) {
+            String input = in.nextLine();
+            if(input.equals("bye")) {
+                break;
+            }
+            bunny.printMessage(input);
+        }
+
         bunny.printMessage("Bye. Hope to see you again soon!");
     }
 }
