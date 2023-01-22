497949fc43746904174f868db3a01f5389bc2340
Mustafa Anis Hussain <mustafahussain793@gmail.com>
2023-01-22 23:48:27 +0800

    Add list to store and display tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,37 @@
 import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("\u2500".repeat(50));
+        String line = ("\u2500".repeat(50));
+        Task[] List = new Task[100];
+
+        System.out.println(line);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
-        System.out.println("\u2500".repeat(50));
+        System.out.println(line);
 
         Scanner myObj = new Scanner(System.in);
+        int index = 0;
 
-        String bye = "bye";
-        boolean isSame = false;
-
-        while (!isSame) {
-            String task = myObj.nextLine();
-            if (task.equals(bye)){
-                System.out.println("\u2500".repeat(50));
+        while (true) {
+            String userInput = myObj.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println(line);
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("\u2500".repeat(50));
-                isSame = true;
-            }
-            else {
-                System.out.println("\u2500".repeat(50));
-                System.out.println(task); // Output user input
-                System.out.println("\u2500".repeat(50));
+                System.out.println(line);
+                break;
+            } else if (userInput.equals("list")) {
+                System.out.println(line);
+                for (int i = 0; i < index; i++) {
+                    System.out.println((i + 1) + ". " + List[i].getTaskDescription());
+                }
+                System.out.println(line);
+            } else {
+                System.out.println(line);
+                List[index] = new Task(userInput);
+                List[index].setTaskDescription(userInput);
+                index++;
+                System.out.println("added: " + userInput);
+                System.out.println(line);
             }
         }
     }
---
+++ src/main/java/Task.java
@@ -0,0 +1,14 @@
+public class Task {
+    protected String taskDescription;
+    public Task(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+}
