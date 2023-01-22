fa9244d11e3630a429cdc3a413c834bf540a207f
Tan Jian Wei <jianwei0911@gmail.com>
2023-01-22 14:42:31 +0800

    Added Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,20 @@
+import java.util.*;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?\n");
-        
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?\n");
+
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+
+        while (!line.equals("bye")) {
+            if (line.length() > 0) {
+                System.out.printf("\t%s\n", line);
+            }
+            line = in.nextLine();
+        }
+
+        System.out.println("\tBye. Hope to see you again soon!");
     }
 }
