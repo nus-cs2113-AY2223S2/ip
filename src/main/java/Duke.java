0a7280a44c928e3e21aa7c058ee3a484c384d1ad
woowenjun99 <woowenjun99@gmail.com>
2023-01-22 20:33:44 +0800

    Add in the formatting for the Duke file

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,8 @@
 public class Duke {
 
-  public static void main(String[] args) {
-    System.out.println("Hello! I'm Duke");
-    System.out.println("What can I do for you?");
-    System.out.println("Bye. Hope to see you again soon!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
 }
