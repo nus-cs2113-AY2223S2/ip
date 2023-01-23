f622db8d89b66c6b7ad02111e06450774969e54b
jacob-stein1 <jmstein@bu.edu>
2023-01-23 12:04:56 +0800

    Implement Duke level-1: greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+        String barrier = "____________________________________________________________";
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -7,9 +13,17 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        String greeting = "____________________________________________________________\nHello! I'm Duke\nWhat can I do for you?";
-        String exit = "____________________________________________________________\nBye. Hope to see you again soon!\n____________________________________________________________";
+        String greeting = barrier + "\nHello! I'm Duke\nWhat can I do for you?\n" + barrier + "\n";
+        String exit = barrier + "\nBye. Hope to see you again soon!\n" + barrier;
         System.out.println(greeting);
+
+        String input = console.nextLine();
+
+        while(!input.equals("bye")){
+            System.out.println( barrier +"\n" + input + "\n" + barrier + "\n");
+            input = console.nextLine();
+        }
+
         System.out.println(exit);
     }
 }
