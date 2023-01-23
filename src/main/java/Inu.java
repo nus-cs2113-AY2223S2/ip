fc8b0848f34e6218052ae0c4512b819e9d408e8a
Leo Zheng Rui Darren <leo.darren@gmail.com>
2023-01-24 02:01:52 +0800

    Style coding standard

--- src/main/java/Inu.java
+++ src/main/java/Inu.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 public class Inu {
 
     private Task[] todoList;
-    public static final String greeting = " Woof Woof! I'm Inu! Your personal Shib-Assistant ^.^\n";
-    public static final String prompt = " What can I do for you today?\n";
-    public static final String farewell = " Bye. Hope to see you again soon ^.^!\n";
-    public static final String listMessage = " Woof! Here are your current tasks ^.^:\n";
-    public static final String markMessage = " Woof! I've marked this task as completed for you:\n";
-    public static final String unmarkMessage = " Woof! I've marked this task as incompleted for you:\n";
-    public static final String divider = "____________________________________________________________\n";
+    public static final String MESSAGE_GREETING = " Woof Woof! I'm Inu! Your personal Shib-Assistant ^.^\n";
+    public static final String MESSAGE_PROMPT = " What can I do for you today?\n";
+    public static final String MESSAGE_FAREWELL = " Bye. Hope to see you again soon ^.^!\n";
+    public static final String MESSAGE_LIST_HEADER = " Woof! Here are your current tasks ^.^:\n";
+    public static final String MESSAGE_MARK_TASK = " Woof! I've marked this task as completed for you:\n";
+    public static final String MESSAGE_UNMARK_TASK = " Woof! I've marked this task as incompleted for you:\n";
+    public static final String MESSAGE_DIVIDER = "____________________________________________________________\n";
 
     public Inu() {
 
-        System.out.println(divider + greeting);
-        System.out.println(prompt + divider);
+        System.out.println(MESSAGE_DIVIDER + MESSAGE_GREETING);
+        System.out.println(MESSAGE_PROMPT + MESSAGE_DIVIDER);
 
         setTodoList();
         readCommand();
 
-        System.out.println(divider + farewell + divider);
+        System.out.println(MESSAGE_DIVIDER + MESSAGE_FAREWELL + MESSAGE_DIVIDER);
 
     }
 
@@ -27,7 +27,8 @@ public class Inu {
 
         for (int i = 0; i < listIndex; ++i) {
 
-            System.out.println((i + 1) + ". " + "[" + todoList[i].getStatusIcon() + "] " + todoList[i].getDescription());
+            System.out.println((i + 1) + ". " + "[" + todoList[i].getStatusIcon() + "] "
+                    + todoList[i].getDescription());
 
         }
 
@@ -55,19 +56,20 @@ public class Inu {
 
             if (checkLine.equals("list")) {
 
-                System.out.println(divider + listMessage);
+                System.out.println(MESSAGE_DIVIDER + MESSAGE_LIST_HEADER);
                 getTodoList(listIndex);
-                System.out.println("\n" + divider);
+                System.out.println("\n" + MESSAGE_DIVIDER);
 
             } else if (checkLine.startsWith("mark ")) {
 
-                /* This ensures that the mark command is read correctly only if an index follows it */
+                // This ensures that the mark command is read correctly only if an index follows it
 
                 String index = checkLine.substring(5, checkLine.length());
                 int taskIndex = Integer.parseInt(index) - 1;
                 todoList[taskIndex].setDone();
 
-                System.out.println(divider + markMessage + "[" + todoList[taskIndex].getStatusIcon() + "] " + todoList[taskIndex].getDescription() + "\n" + divider);
+                System.out.println(MESSAGE_DIVIDER + MESSAGE_MARK_TASK + "[" + todoList[taskIndex].getStatusIcon()
+                        + "] " + todoList[taskIndex].getDescription() + "\n" + MESSAGE_DIVIDER);
 
             } else if (checkLine.startsWith("unmark ")) {
 
@@ -75,7 +77,8 @@ public class Inu {
                 int taskIndex = Integer.parseInt(index) - 1;
                 todoList[taskIndex].resetDone();
 
-                System.out.println(divider + unmarkMessage + "[" + todoList[taskIndex].getStatusIcon() + "] " + todoList[taskIndex].getDescription() + "\n" + divider);
+                System.out.println(MESSAGE_DIVIDER + MESSAGE_UNMARK_TASK + "[" + todoList[taskIndex].getStatusIcon()
+                        + "] " + todoList[taskIndex].getDescription() + "\n" + MESSAGE_DIVIDER);
 
             } else {
 
@@ -83,7 +86,8 @@ public class Inu {
                 todoList[listIndex] = task;
                 ++listIndex;
 
-                System.out.println(divider + "added: " + task.getDescription() + '\n' + divider);
+                System.out.println(MESSAGE_DIVIDER + "added: " + task.getDescription()
+                        + '\n' + MESSAGE_DIVIDER);
 
             }
 
