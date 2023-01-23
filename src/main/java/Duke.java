e5e572717185c4c0738608c005fbe186c758667e
lil1n <lilinongsc@gmail.com>
2023-01-23 16:28:43 +0800

    src/main/java/Duke.java: add (level 1) echo, fix bye format

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void greet() {
         String line = "____________________________________________________________";
@@ -10,11 +11,26 @@ public class Duke {
     public static void bye() {
         String line = "____________________________________________________________";
         String goodbye = " Bye. Hope to see you again soon!";
+        System.out.println(line);
         System.out.println(goodbye);
         System.out.print(line);
     }
+    public static void echo(String input) {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println(" " + input);
+        System.out.println(line);
+    }
     public static void main(String[] args) {
         greet();
+        String input;
+        Scanner in = new Scanner(System.in);
+        input = in.nextLine();
+        while (input.equals("bye") == false) {
+            echo(input);
+            in = new Scanner(System.in);
+            input = in.nextLine();
+        }
         bye();
     }
 }
