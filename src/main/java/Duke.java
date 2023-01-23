0506939cc1e87df7b6f94a254e37defd9436e4f3
LAPTOP-6GKQP4JJ\Douph <pdou@connect.ust.hk>
2023-01-24 03:59:09 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,18 +4,23 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         System.out.println("Hello! I'm Douph");
         System.out.println("What can I do for you?");
-        System.out.println("list:");
+        String[] lists=new String[100];
+        int index=0;
         while(true){
             String line=in.nextLine();
-
-            System.out.println(line);
             if(line.equals("bye")){
+                System.out.println("list:");
+                for(int i=0;i<index;i++){
+                    System.out.println(lists[i]);
+                }
+                System.out.println("bye");
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
+            lists[index]=line;
+            index++;
+            System.out.println(line);
+            System.out.println("\t"+"added: "+line);
         }
-
-
-
     }
 }
