7b475d083b998c6e5f08f330c49555bf2497b21b
Prince Pereira <74498975+Magmanat@users.noreply.github.com>
2023-01-23 19:24:37 +0800

    Level-2 add task adding and listing for GrandDuke

--- src/main/java/IO.java
+++ src/main/java/IO.java
@@ -1,30 +1,31 @@
 import java.util.Scanner;  // Import the Scanner class
 
 public class IO {
+
     public static void println() {
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }         
     public static void output(String message) {
-        System.out.print("      ");
-        IO.println();
-        System.out.print("      ");
+        println();
+        System.out.print("        ");
         System.out.println(message);
-        System.out.print("      ");
-        IO.println();
+        println();
     }                 
     public static void greet() {
-        output("Hello! I'm GrandDuke\n" + "      What can I do for you?");
+        output("Hello! I'm GrandDuke\n" + "        What can I do for you?");
     }
     public static void exit() {
         output("Bye. Hope to see you again soon!");
     }
 
-    public static void parsecommand(String input) {
+    public static void parseCommand(String input) {
         if (input.equals("bye")) {
             IO.exit();
             System.exit(0);
+        } else if (input.equals("list")){
+            Tasks.listTask();
         } else {
-            output(input);
+            Tasks.addTask(input);
         }
     }
     public static void startup() {
@@ -32,7 +33,7 @@ public class IO {
         Scanner input = new Scanner(System.in);  // Create a Scanner object
         while (true) {
             String command = input.nextLine();
-            parsecommand(command);
+            parseCommand(command);
         }
     }
 }
