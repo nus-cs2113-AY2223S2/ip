013200e6a520c4d5430152af9c82968e95b1782b
bawfen <benjamin@beepbeep.tech>
2023-01-23 13:35:11 +0800

    Add feature to store and list tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,31 +2,73 @@ import java.util.Scanner;
 
 public class Duke {
 
+    static int taskLimit = 100;
+
+    static String[] tasks = new String[taskLimit];
+    static int taskCount = 0;
+
+
+
+    public static void addTask(String task){
+        if(taskCount < taskLimit){
+            tasks[taskCount] = task;
+            taskCount++;
+            println("____________________________________________________________");
+            printf(" added: %s\n", task);
+            println("____________________________________________________________");
+        } else {
+            println("____________________________________________________________");
+            println(" Too many tasks!");
+            println("____________________________________________________________");
+        }
+    }
+
+    public static void listTasks(){
+        println("____________________________________________________________");
+        for(int i=1;i<=taskCount;i++){
+            printf(" %d. %s\n", i, tasks[i-1]);
+        }
+        println("____________________________________________________________");
+    }
+
     public static void beginInput(){
         Scanner in = new Scanner(System.in);
         while(true){
             String input = in.nextLine();
-            if(input.equals("bye")){
+            switch(input){
+            case "bye":
                 return;
+            case "list":
+                listTasks();
+                break;
+            default:
+                addTask(input);
+                break;
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(input);
-            System.out.println("____________________________________________________________");
+
         }
+    }
+
+    public static void println(String x){
+        System.out.print("\t");
+        System.out.println(x);
+    }
+
+    public static void printf(String format, Object ...args){
+        System.out.print("\t");
+        System.out.printf(format, args);
     }
     public static void main(String[] args) {
 
-
-
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        println("____________________________________________________________");
+        println(" Hello! I'm Duke");
+        println(" What can I do for you?");
+        println("____________________________________________________________");
 
         beginInput();
 
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        println("____________________________________________________________");
+        println(" Bye. Hope to see you again soon!");
+        println("____________________________________________________________");
     }
 }
