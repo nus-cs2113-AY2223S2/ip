23bc8b6817735f288eca75d91ec638fe0f8eba00
Aden Teo <88359484+adenteo@users.noreply.github.com>
2023-01-23 21:32:13 +0800

    Update Duke.java
    Add Level-2: Add, List functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static int userInputCount = 0;
     public static void main(String[] args) {
+        String[] userInputList = new String[100];
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -13,10 +15,17 @@ public class Duke {
         Scanner scan = new Scanner(System.in);
         while (true) {
             userInput = scan.nextLine();
+            userInputList[userInputCount] = userInput;
+            userInputCount++;
             if (userInput.equals("bye")) {
                 break;
+            } else if (userInput.equals("list")) {
+                for (int i = 1; i < userInputCount; i++) {
+                    System.out.println(i + ". " + userInputList[i]);
+                }
+            } else {
+                System.out.println(userInput);
             }
-            System.out.println(userInput);
         }
         System.out.println("Bye! Hope to see you again soon!");
     }
