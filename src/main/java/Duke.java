26dcedbb9bea8ff9fa1607754726aea8b0b27d0e
azriellee <azriel20005@gmail.com>
2023-01-23 15:48:52 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     private static String line = "____________________________________________________________";
+    private static ArrayList<String> list = new ArrayList<String>();
     public static void greet() {
         System.out.println(line);
         System.out.println("Hello! I'm Duke");
@@ -12,21 +14,30 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
     }
-    public static void processInput() {
-        while(true) {
-            Scanner in  = new Scanner(System.in);
+    public static void processInput(String input) {
+        if (input.toLowerCase().equals("list")) {
+            if(list.size() == 0) {
+                System.out.println("Nothing on your list yet!");
+            } else {
+                for (int i = 0; i < list.size(); i += 1) {
+                    System.out.println((i+1) + ". " + list.get(i));
+                }
+            }
+        } else {
+            System.out.println("added : " + input);
+            list.add(input);
+        }
+    }
+    public static void main(String[] args) {
+        greet();
+        while (true) {
+            Scanner in = new Scanner(System.in);
             String input = in.nextLine();
             if(input.toLowerCase().equals("bye")) {
                 bye();
                 break;
             }
-            System.out.println(line);
-            System.out.println(input);
-            System.out.println(line);
+            processInput(input);
         }
-    }
-    public static void main(String[] args) {
-        greet();
-        processInput();
     }
 }
