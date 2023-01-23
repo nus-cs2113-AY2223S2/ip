beb8e01d6f28779156400b3f877be99eda71430d
shawntangy <shawntang83@gmail.com>
2023-01-23 14:45:41 +0800

    Add Task class and some Duke functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,52 +1,84 @@
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+
+        Duke.logo();
+        Duke.greeting();
+
+        Scanner input = new Scanner(System.in);
+        String line = "";
+
+        Task[] tasks = new Task[100];
+        int tasksCount = 0;
+
+        while (!(line = input.nextLine()).equals("bye")) {
+           tasksCount = Duke.processInput(line, tasks,tasksCount);
+        }
+
+        Duke.ending();
+        System.exit(0);
+
+    }
+
+    public static void logo(){
         String logo =
-            "   _____  .__   _____                   .___\n" +
-        "  /  _  \\ |  |_/ ____\\______   ____   __| _/\n" +
-        " /  /_\\  \\|  |\\   __\\\\_  __ \\_/ __ \\ / __ |\n" +
-        "/    |    \\  |_|  |   |  | \\/\\  ___// /_/ |\n" +
-        "\\____|__  /____/__|   |__|    \\___  >____ |\n" +
-        "        \\/                        \\/     \\/\n";
+                "   _____  .__   _____                   .___\n" +
+                        "  /  _  \\ |  |_/ ____\\______   ____   __| _/\n" +
+                        " /  /_\\  \\|  |\\   __\\\\_  __ \\_/ __ \\ / __ |\n" +
+                        "/    |    \\  |_|  |   |  | \\/\\  ___// /_/ |\n" +
+                        "\\____|__  /____/__|   |__|    \\___  >____ |\n" +
+                        "        \\/                        \\/     \\/\n";
 
         System.out.println("Hello from\n" + logo);
+    }
 
+    public static void greeting(){
         String intro = "____________________________________________________________\n" +
                 " Hello! I'm Alfred Pennyworth.\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n";
         System.out.println(intro);
+    }
 
-        Scanner input = new Scanner(System.in);
-        String line = input.nextLine();
-        boolean isTerminated = false;
+    public static void ending(){
+        String ending = " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(ending);
+    }
 
-        String[] tasks = new String[100];
-        int taskCount = 0;
+    public static int processInput(String line, Task[] tasks, int tasksCount){
 
-        while(!isTerminated) {
-            switch (line) {
-            case "list":
-                int count = 0;
-                for (String task : tasks){
-                    if(task != null){
-                        System.out.println(++count +". " + task);
-                    }
+        String[] words = line.split(" ");
+        String command = words[0];
+
+        switch (command) {
+        case "mark":
+            int markIndex = Integer.parseInt(words[1]) - 1;
+            System.out.println("Nice! I've marked this task as done: ");
+            tasks[markIndex].markAsDone();
+            System.out.println("[" + tasks[markIndex].getStatusIcon() + "] " + tasks[markIndex].getDescription());
+            break;
+        case "unmark":
+            int unmarkIndex = Integer.parseInt(words[1]) - 1;
+            System.out.println("Okay, I've marked this task as not done yet: ");
+            tasks[unmarkIndex].markAsNotDone();
+            System.out.println("[" + tasks[unmarkIndex].getStatusIcon() + "] " + tasks[unmarkIndex].getDescription());
+            break;
+        case "list":
+            int count = 0;
+            for (Task t : tasks){
+                if(t != null){
+                    System.out.println(++count +". " + "[" + t.getStatusIcon() + "] " + t.getDescription());
                 }
-                line = input.nextLine();
-            case "bye":
-                String ending = " Bye. Hope to see you again soon!\n" +
-                        "____________________________________________________________";
-                System.out.println(ending);
-                //isTerminated = true;
-                //not yet in use
-                System.exit(0);
-            default:
-                System.out.println("added: "+line+"\n");
-                tasks[taskCount++] = line;
-                line = input.nextLine();
             }
+            break;
+
+        default:
+            System.out.println("added: "+line+"\n");
+            Task t = new Task(line);
+            tasks[tasksCount++] = t;
         }
+        return tasksCount;
 
     }
 }
