864b84946d6205a849195517fa00fe53bdb4c4e6
wanjuin <wanjuin2310@gmail.com>
2023-01-23 23:21:45 +0800

    Add Increment Level-1 (Echo)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,26 @@
-//import org.eclipse.nebula.paperclips.core.LinePrint;
+import java.util.Scanner;
 public class Duke {
+
+    public static void line () {
+        System.out.println("    =====================================================================================");
+    }
+    public static void greet () {
+        line();
+        System.out.println("    Hello! I'm Duke\n" + "    What can I do for you?\n");
+        line();
+    }
+    public static void sayGoodbye() {
+        line();
+        System.out.println("    Bye. Hope to see you again soon!\n");
+        line();
+    }
+
+    public static void echo(String echoWords){
+        line();
+        System.out.println("    " + echoWords + "\n");
+        line();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,13 +28,18 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        //LinePrint();
-
         greet();
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+
+        while(line.equals("bye") == false){
+            echo(line);
+            line = in.nextLine();
+        }
+
+        sayGoodbye();
     }
 
-    public static void greet () {
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
-    }
 }
