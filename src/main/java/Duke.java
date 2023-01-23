5c87aa97f8f00400a9ae1d4d1054b6ac32f7dac5
jeromeongithub <left4jerry@gmail.com>
2023-01-23 17:24:11 +0800

    Add Level 2 - Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,11 +6,28 @@ public class Duke {
         System.out.println("What can I do for you?\n");
 
         String line;
+        String[] texts = new String[100];
+        int textsIndex = 0;
         Scanner in = new Scanner(System.in);
 
         line = in.nextLine();
         while (!line.equals("bye")) {
-            System.out.println(line);
+            if (!line.equals("list")) {
+                // store the text
+                texts[textsIndex] = line;
+                textsIndex++;
+
+                // let the user know that we stored the text
+                System.out.println("added: " + line);
+            } else { // if line is list, we need to list out what there iss
+                for (int i = 0; i < textsIndex; i++) {
+                    System.out.print(i + 1);
+                    System.out.println(". " + texts[i]);
+                }
+            }
+
+
+            // read the next line
             line = in.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
