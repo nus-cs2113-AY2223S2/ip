b3a6bece9730b93571597a5a6a02a0bfbdd91c4f
Hong Lin Shang <e0774890@u.nus.edu>
2023-01-23 16:33:12 +0800

    Add store and display function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,13 +8,25 @@ public class Duke {
                 " Hello! I'm Duke\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n");
+        String[] store = new String[100];
+        int counter = 0;
         while (true) {
             line = in.nextLine();
             if (line.equals("bye")) {
                 break;
             }
+            if (line.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < counter; i++) {
+                    System.out.println(i+1 + ": " + store[i]);
+                }
+                System.out.println("____________________________________________________________\n");
+                continue;
+            }
+            store[counter] = line;
+            counter++;
             System.out.println("____________________________________________________________\n" +
-                    line + "\n" +
+                    "added: " + line + "\n" +
                     "____________________________________________________________\n");
         }
         System.out.println("____________________________________________________________\n" +
