73bf2d7e5501fec65c3fea566f4706cb355e0fd2
jacob-stein1 <jmstein@bu.edu>
2023-01-23 13:12:18 +0800

    Implement Duke level-3: mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,21 +12,53 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-                
+
         System.out.println("Hello from\n" + logo);
 
-        String greeting = barrier + "\nHello! I'm Duke\nWhat can I do for you?\n" + barrier + "\n";
+        String greeting = barrier + "\n\nHello! I'm Duke\nWhat can I do for you?\nFor a list of commands, please type 'help'\n" + barrier + "\n";
         String exit = barrier + "\n\nBye. Hope to see you again soon!\n" + barrier;
         System.out.println(greeting);
 
         String input = console.nextLine();
 
         while(!input.equals("bye")){
+            // Dynamic mark conditional
+            if(input.indexOf("mark") > -1 && input.indexOf("unmark") == -1){
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                if (index > Task.getTasksArray().size() - 1){
+                    System.out.println("\nOops! That task does not exist, please add tasks first!\n" + barrier + "\n");
+                } else {
+                    Task.getTasksArray().get(index).mark();
+                }
+                input = "handled";
+            }
+            // Dynamic unmark conditional
+            if(input.indexOf("unmark") > -1){
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                if (index > Task.getTasksArray().size() - 1){
+                    System.out.println("\nOops! That task does not exist, please add tasks first!\n" + barrier + "\n");
+                } else {
+                    Task.getTasksArray().get(index).unmark();
+                }
+                input = "handled";
+            }
+
             switch(input){
                 case "list": {
                     System.out.println(barrier + "\n");
-                    Task.printAllTasks();
+                    if(Task.getTasksArray().size() == 0){
+                        System.out.println("You have no takss yet.");
+                    } else {
+                        Task.printAllTasks();
+                    }
                     System.out.println(barrier + "\n");
+                    break;
+                }
+                case "handled": {
+                    break;
+                }
+                case "help": {
+                    help();
                     break;
                 }
                 default: {
@@ -40,5 +72,10 @@ public class Duke {
         }
 
         System.out.println(exit);
+    }
+
+    public static void help(){
+        String barrier = "____________________________________________________________";
+        System.out.println(barrier + "\n\nList of commands: \n- help: gives a list of commands\n- list: lists all current tasks\n- mark x: marks task x as complete\n- unmark x: unmarks task x as complete\n- bye: exists Dule\n- anything else: adds a task with the given description\n" + barrier + "\n");
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,6 +7,8 @@ public class Task {
     String description;
     boolean isComplete;
 
+    public static String barrier = "____________________________________________________________";
+
     public Task (){
         this.description = "";
         this.isComplete = false;
@@ -19,15 +21,39 @@ public class Task {
         tasks.add(this);
     }
 
-    public void completeTask (){
+    public void mark (){
+        if (this.isComplete){
+            System.out.println("\nOops! This task is already complete!\n" + barrier + "\n");
+            return;
+        }
         this.isComplete = true;
+        System.out.println("\nNice! I've marked this task as done: \n");
+        System.out.println("     " + this.printTask() + "\n" + barrier + "\n");
+    }
+
+    public void unmark (){
+        if (!this.isComplete){
+            System.out.println("\nOops! This task is already incomplete!\n" + barrier + "\n");
+            return;
+        }
+        this.isComplete = false;
+        System.out.println("\nOk, I've marked this task as not done yet: \n");
+        System.out.println("     " + this.printTask() + "\n"  + barrier + "\n");
     }
 
     public String printTask (){
-        return this.description;
+        if (this.isComplete){
+            return "[X] " + this.description;
+        }
+        return "[ ] " + this.description; 
+    }
+
+    public static ArrayList<Task> getTasksArray(){
+        return tasks;
     }
 
     public static void printAllTasks(){
+        System.out.println("Time for a productive day! Here's your list of tasks:");
         for(int i = 0; i<tasks.size(); i++){
             System.out.println((i+1) + ". " + tasks.get(i).printTask());
         }
