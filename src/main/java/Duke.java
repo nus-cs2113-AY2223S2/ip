86686a7e56ba0c7108086d32066811bbb1e749f2
lil1n <lilinongsc@gmail.com>
2023-01-23 21:00:44 +0800

    src/main/java/Duke.java: add (level 2) add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,16 +18,39 @@ public class Duke {
     public static void echo(String input) {
         String line = "____________________________________________________________";
         System.out.println(line);
-        System.out.println(" " + input);
+        System.out.println(" added: " + input);
+        System.out.println(line);
+    }
+    public static void add(String[] texts, String input, int counter) { // work on this
+        texts[counter] = input;
+        echo(input);
+    }
+    public static void list(String[] texts, int counter) {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        if (counter == 0) {
+            System.out.println("The list is empty!");
+        } else {
+            for (int i = 0; i < counter; i += 1) {
+                System.out.println(" " + (i + 1) + ". " + texts[i]);
+            }
+        }
         System.out.println(line);
     }
     public static void main(String[] args) {
         greet();
+        String texts[] = new String[100];
+        int counter = 0;
         String input;
         Scanner in = new Scanner(System.in);
         input = in.nextLine();
         while (input.equals("bye") == false) {
-            echo(input);
+            if (input.equals("list")) {
+                list(texts, counter);
+            } else {
+                add(texts, input, counter);
+                counter += 1;
+            }
             in = new Scanner(System.in);
             input = in.nextLine();
         }
