47fe3fc3bd739a2e67532126c8aa20921fcb3edc
L-K-Chng <lipkuang02@gmail.com>
2023-01-23 23:10:35 +0800

    Level 0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___|\n";*/
+        String line = "______________________________\n";
+        System.out.println(line + "Hello! I'm Duke\n" + "What can I do for you?\n" + line + "Bye. Hope to see you again soon!\n" + line);
     }
 }
