638bb50968996d0601b2906486a760bcbe538fff
ysl-28 <88529907+ysl-28@users.noreply.github.com>
2023-01-23 17:08:33 +0800

    Add increment: Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,10 +9,24 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
-        System.out.println("Bye. Hope to see you again soon!");
+        String command;
+        Scanner in = new Scanner(System.in);
+        boolean isReading = true;
+
+        while (isReading) {
+            command = in.nextLine();
+
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                isReading = false;
+            }
+
+            else {
+                System.out.println(command);
+            }
+        }
     }
 }
