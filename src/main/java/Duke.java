c20a609290f7da73c517c78cdaf994457087f6cf
FanZixian <98700507+FanZixian@users.noreply.github.com>
2023-01-23 16:10:49 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void echosCommands(String line) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" " + line);
+        System.out.println("____________________________________________________________" + '\n');
+    }
+
+
+    public static void bye() {
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +21,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        // Greet the user
+        initialGreeting();
+
+        
+
+        // Bye and terminate the program
+        bye();
+
     }
 }
