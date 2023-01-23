972e7a1e624d5396d4bd6a26a44d174438ee47af
LAPTOP-6GKQP4JJ\Douph <pdou@connect.ust.hk>
2023-01-24 02:14:53 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,21 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello! I'm Douph");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("list:");
+        while(true){
+            String line=in.nextLine();
+
+            System.out.println(line);
+            if(line.equals("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
+
+
+
     }
 }
