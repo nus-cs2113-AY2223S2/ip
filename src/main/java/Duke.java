e12dc003de4131b0b53f19d11bdf84031f839969
Benjamin Toh <toh.benjamin123@gmail.com>
2023-01-24 22:23:38 +0800

    Add mark as done and undone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Duke {
-    static String[] list = new String[100];
-    static int listSize = 0;
+    private static Task[] tasks = new Task[100];
+    private static int taskSize = 0;
 
     public static void printDivider() {
         String divider = "\t____________________________________";
@@ -35,9 +35,10 @@ public class Duke {
         printDivider();
     }
 
-    public static void addToList(String inputMessage) {
-        list[listSize] = inputMessage;
-        listSize++;
+    public static void addTasks(String inputMessage) {
+        Task newTask = new Task(inputMessage);
+        tasks[taskSize] = newTask;
+        taskSize++;
         String addedMessage = "\tadded: " + inputMessage;
 
         printDivider();
@@ -45,11 +46,32 @@ public class Duke {
         printDivider();
     }
 
-    public static void printList() {
+    public static void printTasks() {
         printDivider();
-        for (int i = 0; i < listSize; i++) {
-            System.out.println("\t" + (i + 1) + ". " + list[i]);
+        for (int i = 0; i < taskSize; i++) {
+            System.out.println("\t" + (i + 1) + ". " + tasks[i].toString());
         }
+        printDivider();
+    }
+
+    public static void markTask(int taskNum) {
+        Task currentTask = tasks[taskNum - 1];
+        currentTask.markDone();
+
+        String markMessage = "\tNice! I've marked this task as done:\n" + "\t" + currentTask.toString();
+        printDivider();
+        System.out.println(markMessage);
+        printDivider();
+    }
+
+    public static void unmarkTask(int taskNum) {
+        Task currentTask = tasks[taskNum - 1];
+        currentTask.markUndone();
+
+        String unmarkMessage = "\tOK, I've marked this task as not done yet:" + "\t" + currentTask.toString();
+
+        printDivider();
+        System.out.println(unmarkMessage);
         printDivider();
     }
 
@@ -59,22 +81,33 @@ public class Duke {
         Scanner input = new Scanner(System.in);
 
         while (input.hasNextLine()) {
-            String inputString = input.next();
-            switch (inputString) {
+            String inputString = input.nextLine();
+            String[] inputArray = inputString.split(" ");
+            String command = inputArray[0];
+
+            switch (command) {
             case "bye":
                 printExit();
                 System.exit(0);
                 break;
 
             case "list":
-                printList();
+                printTasks();
                 break;
-                
+
+            case "mark":
+                int taskNum = Integer.parseInt(inputArray[1]);
+                markTask(taskNum);
+                break;
+
+            case "unmark":
+                int unmarkTaskNum = Integer.parseInt(inputArray[1]);
+                unmarkTask(unmarkTaskNum);
+                break;
+
             default:
-                addToList(inputString);
-                input.nextLine();
+                addTasks(inputString);
             }
         }
-
     }
 }
