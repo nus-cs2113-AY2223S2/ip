50199b4ce95ab144a33e6249165f42945d1fb592
Khulon <e0725788@u.nus.edu>
2023-01-24 13:01:51 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,54 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+    public static void printSeperator(){
+        System.out.println("____________________________________________________________\n");
+    }
+
+    public static void printWelcome(){
+        System.out.println(
+                "\tHello! I'm Duke\n" +
+                        "\tWhat can I do for you?\n");
+        printSeperator();
+    }
+
+    public static void printGoodbye(){
+        System.out.println(
+                "\tBye. Hope to see you again soon!\n"
+        );
+        printSeperator();
+    }
+    public static void printLogo(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+        System.out.println("\tHello from\n" + logo);
+        printSeperator();
+    }
+
+    public static void echoChatTillBye(){
+        String userInput;
+        while(true) {
+            Scanner in = new Scanner(System.in);
+            userInput = in.nextLine();
+            if (userInput.equals("bye")) {
+                printSeperator();
+                break;
+            }
+            printSeperator();
+            System.out.println("\t" + userInput);
+            printSeperator();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        printLogo();
+        printWelcome();
+        echoChatTillBye();
+        printGoodbye();
     }
 }
