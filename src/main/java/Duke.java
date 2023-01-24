f290012ef6a060ccbf10b13cc8c7bd22f702c8bd
ZhongXiangWong <e0725929@u.nus.edu>
2023-01-24 19:15:50 +0800

    Update code style to SE-EDU Java coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,11 +3,12 @@ public class Duke {
     public static void printDivider() {
         System.out.println("____________________________________________________________");
     }
+
     public static void printGreeting() {
         String logo = " ____\n"
-                    + "|    \\ ___ _ _ ___\n"
-                    + "|  |  | .'| | | -_|\n"
-                    + "|____/|__,|\\_/|___|\n";
+                + "|    \\ ___ _ _ ___\n"
+                + "|  |  | .'| | | -_|\n"
+                + "|____/|__,|\\_/|___|\n";
 
         printDivider();
         System.out.print(logo);
@@ -15,6 +16,7 @@ public class Duke {
                 + "What can I do for you?");
         printDivider();
     }
+
     public static void printBye() {
         System.out.println("Bye. Hope to see you again soon!");
         printDivider();
@@ -29,13 +31,13 @@ public class Duke {
         Task[] tasks = new Task[100];
         int taskCounter = 0;
 
-        boolean status = true;
+        boolean isLooping = true;
         do {
             input = in.nextLine();
             String action = input.split(" ")[0];
             switch(action) {
             case "bye":
-                status = false;
+                isLooping = false;
                 break;
             case "list":
                 if (taskCounter == 0) {
@@ -50,7 +52,7 @@ public class Duke {
                 break;
             case "mark":
             case "unmark":
-                int index = Integer.parseInt(input.split(" ")[1])-1;
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
                 if (index < 0 || index >= taskCounter) {
                     System.out.println("Task number does not exist in list");
                     printDivider();
@@ -70,7 +72,7 @@ public class Duke {
                 printDivider();
                 break;
             }
-        } while (status);
+        } while (isLooping);
 
         printBye();
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,29 +1,34 @@
 public class Task {
-    protected String description;
+    protected final String description;
     protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
+
     public void markDone() {
-        this.isDone = true;
+        isDone = true;
         System.out.println("Nice! I've marked this task as done:");
-        this.printDescWithStatus();
+        printDescWithStatus();
     }
+
     public void markUndone() {
-        this.isDone = false;
+        isDone = false;
         System.out.println("Ok, I've marked this task as not done yet:");
-        this.printDescWithStatus();
+        printDescWithStatus();
     }
+
     public String getDescription() {
-        return this.description;
+        return description;
     }
+
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
+
     public void printDescWithStatus() {
-        System.out.println("[" + this.getStatusIcon() + "] " + this.getDescription());
+        System.out.println("[" + getStatusIcon() + "] " + getDescription());
     }
 }
 
