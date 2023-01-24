52c102cba64b57bd7ad013803d3d35114f396ab1
ngkaiwen123 <e0726144@u.nus.edu>
2023-01-25 00:13:56 +0800

    -Add, list features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,8 @@ public class Duke {
     public static void main(String[] args) {
         //Initialisation
         String line = "start";
+        String[] list = new String[100];
+        int listSize = 0;
         Scanner in = new Scanner(System.in);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -10,10 +12,20 @@ public class Duke {
         //Get commands.
         while (!line.equals("bye")){
             line = in.nextLine();
-            if (line.equals("bye")) {
+            switch(line) {
+            //To list out everything in the array.
+            case "list":
+                for (int x = 0; x < listSize; x += 1) {
+                    System.out.println((x + 1) + ". " + list[x]);
+                }
+            //To terminate program.
+            case "bye":
                 break;
-            } else {
-                System.out.println(line);
+            //Add normal user input to the list array.
+            default:
+                list[listSize] = line;
+                listSize += 1;
+                System.out.println("added: " + line);
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
