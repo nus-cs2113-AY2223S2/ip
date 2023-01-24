ed8109a0c122c5597093e21691fc81ccc2cc8e09
namsengi11 <namsengi11@gmail.com>
2023-01-25 01:17:15 +0800

    completed level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,30 @@
-public class Duke {
-    public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
+import java.util.Scanner;
 
-        String greeting = " Hello! I'm Duke\n" +
-                " What can I do for you?";
-        String exiting = " Bye. Hope to see you again soon!";
-        String lineBreak = "____________________________________________________________";
+public class Duke {
+    private static String lineBreak = " ____________________________________________________________";
+    private static String greeting = "Hello! I'm Duke\n  What can I do for you?";
+    private static String exiting = "Bye. Hope to see you again soon!";
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        speak(greeting);
+        boolean running = true;
+        while (running) {
+            String command = scanner.nextLine();
+            if (!command.equals("bye")) { // whats the difference between equals and ==
+                speak(command);
+            } else {
+                speak(exiting);
+                running = false;
+            }
+        }
+    }
+
+    static void speak(String message) {
         System.out.println(lineBreak);
-        System.out.println(greeting);
+        System.out.println("  " + message);
         System.out.println(lineBreak);
-        System.out.println(exiting);
-        System.out.println(lineBreak);
+        System.out.println();
     }
 }
