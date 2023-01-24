e1d56073560048464819bcb7ecfd24a4d65992b4
Chao Yi-Ju <sketch.2048@gmail.com>
2023-01-24 11:38:57 +0800

    Add echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Duke {
     public static void greet() {
         System.out.println("____________________________________________________________");
@@ -8,6 +10,18 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
+    public static void echo() {
+        Scanner scan = new Scanner(System.in);
+        String s;
+        s = scan.nextLine();
+        if (s.equals("bye")) {
+            goodBye();
+            return;
+        }
+        System.out.println(s);
+        System.out.println("____________________________________________________________");
+        echo();
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -16,6 +30,6 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greet();
-        goodBye();
+        echo();
     }
 }
