ea1af41ae7965112f3446df778de9451e94a18e6
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2023-01-24 14:32:44 +0800

    Level 3. Mark as Done

--- src/main/java/Jonathan.java
+++ src/main/java/Jonathan.java
@@ -10,7 +10,7 @@ public class Jonathan {
                 "    What can I do for you?\n" + line + "\n";
         String goodbye = "    Bye. Hope to see you again soon!\n" + line;
 
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int taskCounter = 0;
 
         System.out.println(welcome);
@@ -21,10 +21,19 @@ public class Jonathan {
             if(Objects.equals(command, "bye")) {
                 System.out.println(goodbye);
                 break;
+
             } else if(Objects.equals(command, "list")) {
                 lookupTask(list, taskCounter);
+
+            } else if(command.split(" ")[0].equals("mark") || command.split(" ")[0].equals("unmark")) {
+                boolean isDone = command.split(" ")[0].equals("mark");
+                int taskNum = Integer.parseInt(command.split(" ")[1]);
+                Task task = list[taskNum-1];
+
+                changeStatus(task, isDone);
+
             } else {
-                list[taskCounter] = command;
+                list[taskCounter] = new Task(command);
                 taskCounter += 1;
                 System.out.println(line + "\n    added: " + command + "\n" + line + "\n");
             }
@@ -32,12 +41,27 @@ public class Jonathan {
         }
     }
 
-    public static void lookupTask(String[] list, int counter) {
+    public static void lookupTask(Task[] list, int counter) {
         System.out.println("    ____________________________________________________________");
         for(int i=0; i < counter; i++) {
-            System.out.println("    " + (i+1) + ". "  + list[i]);
+            System.out.println("    " + (i+1) + ". "  + list[i].stringRepresentation());
         }
         System.out.println("    ____________________________________________________________\n");
     }
 
+    public static void changeStatus(Task task, boolean isDone){
+        task.setDone(isDone);
+        if(isDone) {
+            System.out.println("    ____________________________________________________________\n" +
+                    "     Nice! I've marked this task as done:\n" +
+                    "       " + task.stringRepresentation() + "\n" +
+                    "    ____________________________________________________________");
+        } else {
+            System.out.println("    ____________________________________________________________\n" +
+                    "     OK, I've marked this task as not done yet:\n" +
+                    "       " + task.stringRepresentation() + "\n" +
+                    "    ____________________________________________________________");
+        }
+    }
 }
+
---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        setDescription(description);
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String stringRepresentation() {
+        return "[" + getStatusIcon() + "] " + getDescription();
+    }
+}
