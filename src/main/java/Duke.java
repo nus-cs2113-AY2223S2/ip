5c960e26259239b03d565d874065557866b7fd43
Benjamin Toh <toh.benjamin123@gmail.com>
2023-01-24 19:48:19 +0800

    Add list and add to list function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void printDivider(){
-        String divider = "  ____________________________________";
+    static String[] list = new String[100];
+    static int listSize = 0;
+
+    public static void printDivider() {
+        String divider = "\t____________________________________";
         System.out.println(divider);
     }
 
-    public static void printLogo(){
-        String logo = "  ____        _        \n"
-                + " |  _ \\ _   _| | _____ \n"
-                + " | | | | | | | |/ / _ \\\n"
-                + " | |_| | |_| |   <  __/\n"
-                + " |____/ \\__,_|_|\\_\\___|\n";
+    public static void printLogo() {
+        String logo = "\t ____        _        \n"
+                + "\t|  _ \\ _   _| | _____ \n"
+                + "\t| | | | | | | |/ / _ \\\n"
+                + "\t| |_| | |_| |   <  __/\n"
+                + "\t|____/ \\__,_|_|\\_\\___|\n";
 
         System.out.println(logo);
     }
 
-    public static void printWelcome(){
-        String welcomeMessage = "   Hello! I'm Duke\n   What can I do for you?";
+    public static void printWelcome() {
+        String welcomeMessage = "\tHello! I'm Duke\n\tWhat can I do for you?";
 
         printDivider();
         printLogo();
@@ -25,10 +28,28 @@ public class Duke {
         printDivider();
     }
 
-    public static void printExit(){
-        String exitMessage = "  Bye. Hope to see you again soon!";
+    public static void printExit() {
+        String exitMessage = "\tBye. Hope to see you again soon!";
 
         System.out.println(exitMessage);
+        printDivider();
+    }
+
+    public static void addToList(String inputMessage) {
+        list[listSize] = inputMessage;
+        listSize++;
+        String addedMessage = "\tadded: " + inputMessage;
+
+        printDivider();
+        System.out.println(addedMessage);
+        printDivider();
+    }
+
+    public static void printList() {
+        printDivider();
+        for (int i = 0; i < listSize; i++) {
+            System.out.println("\t" + (i + 1) + ". " + list[i]);
+        }
         printDivider();
     }
 
@@ -37,21 +58,23 @@ public class Duke {
         printWelcome();
         Scanner input = new Scanner(System.in);
 
-        while (input.hasNextLine()){
+        while (input.hasNextLine()) {
             String inputString = input.next();
-            switch(inputString){
+            switch (inputString) {
             case "bye":
                 printExit();
                 System.exit(0);
                 break;
 
+            case "list":
+                printList();
+                break;
+                
             default:
+                addToList(inputString);
                 input.nextLine();
-                printDivider();
-                System.out.println(inputString);
-                printDivider();
             }
         }
-        
+
     }
 }
