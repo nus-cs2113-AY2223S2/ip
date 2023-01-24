e930c866744e37bb86e8fb7a0f1ec040166a0612
Rayleigh47 <yanming8870@outlook.com>
2023-01-24 23:49:57 +0800

    Implement Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,15 +15,31 @@ public class Duke {
 
         String command;
         Scanner in = new Scanner(System.in);
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int listCount = 0;
 
         while(true) {
             command = in.nextLine();
             if(command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i < listCount; i += 1) {
-                    System.out.println((i + 1) + ". " + list[i]);
+                    System.out.println((i + 1) + ". " + list[i].getStatusIcon() + list[i].description);
                 }
+            }
+            //else if(command.substring(0, 4).equals("mark")) {
+            else if(command.matches("mark \\d")) {
+                int number = Integer.parseInt(command.substring(5)) - 1;
+                list[number].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(list[number].getStatusIcon() + list[number].description);
+
+            }
+            //else if(command.substring(0, 6).equals("unmark")) {
+            else if(command.matches("unmark \\d")) {
+                int number = Integer.parseInt(command.substring(7)) - 1;
+                list[number].markAsNotDone();
+                System.out.println("Ok, I've marked this task as not done yet:");
+                System.out.println(list[number].getStatusIcon() + list[number].description);
             }
             else if(command.equals("bye")) {
                 Conversation.farewell();
@@ -32,7 +48,7 @@ public class Duke {
             else {
                 System.out.print("added: ");
                 Conversation.copy(command);
-                list[listCount] = command;
+                list[listCount] = new Task(command);
                 listCount += 1;
             }
         }
