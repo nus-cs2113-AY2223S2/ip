742d3f416e138f9736ee743ea67259e58790dca0
sistine-yu <uusissi@connect.hku.hk>
2023-01-24 19:06:25 +0800

    add Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -14,16 +15,24 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        ArrayList<String> todo = new ArrayList<String>();//Create an arraylist object to store commands
+
         while(true){
             String line = "";
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
-            if(line.equals("bye") == false){
-                System.out.println(line);
-            }
-            else{
+            if(line.equals("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            }
+            else if(line.equals("list")){
+                for(int i = 0; i<todo.size(); i++){
+                    System.out.println(String.valueOf(i+1)+". "+todo.get(i));
+                }
+            }
+            else {
+                todo.add(line);
+                System.out.println("added: "+line);
             }
         }
     }
