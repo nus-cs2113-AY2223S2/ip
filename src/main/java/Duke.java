53c56b0cb339e4a02c114b00db1e109c0a879f07
pyongjie <pyjavatar@gmail.com>
2023-01-24 15:46:02 +0800

    Implement Duke Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,24 +8,38 @@ public class Duke {
                 + "════════════════════════════════════════════════════════════\n\n";
         System.out.print(greet);
 
+        String[] list = new String[100];
+        int listCount = 0;
+
         while (true) {
             String line;
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
 
-            if (!line.equals("bye")) {
+            if (line.equals("list")) {
+                System.out.print("════════════════════════════════════════════════════════════\n");
+
+                for (int i = 0; i < listCount; i++) {
+                    String printList = " " + (i + 1) + ". " + list[i] + "\n";
+                    System.out.print(printList);
+                }
+
+                System.out.print("════════════════════════════════════════════════════════════\n\n");
+            } else if (line.equals("bye")) {
+                String exit = "════════════════════════════════════════════════════════════\n"
+                        + " Goodbye. Hope to see you again soon!\n"
+                        + "════════════════════════════════════════════════════════════\n";
+                System.out.print(exit);
+                break;
+            } else {
+                list[listCount] = line;
+                listCount++;
                 String echo = "════════════════════════════════════════════════════════════\n"
-                        + " "
+                        + " added: "
                         + line
                         + "\n"
                         + "════════════════════════════════════════════════════════════\n\n";
                 System.out.print(echo);
-            } else {
-                String exit = "════════════════════════════════════════════════════════════\n"
-                        + " Goodbye. Hope to see you again soon!\n"
-                        + "════════════════════════════════════════════════════════════";
-                System.out.print(exit);
-                break;
             }
         }
     }
