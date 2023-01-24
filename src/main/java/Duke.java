1aff813bfb28cd54cc2307122d2858b6d46d03b5
thomasjlalba <e0773507@u.nus.edu>
2023-01-24 22:14:48 +0800

    Update Greet in Main Add Echo and Exit in Main

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
+    
+    public static void greet() {
+        String greet = "____________________________________________________________\n" +
+                "Hello! I'm Bob\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n";
+        System.out.println(greet);
+    }
+    
+    public static void exit() {
+        String exit = "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(exit);
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        greet();
+        
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (!line.matches("bye")) {
+            String horizontalLine = "____________________________________________________________\n";
+            System.out.println(horizontalLine + line + '\n' + horizontalLine);
+            line = in.nextLine();
+        }
+        
+        exit();
     }
 }
