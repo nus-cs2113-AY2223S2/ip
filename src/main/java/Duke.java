de8ed4d3acb71266dbfc40181b2df48d504ca29a
DESKTOP-VAFN8PH\Gerald <geraldkoh4@gmail.com>
2023-01-25 01:24:15 +0800

    Achieved Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,13 @@ public class Duke {
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                + "|____/ \\__,_|_|\\_\\___|\n"
+                + "__________________________\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! Do you need anything from me?\n"
+                + "I have only been trained to greet you so far.\n"
+                + "Once my owner is more proficient in what he does, he will give me more functions!");
+        //To be added to make sure user input is read.
+        System.out.println("Thats all from me! Goodbye!");
     }
 }
