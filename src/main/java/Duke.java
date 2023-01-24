c5bcb23d76c8b5cbc0674d170198faaca0182f21
ngkaiwen123 <e0726144@u.nus.edu>
2023-01-24 23:53:54 +0800

    -Greet, echo and exit function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        //Initialisation
+        String line = "start";
+        Scanner in = new Scanner(System.in);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println();
+
+        //Get commands.
+        while (!line.equals("bye")){
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                break;
+            } else {
+                System.out.println(line);
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
