135c94b73eb9849241d8f8fd1cfe444049b84e93
ChongQiRong <e0727229@u.nus.edu>
2023-01-24 13:22:01 +0800

    Add echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
 public class Duke {
-    public static void greetExit() {
+    public static void greet() {
         System.out.println("____________________________________________________________\n"
                 + "Hello! I'm Duke\nWhat can I do for you?\n"
-                + "____________________________________________________________\n"
+                + "____________________________________________________________\n");
+    }
+
+    public static void exit(){
+        System.out.println("____________________________________________________________\n"
                 + "Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________\n");
+    }
+
+    public static void echo(String input){
+        System.out.println("____________________________________________________________\n"
+                + input + "\n____________________________________________________________\n");
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -13,6 +23,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        greetExit();
+        
+        greet();
+        while (true){
+            Scanner in = new Scanner(System.in);
+            String userInput = in.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            echo(userInput);
+        }
+        exit();
     }
 }
