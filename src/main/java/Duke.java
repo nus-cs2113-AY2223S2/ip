c623466c3280af62a6c65b932cafb9b0ae364397
raydent30 <raydentwx@gmail.com>
2023-01-25 00:33:12 +0800

    Duke class: add Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -22,26 +22,35 @@ public class Duke {
         String userCommand = scanner.nextLine();
 
         boolean exit = false;
-        while(true){
+        String[] storedUserTexts = new String[100];
+        int userTextCount = 0;
+        while(!exit){
+            System.out.println(userCommand);
             switch (userCommand){
+                case "list":
+                    // display list
+                    System.out.println(divider);
+                    for(int i=0;i<userTextCount;i++){
+                        System.out.println((i+1)+". "+storedUserTexts[i]);
+                    }
+                    System.out.println(divider);
+                    userCommand = scanner.nextLine();
+                    break;
                 case "bye":
                     exit = true;
+                    break;
                 default:
-            }
-            if(exit){
-                System.out.println(divider);
-                break;
-            }
-            else{
-                System.out.println(divider);
-                System.out.println(userCommand);
-                System.out.println(divider);
-                userCommand = scanner.nextLine();
-
+                    System.out.println(divider);
+                    System.out.println("added: " + userCommand);
+                    System.out.println(divider);
+                    storedUserTexts[userTextCount] = userCommand;
+                    userCommand = scanner.nextLine();
+                    userTextCount++;
+                    break;
             }
         }
 
-
+        System.out.println(divider);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(divider);
     }
