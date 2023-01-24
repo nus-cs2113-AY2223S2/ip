15007d6596b737e1a7ae905e0e789f02b8feec87
Benjamin Toh <toh.benjamin123@gmail.com>
2023-01-24 18:41:39 +0800

    Add greet, echo and exit fuction

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,57 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-
-        String divider = "____________________________________";
-
-        String welcomeMessage = "Hello! I'm Duke\nWhat can I do for you?";
-
-        String exitMessage = "Bye. Hope to see you again soon!";
-
+    public static void printDivider(){
+        String divider = "  ____________________________________";
         System.out.println(divider);
+    }
+
+    public static void printLogo(){
+        String logo = "  ____        _        \n"
+                + " |  _ \\ _   _| | _____ \n"
+                + " | | | | | | | |/ / _ \\\n"
+                + " | |_| | |_| |   <  __/\n"
+                + " |____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println(logo);
-        System.out.println(welcomeMessage + '\n' + divider);
-        System.out.println(exitMessage + '\n' + divider);
+    }
+
+    public static void printWelcome(){
+        String welcomeMessage = "   Hello! I'm Duke\n   What can I do for you?";
+
+        printDivider();
+        printLogo();
+        System.out.println(welcomeMessage);
+        printDivider();
+    }
+
+    public static void printExit(){
+        String exitMessage = "  Bye. Hope to see you again soon!";
+
+        System.out.println(exitMessage);
+        printDivider();
+    }
+
+    public static void main(String[] args) {
+
+        printWelcome();
+        Scanner input = new Scanner(System.in);
+
+        while (input.hasNextLine()){
+            String inputString = input.next();
+            switch(inputString){
+            case "bye":
+                printExit();
+                System.exit(0);
+                break;
+
+            default:
+                input.nextLine();
+                printDivider();
+                System.out.println(inputString);
+                printDivider();
+            }
+        }
+        
     }
 }
