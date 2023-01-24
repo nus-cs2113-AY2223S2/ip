78cbbccd2ec0e1723e3c6585e05528c45b685caa
thomasjlalba <e0773507@u.nus.edu>
2023-01-24 21:48:22 +0800

    [Level-0] Add Greet

---
+++ src/main/java/Greet.java
@@ -0,0 +1,12 @@
+public class Greet {
+    public static void greet() {
+        String greet = "____________________________________________________________\n" +
+                "Hello! I'm Duke\n" +
+                "What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+    
+        System.out.println(greet);
+    }
+}
