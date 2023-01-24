54f83fb758b9106ad1e8df7dad5a4cafa405889b
cleoncheng2000 <e0725796@u.nus.edu>
2023-01-25 04:11:03 +0800

    Add Level 2. add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,17 +6,32 @@ public class Duke {
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-
+        
         Scanner in = new Scanner(System.in);
+        String[] list = new String[100];
+        int currentIndex = 0;
+
         while (true) {
             String input;
             input = in.nextLine();
             if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (String item : list){
+                    if (item != null) {
+                        System.out.println(item);
+                    }
+                }
+                System.out.println("____________________________________________________________");
             } else {
-                System.out.println(input);
+                list[currentIndex] = (currentIndex + 1) + ". " + input;
+                currentIndex++;
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + input);
                 System.out.println("____________________________________________________________");
             }
         }
