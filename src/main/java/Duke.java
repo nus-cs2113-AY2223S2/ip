089a7cef55bbc0bca9d388d9fd2cb69bc96eb3c4
raydent30 <raydentwx@gmail.com>
2023-01-25 00:09:50 +0800

    Duke class: add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,48 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        Scanner scanner = new Scanner(System.in);
+
+        String logo =
+                  " __   __\n"
+                + "|  | |  |   ____     _  _   _    _\n"
+                + "|  |_|  |  / _  \\   | |/_\\ | |  | |\n"
+                + "|   _   | | |_|  \\  |  /   | \\_/  |\n"
+                + "|__| |__|  \\___/\\_\\ |_|     \\__/|_|\n";
 
         String divider = "____________________________________________________________";
 
         System.out.println(divider);
-
         System.out.println(logo);
-        System.out.println("Hello! I'm Duke");
+        System.out.println("Hello! I'm Haru");
         System.out.println("What can I do for you?");
-
         System.out.println(divider);
 
-        System.out.println("Bye. Hope to see you again soon!");
+        String userCommand = scanner.nextLine();
 
+        boolean exit = false;
+        while(true){
+            switch (userCommand){
+                case "bye":
+                    exit = true;
+                default:
+            }
+            if(exit){
+                System.out.println(divider);
+                break;
+            }
+            else{
+                System.out.println(divider);
+                System.out.println(userCommand);
+                System.out.println(divider);
+                userCommand = scanner.nextLine();
+
+            }
+        }
+
+
+        System.out.println("Bye. Hope to see you again soon!");
         System.out.println(divider);
     }
 }
