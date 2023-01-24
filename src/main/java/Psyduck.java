d90300a1aed2c3c091200769f797abdf197690dc
dendendenden04 <dennisokq@gmail.com>
2023-01-24 23:20:56 +0800

    Level - 01
    Added echo function to Psyduck bot.
    Use command "Bye" to exit the bot.

--- src/main/java/Psyduck.java
+++ src/main/java/Psyduck.java
@@ -1,5 +1,6 @@
-public class Psyduck {
+import java.util.Scanner;
 
+public class Psyduck {
     public static void linePrint(){
         for (long i = 0; i < 100; i++) {
             System.out.print("-");
@@ -7,9 +8,17 @@ public class Psyduck {
         System.out.println();
     }
     public static void main(String[] args) {
+        String command;
+        Scanner in = new Scanner(System.in);
         linePrint();
         System.out.println("Psyduck! Psy Psy Psyduck! (Hello! How can I help you?)");
         linePrint();
+        do {
+            command = in.nextLine();
+            linePrint();
+            System.out.println(command);
+            linePrint();
+        } while (command.equals("Bye") == false);
         System.out.println("Psyduck! (Buh Bye!)");
         linePrint();
     }
