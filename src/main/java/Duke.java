cba3a84f12f30ae1bf3933e01f534a1c8f500e7b
firwer <firwer@gmail.com>
2023-01-25 00:57:42 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String welcome = "Hello! I'm Clippy\n"
+                + "What can I do for you?\n"
+                + "Bye. Hope to see you again soon!\n";
+        System.out.println(welcome);
     }
 }
