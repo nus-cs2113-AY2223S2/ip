cd176a01d6f606b2b61c21b5cd114363b737099a
lil1n <lilinongsc@gmail.com>
2023-01-24 14:46:45 +0800

    src/main/java/Task.java: add task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    protected String taskName;
+    protected boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        if (isDone) {
+            return "X";
+        }
+        return " ";
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+}
