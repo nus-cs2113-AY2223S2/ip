de360ab97be3874d74caf008f36256bd05e345c6
pyongjie <pyjavatar@gmail.com>
2023-01-24 14:26:05 +0800

    Implement Duke Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String greet = "____________________________________________________________\n"
-                + " Hello! I'm Duke\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
-
+        String greet = "════════════════════════════════════════════════════════════\n"
+                + " Hello! I'm Chatty\n"
+                + " How can I help you?\n"
+                + "════════════════════════════════════════════════════════════\n\n";
         System.out.print(greet);
+
+        while (true) {
+            String line;
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+
+            if (!line.equals("bye")) {
+                String echo = "════════════════════════════════════════════════════════════\n"
+                        + " "
+                        + line
+                        + "\n"
+                        + "════════════════════════════════════════════════════════════\n\n";
+                System.out.print(echo);
+            } else {
+                String exit = "════════════════════════════════════════════════════════════\n"
+                        + " Goodbye. Hope to see you again soon!\n"
+                        + "════════════════════════════════════════════════════════════";
+                System.out.print(exit);
+                break;
+            }
+        }
     }
 }
