0423aa41e98dcd18be68e48cee6908d5bddbb141
Arif-Khalid <arifkhalid99@gmail.com>
2023-01-24 20:47:26 +0800

    Add echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
 public class Duke {
-    public static void main(String[] args) {
+    public static void printGreeting(){
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+    }
+
+    public static void printFarewell(){
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        printGreeting();
+        while(true)
+        {
+            String userInput = in.nextLine();
+            if(userInput.equals("bye")){
+                break;
+            }
+            System.out.println(userInput);
+        }
+        printFarewell();
     }
 }
