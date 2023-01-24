e524b53d243093856d4f98a217322c2212b1c09d
Stella1585 <88623728+Stella1585@users.noreply.github.com>
2023-01-25 01:26:16 +0800

    level 0

---
+++ src/main/java/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] args) {
+        String sentence = "____________________________________________________________\n" +
+                " Hello! I'm Duke\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
+        System.out.println(sentence);
+    }
+
+}
