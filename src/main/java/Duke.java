a17b6512f9ffb3fc2cfe8737c079a7689484b01a
Masahiro21 <e0774120@u.nus.edu>
2023-01-25 04:25:47 +0800

    Add Increment Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,21 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Duke {
+    private static String[] Tasks = new String[100];
+    private static int numOfTask = 0;
+    //Method to Add to the list of task
+    public static void addTask(String args){
+        numOfTask += 1;
+        Tasks[numOfTask-1] = args;
+        System.out.println("added: " + args);
+    }
+
+    public static void listTask(String[] args){
+        for(int i = 1; i <= numOfTask; i+= 1){
+            System.out.println(i + ". " + args[i-1]);
+        }
+    }
+    /*
     public static Boolean Exit(String args){
         if(args.equals("bye")){
             return true;
@@ -7,6 +23,20 @@ public class Duke {
             return false;
         }
     }
+     */
+     public static boolean actionCommand(String args){
+         switch(args){
+             case "bye":
+                 System.out.println("Bye. Hope to see you again soon!");
+                 return true;
+             case "list":
+                 listTask(Tasks);
+                 return false;
+             default:
+                 addTask(args);
+                 return false;
+         }
+     }
     public static void main(String[] args) {
         String greeting = "____________________________________________________________\n" +
                 " Hello! I'm Mike\n" +
@@ -15,13 +45,10 @@ public class Duke {
         System.out.println(greeting);
 
         Scanner sc= new Scanner(System.in);
-        String command = sc.nextLine();
+        String in = sc.nextLine();
 
-        while(Exit(command)==false) {
-            System.out.println(command);
-            command = sc.nextLine();
+        while(actionCommand(in)==false) {
+            in = sc.nextLine();
         }
-        
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
