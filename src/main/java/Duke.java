368143bfdb995e8148f454db0160fc1e4500c57b
Goh Chok Yao <chokyao@gmail.com>
2023-01-24 19:20:24 +0800

    Add echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String greetMsg = "____________________________________________________________\n"
-                + "Hello! I'm Duke\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
+        String greetMsg = "\t____________________________________________________________\n"
+                + "\tHello! I'm Duke\n"
+                + "\tWhat can I do for you?\n"
+                + "\t____________________________________________________________\n";
         System.out.println(greetMsg);
+
+        String input;
+        String chatOutput;
+        Scanner in = new Scanner(System.in);
+        Boolean shouldContinueChat = true;
+        do {
+            input = in.nextLine();
+            if (input.equals("bye")) {
+                shouldContinueChat = false;
+                chatOutput = "\tBye. Hope to see you again soon!";
+            } else {
+                chatOutput = "\t" + input;
+            }
+            String horizontalLine = "\t____________________________________________________________\n";
+            System.out.println(horizontalLine + chatOutput + System.lineSeparator() + horizontalLine);
+        } while (shouldContinueChat);
     }
 }
