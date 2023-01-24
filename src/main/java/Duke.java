893be323a411a92af365221a18e642ca85716d9f
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2023-01-24 12:21:49 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,27 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(logo);
+        Scanner input = new Scanner((System.in));
+        String command;
+        String line = "    ____________________________________________________________";
+        String welcome = line + "\n    Hello! I'm Piss\n" +
+                "    What can I do for you?\n" + line + "\n";
+        String goodbye = "    Bye. Hope to see you again soon!\n" + line;
 
-        String welcome = " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________";
         System.out.println(welcome);
 
-        String goodbye = " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
-        System.out.println(goodbye);
+        while(true) {
+            command = input.nextLine();
+
+            if(!Objects.equals(command, "bye")) {
+                System.out.println(line + "\n" + command + "\n" + line + "\n");
+            } else {
+                System.out.println(goodbye);
+                break;
+            }
+
+        }
     }
 }
