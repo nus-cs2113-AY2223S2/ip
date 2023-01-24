65280796dc84c0267af8c5152c50f062153017e1
sistine-yu <uusissi@connect.hku.hk>
2023-01-24 18:47:44 +0800

    add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -11,7 +13,19 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while(true){
+            String line = "";
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            if(line.equals("bye") == false){
+                System.out.println(line);
+            }
+            else{
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
     }
 
 }
