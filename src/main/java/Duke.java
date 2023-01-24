a52114d57c24f6d5d2cb070e13092d46368f00d6
Mnsd05 <thangngdc03bn@gmail.com>
2023-01-24 10:10:45 +0800

    Add "Mark as done"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,40 @@
+
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void createHorizontalLine() {
         System.out.println("____________________________________________________________");
     }
 
-    public static void printItems(String[] container) {
-        for (int i = 0; i < container.length; ++i) {
-            if (container[i] == null) {
+    public static void printItems(ArrayList<Task> container) {
+        for (int i = 0; i < container.size(); ++i) {
+            if (container.get(i) == null) {
                 break;
             } else {
-                System.out.println((i + 1) + ". " + container[i]);
+                System.out.println((i + 1) + ". " + "[" + container.get(i).getMarkingStatus() + "]" + container.get(i).getContent());
             }
+        }
+    }
+
+    public static void changeTaskStatus(String command, ArrayList<Task> container) {
+        try {
+            int lastCharacterInInteger = Integer.parseInt(command.substring(command.length() - 1));
+            if (lastCharacterInInteger > container.size()) {
+                System.out.println("The task number is bigger than the number of tasks");
+            } else {
+                boolean doesCommandContainsUnmark = command.contains("unmark ");
+                if (doesCommandContainsUnmark) {
+                    container.get(lastCharacterInInteger - 1).unMark();
+                    System.out.println("Ok! I've marked this task as not done yet:");
+                } else {
+                    container.get(lastCharacterInInteger - 1).mark();
+                    System.out.println("Nice! I've marked this task as done:");
+                }
+                System.out.println(lastCharacterInInteger + ". " + "[" + container.get(lastCharacterInInteger - 1).getMarkingStatus()
+                        + "]" + container.get(lastCharacterInInteger - 1).getContent());
+            }
+        } catch (NumberFormatException error) {
+            System.out.println("The mark request is not valid");
         }
     }
     public static void main(String[] args) {
@@ -20,9 +44,7 @@ public class Duke {
 
         String userInput;
         Scanner in = new Scanner(System.in);
-        String[] tasks = new String[100];
-
-        int currentIndex = 0;
+        ArrayList<Task> tasks = new ArrayList<Task>();
 
         while (true) {
             createHorizontalLine();
@@ -35,10 +57,12 @@ public class Duke {
                 break;
             } else if (userInput.equals("list")) {
                 printItems(tasks);
+            } else if (userInput.substring(0,5).equals("mark ") || userInput.substring(0,7).equals("unmark ")) {
+                changeTaskStatus(userInput, tasks);
             } else {
-                tasks[currentIndex] = userInput;
+                Task task = new Task(userInput);
+                tasks.add(task);
                 System.out.println("added: " + userInput);
-                ++currentIndex;
             }
         }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+
+
+public class Task {
+    private String content;
+    private boolean isMarked;
+
+    public Task(String content) {
+        this.content = content;
+        this.isMarked = false;
+    }
+
+    public void mark() {
+        this.isMarked = true;
+    }
+
+    public void unMark() {
+        this.isMarked = false;
+    }
+
+    public String getMarkingStatus() {
+        return isMarked ? "X" : " ";
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
