6de146b16f38ef50438c335e475f7a3715b3a4a9
ngyida <e0726193@u.nus.edu>
2023-01-25 17:28:48 +0800

    Update Greet, Add Echo and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,40 @@
+import java.util.Scanner;
 public class Duke {
+    private static String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    private static String line = "____________________________________________________________\n";
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        String line = "____________________________________________________________\n";
-        System.out.println(line + " Hello! I'm Duke\n What can I do for you?\n" + line + " Bye. Hope to see you again soon!\n" + line);
+        greetUser();
+        doTask();
+    }
+
+    private static void greetUser() {
+        System.out.println(logo);
+        System.out.println(line + " Hello! I'm Duke\n What can I do for you?\n" + line);
+    }
+
+    private static void exitDuke() {
+        System.out.println(line + " Bye. Hope to see you again soon!\n" + line);
+    }
+
+    private static void echoString(String input) {
+        System.out.println(line + " " + input + "\n" + line);
+    }
+
+    private static void doTask() {
+        String input;
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            input = in.nextLine();
+            if (input.equals("bye")) {
+                exitDuke();
+                return;
+            } else {
+                echoString(input);
+            }
+        }
     }
 }
