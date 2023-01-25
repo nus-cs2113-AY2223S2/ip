25f073000468497176065a6737e96a0080382b14
jacob-stein1 <jmstein@bu.edu>
2023-01-25 14:50:06 +0800

    Implement A-CodingStandard, spacing, and comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,10 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 
-        Scanner console = new Scanner(System.in);
-
+        // Text declarations and initial greeting
         String barrier = "____________________________________________________________";
-
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -14,36 +12,45 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
         System.out.println("Hello from\n" + logo);
-
         String greeting = barrier + "\n\nAhoy there! I be Duke.\nWhat can I do for ye?\nFor a list of commands, type 'help' me hearties!\n" + barrier + "\n";
         String exit = barrier + "\n\nFarewell! Hope to see ye again soon, ye scallywag!\n" + barrier;
         System.out.println(greeting);
 
+        // Scanner declaration and initial input
+        Scanner console = new Scanner(System.in);
         String input = console.nextLine();
 
+        // Read input such that it is not "bye"
         while(!input.equals("bye")){
+
             // Dynamic mark conditional
             if(input.indexOf("mark") > -1 && input.indexOf("unmark") == -1){
                 int index = Integer.parseInt(input.substring(5)) - 1;
                 if (index > Task.getTasksArray().size() - 1){
                     System.out.println("\nBlast! That task does not exist, please add tasks first, ye landlubbers!\n" + barrier + "\n");
                 } else {
-                    Task.getTasksArray().get(index).mark();
+                    Task.getTasksArray().get(index).markAsComplete();
                 }
+                // Label the input as handled to dodge the switch
                 input = "handled";
             }
+
             // Dynamic unmark conditional
             if(input.indexOf("unmark") > -1){
                 int index = Integer.parseInt(input.substring(7)) - 1;
                 if (index > Task.getTasksArray().size() - 1){
                     System.out.println("\nBlast! That task does not exist, please add tasks first, ye landlubbers!\n" + barrier + "\n");
                 } else {
-                    Task.getTasksArray().get(index).unmark();
+                    Task.getTasksArray().get(index).unmarkAsComplete();
                 }
+                // Label the input as handled to dodge the switch
                 input = "handled";
             }
 
+            // Switch to check input
             switch(input){
+
+                // Lists all tasks using the Task class printAllTask() method
                 case "list": {
                     System.out.println(barrier + "\n");
                     if(Task.getTasksArray().size() == 0){
@@ -54,13 +61,19 @@ public class Duke {
                     System.out.println(barrier + "\n");
                     break;
                 }
+
+                // Breaks the switch of mark or unmark was called
                 case "handled": {
                     break;
                 }
+
+                // Prints a list of commands and description
                 case "help": {
-                    help();
+                    printHelpList();
                     break;
                 }
+
+                // Adds a new task if no case is hit
                 default: {
                     System.out.println(barrier + "\n");
                     Task temp = new Task(input);
@@ -68,13 +81,16 @@ public class Duke {
                     System.out.println(barrier + "\n");
                 }
             }
+
+            // Reads next input
             input = console.nextLine();
         }
 
         System.out.println(exit);
     }
 
-    public static void help(){
+    // Local Duke method to list all functions
+    public static void printHelpList(){
         String barrier = "____________________________________________________________";
         System.out.println(barrier + "\n\nAvast! Here be the commands ye can use to make me do yer bidding!\n- list: lists all current tasks\n- mark x: marks task x as complete\n- unmark x: unmarks task x as complete\n- bye: exists Dule\n- anything else: adds a task with the given description\n" + barrier + "\n");
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,26 +2,32 @@ import java.util.ArrayList;
 
 public class Task {
     
+    // Static ArrayList of all tasks
     public static ArrayList<Task> tasks = new ArrayList<Task>(); 
 
+    // Task class attributes
     String description;
     boolean isComplete;
 
+    // Reusable barrier
     public static String barrier = "____________________________________________________________";
 
+    // Sets default task to no description and incomplete then adds to list
     public Task (){
         this.description = "";
         this.isComplete = false;
         tasks.add(this);
     }
 
+    // Dynamic task constructor
     public Task (String description) {
         this.description = description;
         this.isComplete = false;
         tasks.add(this);
     }
 
-    public void mark (){
+    // Changes task isComplete attribute to true
+    public void markAsComplete (){
         if (this.isComplete){
             System.out.println("\nBlast! This task be already complete, ye swab!\n" + barrier + "\n");
             return;
@@ -31,7 +37,8 @@ public class Task {
         System.out.println("     " + this.printTask() + "\n" + barrier + "\n");
     }
 
-    public void unmark (){
+    // Changes task isComplete attribute to true
+    public void unmarkAsComplete (){
         if (!this.isComplete){
             System.out.println("\nBlast! This task be already incomplete, ye bilge rat!\n" + barrier + "\n");
             return;
@@ -41,6 +48,7 @@ public class Task {
         System.out.println("     " + this.printTask() + "\n"  + barrier + "\n");
     }
 
+    // Prints the task completion status and description
     public String printTask (){
         if (this.isComplete){
             return "[X] " + this.description;
@@ -48,10 +56,12 @@ public class Task {
         return "[ ] " + this.description; 
     }
 
+    // Accessor method for the list of tasks
     public static ArrayList<Task> getTasksArray(){
         return tasks;
     }
 
+    // Prints all tasks
     public static void printAllTasks(){
         System.out.println("Time for a productive day, me hearties! Here be yer list of tasks: ");
         for(int i = 0; i<tasks.size(); i++){
