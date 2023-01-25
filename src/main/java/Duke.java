6d10d3fc70c74c850f429e0d48c09109e402d3d5
DESKTOP-VAFN8PH\Gerald <geraldkoh4@gmail.com>
2023-01-26 01:26:07 +0800

    Finished Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,7 +13,7 @@ public class Duke {
                 + "Once my owner is more proficient in what he does, he will give me more functions!\n"
                 + "Key in a number based on the function\n 1) Echo \n 2) List");
         //To be added to make sure user input is read.
-        String[] items = new String[100];
+        Task[] actions = new Task[100];
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
@@ -27,26 +27,89 @@ public class Duke {
                 }
                 break;
             case 2:
-                int itemCounter = 0;
-                System.out.println("You may type anything to add to a list of a 100 items.\n"
+                int actionCounter = 0;
+                System.out.println("You may type anything to add to a list of a 100 things to do.\n"
                         + "Type List to list down everything you have added so far.\n"
                         + "Type Bye to exit.");
                 line = in.nextLine();
                 while (!line.equals("Bye")) {
                     if (line.equals("List")) {
-                        for (int iterator = 0; iterator < itemCounter; iterator++) {
-                            System.out.println(iterator + 1 + ")" + items[iterator]);
+                        if (actionCounter == 0) {
+                            System.out.println("There is nothing to list!");
+                            line = in.nextLine();
+                            continue;
+                        }
+                        for (int iterator = 0; iterator < actionCounter; iterator++) {
+                            System.out.println(iterator + 1
+                                    + ")[" + actions[iterator].getStatusIcon()
+                                    + "]"
+                                    + actions[iterator].getDescription()
+                                    );
+
+                        }
+                        System.out.println("You may now mark or unmark your tasks by typing Action followed by the task number! E.g. unmark 1 or mark 2\n"
+                                + "To go back to editing your list, type edit");
+                        line = in.nextLine();
+                        while (!line.equals("edit")) {
+                            String[] words = line.split(" ");
+                            if (!(words[0].equals("mark") || words[0].equals("unmark") || words[0].equals("edit"))) {
+                                System.out.println("Invalid Input!");
+                                line = in.nextLine();
+                                continue;
+                            }
+                            int taskNumber = Integer.parseInt(words[1]) - 1;
+                            if (words[0].equals("mark")) {
+                                actions[taskNumber].mark();
+                            } else {
+                                // must be unmarked or some weird other mistype
+                                actions[taskNumber].unmark();
+                            }
+                            System.out.println("Done!\n"
+                                    + "["
+                                    + actions[taskNumber].getStatusIcon()
+                                    + "]"
+                                    + actions[taskNumber].getDescription()
+                            );
+                            line = in.nextLine();
+                        }
+                        // have to edit before exit, might change this in the future
+                        if (line.equals("edit")) {
+                            System.out.println("Input to add to your list!");
                         }
                         line = in.nextLine();
                     } else {
                         System.out.println("Added: " + line);
-                        items[itemCounter] = line;
-                        itemCounter++;
+                        Task toBeAdded = new Task(line);
+                        actions[actionCounter] = toBeAdded;
+                        actionCounter++;
                         line = in.nextLine();
                     }
                 }
                 break;
         }
         System.out.println("That's all from me! Goodbye!");
+    }
+
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+        public void mark() {
+            this.isDone = true;
+        }
+        public void unmark() {
+            this.isDone = false;
+        }
+        public String getDescription() {
+            return(this.description);
+        }
     }
 }
