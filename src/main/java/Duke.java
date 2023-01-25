06b1fc528c825d954f896e2b77c8e5882a4fdd16
MuxPotato <maxchewjw@gmail.com>
2023-01-26 02:25:04 +0800

    add list feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-
 import java.util.Arrays;
 import java.util.Scanner;
 public class Duke {
@@ -9,11 +8,19 @@ public class Duke {
         String userCommand;
         Scanner in = new Scanner(System.in);
 
+        String[] userCommands = new String[0];
+
         Boolean isContinue = true;
         while (isContinue) {
             userCommand = in.nextLine();
 
             switch (userCommand) {
+
+            case "list":
+                for(int i = 0; i < userCommands.length; i++) {
+                    System.out.println( (i+1) + ". " + userCommands[i]);
+                }
+                break;
 
             case "bye":
                 System.out.println("Bye. Hope to see you again soon!");
@@ -21,7 +28,9 @@ public class Duke {
                 break;
 
             default:
-                System.out.println(userCommand);
+                userCommands = Arrays.copyOf(userCommands, userCommands.length+1);
+                userCommands[userCommands.length-1] = userCommand;
+                System.out.println("added: " + userCommand);
                 break;
             }
         }
