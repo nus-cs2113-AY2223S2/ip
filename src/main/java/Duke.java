17b5e6f318b216a684c65ba859489d033d76a82a
geeeetyx <e0725682@u.nus.edu>
2023-01-25 13:17:00 +0800

    fix: level-0 removed feat: level-1 added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,10 +10,23 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
 
-        //level -0
-        System.out.println("____________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?\n");
+        //level -1
+        System.out.println("    ____________________________________");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?\n");
+        System.out.println("    ____________________________________");
+
+        Scanner userInput = new Scanner(System.in);
+        while (true) {
+            String text = userInput.nextLine();
+            if ("bye".equalsIgnoreCase(text)) {
+                break;
+            }
+
+            System.out.println("    ____________________________________");
+            System.out.println("    " + text);
+            System.out.println("    ____________________________________");
+        }
 
         System.out.println("____________________________________");
         System.out.println("Bye. Hope to see you again soon!\n");
