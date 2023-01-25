3b6797775aabdd63f32c3c38a7c2d67efc9cb553
LAPTOP-6GKQP4JJ\Douph <pdou@connect.ust.hk>
2023-01-26 01:43:14 +0800

    mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,25 +2,37 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("--------------------------------");
         System.out.println("Hello! I'm Douph");
         System.out.println("What can I do for you?");
-        String[] lists=new String[100];
+        System.out.println("--------------------------------");
+        Task[] lists=new Task[100];
         int index=0;
         while(true){
             String line=in.nextLine();
             if(line.equals("bye")){
+                System.out.println("--------------------------------");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("--------------------------------");
+                break;
+            }else if(line.substring(0,4).equals("mark")){
+                String number=line.substring(5,6);
+                int markIndex=Integer.parseInt(number);
+                lists[markIndex-1].isDone=true;
+            }else if(line.contains("unmark")){
+                String number=line.substring(7,8);
+                int unMarkIndex=Integer.parseInt(number);
+                lists[unMarkIndex-1].isDone=false;
+            } else if(line.equals("list")) {
                 System.out.println("list:");
                 for(int i=0;i<index;i++){
-                    System.out.println(lists[i]);
+                    System.out.println("\t"+(i+1)+".["+lists[i].getStatusIcon()+"]"+lists[i].description);
                 }
-                System.out.println("bye");
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
+            } else{
+                lists[index]=new Task(line);
+                index++;
             }
-            lists[index]=line;
-            index++;
-            System.out.println(line);
-            System.out.println("\t"+"added: "+line);
+
         }
     }
 }
