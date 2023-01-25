56d6bb3232b44c3458dbfdc81f63f3b210ef8139
haoyangw <haoyangw@gmail.com>
2023-01-26 01:08:02 +0800

    Implement increment Level-0: Read commands and reply

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String LINE = "____________________________________________________________";
+    private static final String BYE_TRIGGER = "bye";
+    private static final String BYE_PHRASE = " Bye. Hope to see you again soon!";
     public static void main(String[] args) {
         // Define horizontal line in a variable since it's printed multiple times
-        String line = "____________________________________________________________";
+
+        printlnWithIndent(Duke.LINE);
+        printlnWithIndent(" Hello! I'm Duke");
+        printlnWithIndent(" What can I do for you?");
+        printlnWithIndent(Duke.LINE);
+        Duke.runCommands();
+    }
+
+    private static void printlnWithIndent(String line) {
+        System.out.print("    ");
         System.out.println(line);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+    }
+
+    private static void runCommands() {
+        Scanner in = new Scanner(System.in);
+        String command = "";
+        do {
+            command = in.nextLine();
+            printlnWithIndent(Duke.LINE);
+            if (!command.equals(Duke.BYE_TRIGGER)) {
+                printlnWithIndent(" " + command);
+            } else {
+                printlnWithIndent(Duke.BYE_PHRASE);
+            }
+            printlnWithIndent(Duke.LINE);
+        } while (!command.equals(Duke.BYE_TRIGGER));
     }
 }
