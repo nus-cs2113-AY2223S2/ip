6fee83bd2db6dc50ef00bb84874dd5f4c9eaa6c9
khooyourun <khooyourun@hotmail.com>
2023-01-25 13:13:22 +0800

    Add add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
@@ -5,13 +6,30 @@ public class Duke {
         System.out.println("Good day. YodaBot, I am.");
         System.out.println("Assistance, you need?");
         Scanner scanner = new Scanner(System.in);
-        while(true) {
-            String command = scanner.nextLine();
+        String[] stringList = new String[100]; //array to store strings
+        int stringIndex = 0;
+        while (true) {
+            String command = scanner.nextLine(); //reads in input
+            if (!command.equalsIgnoreCase("list") && !command.equalsIgnoreCase("bye")) {
+                stringList[stringIndex] = command;
+                ++stringIndex;
+                System.out.println(command + " - Added, I have.");
+            }
+            if (command.equalsIgnoreCase("list")) {
+                if (stringIndex == 0) {
+                    System.out.println("Empty, list is.");
+                }
+                else {
+                    System.out.println("As shown, list is:");
+                    for (int j = 0; j < stringIndex; ++j) {
+                        System.out.println((j + 1) + ". " + stringList[j]);
+                    }
+                }
+            }
             if (command.equalsIgnoreCase("bye")) {
                 System.out.println("See you soon, I hope. Goodbye.");
                 break;
             }
-            else System.out.println(command);
         }
     }
 }
