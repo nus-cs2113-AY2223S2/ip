e164b24d874d56723ac1123d62f821663252714f
Toh-HongFeng <hf.toh2@gmail.com>
2023-01-25 16:07:42 +0800

    add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Duke {
     public static void main(String[] args) {
+        Random rand = new Random();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -11,7 +15,30 @@ public class Duke {
         // print hello greeting
         System.out.println(" Hello! I'm Duke\n" + " What can I do for you?\n" + spacer);
 
+        // print input and exit condition
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        while (!word.equals("bye")) {
+            System.out.println(spacer);
+            System.out.println(word + "\n" + spacer);
+            word = sc.nextLine();
+        }
+        System.out.println(spacer);
+        sc.close();
+
         //print exit greeting
-        System.out.println(" Bye. Hope to see you again soon!\n" + spacer);
+        switch (rand.nextInt() % 2) {
+        case 0:
+            System.out.println(" Bye. Hope to see you again soon!\n" + spacer);
+            break;
+
+        case 1:
+            System.out.println("Argh. I don\'t feel so good...\n" + spacer);
+            break;
+        
+        default:
+            System.out.println("Nooo. Free me..." + spacer);
+            break;
+        }
     }
 }
