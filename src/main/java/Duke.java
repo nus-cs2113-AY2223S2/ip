546d365ba73b886851abaf2ba3745a9a13fe5a20
tliangac <tliangac@connect.ust.hk>
2023-01-25 23:43:49 +0800

    Level-1 Bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,16 @@
+import java.util.List;
+import java.util.Scanner;
 public class Duke {
+    public static List list;
     public static void main(String[] args) {
         String lineBreak = "-----------------";
-        System.out.println(lineBreak + '\n' + "Hello! I'm Duke" + '\n' + "What can I do for you?" + '\n');
-        System.out.println(lineBreak + '\n' + "Bye. Hope to see you again soon!" + '\n' + lineBreak);
-
+        System.out.println(lineBreak + '\n' + "Hello! I'm Duke" + '\n' + "What can I do for you?" + '\n' + lineBreak);
+        Scanner myObj = new Scanner(System.in);
+        String instruction = myObj.nextLine();
+        if(instruction.equalsIgnoreCase("bye")){
+            System.out.println("Bye. Hope to see you again soon!");
+        }else if(instruction.equalsIgnoreCase("list")){
+            System.out.println(list);
+        }
     }
 }
