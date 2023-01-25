2599d4ddbd4bfa9e145eda6a2cc6ed3dba9b2ce8
Vu Van Dung <me@joulev.dev>
2023-01-25 19:55:57 +0800

    Make the corners of the box more rounded
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/main/java/BoxDrawingCharacter.java
+++ src/main/java/BoxDrawingCharacter.java
@@ -4,13 +4,13 @@
  * @see {@link https://en.wikipedia.org/wiki/Box-drawing_character}
  */
 public class BoxDrawingCharacter {
-    // ┌─┐
+    // ╭─╮
     // │ │
-    // └─┘
+    // ╰─╯
     public static final char HORIZONTAL_LINE = '─';
     public static final char VERTICAL_LINE = '│';
-    public static final char TOP_LEFT_CORNER = '┌';
-    public static final char TOP_RIGHT_CORNER = '┐';
-    public static final char BOTTOM_LEFT_CORNER = '└';
-    public static final char BOTTOM_RIGHT_CORNER = '┘';
+    public static final char TOP_LEFT_CORNER = '╭';
+    public static final char TOP_RIGHT_CORNER = '╮';
+    public static final char BOTTOM_LEFT_CORNER = '╰';
+    public static final char BOTTOM_RIGHT_CORNER = '╯';
 }
