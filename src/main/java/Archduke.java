01fcc27021e34cbded2c4ad6a7ca2dde69bcf33e
Vu Van Dung <me@joulev.dev>
2023-01-25 19:45:33 +0800

    Implement marking tasks as done/undone
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/main/java/Archduke.java
+++ src/main/java/Archduke.java
@@ -1,33 +1,98 @@
 public class Archduke {
-    public static void main(String[] args) {
-        IO io = new IO();
-        TaskStore store = new TaskStore();
-
+    static void greet() {
         IO.printBoxTopBorder();
         IO.printLogo();
         IO.printf("Hello! I'm Archduke. What do you want to do?");
         IO.printBoxBottomBorder();
+    }
+
+    static void bye() {
+        IO.printBoxTopBorder();
+        IO.printf("Bye. Hope to see you again soon!");
+        IO.printBoxBottomBorder();
+    }
+
+    static void printError(String err) {
+        IO.printBoxTopBorder();
+        IO.printf("ERROR: %s", err);
+        IO.printBoxBottomBorder();
+    }
+
+    static void listTasks(TaskStore store) {
+        IO.printBoxTopBorder();
+        IO.printf("Here are your tasks:");
+        store.listTasks();
+        IO.printBoxBottomBorder();
+    }
+
+    static void addTask(TaskStore store, String description) {
+        store.addTask(description);
+        IO.printBoxTopBorder();
+        IO.printf("Added task: %s", description);
+        IO.printBoxBottomBorder();
+    }
+
+    static void markTaskAsCompleted(TaskStore store, int index) {
+        Task task = store.getTask(index);
+        boolean isSuccessful = task.setAsCompleted();
+        IO.printBoxTopBorder();
+        if (isSuccessful) {
+            IO.printf("Understood. This task has been marked as done.");
+        } else {
+            printError("This task has already been marked as done.");
+        }
+        task.printTask(index);
+        IO.printBoxBottomBorder();
+    }
+
+    static void markTaskAsUncompleted(TaskStore store, int index) {
+        Task task = store.getTask(index);
+        boolean isSuccessful = task.setAsUncompleted();
+        IO.printBoxTopBorder();
+        if (isSuccessful) {
+            IO.printf("Understood. This task has been marked as not done yet.");
+        } else {
+            printError("This task has already been marked as not done.");
+        }
+        task.printTask(index);
+        IO.printBoxBottomBorder();
+    }
+
+    public static void main(String[] args) {
+        IO io = new IO();
+        TaskStore store = new TaskStore();
+
+        greet();
 
         while (true) {
             String command = io.readUserInput();
 
             switch (command) {
             case "list":
-                IO.printBoxTopBorder();
-                IO.printf("Here are your tasks:");
-                store.listTasks();
-                IO.printBoxBottomBorder();
+                listTasks(store);
                 continue;
             case "bye":
-                IO.printBoxTopBorder();
-                IO.printf("Bye. Hope to see you again soon!");
-                IO.printBoxBottomBorder();
+                bye();
                 return;
             default:
-                store.addTask(command);
-                IO.printBoxTopBorder();
-                IO.printf("Added task: %s", command);
-                IO.printBoxBottomBorder();
+                if (command.startsWith("mark") || command.startsWith("unmark")) {
+                    try {
+                        String[] parts = command.split(" ");
+                        int index = Integer.parseInt(parts[1]) - 1;
+                        if (parts[0].equals("mark")) {
+                            markTaskAsCompleted(store, index);
+                        } else {
+                            markTaskAsUncompleted(store, index);
+                        }
+                    } catch (NumberFormatException e) {
+                        printError("The provided index is not a valid integer.");
+                    } catch (IndexOutOfBoundsException e) {
+                        printError("The task can not be found.");
+                    }
+                    continue;
+                }
+
+                addTask(store, command);
             }
         }
     }
--- src/main/java/TaskStore.java
+++ src/main/java/TaskStore.java
@@ -1,21 +1,25 @@
 import java.util.Arrays;
 
 public class TaskStore {
-    private String[] tasks;
+    private Task[] tasks;
 
     public TaskStore() {
-        this.tasks = new String[0];
+        this.tasks = new Task[0];
     }
 
     public void addTask(String taskName) {
         int length = this.tasks.length;
         this.tasks = Arrays.copyOf(this.tasks, length + 1);
-        this.tasks[length] = taskName;
+        this.tasks[length] = new Task(taskName);
     }
 
     public void listTasks() {
         for (int i = 0; i < this.tasks.length; i++) {
-            IO.printf("  %d: %s", i + 1, this.tasks[i]);
+            this.tasks[i].printTask(i);
         }
+    }
+
+    public Task getTask(int index) {
+        return this.tasks[index];
     }
 }
