f4f40cd0f4e85a84a043c164a9356dcfd5460bb4
coregano <liberwifi@gmail.com>
2023-01-25 19:27:20 +0800

    completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,30 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        /*String logo = " ____        _        \n"
+        /*
+        String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);*/
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
+        System.out.println("Hello from\n" + logo);
+        */
+        String greet ="____________________________________________________________\n" +
+                " Hello! I'm kimo\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+                "____________________________________________________________\n";
+        System.out.println(greet);
+        String userInput;
+        Scanner in = new Scanner(System.in);
+        userInput = in.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println("    ____________________________________________________________\n" +
+                    "     " + userInput + "\n" +
+                    "    ____________________________________________________________\n ");
+            userInput = in.nextLine();
+        }
+        System.out.println("    ____________________________________________________________\n" +
+                "     Bye. Hope to see you again soon!\n" +
+                "    ____________________________________________________________\n");
     }
 }
