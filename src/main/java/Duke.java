d4fcbc2537cc267d7b7c17a3b28a1157f4791c80
darrenlsx <darren.lim@live.com.sg>
2023-01-26 00:13:33 +0800

    Level-3 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Duke {
     public static String linePrint = "____________________________________________________________\n";
     public static void addList(String s, ArrayList<String> myList){
-        myList.add(s);
+        myList.add("[ ] " + s);
         System.out.println(linePrint + "added: " + s + "\n" + linePrint);
     }
     public static void printList(ArrayList<String> myList){
@@ -14,7 +14,24 @@ public class Duke {
         }
         System.out.println(linePrint);
     }
-
+    public static void markDone(String s, ArrayList<String> myList){
+        String taskToMarkString = s.substring(s.length() - 1);
+        int taskToMark = Integer.parseInt(taskToMarkString) - 1;
+        myList.set(taskToMark,myList.get(taskToMark).replaceFirst(" ", "X"));
+        System.out.println(linePrint
+                + "Nice! I've marked this task as done:\n"
+                + myList.get(taskToMark) + "\n"
+                + linePrint);
+    }
+    public static void markUndone(String s, ArrayList<String> myList){
+        String taskToUnmarkString = s.substring(s.length() - 1);
+        int taskToUnmark = Integer.parseInt(taskToUnmarkString) - 1;
+        myList.set(taskToUnmark,myList.get(taskToUnmark).replaceFirst("X", " "));
+        System.out.println(linePrint
+                + "OK, I've marked this task as not done yet:\n"
+                + myList.get(taskToUnmark) + "\n"
+                + linePrint);
+    }
     public static void main(String[] args) {
         String logo = "____________________________________________________________\n"
                 + "Hello! I'm Duke\n"
@@ -28,6 +45,8 @@ public class Duke {
         while (!flag){
             Scanner in = new Scanner(System.in);
             String s = in.nextLine();
+            String sArray[] = s.split(" ");
+            String firstWord = sArray[0];
 
             if (s.toLowerCase().equals("bye")){
                 System.out.println(byeLine);
@@ -35,6 +54,12 @@ public class Duke {
             }
             else if (s.toLowerCase().equals("list")){
                 printList(myList);
+            }
+            else if (firstWord.toLowerCase().equals("mark")){
+                markDone(s,myList);
+            }
+            else if (firstWord.toLowerCase().equals("unmark")){
+                markUndone(s,myList);
             }
             else {
                 addList(s, myList);
