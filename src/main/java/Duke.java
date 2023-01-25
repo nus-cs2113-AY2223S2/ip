eed39fa231ef7caace2247f3967eafbb365027ba
Chao Yi-Ju <sketch.2048@gmail.com>
2023-01-25 09:48:50 +0800

    Add mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Objects;
 import java.util.Scanner;
 public class Duke {
-    static String[] list = new String[100];
+    static Task[] list = new Task[100];
     static int listId = 0;
     public static void greet() {
         System.out.println("____________________________________________________________");
@@ -9,7 +9,7 @@ public class Duke {
         System.out.println("____________________________________________________________");
     }
     public static void goodBye() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Bye. Hope to see you again soon!ﾉ~");
         System.out.println("____________________________________________________________");
     }
     public static void echo() {
@@ -23,11 +23,39 @@ public class Duke {
         if (s.equals("list")) {
             for (int i = 0; i < listId; i += 1) {
                 System.out.print(i+1);
-                System.out.println(". " + list[i]);
+                System.out.println(". [" + list[i].getStatusIcon() + "] " + list[i].description);
             }
             System.out.println("____________________________________________________________");
-        } else {
-            list[listId] = s;
+        } else if (s.equals("mark")) {
+            Scanner id = new Scanner(System.in);
+            System.out.println("Please enter the index of the task you want to mark: ");
+            int markId = id.nextInt()-1;
+            if (list[markId].isDone) {
+                System.out.println("This task has already been marked as done ੯•໒꒱❤︎");
+                System.out.println("____________________________________________________________");
+            } else {
+                list[markId].markAsDone();
+                System.out.println("I've marked this task as done ੯•໒꒱❤︎:");
+                System.out.println("[X] " + list[markId].description);
+                System.out.println("____________________________________________________________");
+            }
+        } else if (s.equals("unmark")) {
+            Scanner id = new Scanner(System.in);
+            System.out.println("Please enter the index of the task you want to unmark: ");
+            int markId = id.nextInt()-1;
+            if (list[markId].isDone == false) {
+                System.out.println("This task hasn't been marked as done yet ∪･ω･∪");
+                System.out.println("____________________________________________________________");
+            } else {
+                list[markId].markAsNotDone();
+                System.out.println("I've unmarked this task ∪･ω･∪:");
+                System.out.println("[ ] " + list[markId].description);
+                System.out.println("____________________________________________________________");
+            }
+        }
+        else {
+            Task t = new Task(s);
+            list[listId] = t;
             System.out.println("____________________________________________________________");
             System.out.println("added: " + s);
             System.out.println("____________________________________________________________");
