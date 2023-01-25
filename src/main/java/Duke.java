7af6b662ff143f6614b160b961ab9d6ba3e7d215
xiaoge26 <liuxiaoge0206@gmail.com>
2023-01-26 00:30:21 +0800

    Tweak the code to comply with a coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,17 @@
 import java.util.Scanner;
 public class Duke {
     public static Scanner input = new Scanner(System.in);
-    //public static String[] list = new String[100];
-
     public static Task[] list= new Task[100];
-    public static Boolean exit = false;
+    public static Boolean shouldExit = false;
     public static String[] marker = new String[2];
-
     public static int currentTask = 0;
 
     public static void printList(int currentTask){
         int currentPrintedTask = 0;
         int tempPos = currentTask;
-        if (tempPos == 0){
+        if (tempPos == 0) {
             System.out.println("No Task!");
-        }else {
+        } else {
             while (tempPos > 0) {
                 System.out.println(currentPrintedTask + 1 + ". " + list[currentPrintedTask].taskStatus());
                 currentPrintedTask++;
@@ -30,37 +27,36 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String lineBreaker = "____________________________________________________________\n";
-        String greeting ="____________________________________________________________\n" +
+        String greeting = lineBreaker +
                 " Hi! I'm Duke\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n";
+                lineBreaker;
         String farewellMessage =
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
+                " Bye. Hope to see you again soon!\n" + lineBreaker;
         String errorMessage = lineBreaker + "Invalid input. Please try again!\n" + lineBreaker;
         System.out.println("Hello from\n" + logo);
         System.out.println(greeting);
 
-        while (!exit){
-            String userInput = input.nextLine();  // Read user input
+        while (!shouldExit){
+            String userInput = input.nextLine();
 
-            if (userInput.startsWith("mark")){
+            if (userInput.startsWith("mark")) {
                 marker = userInput.split(" ");
                 int posOfTask = Integer.parseInt(marker[1]) - 1;
 
-                if (posOfTask < 0 || posOfTask > currentTask){
+                if (posOfTask < 0 || posOfTask > currentTask) {
                     System.out.println(errorMessage);
-                }else{
+                } else {
                     System.out.println(lineBreaker);
                     list[posOfTask].markAsDone();
                     System.out.println(lineBreaker);
                 }
-            }else if (userInput.startsWith("unmark")){
+            } else if (userInput.startsWith("unmark")) {
                 marker = userInput.split(" ");
                 int posOfTask = Integer.parseInt(marker[1]) - 1;
-                if (posOfTask < 0 || posOfTask > currentTask){
+                if (posOfTask < 0 || posOfTask > currentTask) {
                     System.out.println(errorMessage);
-                }else {
+                } else {
                     System.out.println(lineBreaker);
                     list[posOfTask].markAsUndone();
                     System.out.println(lineBreaker);
@@ -76,7 +72,7 @@ public class Duke {
                     break;
                 case "bye":
                     System.out.println(farewellMessage);
-                    exit = true;
+                    shouldExit = true;
                     break;
                 default:
                     System.out.println(lineBreaker);
