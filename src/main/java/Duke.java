32b6adf347f045bb1d438287c271e8f714c5e9a2
Samueltansw <samuetan123@gmail.com>
2023-01-25 20:38:41 +0800

    Added greetUser and sayBye functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        greetUser();
+        sayBye();
+    }
+
+    /**
+     * Prints BtB logo and greets the user
+     */
+    public static void greetUser() {
+
+        String logo = " ____     _    ____\n"
+                + "|  _ \\ __| |__|  _ \\\n"
+                + "| | //|__   __| | //\n"
+                + "| |_\\\\   |  |_| |_\\\\\n"
+                + "|____/   |___/|____/\n";
+        String dottedLine = "--------------------------------------------------\n";
+        String greetings = " Hello! I'm Bob the Bot, aka BtB.\n"
+                + " What can I do for you?\n";
+
+        System.out.println(dottedLine + logo + '\n' + greetings
+                + dottedLine);
+    }
+
+    /**
+     * Say bye to user
+     */
+    public static void sayBye() {
+        String dottedLine = "--------------------------------------------------";
+
+        System.out.println(" Bye. Hope to see you again sooooooon (┬┬﹏┬┬)!");
+        System.out.println(dottedLine);
     }
 }
