79091aed2b7327c80ba659f8b6403ad5762a3100
geeeetyx <e0725682@u.nus.edu>
2023-01-25 15:32:04 +0800

    feat: level-2 added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,11 +10,14 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
 
-        //level -1
+        //level-2
         System.out.println("    ____________________________________");
         System.out.println("    Hello! I'm Duke");
         System.out.println("    What can I do for you?\n");
         System.out.println("    ____________________________________");
+
+        String[] userInputs = new String[100];
+        int index = 0;
 
         Scanner userInput = new Scanner(System.in);
         while (true) {
@@ -23,13 +26,27 @@ public class Duke {
                 break;
             }
 
-            System.out.println("    ____________________________________");
-            System.out.println("    " + text);
-            System.out.println("    ____________________________________");
+            if ("list".equalsIgnoreCase(text)) {
+                for (int i = 0; i < userInputs.length; i += 1) {
+                    if (userInputs[i] == null) {
+                        break;
+                    }
+                    System.out.println((i+1) + ". " + userInputs[i]);
+                }
+                System.out.println("    ____________________________________");
+            } else {
+                userInputs[index] = text;
+
+                System.out.println("    ____________________________________");
+                System.out.println("     added: " + userInputs[index]);
+                System.out.println("    ____________________________________");
+
+                index += 1;
+            }
         }
 
         System.out.println("____________________________________");
-        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________");
     }
 }
