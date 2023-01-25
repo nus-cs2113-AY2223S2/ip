cd446fc3eb76f728b16b13bfdb2f346c027cc7f8
ong-ck <e0774208@u.nus.edu>
2023-01-26 01:35:15 +0800

    Add: Level-1 (Greet, Echo, Exit)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("____________________________________________________________");
-        System.out.println(logo + "\n");
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner inputScanner = new Scanner(System.in);
+        String userInput;
+
+        // Greeting the user
+        Messages.line();
+        Messages.logo();
+        Messages.hello();
+        Messages.line();
+        
+        do {
+            userInput = inputScanner.nextLine();
+            Messages.echo(userInput);
+        } while (!(userInput.equals("bye")));
+
+        Messages.bye();
+        Messages.line();
     }
 }
---
+++ src/main/java/Messages.java
@@ -0,0 +1,30 @@
+public class Messages {
+
+  public static void logo() {
+    String logo = " ____        _        \n"
+    + "|  _ \\ _   _| | _____ \n"
+    + "| | | | | | | |/ / _ \\\n"
+    + "| |_| | |_| |   <  __/\n"
+    + "|____/ \\__,_|_|\\_\\___|\n";
+
+    System.out.println(logo + "\n");
+  }
+
+  public static void line() {
+    System.out.println("____________________________________________________________");
+  }
+
+  public static void hello() {
+    System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+  }
+
+  public static void bye() {
+    System.out.println("Bye. Hope to see you again soon!");
+  }
+
+  public static void echo(String userInput) {
+    Messages.line();
+    System.out.println(userInput);
+    Messages.line();
+  }
+}
