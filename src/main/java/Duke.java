2a1b9895aa89e878f060faf17a6be7a2b8d5ac5e
Akhil <mnakhilwork@gmail.com>
2023-01-25 16:03:33 +0800

    This is level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,6 +7,15 @@
  *      All passed so far
  */
 
+/**
+ * Level 2:
+ * A few more tests are remaining:
+ *     1. Possible to list 2 same things
+ *     2. Line spacing has been ensured
+ *     3. added line has one space after it
+ *     4. new change of indenting all the lines is in progress/ completed
+ */
+
 
 import java.util.Scanner;
 public class Duke {
@@ -23,19 +32,29 @@ public class Duke {
         System.out.println("---------------------------------------------------------------------------------");
         Scanner in = new Scanner(System.in);
         String userInput= " ";
+        String[] taskList = new String[100];
+        int currentIndex = 0;
         while (true) { // ensure that the loop can stay on forever if needed.
             userInput = in.nextLine();
-            if(userInput.equals("bye")) {
-                break; // break once "bye" is met
+            if(userInput.equals("bye")) { // exit command
+                break;
+            } else if(userInput.equals("list")) { //displays the list if needed
+                System.out.println("\t---------------------------------------------------------------------------------");
+                for(int i = 0; i<currentIndex;i+=1) {
+                    System.out.println('\t' + Integer.toString(i+1) + ". " + taskList[i]);
+                }
+                System.out.println("\t---------------------------------------------------------------------------------");
+            } else { // tells the user that we have added the task in
+                taskList[currentIndex] = userInput;
+                currentIndex+=1;
+                System.out.println("\t---------------------------------------------------------------------------------");
+                System.out.println("\tadded: " + userInput);
+                System.out.println("\t---------------------------------------------------------------------------------");
             }
-            System.out.println("---------------------------------------------------------------------------------");
-            System.out.println("\t" + userInput);
-            System.out.println("---------------------------------------------------------------------------------");
+
         }
-        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("\t---------------------------------------------------------------------------------");
         System.out.println("\tBye. Hope to see you again soon!");
-        System.out.println("---------------------------------------------------------------------------------");
-
-
+        System.out.println("\t---------------------------------------------------------------------------------");
     }
 }
