f489c6301594c906e4cb7bb2fb30bd415b27d6c1
darrenlsx <darren.lim@live.com.sg>
2023-01-25 17:31:28 +0800

    Level-1 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class Duke {
+
+    public static void printCommand(String s){
+        String toPrint = "____________________________________________________________\n"
+                + s + "\n"
+                + "____________________________________________________________\n";
+        System.out.println(toPrint);
+
+    }
     public static void main(String[] args) {
-        String logo = "Hello! I'm Duke\n"
+        String logo = "____________________________________________________________\n"
+                + "Hello! I'm Duke\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n";
+                + "____________________________________________________________\n";
+        String byeLine = "Bye. Hope to see you again soon!\n";
         System.out.println(logo);
+        boolean flag = false;
+
+        while (!flag){
+            Scanner in = new Scanner(System.in);
+            String s = in.nextLine();
+            if (s.toLowerCase().equals("bye")){
+                System.out.println(byeLine);
+                flag = true;
+            }
+            else {
+                printCommand(s);
+            }
+        }
     }
 }
