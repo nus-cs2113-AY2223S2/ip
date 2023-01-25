2b5f57ec1a23d5a2a5295e15d8586cdaee7df7f0
xiaoge26 <liuxiaoge0206@gmail.com>
2023-01-26 00:33:48 +0800

    Further tweak the code to comply with the coding standard

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,33 +1,28 @@
 public class Task {
     protected String description;
     protected boolean isDone;
-
-    protected void list(){
-
-    }
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
-    public void markAsDone(){
+    public void markAsDone() {
         this.isDone = true;
         System.out.println("Nice! I've marked this task as DONE:");
         System.out.println(taskStatus());
     }
 
-    public void markAsUndone(){
+    public void markAsUndone() {
         this.isDone = false;
         System.out.println("OK! I've marked this task as NOT DONE YET:");
         System.out.println(taskStatus());
     }
 
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        return (isDone ? "X" : " ");
     }
 
-    public String taskStatus(){
+    public String taskStatus() {
         return ("[" + getStatusIcon() + "] " + description);
     }
-    //...
 }
