08140de0ba64ea9e7d4ba309ac3b4cef7dea8d69
darrenlsx <darren.lim@live.com.sg>
2023-01-26 00:12:36 +0800

    Level-2 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,20 @@
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
-
-    public static void printCommand(String s){
-        String toPrint = "____________________________________________________________\n"
-                + s + "\n"
-                + "____________________________________________________________\n";
-        System.out.println(toPrint);
-
+    public static String linePrint = "____________________________________________________________\n";
+    public static void addList(String s, ArrayList<String> myList){
+        myList.add(s);
+        System.out.println(linePrint + "added: " + s + "\n" + linePrint);
     }
+    public static void printList(ArrayList<String> myList){
+        System.out.println(linePrint);
+        for (int i = 0; i < myList.size(); ++i) {
+            System.out.println(i+1 + ". " + myList.get(i));
+        }
+        System.out.println(linePrint);
+    }
+
     public static void main(String[] args) {
         String logo = "____________________________________________________________\n"
                 + "Hello! I'm Duke\n"
@@ -17,16 +23,21 @@ public class Duke {
         String byeLine = "Bye. Hope to see you again soon!\n";
         System.out.println(logo);
         boolean flag = false;
+        ArrayList<String> myList = new ArrayList<String>();
 
         while (!flag){
             Scanner in = new Scanner(System.in);
             String s = in.nextLine();
+
             if (s.toLowerCase().equals("bye")){
                 System.out.println(byeLine);
                 flag = true;
             }
+            else if (s.toLowerCase().equals("list")){
+                printList(myList);
+            }
             else {
-                printCommand(s);
+                addList(s, myList);
             }
         }
     }
