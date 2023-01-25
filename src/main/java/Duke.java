bc93fd2e550c1eddef7e2f9f2c63158eabf38d5d
DESKTOP-VAFN8PH\Gerald <geraldkoh4@gmail.com>
2023-01-25 23:41:02 +0800

    Finished Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,9 +9,17 @@ public class Duke {
                 + "__________________________\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! Do you need anything from me?\n"
-                + "I have only been trained to greet you so far.\n"
+                + "I have only been trained to greet and echo you so far.\n"
                 + "Once my owner is more proficient in what he does, he will give me more functions!");
         //To be added to make sure user input is read.
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (!line.equals("Bye")) {
+            System.out.println(line);
+            line = in.nextLine();
+        }
         System.out.println("That's all from me! Goodbye!");
     }
 }
