9877b901dc55b31b0c85f3b360fde92591e77155
QUANGANH <e0774912@u.nus.edu>
2023-01-26 02:48:24 +0800

    Add ability to echo input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,6 +10,12 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you ?");
+        Scanner ScanObj = new Scanner(System.in);
+        String UserCmd = "no";
+        while (!UserCmd.equals("bye")) {
+            UserCmd = ScanObj.nextLine();
+            System.out.println(UserCmd);
+        }
         System.out.println("Bye. Hope to see you again soon !");
     }
 }
