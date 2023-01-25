67b06d551cbac27679ebe164e92f6b7e8810c10e
ngyida <e0726193@u.nus.edu>
2023-01-25 18:17:04 +0800

    Add Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,6 +6,10 @@ public class Duke {
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
     private static String line = "____________________________________________________________\n";
+    private static int textStoreSize = 0;
+    private static int maxTextStoreSize = 100;
+    private static String[] textStore = new String[maxTextStoreSize];
+
     public static void main(String[] args) {
         greetUser();
         doTask();
@@ -24,6 +28,25 @@ public class Duke {
         System.out.println(line + " " + input + "\n" + line);
     }
 
+    private static void addTextStore(String text) {
+        System.out.print(line);
+        if (textStoreSize >= maxTextStoreSize) {
+            System.out.println("failed to be added as store is full\n" + line);
+            return;
+        }
+        textStore[textStoreSize] = text;
+        textStoreSize += 1;
+        System.out.println(" added: " + text);
+        System.out.print(line);
+    }
+
+    private static void listTextStore() {
+        System.out.print(line);
+        for (int i = 0; i < textStoreSize; i++) {
+            System.out.println(Integer.toString(i+1) + ": " + textStore[i]);
+        }
+        System.out.print(line);
+    }
     private static void doTask() {
         String input;
         Scanner in = new Scanner(System.in);
@@ -32,8 +55,11 @@ public class Duke {
             if (input.equals("bye")) {
                 exitDuke();
                 return;
+            } else if (input.equals("list")) {
+                listTextStore();
             } else {
-                echoString(input);
+                // echoString(input);
+                addTextStore(input);
             }
         }
     }
