58b8bbf493fcbd05b1a2c012bf31a679b3584103
guillaume-grn <guillaume.gerony@hotmail.com>
2023-01-25 20:26:27 +0800

    Duke : add ask and echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String ask() {
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        
+        line = in.nextLine();
+        return line;
+        }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,8 +20,22 @@ public class Duke {
         System.out.println(" Hello! I'm Duke ");
         System.out.println(" What can I do for you? \n");
         System.out.println("____________________________________________________________\n");
-        System.out.println(" Bye. Hope to see you again soon!\n");
-        System.out.println("____________________________________________________________");
+        boolean not_finished= true;
+        while (not_finished==true) {
+            String line=ask();
+            if (line.equals("bye")){
+                System.out.println("____________________________________________________________\n");
+                System.out.println("\t Bye. Hope to see you again soon!\n");
+                System.out.println("____________________________________________________________\n");
+                not_finished=false;
+            }
+            else{
+                System.out.println("____________________________________________________________");
+                System.out.println("\t "+line);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
 
     }
 }
