6692eca843f417274404f27c6627e72e439d03e5
geeeetyx <e0725682@u.nus.edu>
2023-01-25 16:26:04 +0800

    fix: add indentations to level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,52 +1,45 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+	public static void main(String[] args) {
 
+		//level-2
+		System.out.println("    ____________________________________");
+		System.out.println("    Hello! I'm Duke");
+		System.out.println("    What can I do for you?\n");
+		System.out.println("    ____________________________________");
 
-        //level-2
-        System.out.println("    ____________________________________");
-        System.out.println("    Hello! I'm Duke");
-        System.out.println("    What can I do for you?\n");
-        System.out.println("    ____________________________________");
+		String[] userInputs = new String[100];
+		int index = 0;
 
-        String[] userInputs = new String[100];
-        int index = 0;
+		Scanner userInput = new Scanner(System.in);
+		while (true) {
+			String text = userInput.nextLine();
+			if ("bye".equalsIgnoreCase(text)) {
+				break;
+			}
 
-        Scanner userInput = new Scanner(System.in);
-        while (true) {
-            String text = userInput.nextLine();
-            if ("bye".equalsIgnoreCase(text)) {
-                break;
-            }
+			if ("list".equalsIgnoreCase(text)) {
+				System.out.println("    ____________________________________");
+				for (int i = 0; i < userInputs.length; i += 1) {
+					if (userInputs[i] == null) {
+						break;
+					}
+					System.out.println("     " + (i + 1) + ". " + userInputs[i]);
+				}
+				System.out.println("    ____________________________________");
+			} else {
+				userInputs[index] = text;
+				index += 1;
 
-            if ("list".equalsIgnoreCase(text)) {
-                for (int i = 0; i < userInputs.length; i += 1) {
-                    if (userInputs[i] == null) {
-                        break;
-                    }
-                    System.out.println((i+1) + ". " + userInputs[i]);
-                }
-                System.out.println("    ____________________________________");
-            } else {
-                userInputs[index] = text;
+				System.out.println("    ____________________________________");
+				System.out.println("     added: " + text);
+				System.out.println("    ____________________________________");
+			}
+		}
 
-                System.out.println("    ____________________________________");
-                System.out.println("     added: " + userInputs[index]);
-                System.out.println("    ____________________________________");
-
-                index += 1;
-            }
-        }
-
-        System.out.println("____________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________");
-    }
+		System.out.println("    ____________________________________");
+		System.out.println("    Bye. Hope to see you again soon!");
+		System.out.println("    ____________________________________");
+	}
 }
