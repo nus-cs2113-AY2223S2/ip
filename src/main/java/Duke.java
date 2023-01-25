592da3724f2ab87b683b8f3405786c1dd8c4712c
raydent30 <raydentwx@gmail.com>
2023-01-25 11:49:05 +0800

    Duke class: add A-CodingStandard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,11 +21,11 @@ public class Duke {
 
         String userCommand = scanner.nextLine();
 
-        boolean exit = false;
+        boolean isExit = false;
         Task[] storedUserTasks = new Task[100];
         int taskIndex = -1;
         int userTextCount = 0;
-        while(!exit){
+        while(!isExit){
             if(userCommand.split(" ")[0].equals("mark")||userCommand.split(" ")[0].equals("unmark")){
                 try {
                     taskIndex = Integer.parseInt(userCommand.split(" ")[1]);
@@ -36,42 +36,42 @@ public class Duke {
                 }
             }
             switch (userCommand){
-                case "list":
-                    System.out.println(divider);
-                    System.out.println("Here are the tasks in your lists:");
-                    for(int i=0;i<userTextCount;i++){
-                        System.out.println((i+1)+"."+"["+storedUserTasks[i].getStatusIcon()+"] "+storedUserTasks[i].description);
-                    }
-                    System.out.println(divider);
-                    userCommand = scanner.nextLine();
-                    break;
-                case "bye":
-                    exit = true;
-                    break;
-                case "mark":
-                    System.out.println(divider);
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("[X] "+storedUserTasks[taskIndex-1].description);
-                    storedUserTasks[taskIndex-1].isDone = true;
-                    System.out.println(divider);
-                    userCommand = scanner.nextLine();
-                    break;
-                case "unmark":
-                    System.out.println(divider);
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println("[ ] "+storedUserTasks[taskIndex-1].description);
-                    storedUserTasks[taskIndex-1].isDone = false;
-                    System.out.println(divider);
-                    userCommand = scanner.nextLine();
-                    break;
-                default:
-                    System.out.println(divider);
-                    System.out.println("added: " + userCommand);
-                    System.out.println(divider);
-                    storedUserTasks[userTextCount] = new Task(userCommand);
-                    userCommand = scanner.nextLine();
-                    userTextCount++;
-                    break;
+            case "list":
+                System.out.println(divider);
+                System.out.println("Here are the tasks in your lists:");
+                for(int i=0;i<userTextCount;i++){
+                    System.out.println((i+1)+"."+"["+storedUserTasks[i].getStatusIcon()+"] "+storedUserTasks[i].description);
+                }
+                System.out.println(divider);
+                userCommand = scanner.nextLine();
+                break;
+            case "bye":
+                isExit = true;
+                break;
+            case "mark":
+                System.out.println(divider);
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] "+storedUserTasks[taskIndex-1].description);
+                storedUserTasks[taskIndex-1].isDone = true;
+                System.out.println(divider);
+                userCommand = scanner.nextLine();
+                break;
+            case "unmark":
+                System.out.println(divider);
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[ ] "+storedUserTasks[taskIndex-1].description);
+                storedUserTasks[taskIndex-1].isDone = false;
+                System.out.println(divider);
+                userCommand = scanner.nextLine();
+                break;
+            default:
+                System.out.println(divider);
+                System.out.println("added: " + userCommand);
+                System.out.println(divider);
+                storedUserTasks[userTextCount] = new Task(userCommand);
+                userCommand = scanner.nextLine();
+                userTextCount++;
+                break;
             }
         }
 
