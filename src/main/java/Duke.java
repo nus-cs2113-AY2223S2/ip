6fca4fbe6fefa23bef72e60e4a09ae899ef4b70f
MuxPotato <maxchewjw@gmail.com>
2023-01-26 01:24:48 +0800

    Add user command and respond to various user command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,36 @@
+
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        String userCommand;
+        Scanner in = new Scanner(System.in);
+
+
+        Boolean isContinue = true;
+        while (isContinue) {
+            userCommand = in.nextLine();
+            switch (userCommand) {
+
+            case "list":
+                System.out.println("list");
+                break;
+
+            case "blah":
+                System.out.println("blah");
+                break;
+
+            case "bye":
+                System.out.println("Bye. Hope to see you again soon!");
+                isContinue = false;
+                break;
+
+            default:
+                System.out.println("Sorry, I do not understand what you input");
+                break;
+            }
+        }
     }
 }
