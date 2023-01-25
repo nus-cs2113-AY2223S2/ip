d97cacd2592ef7f050c43ff78387811c6a1a1503
Gan868611 <y.zganz.y@gmail.com>
2023-01-25 17:55:27 +0800

    Add Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void startBot(){
+        Scanner in = new Scanner((System.in));
+        while(true){
+            String input  = in.nextLine();
+            if(input.equals("bye")){
+                return;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+
+        }
+    }
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
+                " What can I do for you?\n");
+
+        startBot();
+        System.out.println( "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
+
     }
 }
