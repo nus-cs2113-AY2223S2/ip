c3bc4579ae54bf22f5bf3df4024296c7b1042e6d
marekpinto <marek.pinto@gmail.com>
2023-01-25 18:48:05 +0800

    Level-1: Echo commands back to user and exit on bye command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,12 +8,30 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         //System.out.println("Hello from\n" + logo);
+        Scanner in = new Scanner(System.in);
+        
         String line = "____________________________________________________________";
 
         System.out.println(line);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        
+        while(true){
+            System.out.println();
+            String cmd = in.nextLine();
+        
+            if (cmd.equals("bye")){
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            else{
+                System.out.println(line);
+                System.out.println(cmd);
+                System.out.println(line);
+            }
+        }   
+        
     }
 }
