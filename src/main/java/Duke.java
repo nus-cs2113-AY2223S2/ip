3331175e443f07eadf91069b320c3f0d6e091f82
LAPTOP-52GQO9KI\zhito <e0725202@u.nus.edu>
2023-01-26 19:44:41 +0800

    added echo feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         DukeMessage.greet();
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        DukeMessage.lineBreak();
+        line = in.nextLine();
+
+            while (!line.equals("bye")){
+
+                System.out.println(line);
+                DukeMessage.lineBreak();
+                line = in.nextLine();
+
+            }
+
         DukeMessage.goodbye();
 
     }
