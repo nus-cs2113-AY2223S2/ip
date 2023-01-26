ecc9761324de7ace923fedcbde79d8cedb7a758c
typingpanda <chanxz@keane.gmail.com>
2023-01-26 09:42:15 +0800

    add echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,19 @@
+
+// import java.util.Arrays;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?\n");
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        Scanner in = new Scanner(System.in);
+        String line;
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println(line + "\n");
+            line = in.nextLine();
+        }
+
+        in.close();
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
