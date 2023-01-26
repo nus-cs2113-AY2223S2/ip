1c0a91b0ef3a8fc2ea9fd9ecf5afc4c3dc5fb56e
LAPTOP-52GQO9KI\zhito <e0725202@u.nus.edu>
2023-01-26 20:05:02 +0800

    Added the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,16 +4,29 @@ public class Duke {
     public static void main(String[] args) {
         DukeMessage.greet();
 
+        int index = 1;
         String line;
+        String[] storedLines = new String[102];
         Scanner in = new Scanner(System.in);
         DukeMessage.lineBreak();
         line = in.nextLine();
 
             while (!line.equals("bye")){
 
-                System.out.println(line);
-                DukeMessage.lineBreak();
-                line = in.nextLine();
+                if(line.equals("list")){
+                    for (int i = 1; i < index; i++){
+                        System.out.println(i + ": " + storedLines[i]);
+                    }
+                }
+                else {
+                    System.out.println("added: " + line);
+                    storedLines[index] = line;
+                    index +=1;
+                }
+
+                    DukeMessage.lineBreak();
+                    line = in.nextLine();
+
 
             }
 
