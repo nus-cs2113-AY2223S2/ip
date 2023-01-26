46f2567351594b908e8ec8617e5bc7743835baf9
tyuyang <tayyuyang@gmail.com>
2023-01-26 14:25:12 +0800

    Added level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
+        //initial greeting
         System.out.println("____________________________________________________________");
         System.out.println("Hello, I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+
+
+        //echo
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String line = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            System.out.println(line);
+            System.out.println("____________________________________________________________");
+            if (line.equals("bye")) {
+                break;
+            }
+
+        }
+
+        //end greeting
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
