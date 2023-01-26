06b5531ade889a67ec25e6bb89d5e542ac6f3db2
guillaume-grn <guillaume.gerony@hotmail.com>
2023-01-26 10:53:39 +0800

    Duke : Add items storage and list display

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,33 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static String[] createList(){
+        String[] commands_list=new String[100];
+        commands_list[99]="1";
+        return commands_list;
+    }
+    public static int next_index(String[] commands_list){
+        return Integer.parseInt(commands_list[99]);
+    }
+    public static void addToList(String line,String[] commands_list){
+        int next_index=next_index(commands_list);
+        commands_list[next_index]=line;
+        next_index++;
+        commands_list[99]=Integer.toString(next_index);
+    }
+    public static void displayCommandsList(String[] commands_list){
+        int next_index=next_index(commands_list);
+        int i=1;
+        System.out.println("\t_____________________________________________________");
+        while (i<next_index){
+            System.out.println("\t"+i+". "+commands_list[i]);
+            i++;
+        }
+        System.out.println("\t_____________________________________________________");
+    }
     public static String ask() {
         String line;
         Scanner in = new Scanner(System.in);
-
-        
         line = in.nextLine();
         return line;
         }
@@ -20,6 +42,7 @@ public class Duke {
         System.out.println(" Hello! I'm Duke ");
         System.out.println(" What can I do for you? \n");
         System.out.println("____________________________________________________________\n");
+        String[] commands_list=createList();
         boolean not_finished= true;
         while (not_finished==true) {
             String line=ask();
@@ -29,10 +52,14 @@ public class Duke {
                 System.out.println("____________________________________________________________\n");
                 not_finished=false;
             }
+            else if (line.equals("list")){
+                displayCommandsList(commands_list);
+            }
             else{
-                System.out.println("____________________________________________________________");
-                System.out.println("\t "+line);
-                System.out.println("____________________________________________________________");
+                addToList(line, commands_list);
+                System.out.println("\t_____________________________________________________");
+                System.out.println("\t added: "+line);
+                System.out.println("\t_____________________________________________________");
             }
         }
 
