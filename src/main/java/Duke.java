30e0479d7096fdd8841f51edbfc5a9dc7d290b07
Benjamin Toh <toh.benjamin123@gmail.com>
2023-01-26 17:05:24 +0800

    Add Todos,Events and Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static Task[] tasks = new Task[100];
-    private static int taskSize = 0;
+    private static Task[] taskList = new Task[100];
+    private static int listSize = 0;
 
     public static void printDivider() {
         String divider = "\t____________________________________";
@@ -20,7 +20,8 @@ public class Duke {
     }
 
     public static void printWelcome() {
-        String welcomeMessage = "\tHello! I'm Duke\n\tWhat can I do for you?";
+        String welcomeMessage = "\tHello! I'm Duke\n" 
+                            + "\tWhat can I do for you?";
 
         printDivider();
         printLogo();
@@ -35,40 +36,95 @@ public class Duke {
         printDivider();
     }
 
-    public static void addTasks(String inputMessage) {
-        Task newTask = new Task(inputMessage);
-        tasks[taskSize] = newTask;
-        taskSize++;
-        String addedMessage = "\tadded: " + inputMessage;
-
-        printDivider();
-        System.out.println(addedMessage);
-        printDivider();
-    }
-
     public static void printTasks() {
         printDivider();
-        for (int i = 0; i < taskSize; i++) {
-            System.out.println("\t" + (i + 1) + ". " + tasks[i].toString());
+        System.out.println("\tHere are the tasks in your list:");
+        for (int i = 0; i < listSize; i++) {
+            System.out.println("\t" + (i + 1) + ". " + taskList[i]);
         }
         printDivider();
     }
 
+    public static void addTodo(String inputMessage) {
+        Task todo = new Todo(inputMessage);
+        if (listSize >= 100) {
+            System.out.println("List size exceeded!");
+            return;
+        }
+        taskList[listSize] = todo;
+        listSize++;
+
+        String addedMessage = "\tGot it. I've added this task:\n" 
+                                + "\t  " + todo.toString();
+        String sizeMessage = "\tNow you have " + listSize + " tasks in the list.";
+
+        printDivider();
+        System.out.println(addedMessage);
+        System.out.println(sizeMessage);
+        printDivider();
+    }
+
+    public static void addDeadline(String inputMessage, String by) {
+        Task deadline = new Deadline(inputMessage, by);
+        if (listSize >= 100) {
+            System.out.println("List size exceeded!");
+            return;
+        }
+        taskList[listSize] = deadline;
+        listSize++;
+
+        String addedMessage = "\tGot it. I've added this task:\n" 
+                                + "\t  " + deadline.toString();
+        String sizeMessage = "\tNow you have " + listSize + " tasks in the list.";
+
+        printDivider();
+        System.out.println(addedMessage);
+        System.out.println(sizeMessage);
+        printDivider();
+    }
+
+    public static void addEvent(String inputMessage, String from, String to) {
+        Task event = new Event(inputMessage, from, to);
+        if (listSize >= 100) {
+            System.out.println("List size exceeded!");
+            return;
+        }
+        taskList[listSize] = event;
+        listSize++;
+
+        String addedMessage = "\tGot it. I've added this task:\n" 
+                                + "\t  " + event.toString();
+        String sizeMessage = "\tNow you have " + listSize + " tasks in the list.";
+
+        printDivider();
+        System.out.println(addedMessage);
+        System.out.println(sizeMessage);
+        printDivider();
+    }
+
     public static void markTask(int taskNum) {
-        Task currentTask = tasks[taskNum - 1];
+        Task currentTask = taskList[taskNum - 1];
+        if (taskNum > listSize || taskNum <= 0){
+            System.out.println("\tNo such task exists! Please try again");
+            return;
+        }
         currentTask.markDone();
 
-        String markMessage = "\tNice! I've marked this task as done:\n" + "\t" + currentTask.toString();
+        String markMessage = "\tNice! I've marked this task as done:\n" + "\t  " + currentTask.toString();
         printDivider();
         System.out.println(markMessage);
         printDivider();
     }
 
     public static void unmarkTask(int taskNum) {
-        Task currentTask = tasks[taskNum - 1];
+        Task currentTask = taskList[taskNum - 1];
+        if (taskNum > listSize || taskNum <= 0){
+            System.out.println("\tNo such task exists! Please try again");
+            return;
+        }
         currentTask.markUndone();
 
-        String unmarkMessage = "\tOK, I've marked this task as not done yet:" + "\t" + currentTask.toString();
+        String unmarkMessage = "\tOK, I've marked this task as not done yet:" + "\t  " + currentTask.toString();
 
         printDivider();
         System.out.println(unmarkMessage);
@@ -82,13 +138,38 @@ public class Duke {
 
         while (input.hasNextLine()) {
             String inputString = input.nextLine();
-            String[] inputArray = inputString.split(" ");
+            String[] inputArray = inputString.split(" ",2);
             String command = inputArray[0];
 
             switch (command) {
-            case "bye":
-                printExit();
-                System.exit(0);
+            case "todo":
+                String todoDetails = inputArray[1];
+                addTodo(todoDetails);
+                break;
+
+            case "deadline":
+                if (!inputArray[1].contains("/by")){
+                    System.out.println("\tWrong format! Please include /by.");
+                    break;
+                }
+                
+                String[] deadlineDetails = inputArray[1].split(" /by ",2);
+                String deadlineDescription = deadlineDetails[0];
+                String by = deadlineDetails[1];
+                addDeadline(deadlineDescription, by);
+                break;
+
+            case "event":
+                if (!inputArray[1].contains("/from") && !inputArray[1].contains("/to")) {
+                    System.out.println("\tWrong format! Please include /from and /to.");
+                    break;
+                }
+
+                String[] eventDetails = inputArray[1].split(" /from | /to ",3);
+                String eventDescription = eventDetails[0];
+                String from = eventDetails[1];
+                String to = eventDetails[2];
+                addEvent(eventDescription, from, to);
                 break;
 
             case "list":
@@ -105,8 +186,13 @@ public class Duke {
                 unmarkTask(unmarkTaskNum);
                 break;
 
+            case "bye":
+                printExit();
+                System.exit(0);
+                break;
+            
             default:
-                addTasks(inputString);
+                break;
             }
         }
     }
