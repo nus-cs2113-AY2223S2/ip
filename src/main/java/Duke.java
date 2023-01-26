ad606d7f8cb16c8fc7116c5b7f2df5a000730f40
kyrixn <keyrixn@gmail.com>
2023-01-26 23:11:52 +0800

    finished level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,30 @@
-public class Duke {
+import java.util.Scanner;
 
-    public static void printGreet() {
-        String line = "____________________________________________________________";
+public class Duke {
+    private static String line = "---------------------------------------------------------";
+    
+    private static void printGreeting() {        
         System.out.println(line);
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Hello! I'm Duke"+System.lineSeparator()+"What can I do for you?");
         System.out.println(line);
     }
-
     public static void main(String[] args) {
-        printGreet();
+        printGreeting();
+        Scanner in = new Scanner(System.in);
+        String command = in.nextLine();
+        
+        while(!command.equals("bye")) {
+            System.out.println(line);
+            System.out.println("    > "+command);
+            System.out.println(line);
+
+            command = in.nextLine();
+        }
+        
+        System.out.println(line);
+        System.out.println("    > Bye. Hope to see you again soon!");
+        System.out.println(line);
+
+        in.close();
     }
 }
