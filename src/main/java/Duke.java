fdcb4b9bb87965ac952275a37628865adf434b15
saint thiri zaw <saintstz@gmail.com>
2023-01-26 17:40:14 +0800

    Added Mark As Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,25 +23,48 @@ public class Duke {
             if (command.equals("list"))
             {
                 System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 1; i <= count; i++ )
                 {
-                    System.out.println(i + ". " + list[i-1]);
+                    System.out.println(i + "." + list[i-1]);
                 }
                 System.out.println("____________________________________________________________");
-                command = in.nextLine(); //read next command
-
             } else {
-                System.out.println("____________________________________________________________");
-                System.out.println("added: " + command); //new item added
-                System.out.println("____________________________________________________________");
-                list[count] = command;
-                count++;
-                command = in.nextLine(); //read next command
+                String arr[] = command.split(" ", 2);
+                //mark task as done
+                if (arr[0].equals("mark")) {
+                    int index = Integer.parseInt(arr[1]);
+                    String cmd[] = list[index-1].split("] ");
+                    list[index-1] = "[X] " + cmd[1]; //cmd[1] is the original command
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(list[index-1]);
+                    System.out.println("____________________________________________________________");
+                }
+                //unmark task
+                else if (arr[0].equals("unmark")) {
+                    int index = Integer.parseInt(arr[1]);
+                    String cmd[] = list[index - 1].split("] ");
+                    list[index - 1] = "[ ] " + cmd[1]; //cmd[1] is the original command
+                    System.out.println("____________________________________________________________");
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(list[index - 1]);
+                    System.out.println("____________________________________________________________");
+                }
+                //add new task
+                else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println("added: " + command); //new item added
+                    System.out.println("____________________________________________________________");
+                    list[count] = "[ ] " + command;
+                    count++;
+                }
             }
+            command = in.nextLine(); //read next command
         }
 
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Bye. Hope to see you again soon! :)");
         System.out.println("____________________________________________________________");
     }
 }
