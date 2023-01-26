545958084ae574eb48a52fc137e126e0b15324b2
Richardtok <e0902030@u.nus.edu>
2023-01-26 15:51:59 +0800

    Revert "Level 0"
    This reverts commit 8460868370b12f6d5e3931500592092d22765d3c.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = "Hello! I'm Duke\n"+"What can I do for you?\n"+"Bye!. Hope to see you again.\n";
-        System.out.println(logo);
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
     }
 }
