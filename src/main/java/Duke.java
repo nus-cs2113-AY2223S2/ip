dc3f098821c6f54123f872cecff93a566352b36b
Zemdalk <lvxingyu20@mails.ucas.ac.cn>
2023-01-26 14:35:00 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,13 +8,26 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        String enter_greet = "____________________________________________________________\n"
-                           + " Hello! I'm Duke\n"
-                           + " What can I do for you?";
-        String exit_prompt = "____________________________________________________________\n"
-                           + " Bye. Hope to see you again soon!\n"
-                           + "____________________________________________________________";
+        String enter_greet = "    ____________________________________________________________\n"
+                           + "     Hello! I'm Duke\n"
+                           + "     What can I do for you?\n"
+                           + "    ____________________________________________________________\n";
+        String exit_prompt = "    ____________________________________________________________\n"
+                           + "     Bye. Hope to see you again soon!\n"
+                           + "    ____________________________________________________________\n";
         System.out.println(enter_greet);
-        System.out.println(exit_prompt);
+        while(true){
+            Scanner in = new Scanner(System.in);
+            String line = in.next();            // get user input
+            if(line.equals("bye")){             // quit
+                System.out.println(exit_prompt);
+                break;
+            }else{                              // simply echo the input
+                String repeatInput = "    ____________________________________________________________\n"
+                                   + "    " + line + "\n"
+                                   + "    ____________________________________________________________\n";
+                System.out.println(repeatInput);
+            }
+        }
     }
 }
