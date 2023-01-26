a4d1640788d370ba543a46959174a0d823ba568d
ZiqiuZeng <88576865+ZiqiuZeng@users.noreply.github.com>
2023-01-27 00:55:58 +0800

    Increment: Duke Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 public class Duke {
+    public static void printList(String[] tasks, int taskCount){
+        for(int i = 0; i < taskCount; i++){
+            System.out.println(tasks[i]);
+        }
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,12 +13,22 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke" + System.lineSeparator() + "What can I do for you?");
+        String[] tasks;
+        tasks = new String[100];
+        int taskCount = 0;
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         while(!line.equals("bye")){
-            System.out.println(line);
+            if(line.equals("list")){
+                printList(tasks, taskCount);
+            }
+            else{
+                tasks[taskCount] = line;
+                System.out.println("added: " + line);
+            }
             line = in.nextLine();
+            taskCount++;
         }
         System.out.println("Bye. Hope to see you again soon!");
 
