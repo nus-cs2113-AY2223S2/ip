f43bb95c3a34bd56b3fd1c3cc61d2c1012453415
kyrixn <keyrixn@gmail.com>
2023-01-26 23:29:36 +0800

    finished level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 
 public class Duke {
     private static String line = "---------------------------------------------------------";
-    
+    private static Task taskList[] = new Task[100];
+    private static int taskNum = 0;
+
     private static void printGreeting() {        
         System.out.println(line);
         System.out.println("Hello! I'm Duke"+System.lineSeparator()+"What can I do for you?");
+        System.out.println(line);
+    }
+
+    private static void showTasks() {
+        System.out.println(line);
+        for(int i =0; i <taskNum; i++) {
+            System.out.println("   > "+Integer.toString(i+1)+". "+taskList[i].getTaskDiscription());
+        }
         System.out.println(line);
     }
     public static void main(String[] args) {
@@ -14,9 +24,16 @@ public class Duke {
         String command = in.nextLine();
         
         while(!command.equals("bye")) {
-            System.out.println(line);
-            System.out.println("    > "+command);
-            System.out.println(line);
+            if (command.equals("list")) {
+                showTasks();
+            }
+            else {
+                System.out.println(line);
+                System.out.println("   > add: "+command);
+                System.out.println(line);
+
+                taskList[taskNum++] = new Task(command);
+            }
 
             command = in.nextLine();
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,17 +1,11 @@
 public class Task {
     protected String taskDiscription;
-    protected boolean isDone;
 
     public Task(String task){
         this.taskDiscription = task;
-        this.isDone = false;
     }
 
-    public String getTaskStatus() {
-        return (this.isDone ? "X" : " ");
-    }
-
-    public void markAsDone(){
-        this.isDone = true;
+    public String getTaskDiscription() {
+        return this.taskDiscription;
     }
 }
