f9bde6c7eb0af1f0847fa2f091c0bc1094c01d86
ZiqiuZeng <88576865+ZiqiuZeng@users.noreply.github.com>
2023-01-27 01:48:58 +0800

    Increment: Duke Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 public class Duke {
-    public static void printList(String[] tasks, int taskCount){
+    public static void printList(Task[] tasks, int taskCount){
+        System.out.println("Here are the tasks in your list:");
         for(int i = 0; i < taskCount; i++){
-            System.out.println(tasks[i]);
+            System.out.println(i + 1 + "." + "[" + tasks[i].getStatusIcon() + "] " + tasks[i].description);
         }
     }
     public static void main(String[] args) {
@@ -13,8 +14,8 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke" + System.lineSeparator() + "What can I do for you?");
-        String[] tasks;
-        tasks = new String[100];
+        Task[] tasks;
+        tasks = new Task[100];
         int taskCount = 0;
         String line;
         Scanner in = new Scanner(System.in);
@@ -23,12 +24,24 @@ public class Duke {
             if(line.equals("list")){
                 printList(tasks, taskCount);
             }
+            else if(line.contains("mark") && !line.contains("unmark")){
+                int taskNum = Integer.parseInt(line.substring(5)) - 1;
+                tasks[taskNum].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + tasks[taskNum].getStatusIcon() + "] " + tasks[taskNum].description);
+            }
+            else if(line.contains("unmark")){
+                int unmarkNum = Integer.parseInt(line.substring(7)) - 1;
+                tasks[unmarkNum].unmarkAsDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[" + tasks[unmarkNum].getStatusIcon() + "] " + tasks[unmarkNum].description);
+            }
             else{
-                tasks[taskCount] = line;
+                tasks[taskCount] = new Task(line);
                 System.out.println("added: " + line);
+                taskCount++;
             }
             line = in.nextLine();
-            taskCount++;
         }
         System.out.println("Bye. Hope to see you again soon!");
 
