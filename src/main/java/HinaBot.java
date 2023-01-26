7faa1cb6252a3d11347155913a847707a411833b
waiter-palypoo <gohjinghong2000@gmail.com>
2023-01-26 13:12:38 +0800

    Add HinaBot.java

---
+++ src/main/java/HinaBot.java
@@ -0,0 +1,7 @@
+public class HinaBot {
+    public static void main(String[] args) {
+        System.out.println("Hello master!");
+        System.out.println("What are your orders?");
+        System.out.println("Goodbye master, let's meet again soon...");
+    }
+}
