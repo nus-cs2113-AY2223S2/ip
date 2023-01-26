38e3d60c3804d9c7c551e3b24f43219eb5a8dd75
nichyjt <nicholas.yek@gmail.com>
2023-01-26 11:38:49 +0800

    Add new command handlers - <mark> and <unmark> tasks. Refactor all commands to run under a switch case. Refactor tasklist to use a Task class instead of a String primitive.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 // TODO: Add docstrings for the functions
 public class Duke {
     private static boolean keepAlive;
-    private static String[] tasklist = new String[100];
+    private static Task[] tasklist = new Task[100];
     private static int numTasks;
 
 
@@ -16,33 +16,74 @@ public class Duke {
     }
 
     public static void Exit(){
-        PrintBorder();
         System.out.print("Goodbye! Thank you for using Duke.\n");
-        PrintBorder();
     }
 
     // TODO: Add more robust command string handling
     // TODO: Add a '$' Symbol to hint user to type in a command
-    public static void handleCommand(String cmd){
+    public static void handleCommand(String command){
         // Update the keepAlive flag
         // TODO: Refactor into a switch case when commands start to get bloated
-        if(cmd.equals("Bye") || cmd.equals("bye") || cmd.equals("exit")){
-            // Termination
+        String[] commandList = command.split(" ");
+        String cmd = commandList[0];
+        switch (cmd){
+        case "Bye":
+        case "bye":
+        case "exit":
             setKeepAlive(false);
-        }else if (cmd.equals("list")){
-            PrintBorder();
+            Exit();
+            break;
+        case "list":
+            System.out.println("Here's what's in your list:");
             for(int i=0; i<numTasks; ++i){
-                System.out.println(i+1 + ". " + tasklist[i]);
+                // Print number, box, description in that order
+                Task curr = tasklist[i];
+                System.out.print(i+1 + ". " + curr.getDescription() +'\n');
             }
-            PrintBorder();
-        }else{
-            // Echo out the command
-            PrintBorder();
-            System.out.println("Okay, adding: \"" + cmd +"\"!");
-            PrintBorder();
+            break;
+        case "mark":
+            if(commandList.length < 2){
+                System.out.println("Missing argument: Task number");
+                return;
+            }
+            // TODO: parsing int from command is inherently unsafe. Should try-catch this
+            int taskNum = Integer.parseInt(commandList[1]) - 1; // Convert to 0-idx
+            // Update its value
+            tasklist[taskNum].markAsDone();
+            // TODO: add an additional check to see if it is already marked as done and print different text
+            System.out.println("Okay, marking this task as done: ");
+            System.out.println(tasklist[taskNum].getDescription());
+            break;
+        case "unmark":
+            if(commandList.length < 2){
+                System.out.println("Missing argument: Task number");
+                return;
+            }
+            // TODO: parsing int from command is inherently unsafe. Should try-catch this
+            taskNum = Integer.parseInt(commandList[1]) - 1; // Convert to 0-idx
+            // Update its value
+            tasklist[taskNum].markAsUndone();
+            // TODO: add an additional check to see if it is already marked as done and print different text
+            System.out.println("Okay, setting this task as undone: ");
+            System.out.println(tasklist[taskNum].getDescription());
+            break;
+        default:
+            // Default behaviour is to add the command
             // Insert command into tasklist
-            tasklist[numTasks] = cmd;
+            String desc = "";
+            // TODO: Use a more efficient way of string building
+            for(String s: commandList){
+                if(desc.length() > 0){
+                    desc = desc + " " + s;
+                }else{
+                    desc = s;
+                }
+
+            }
+            Task t = new Task(desc);
+            tasklist[numTasks] = t;
             ++numTasks;
+            System.out.println("Okay, adding: \"" + desc +"\"!");
         }
     }
 
@@ -72,8 +113,9 @@ public class Duke {
         // Event driver loop
         while(keepAlive){
             String command = input.nextLine();
+            PrintBorder();
             handleCommand(command);
+            PrintBorder();
         }
-        Exit();
     }
 }
