711f1fc4b3e23c60b5bc482cb272669e80d41808
YongbinWang <yongbin_01@hotmail.com>
2023-01-26 20:21:17 +0800

    Level-0: Duke Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greet = "Hello! I'm Duke";
+        String question = "What can I do for you?";
+        String exit = "Bye. Hope to see you again soon!";
+        String horizontalLine = "____________________________________________________________";
+        System.out.println(horizontalLine);
+        System.out.println(greet);
+        System.out.println(question);
+        System.out.println(horizontalLine);
+        System.out.println(exit);
+        System.out.println(horizontalLine);
     }
 }
