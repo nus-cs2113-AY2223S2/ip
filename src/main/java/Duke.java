a83e00f51a9cb8d63b4c78a5839725c424281080
Elijah <ngfei25@gmail.com>
2023-01-26 22:37:35 +0800

    Add items

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static final String NAME = "Duke";
-    private static final String INITIAL_MSG = String.format("    Hello! I'm %s\n   What can I do for you?", NAME); 
+    private static final String BOT_NAME = "Duke";
+    private static final String INITIAL_MSG = String.format("    Hello! I'm %s\n   What can I do for you?", BOT_NAME); 
+    private static final String BYE_MSG = "     Bye. Hope to see you again soon!";
+    private static String[] itemList = new String[100];
+    private static int numItems = 0;
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,9 +19,32 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
-            printResponse(line);
+            handleInput(line);
         }
         scanner.close();
+    }
+
+    private static void handleInput(String line) {
+        if (line.equals("bye")) {
+            printResponse(BYE_MSG);
+        } else if (line.equals("list")) {
+            printItems();
+        } else { // add item
+            addItem(line);
+        }
+    }
+
+    private static void addItem(String item) {
+        itemList[numItems++] = item;
+        printResponse("    added: " + item);
+    }
+
+    private static void printItems() {
+        String response = "";
+        for (int i = 0; i < numItems; i++) {
+            response += String.format("   %d. %s\n", i+1, itemList[i]);
+        }
+        printResponse(response);
     }
 
     private static void printResponse(String response) {
