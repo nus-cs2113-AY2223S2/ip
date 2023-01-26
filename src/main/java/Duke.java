51710e30139e35417f00b1df99e4710202529987
wuzhzn <dagreatbao@gmail.com>
2023-01-26 15:00:47 +0800

    Add adding and list code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,39 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
         String input;
         Scanner in = new Scanner(System.in);
+        String[] items = new String[100];
+        int index = 0;
+        int itemCount = 0;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         final String HORIZONTAL_LINE = "____________________________________________________________";
+
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println(HORIZONTAL_LINE);
+
         do {
-            System.out.println("");
             input = in.nextLine();
             System.out.println(HORIZONTAL_LINE);
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
+            } else if (input.equals("list")) {
+                for (int i = 0; i < itemCount; i++) {
+                    System.out.println((i + 1) + ". " + items[i]);
+                }
             } else {
-                System.out.println(input);
+                items[index] = input;
+                index++;
+                itemCount++;
+                System.out.println("added: " + input);
             }
             System.out.println(HORIZONTAL_LINE);
         } while (!input.equals("bye"));
