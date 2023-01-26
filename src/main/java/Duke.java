3e9f9bdd4043f4763144b8faf6a02aabecffa30c
vishnuvk47 <e0727211@u.nus.edu>
2023-01-26 13:37:26 +0800

    made duke read and echo user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("what can i do for you");
+        String input = myObj.nextLine();
+        while(!input.equals("bye"))
+        {
+            System.out.println(input);
+            input = myObj.nextLine();
+        }
+        System.out.println("Bye! see you soon!");
+
     }
+
 }
