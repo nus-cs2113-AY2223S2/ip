6c21be7bab714267675b33e8a1607778cb832e38
ChongQiRong <e0727229@u.nus.edu>
2023-01-26 09:42:19 +0800

    Add Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,58 @@
 import java.util.Scanner;
+import java.util.Arrays;
+
 public class Duke {
+    public static void horizontalLine() {
+        System.out.println("____________________________________________________________");
+    }
+
     public static void greet() {
-        System.out.println("____________________________________________________________\n"
-                + "Hello! I'm Duke\nWhat can I do for you?\n"
-                + "____________________________________________________________\n");
+        horizontalLine();
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+        horizontalLine();
     }
 
     public static void exit() {
-        System.out.println("____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n");
+        horizontalLine();
+        System.out.println("Bye. Hope to see you again soon!\n");
+        horizontalLine();
     }
 
-    public static void echo(String input) {
-        System.out.println("____________________________________________________________\n"
-                + "added: " + input + "\n____________________________________________________________\n");
+    public static void echo(String userInput) {
+        horizontalLine();
+        System.out.println("added: " + userInput);
+        horizontalLine();
+    }
+
+    public static String[] processLine(String userInput) {
+        String[] words = userInput.split(" ");
+        return words;
+    }
+
+    public static void processCommands(Task[] tasks, String userInput) {
+        if (userInput.equals("list")) {
+            horizontalLine();
+            for (int i = 0; i < Task.getNumberOfTasks(); i += 1) {
+                System.out.println(tasks[i].getTaskNumber() + ".[" + tasks[i].getStatusIcon() + "]" + tasks[i].getDescription());
+            }
+            horizontalLine();
+        } else if (userInput.contains("mark")) {
+            String[] words = processLine(userInput);
+            Integer taskNumber = Integer.parseInt(words[1]) - 1;
+            if (words[0].equals("mark")) {
+                tasks[taskNumber].markDone();
+                System.out.println("Nice! I've marked this task as done:");
+            } else {
+                tasks[taskNumber].markUndone();
+                System.out.println("OK, I've marked this task as not done yet:");
+            }
+            horizontalLine();
+            System.out.println("[" + tasks[taskNumber].getStatusIcon() + "]" + tasks[taskNumber].getDescription());
+            horizontalLine();
+        } else {
+            echo(userInput);
+            tasks[Task.getNumberOfTasks()] = new Task(userInput);
+        }
     }
 
     public static void main(String[] args) {
@@ -24,28 +62,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        String[] tasks = new String[100];
-        int taskNumber = 0;
+        Task[] tasks = new Task[100];
         boolean byeCommand = false;
 
         greet();
-        while (byeCommand == false){
+        while (byeCommand == false) {
             Scanner in = new Scanner(System.in);
             String userInput = in.nextLine();
-            switch (userInput) {
-            case "bye":
+            if (userInput.equals("bye")) {
                 byeCommand = true;
-                break;
-            case "list":
-                for (int i = 0; i < taskNumber; i += 1){
-                    System.out.println((i + 1) + ". " + tasks[i]);
-                }
-                break;
-            default:
-                echo(userInput);
-                tasks[taskNumber] = userInput;
-                taskNumber += 1;
-                break;
+            } else {
+                processCommands(tasks, userInput);
             }
         }
         exit();
---
+++ src/main/java/Task.java
@@ -0,0 +1,35 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+    private int taskNumber;
+    private static int numberOfTasks = 0;
+
+    public Task(String description){
+        numberOfTasks += 1;
+        this.description = description;
+        this.isDone = false;
+        this.taskNumber = numberOfTasks;
+    }
+    public void markDone(){
+        isDone = true;
+    }
+
+    public void markUndone(){
+        isDone = false;
+    }
+    public String getStatusIcon() {
+        return (isDone ? "X" : "");
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getTaskNumber(){
+        return taskNumber;
+    }
+
+    public static int getNumberOfTasks(){
+        return numberOfTasks;
+    }
+}
