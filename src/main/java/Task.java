2fef3ab3233a09fa8dd7ff4ca9fb451023bb6f7e
tyuyang <tayyuyang@gmail.com>
2023-01-26 17:34:50 +0800

    Started on level-3, left with modifying Duke.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,21 @@
 public class Task {
     private String name;
+    private boolean completionStatus;
 
     public Task(String name) {
         this.name = name;
+        this.completionStatus = false;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public void setStatus(boolean status) {
+        this.completionStatus = status;
+    }
+
+    public boolean isDone() {
+        return this.completionStatus;
     }
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -2,6 +2,18 @@ public class TaskList {
     private Task[] tasks;
     private int noOfTasks;
 
+    private void printStatus(int taskNo) {
+        if (tasks[taskNo].isDone()) {
+            System.out.print("[X] ");
+        } else {
+            System.out.print("[ ] ");
+        }
+    }
+
+    private void printTaskName(int taskNo) {
+        System.out.print(tasks[taskNo].getName());
+    }
+
     public TaskList(int size) {
         this.tasks = new Task[size];
         noOfTasks = 0;
@@ -18,7 +30,33 @@ public class TaskList {
             System.out.println("No tasks yet. Please input a task");
         }
         for (int i = 1; i <= noOfTasks; i++) {
-            System.out.println(i + ". " + tasks[i].getName());
+            System.out.print(i + ". ");
+            printStatus(i);
+            System.out.println(tasks[i].getName());
+        }
+    }
+
+    public void markDone(int taskNo) {
+        if (tasks[taskNo].isDone()) {
+            System.out.println("Already done.");
+        } else {
+            tasks[taskNo].setStatus(true);
+            System.out.println("Nice! I have marked this task as done.");
+            printStatus(taskNo);
+            printTaskName(taskNo);
+            System.out.println();
+        }
+    }
+
+    public void unmarkDone(int taskNo) {
+        if (!tasks[taskNo].isDone()) {
+            System.out.println("Not done yet. Please finish it.");
+        } else {
+            tasks[taskNo].setStatus(false);
+            System.out.println("Ok I have marked this as not done yet.");
+            printStatus(taskNo);
+            printTaskName(taskNo);
+            System.out.println();
         }
     }
 }
