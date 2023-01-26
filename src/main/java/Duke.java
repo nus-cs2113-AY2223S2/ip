aadbc3849340d68acc56ac3a446245fa248e3e5c
mitch malvin <e0773911@u.nus.edu>
2023-01-26 13:06:53 +0800

    Level-2 Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        String line = "___________________________________________";
 
+    private static String[] list = new String[100];
+    private static int counter = 0;
+    private static String line = "___________________________________________";
+
+    public static void printList() {
+        System.out.println("\t" + line);
+        System.out.println("\t Here are the tasks for your list: ");
+        for(int i=0; i< counter; i++) {
+            System.out.println("\t" + (i+1) + ". " + list[i]);
+        }
+        System.out.println("\t" + line);
+    }
+
+    public static void main(String[] args) {
         System.out.println("\t" + line);
         System.out.println("\t Hello I'm Duke, your personal chatbot.");
         System.out.println("\t Is there anything I can do for you");
@@ -14,16 +26,25 @@ public class Duke {
 
         while(true) {
             input = in.nextLine().trim();
+
            
             if (input.equals("bye")) {
                 System.out.println("\t" + line);
-                System.out.println("Bye! Do let me know if you need any further assistance");
+                System.out.println("\t Bye! Do let me know if you need any further assistance");
                 System.out.println("\t" + line);
                 break;
             }
 
+            else if (input.equals("list")) {
+                printList();
+                continue;
+            }
+
+            list[counter] = input;
+            counter++;
+
             System.out.println("\t" + line);
-            System.out.println("\t" + input);
+            System.out.println("\t" + "added: " + input);
             System.out.println("\t" + line);
         }
         
