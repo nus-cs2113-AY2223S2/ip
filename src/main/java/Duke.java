500d0e274b1294d28156e30d8ad1a2e1a6ee2c25
Samueltansw <samuetan123@gmail.com>
2023-01-26 13:05:05 +0800

    added echoCommand function to chatBot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,35 +1,74 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
 
+        final String DOTTED_LINE = "    --------------------------------------------------";
+        final String TAB_SIZE = "     ";
+
+        // greets user
+        System.out.println(DOTTED_LINE);
+        printLogo();
         greetUser();
-        sayBye();
+        System.out.println(DOTTED_LINE + '\n');
+
+        String userCommand;
+
+        // ask for user inputs and echo those inputs
+        do {
+            Scanner scanner = new Scanner(System.in);
+            userCommand = scanner.nextLine();
+
+            // top dotted line
+            System.out.println(DOTTED_LINE);
+
+            // body
+            System.out.print(TAB_SIZE);
+            echoCommand(userCommand);
+
+            // bottom dotted line
+            System.out.println(DOTTED_LINE + '\n');
+
+        } while (!userCommand.equals("bye"));
+
     }
 
-    /**
-     * Prints BtB logo and greets the user
-     */
+    /** prints the logo of the chatBot */
+    public static void printLogo() {
+
+        String logo = "      ____     _    ____\n"
+                + "     |  _ \\ __| |__|  _ \\\n"
+                + "     | | //|__   __| | //\n"
+                + "     | |_\\\\   |  |_| |_\\\\\n"
+                + "     |____/   |___/|____/\n";
+
+        System.out.println(logo);
+    }
+
+    /** greets the user */
     public static void greetUser() {
 
-        String logo = " ____     _    ____\n"
-                + "|  _ \\ __| |__|  _ \\\n"
-                + "| | //|__   __| | //\n"
-                + "| |_\\\\   |  |_| |_\\\\\n"
-                + "|____/   |___/|____/\n";
-        String dottedLine = "--------------------------------------------------\n";
-        String greetings = " Hello! I'm Bob the Bot, aka BtB.\n"
-                + " What can I do for you?\n";
+        System.out.println("     Hello! I'm Bob the Bot, aka BtB."); // five spaces
+        System.out.println("     What can I do for you?");
+    }
 
-        System.out.println(dottedLine + logo + '\n' + greetings
-                + dottedLine);
+    /** Say bye to the user */
+    public static void sayBye() {
+
+        System.out.println("Bye. Hope to see you again sooooooon (┬┬﹏┬┬)!");
     }
 
     /**
-     * Say bye to user
+     * Echos out the user's command onto the terminal
+     *
+     * @param userCommand the CLI input that the user key in
      */
-    public static void sayBye() {
-        String dottedLine = "--------------------------------------------------";
+    public static void echoCommand(String userCommand) {
 
-        System.out.println(" Bye. Hope to see you again sooooooon (┬┬﹏┬┬)!");
-        System.out.println(dottedLine);
+        if (userCommand.equals("bye")) {
+            sayBye();
+        } else {
+            System.out.println(userCommand);
+        }
     }
 }
