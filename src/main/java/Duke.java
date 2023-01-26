e4bc222cfff88d99818a413e5f4a139e56861935
guillaume-grn <guillaume.gerony@hotmail.com>
2023-01-26 15:40:40 +0800

    Duke : add mark and unmark tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
+import util.Task;
 
 public class Duke {
-    public static String[] createList(){
-        String[] commands_list=new String[100];
-        commands_list[99]="1";
+    public static int next_index=1;
+
+    public static Task[] createList(){
+        Task[] commands_list=new Task[100];
         return commands_list;
     }
-    public static int next_index(String[] commands_list){
-        return Integer.parseInt(commands_list[99]);
-    }
-    public static void addToList(String line,String[] commands_list){
-        int next_index=next_index(commands_list);
-        commands_list[next_index]=line;
+    
+    public static void addToList(String line,Task[] commands_list){
+        commands_list[next_index]=new Task(line);
         next_index++;
-        commands_list[99]=Integer.toString(next_index);
     }
-    public static void displayCommandsList(String[] commands_list){
-        int next_index=next_index(commands_list);
+    public static void displayCommandsList(Task[] commands_list){
         int i=1;
         System.out.println("\t_____________________________________________________");
         while (i<next_index){
-            System.out.println("\t"+i+". "+commands_list[i]);
+            System.out.println("\t"+i+".["+commands_list[i].getStatusIcon()+"] "+commands_list[i].getDescription());
             i++;
         }
         System.out.println("\t_____________________________________________________");
@@ -42,7 +39,7 @@ public class Duke {
         System.out.println(" Hello! I'm Duke ");
         System.out.println(" What can I do for you? \n");
         System.out.println("____________________________________________________________\n");
-        String[] commands_list=createList();
+        Task[] commands_list=createList();
         boolean not_finished= true;
         while (not_finished==true) {
             String line=ask();
@@ -54,6 +51,18 @@ public class Duke {
             }
             else if (line.equals("list")){
                 displayCommandsList(commands_list);
+            }
+            else if (line.length()>5 && line.substring(0, 4).equals("mark")){
+                int index=Integer.parseInt(line.substring(5,6));
+                commands_list[index].setDone(true);
+                System.out.println("\t Nice! I've marked this task as done:");
+                System.out.println("\t"+index+".["+commands_list[index].getStatusIcon()+"] "+commands_list[index].getDescription());
+            }
+            else if (line.length()>7 && line.substring(0, 6).equals("unmark")){
+                int index=Integer.parseInt(line.substring(7,8));
+                commands_list[index].setDone(false);
+                System.out.println("\t OK, I've marked this task as not done yet:");
+                System.out.println("\t"+index+".["+commands_list[index].getStatusIcon()+"] "+commands_list[index].getDescription());
             }
             else{
                 addToList(line, commands_list);
---
+++ src/main/java/util/Task.java
@@ -0,0 +1,39 @@
+package util; 
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+    
+    public Task() {
+        this("");
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+    
+
+    
+}
+
