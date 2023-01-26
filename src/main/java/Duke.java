fb8955d41494b948d133b2dd74c102608e6d128b
mitch malvin <e0773911@u.nus.edu>
2023-01-26 13:59:16 +0800

    Level-3, follow coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,16 +2,44 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private static String[] list = new String[100];
+    private static Task[] taskList = new Task[100];
     private static int counter = 0;
-    private static String line = "___________________________________________";
+    private static final String line = "___________________________________________";
 
     public static void printList() {
         System.out.println("\t" + line);
         System.out.println("\t Here are the tasks for your list: ");
         for(int i=0; i< counter; i++) {
-            System.out.println("\t" + (i+1) + ". " + list[i]);
+            System.out.println("\t" + (i+1) + ". [" + taskList[i].getStatusIcon() + "] " +  taskList[i].description);
         }
+        System.out.println("\t" + line);
+    }
+
+    public static void markTaskAndPrint(int taskIndex) {
+        if(taskIndex < 0  || taskList[taskIndex] == null) {
+            System.out.println("\t" + line);
+            System.out.println("\t Please ensure that you enter the correct task number");
+            System.out.println("\t" + line);
+            return;
+        }
+        taskList[taskIndex].isDone = true;
+        System.out.println("\t" + line);
+        System.out.println("\t Nice, I have marked this task as done: ");
+        System.out.println("\t [" + taskList[taskIndex].getStatusIcon() + "] " +  taskList[taskIndex].description);
+        System.out.println("\t" + line);
+    }
+
+    public static void unmarkTaskAndPrint(int taskIndex) {
+        if(taskIndex < 0 || taskList[taskIndex] == null) {
+            System.out.println("\t" + line);
+            System.out.println("\t Please ensure that you enter the correct task number");
+            System.out.println("\t" + line);
+            return;
+        }
+        taskList[taskIndex].isDone = false;
+        System.out.println("\t" + line);
+        System.out.println("\t Ouch, I have unmarked this task: ");
+        System.out.println("\t [" + taskList[taskIndex].getStatusIcon() + "] " +  taskList[taskIndex].description);
         System.out.println("\t" + line);
     }
 
@@ -25,26 +53,34 @@ public class Duke {
         Scanner in = new Scanner(System.in);
 
         while(true) {
-            input = in.nextLine().trim();
+            input = in.nextLine();
 
-           
             if (input.equals("bye")) {
                 System.out.println("\t" + line);
                 System.out.println("\t Bye! Do let me know if you need any further assistance");
                 System.out.println("\t" + line);
                 break;
             }
-
-            else if (input.equals("list")) {
+            if (input.equals("list")) {
                 printList();
+                continue;
+            } 
+            if (input.length() >= 4 && input.substring(0,4).equals("mark")){
+                int indexToMark = Integer.parseInt(input.substring(5))-1; //convert to 0-based index
+                markTaskAndPrint(indexToMark);
+                continue;
+            }
+            if (input.length() >= 6 && input.substring(0,6).equals("unmark")){
+                int indexToUnmark = Integer.parseInt(input.substring(7))-1; //convert to 0-based index
+                unmarkTaskAndPrint(indexToUnmark);
                 continue;
             }
 
-            list[counter] = input;
+            taskList[counter] = new Task(input);
             counter++;
 
             System.out.println("\t" + line);
-            System.out.println("\t" + "added: " + input);
+            System.out.println("\t" + "added: " + input );
             System.out.println("\t" + line);
         }
         
