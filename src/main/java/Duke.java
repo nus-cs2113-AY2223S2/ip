5048808129f8a8143a381f0dc378328f010b4922
zzs-RedCocoon <xgps_0703zzs@126.com>
2023-01-26 18:56:02 +0800

    Add Level-0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,13 @@
 public class Duke {
+    public static void sendGreeting(){
+        String greeting_word="____________________________________________________________\n"
+            + "Hello! I'm Duke\n"
+            + "What can I do for you?\n"
+            + "____________________________________________________________\n"
+            + "Bye. Hope to see you again soon!\n"
+            + "____________________________________________________________";
+        System.out.println(greeting_word);
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +15,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        sendGreeting();
     }
 }
