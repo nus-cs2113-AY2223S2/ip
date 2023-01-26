f8cc0a1a3c495220378d692647c9ea2369630740
Gurman Kalkat <gurmankkalkat@gmail.com>
2023-01-26 23:38:21 +0800

    Level-2 code added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    static final String[] TASKS = new String[101];
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -8,6 +11,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        run();
     }
 
     public static void run() {
@@ -15,10 +20,19 @@ public class Duke {
         System.out.println("What can I do for you?");
 
         Scanner scan = new Scanner(System.in);
-        String command = scan.next();
+        String command = scan.nextLine();
+        int tasksI = 1;
         while (!command.equals("bye")) {
-            System.out.println(command);
-            command = scan.next();
+            if (command.equals("list")) {
+                for (int i = 1; i < tasksI; i++) {
+                    System.out.println(i + ". " + TASKS[i]);
+                }
+            } else {
+                TASKS[tasksI] = command;
+                tasksI++;
+                System.out.println("added: " + command);
+            }
+            command = scan.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again soon!");
