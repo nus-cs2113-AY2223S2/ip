66df6f055248e174319d2e835ee028f2762c3024
Ju-Can <ju.can.0860@gmail.com>
2023-01-26 14:26:24 +0800

    Level-2 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,14 +15,38 @@ public class Duke {
                 line;
         System.out.println(greet);
 
-        // Level 1
+//        // Level 1
+//        Scanner in = new Scanner(System.in);
+//        String userInput = in.nextLine();
+//        while (!userInput.equals("bye")) {
+//            System.out.println(line + userInput + "\n" + line);
+//            userInput = in.nextLine();
+//        }
+//        System.out.println("Thank you for using Duke. Hope to see you soon!");
+
+        // Level 2 create list
         Scanner in = new Scanner(System.in);
-        String userInput = in.nextLine();
-        while (!userInput.equals("bye")) {
-            System.out.println(line + userInput + "\n" + line);
-            userInput = in.nextLine();
+        String[] list = new String[100];
+        int countListEntry = 0;
+        while (true){
+            String userInput = in.nextLine();
+            if (userInput.equals("bye")){
+                System.out.println("Thank you for using Duke. Hope to see you soon!");
+                break;
+            }
+            if (userInput.equals("list")){
+                System.out.println(line);
+                for (int i = 0; i < countListEntry; i++){
+                    int index = i+1;
+                    System.out.println( index + ". " + list[i] + "\n");
+                }
+                System.out.println(line);
+            } else {
+                list[countListEntry] = userInput;
+                countListEntry++;
+                System.out.println(line + "added: " + userInput + "\n" + line);
+            }
         }
-        System.out.println("Thank you for using Duke. Hope to see you soon!");
 
     }
 }
