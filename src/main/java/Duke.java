07a8c29f39a0ed0107f036ded754b48ada15b8e9
GraceZhuXY <grace.zhu.xing.yu@gmail.com>
2023-01-26 10:45:44 +0800

    Implement Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        System.out.println(" Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Hi, I'm bob");
+        System.out.println("What's up");
+        String word = reader.nextLine();
+        while (!word.equals("bye")) {
+            System.out.println(word);
+            word = reader.nextLine();
+        }
+        System.out.println("Bye");
     }
 }
