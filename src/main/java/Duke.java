8bfd301f109c970de52cd0a813bac6c4554940e7
mitch malvin <e0773911@u.nus.edu>
2023-01-26 14:08:04 +0800

    fix coding standard : capitalized constants

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,47 +7,47 @@ public class Duke {
     private static final String LINE = "___________________________________________";
 
     public static void printList() {
-        System.out.println("\t" + line);
+        System.out.println("\t" + LINE);
         System.out.println("\t Here are the tasks for your list: ");
         for(int i=0; i< counter; i++) {
             System.out.println("\t" + (i+1) + ". [" + taskList[i].getStatusIcon() + "] " +  taskList[i].description);
         }
-        System.out.println("\t" + line);
+        System.out.println("\t" + LINE);
     }
 
     public static void markTaskAndPrint(int taskIndex) {
         if(taskIndex < 0  || taskList[taskIndex] == null) {
-            System.out.println("\t" + line);
+            System.out.println("\t" + LINE);
             System.out.println("\t Please ensure that you enter the correct task number");
-            System.out.println("\t" + line);
+            System.out.println("\t" + LINE);
             return;
         }
         taskList[taskIndex].isDone = true;
-        System.out.println("\t" + line);
+        System.out.println("\t" + LINE);
         System.out.println("\t Nice, I have marked this task as done: ");
         System.out.println("\t [" + taskList[taskIndex].getStatusIcon() + "] " +  taskList[taskIndex].description);
-        System.out.println("\t" + line);
+        System.out.println("\t" + LINE);
     }
 
     public static void unmarkTaskAndPrint(int taskIndex) {
         if(taskIndex < 0 || taskList[taskIndex] == null) {
-            System.out.println("\t" + line);
+            System.out.println("\t" + LINE);
             System.out.println("\t Please ensure that you enter the correct task number");
-            System.out.println("\t" + line);
+            System.out.println("\t" + LINE);
             return;
         }
         taskList[taskIndex].isDone = false;
-        System.out.println("\t" + line);
+        System.out.println("\t" + LINE);
         System.out.println("\t Ouch, I have unmarked this task: ");
         System.out.println("\t [" + taskList[taskIndex].getStatusIcon() + "] " +  taskList[taskIndex].description);
-        System.out.println("\t" + line);
+        System.out.println("\t" + LINE);
     }
 
     public static void main(String[] args) {
-        System.out.println("\t" + line);
+        System.out.println("\t" + LINE);
         System.out.println("\t Hello I'm Duke, your personal chatbot.");
         System.out.println("\t Is there anything I can do for you");
-        System.out.println("\t" + line);
+        System.out.println("\t" + LINE);
 
         String input = "";
         Scanner in = new Scanner(System.in);
@@ -56,9 +56,9 @@ public class Duke {
             input = in.nextLine();
 
             if (input.equals("bye")) {
-                System.out.println("\t" + line);
+                System.out.println("\t" + LINE);
                 System.out.println("\t Bye! Do let me know if you need any further assistance");
-                System.out.println("\t" + line);
+                System.out.println("\t" + LINE);
                 break;
             }
             if (input.equals("list")) {
@@ -79,9 +79,9 @@ public class Duke {
             taskList[counter] = new Task(input);
             counter++;
 
-            System.out.println("\t" + line);
+            System.out.println("\t" + LINE);
             System.out.println("\t" + "added: " + input );
-            System.out.println("\t" + line);
+            System.out.println("\t" + LINE);
         }
         
        
