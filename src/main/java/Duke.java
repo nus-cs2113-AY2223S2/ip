723852334ea3fc77d8af3a51b15c078fd6cea344
ZiqiuZeng <88576865+ZiqiuZeng@users.noreply.github.com>
2023-01-27 00:26:58 +0800

    Revert "Revert "Increment: Duke Level-0: Greet""
    This reverts commit a9f524e95891058f4189e7e70b104aeed8f9995c.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke" + System.lineSeparator() + "What can I do for you?");
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while(!line.equals("bye")){
+            System.out.println(line);
+            line = in.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
+
 }
