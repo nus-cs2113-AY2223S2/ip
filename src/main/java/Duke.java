d417377455a473031e7f179d915603599eeb980a
Sophie <sophieyxtang@gmail.com>
2023-01-26 16:56:10 +0800

    Implement Dukebot Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String openingMsg = " Hello! I'm Duke\n What can I do for you?";
-        String closingMsg = " Bye. Hope to see you again soon!";
+        String openingMsg = " Hello! I'm Dukebot\n What can I do for you?";
+        String closingMsg = " Goodbye! Hope to see you again soon ^^!";
         String horizontal = "---------------------------------";
-        System.out.println(horizontal);
-        System.out.println(openingMsg);
-        System.out.println(horizontal);
-        System.out.println(closingMsg);
-        System.out.println(horizontal);
+
+        System.out.println(horizontal + "\n" + openingMsg + "\n" + horizontal);
+
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+
+        while (!line.equals("bye")) {
+            System.out.println(horizontal + "\n" + line + "\n" + horizontal);
+            line = in.nextLine();
+        }
+
+        System.out.println(horizontal + "\n" + closingMsg + "\n" + horizontal);
+
 
     }
 }
