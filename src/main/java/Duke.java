df012f3e397b39848538ec64bd50a19374b74bac
khooyourun <khooyourun@hotmail.com>
2023-01-26 20:37:09 +0800

    Change to switch

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.util.Objects;
 import java.util.Scanner;
 
+
 public class Duke {
     public static void main(String[] args) {
 
@@ -9,39 +10,12 @@ public class Duke {
         Scanner scanner = new Scanner(System.in);
         Task[] taskArray = new Task[100];
         int taskIndex = 0;
+        boolean isEnd = false;
         while (true) {
             String command = scanner.nextLine(); //reads in input
-            String[] split = command.split(" "); //separates mark and integer
-            //if command is mark, mark the appropriate entry in the list
-            if (split[0].equalsIgnoreCase("mark")) {
-                if (taskIndex == 0) {
-                    System.out.println("Empty, list is.");
-                } else {
-                    int listIndex = Integer.parseInt(split[1]);
-                    taskArray[listIndex].setIsDone("[X]");
-                    System.out.println("Marked it, I have:");
-                    System.out.println(listIndex + ". " + taskArray[listIndex].getIsDone() + " " + taskArray[listIndex].getDescription());
-                }
-            }
-            //if command is unmark, unmark the appropriate entry in the list
-            if (split[0].equalsIgnoreCase("unmark")) {
-                if (taskIndex == 0) {
-                    System.out.println("Empty, list is.");
-                } else {
-                    int listIndex = Integer.parseInt(split[1]);
-                    taskArray[listIndex].setIsDone("[ ]");
-                    System.out.println("Unmarked it, I have:");
-                    System.out.println(listIndex + ". " + taskArray[listIndex].getIsDone() + " " + taskArray[listIndex].getDescription());
-                }
-            }
-            //if command is not list or bye or mark, add to list
-            if (!command.equalsIgnoreCase("list") && !command.equalsIgnoreCase("bye") && !split[0].equalsIgnoreCase("mark")&& !split[0].equalsIgnoreCase("unmark")) {
-                taskArray[taskIndex] = new Task(command, "[ ]");
-                System.out.println(command + " - Added, I have.");
-                ++taskIndex;
-            }
+            switch (command) {
             //if command is list, either display empty or display list
-            if (command.equalsIgnoreCase("list")) {
+            case "list":
                 if (taskIndex == 0) {
                     System.out.println("Empty, list is.");
                 } else {
@@ -50,10 +24,46 @@ public class Duke {
                         System.out.println(j + ". " + taskArray[j].getIsDone() + " " + taskArray[j].getDescription());
                     }
                 }
-            }
-            //if command is bye, exit
-            if (command.equalsIgnoreCase("bye")) {
+                break;
+            //if command is mark, mark the appropriate entry in the list
+            case "mark":
+                System.out.println("Mark what, I ask?");
+                String listIndex = scanner.nextLine();
+                int temp1 = Integer.parseInt(listIndex);
+                if (taskIndex == 0 || temp1 > taskIndex) {
+                    System.out.println("In list, it is not.");
+                } else {
+                    taskArray[temp1].setIsDone("[X]");
+                    System.out.println("Marked it, I have:");
+                    System.out.println(temp1 + ". " + taskArray[temp1].getIsDone() + " " + taskArray[temp1].getDescription());
+                }
+                break;
+                //if command is unmark, unmark the appropriate entry in the list
+            case "unmark":
+                System.out.println("Unmark what, I ask?");
+                String listIndex2 = scanner.nextLine();
+                int temp2 = Integer.parseInt(listIndex2);
+                if (taskIndex == 0 || temp2 > taskIndex) {
+                    System.out.println("In list, it is not.");
+                    break;
+                } else {
+                    taskArray[temp2].setIsDone("[ ]");
+                    System.out.println("Unmarked it, I have:");
+                    System.out.println(temp2 + ". " + taskArray[temp2].getIsDone() + " " + taskArray[temp2].getDescription());
+                }
+                break;
+                //if command is bye, end program
+            case "bye":
                 System.out.println("See you soon, I hope. Goodbye.");
+                isEnd = true;
+                break;
+            default:
+                taskArray[taskIndex] = new Task(command, "[ ]");
+                System.out.println(command + " - Added, I have.");
+                ++taskIndex;
+                break;
+            }
+            if(isEnd) {
                 break;
             }
         }
