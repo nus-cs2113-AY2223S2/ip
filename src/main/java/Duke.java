13506d3d0dc02b99d9b55480bc304eb01cfce23e
TayJiunYuan <tayjy2000@gmail.com>
2023-01-26 20:34:14 +0800

    Added Tasks and Task List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,49 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    public static String[] Tasks= new String[100]; 
+    
+    public static int numberOfTasks = 0;
+
     public static void printLine(){
-        String horizontalLine = "――――――――――――――――――――――――――――――――――――――――――";
+        String horizontalLine = "\t――――――――――――――――――――――――――――――――――――――――――";
         System.out.println(horizontalLine);
     }
     
     public static void printWelcome(){
         printLine();
-        System.out.println("Well hello there!\n" +
-                           "What can I do for you today?");
+        System.out.println("\tWell hello there!\n" +
+                           "\tWhat can I do for you today?");
         printLine();
     }
 
     public static void printGoodbye(){
         printLine();
-        System.out.println( "Thank you! See you again soon :)");
+        System.out.println( "\tThank you! See you again soon :)");
         printLine();
     }
 
-    public static void echoUserInput(String userInput){
+    /*public static void echoUserInput(String userInput){
         printLine();
-        System.out.println(userInput);
+        System.out.println(userInput);  
+        printLine();
+    }  */
+
+    public static void addToList(String userInput){
+        Tasks[numberOfTasks] = userInput;
+        numberOfTasks += 1;
+        printLine();
+        System.out.println("\tadded: " + userInput);
+        printLine(); 
+    }
+
+    public static void displayTaskList(){
+        printLine();
+        for (int i = 0; i < numberOfTasks; i++){
+            String taskNumber = Integer.toString(i+1);
+            System.out.println("\t" + taskNumber + ". " + Tasks[i]);
+        }
         printLine();
     }
 
@@ -30,16 +52,18 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         while (true){
             String userInput;
-            userInput = in.nextLine(); 
-            if (userInput.equals("bye")){
+            userInput = in.nextLine();
+            switch (userInput){
+            case "bye": 
                 printGoodbye();
                 return;
-            }
-            else{
-                echoUserInput(userInput);
+            case "list":
+                displayTaskList();
+                break;
+            default:
+                addToList(userInput);
+                break;
             }
         }
-        
-        
     }
 }
