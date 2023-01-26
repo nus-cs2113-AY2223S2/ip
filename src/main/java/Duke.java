8ac679824e4bc042b9a72dfb2110ce7780115d33
clement559 <clementsiut@gmail.com>
2023-01-26 22:39:53 +0800

    Added Level 2 increment to Add, List. Program now remembers the input items and can output the items in the list using the command "list".

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Duke {
     public static void main(String[] args) {
         String lineBreak = "---------------------------------------------";
         String line = "";
+        String[] list = new String[100];
+        int index = 0;
         Scanner in = new Scanner(System.in);
         /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -17,12 +20,25 @@ public class Duke {
         while (true) {
             line = in.nextLine();
             System.out.println(lineBreak);
-            if (line.equals("bye")) {
+            if (line.equals("list")) {
+                int itemNumber = 1;
+                String[] fullList = Arrays.copyOf(list, index);
+                for (String item : fullList) {
+                    System.out.println(itemNumber + ". " + item);
+                    itemNumber++;
+                }
+                System.out.println(lineBreak);
+            }
+            else if (line.equals("bye")) {
                 break;
             }
-            System.out.println(line);
-            System.out.println(lineBreak);
+            else {
+                list[index] = line;
+                index++;
+                System.out.println("added: " + line);
+                System.out.println(lineBreak);
             }
+        }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("---------------------------------------------");
         return;
