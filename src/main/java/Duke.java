31c22ad1c73c0837d2e8fe7fa23f477435ce9963
Elijah <ngfei25@gmail.com>
2023-01-26 23:06:15 +0800

    Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.Set;
 import java.util.Scanner;
+import java.util.HashSet;
 
 public class Duke {
+    // Messages
     private static final String BOT_NAME = "Duke";
     private static final String INITIAL_MSG = String.format("    Hello! I'm %s\n   What can I do for you?", BOT_NAME); 
     private static final String BYE_MSG = "     Bye. Hope to see you again soon!";
-    private static String[] itemList = new String[100];
+    private static final String MARK_MSG = "     Nice! I've marked this task as done";
+    private static final String UNMARK_MSG = "     OK, I've marked this task as not done yet:";
+    // Constants 
+    private static final int MAX_ITEMS = 100;
+    private static String[] itemList = new String[MAX_ITEMS];
+    private static Set<String> markedItems = new HashSet<String>(MAX_ITEMS);
     private static int numItems = 0;
 
     public static void main(String[] args) {
@@ -25,13 +33,33 @@ public class Duke {
     }
 
     private static void handleInput(String line) {
-        if (line.equals("bye")) {
+        String[] lineParts = line.split(" ");
+        String command = lineParts[0];
+        if (command.equals("bye")) {
             printResponse(BYE_MSG);
-        } else if (line.equals("list")) {
+        } else if (command.equals("list")) {
             printItems();
+        } else if (command.equals("mark")) {
+            // String item = line.substring(4); // if want to mark by name
+            markItem(Integer.parseInt(lineParts[1]));
+        } else if (command.equals("unmark")) {
+            // String item = line.substring(6); // if want to unmark by name
+            unmarkItem(Integer.parseInt(lineParts[1]));
         } else { // add item
             addItem(line);
         }
+    }
+
+    private static void markItem(int itemNo) {
+        String item = itemList[itemNo - 1];
+        markedItems.add(item);
+        printResponse("    " + MARK_MSG + "\n" + "        [X] " + item);
+    }
+
+    private static void unmarkItem(int itemNo) {
+        String item = itemList[itemNo - 1];
+        markedItems.remove(item);
+        printResponse("    " + UNMARK_MSG + "\n" + "        [ ] " + item);
     }
 
     private static void addItem(String item) {
@@ -42,14 +70,20 @@ public class Duke {
     private static void printItems() {
         String response = "";
         for (int i = 0; i < numItems; i++) {
-            response += String.format("   %d. %s\n", i+1, itemList[i]);
+            char mark;
+            if (markedItems.contains(itemList[i])) {
+                mark = 'X';
+            } else {
+                mark = ' ';
+            }
+            response += String.format("   %d.[%c] %s\n", i+1, mark, itemList[i]);
         }
         printResponse(response);
     }
 
     private static void printResponse(String response) {
         System.out.println("    ____________________________________________________________");
-        System.out.println(String.format("    %s", response));
+        System.out.println(String.format("%s", response));
         System.out.println("    ____________________________________________________________");
     }
 }
