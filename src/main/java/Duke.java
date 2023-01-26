3618da3defc672084e98278593dcac1f86a505e4
Gurman Kalkat <gurmankkalkat@gmail.com>
2023-01-27 00:24:39 +0800

    Fixed code to follow coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,10 +12,10 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        run();
+        runBot();
     }
 
-    public static void run() {
+    public static void runBot() {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
@@ -23,23 +23,24 @@ public class Duke {
         String command = scan.nextLine();
         int tasksI = 1;
         while (!command.equals("bye")) {
-            Task task = new Task(command);
             if (command.equals("list")) {
                 System.out.println("Here are the tasks in your list: ");
                 for (int i = 1; i < tasksI; i++) {
-                    System.out.println(i + ".[" + TASKS[i].getStatusIcon() + "] " + TASKS[i].getDescription());
+                    Task currTask = TASKS[i];
+                    System.out.println(i + ".[" + currTask.getStatusIcon() + "] " + currTask.getDescription());
                 }
             } else if (command.contains("mark")) {
+                String[] splitCommand = command.split(" ");
+                int taskNum = Integer.parseInt(splitCommand[1]);
+                Task currTask = TASKS[taskNum];
                 if (command.substring(0, 4).equals("mark")) {
-                    int taskNum = Integer.parseInt("" + command.charAt(5));
-                    TASKS[taskNum].mark();
+                    currTask.mark();
                     System.out.println("Nice! I've marked this task as done: ");
-                    System.out.println("[" + TASKS[taskNum].getStatusIcon() + "] "  + TASKS[taskNum].getDescription());
+                    System.out.println("[" + currTask.getStatusIcon() + "] "  + currTask.getDescription());
                 } else {
-                    int taskNum = Integer.parseInt("" + command.charAt(7));
-                    TASKS[taskNum].unmark();
+                    currTask.unmark();
                     System.out.println("OK, I've marked this task as not done yet: ");
-                    System.out.println("[" + TASKS[taskNum].getStatusIcon() + "] "  + TASKS[taskNum].getDescription());
+                    System.out.println("[" + currTask.getStatusIcon() + "] "  + currTask.getDescription());
                 }
             } else {
                 TASKS[tasksI] = new Task(command);
