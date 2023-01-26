eaba19da91d4c70adfe732128aef7018d5707b93
tliangac <tliangac@connect.ust.hk>
2023-01-26 11:42:05 +0800

    Level-1 List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,28 @@
-import java.util.List;
+
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
-    public static List list=null;
+    public static ArrayList<String> list = new ArrayList<String>();
     public static void main(String[] args) {
         String lineBreak = "-----------------";
         System.out.println(lineBreak + '\n' + "Hello! I'm Duke" + '\n' + "What can I do for you?" + '\n' + lineBreak);
-        Scanner myObj = new Scanner(System.in);
-        String instruction = myObj.nextLine();
-        if(instruction.equalsIgnoreCase("bye")){
-            System.out.println("Bye. Hope to see you again soon!");
-        }else if(instruction.equalsIgnoreCase("list")){
-            System.out.println(list);
+        String instruction;
+        while(true){
+            Scanner myObj = new Scanner(System.in);
+            instruction = myObj.nextLine();
+            if(instruction.equalsIgnoreCase("list")) {
+                System.out.println(lineBreak);
+                for(int i=0;i<list.size();i++){
+                    System.out.println(i+1+". "+list.get(i));
+                }
+                System.out.println(lineBreak);
+            } else if (instruction.equalsIgnoreCase("bye")) {
+                System.out.println(lineBreak+'\n'+"Bye. Hope to see you again soon!"+'\n'+lineBreak);
+                break;
+            } else{
+                list.add(instruction);
+                System.out.println(lineBreak+'\n'+"added: "+instruction+'\n'+lineBreak);
+            }
         }
     }
 }
