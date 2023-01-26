2af5021cb8a8a4db68c1a829462102cc438ab0c0
itszhixuan <leezhixuan001@gmail.com>
2023-01-26 20:44:10 +0800

    Level 0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,14 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String greeting = "____________________________________________________________\n" +
+                " Hello! I'm Duke\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+
         System.out.println("Hello from\n" + logo);
+        System.out.println(greeting);
     }
 }
