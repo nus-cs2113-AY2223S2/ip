d7c6c5505962bab82616fb3db3454c802ac752bf
Gurman Kalkat <gurmankkalkat@gmail.com>
2023-01-26 23:59:00 +0800

    Level-3 code added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
 
-    static final String[] TASKS = new String[101];
+    static final Task[] TASKS = new Task[101];
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -23,12 +23,26 @@ public class Duke {
         String command = scan.nextLine();
         int tasksI = 1;
         while (!command.equals("bye")) {
+            Task task = new Task(command);
             if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list: ");
                 for (int i = 1; i < tasksI; i++) {
-                    System.out.println(i + ". " + TASKS[i]);
+                    System.out.println(i + ".[" + TASKS[i].getStatusIcon() + "] " + TASKS[i].getDescription());
+                }
+            } else if (command.contains("mark")) {
+                if (command.substring(0, 4).equals("mark")) {
+                    int taskNum = Integer.parseInt("" + command.charAt(5));
+                    TASKS[taskNum].mark();
+                    System.out.println("Nice! I've marked this task as done: ");
+                    System.out.println("[" + TASKS[taskNum].getStatusIcon() + "] "  + TASKS[taskNum].getDescription());
+                } else {
+                    int taskNum = Integer.parseInt("" + command.charAt(7));
+                    TASKS[taskNum].unmark();
+                    System.out.println("OK, I've marked this task as not done yet: ");
+                    System.out.println("[" + TASKS[taskNum].getStatusIcon() + "] "  + TASKS[taskNum].getDescription());
                 }
             } else {
-                TASKS[tasksI] = command;
+                TASKS[tasksI] = new Task(command);
                 tasksI++;
                 System.out.println("added: " + command);
             }
