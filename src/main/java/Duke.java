8ffa03d5d71079d251f053d13ae4adf7a0fb446e
Gurman Kalkat <gurmankkalkat@gmail.com>
2023-01-26 23:23:15 +0800

    Level-1 code added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,4 +9,21 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
     }
+
+    public static void run() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+        Scanner scan = new Scanner(System.in);
+        String command = scan.next();
+        while (!command.equals("bye")) {
+            System.out.println(command);
+            command = scan.next();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+    }
 }
+
+
+
