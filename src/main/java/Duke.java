5b67dd0e53e664ee7fb27e90163700ccf8a602d2
Elijah <ngfei25@gmail.com>
2023-01-26 22:19:37 +0800

    Response

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String NAME = "Duke";
+    private static final String INITIAL_MSG = String.format("    Hello! I'm %s\n   What can I do for you?", NAME); 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +10,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        printResponse(INITIAL_MSG);
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            printResponse(line);
+        }
+        scanner.close();
+    }
+
+    private static void printResponse(String response) {
+        System.out.println("    ____________________________________________________________");
+        System.out.println(String.format("    %s", response));
+        System.out.println("    ____________________________________________________________");
     }
 }
