54fabeac8b33a0972f25d4058f9eadef8f69c3b9
KedrianLoh <e0726084@u.nus.edu>
2023-01-26 15:42:21 +0800

    A-CodingStandard. Following the Coding Standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,52 +1,63 @@
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
+
 public class Duke {
+    /**
+     * Determines if the string is an integer
+     * @param str Str is the string we are checking
+     * @return True if str is an integer, false otherwise
+     * @catch NumberFormatException If str cannot be converted into integer
+     */
+    public static boolean isInt(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException exception) {
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = "Hello! I'm Duke\n"
-                + "What can I do for you?\n";
+        String logo = "Hello! I'm Duke\n" + "What can I do for you?\n";
         System.out.println(logo);
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        Task[] storage = new Task[100];
+        Task[] listOfTasks = new Task[100];
         int idx = 0;
         while (!input.equals("bye")) {
+            String[] words = input.split(" ");
             if (input.equals("list")) {
-                for(int i = 0; i < 100; ++i) {
-                    if (storage[i] == null) { break; }
-                    System.out.println(i + 1 + "." + storage[i].getStatusIcon() + " " + storage[i].description);
+                for (int i = 0; i < 100; ++i) {
+                    if (listOfTasks[i] == null) {
+                        break;
+                    }
+                    System.out.println(i + 1 + "." + listOfTasks[i].getStatusIcon() + " " + listOfTasks[i].description);
                 }
-                input = sc.nextLine();
-            } else if (input.contains("unmark")) {
-                String[] separated = input.split(" ");
-                int number = Integer.parseInt(separated[1]);
-                if (number <= 0 || number > 100 || storage[number - 1] == null ) {
+            } else if (input.contains("unmark") && words.length == 2 && isInt(words[1])) {
+                //
+                int number = Integer.parseInt(words[1]);
+                if (number <= 0 || number > 100 || listOfTasks[number - 1] == null) {
                     System.out.println("Please unmark only valid tasks");
-                    input = sc.nextLine();
                 } else {
-                    storage[number - 1].unmarkDone();
-                    System.out.println(" " + storage[number - 1].getStatusIcon() + " " + storage[number - 1].description);
-                    input = sc.nextLine();
+                    listOfTasks[number - 1].unmarkDone();
+                    System.out.println(" " + listOfTasks[number - 1].getStatusIcon() + " " + listOfTasks[number - 1].description);
                 }
-            } else if (input.contains("mark")) {
-                String[] separated = input.split(" ");
-                int number = Integer.parseInt(separated[1]);
-                if (number <= 0 || number > 100 || storage[number - 1] == null ) {
+            } else if (input.contains("mark") && words.length == 2 && isInt(words[1])) {
+                int number = Integer.parseInt(words[1]);
+                if (number <= 0 || number > 100 || listOfTasks[number - 1] == null) {
                     System.out.println("Please mark only valid tasks");
-                    input = sc.nextLine();
                 } else {
-                    storage[number - 1].markAsDone();
-                    System.out.println("  " + storage[number - 1].getStatusIcon() + " " + storage[number - 1].description);
-                    input = sc.nextLine();
+                    listOfTasks[number - 1].markAsDone();
+                    System.out.println("  " + listOfTasks[number - 1].getStatusIcon() + " " + listOfTasks[number - 1].description);
                 }
-            }
-            else {
-                Task tX = new Task(input);
-                storage[idx] = tX;
+            } else {
+                Task newTodo = new Task(input);
+                listOfTasks[idx] = newTodo;
                 ++idx;
                 System.out.println("added: " + input);
-                input = sc.nextLine();
             }
+            input = sc.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,20 +2,33 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Initialization of the object
+     * @param description
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * @return String that corresponding to the status of the Task
+     */
     public String getStatusIcon() {
         return (isDone ? "[X]" : "[ ]");
     }
 
+    /**
+     * To change the boolean of the object to true, meaning that is has been done
+     */
     public void markAsDone() {
         this.isDone = true;
         System.out.println("Nice! I've marked this task as done:");
     }
 
+    /**
+     * To change the boolean of the object to false, meaning that is has NOT been done
+     */
     public void unmarkDone() {
         this.isDone = false;
         System.out.println("OK, I've marked this task as not done yet:");
