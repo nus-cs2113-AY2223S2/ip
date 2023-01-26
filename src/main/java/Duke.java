b99ce124a85d06a52a865f4b32a4530fded29f67
Wilson Lee Jun Wei <leejunwei2000@gmail.com>
2023-01-27 02:21:33 +0800

    Add comments to code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,9 +4,9 @@ public class Duke {
     public static void read_Command() {
         Scanner command = new Scanner(System.in);
         String input_Command = command.nextLine();
-        if ("bye".equals(input_Command)) {
+        if ("bye".equals(input_Command)) { // if input is "bye"
             System.out.println("I look forward to seeing you again! Goodbye!");
-        } else {
+        } else { // if input is not "bye"
             System.out.println(input_Command);
             read_Command();
         }
