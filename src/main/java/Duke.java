7fed88437c27f33229e5207247307cbbf6fc54c8
0nandon <tommy9905@gmail.com>
2023-01-27 16:27:34 +0800

    iP : Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,22 +12,41 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         String input_text;
+        String[] input_list = new String[100];
+        int cnt = 0;
+
         while(true){
             input_text = in.nextLine();
             if(input_text.length() == 0)
                 break;
+
+            input_list[cnt++] = input_text;
             System.out.println(
                     "    ____________________________________________________________\n\t " +
-                    input_text +
-                    "     \n\t____________________________________________________________\n"
+                    " added: " + input_text +
+                    "     \n\t____________________________________________________________"
+            );
+        }
+
+        System.out.println(
+                "list\n" +
+                "    ____________________________________________________________"
+        );
+        for(int i=0; i<cnt; i++){
+            System.out.println(
+                    "\t " + Integer.toString(i+1) + ". " + input_list[i]
             );
         }
         System.out.println(
+                "    ____________________________________________________________"
+        );
+
+        System.out.println(
+                "\nbye\n" +
                 "     ____________________________________________________________\n" +
                 "      Bye. Hope to see you again soon!\n" +
                 "     ____________________________________________________________\n"
         );
-
         in.close();
     }
 }
