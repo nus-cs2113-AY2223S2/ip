7c6ce4f5f3ad23b557cfee8a825e59fde1af71cb
Thunderdragon221 <81968036+Thunderdragon221@users.noreply.github.com>
2023-01-27 23:04:53 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,14 @@
 public class Duke {
+
+    public static void greet() {
+        System.out.println("________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +16,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        greet();
     }
 }
