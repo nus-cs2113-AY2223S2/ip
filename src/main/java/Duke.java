9dad5e3dffe8f3e5dd6f6e28c0d3ca7abf88f5e7
iantenkai <iantenkai@gmail.com>
2023-01-27 17:38:37 +0800

    level 3 added disappointment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,46 @@
 
 import java.util.Scanner;
 
+class Task {
+    String taskName;
+    boolean status;
+
+    Task() {
+        this.status = false;
+    }
+
+    public String getTaskStatus() {
+        String status = " ";
+
+        if (this.status == true) {
+            status = "X";
+        }
+        return status;
+    }
+
+    public void mark() {
+        this.status = true;
+        System.out.println("目標達成！- [" + this.getTaskStatus() + "] " + this.taskName);
+
+    }
+
+    public void unMark() {
+        this.status = false;
+        System.out.println("えええ？噓つき。- [" + this.getTaskStatus() + "] " + this.taskName);
+    }
+}
+
 public class Duke {
     public static void main(String[] args) {
-        String[] library = new String[100];
+        Task[] taskList = new Task[100];
+        System.out.println("おかえり～　ご飯にする？お風呂にする？それとも。。。　わ・た・し？");
+
         int index = 0;
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("おかえり～　ご飯にする？お風呂にする？それとも。。。　わ・た・し？");
             String input = scanner.nextLine();
-
+            String[] words = input.split(" ");
+            String firstWord = words[0];
             if (input.equals("bye")) {
                 break;
             }
@@ -19,11 +50,19 @@ public class Duke {
                     System.out.println("何もいない。。。頭のように。。。");
                 } else {
                     for (int i = 0; i < index; i++) {
-                        System.out.println(i + 1 + ". " + library[i]);
+                        System.out.println(i + 1 + ". [" + taskList[i].getTaskStatus() + "] " + taskList[i].taskName);
                     }
                 }
+            } else if (firstWord.equals("mark")) {
+                taskList[Integer.parseInt(words[1]) - 1].mark(); // shld break if i type mark 0
+            } else if (firstWord.equals("unmark")) {
+                taskList[Integer.parseInt(words[1]) - 1].unMark(); // shld break if i type mark 0
+
             } else if (index < 100) {
-                library[index] = input;
+                Task newTask = new Task();
+                newTask.taskName = input;
+                taskList[index] = newTask;
+
                 System.out.println("覚えましたよ～ " + input);
                 index++;
             } else {
