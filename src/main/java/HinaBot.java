0e9f1534c5bc0d08f0ce942ed132daee54465a38
waiter-palypoo <gohjinghong2000@gmail.com>
2023-01-27 19:08:56 +0800

    Refactor greeting message and add handleCommand method to echo user input

--- src/main/java/HinaBot.java
+++ src/main/java/HinaBot.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 public class HinaBot {
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
+        showGreeting();
+        while (true) {
+            line = in.nextLine();
+            handleCommand(line);
+        }
+    }
+
+    private static void showGreeting() {
         System.out.println("Hello master!");
         System.out.println("What are your orders?");
-        System.out.println("Goodbye master, let's meet again soon...");
+    }
+
+    public static void handleCommand(String command) {
+        if (!command.equalsIgnoreCase("bye")) {
+            System.out.println(command);
+        }
+        else {
+            System.out.println("Goodbye master, let's meet again soon...");
+            System.exit(0);
+        }
     }
 }
