8438a828af108c63df4ec858e7c3fd809c0caff6
denzelcjy <denzel_cjy@hotmail.com>
2023-01-27 11:34:30 +0800

    Add Todo, Deadline, Event Classes

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,21 @@
+public class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String details, String by) {
+        super(details);
+        this.by = by;
+    }
+
+    public String getBy() {
+        return this.by;
+    }
+
+    public void setBy(String by) {
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() +   " (by: " + this.by + ")";
+    }
+}
---
+++ src/main/java/Event.java
@@ -0,0 +1,32 @@
+public class Event extends Task {
+
+    protected String from;
+    protected String to;
+
+    public Event(String details, String from, String to) {
+        super(details);
+        this.from = from;
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return this.from;
+    }
+
+    public String getTo() {
+        return this.to;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() +   " (from: " + this.from + " to: " + this.to + ")";
+    }
+}
