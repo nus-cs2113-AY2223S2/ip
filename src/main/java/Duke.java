61697851723d68941506ba9bf3671aa002d38f65
Gujimugger <wangsilangCHANGE@gmail.com>
2023-01-27 10:42:31 +0800

    Level 0: adding greetLine and exitLine

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greetLine();
+        exitLine();
+    }
+    public static void greetLine(){
+        System.out.println("How may I be of service?");
+    }
+
+    public static void exitLine(){
+        System.out.println("Glad I could be of help!");
     }
 }
