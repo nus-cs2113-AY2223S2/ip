f7cf7c52c6c1d07a83956d50333a911a07f5a41b
Brennanzuz <timingout365@gmail.com>
2023-01-27 16:31:58 +0800

    Added level-0. greet
    Implemented an initial skeletal version of the Duke that simply greets the user and exits.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
 public class Duke {
+    public static void printHorizontalLine()
+    {
+        System.out.println("____________________________________________________________");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +10,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printHorizontalLine();
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        printHorizontalLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontalLine();
     }
 }
