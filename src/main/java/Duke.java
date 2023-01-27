506d2c46e4443fadca9d821b9136c8f4fbc84fad
Vignesh-30 <30vignesh@gmail.com>
2023-01-27 15:36:55 +0800

    Added Level 4.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,21 +6,70 @@ public class Duke {
     static String commandList = "list";
     static String commandMark = "mark";
     static String commandUnmark = "unmark";
+    static String commandEvent = "event";
+    static String commandTodo = "todo";
+    static String commandDeadline = "deadline";
     static Task[] tasks = new Task[100];
     static int textCount = 0;
 
-    public static int commandType(String userCommand) {
-        String firstWord = userCommand.split(" ")[0];
+    public static void commandType(String userCommand) {
+        String[] extract_firstWord = userCommand.split(" ", 2);
+        String firstWord = extract_firstWord[0];
         if (firstWord.equals(commandMark)) {
-            return 0;
+            int taskNum = Integer.parseInt(extract_firstWord[1]);
+            tasks[--taskNum].setStatus(true);
+            System.out.println(line);
+            System.out.println("\tNoted. Task " + (taskNum + 1) + " has been marked as \"complete\":");
+            System.out.println("\t  " + tasks[taskNum].getTaskNameAndStatus());
+            System.out.println(line);
         } else if (firstWord.equals(commandUnmark)) {
-            return 1;
+            int taskNum = Integer.parseInt(extract_firstWord[1]);
+            tasks[--taskNum].setStatus(false);
+            System.out.println(line);
+            System.out.println("\tOh, ok. Task " + (taskNum + 1) + " has been marked as \"incomplete\":");
+            System.out.println("\t  " + tasks[taskNum].getTaskNameAndStatus());
+            System.out.println(line);
         } else if (firstWord.equals(commandList)) {
-            return 2;
+            System.out.println(line);
+            System.out.println("\tHere are your tasks:");
+            int count = 1;
+            for (int index = 0; index < textCount; index++) {
+                System.out.print("\t" + count + ".");
+                System.out.println(tasks[index]);
+                count++;
+            }
+            System.out.println(line);
         } else if (firstWord.equals(commandBye)) {
-            return 3;
+            Bye();
+        } else if (firstWord.equals(commandTodo)) {
+            tasks[textCount] = new Todo(extract_firstWord[1]);
+            textCount++;
+            System.out.println(line);
+            System.out.println("\t" + "Task added!");
+            System.out.println("\t  " + tasks[textCount - 1]);
+            System.out.println("\t" + "Now you have " + textCount + " tasks in this list.");
+            System.out.println(line);
+        } else if (firstWord.equals(commandDeadline)) {
+            int index = extract_firstWord[1].indexOf("/by");
+            tasks[textCount] = new Deadline(extract_firstWord[1].substring(0, index), extract_firstWord[1].substring(index + 4));
+            textCount++;
+            System.out.println(line);
+            System.out.println("\t" + "Task added!");
+            System.out.println("\t  " + tasks[textCount - 1]);
+            System.out.println("\t" + "Now you have " + textCount + " tasks in this list.");
+            System.out.println(line);
+        } else if (firstWord.equals(commandEvent)) {
+            int index = extract_firstWord[1].indexOf("/from");
+            int index2 = extract_firstWord[1].indexOf("/to");
+            tasks[textCount] = new Event(extract_firstWord[1].substring(0, index), extract_firstWord[1].substring(index + 6, index2 - 1), extract_firstWord[1].substring(index2 + 4));
+            textCount++;
+            System.out.println(line);
+            System.out.println("\t" + "Task added!");
+            System.out.println("\t  " + tasks[textCount - 1]);
+            System.out.println("\t" + "Now you have " + textCount + " tasks in this list.");
+            System.out.println(line);
         } else {
-            return 4;
+            System.out.println("Please input a valid command!");
         }
     }
 
@@ -44,8 +93,8 @@ public class Duke {
 
         do {
             userCommand = in.nextLine();
-            int checker = commandType(userCommand);
-
+            commandType(userCommand);
+            /*
             if (checker == 0 || checker == 1) {
                 if (checker == 0) {
                     int taskNum = Integer.parseInt(userCommand.substring(userCommand.indexOf(" ") + 1));
@@ -81,7 +130,7 @@ public class Duke {
                 System.out.println(line);
                 System.out.println("\t" + "Task added: " + userCommand);
                 System.out.println(line);
-            }
+            }*/
         } while (!userCommand.equals(commandBye));
     }
 }
