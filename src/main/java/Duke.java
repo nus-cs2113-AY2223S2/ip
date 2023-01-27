fdd85e2c7a85ac5a80aa5af6aa1c7844c79db259
Koh_Jing_Jie_Marcus <88085589+Koh-Jing-Jie-Marcus@users.noreply.github.com>
2023-01-27 09:14:12 +0800

    Add MarkasDone

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,8 +16,11 @@ public class Duke {
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
         boolean isBye = false;
-        String[] inputs;
-        inputs = new String[100];
+        Task[] inputs = new Task[100];
+        for(int k = 0; k < 100; k++) {
+            inputs[k] = new Task("");
+        }
+
         int count = 0;
         int number = 1;
 
@@ -27,22 +30,37 @@ public class Duke {
                 isBye = true;
                 break;
             }
+
             else if (Objects.equals(input, "list")) {
                 for (int i = 0; i < count; i++) {
-                    System.out.println(number + ". " + inputs[i]);
+                    System.out.println(number + ".[" + inputs[i].getStatusIcon() + "] " + inputs[i].description);
                     number++;
                 }
                 number = 1;
             }
+
+            else if(input.length() > 5 && (input.substring(0,5)).equals("mark ") && input.substring(5, input.length()).matches("[0-9]+")) {
+                    Integer order = Integer.valueOf(input.substring(5, input.length()));
+                    inputs[order - 1].markDone();
+                    System.out.println("Nice! I've marked this task as done:\n" + "[" + inputs[order - 1].getStatusIcon() + "] " + inputs[order - 1].description);
+
+                }
+
+            else if(input.length() > 7 && (input.substring(0,7)).equals("unmark ") && input.substring(7, input.length()).matches("[0-9]+")) {
+                    Integer order = Integer.valueOf(input.substring(7, input.length()));
+                    inputs[order - 1].markUndone();
+                System.out.println("OK, I've marked this task as not done yet:\n" + "[" + inputs[order - 1].getStatusIcon() + "] " + inputs[order - 1].description);
+                }
+
             else {
                 System.out.println("added: " + input);
-                inputs[count] = input;
+                inputs[count].description = input;
+                inputs[count].isDone = false;
                 count++;
             }
             input = scan.nextLine();
         }
 
         System.out.println("Bye. Hope to see you again soon!\n");
-
     }
 }
