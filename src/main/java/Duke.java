900b481f40306dfb6a6cca93c10fa80d7b8813e9
NgYJ02 <91600220+NgYJ02@users.noreply.github.com>
2023-01-27 10:49:19 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -10,10 +11,20 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
+        ArrayList<String> inputList = new ArrayList<String>();
         Scanner scan = new Scanner(System.in);
-        String input=scan.nextLine();
+        String input = scan.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                int a = 1;
+                for (String i : inputList) {
+                    System.out.println(a + ". " + i);
+                    a++;
+                }
+            } else {
+                System.out.println("added: " + input);
+                inputList.add(input);
+            }
             input = scan.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
