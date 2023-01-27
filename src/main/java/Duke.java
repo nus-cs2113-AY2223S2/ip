d047c4ea218930a279de4f6128a4f47525736171
Gujimugger <wangsilangCHANGE@gmail.com>
2023-01-27 11:00:50 +0800

    Level 1: Adding echo function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,12 +9,20 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greetLine();
+        echo();
         exitLine();
     }
     public static void greetLine(){
         System.out.println("How may I be of service?");
     }
-
+    public static void echo(){
+        String line = " ";
+        Scanner in = new Scanner(System.in);
+        while(!line.equals("bye")) {
+            line = in.nextLine();
+            System.out.println(line);
+        }
+    }
     public static void exitLine(){
         System.out.println("Glad I could be of help!");
     }
