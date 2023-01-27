489e53d715d97fa94d4516833ee40977fa6b35ee
iantenkai <iantenkai@gmail.com>
2023-01-27 10:23:22 +0800

    add scanner

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -6,7 +9,11 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
-        System.out.println("hi\n" );
-        System.out.println("bye\n");
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter some text: ");
+        String input = scanner.nextLine();
+        System.out.println("You entered: " + input);
     }
 }
