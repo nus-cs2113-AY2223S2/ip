f1f0b8c96eb6fb9a3abd2aae495e88eb5dadd443
Wilson Lee Jun Wei <leejunwei2000@gmail.com>
2023-01-27 12:58:38 +0800

    Add while loop

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,16 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void read_Command() {
-        Scanner command = new Scanner(System.in);
-        String input_Command = command.nextLine();
-        if ("bye".equals(input_Command)) { // if input is "bye"
-            System.out.println("I look forward to seeing you again! Goodbye!");
-        } else { // if input is not "bye"
-            System.out.println(input_Command);
-            read_Command();
-        }
-    }
     public static void main(String arguments[]) {
         System.out.println("Hi there! My name is Coffee");
         System.out.println("How can I help you today?");
-        read_Command();
+        Scanner command = new Scanner(System.in);
+        String input_Command = command.nextLine();
+        while (!"bye".equals(input_Command)) {
+                System.out.println(input_Command);
+                command = new Scanner(System.in);
+                input_Command = command.nextLine();
+            }
+        System.out.println("I look forward to seeing you again! Goodbye!");
+        }
     }
-}
