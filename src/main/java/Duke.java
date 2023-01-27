07a2178cff1ab7ea89db75c08e469b90a96db1fe
11-Navya <e0913130@u.nus.edu>
2023-01-27 17:42:55 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,10 +12,17 @@ public class Duke {
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Duke\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
-
-
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while(!line.equals("bye")) {
+            System.out.println("____________________________________________________________\n");
+            System.out.println("\t" + line);
+            System.out.println("____________________________________________________________\n");
+            line = in.nextLine();
+        }
+        System.out.println(" Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n");
     }
 }
