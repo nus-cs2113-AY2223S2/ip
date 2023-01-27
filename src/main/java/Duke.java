410b313e818aafbc5e48c8f7afcb221082865e5a
11-Navya <e0913130@u.nus.edu>
2023-01-27 18:22:48 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -14,15 +15,24 @@ public class Duke {
                 " What can I do for you?\n" +
                 "____________________________________________________________\n");
         String line;
+        List <String> tasks = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         while(!line.equals("bye")) {
             System.out.println("____________________________________________________________\n");
-            System.out.println("\t" + line);
-            System.out.println("____________________________________________________________\n");
+            if (line.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println("\t" + (i+1) + "." + " " + tasks.get(i));
+                }
+                System.out.println("____________________________________________________________\n");
+            } else {
+                System.out.println("\t" + "added: " + line);
+                System.out.println("____________________________________________________________\n");
+                tasks.add(line);
+            }
             line = in.nextLine();
         }
         System.out.println(" Bye. Hope to see you again soon!\n" +
-                        "____________________________________________________________\n");
+                "____________________________________________________________\n");
     }
 }
