ab76983039e218197b246d665c3cc6a16d3d435d
Liam Van <74201538+SpeciLiam@users.noreply.github.com>
2023-01-27 17:18:46 +0800

    Level-0 Increment, Greet and Exit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,22 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        greetUser();
+        exit();
     }
+
+    public static void greetUser() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void exit() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+    }
+
 }
