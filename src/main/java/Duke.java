29872d23658637580519de644b9006a19d64e7b3
Ng-YZ <e0772909@u.nus.edu>
2023-01-27 16:03:38 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
 
     public static void greet() {
-        String line = "____________________";
+        String line = "__________________________________________________________";
         System.out.println(line);
         System.out.println("Hello! i'm Duke");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine();
+        boolean exit = false;
+        while (!exit) {
+            if (userInput.equals("bye")) {
+                break;
+            } else {
+                System.out.println(line);
+                System.out.println(userInput);
+                System.out.println(line);
+                userInput = in.nextLine();
+            }
+        }
         System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line + '\n');
+        in.close();
+        return;
     }
 
     public static void main(String[] args) {
