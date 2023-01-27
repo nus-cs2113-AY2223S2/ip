d0423b78f8f231f9a8465fd20aba127e417993a2
zzs-RedCocoon <xgps_0703zzs@126.com>
2023-01-27 23:24:40 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,11 @@
 import java.util.*;
 
 public class Duke {
-    static final String HORIZONTAL_LINES ="____________________________________________________________";
+
+    public static final int TASK_LENGTH = 100;
+    static String[] tasksList = new String[TASK_LENGTH];
+    static int tasksListIndex = 0;
+
     public static void sendLogo(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -12,12 +16,12 @@ public class Duke {
     }
 
     public static void sendGreeting(){
-        String greeting_word="____________________________________________________________\n"
+        String greeting_word="------------------------------------------------------------\n"
             + "Hello! I'm miniJohn\n"
             + "What can I do for you?\n"
-            + "____________________________________________________________\n"
+            + "------------------------------------------------------------\n"
             + "Bye. Hope to see you again soon!\n"
-            + "____________________________________________________________";
+            + "------------------------------------------------------------";
         System.out.println(greeting_word);
     }
 
@@ -27,19 +31,70 @@ public class Duke {
         return line;
     }
 
+    public static String[] getCommandTypeAndParams(String userCommand){
+        String[] userCommandWords = userCommand.split(" ");
+        String commandType = userCommandWords[0];
+        String commandParams = null;
+        if(userCommandWords.length > 1){
+            commandParams = String.join(" ",Arrays.copyOfRange(userCommandWords,1,userCommandWords.length));
+        }
+        String[] commandTypeAndParams = {commandType, commandParams};
+        return commandTypeAndParams;
+    }
+
+    public static String executeListCommand(){
+        String tasksListString = "";
+        int sequence = 1;
+        for(String task: tasksList){
+            if(task == null)break;
+            tasksListString += sequence + ". " + task + "\n";
+            sequence += 1;
+        }
+        return tasksListString;
+    }
+
+    public static String executeAddTaskCommand(String newTask){
+        if(newTask == null)return "";
+
+        tasksList[tasksListIndex] = newTask;
+        tasksListIndex += 1;
+
+        String feedback = "added: " + newTask;
+        return feedback;
+    }
+
     public static String executeCommand(String userCommand){
-        String feedback = userCommand;
-        if(userCommand.equals("bye")){
-            feedback = "END";
+        String[] commandTypeAndParams = getCommandTypeAndParams(userCommand);
+        String commandType = commandTypeAndParams[0];
+        String commandParams = commandTypeAndParams[1];
+
+//        System.out.println("commandType="+commandType);
+//        System.out.println("commandParams="+commandParams);
+
+        String feedback = null;
+        if(commandType.equals("list")){
+            feedback = executeListCommand();
+        }else{//add an item
+            feedback = executeAddTaskCommand(commandParams);
         }
         return feedback;
     }
 
     public static void showResultToUser(String feedback){
-        System.out.println("\t"+HORIZONTAL_LINES);
-        System.out.println("\t"+feedback);
-        System.out.println("\t"+HORIZONTAL_LINES);
+        if(feedback == ""){
+            return;
+        }
+        //print String feedback line by line
+        System.out.println("\t------------------------------------------------------------");
+        Scanner scanner = new Scanner(feedback);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println('\t'+line);
+        }
+        scanner.close();
+        System.out.println("\t------------------------------------------------------------");
 
+        return;
     }
 
     public static void main(String[] args) {
@@ -47,12 +102,12 @@ public class Duke {
         sendGreeting();
         while(true){
             String userCommand = getUserInput();
-            String feedback = executeCommand(userCommand);
-            if(feedback=="END"){
+            if(userCommand.toLowerCase().equals("bye")){
                 break;
             }
+            String feedback = executeCommand(userCommand);
             showResultToUser(feedback);
-
         }
+        return;
     }
 }
