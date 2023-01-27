ef8d231475c0e1288496d48269d5bc485ce35ea6
monihog <e0725619@u.nus.edu>
2023-01-27 12:26:25 +0800

    Added echo Changed Duke's Name

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 public class Duke {
     public static void exitMessage() {
-        System.out.println("Goodbye!");
+        System.out.println("Go away Anna");
+        System.out.println("O-kay bye......");
     }
     public static void main(String[] args) {
-        System.out.println("Hi it's Duke\nWhat can I do for you?");
-        exitMessage();
+        System.out.println("Hi it's Anna!\nWhat can I do for you?");
+        Scanner in = new Scanner(System.in);
+        String input;
+        boolean exit = false;
+        while (!exit) {
+            input = in.nextLine();
+            if (input.equals("bye")) {
+                exitMessage();
+                exit = true;
+            } else {
+                System.out.println(input);
+            }
+        }
     }
 }
