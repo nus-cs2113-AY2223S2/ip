36b7e1ef2676527895c9db7b145331e36fbc9d2c
Nigel Chen Chin Hao <nigel_ncch@hotmail.com>
2023-01-27 21:42:55 +0800

    Add read user input and echo back on console

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        final String HORIZONTAL_LINE = "____________________________________________________________";
+
+        System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(HORIZONTAL_LINE + '\n');
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String userInput = sc.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            System.out.println(HORIZONTAL_LINE);
+            System.out.println(userInput);
+            System.out.println(HORIZONTAL_LINE + '\n');
+        }
+
+        System.out.println(HORIZONTAL_LINE);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(HORIZONTAL_LINE + '\n');
     }
 }
