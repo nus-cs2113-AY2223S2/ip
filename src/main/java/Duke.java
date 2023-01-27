9e15420b9c46b372036ee7575c3ec64be873266b
Koh_Jing_Jie_Marcus <88085589+Koh-Jing-Jie-Marcus@users.noreply.github.com>
2023-01-27 17:38:00 +0800

    Update Coding Standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -29,35 +29,31 @@ public class Duke {
             if (Objects.equals(input, "bye")) {
                 isBye = true;
                 break;
-            }
 
-            else if (Objects.equals(input, "list")) {
+            } else if (Objects.equals(input, "list")) {
                 for (int i = 0; i < count; i++) {
                     System.out.println(number + ".[" + inputs[i].getStatusIcon() + "] " + inputs[i].description);
                     number++;
                 }
                 number = 1;
-            }
 
-            else if (input.length() > 5 && (input.substring(0,5)).equals("mark ") && input.substring(5, input.length()).matches("[0-9]+")) {
+            } else if (input.length() > 5 && (input.substring(0,5)).equals("mark ") && input.substring(5, input.length()).matches("[0-9]+")) {
                     Integer order = Integer.valueOf(input.substring(5, input.length()));
                     inputs[order - 1].markDone();
                     System.out.println("Nice! I've marked this task as done:\n" + "[" + inputs[order - 1].getStatusIcon() + "] " + inputs[order - 1].description);
 
-                }
-
-            else if (input.length() > 7 && (input.substring(0,7)).equals("unmark ") && input.substring(7, input.length()).matches("[0-9]+")) {
+            } else if (input.length() > 7 && (input.substring(0,7)).equals("unmark ") && input.substring(7, input.length()).matches("[0-9]+")) {
                     Integer order = Integer.valueOf(input.substring(7, input.length()));
                     inputs[order - 1].markUndone();
                     System.out.println("OK, I've marked this task as not done yet:\n" + "[" + inputs[order - 1].getStatusIcon() + "] " + inputs[order - 1].description);
-                }
 
-            else {
+            } else {
                 System.out.println("added: " + input);
                 inputs[count].description = input;
                 inputs[count].isDone = false;
                 count++;
             }
+
             input = scan.nextLine();
         }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -5,6 +5,7 @@ public class Task {
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        
     }
 
     public String getStatusIcon() {
