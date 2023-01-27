5e28b15df81d7b7c7bba4d4f5b322133f38f0f4b
Thunderdragon221 <81968036+Thunderdragon221@users.noreply.github.com>
2023-01-27 23:24:03 +0800

    Changed name, added bye(), increment level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,30 @@
+import java.util.Scanner;
 public class Duke {
 
     public static void greet() {
         System.out.println("________________________________");
-        System.out.println("Hello! I'm Duke");
+        System.out.println("Hello! I'm ChatTPG");
         System.out.println("What can I do for you?");
+        System.out.println("________________________________");
+    }
+
+    public static void bye(){
         System.out.println("________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("________________________________");
     }
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
         greet();
+        Scanner in = new Scanner(System.in);
+        String input;
+        input = in.nextLine();
+        while (input.compareTo("bye") != 0){
+            System.out.println("________________________________");
+            System.out.println(input);
+            System.out.println("________________________________");
+            input = in.nextLine();
+        }
+        bye();
     }
 }
