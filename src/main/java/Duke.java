04eb17e64d0900e69fddd2d00f0b2011540f9d89
chiayuxuan <chiayuxuan1909@gmail.com>
2023-01-27 16:22:08 +0800

    Add Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,11 +6,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.print("____________________________________________________________\n"
+                + "Hello! I'm Duke\nWhat can I do for you?\n"
+                +"____________________________________________________________\n"
+                +"Bye. Hope to see you again soon!\n" 
+                +"____________________________________________________________\n");
+
     }
 }
