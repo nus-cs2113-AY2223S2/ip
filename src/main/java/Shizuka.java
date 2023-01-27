189898d8bc3e3b4b925bc09dee1bf18622fcfbc2
SpawnageLoong <richardloongcj@gmail.com>
2023-01-27 17:50:35 +0800

    Refactor code

--- src/main/java/Shizuka.java
+++ src/main/java/Shizuka.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 
 public class Shizuka {
+    public static String parseCommand(String args) {
+        String command;
+        int endIndex = args.indexOf(' ') + 1;
+        if (endIndex != 0) {
+            command = args.substring(0, endIndex - 1);
+        } else {
+            command = args;
+        }
+        return command;
+    }
+
+    public static int parseNumber(String args) {
+        int endIndex = args.indexOf(' ') + 1;
+        return Integer.parseInt(args.substring(endIndex));
+    }
+
     public static void main(String[] args) {
         final String LOGO = "   _____  __     _               __         \n"
                 + "  / ___/ / /_   (_)____  __  __ / /__ ____ _\n"
@@ -12,37 +28,30 @@ public class Shizuka {
         final String EXIT = "Goodbye.\n";
         System.out.println(LINE_BREAK + LOGO + LINE_BREAK + GREETING + LINE_BREAK);
         Scanner in = new Scanner(System.in);
-        String line;
+        String line, lineTrimmed;
         do {
             line = in.nextLine();
-            String lineTrimmed = line.trim();
-            int wordEnd = lineTrimmed.indexOf(' ') + 1;
-            String command;
-            if (wordEnd != 0) {
-                command = lineTrimmed.substring(0, wordEnd - 1);
-            } else {
-                command = lineTrimmed;
-            }
+            lineTrimmed = line.trim();
             int taskNum;
-            switch (command) {
+            switch (parseCommand(line)) {
             case "bye":
                 break;
             case "list":
                 TodoList.list();
                 break;
             case "mark":
-                taskNum = Integer.parseInt(lineTrimmed.substring(wordEnd));
+                taskNum = parseNumber(lineTrimmed);
                 TodoList.mark(taskNum);
                 break;
             case "unmark":
-                taskNum = Integer.parseInt(lineTrimmed.substring(wordEnd));
+                taskNum = parseNumber(lineTrimmed);
                 TodoList.unmark(taskNum);
                 break;
             default:
-                TodoList.add(line);
+                TodoList.add(lineTrimmed);
             }
         }
-        while (!line.equals("bye"));
+        while (!lineTrimmed.equals("bye"));
         System.out.println(LINE_BREAK + EXIT + LINE_BREAK);
     }
 }
