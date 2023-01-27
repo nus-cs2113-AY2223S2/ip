a6c97a1b2cf8ca8895ce5a238f88e622046e77a2
tzixi <tanzixi9@gmail.com>
2023-01-27 12:10:24 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
         //String logo = " ____        _        \n"
         //        + "|  _ \\ _   _| | _____ \n"
@@ -6,9 +10,24 @@ public class Duke {
         //        + "| |_| | |_| |   <  __/\n"
         //        + "|____/ \\__,_|_|\\_\\___|\n";
         //System.out.println("Hello from\n" + logo);
+        String line;
+        Scanner input = new Scanner(System.in);
         String space = "____________________________";
+
+        //greet
         System.out.println(space + "\nHello! I'm Duke");
         System.out.println("What can I do for you?\n" + space);
-        System.out.println("Bye. Hope to see you again soon!" + space);
+
+        //echo
+        while (true){
+            line = input.nextLine();
+            if (Objects.equals(line, "bye")){
+                break;
+            }
+            System.out.println(line + '\n' + space);
+        }
+
+        //exit
+        System.out.println(space + "\nBye. Hope to see you again soon!\n" + space);
     }
 }
