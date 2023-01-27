b1f348108572330d72028a67a1b90125663a49dc
Gujimugger <wangsilangCHANGE@gmail.com>
2023-01-27 15:04:06 +0800

    Level 1,2: Rectify mistake in echo, where "bye" will also be echoed. Complete level 2.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -9,21 +10,47 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greetLine();
-        echo();
+        addList();
         exitLine();
     }
     public static void greetLine(){
         System.out.println("How may I be of service?");
     }
-    public static void echo(){
-        String line = " ";
+    public static void echo()
+    {
+        String line;
         Scanner in = new Scanner(System.in);
+        line = in.nextLine();
         while(!line.equals("bye")) {
-            line = in.nextLine();
             System.out.println(line);
+            line = in.nextLine();
         }
     }
     public static void exitLine(){
         System.out.println("Glad I could be of help!");
+    }
+
+    public static void addList()
+    {
+        String[] list = new String[100];
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        int numOfItems = 0;
+        while(!line.equals("bye")) {
+            if(line.equals("list")) { // users wants to know all text so far
+                String[] subList = Arrays.copyOf(list, numOfItems);
+                for(int i = 0; i < subList.length; ++i)
+                {
+                    System.out.println(subList[i]);
+                }
+            }
+            else { // text from user
+                String item = (numOfItems + 1) + ". " + line; // convert to 1-based
+                list[numOfItems] = item;
+                ++numOfItems;
+            }
+            line = in.nextLine();
+        }
     }
 }
