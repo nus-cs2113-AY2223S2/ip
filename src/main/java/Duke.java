cebb94d4dd03bdbc40a66eefc33efa179b91f7a5
Koh_Jing_Jie_Marcus <88085589+Koh-Jing-Jie-Marcus@users.noreply.github.com>
2023-01-27 09:23:42 +0800

    Update Coding Standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -39,17 +39,17 @@ public class Duke {
                 number = 1;
             }
 
-            else if(input.length() > 5 && (input.substring(0,5)).equals("mark ") && input.substring(5, input.length()).matches("[0-9]+")) {
+            else if (input.length() > 5 && (input.substring(0,5)).equals("mark ") && input.substring(5, input.length()).matches("[0-9]+")) {
                     Integer order = Integer.valueOf(input.substring(5, input.length()));
                     inputs[order - 1].markDone();
                     System.out.println("Nice! I've marked this task as done:\n" + "[" + inputs[order - 1].getStatusIcon() + "] " + inputs[order - 1].description);
 
                 }
 
-            else if(input.length() > 7 && (input.substring(0,7)).equals("unmark ") && input.substring(7, input.length()).matches("[0-9]+")) {
+            else if (input.length() > 7 && (input.substring(0,7)).equals("unmark ") && input.substring(7, input.length()).matches("[0-9]+")) {
                     Integer order = Integer.valueOf(input.substring(7, input.length()));
                     inputs[order - 1].markUndone();
-                System.out.println("OK, I've marked this task as not done yet:\n" + "[" + inputs[order - 1].getStatusIcon() + "] " + inputs[order - 1].description);
+                    System.out.println("OK, I've marked this task as not done yet:\n" + "[" + inputs[order - 1].getStatusIcon() + "] " + inputs[order - 1].description);
                 }
 
             else {
