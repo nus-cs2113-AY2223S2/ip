a065a690cf8ee7af15437ff62f60b59d6569f03e
0nandon <tommy9905@gmail.com>
2023-01-27 15:48:34 +0800

    iP : Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,33 @@
+
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println(
-                "____________________________________________________________\n" +
-                " Hello! I'm Duke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n" +
-                "\n"
+                "    ____________________________________________________________\n" +
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________\n"
         );
+
+        Scanner in = new Scanner(System.in);
+        String input_text;
+        while(true){
+            input_text = in.nextLine();
+            if(input_text.length() == 0)
+                break;
+            System.out.println(
+                    "    ____________________________________________________________\n\t " +
+                    input_text +
+                    "     \n\t____________________________________________________________\n"
+            );
+        }
+        System.out.println(
+                "     ____________________________________________________________\n" +
+                "      Bye. Hope to see you again soon!\n" +
+                "     ____________________________________________________________\n"
+        );
+
+        in.close();
     }
 }
