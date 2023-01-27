66f05b6900771150d6db68a3cf3e7bd804b6beb0
Liam Van <74201538+SpeciLiam@users.noreply.github.com>
2023-01-27 17:35:36 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Duke {
+
+    private ArrayList<String> userInput;
+
+    public Duke() {
+        this.userInput = new ArrayList<>();
+    }
+
     public static void main(String[] args) {
+
+        Duke duke = new Duke();
+        boolean isRun = true;
+        Scanner scan = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -8,19 +22,39 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         greetUser();
+
+        while (true) {
+            String input = scan.nextLine();
+            duke.userInput.add(input);
+            if (input.equals("bye")) {
+                break;
+            }
+            echoCmd(input);
+
+
+        }
         exit();
     }
 
+    public static void echoCmd(String cmd) {
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t " + cmd);
+        System.out.println("\t____________________________________________________________");
+
+    }
+
     public static void greetUser() {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tHello! I'm Duke");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("\t____________________________________________________________");
+
     }
 
     public static void exit() {
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("\t____________________________________________________________");
 
     }
 
