40fd7a08bba137c7d66fdae0e35f59b016626a84
iantenkai <iantenkai@gmail.com>
2023-01-27 16:58:23 +0800

    add level 2 add waifu material

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,22 +3,31 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
+        String[] library = new String[100];
+        int index = 0;
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.print("あなたの願いを入力下さい");
+            System.out.println("おかえり～　ご飯にする？お風呂にする？それとも。。。　わ・た・し？");
             String input = scanner.nextLine();
 
             if (input.equals("bye")) {
                 break;
-            } else {
-                System.out.println("You entered: " + input);
+            }
 
+            if (input.equals("list")) {
+                if (index == 0) {
+                    System.out.println("何もいない。。。頭のように。。。");
+                } else {
+                    for (int i = 0; i < index; i++) {
+                        System.out.println(i + 1 + ". " + library[i]);
+                    }
+                }
+            } else if (index < 100) {
+                library[index] = input;
+                System.out.println("覚えましたよ～ " + input);
+                index++;
+            } else {
+                System.out.println("もういっぱい～！\" ");
             }
 
         }
