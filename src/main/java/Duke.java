23d6732dadea7fab9418550433262704f0660e2b
Nigel Chen Chin Hao <nigel_ncch@hotmail.com>
2023-01-27 21:50:35 +0800

    Add store user input and display back when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,30 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    final static String HORIZONTAL_LINE = "____________________________________________________________";
 
-    public static void main(String[] args) {
-        final String HORIZONTAL_LINE = "____________________________________________________________";
-
+    public static void greeting() {
         System.out.println(HORIZONTAL_LINE);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println(HORIZONTAL_LINE + '\n');
 
+        return;
+    }
+
+    public static void exit() {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(HORIZONTAL_LINE + '\n');
+
+        return;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> userTexts = new ArrayList<String>();
+
+        greeting();
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -17,13 +32,22 @@ public class Duke {
             if (userInput.equals("bye")) {
                 break;
             }
+
+            String firstWord = userInput.split(" ", 2)[0];
+
             System.out.println(HORIZONTAL_LINE);
-            System.out.println(userInput);
+            if (firstWord.equals("list")) {
+                for (int i = 0; i < userTexts.size(); i++) {
+                    System.out.println((i + 1) + ". " + userTexts.get(i));
+                }
+            } else {
+                userTexts.add(userInput);
+                System.out.println("added: " + userInput);
+            }
             System.out.println(HORIZONTAL_LINE + '\n');
         }
 
-        System.out.println(HORIZONTAL_LINE);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(HORIZONTAL_LINE + '\n');
+        exit();
+
     }
 }
