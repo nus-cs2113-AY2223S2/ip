43c3f234c1325dadf90fce3f634d1f71e7d22248
AbijithRam <abijithramcr7@gmail.com>
2023-01-28 11:27:01 +0800

    Committing Level-2

--- src/main/java/Rolex.java
+++ src/main/java/Rolex.java
@@ -1,40 +1,42 @@
 import java.util.Scanner;
 public class Rolex {
 
-    //method to print bye and exit program.
-    public static void printBye(){
-        System.out.println("_______________________________________");
-        System.out.println("Bye. Hope to see you again.");
-        System.out.println("_______________________________________");
-        System.exit(0);
-
-    }
-
-    //method to echo input given by user
-    public static void printEcho(String UserInput){
-        System.out.println("_______________________________________");
-        System.out.println(UserInput);
-        System.out.println("_______________________________________");
-    }
     public static void main(String[] args) {
 
         System.out.println("Hello! I'm ROLEX");
-        System.out.println("What can I do for you?\n");
+        System.out.println("What can I do for you?\n\n");
 
         Scanner ReadInput = new Scanner(System.in);
-        int count = 0;
 
-        while(count==0){
+        String[] books = new String[100];
+        int BookCount = 0;
+
+        while(true){
             String UserInput = ReadInput.nextLine();
 
             if(UserInput.equalsIgnoreCase("bye")){
-                printBye();
-                count++;
-            }
-            else{
-                printEcho(UserInput);
+
+                System.out.println("--------------------------------------------------");
+                System.out.println("Bye. Hope to see you again soon!\n");
+                System.out.println("--------------------------------------------------");
+                System.exit(0);
             }
 
+            else if(UserInput.equalsIgnoreCase("list")){
+                System.out.println("--------------------------------------------------");
+                for(int i=0; i<BookCount; i++) {
+                    System.out.println(i+1 + ". " + books[i]);
+                }
+                System.out.println("--------------------------------------------------");
+            }
+
+            else{
+                books[BookCount] = UserInput;
+                BookCount++;
+                System.out.println("--------------------------------------------------");
+                System.out.println("added: " + books[BookCount-1]);
+                System.out.println("--------------------------------------------------");
+            }
         }
 
     }
