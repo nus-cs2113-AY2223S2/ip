c85dc79dd8f201868a263106c791bed6c8b3c26f
AbijithRam <abijithramcr7@gmail.com>
2023-01-28 11:01:11 +0800

    Committing Level 0

---
+++ src/main/java/Rolex.java
@@ -0,0 +1,18 @@
+public class Rolex {
+    public static void main(String[] args) {
+
+        System.out.println("Hello from ROLEX!");
+
+        System.out.println("Hello! I'm ROLEX!");
+
+        System.out.println("What can I do for you?");
+
+        System.out.println("Bye. Hope to see you again soon!");
+
+    }
+
+}
+
+
+
+
