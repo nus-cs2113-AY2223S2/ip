3e02793ce568d272de5e7a1bcfa80099ce68827a
NivethaUdayakumar <nivethaus4@gmail.com>
2023-01-29 00:23:49 +0800

    Update Duke to support todo, deadline, and event tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,4 @@
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -13,8 +11,8 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
 
         String line;
@@ -23,40 +21,66 @@ public class Duke {
         int taskCount = 0;
         while (true) {
             line = in.nextLine();
-            if ("bye".equalsIgnoreCase(line)) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
-                break;
-            } else if ("list".equalsIgnoreCase(line)) {
+            if ("list".equalsIgnoreCase(line)) {
                 System.out.println("____________________________________________________________");
                 System.out.println(" Here are the tasks in your list:");
                 int index = 1;
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println(" " + index + ". [" + tasks[i].getStatusIcon() +"] " + tasks[i].getDescription());
+                    System.out.println("  " + index + "." + tasks[i].toString());
                     index++;
                 }
                 System.out.println("____________________________________________________________");
-            } else if ((line.substring(0,4)).equals("mark")) {
+            } else if ((line.substring(0,4)).equalsIgnoreCase("mark")) {
                 System.out.println("____________________________________________________________");
                 System.out.println(" Nice! I've marked this task as done:");
                 int index = Integer.parseInt(line.substring(5));
                 tasks[index-1].markAsDone();
-                System.out.println(" [" + tasks[index-1].getStatusIcon() +"] " + tasks[index-1].getDescription());
+                System.out.println(tasks[index-1].toString());
                 System.out.println("____________________________________________________________");
-            } else if ((line.substring(0,6)).equals("unmark")) {
+            } else if ((line.substring(0,6)).equalsIgnoreCase("unmark")) {
                 System.out.println("____________________________________________________________");
-                System.out.println(" OK, I've marked this task as not done yet:");
+                System.out.println(" Okay, I've marked this task as not done yet:");
                 int index = Integer.parseInt(line.substring(7));
                 tasks[index-1].markAsNotDone();
-                System.out.println(" [" + tasks[index-1].getStatusIcon() +"] " + tasks[index-1].getDescription());
+                System.out.println(tasks[index-1].toString());
                 System.out.println("____________________________________________________________");
-            } else {
-                tasks[taskCount] = new Task(line);
+            } else if((line.substring(0,4)).equalsIgnoreCase("todo")) {
+                tasks[taskCount] = new Todo(line.substring(5));
+                System.out.println("____________________________________________________________");
+                System.out.println(" Got it. I've added this task:");
+                System.out.println(tasks[taskCount].toString());
                 taskCount++;
+                System.out.println(" Now you have " + taskCount + " tasks in your list.");
                 System.out.println("____________________________________________________________");
-                System.out.println(" added: " + line);
+            } else if((line.substring(0,8)).equalsIgnoreCase("deadline")) {
+                int dividerPosition = line.indexOf("/by");
+                String taskDescription = line.substring(9, dividerPosition);
+                String deadline = line.substring(dividerPosition+4);
+                tasks[taskCount] = new Deadline(taskDescription, deadline);
                 System.out.println("____________________________________________________________");
+                System.out.println(" Got it. I've added this task:");
+                System.out.println(tasks[taskCount].toString());
+                taskCount++;
+                System.out.println(" Now you have " + taskCount + " tasks in your list.");
+                System.out.println("____________________________________________________________");
+            } else if((line.substring(0,5)).equalsIgnoreCase("event")) {
+                int divider1 = line.indexOf("/from");
+                String event = line.substring(6, divider1);
+                int divider2 = line.indexOf("/to");
+                String eventStart = line.substring(divider1+6, divider2);
+                String eventEnd = line.substring(divider2+4);
+                tasks[taskCount] = new Event(event, eventStart, eventEnd);
+                System.out.println("____________________________________________________________");
+                System.out.println(" Got it. I've added this task:");
+                System.out.println(tasks[taskCount].toString());
+                taskCount++;
+                System.out.println(" Now you have " + taskCount + " tasks in your list.");
+                System.out.println("____________________________________________________________");
+            } else if ("bye".equalsIgnoreCase(line)) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
             }
         }
     }
