9f44c70b3f1b15b3a8efacb7b0994d47b74607b5
hyperbola-bear <hung.hin.wang@gmail.com>
2023-01-28 17:22:42 +0800

    lvl-2 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -12,12 +13,22 @@ public class Duke {
         System.out.println("What can I do for you?");
         Scanner in = new Scanner(System.in);
         String input;
-        while((input = in.next()) != "") {
+        ArrayList<String> items = new ArrayList<String>();
+        while((input = in.nextLine()) != "") {
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
+                in.close();
                 return;
             }
-            System.out.println(input);
+            else if (input.equals("list")) {
+                int count = 1;
+                for(String list_item : items) {
+                    System.out.println(Integer.toString(count) +". " + list_item);
+                    count++;
+                }
+            }
+            items.add(input);
+            System.out.println("added: " + input);
         }
         
     }
