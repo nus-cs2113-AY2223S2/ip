765e4c14dccfaf9773393d6eeb8bbfa7e1680714
AbijithRam <abijithramcr7@gmail.com>
2023-01-28 11:03:27 +0800

    Committing Level-1

--- src/main/java/Rolex.java
+++ src/main/java/Rolex.java
@@ -1,13 +1,41 @@
+import java.util.Scanner;
 public class Rolex {
+
+    //method to print bye and exit program.
+    public static void printBye(){
+        System.out.println("_______________________________________");
+        System.out.println("Bye. Hope to see you again.");
+        System.out.println("_______________________________________");
+        System.exit(0);
+
+    }
+
+    //method to echo input given by user
+    public static void printEcho(String UserInput){
+        System.out.println("_______________________________________");
+        System.out.println(UserInput);
+        System.out.println("_______________________________________");
+    }
     public static void main(String[] args) {
 
-        System.out.println("Hello from ROLEX!");
+        System.out.println("Hello! I'm ROLEX");
+        System.out.println("What can I do for you?\n");
 
-        System.out.println("Hello! I'm ROLEX!");
+        Scanner ReadInput = new Scanner(System.in);
+        int count = 0;
 
-        System.out.println("What can I do for you?");
+        while(count==0){
+            String UserInput = ReadInput.nextLine();
 
-        System.out.println("Bye. Hope to see you again soon!");
+            if(UserInput.equalsIgnoreCase("bye")){
+                printBye();
+                count++;
+            }
+            else{
+                printEcho(UserInput);
+            }
+
+        }
 
     }
 
