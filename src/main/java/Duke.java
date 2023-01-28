8c0b21f442ea56c524642f8581b5553125935b82
hyperbola-bear <hung.hin.wang@gmail.com>
2023-01-28 17:39:48 +0800

    lvl-3 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -14,6 +16,8 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         String input;
         ArrayList<String> items = new ArrayList<String>();
+        ArrayList<Boolean> marked = new ArrayList<Boolean>();
+        
         while((input = in.nextLine()) != "") {
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
@@ -21,14 +25,29 @@ public class Duke {
                 return;
             }
             else if (input.equals("list")) {
-                int count = 1;
-                for(String list_item : items) {
-                    System.out.println(Integer.toString(count) +". " + list_item);
-                    count++;
+                
+                for(int i = 0; i < items.size(); i++) {
+                    System.out.println(i+1 +". " +  (marked.get(i) ? "[X] " : "[ ] ")   + items.get(i));
+                    
                 }
             }
-            items.add(input);
-            System.out.println("added: " + input);
+            else if (input.startsWith("mark")) {
+                String[] strArray = input.split(" ");
+                int num = Integer.parseInt(strArray[1]);
+                marked.set(num-1, true);
+                System.out.println("Nice! I've marked this task as done:\n" + "[X] " + items.get(num-1));
+            }
+            else if (input.startsWith("unmark")) {
+                String[] strArray = input.split(" ");
+                int num = Integer.parseInt(strArray[1]);
+                marked.set(num-1, false);
+                System.out.println("Ok, I've marked this task as not done yet:\n" + "[ ] " + items.get(num-1));
+            }
+            else {
+                items.add(input);
+                marked.add(false);
+                System.out.println("added: " + input);
+            }
         }
         
     }
