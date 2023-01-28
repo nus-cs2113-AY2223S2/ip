415907f0c2d93754cc149726d33afbbb045f551f
LAPTOP-52GQO9KI\zhito <e0725202@u.nus.edu>
2023-01-28 17:21:25 +0800

    refactored some of the mess in Duke main()

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,6 +18,7 @@ public class Duke
     {
         lineBreak ();
         System.out.println (GREETING);
+        lineBreak ();
     }
 
     public static void goodbye ()
@@ -32,29 +33,43 @@ public class Duke
 
         int index = 1;
         String line;
-        Task[] tasks = new Task[102];
+        Task[] tasks = new Task[101];
         Scanner in = new Scanner (System.in);
-        lineBreak ();
         line = in.nextLine ();
 
-        while (!line.equals ("bye")) {
+        while (true) {
 
+            if (line.equals ("bye"))
+            {
+                goodbye ();
+                System.exit (0);
+            }
 
-            if (line.equals ("list")) {
+            if (line.equals ("list"))
+            {
                 for (int i = 1; i < index; i++) {
                     System.out.println (i + ": " + tasks[i].getTaskStatus ());
                 }
-            } else if (line.length () > 5 && line.startsWith ("mark")) {
+
+            }
+            else if (line.length () > 5 && line.startsWith ("mark"))
+            {
                 int pointer = Integer.parseInt (line.substring (5));
                 tasks[pointer].setDone ();
-                System.out.println ("Nice! I've marked this task as done:\n" + tasks[pointer].getTaskStatus ());
+                System.out.println ("Nice! I've marked this task as done:\n"
+                        + tasks[pointer].getTaskStatus ());
 
-            } else if (line.length () > 7 && line.startsWith ("unmark")) {
+            }
+            else if (line.length () > 7 && line.startsWith ("unmark"))
+            {
                 int pointer = Integer.parseInt (line.substring (7));
                 tasks[pointer].setNotDone ();
-                System.out.println ("Yikes! I've marked this task as not done:\n" + tasks[pointer].getTaskStatus ());
+                System.out.println ("Yikes! I've marked this task as not done:\n"
+                        + tasks[pointer].getTaskStatus ());
 
-            } else {
+            }
+            else
+            {
                 System.out.println ("added: " + line);
                 tasks[index] = new Task (line);
                 index += 1;
@@ -65,8 +80,5 @@ public class Duke
 
 
         }
-
-        goodbye ();
-
     }
 }
