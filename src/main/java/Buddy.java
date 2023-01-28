f2d0bfe97da4393274fe4606e20cf5f71978e4d1
SaiChaitanya13 <gchaitanya@u.nus.edu>
2023-01-28 22:57:06 +0800

    Added Increment - Level 1

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 public class Buddy {
     public static void main(String[] args) {
         String greeting = "Hello there! I'm Buddy\n"
-                        + "How may I assist you?";
+                + "How may I assist you?";
         String exitMessage = "Hope I was of help to you! Have a great day and see you again, Buddy :)";
         String divider = "________________________________________________________________________________";
+
         System.out.println(divider);
         System.out.println(greeting);
         System.out.println(divider);
+
+        String command;
+        Scanner in = new Scanner(System.in);
+        command = in.nextLine();
+
+        while (! command.equals("bye")){
+            System.out.println(divider);
+            System.out.println(command);
+            System.out.println(divider);
+            command = in.nextLine();
+        }
+
+        System.out.println(divider);
         System.out.println(exitMessage);
+        System.out.println(divider);
 
     }
 }
