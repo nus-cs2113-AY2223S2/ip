5b005888922e8b36c91dc0f49deda57e0f0b144b
chiayuxuan <chiayuxuan1909@gmail.com>
2023-01-28 18:45:55 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,32 @@
+
+import java.util.Scanner;
 public class Duke {
+    public static void echoCommands(String input){
+        System.out.println("\t____________________________________________________________\n\t "
+                 + input + "\n\t____________________________________________________________\n");
+
+    }
     public static void main(String[] args) {
+        String divider = "\t____________________________________________________________\n";
+        Scanner in = new Scanner(System.in);
+        String line;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.print("____________________________________________________________\n"
-                + " Hello! I'm Duke\n What can I do for you?\n"
-                +"____________________________________________________________\n"
-                +" Bye. Hope to see you again soon!\n"
-                +"____________________________________________________________\n");
+        System.out.print(divider
+                + " \t Hello! I'm Duke\n\t What can I do for you?\n"+ divider);
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            echoCommands(line);
+            line = in.nextLine();
+            }
+
+        System.out.print(divider +"\t Bye. Hope to see you again soon!\n" +divider);
+
+
 
     }
 }
