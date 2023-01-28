cbe03ba3bd3e407dd9ea03adc02a231940324c8e
SaiChaitanya13 <gchaitanya@u.nus.edu>
2023-01-28 22:22:32 +0800

    Added Increment - Level 0

---
+++ src/main/java/Buddy.java
@@ -0,0 +1,13 @@
+public class Buddy {
+    public static void main(String[] args) {
+        String greeting = "Hello there! I'm Buddy\n"
+                        + "How may I assist you?";
+        String exitMessage = "Hope I was of help to you! Have a great day and see you again, Buddy :)";
+        String divider = "________________________________________________________________________________";
+        System.out.println(divider);
+        System.out.println(greeting);
+        System.out.println(divider);
+        System.out.println(exitMessage);
+
+    }
+}
