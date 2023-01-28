d4a0bf5797fae6afad794b8b8d54bed3659b0cbd
vishnu4157 <e0727211@u.nus.edu>
2023-01-28 15:56:04 +0800

    level-3 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,8 @@
 import java.util.Locale;
 import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
 public class Duke {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
@@ -12,19 +15,35 @@ public class Duke {
         System.out.println("what can i do for you");
 
         String input = myObj.nextLine();
-        String[] todo = new String[100];
+        Todos[] todo = new Todos[100];
         int c = 0;
         while(!input.equalsIgnoreCase("bye")) {
             if (input.equalsIgnoreCase("list")) {
+                System.out.println("-------TODO-LIST------");
                 System.out.println("----------------------");
                 for (int i = 0; i < todo.length && todo[i] != null; i++) {
-                    System.out.println(i + ". " + todo[i]);
+                    char x = ' ';
+                    if(todo[i].marked) {
+                        x = 'X';
+                    }
+                    System.out.println(i+1 + ". " + "[" + x + "] " + todo[i].item);
                 }
                 System.out.println("----------------------");
-            } else {
-                todo[c] = input;
-                System.out.println("Added: " + input);
+            } else if (input.startsWith("mark")){
+                int i = Integer.parseInt(input.substring(5));
+                todo[i-1].setMark();
+                System.out.println("Item has been marked");
+                System.out.println(i + ". " + "[X] " + todo[i-1].item);
+            } else if (input.startsWith("unmark")){
+                int i = Integer.parseInt(input.substring(7));
+                todo[i-1].unMark();
+                System.out.println("Item has been unmarked");
+                System.out.println(i + ". " + "[ ] " + todo[i-1].item);
+            }
 
+            else {
+                todo[c] = new Todos(input, false);
+                System.out.println("Added: " + input);
             }
             input = myObj.nextLine();
             c++;
