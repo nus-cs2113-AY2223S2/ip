89a84ac3481ab6d0dfdffd2a7d758e0e7b5b0dff
DESKTOP-ULU427O\jingy <jingya.lin02@gmail.com>
2023-01-28 12:28:53 +0800

    Add level-1: echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,6 +9,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greet();
+        echo();
     }
 
     public static void greet() {
@@ -14,9 +17,26 @@ public class Duke {
                 + "Hello! I'm Duke\n"
                 + "What can I do for you?\n"
                 + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n"
                 + "\n";
                 System.out.println(greeting);
+    }
+
+    public static String getUserInput() {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        return input;
+    }
+
+    public static void echo() {
+        for (int x = 0; x < 100; x += 1) {
+            String command = getUserInput();
+            System.out.println("____________________________________________________________\n");
+            if (command.compareTo("bye") == 0) {
+                System.out.println("Bye. Hope to see you again soon!\n" + "____________________________________________________________\n");
+                break;
+            } else {
+                System.out.println(command + "\n" + "____________________________________________________________\n");
+            }
+        }
     }
 }
