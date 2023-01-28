ef391d8d36727b363be9f44f486807750d89b101
hyperbola-bear <hung.hin.wang@gmail.com>
2023-01-28 17:08:23 +0800

    lvl-1 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello I'm Duke");
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner in = new Scanner(System.in);
+        String input;
+        while((input = in.next()) != "") {
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            }
+            System.out.println(input);
+        }
+        
     }
 }
