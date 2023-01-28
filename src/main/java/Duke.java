0874c5c3314bc362f14a54577ea0e30d8ba077d1
woowenjun99 <woowenjun99@gmail.com>
2023-01-28 18:20:44 +0800

    Refactor the code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,105 +1,115 @@
+/**
+ * @author Wen Jun
+ * @version 0.0.4
+ */
 import java.util.Scanner;
 
 public class Duke {
 
-    private static int MAX_TASKS = 100;
+    private static final int MAX_TASKS = 100;
 
     private static final Task[] tasks = new Task[MAX_TASKS];
 
     private static int counter = 0;
 
+    private static boolean isRunning = true;
+
     /**
-     * Prints out the status of the task and the task name
+     * Prints out all the tasks in the tasks array
      */
     private static void printTasks() {
         System.out.println(Message.LIST_TASKS.message);
         for (int i = 0; i < counter; ++i) {
-            boolean isDone = tasks[i].isDone();
-            String symbol = isDone ? "X" : " ";
-            System.out.printf(
-                    "%d.[%s] %s\n",
-                    (i + 1),
-                    symbol,
-                    tasks[i].getTaskName()
-            );
+            System.out.printf("%d. %s\n", (i + 1), tasks[i].getDescriptionText());
         }
     }
 
     /**
-     * Mark or unmark a task based on the command and index
+     * Adds a todo task into the array and prints out the 
+     * corresponding message
      *
-     * @param line Command to mark or un-mark
-     * @param num  Index to mark or un-mark
+     * @param line The user input text
      */
-    private static void markOrUnmarkTask(String line, String num) {
-        // Get the integer form of the index
-        int index = Integer.parseInt(num) - 1;
-
-        // Check if the command is to mark
-        boolean isMark = line.equals("mark");
-
-        // Determine the comment to make
-        String remark = isMark
-                ? Message.MARKED.message
-                : Message.UNMARKED.message;
-
-        // Set the task status based on the command
-        tasks[index].setDone(isMark);
-
-        // Check whether status is now marked
-        boolean isDone = tasks[index].isDone();
-
-        // Get the correct symbol
-        String symbol = isDone ? "X" : " ";
-
-        // Print
+    private static void addTodoTask(String line) {
+        int START_INDEX_OF_TODO_TASK = 5;
+        String taskDescription = line.substring(START_INDEX_OF_TODO_TASK);
+        Todo newTodo = new Todo(taskDescription);
+        tasks[counter] = newTodo;
+        counter += 1;
         System.out.printf(
-                "%s\n[%s] %s\n",
-                remark,
-                symbol,
-                tasks[index].getTaskName()
+                Message.TODO_TASK_ADDED.message,
+                newTodo.getDescriptionText(),
+                counter
         );
     }
 
     /**
-     * Adds a new task into the list
-     *
-     * @param line The task to be added into the list
+     * 
+     * @param description A string after removing the command
      */
-    private static void addTask(String line) {
-        Task newTask = new Task(line);
-        tasks[counter] = newTask;
+    private static void addDeadlineTask(String line) {
+        int indexOfEndDate = line.indexOf("/by");
+        int LENGTH_OF_END_DATE_COMMAND = 4;
+
+        String endDate = line.substring(indexOfEndDate + LENGTH_OF_END_DATE_COMMAND);
+        String task = line.substring(line.indexOf("deadline") + 9, indexOfEndDate);
+        Deadline newDeadline = new Deadline(task, endDate);
+        tasks[counter] = newDeadline;
         counter += 1;
+    }
+
+    /**
+     * Toggles the task as mark or un-mark based on the user
+     * request. Combined the two tasks together to abide by
+     * Single Responsibility Principle and Don't Repeat Yourself
+     * 
+     * @param isMark Whether the user wants to mark the task
+     * @param index The index of the task to be marked
+     */
+    private static void toggleMark(boolean isMark, int index) {
+        tasks[index].setDone(isMark);
+        System.out.printf(
+                "%s\n%s\n",
+                isMark ? Message.MARKED.message : Message.UNMARKED.message,
+                tasks[index].getDescriptionText()
+        );
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        boolean isRunning = true;
         System.out.println(Message.WELCOME.message);
 
         while (isRunning) {
-            // MARK: It is important to check for error.
-            if (!in.hasNextLine()) { 
+            if (!in.hasNextLine()) {
                 break;
             }
 
             String line = in.nextLine().trim();
-            String[] instructions = line.split(" ", 2);
-            switch (instructions[0]) {
+            String[] words = line.split(" ", 2);
+            String command = words[0];
+            int index;
+
+            switch (command) {
             case "list":
                 printTasks();
                 break;
+            case "deadline":
+                addDeadlineTask(line);
+                break;
+            case "todo":
+                addTodoTask(line);
+                break;
             case "mark":
             case "unmark":
-                markOrUnmarkTask(instructions[0], instructions[1]);
+                index = Integer.parseInt(words[1]) - 1;
+                boolean isMark = words[0].equals("mark");
+                toggleMark(isMark, index);
                 break;
             case "bye":
                 isRunning = false;
                 break;
             default:
-                addTask(line);
-                System.out.println("added: " + line);
-                break;
+                System.out.println("An invalid command has been provided.");
             }
         }
         System.out.println(Message.GOODBYE.message);
