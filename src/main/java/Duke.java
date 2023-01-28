0175e6781a8b66b2a30e031e73670db6e98cb303
kdh3799 <379914588@qq.com>
2023-01-28 21:24:05 +0800

    Completed WK3 lvl1 Greet, Echo, Exit and re-organized code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,57 @@
-public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String hellomsg = " Hello! I'm Duke\n" +
-                " What can I do for you?\n";
-        String byemsg = " Bye. Hope to see you again soon!\n";
-        String horizontalLine = "____________________________________________________________";
+import java.util.Scanner;
 
-        System.out.println("Hello from\n" + logo);
-        System.out.println(horizontalLine);
-        System.out.println(hellomsg);
-        System.out.println(horizontalLine);
-        System.out.println(byemsg);
+public class Duke {
+    private static final String LOGO = " ____        _        \n" + "|  _ \\ _   _| | _____ \n" + "| | | | | | | |/ / _ \\\n" + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
+    private static final String HELLO_MESSAGE = " Hello! I'm Duke\n" + "     What can I do for you?\n";
+    private static final String BYE_MESSAGE = " Bye. Hope to see you again soon!";
+    private static final String SEPARATOR = "________________________________________________________." +
+            "____";
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String COMMAND_EXIT_WORD = "bye";
+    private static final String LINE_PREFIX = "    ";
+
+    public static void main(String[] args) {
+        // reused from contacts Contacts1.java with modification
+        showWelcomeMessage();
+        while (true) {
+            String userCommand = getUserInput();
+            String feedback = executeCommand(userCommand);
+            showResultToUser(feedback);
+        }
+    }
+
+    private static void showWelcomeMessage() {
+        showToUser("Hello from\n" + LOGO);
+        showToUser(SEPARATOR);
+        showToUser(HELLO_MESSAGE);
+        showToUser(SEPARATOR);
+    }
+
+    private static String getUserInput() {
+        return SCANNER.nextLine();
+    }
+
+    private static String executeCommand(String userInputString) {
+        switch (userInputString) {
+        case COMMAND_EXIT_WORD:
+            exitProgram();
+        default:
+            return userInputString;
+        }
+    }
+
+    private static void showResultToUser(String result) {
+        showToUser(SEPARATOR, result, SEPARATOR);
+    }
+
+    private static void showToUser(String... message) {
+        for (String m : message) {
+            System.out.println(LINE_PREFIX + m);
+        }
+    }
+
+    private static void exitProgram() {
+        showToUser(SEPARATOR, BYE_MESSAGE, SEPARATOR);
+        System.exit(0);
     }
 }
