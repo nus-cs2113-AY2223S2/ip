fa45d5964db3a480308ec934d099e9300b2b0eff
NivethaUdayakumar <nivethaus4@gmail.com>
2023-01-29 00:41:54 +0800

    Refactor code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,22 +3,12 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
 
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Duke");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-
+        printWelcomeMessage();
         String line;
         Scanner in = new Scanner(System.in);
         Task[] tasks = new Task[100];
         int taskCount = 0;
+
         while (true) {
             line = in.nextLine();
             if ("list".equalsIgnoreCase(line)) {
@@ -46,23 +36,15 @@ public class Duke {
                 System.out.println("____________________________________________________________");
             } else if((line.substring(0,4)).equalsIgnoreCase("todo")) {
                 tasks[taskCount] = new Todo(line.substring(5));
-                System.out.println("____________________________________________________________");
-                System.out.println(" Got it. I've added this task:");
-                System.out.println(tasks[taskCount].toString());
                 taskCount++;
-                System.out.println(" Now you have " + taskCount + " tasks in your list.");
-                System.out.println("____________________________________________________________");
+                printTaskAddedDescription(tasks, taskCount);
             } else if((line.substring(0,8)).equalsIgnoreCase("deadline")) {
                 int dividerPosition = line.indexOf("/by");
                 String taskDescription = line.substring(9, dividerPosition);
                 String deadline = line.substring(dividerPosition+4);
                 tasks[taskCount] = new Deadline(taskDescription, deadline);
-                System.out.println("____________________________________________________________");
-                System.out.println(" Got it. I've added this task:");
-                System.out.println(tasks[taskCount].toString());
                 taskCount++;
-                System.out.println(" Now you have " + taskCount + " tasks in your list.");
-                System.out.println("____________________________________________________________");
+                printTaskAddedDescription(tasks, taskCount);
             } else if((line.substring(0,5)).equalsIgnoreCase("event")) {
                 int divider1 = line.indexOf("/from");
                 String event = line.substring(6, divider1);
@@ -70,18 +52,41 @@ public class Duke {
                 String eventStart = line.substring(divider1+6, divider2);
                 String eventEnd = line.substring(divider2+4);
                 tasks[taskCount] = new Event(event, eventStart, eventEnd);
-                System.out.println("____________________________________________________________");
-                System.out.println(" Got it. I've added this task:");
-                System.out.println(tasks[taskCount].toString());
                 taskCount++;
-                System.out.println(" Now you have " + taskCount + " tasks in your list.");
-                System.out.println("____________________________________________________________");
+                printTaskAddedDescription(tasks, taskCount);
             } else if ("bye".equalsIgnoreCase(line)) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                printByeMessage();
                 break;
             }
         }
     }
+
+    private static void printWelcomeMessage() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void printTaskAddedDescription(Task[] tasks, int taskCount) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Got it. I've added this task:");
+        System.out.println(tasks[taskCount-1].toString());
+        System.out.println(" Now you have " + taskCount + " tasks in your list.");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void printByeMessage() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
 }
