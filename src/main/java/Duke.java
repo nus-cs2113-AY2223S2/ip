e3cf3828193adf1e8525544e20097154ea53e709
stevenantya <81511355+stevenantya@users.noreply.github.com>
2023-01-29 01:14:22 +0800

    Add Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+// Java program to read data of various types using Scanner class.
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,10 +11,38 @@ public class Duke {
         String welcomeMessage = "____________________________________________________________\n" +
                 " Hello! I'm Duke, ready to serve you\n" +
                 " What can I do for my master?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Talk to you later!\n" +
-                "____________________________________________________________";
+                "____________________________________________________________\n";
         System.out.println(welcomeMessage);
+
+        // Declare the object and initialize with
+        // predefined standard input object
+        Scanner sc = new Scanner(System.in);
+
+        // String input
+        String input = "";
+
+        while(true) {
+            // Scan the input
+            input = sc.nextLine();
+
+            // Check if it read
+//            if (input.contains("read")) {
+//                System.out.println("ada");
+//            }
+
+            // Create Text Line
+            String textLine = "\n\n____________________________________________________________\n\n";
+            // Prints output
+            if (input.equals("bye")) {
+                String byeText = "Bye. Hope to see you again soon!";
+                System.out.println(textLine + byeText + textLine);
+                break;
+            }
+            System.out.println(textLine + input + textLine);
+        }
+
+
+
 
     }
 }
