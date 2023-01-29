dede8dd85492b057c3e14c60838f13989f5f263f
Nikhil Shashidhar <e0725992@u.nus.edu>
2023-01-29 23:47:30 +0800

    Add list

--- ThomasShelby.java
+++ ThomasShelby.java
@@ -1,15 +1,22 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ThomasShelby {
     public static void main(String[] args) {
         System.out.print("Good day, I'm Thomas Shelby\nTo what do I owe the pleasure?\n");
+        ArrayList<String> inputStrings = new ArrayList<>();
         Scanner in = new Scanner(System.in);
         while (true) {
             String line = in.nextLine();
             if (line.equals("bye")) {
                 break;
+            } else if (line.equals("list")) {
+                for (int i = 0; i < inputStrings.size(); i++) {
+                    System.out.println((i + 1) + ". " + inputStrings.get(i));
+                }
             } else {
-                System.out.println(line);
+                inputStrings.add(line);
+                System.out.println("added: " + line);
             }
         }
         System.out.println("There's no rest for me in this world. Perhaps in the next. Goodbye.");
