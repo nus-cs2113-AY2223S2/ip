ea4fa81e4542d3e855c78cbd1efee0f5395f39a5
yixuann02 <gohyixuan02@gmail.com>
2023-01-29 17:58:31 +0800

    Add Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,33 @@
+public class Task {
+    protected String taskDescription;
+    protected boolean isDone;
+
+    public Task(String taskDescription) {
+        this.taskDescription = taskDescription;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        // mark task as done with X
+        return (isDone ? "X" : " ");
+    }
+
+    // mark and un-mark done
+    // true -> done; false -> not done
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+}
