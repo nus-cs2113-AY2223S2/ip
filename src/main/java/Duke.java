2cfbf76938d7b67e12933ac6fd84bea7a6717ad6
Khulon <e0725788@u.nus.edu>
2023-01-29 15:55:09 +0800

    Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,15 @@
 import java.util.Scanner;
 
 public class Duke {
+
     public static Task[] tasks = new Task[100];
     public static int tasksLength = 0;
 
-
-    public static void addToList(String userInput) {
-        Task newTask = new Task(userInput);
-        tasks[tasksLength] = newTask;
-        tasksLength += 1;
+    public static void printNewTask(int taskNumber) {
         Greeting.printSeperator();
-        System.out.println("\tadded: " + userInput);
-        Greeting.printSeperator();
-    }
-
-    public static void printTasks() {
-        Greeting.printSeperator();
-        for (int i = 0; i < tasksLength; i++) {
-            System.out.println("\t" + (i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getTask());
-        }
+        System.out.println("\tGot it. I've added this task: \n"+ "\t\t" + tasks[tasksLength].printTask());
+        tasksLength ++;
+        System.out.println("\tNow you have " + tasksLength + " tasks in the list.");
         Greeting.printSeperator();
     }
 
@@ -46,22 +37,67 @@ public class Duke {
         while (true) {
             Scanner in = new Scanner(System.in);
             userInput = in.nextLine();
+            String[] inputType = userInput.split(" ", 2);
+            //System.out.println(userInput);
 
-            if (userInput.equals("bye")) {
-                Greeting.printSeperator();
-                break;
-            }
+            switch (inputType[0]) {
+                case "bye": 
+                    Greeting.printSeperator();
+                    return;
 
-            if (userInput.equals("list")) {
-                printTasks();
-            } else if (userInput.contains("unmark")) {
-                userInput = userInput.substring(7);
-                unmark(userInput);
-            } else if (userInput.contains("mark")) {
-                userInput = userInput.substring(5);
-                mark(userInput);
-            } else {
-                addToList(userInput);
+                case "list":
+                    Greeting.printSeperator();
+                    System.out.println("\tHere are the tasks in your list:");
+                    for (int i = 0; i < tasksLength; i++) {
+                        System.out.println("\t" + (i+1) + "." + tasks[i].printTask());
+                    }
+                    Greeting.printSeperator();
+                    break;
+
+                case "unmark":
+                    String taskNumberUnmark = userInput.substring(7);
+                    unmark(taskNumberUnmark);
+                    break;
+
+                case "mark":
+                    String taskNumberMark = userInput.substring(5);
+                    mark(taskNumberMark);
+                    break;
+
+                case "todo":
+                    String todoTask = userInput.substring(4);
+                    tasks[tasksLength] = new Todo (todoTask);
+                    printNewTask(tasksLength);
+                   
+                    break;
+
+                case "deadline":
+                    if ((userInput.indexOf("/by") < 0 )){
+                        Greeting.printHelp();;
+                        break;
+                    }
+                    String deadlineTask = userInput.substring(8, userInput.indexOf("/by"));
+                    String deadlineDay = userInput.substring(userInput.indexOf("/by") + 4);
+                    tasks[tasksLength] = new Deadline(deadlineTask, deadlineDay);
+                    printNewTask(tasksLength);
+
+                    break;
+
+                case "event":
+                    if ((userInput.indexOf("/from") < 0 ) || (userInput.indexOf("/to") < 0 )){
+                        Greeting.printHelp();;
+                        break;
+                    }
+                    String eventTask = userInput.substring(5, userInput.indexOf("/"));
+                    String eventFrom = userInput.substring(userInput.indexOf("/from") + 6, userInput.indexOf("/to"));
+                    String eventTo = userInput.substring(userInput.indexOf("/to") + 4);
+                    tasks[tasksLength] = new Event(eventTask, eventFrom, eventTo);
+                    printNewTask(tasksLength);
+                    
+                    break;
+
+                default:
+                    Greeting.printHelp();
             }
         }
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,5 @@
 public class Task {
+
     protected String description;
     protected boolean isDone;
 
@@ -9,10 +10,19 @@ public class Task {
         this.description = description;
         this.isDone = false;
     }
+
+    public String printTask() {
+        return ("[" + this.getTaskIcon() + "] " + "[" + this.getStatusIcon() + "] " + this.getTask());
+    }
+
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        return (isDone ? "X" : " "); 
+    }
+    public String getTaskIcon() {
+        return " ";
     }
     public void MarkStatusDone() {
+
         this.isDone = true;
     }
     public void MarkStatusUndone() {
