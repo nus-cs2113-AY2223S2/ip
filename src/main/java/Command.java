ac4861b525225fdd3e1d1de40d3803bb20b9dfcc
pinyoko573 <pinyoko573@gmail.com>
2023-01-29 14:11:32 +0800

    Refractor command file
    Removed unnecessary command parameters

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,20 +1,9 @@
 public enum Command {
-    LIST("list"),
-    MARK("mark"),
-    UNMARK("unmark"),
-    REMIND("remind"),
-    DEADLINE("deadline"),
-    EVENT("event"),
-    EXIT("bye");
-
-    private final String name;
-
-    private Command(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
+    LIST,
+    MARK,
+    UNMARK,
+    REMIND,
+    DEADLINE,
+    EVENT,
+    EXIT;
 }
