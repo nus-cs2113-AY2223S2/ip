eb699688f4e82382091c002bb50329ada2b3631f
yixuann02 <gohyixuan02@gmail.com>
2023-01-29 13:10:42 +0800

    Add increment Level 1 Greet Echo Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String horizontalLines = "____________________________________________________________\n";
-        System.out.println(horizontalLines + "Hello! I'm Duke\nWhat can I do for you?\n" +
-                horizontalLines + "Bye. Hope to see you again soon!\n" + horizontalLines);
+        String indentations = "    ";
+        System.out.println(indentations + horizontalLines +
+                indentations + "Hello! I'm Duke\n" +
+                indentations + "What can I do for you?\n" +
+                indentations + horizontalLines);
+        Scanner in = new Scanner(System.in);
+        String readLine;
+        while(true) {
+            readLine = in.nextLine();
+            if(readLine.equals("bye")) {
+                System.out.println(indentations + horizontalLines +
+                        indentations + "Bye. Hope to see you again soon!\n" +
+                        indentations + horizontalLines);
+                break;
+            } else {
+                System.out.println(indentations + horizontalLines +
+                        indentations + readLine + '\n' +
+                        indentations + horizontalLines);
+            }
+        }
 
     }
 }
