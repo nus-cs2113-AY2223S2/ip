2e2419669352c8de9437c7083d2a41442dff7c4a
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-01-29 10:48:31 +0000

    Merge all types of tasks into single Task class file

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -24,3 +24,51 @@ public class Task {
     }
 }
 
+//Create a todo class that inherits from the task class
+class Todo extends Task {
+    public Todo(String taskName){
+        super(taskName);
+    }
+    //toString method to print the status of the task followed by the task name
+    public String toString(){
+        if(this.isDone){
+            return "[T][X] " + this.taskName;
+        }else{
+            return "[T][ ] " + this.taskName;
+        }
+    }
+}
+
+//Create a deadline class that inherits from the task class
+class Deadline extends Task {
+    private String deadline;
+    public Deadline(String taskName, String deadline){
+        super(taskName);
+        this.deadline = deadline;
+    }
+    //toString method to print the status of the task followed by the task name
+    public String toString(){
+        if(this.isDone){
+            return "[D][X] " + this.taskName + " (" + this.deadline + ")";
+        }else{
+            return "[D][ ] " + this.taskName + " (" + this.deadline + ")";
+        }
+    }
+}
+
+//Create an event class that inherits from the task class
+class Event extends Task {
+    private String eventTime;
+    public Event(String taskName, String eventTime){
+        super(taskName);
+        this.eventTime = eventTime;
+    }
+    //toString method to print the status of the task followed by the task name
+    public String toString(){
+        if(this.isDone){
+            return "[E][X] " + this.taskName + " (" + this.eventTime + ")";
+        }else{
+            return "[E][ ] " + this.taskName + " (" + this.eventTime + ")";
+        }
+    }
+}
