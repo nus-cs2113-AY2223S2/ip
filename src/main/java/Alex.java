84bc23496a48a32960af3274dca763aa5b92f553
YongbinWang <yongbin_01@hotmail.com>
2023-01-29 23:47:33 +0800

    Completed Level 3 and updated some of the older code.

--- src/main/java/Alex.java
+++ src/main/java/Alex.java
@@ -2,25 +2,40 @@ import java.util.Scanner;
 
 public class Alex {
     public static void main(String[] args) {
+        String[] instructions = new String[100];
+        int numberOfInstructions = 0;
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         greeting();
         boolean isBye = false;
         while(!isBye){
             String userInput = myObj.nextLine();
-            printLine();
-            if(userInput.toLowerCase().equals("bye")){
+            if(userInput.toLowerCase().equals("list")){
+                for(int i = 1; i <= numberOfInstructions; i++){
+                    System.out.print(i);
+                    System.out.println(". " + instructions[i-1]);
+                }
+                printLine();
+
+            }
+            else if(userInput.toLowerCase().equals("bye")){
                 exit();
                 isBye = true;
                 break;
             }
-            echoResponse(userInput);
+            else{
+                instructions[numberOfInstructions] = userInput;
+                numberOfInstructions += 1;
+                printLine();
+                echoResponse(userInput);
+            }
+
 
         }
 
 
     }
     public static void echoResponse(String userInput){
-        System.out.println(userInput);
+        System.out.println("added: " + userInput);
         printLine();
     }
     public static void printLine(){
@@ -29,14 +44,14 @@ public class Alex {
     }
     public static void greeting(){
         printLine();
-        String greet = "Hello! I'm Alex";
-        String question = "What can I do for you?";
+        String greet = "Greetings! I'm Alex\nWhat can I do for you today?";
+        //String question = "What can I do for you today?";
         System.out.println(greet);
-        System.out.println(question);
+        //System.out.println(question);
         printLine();
     }
     public static void exit(){
-        String exit = "Bye. Hope to see you again soon!";
+        String exit = "Bye. Hope to chat with you again soon!";
         System.out.println(exit);
         printLine();
     }
