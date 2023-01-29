a1509173410c347ac5096896afdaaa22ea2aa2cd
Nikhil Shashidhar <e0725992@u.nus.edu>
2023-01-30 00:00:33 +0800

    Refactor code to A-CodingStandards

--- ThomasShelby.java
+++ ThomasShelby.java
@@ -14,7 +14,9 @@ public class ThomasShelby {
             } else if (cmd.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + taskManager[i].getStatusIcon() + " " + taskManager[i].getDescription());
+                    System.out.println((i + 1) + ". "
+                            + taskManager[i].getStatusIcon() + " "
+                            + taskManager[i].getDescription());
                 }
             } else { // add cmd, mark or unmark
                 String[] cmdSplit = cmd.split(" ", 2);
@@ -22,12 +24,14 @@ public class ThomasShelby {
                     int whichTask = Integer.parseInt(cmdSplit[1]) - 1; // shift back index
                     taskManager[whichTask].setIsDone(true);
                     System.out.println("That was long due, well done.");
-                    System.out.println(taskManager[whichTask].getStatusIcon() + " " + taskManager[whichTask].getDescription());
+                    System.out.println(taskManager[whichTask].getStatusIcon()
+                            + " " + taskManager[whichTask].getDescription());
                 } else if (cmdSplit[0].equals("unmark") && Character.isDigit(cmdSplit[1].charAt(0))) { // set cross
                     int whichTask = Integer.parseInt(cmdSplit[1]) - 1; // shift back index
                     taskManager[whichTask].setIsDone(false);
                     System.out.println("You've gotten lazy.");
-                    System.out.println(taskManager[whichTask].getStatusIcon() + " " + taskManager[whichTask].getDescription());
+                    System.out.println(taskManager[whichTask].getStatusIcon()
+                            + " " + taskManager[whichTask].getDescription());
                 } else if (cmdSplit[0].equals("add")) { // add new task
                     Task newTask = new Task(cmdSplit[1]);
                     newTask.setDescription(cmdSplit[1]);
