b5e596e61ef1fe94543b47a0e76bbe08a82a5d4d
yuanners <beetreeear@gmail.com>
2023-01-29 15:06:36 +0800

    Add Level-4: ToDo, Event, Deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,17 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
 	public static void main (String[] args) {
-		String logo = " ____        _        \n"
-				+ "|  _ \\ _   _| | _____ \n"
-				+ "| | | | | | | |/ / _ \\\n"
-				+ "| |_| | |_| |   <  __/\n"
-				+ "|____/ \\__,_|_|\\_\\___|\n";
-		System.out.println ("Hello from\n" + logo);
-
-		System.out.println ("____________________________________________________________");
-		System.out.println ("Hello! I'm Duke");
-		System.out.println ("What can I do for you?");
-		System.out.println ("____________________________________________________________");
+		printStart ();
 
 		Scanner scan = new Scanner (System.in);
 		String input = scan.nextLine ();
@@ -23,39 +13,92 @@ public class Duke {
 
 		while (!(input.equalsIgnoreCase (exit)) && !(input.isEmpty ())) {
 			if (input.equalsIgnoreCase (list)) {
-				System.out.println ("____________________________________________________________");
-				System.out.println ("Here are the tasks in your list:");
-				for (int i = 0; i < count; i++) {
-					System.out.println ((i + 1) + ". [" + tasks[i].getStatusIcon () + "] " + tasks[i].description);
-				}
-				System.out.println ("____________________________________________________________");
+				listTasks (tasks, count);
 			} else if (input.startsWith ("mark") || input.startsWith ("unmark")) {
-				String[] arrInput = input.split (" ", 2);
-				int index = Integer.parseInt (arrInput[1]);
-				index -= 1;
-				if (input.startsWith ("mark")) {
-					tasks[index].isDone = true;
-				} else {
-					tasks[index].isDone = false;
-				}
-				System.out.println ("____________________________________________________________");
-				System.out.println ("Nice! I've marked this task as done:");
-				System.out.println ("  [" + tasks[index].getStatusIcon () + "] " + tasks[index].description);
-				System.out.println ("____________________________________________________________");
+				changeStatus (input, tasks);
+			} else if (input.startsWith ("todo") || input.startsWith ("deadline") || input.startsWith ("event")) {
+				count = addTask (input, tasks, count);
 			} else {
-				System.out.println ("____________________________________________________________");
-				System.out.println ("added: " + input);
-				tasks[count] = new Task (input);
-				count++;
-				System.out.println ("____________________________________________________________");
+				break;
 			}
 			input = scan.nextLine ();
 		}
+		printEnd ();
 
-		System.out.println ("____________________________________________________________");
+	}
+
+	public static int addTask (String input, Task[] tasks, int count) {
+		String task = "Todo";
+		String deadline = "deadline";
+		String event = "event";
+		String[] arrInput = input.split (" ", 2);
+		printLine ();
+		System.out.println ("Got it. I've added this task:");
+		if (arrInput[0].equalsIgnoreCase (task)) {
+			tasks[count] = new Task (arrInput[1]);
+		} else if (arrInput[0].equalsIgnoreCase (deadline)) {
+			String[] arrTask = arrInput[1].split ("/by");
+			tasks[count] = new Deadline (arrTask[0], arrTask[1]);
+		} else {
+			String[] arrTask = arrInput[1].split ("/from");
+			String[] eventTask = arrTask[1].split ("/to");
+			tasks[count] = new Event (arrTask[0], eventTask[0], eventTask[1]);
+		}
+		System.out.println (tasks[count].getType () + tasks[count].toString ());
+		count++;
+		System.out.println ("Now you have " + count + " tasks in the list.");
+		printLine ();
+		return count;
+	}
+
+	public static void changeStatus (String input, Task[] tasks) {
+		String[] arrInput = input.split (" ", 2);
+		int index = Integer.parseInt (arrInput[1]);
+		index -= 1;
+		if (input.startsWith ("mark")) {
+			tasks[index].isDone = true;
+		} else {
+			tasks[index].isDone = false;
+		}
+		printLine ();
+		System.out.println ("Nice! I've marked this task as done:");
+		System.out.println (tasks[index].getType () + tasks[index].toString ());
+		printLine ();
+	}
+
+	public static void listTasks (Task[] tasks, int count) {
+		printLine ();
+		System.out.println ("Here are the tasks in your list:");
+		for (int i = 0; i < count; i++) {
+			System.out.println ((i + 1) + ". " + tasks[i].getType () + tasks[i].toString ());
+		}
+		printLine ();
+	}
+
+	public static void printStart () {
+		String logo = " ____        _        \n"
+				+ "|  _ \\ _   _| | _____ \n"
+				+ "| | | | | | | |/ / _ \\\n"
+				+ "| |_| | |_| |   <  __/\n"
+				+ "|____/ \\__,_|_|\\_\\___|\n";
+		System.out.println ("Hello from\n" + logo);
+
+		printLine ();
+		System.out.println ("Hello! I'm Duke");
+		System.out.println ("What can I do for you?");
+		printLine ();
+	}
+
+	public static void printEnd () {
+		printLine ();
 		System.out.println ("Bye. Hope to see you again soon!");
+		printLine ();
+	}
+
+	public static void printLine () {
 		System.out.println ("____________________________________________________________");
 	}
+
 }
 
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,26 @@
 public class Task {
 	protected String description;
+	protected String type;
 	protected boolean isDone;
 
 	public Task (String description) {
 		this.description = description;
 		this.isDone = false;
+		this.type = "[T]";
 	}
 
+	public String getType () {
+		return type;
+	}
+
+	@Override
+	public String toString () {
+		return "[" + getStatusIcon () + "] " + description;
+	}
+
+
 	public String getStatusIcon () {
-		return (isDone ? "X" : " "); // mark done task with X
+		return (isDone ? "X" : " ");
 	}
 
 }
