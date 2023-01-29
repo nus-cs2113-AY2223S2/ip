ef4b3fb580a9045d29f7dc100f702580fef68a56
NgYJ02 <91600220+NgYJ02@users.noreply.github.com>
2023-01-29 15:15:27 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,23 +11,38 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
-        ArrayList<String> inputs = new ArrayList<String>();
+        ArrayList<Task> tasks = new ArrayList<Task>();
         Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
-        while (!input.equals("bye")) {
-            if (input.equals("list")) {
+        String desc = scan.nextLine();
+        while (!desc.equals("bye")) {
+            if (desc.equals("list")) {
                 int a = 1;
-                for (String i : inputs) {
-                    System.out.println(a + ". " + i);
+                System.out.println("Here are the tasks in the list:");
+                for (Task i : tasks) {
+                    System.out.println(a + ". [" + i.getStatusIcon() + "] " + i.getDescription());
                     a++;
                 }
+            } else if (desc.matches("mark [0-9]{1,2}")) {
+                String[] marks = desc.split(" ");
+                tasks.get(Integer.parseInt(marks[1]) - 1).markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + tasks.get(Integer.parseInt(marks[1]) - 1).getStatusIcon() + "] " +
+                        tasks.get(Integer.parseInt(marks[1]) - 1).getDescription());
+            } else if (desc.matches("unmark [0-9]{1,2}")) {
+                String[] marks = desc.split(" ");
+                tasks.get(Integer.parseInt(marks[1]) - 1).markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[" + tasks.get(Integer.parseInt(marks[1]) - 1).getStatusIcon() + "] " +
+                        tasks.get(Integer.parseInt(marks[1]) - 1).getDescription());
             } else {
-                System.out.println("added: " + input);
-                inputs.add(input);
+                Task task = new Task(desc);
+                System.out.println("added: " + desc);
+                tasks.add(task);
             }
-            input = scan.nextLine();
+            desc = scan.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
 
     }
+
 }
