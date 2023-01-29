8c316c897258811c5c31662f5037e673feb19c0f
chiayuxuan <chiayuxuan1909@gmail.com>
2023-01-30 00:41:49 +0800

    Add Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,13 @@
 
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Duke {
-//    public static void addToList(String input) {
-//        System.out.print("\t____________________________________________________________\n\t "
-//                + "added: " + input + "\n\t____________________________________________________________\n");
-//    }
+
 
     public static void main(String[] args) {
         String divider = "\t____________________________________________________________";
         Scanner in = new Scanner(System.in);
-        ArrayList<String> toDoList = new ArrayList<>();
+       // ArrayList<Tasks> taskList = new ArrayList<>();
         String line;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -22,27 +17,45 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.print(divider + '\n'
                 + " \t Hello! I'm Duke\n\t What can I do for you?\n" + divider + "\n\n");
-        line = in.nextLine(); //obtain next input
+        line = in.nextLine(); //first input
         while (!line.equals("bye")) {
-            if (line.equals("list")) { //list out to-do list
-                Iterator<String> it = toDoList.iterator();
-                int num = 1;
+            String[] userInput = line.split(" ", 2);
+            String command = userInput[0];
+            switch (command) {
+            case "mark":
+                Tasks markTask =  Tasks.getTaskList().get(Integer.parseInt(userInput[1]) - 1);
+                markTask.setMarked(true);
+                System.out.println(divider + "\n\t Nice! I've marked this task as done:\n\t  " +
+                        "[X] " +  markTask.getItem());
                 System.out.println(divider);
-                while (it.hasNext()) {
-                    System.out.println("\t "+ num + "." + it.next());
+                break;
+            case "unmark":
+                Tasks unMarkTask = Tasks.getTaskList().get(Integer.parseInt(userInput[1]) - 1);
+                unMarkTask.setMarked(false);
+                System.out.println(divider + "\n\t Nice! I've marked this task as not done yet:\n\t  " +
+                        "[ ] " +  unMarkTask.getItem());
+                System.out.println(divider);
+                break;
+            case "list":
+                int timer = Tasks.getNumberOfTasks();
+                int num = 1;
+                System.out.println(divider + "\n\t Here are the tasks in your list:");
+                while (num <= timer) {
+                    Tasks thisTask = Tasks.getTaskList().get(num - 1);
+                    if (thisTask.isMarked()) {
+                        System.out.println("\t " + Integer.toString(num) + ".[X] " + thisTask.getItem());
+                    } else {
+                        System.out.println("\t " + Integer.toString(num) + ".[ ] " + thisTask.getItem());
+                    }
                     ++num;
                 }
                 System.out.println(divider);
-            } else {
-                toDoList.add(line);
-                System.out.println(divider + "\n\t " + line +'\n' + divider);
-                //addToList(line);
+                break;
+            default:
+                Tasks.addToList(new Tasks(line, false));
             }
-            line = in.nextLine();
+            line = in.nextLine(); //new user input
         }
-
         System.out.println(divider + "\n\t Bye. Hope to see you again soon!\n" + divider);
-
-
     }
 }
