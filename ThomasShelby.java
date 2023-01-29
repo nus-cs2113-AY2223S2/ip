8f0daa6b4d1fce27b659a44ee04a8422bea275f4
Nikhil Shashidhar <e0725992@u.nus.edu>
2023-01-29 23:37:29 +0800

    Add greeting and farewell

---
+++ ThomasShelby.java
@@ -0,0 +1,6 @@
+public class ThomasShelby {
+    public static void main(String[] args) {
+        System.out.print("Good day, I'm Thomas Shelby\nTo what do I owe the pleasure?\n");
+        System.out.println("There's no rest for me in this world. Perhaps in the next. Goodbye.");
+    }
+}
