8b4a9dd97665df5c07c600754f7d70210384fe14
stevenantya <81511355+stevenantya@users.noreply.github.com>
2023-01-29 18:20:43 +0800

    Add List and Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -21,24 +21,94 @@ public class Duke {
         // String input
         String input = "";
 
+        // String data
+        Task[] tasks = new Task[100];
+
+        int counter = 1;
+
         while(true) {
             // Scan the input
             input = sc.nextLine();
 
-            // Check if it read
-//            if (input.contains("read")) {
-//                System.out.println("ada");
-//            }
-
             // Create Text Line
-            String textLine = "\n\n____________________________________________________________\n\n";
+            String textLine = "\n___________________________******___________________________\n";
+
+            // Print begin text line
+            System.out.println(textLine);
+
             // Prints output
             if (input.equals("bye")) {
                 String byeText = "Bye. Hope to see you again soon!";
-                System.out.println(textLine + byeText + textLine);
+                System.out.println(byeText);
                 break;
             }
-            System.out.println(textLine + input + textLine);
+            else if (input.equals("list")){
+
+                for (int index = 1; index < counter; index += 1){
+                    System.out.println((index) + "." + "[" + tasks[index].getStatusIcon() + "]" + " " + tasks[index].getDescription());
+                }
+            }
+            else if (input.substring(0, 4).equals("mark")) {
+                if (input.length() <= 5) {
+                    System.out.println("Please enter a valid number after space, e.g. mark 5");
+                    continue;
+                }
+                String indexString = input.substring(5);
+                indexString = indexString.replaceAll("\\s", "");
+                int index = Integer.parseInt(indexString);
+                if (index < counter && index != 0) {
+                    if (tasks[index].isDone())  {
+                        System.out.println("  It is already done silly! Do something else...");
+                        continue;
+                    }
+                    else {
+                        tasks[index].markAsDone();
+                        System.out.println("  Nice! I've marked this task as done:\n"+"    [" +
+                                tasks[index].getStatusIcon() + "]" + " " + tasks[index].getDescription());
+                    }
+
+                }
+                else {
+                    System.out.println("Please enter a valid number");
+                    continue;
+                }
+
+            }
+            else if (input.substring(0, 6).equals("unmark")) {
+                if (input.length() <= 5) {
+                    System.out.println("Please enter a valid number after space, e.g. mark 5");
+                    continue;
+                }
+                String indexString = input.substring(7);
+                indexString = indexString.replaceAll("\\s", "");
+                int index = Integer.parseInt(indexString);
+                if (index < counter && index != 0) {
+                    if (!tasks[index].isDone())  {
+                        System.out.println("  It is not done silly! Do it quickly!");
+                        continue;
+                    }
+                    else {
+                        tasks[index].markAsNotDone();
+                        System.out.println("  Okay... whatever you say sir.... unmarked!\n"+"    [" +
+                                tasks[index].getStatusIcon() + "]" + " " + tasks[index].getDescription());
+                    }
+
+                }
+                else {
+                    System.out.println("Please enter a valid number");
+                    continue;
+                }
+            }
+            else {
+                Task t = new Task(input);
+                tasks[counter] = t;
+                counter += 1;
+                System.out.println(input);
+            }
+
+            // Print end text line
+            System.out.println(textLine);
+
         }
 
 
