0bb4fe73b2cbede8f169a7b6deb21d9f904ca970
Nikhil Shashidhar <e0725992@u.nus.edu>
2023-01-29 23:42:03 +0800

    Add echo

--- ThomasShelby.java
+++ ThomasShelby.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class ThomasShelby {
     public static void main(String[] args) {
         System.out.print("Good day, I'm Thomas Shelby\nTo what do I owe the pleasure?\n");
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String line = in.nextLine();
+            if (line.equals("bye")) {
+                break;
+            } else {
+                System.out.println(line);
+            }
+        }
         System.out.println("There's no rest for me in this world. Perhaps in the next. Goodbye.");
     }
 }
