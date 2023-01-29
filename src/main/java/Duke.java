a663fab1a4944a5e8d1d2aee891e8163a66e3867
kdh3799 <379914588@qq.com>
2023-01-29 17:26:49 +0800

    Wk3 Lvl2 add list Fn added, task class added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,19 +4,21 @@ public class Duke {
     private static final String LOGO = " ____        _        \n" + "|  _ \\ _   _| | _____ \n" + "| | | | | | | |/ / _ \\\n" + "| |_| | |_| |   <  __/\n" + "|____/ \\__,_|_|\\_\\___|\n";
     private static final String HELLO_MESSAGE = " Hello! I'm Duke\n" + "     What can I do for you?\n";
     private static final String BYE_MESSAGE = " Bye. Hope to see you again soon!";
-    private static final String SEPARATOR = "________________________________________________________." +
-            "____";
+    private static final String SEPARATOR = "____________________________________________________________";
     private static final Scanner SCANNER = new Scanner(System.in);
+    private static final String COMMAND_LIST_WORD = "list";
     private static final String COMMAND_EXIT_WORD = "bye";
     private static final String LINE_PREFIX = "    ";
+
+    private static final Task[] taskList = new Task[100];
+    private static int taskCount = 0;
 
     public static void main(String[] args) {
         // reused from contacts Contacts1.java with modification
         showWelcomeMessage();
         while (true) {
             String userCommand = getUserInput();
-            String feedback = executeCommand(userCommand);
-            showResultToUser(feedback);
+            executeCommand(userCommand);
         }
     }
 
@@ -31,17 +33,18 @@ public class Duke {
         return SCANNER.nextLine();
     }
 
-    private static String executeCommand(String userInputString) {
+    private static void executeCommand(String userInputString) {
+        showToUser(SEPARATOR);
         switch (userInputString) {
+        case COMMAND_LIST_WORD:
+            listTask();
+            break;
         case COMMAND_EXIT_WORD:
             exitProgram();
         default:
-            return userInputString;
+            addTask(userInputString);
         }
-    }
-
-    private static void showResultToUser(String result) {
-        showToUser(SEPARATOR, result, SEPARATOR);
+        showToUser(SEPARATOR);
     }
 
     private static void showToUser(String... message) {
@@ -50,8 +53,20 @@ public class Duke {
         }
     }
 
+    private static void addTask(String newTaskDescription) {
+        taskList[taskCount] = new Task(newTaskDescription);
+        taskCount += 1;
+        showToUser("added: " + newTaskDescription);
+    }
+
+    private static void listTask() {
+        for (int i = 0; i < taskCount; i++) {
+            showToUser((i+1) + ". " + taskList[i].description);
+        }
+    }
+
     private static void exitProgram() {
-        showToUser(SEPARATOR, BYE_MESSAGE, SEPARATOR);
+        showToUser(BYE_MESSAGE, SEPARATOR);
         System.exit(0);
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,17 @@
+
+//initial skeleton adapted from https://nus-cs2113-ay2223s2.github.io/website/schedule/week3/project.html partial solution
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    //...
+}
