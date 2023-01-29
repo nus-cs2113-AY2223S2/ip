49f8cbfc2ab2ea2879d3e2a7396d46bb33389d11
nicholas132000 <beefnlemon@gmail.com>
2023-01-29 20:29:01 +0800

    Add: Test expected vs actual output

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -6,7 +6,7 @@ public class Event extends Task{
         this.start = start;
         this.end = end;
     }
-    
+
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + start + "to: " + end + ")";
