d6c7d3313878daeb8af7107519328444f2bbb840
HiIAmTzeKean <ngtzekean600@gmail.com>
2023-01-29 12:40:40 +0800

    doc: Java documentation for interface

--- src/main/java/parser/IParser.java
+++ src/main/java/parser/IParser.java
@@ -6,6 +6,16 @@ public interface IParser {
      * @return last message String by user
      */
     public String getMessage();
+        /**
+     * Parses the user input from console and obtains the arguments.
+     * @param command type of command to obtain arguments for
+     * @return Argument class containing arguments from user
+     */
     public Command getCommand() throws EmptyCommandException;
+    /**
+     * Parses the user input from console and obtains the arguments.
+     * @param command type of command to obtain arguments for
+     * @return Argument class containing arguments from user
+     */
     public Argument getCommandArguments(Command command);
 }
--- src/main/java/task/ITaskController.java
+++ src/main/java/task/ITaskController.java
@@ -5,7 +5,22 @@ import java.util.ArrayList;
 import parser.Argument;
 
 public interface ITaskController {
-	public ArrayList<Task> getTask() throws EmptyTaskList;
+	/**
+	 * Method call for obtaining ArrayList of Task user has.
+	 * @return ArrayList<Task>
+	 * @throws EmptyTaskListException
+	 */
+	public ArrayList<Task> getTask() throws EmptyTaskListException;
+	/**
+	 * Adds new task to controller. Task will be stored in ArrayList.
+	 * @param description
+	 */
 	public void addTask(String description);
+    /**
+	 * 
+	 * @param arg
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
 	public String markTask(Argument arg) throws IllegalArgumentException;
 }
