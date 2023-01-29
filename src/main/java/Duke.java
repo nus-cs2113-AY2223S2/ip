6ac55b15f1eaa2c5edcc7e8be8d1ef4ff4a9cedb
coregano <liberwifi@gmail.com>
2023-01-29 15:48:47 +0800

    Added a few string constants and improved readability

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,63 +2,56 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Duke {
-
     public static final String LINE_PARTITION = "    ____________________________________________________________\n";
     public static final String FAREWELL = LINE_PARTITION +
-            "     Bye. Hope to see you again soon!\n" +
+            "\t  Bye. Hope to see you again soon!\n" +
             LINE_PARTITION;
     public static final String GREET = LINE_PARTITION +
-            "     Hello! I'm kimo\n" +
-            "     What can I do for you?\n" +
+            "\t  Hello! I'm kimo\n" +
+            "\t  What can I do for you?\n" +
             LINE_PARTITION;
 
-    public static void printAdd(String prompt) {
+    public static void printAdd(String task) {
         System.out.print(LINE_PARTITION +
-                "     added: " + prompt + "\n" +
+                "\t  added: " + task + "\n" +
                 LINE_PARTITION);
     }
     public static void printTaskNotFound(int taskNum) {
         System.out.print(LINE_PARTITION +
-                "Task number " + taskNum + " does not exist, please try again." +
+                "\t  Task number " + taskNum + " does not exist, please try again.\n" +
                 LINE_PARTITION);
     }
-    public static void printMark(Task[] prompt, int number) {
-        prompt[number].setDone(true);
+    public static void printMark(Task[] taskList, int number) {
+        taskList[number].setDone(true);
         System.out.print(LINE_PARTITION +
-                "     Great job! I will mark this task as done: \n" +
-                "     [X] " + prompt[number].getName() + "\n" +
+                "\t  Great job! I will mark this task as done: \n" +
+                "\t  [X] " + taskList[number].getName() + "\n" +
                 LINE_PARTITION);
     }
-    public static void printUnmark(Task[] prompt, int number) {
-        prompt[number].setDone(false);
+    public static void printUnmark(Task[] taskList, int taskNum) {
+        taskList[taskNum].setDone(false);
         System.out.print(LINE_PARTITION +
-                "     Alright, I have marked this task as undone: \n" +
-                "     [ ] " + prompt[number].getName() + "\n" +
+                "\t  Alright, I have marked this task as undone: \n" +
+                "\t  [ ] " + taskList[taskNum].getName() + "\n" +
                 LINE_PARTITION);
     }
 
     public static void printList(Task[] taskList) {
-        System.out.print(LINE_PARTITION +
-                "     These are the tasks in your list: ");
+        System.out.println(LINE_PARTITION +
+                "\t  These are the tasks in your list: ");
         int counter = 1;
         for (Task task : taskList) {
-            System.out.println("     " + counter + ".[" + task.getStatus() + "] " + task.getName());
+            System.out.println("\t  " + counter + ".[" + task.getStatus() + "] " + task.getName());
             counter++;
         }
         System.out.print(LINE_PARTITION);
-
     }
 
-    public static void main(String[] args) {
-
-        System.out.print(GREET);
-
+    public static void takeUserInput() {
         Task[] taskList = new Task[100];
         String userInput;
         int listLength = 0;
         Scanner in = new Scanner(System.in);
-
-        inputLoop:
         while (true) {
             userInput = in.nextLine();
             switch (userInput) {
@@ -67,28 +60,32 @@ public class Duke {
                 break;
 
             case "bye":
-                break inputLoop;
+                return;
 
             default:
-                if (userInput.matches("mark \\d+") ) { // check whether format of input is "mark (integer)"
+                if (userInput.matches("mark \\d+") ) { // check if format of input is "mark (integer)"
                     String[] inputArray = userInput.split(" ");
                     int taskNum = Integer.parseInt(inputArray[1]);
-                    if (taskNum - 1 < (listLength)) {  // check whether integer provided exceeds length of taskList
+                    if (taskNum - 1 < (listLength) && taskNum > 0) {
+                        // check if integer provided exceeds length of taskList & ensure taskNum is positive
                         printMark(taskList, taskNum - 1);
                     } else {
                         printTaskNotFound(taskNum);
                     }
                 }
-                else if (userInput.matches("unmark \\d+")) { // check whether format of input is "unmark (integer)"
+
+                else if (userInput.matches("unmark \\d+")) { // check if format of input is "unmark (integer)"
                     String[] inputArray = userInput.split(" ");
                     int taskNum = Integer.parseInt(inputArray[1]);
-                    if (taskNum - 1 < (listLength)) { // check whether integer provided exceeds length of taskList
+                    if (taskNum - 1 < (listLength) && taskNum > 0) {
+                        // check if integer provided exceeds length of taskList & ensure taskNum is positive
                         printUnmark(taskList, taskNum - 1);
                     } else {
                         printTaskNotFound(taskNum);
                     }
                 }
-                else {
+
+                else { // default add to list case
                     Task task = new Task(userInput);
                     taskList[listLength] = task;
                     listLength++;
@@ -96,16 +93,20 @@ public class Duke {
                 }
             }
         }
-
+    }
+    public static void main(String[] args) {
+        System.out.print(GREET);
+        takeUserInput();
         System.out.println(FAREWELL);
     }
 }
 
 /*
-String logo = " ____        _        \n"
-        + "|  _ \\ _   _| | _____ \n"
-        + "| | | | | | | |/ / _ \\\n"
-        + "| |_| | |_| |   <  __/\n"
-        + "|____/ \\__,_|_|\\_\\___|\n";
+String logo =
+        " ____        _        \n" +
+        "|  _ \\ _   _| | _____ \n" +
+        "| | | | | | | |/ / _ \\\n" +
+        "| |_| | |_| |   <  __/\n" +
+        "|____/ \\__,_|_|\\_\\___|\n";
 System.out.println("Hello from\n" + logo);
 */
