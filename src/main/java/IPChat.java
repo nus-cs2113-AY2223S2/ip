b7a628c918c7a346ba6284766033cf071da4c27e
LAPTOP-DQSB43T9\deepa <deepanjali.dhawan@gmail.com>
2023-01-30 21:24:16 +0800

    Add IPChat.java

---
+++ src/main/java/IPChat.java
@@ -0,0 +1,8 @@
+public class IPChat {
+
+    // Level 0: Greet:
+    System.out.println("Hello! I'm IPChat...");
+    System.out.println("How may I help you ??");
+    System.out.println();
+    System.out.println("Bye. Hope to see you again soon!");
+}
