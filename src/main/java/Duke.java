7c1833cf9dad0e1bdb771f41e02ddff48da8b48c
Muhammad Rafi Adzikra Sujai <E1100936@u.nus.edu>
2023-01-30 18:24:59 +0800

    Level 3 commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,23 +7,39 @@ public class Duke {
         "Hello! I'm Duke\n"+
         "What can I do for you?\n"
    );
-   ArrayList<String> items = new ArrayList<String>();
+   ArrayList<Task> items = new ArrayList<Task>();
    String input = "";
    while (!input.equals("bye"))
    {
-    input = sc.next();
-    System.out.println("____________________________________________________________\n" + "  " + "added: "+ input + "\n" + "____________________________________________________________\n");
-    if (!input.equals("list")) {
-        items.add(input);
+       input = sc.next();
+    
+    int order;
+    if (input.equals("mark")){
+        order = sc.nextInt();
+        items.get(order-1).setMark(true);
+        System.out.println("____________________________________________________________\n" + "  " + "Nice! I've marked this task as done:\n" + "[X] " +items.get(order-1).getDescription() + "\n" + "____________________________________________________________\n");
+
     }
-    else {
+    else if (input.equals("unmark")){
+        order = sc.nextInt();
+        items.get(order-1).setMark(false);
+        System.out.println("____________________________________________________________\n" + "  " + " OK, I've marked this task as not done yet:\n" + "[X] " +items.get(order-1).getDescription() + "\n" + "____________________________________________________________\n");
+    }
+    else if (input.equals("list")) {
         System.out.println("____________________________________________________________\n");
         for(int i = 1; i <= items.size(); i++){
-            System.out.println(" " + i + ". " + items.get(i-1));
+            System.out.println(" " + i + ". " + items.get(i-1).getDescription() + "[" + items.get(i-1).getStatusIcon() + "]");
         }
         System.out.println("\n");
         System.out.println("____________________________________________________________\n");
     }
+
+    else {
+        Task task = new Task(input);
+        items.add(task);
+        System.out.println("____________________________________________________________\n" + "  " + "added: "+ input + "\n" + "____________________________________________________________\n");
+    }
+        
    
     }
     
---
+++ src/main/java/Task.java
@@ -0,0 +1,30 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+    private static int total;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+        total++;
+
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setMark(boolean done){
+        this.isDone =  done;
+    }
+    
+
+
+
+    //...
+}
+
