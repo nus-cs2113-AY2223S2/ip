4fe2c3ab520e21e205931eb8528352ce32e3f693
briantjs00 <briantjs00@gmail.com>
2023-01-30 18:29:01 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 public class Duke {
     private static String line = "____________________________________________________________";
     public static void greet() {
         System.out.println(line);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
         System.out.println(line);
+    }
+    public static void bye() {
+        System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -14,5 +20,19 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greet();
+        String list;
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            list = in.nextLine();
+            if (list.equalsIgnoreCase("bye")) {
+                bye();
+                break;
+            }
+            else {
+                System.out.println(line);
+                System.out.println(list);
+                System.out.println(line);
+            }
+        }
     }
 }
