7fddbdc76dc486ad7f20bc062ca71ec0057f60b0
shawntangy <shawntang83@gmail.com>
2023-01-30 17:04:41 +0800

    Add Todo, Deadline, Event classes and changed numberOfTasks to be a static variable.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,10 +9,10 @@ public class Duke {
         String line = "";
 
         Task[] tasks = new Task[100];
-        int tasksCount = 0;
+        //int tasksCount = 0;
 
         while (!(line = input.nextLine()).equals("bye")) {
-           tasksCount = Duke.processInput(line, tasks,tasksCount);
+           Duke.processInput(line, tasks);
         }
 
         Duke.ending();
@@ -45,30 +45,62 @@ public class Duke {
                 "____________________________________________________________";
         System.out.println(ending);
     }
+    public static void printAddTaskMessage(Task t){
+        System.out.println("Got it. I've added this task:\n  " + t + "\nNow you have " + t.getNumberOfTasks() +" tasks in the list.");
+    }
+    public static void processInput(String line, Task[] tasks){
 
-    public static int processInput(String line, Task[] tasks, int tasksCount){
-
-        String[] words = line.split(" ");
+        String[] words = line.split(" ",2);
         String command = words[0];
+        int tasksCount = 0;
+        if(tasks[0]!=null){
+            tasksCount = tasks[0].getNumberOfTasks();
+        }
+        // words[0] is the command, words[n] is the next few words
 
         switch (command) {
+        case "todo":
+            line = words[1]; // to remove the command
+            Task td = new Todo(words[1]);
+            tasks[tasksCount] = td;
+            printAddTaskMessage(td);
+            break;
+        case "deadline":
+            line = words[1]; // to remove the command
+            String[] deadlineDetails = line.split(("/by "));
+            Task d = new Deadline(deadlineDetails[0],deadlineDetails[1]);
+            tasks[tasksCount++] = d;
+            printAddTaskMessage(d);
+            break;
+        case "event":
+            line = words[1]; // to remove the command
+            String[] eventDetails = line.split(("/from "));
+            String eventName = eventDetails[0];
+            String from = line.substring(line.indexOf("/from ") + 6, line.indexOf(" /to"));
+            eventDetails = line.split(("/to "));
+            String to = eventDetails[1];
+            Task e = new Event(eventName,from,to);
+            tasks[tasksCount] = e;
+            printAddTaskMessage(e);
+            break;
+
         case "mark":
             int markIndex = Integer.parseInt(words[1]) - 1;
             System.out.println("Nice! I've marked this task as done: ");
             tasks[markIndex].markAsDone();
-            System.out.println("[" + tasks[markIndex].getStatusIcon() + "] " + tasks[markIndex].getDescription());
+            System.out.println(tasks[markIndex]);
             break;
         case "unmark":
             int unmarkIndex = Integer.parseInt(words[1]) - 1;
             System.out.println("Okay, I've marked this task as not done yet: ");
             tasks[unmarkIndex].markAsNotDone();
-            System.out.println("[" + tasks[unmarkIndex].getStatusIcon() + "] " + tasks[unmarkIndex].getDescription());
+            System.out.println(tasks[unmarkIndex]);
             break;
         case "list":
             int count = 0;
             for (Task t : tasks){
                 if(t != null){
-                    System.out.println(++count +". " + "[" + t.getStatusIcon() + "] " + t.getDescription());
+                    System.out.println(++count +". " + t);
                 }
             }
             break;
@@ -76,9 +108,9 @@ public class Duke {
         default:
             System.out.println("added: "+line+"\n");
             Task t = new Task(line);
-            tasks[tasksCount++] = t;
+            tasks[tasksCount] = t;
         }
-        return tasksCount;
+
 
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,19 +1,28 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    private static int numberOfTasks = 0;
+
+    public int getNumberOfTasks() {
+        return numberOfTasks;
+    }
+
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        numberOfTasks++;
     }
+
+
 
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
-    public String getDescription() {
-        return this.description;
-    }
+//    public String getDescription() {
+//        return this.description;
+//    }
 
     public void markAsDone() {
         this.isDone = true;
@@ -21,5 +30,10 @@ public class Task {
 
     public void markAsNotDone() {
         this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + description;
     }
 }
