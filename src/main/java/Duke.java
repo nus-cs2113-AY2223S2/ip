834bf4d30853fb225c71bad59091b93310fa0d90
briantjs00 <briantjs00@gmail.com>
2023-01-30 17:54:39 +0800

    Level 0 - Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,11 @@
 public class Duke {
+    private static String line = "____________________________________________________________";
+    public static void greet() {
+        System.out.println(line);
+        System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +13,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
     }
 }
