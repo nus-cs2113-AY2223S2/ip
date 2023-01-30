eca39c2abc5a72fd98a39acd37f9d3ff4fe06c4d
GraceZhuXY <grace.zhu.xing.yu@gmail.com>
2023-01-30 20:59:53 +0800

    Implement Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        String[] items = new String[100];
+        Task[] items = new Task[100];
         int counter = 0;
         System.out.println("Hi, I'm bob");
         System.out.println("What's up");
@@ -10,15 +10,24 @@ public class Duke {
         while (!word.equals("bye")) {
             if (word.equals("list") && counter > 0) {
                 for (int i = 1; i <= counter; i++) {
-                    System.out.println(i + ". " + items[i-1]);
+                    System.out.println(i + ".[" + items[i-1].getStatusIcon() + "] " + items[i-1].description);
                 }
-                word = reader.nextLine();
+            } else if (word.contains("mark")) {
+                int taskNo;
+                taskNo = Character.getNumericValue(word.charAt(word.length()-1));
+                if (word.contains("un")) {
+                    items[taskNo-1].unmarkAsDone();
+                    System.out.println("This task is not done: " + "[" + items[taskNo-1].getStatusIcon() + "] " + items[taskNo-1].description);
+                } else {
+                    items[taskNo-1].markAsDone();
+                    System.out.println("This task is done: " + "[" + items[taskNo-1].getStatusIcon() + "] " + items[taskNo-1].description);
+                }
             } else {
                 System.out.println("added: " + word);
-                items[counter] = word;
+                items[counter] = new Task(word);
                 counter += 1;
-                word = reader.nextLine();
             }
+            word = reader.nextLine();
         }
         System.out.println("Bye");
     }
