acd38d987accf9d8b6784d43a796411bd4c15bb7
Thomas Jiralerspong <thomasjiralerspong@gmail.com>
2023-01-30 20:02:45 +0800

    Fix bug with adding todo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -73,6 +73,9 @@ public class Duke {
                     inputSections[2].replaceFirst("to", "")
             );
             break;
+        case "todo":
+            taskToAdd = new ToDo(taskDescription);
+            break;
         default:
             printInvalidInputMessage();
             return null;
