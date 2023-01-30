f35ad2c230bb0be06d99ac072bb61610e97a7365
Muhammad Rafi Adzikra Sujai <E1100936@u.nus.edu>
2023-01-30 16:43:06 +0800

    Level 1 commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\r\n"
-            + " Hello! I'm Duke\r\n"
-            + " What can I do for you?\r\n"
-            + "____________________________________________________________\r\n"
-            + " Bye. Hope to see you again soon!\r\n"
-            + "____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("____________________________________________________________\n" + 
+        "Hello! I'm Duke\n"+
+        "What can I do for you?\n"
+   );
+   String input = sc.next();
+   while (!input.equals("bye"))
+   {
+    System.out.println("____________________________________________________________\n" + "  " + input + "\n" + "____________________________________________________________\n");
+    input = sc.next();;
+
+        
     }
+    System.out.println("____________________________________________________________\n" + "  " + "Bye. Hope to see you again soon!" + "\n" + "____________________________________________________________\n");
+}
 }
