46634c678bdf796c11b55cbf3eecd3a986124421
Richardtok <e0902030@u.nus.edu>
2023-01-30 11:08:11 +0800

    Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,6 @@
 public class Duke {
     public static void main(String[] args) {
         String logo =  "Hello! I'm Duke\n" + "What can I do for you?\n" + "Bye. Hope to see you again soon!";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
     }
 }
