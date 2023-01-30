8305c4bdf7325ece0d66d2dbd2ed1b44b3b4e38f
Chick3nBoy <leonardo.ongdc@gmail.com>
2023-01-30 16:50:05 +0800

    Finished level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,29 @@
+import java.util.Scanner;
 public class Duke {
+
+
+    private static final String LINE = "____________________________________________________________\n";
+    private static final String BLANK = "    ";
+
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
-                + " Hello! I'm Duke\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = BLANK + LINE
+                + BLANK + "Hello! I'm Duke\n"
+                + BLANK + "What can I do for you?\n"
+                + BLANK + LINE;
+        System.out.println("\n" + logo);
+        Scanner in = new Scanner(System.in);
+        String inputString;
+        inputString = in.nextLine();
+        boolean isExit = inputString.equals("bye");
+        while(!isExit) {
+            System.out.println(BLANK + LINE
+                               + BLANK + inputString + "\n"
+                               + BLANK + LINE);
+            inputString = in.nextLine();
+            isExit = inputString.equals("bye");
+        }
+        System.out.println(BLANK + LINE +
+                           BLANK + "Bye. Hope to see you again soon!\n" +
+                           BLANK + LINE);
     }
 }
