9e3786fed821bba5b4e29b86329d91a43709e6ca
Bui Phuong Nam <88639992+arsdorintbp2003@users.noreply.github.com>
2023-01-30 13:46:16 +0700

    Level 0 Done. Add greet + Rename Project

---
+++ src/main/java/Arsdorint.java
@@ -0,0 +1,20 @@
+public class Arsdorint {
+    public static void main(String[] args) {
+        String logo = "    ___                         _                                 _\n"
+                + "   / _ \\     _____   _____  ___| |   ___    _____   _   _____   _| |_\n"
+                + "  / /_\\ \\   /  ___| /  __/ /  _  |  / _ \\  /  ___| | | |  _  \\ |_   _|\n"
+                + " / _____ \\  | /    __\\ \\   | |_| | | |_| | | /     | | | | | |   | |\n"
+                + "/_/     \\_\\ |_|   /____/   \\_____|  \\___/  |_|     |_| |_| |_|   |_|\n";
+        System.out.println("Hello from\n" + logo);
+        //System.out.println("____________________________________________________________\n" +
+                //" Hello! I'm Arsdorint\n" +
+                //" What can I do for you?");
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm Arsdorint\n" +
+                " What can I do for you?\n");
+
+        System.out.println("____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+    }
+}
