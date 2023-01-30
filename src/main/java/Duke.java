a9035d4cb21a18882eb0cd927b431708de64c0de
darrenlsx <darren.lim@live.com.sg>
2023-01-31 00:54:35 +0800

    Revert "Level-4 increment"
    This reverts commit 1502cc7632f00b743ddecb5aa68911ca358ccb01.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,13 +6,14 @@ public class Duke {
     public static String LINE_PRINT = "____________________________________________________________\n";
 
     /**
-     * Adds the input text into the list
+     * Adds the text entered by the user into the list and prints the added text
      *
-     * @param t
+     * @param s
      * @param myList
      */
-    public static void addList(Task t, ArrayList<Task> myList) {
-        myList.add(t);
+    public static void addList(String s, ArrayList<String> myList) {
+        myList.add("[ ] " + s);
+        System.out.println(LINE_PRINT + "added: " + s + "\n" + LINE_PRINT);
     }
 
     /**
@@ -20,8 +21,8 @@ public class Duke {
      *
      * @param myList
      */
-    public static void printList(ArrayList<Task> myList) {
-        System.out.println(LINE_PRINT + "Here are the tasks in your list: \n");
+    public static void printList(ArrayList<String> myList) {
+        System.out.println(LINE_PRINT);
         for (int i = 0; i < myList.size(); ++i) {
             System.out.println(i + 1 + ". " + myList.get(i));
         }
@@ -34,13 +35,13 @@ public class Duke {
      * @param s
      * @param myList
      */
-    public static void markDone(String s, ArrayList<Task> myList) {
+    public static void markDone(String s, ArrayList<String> myList) {
         String taskToMarkString = s.substring(s.length() - 1);
         int taskToMark = Integer.parseInt(taskToMarkString) - 1;
-        myList.get(taskToMark).setDone();
+        myList.set(taskToMark, myList.get(taskToMark).replaceFirst(" ", "X"));
         System.out.println(LINE_PRINT
                 + "Nice! I've marked this task as done:\n"
-                + myList.get(taskToMark).description + "\n"
+                + myList.get(taskToMark) + "\n"
                 + LINE_PRINT);
     }
 
@@ -50,69 +51,13 @@ public class Duke {
      * @param s
      * @param myList
      */
-    public static void markUndone(String s, ArrayList<Task> myList) {
+    public static void markUndone(String s, ArrayList<String> myList) {
         String taskToUnmarkString = s.substring(s.length() - 1);
         int taskToUnmark = Integer.parseInt(taskToUnmarkString) - 1;
-        myList.get(taskToUnmark).setUndone();
-        //myList.set(taskToUnmark, myList.get(taskToUnmark).replaceFirst("X", " "));
+        myList.set(taskToUnmark, myList.get(taskToUnmark).replaceFirst("X", " "));
         System.out.println(LINE_PRINT
                 + "OK, I've marked this task as not done yet:\n"
                 + myList.get(taskToUnmark) + "\n"
-                + LINE_PRINT);
-    }
-
-    /**
-     * Adds the input text and marks the task as a To-Do
-     *
-     * @param s
-     * @param myList
-     */
-    public static void toDoFunction(String s, ArrayList<Task> myList) {
-        String newTask[] = s.split(" ", 2);
-        ToDo toDoTask = new ToDo(newTask[1]);
-        myList.add(toDoTask);
-        System.out.println(LINE_PRINT
-                + "Got it. I've added this task:\n"
-                + myList.get(myList.size() - 1) + "\n"
-                + "Now you have " + myList.size() + " in the list.\n"
-                + LINE_PRINT);
-    }
-
-    /**
-     * Adds the input text and marks the task as a Deadline
-     *
-     * @param s
-     * @param myList
-     */
-    public static void deadlinesFunction(String s, ArrayList<Task> myList) {
-        String newTask[] = s.split(" ", 2);
-        String split[] = newTask[1].split(" /by ");
-        Deadline deadlineTask = new Deadline(split[0], split[1]);
-        myList.add(deadlineTask);
-        System.out.println(LINE_PRINT
-                + "Got it. I've added this task:\n"
-                + myList.get(myList.size() - 1) + "\n"
-                + "Now you have " + myList.size() + " in the list.\n"
-                + LINE_PRINT);
-    }
-
-    /**
-     * Adds the input text and marks the task as an Event
-     *
-     * @param s
-     * @param myList
-     */
-    public static void eventFunction(String s, ArrayList<Task> myList) {
-        String newTask[] = s.split(" ", 2);
-        String split[] = newTask[1].split(" /");
-        String timeFrom = split[1].substring(5);
-        String timeTo = split[2].substring(3);
-        Event eventTask = new Event(split[0], timeFrom, timeTo);
-        myList.add(eventTask);
-        System.out.println(LINE_PRINT
-                + "Got it. I've added this task:\n"
-                + myList.get(myList.size() - 1) + "\n"
-                + "Now you have " + myList.size() + " in the list.\n"
                 + LINE_PRINT);
     }
 
@@ -124,30 +69,25 @@ public class Duke {
         String byeLine = "Bye. Hope to see you again soon!\n";
         System.out.println(logo);
         boolean canExit = false;
-        ArrayList<Task> myList = new ArrayList<Task>();
+        ArrayList<String> myList = new ArrayList<String>();
 
         while (!canExit) {
             Scanner in = new Scanner(System.in);
             String s = in.nextLine();
+            String sArray[] = s.split(" ");
+            String firstWord = sArray[0];
+
             if (s.toLowerCase().equals("bye")) {
                 System.out.println(byeLine);
                 canExit = true;
             } else if (s.toLowerCase().equals("list")) {
                 printList(myList);
-            } else if (s.toLowerCase().startsWith("mark")) {
+            } else if (firstWord.toLowerCase().equals("mark")) {
                 markDone(s, myList);
-            } else if (s.toLowerCase().startsWith("unmark")) {
+            } else if (firstWord.toLowerCase().equals("unmark")) {
                 markUndone(s, myList);
-            } else if (s.toLowerCase().startsWith("todo")) {
-                toDoFunction(s, myList);
-            } else if (s.toLowerCase().startsWith("deadline")) {
-                deadlinesFunction(s, myList);
-            } else if (s.toLowerCase().startsWith("event")) {
-                eventFunction(s, myList);
             } else {
-                Task t = new Task(s);
-                addList(t, myList);
-                System.out.println(LINE_PRINT + "added: " + t.description + "\n" + LINE_PRINT);
+                addList(s, myList);
             }
         }
     }
