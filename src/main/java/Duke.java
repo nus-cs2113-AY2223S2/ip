b40dcc77ebe844dfd432afdcdfea2570666c112c
adenteo <adenteo@gmail.com>
2023-01-30 16:02:15 +0800

    Improve formatting of output

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,56 +1,70 @@
 import java.util.Scanner;
 
-
-
 public class Duke {
     private static int userInputCount = 0;
-    public static void main(String[] args) {
-        Task[] userInputList = new Task[100];
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    private static final String LINE = "--------------------------------------------";
+    private static final String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    private static Task[] userTaskList = new Task[100];
+    private static String userInput;
+    private static Scanner scan = new Scanner(System.in);
+
+    public static void greetUser() {
+        System.out.println(LINE);
+        System.out.println("Hello from\n" + LOGO);
         System.out.println("How can I help you?");
-        String userInput;
-        Scanner scan = new Scanner(System.in);
+        System.out.println(LINE);
+    }
+
+    public static void main(String[] args) {
+        greetUser();
         while (true) {
             userInput = scan.nextLine();
             if (userInput.equals("bye")) {
-                break;
-            } else if (userInput.equals("list")) {
+                scan.close();
+                System.out.println(LINE);
+                System.out.println("Bye! Hope to see you again soon!");
+                System.out.print(LINE);
+                System.exit(0);
+            }
+            if (userInput.equals("list")) {
+                System.out.println("These are the following tasks you have:");
                 for (int i = 0; i < userInputCount; i++) {
+                    System.out.println(LINE);
                     int index = i;
                     index++;
-                    System.out.println(index + ".[" + userInputList[i].getStatusIcon() + "] " + userInputList[i].description);
+                    System.out.println(
+                            index + ".[" + userTaskList[i].getStatusIcon() + "] " + userTaskList[i].description);
+                    System.out.println(LINE);
                 }
-            }
-            else {
-                String[] userInputSplit = userInput.split(" ");
-                if (userInputSplit.length > 1) {
-                    if (userInputSplit[0].equals("mark")){
-                        int taskIndex = Integer.parseInt(userInputSplit[1]);
+            } else {
+                String[] userInputArray = userInput.split(" ");
+                if (userInputArray.length > 1) {
+                    if (userInputArray[0].equals("mark")) {
+                        int taskIndex = Integer.parseInt(userInputArray[1]);
                         taskIndex--;
-                        userInputList[taskIndex].markAsDone();
-                        System.out.print("The following task has been marked done: ");
-                        System.out.println(userInputList[taskIndex].description);
+                        userTaskList[taskIndex].markAsDone();
+                        System.out.print(LINE + System.lineSeparator() + "The following task has been marked done: "
+                                + userTaskList[taskIndex].description + System.lineSeparator() + LINE);
                         continue;
-                    }  else if (userInputSplit[0].equals("unmark")) {
-                        int taskIndex = Integer.parseInt(userInputSplit[1]);
+                    } else if (userInputArray[0].equals("unmark")) {
+                        int taskIndex = Integer.parseInt(userInputArray[1]);
                         taskIndex--;
-                        userInputList[taskIndex].markAsUndone();
+                        userTaskList[taskIndex].markAsUndone();
                         System.out.print("The following task has been marked done: ");
-                        System.out.println(userInputList[taskIndex].description);
+                        System.out.println(userTaskList[taskIndex].description);
                         continue;
                     }
                 }
                 Task t = new Task(userInput);
                 System.out.println(userInput);
-                userInputList[userInputCount] = t;
+                userTaskList[userInputCount] = t;
                 userInputCount++;
             }
         }
-        System.out.println("Bye! Hope to see you again soon!");
+
     }
 }
