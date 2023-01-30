954b884fe7bfba010501bc957a29c2f512797bc7
briantjs00 <briantjs00@gmail.com>
2023-01-30 18:37:04 +0800

    Remove level0Greet file

--- src/main/java/level0Greet.java
+++
@@ -1,5 +0,0 @@
-public class level0Greet {
-    public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n" + "Bye. Hope to see you again soon!\n");
-    }
-}
