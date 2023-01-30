beb0e26d964f3324f4d073653c62786890d0f1d3
leonghuenweng <88919450+leonghuenweng@users.noreply.github.com>
2023-01-30 16:34:45 +0800

    Implemented Add, List function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.ArrayList;
 public class Duke{
     public static void main(String[] args){
         String dash = "__________________________________";
         String greet = dash+"\nHello I'm Duke\nWhat can I do for you?\n"+dash;
         System.out.println(greet);
+
+        ArrayList<String> array =new ArrayList<String>();
 
         while(true){
             Scanner scan = new Scanner(System.in);
@@ -12,8 +16,18 @@ public class Duke{
                 System.out.println(dash+"\nBye. Hope to see you again soon!\n"+dash);
                 break;
             }
+            else if (message.equals("list")) {
+                String[] outputArray = new String[array.size()];
+                outputArray = array.toArray(outputArray);
+                System.out.println(dash+"\n");
+                for(int a =0;a<array.size();a++) {
+                    System.out.println(Integer.toString(a+1)+". "+outputArray[a]+"\n");
+                }
+                System.out.println(dash+"\n");
+            }
             else{
-                System.out.println(dash+"\n"+message+"\n"+dash);
+                array.add(message);
+                System.out.println(dash+"\nadded: "+message+"\n"+dash);
             }
         }
     }
