bbfd4955c3ab34c603a84a99ecd346153d9ec775
alexgoexercise <alexzhaolixiuqi@gmail.com>
2023-01-30 19:47:18 +0800

    Add echo function to Duke.java for level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 public class Duke {
+
     public static void drawLine(String symbol) {
         for (int i = 0; i < 40; i++) {
             System.out.print(symbol);
         }
         System.out.println();
+    }
+    public static void echo(String text) {
+        drawLine("_");
+        System.out.println(text);
+        drawLine("_");
+    }
+    public static void loopEcho(){
+        Scanner in = new Scanner(System.in);
+        String text = in.nextLine();
+        while (!text.equals("bye")){
+            echo(text);
+            text = in.nextLine();
+        }
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -15,6 +30,8 @@ public class Duke {
         drawLine("_");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        drawLine("_");
+        loopEcho();
         drawLine("_");
         System.out.println("Bye. Hope to see you again soon!");
         drawLine("_");
