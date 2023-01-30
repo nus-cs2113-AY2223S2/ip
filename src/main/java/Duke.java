c24b6482f033b0bdcb68e82c0a50adb9a6f664e7
alexgoexercise <alexzhaolixiuqi@gmail.com>
2023-01-30 19:45:29 +0800

    Set up level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,10 @@
 public class Duke {
+    public static void drawLine(String symbol) {
+        for (int i = 0; i < 40; i++) {
+            System.out.print(symbol);
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +12,11 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        drawLine("_");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        drawLine("_");
+        System.out.println("Bye. Hope to see you again soon!");
+        drawLine("_");
     }
 }
