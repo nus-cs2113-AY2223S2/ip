faaf34e5f30a8a214993c22a3597471987855f16
Leo Zheng Rui Darren <leo.darren@gmail.com>
2023-01-30 21:12:50 +0800

    Edit Event class toString() method

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -26,7 +26,7 @@ public class Event extends Task {
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + "(from: " + getFrom() + "to " + getTo() + ")";
+        return "[E]" + super.toString() + "(from: " + getFrom() + "to: " + getTo() + ")";
     }
 
 }
