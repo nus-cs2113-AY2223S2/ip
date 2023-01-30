cc01949170569058e5da5c194e0bfcca500b8730
briantjs00 <briantjs00@gmail.com>
2023-01-30 19:53:30 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     private static String line = "____________________________________________________________";
+    public static final int MAX_SIZE = 100;
     public static void greet() {
         System.out.println(line);
         System.out.println("Hello! I'm Duke\n" + "What can I do for you?");
@@ -20,18 +21,37 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greet();
-        String list;
+        String input;
+        String[] list = new String[MAX_SIZE];
+        int inputCounter = 0;
         while (true) {
             Scanner in = new Scanner(System.in);
-            list = in.nextLine();
-            if (list.equalsIgnoreCase("bye")) {
+            input = in.nextLine();
+            list[inputCounter] = input;
+            if (input.equalsIgnoreCase("bye")) {
                 bye();
                 break;
             }
+            else if (input.equalsIgnoreCase("list")){
+                System.out.println(line);
+                for (int i = 0; i < inputCounter; i++) {
+                    if (i == 0) {
+                        System.out.println((i+1) + ". " + list[i]);
+                    }
+                    else if (i == (inputCounter - 1)) {
+                        System.out.println((i+1) + ". " + list[i]);
+                        System.out.println(line);
+                    }
+                    else {
+                        System.out.println((i+1) + ". " + list[i]);
+                    }
+                }
+            }
             else {
                 System.out.println(line);
-                System.out.println(list);
+                System.out.println("added: " + list[inputCounter]);
                 System.out.println(line);
+                inputCounter++;
             }
         }
     }
