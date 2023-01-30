814b3e9c5ecf6b1c9dd2ddd2425e6c270bea7e33
Richardtok <e0902030@u.nus.edu>
2023-01-30 16:02:38 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo =  "Hello! I'm Duke\n" + "What can I do for you?\n" + "Bye. Hope to see you again soon!";
+        String logo =  "Hello! I'm Duke\n" + "What can I do for you?\n";
         System.out.println(logo);
+        Scanner in = new Scanner(System.in);
+        String line;
+        //String[] todo = new String[100];
+        while(true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+            }
+            System.out.println("added:"+ line);
+        }
     }
 }
