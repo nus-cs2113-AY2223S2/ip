ee4542a5ff52fefb0e8815d585a529eb75a6e92b
Muhammad Rafi Adzikra Sujai <E1100936@u.nus.edu>
2023-01-30 17:20:02 +0800

    Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -7,14 +7,27 @@ public class Duke {
         "Hello! I'm Duke\n"+
         "What can I do for you?\n"
    );
-   String input = sc.next();
+   ArrayList<String> items = new ArrayList<String>();
+   String input = "";
    while (!input.equals("bye"))
    {
-    System.out.println("____________________________________________________________\n" + "  " + input + "\n" + "____________________________________________________________\n");
-    input = sc.next();;
-
-        
+    input = sc.next();
+    System.out.println("____________________________________________________________\n" + "  " + "added: "+ input + "\n" + "____________________________________________________________\n");
+    if (!input.equals("list")) {
+        items.add(input);
     }
-    System.out.println("____________________________________________________________\n" + "  " + "Bye. Hope to see you again soon!" + "\n" + "____________________________________________________________\n");
+    else {
+        System.out.println("____________________________________________________________\n");
+        for(int i = 1; i <= items.size(); i++){
+            System.out.println(" " + i + ". " + items.get(i-1));
+        }
+        System.out.println("\n");
+        System.out.println("____________________________________________________________\n");
+    }
+   
+    }
+    
+    
+        System.out.println("____________________________________________________________\n" + "  " + "Bye. Hope to see you again soon!" + "\n" + "____________________________________________________________\n");
 }
 }
