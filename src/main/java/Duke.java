5481a6ae3db4b32da98576548a974b06460b1c99
DESKTOP-ULU427O\jingy <jingya.lin02@gmail.com>
2023-01-30 21:11:53 +0800

    Add level-2: list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class Duke {
+
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -12,12 +14,15 @@ public class Duke {
         echo();
     }
 
+    private static String[] list = new String[100];
+    private static int listCount = 0;
+
+
     public static void greet() {
         String greeting = "____________________________________________________________\n"
                 + "Hello! I'm Duke\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "\n";
+                + "____________________________________________________________";
                 System.out.println(greeting);
     }
 
@@ -27,15 +32,27 @@ public class Duke {
         return input;
     }
 
+    public static void addTask(String userInput) {
+        list[listCount] = userInput;
+        listCount ++;
+    }
+
     public static void echo() {
         for (int x = 0; x < 100; x += 1) {
             String command = getUserInput();
-            System.out.println("____________________________________________________________\n");
+            System.out.println("____________________________________________________________");
             if (command.compareTo("bye") == 0) {
-                System.out.println("Bye. Hope to see you again soon!\n" + "____________________________________________________________\n");
+                System.out.println("Bye. Hope to see you again soon!\n" + "____________________________________________________________");
                 break;
+            } else if (command.compareTo("list") == 0) {
+                for (int y = 0; y < listCount; y += 1) {
+                    int taskN = y + 1;
+                    System.out.println(taskN + ". " + list[y]);
+                }
+                System.out.println("____________________________________________________________");
             } else {
-                System.out.println(command + "\n" + "____________________________________________________________\n");
+                addTask(command);
+                System.out.println("added: " + command + "\n" + "____________________________________________________________");
             }
         }
     }
