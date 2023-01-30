50e943829988ec3ada29b2e76d8cff3191a38488
TJ\user <e0773494@u.nus.edu>
2023-01-30 22:06:58 +0800

    Revert "no message"
    This reverts commit a1fb5b38852c099c29cf67d489d5280a213c1b54.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
     }
 }
