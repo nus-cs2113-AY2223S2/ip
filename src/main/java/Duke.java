3227bffae9d7273c98d9b6e79240251792c753e5
Yi Daeun <dawnayi2002@gmail.com>
2023-01-30 18:34:26 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String HORIZONTAL_LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,13 +11,43 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        //Greeting
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        //Exit
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.print("____________________________________________________________");
+        greetUser();
+        String command = getUserCommand();
+        while(!command.equals("bye")){
+            executeCommand(command);
+            command = getUserCommand();
+        }
+        exitProgram();
     }
+
+    public static void greetUser(){
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("[Duke]: Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void exitProgram(){
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("[Duke]: Bye. Hope to see you again soon!");
+        System.out.print(HORIZONTAL_LINE);
+    }
+
+    public static String getUserCommand(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("[User]: ");
+        return scanner.nextLine();
+    }
+
+    public static void executeCommand(String command){
+        echo(command);
+    }
+
+    public static void echo(String command){
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("[DUKE]: "+ command);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+
 }
