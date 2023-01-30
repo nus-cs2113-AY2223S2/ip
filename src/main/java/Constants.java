e488236e101f0f81c5f8f0941cdc5f798329a3f3
chongyongrui <yrchong2000@gmail.com>
2023-01-30 21:27:33 +0800

    Add input checks for Duke functions, new constants class

---
+++ src/main/java/Constants.java
@@ -0,0 +1,4 @@
+public final class Constants {
+    public static final int MAX_NUMBER_OF_ENTRIES = 100;
+    public static final String[] listOfKeywords = {"event","todo","deadline","mark","unmark","list"};
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Scanner;
 
 
 public class Duke {
 
-    private static final Task[] tasks = new Task[100];
+    private static final Task[] tasks = new Task[Constants.MAX_NUMBER_OF_ENTRIES];
 
     public static void main(String[] args) {
 
@@ -17,33 +18,55 @@ public class Duke {
 
         while (numberOfTasks < 100) {
             command = latestResponse;
+
+
             if (!hasAdditionalTask) {
                 System.out.println("What can I do for your today?");
                 command = readTask();
             }
+            boolean hasDuplicateKeywords = checkForDuplicateKeyword(command);
+
+            while (hasDuplicateKeywords == true) {
+                printDuplicateKeywordErrorMessage();
+                command = readTask();
+                hasDuplicateKeywords = checkForDuplicateKeyword(command);
+            }
             printHorizontalBar();
 
-            if (Objects.equals(command, "list")) {
+            if (command.contains("list")) {
                 printTaskList();
 
             } else if (Objects.equals(command, "bye")) {
                 break;
 
             } else if (command.contains("unmark")) {
-                unmarkTask(command, numberOfTasks);
+                boolean isValidUnmarkInput = checkUnmarkInput(command, numberOfTasks);
+                if (isValidUnmarkInput) {
+                    unmarkTask(command, numberOfTasks);
+                }
 
 
             } else if (command.contains("mark")) {
-                markTask(command, numberOfTasks);
+                boolean isValidMarkInput = checkMarkInput(command, numberOfTasks);
+                if (isValidMarkInput) {
+                    markTask(command, numberOfTasks);
+                }
 
             } else if (command.contains("deadline")) {
-                createDeadline(command, numberOfTasks);
-                numberOfTasks++;
+                boolean isValidDeadlineInput = checkDeadlineInput(command);
+                if (isValidDeadlineInput) {
+                    createDeadline(command, numberOfTasks);
+                    numberOfTasks++;
+                }
 
 
             } else if (command.contains("event")) {
-                createEvent(command, numberOfTasks);
-                numberOfTasks++;
+                boolean isValidEventInput = checkEventInput(command);
+                if (isValidEventInput) {
+                    createEvent(command, numberOfTasks);
+                    numberOfTasks++;
+                }
+
 
 
             } else {
@@ -68,6 +91,172 @@ public class Duke {
         System.out.println("Bye! Hope to see you again soon!\n");
         printHorizontalBar();
 
+    }
+
+    private static boolean checkDeadlineInput(String command) {
+        command = command.toLowerCase();
+        String keyword = command.substring(0, 8);
+
+        int numberOfErrors = 0;
+        if (!keyword.equals("deadline")) {
+            System.out.println("The keyword 'deadline' should be the first word");
+            return false;
+        }
+
+        String[] words = command.split(" ");
+        char slash = words[words.length - 1].charAt(0);
+        int compareChars = Character.compare(slash, '/');
+        if (compareChars != 0) {
+
+            System.out.println("ERROR!  INVALID FORMAT! \nThe deadline function is used in the following format:");
+            System.out.println("deadline your_item_name /(due date or time)\n");
+            return false;
+        }
+
+        if (words.length<3){
+            System.out.println("ERROR! Deadline name is empty");
+            return false;
+        }
+
+        return true;
+
+
+    }
+
+    private static boolean checkEventInput(String command) {
+        command = command.toLowerCase();
+        String keyword = command.substring(0, 5);
+
+        int numberOfErrors = 0;
+        if (!keyword.equals("event")) {
+            System.out.println("The keyword 'event' should be the first word");
+            return false;
+        }
+
+        String[] words = command.split(" ");
+        char fromSlash = words[words.length - 2].charAt(0);
+        char toSlash = words[words.length - 1].charAt(0);
+        int compareFromSlash = Character.compare(fromSlash, '/');
+        int compareToSlash = Character.compare(toSlash, '/');
+        if (compareFromSlash != 0 || compareToSlash != 0) {
+
+            System.out.println("ERROR! INVALID FORMAT! \n" + "The event function is used in the following format:");
+            System.out.println("event your_item_name /(start time) /(end time) \n");
+            return false;
+        }
+
+        if (words.length<4){
+            System.out.println("ERROR! Event name is empty");
+            return false;
+        }
+
+
+        return true;
+
+
+    }
+
+    private static boolean checkUnmarkInput(String command, int numberOfTasks) {
+        command = command.toLowerCase();
+        String[] words = command.split(" ");
+
+        int numberOfErrors = 0;
+        if (words.length > 2) {
+            System.out.println("The unmark function is used in the following format:");
+            System.out.println("Unmark 'task number'");
+            numberOfErrors++;
+        }
+        if (!Objects.equals(words[0], "unmark")) {
+            System.out.println("The keyword 'unmark' should be the first word");
+            numberOfErrors++;
+        }
+
+        int taskNumber = Integer.parseInt(words[1]);
+
+        if (taskNumber > numberOfTasks || taskNumber < 1) {
+            System.out.println("The task number is invalid!");
+            numberOfErrors++;
+        }
+
+        if (numberOfErrors > 0) {
+            System.out.println("Please input again in the correct format!");
+            return false;
+        }
+        return true;
+
+
+    }
+
+    private static boolean checkMarkInput(String command, int numberOfTasks) {
+        command = command.toLowerCase();
+        String[] words = command.split(" ");
+
+        int numberOfErrors = 0;
+        if (words.length > 2) {
+            System.out.println("The mark function is used in the following format:");
+            System.out.println("mark 'task number'");
+            numberOfErrors++;
+        }
+        if (!Objects.equals(words[0], "mark")) {
+            System.out.println("The keyword 'mark' should be the first word");
+            numberOfErrors++;
+        }
+
+        int taskNumber = Integer.parseInt(words[1]);
+
+        if (taskNumber > numberOfTasks || taskNumber < 1) {
+            System.out.println("The task number is invalid!");
+            numberOfErrors++;
+        }
+
+        if (numberOfErrors > 0) {
+            System.out.println("Please input again in the correct format!");
+            return false;
+        }
+        return true;
+
+
+    }
+
+
+    private static void printDuplicateKeywordErrorMessage() {
+        System.out.println("ERROR! Multiple keywords have been detected!");
+        System.out.println("The keywords are:");
+        printAllKeywords();
+        System.out.println("Make sure your input only contains 1 keyword!");
+
+    }
+
+    private static void printAllKeywords() {
+        String[] keywords = Constants.listOfKeywords;
+        int len = keywords.length;
+        int i = 0;
+        while (i < len) {
+            System.out.println((i + 1) + ": " + keywords[i]);
+            i++;
+        }
+
+    }
+
+    private static boolean checkForDuplicateKeyword(String input) {
+        input = input.toLowerCase();
+        String[] contents = input.split(" ");
+        int i = 0;
+        int len = contents.length;
+        int numberOfKeywords = 0;
+        while (i < len) {
+            if (contents[i].equals("list") || contents[i].equals("mark") || contents[i].equals("unmark") || contents[i].equals("deadline") || contents[i].equals("event") || contents[i].equals("todo")) {
+                numberOfKeywords++;
+            }
+            i++;
+        }
+
+        if (numberOfKeywords > 1) {
+            return true;
+        }
+
+
+        return false;
     }
 
     private static void greetUser() {
