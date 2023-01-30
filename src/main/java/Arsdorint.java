f035cec5bea26f6bfff6a6a24552423d1465c1e1
Bui Phuong Nam <e0851451@u.nus.edu>
2023-01-30 14:21:15 +0700

    Finish Level-1 (Add Greet, Echo, Exit)

--- src/main/java/Arsdorint.java
+++ src/main/java/Arsdorint.java
@@ -1,20 +1,39 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class Arsdorint {
+    static Scanner input = new Scanner(System.in);
+    static String logo = "    ___                         _                                 _\n"
+            + "   / _ \\     _____   _____  ___| |   ___    _____   _   _____   _| |_\n"
+            + "  / /_\\ \\   /  ___| /  __/ /  _  |  / _ \\  /  ___| | | |  _  \\ |_   _|\n"
+            + " / _____ \\  | /    __\\ \\   | |_| | | |_| | | /     | | | | | |   | |\n"
+            + "/_/     \\_\\ |_|   /____/   \\_____|  \\___/  |_|     |_| |_| |_|   |_|\n";
+
+    public static void echo(String command) {
+        System.out.println("Arsdorint says: " + command);
+    }
+
+    public static String ask() {
+        String ask;
+        return ask = input.nextLine();
+    }
+
     public static void main(String[] args) {
-        String logo = "    ___                         _                                 _\n"
-                + "   / _ \\     _____   _____  ___| |   ___    _____   _   _____   _| |_\n"
-                + "  / /_\\ \\   /  ___| /  __/ /  _  |  / _ \\  /  ___| | | |  _  \\ |_   _|\n"
-                + " / _____ \\  | /    __\\ \\   | |_| | | |_| | | /     | | | | | |   | |\n"
-                + "/_/     \\_\\ |_|   /____/   \\_____|  \\___/  |_|     |_| |_| |_|   |_|\n";
-        System.out.println("Hello from\n" + logo);
         //System.out.println("____________________________________________________________\n" +
                 //" Hello! I'm Arsdorint\n" +
                 //" What can I do for you?");
+        String command = " ";
+        System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________\n" +
-                " Hello! I'm Arsdorint\n" +
-                " What can I do for you?\n");
+                " Hello! I'm Arsdorint, a member of Arsdorint Team\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n");
+        while (true) {
+            command = ask();
+            if (command.toLowerCase().contains("bye")) break;
+            echo(command);
+        }
 
-        System.out.println("____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
+        System.out.println("Arsdorint says:" + " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
     }
 }
