896453e51a995733580c7c70c358ad0005f7fc0c
tyuyang <tayyuyang@gmail.com>
2023-01-31 11:21:15 +0800

    Added attribute taskType in Task

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,20 @@
 public class Task {
     private String name;
     private boolean completionStatus;
+    private String taskType;
 
-    public Task(String name) {
+    public Task(String name, String taskType) {
         this.name = name;
+        this.taskType = taskType;
         this.completionStatus = false;
     }
 
     public String getName() {
         return this.name;
+    }
+
+    public String getTaskType() {
+        return this.taskType;
     }
 
     public void setStatus(boolean status) {
