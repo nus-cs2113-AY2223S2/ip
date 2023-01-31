63be52d4b509922d8111273a9773b7410fa78486
Nikhil Shashidhar <e0725992@u.nus.edu>
2023-01-31 10:48:46 +0800

    Refactor "if-else" to "switch" statements

--- ThomasShelby.java
+++ ThomasShelby.java
@@ -9,41 +9,45 @@ public class ThomasShelby {
         Scanner in = new Scanner(System.in);
         while (true) {
             String cmd = in.nextLine();
-            if (cmd.equals("bye")) {
-                break;
-            } else if (cmd.equals("list")) {
+            String[] cmdSplit = cmd.split(" ", 2); // array of words
+            int whichTask = 0; // variable holding mark/unmark task num
+            switch (cmdSplit[0]) {
+            case "bye":
+                System.out.println("Cheers.");
+                return;
+            case "list":
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". "
                             + taskManager[i].getStatusIcon() + " "
                             + taskManager[i].getDescription());
                 }
-            } else { // add cmd, mark or unmark
-                String[] cmdSplit = cmd.split(" ", 2);
-                if (cmdSplit[0].equals("mark") && Character.isDigit(cmdSplit[1].charAt(0))) { // set tick
-                    int whichTask = Integer.parseInt(cmdSplit[1]) - 1; // shift back index
-                    taskManager[whichTask].setIsDone(true);
-                    System.out.println("That was long due, well done.");
-                    System.out.println(taskManager[whichTask].getStatusIcon()
-                            + " " + taskManager[whichTask].getDescription());
-                } else if (cmdSplit[0].equals("unmark") && Character.isDigit(cmdSplit[1].charAt(0))) { // set cross
-                    int whichTask = Integer.parseInt(cmdSplit[1]) - 1; // shift back index
-                    taskManager[whichTask].setIsDone(false);
-                    System.out.println("You've gotten lazy.");
-                    System.out.println(taskManager[whichTask].getStatusIcon()
-                            + " " + taskManager[whichTask].getDescription());
-                } else if (cmdSplit[0].equals("add")) { // add new task
-                    Task newTask = new Task(cmdSplit[1]);
-                    newTask.setDescription(cmdSplit[1]);
-                    taskManager[taskCount] = newTask;
-                    taskCount++;
-                    System.out.print("added: ");
-                    System.out.println(cmdSplit[1]);
-                } else { // echo cmd
-                    System.out.println(cmd);
-                }
+                break;
+            case "add":
+                Task newTask = new Task(cmdSplit[1]);
+                newTask.setDescription(cmdSplit[1]);
+                taskManager[taskCount] = newTask;
+                taskCount++;
+                System.out.println("added: " + cmdSplit[1]);
+                break;
+            case "mark":
+                whichTask = Integer.parseInt(cmdSplit[1]) - 1; // shift back index
+                taskManager[whichTask].setIsDone(true);
+                System.out.println("That was long due, well done.");
+                System.out.println(taskManager[whichTask].getStatusIcon()
+                        + " " + taskManager[whichTask].getDescription());
+                break;
+            case "unmark":
+                whichTask = Integer.parseInt(cmdSplit[1]) - 1; // shift back index
+                taskManager[whichTask].setIsDone(false);
+                System.out.println("You've gotten lazy.");
+                System.out.println(taskManager[whichTask].getStatusIcon()
+                        + " " + taskManager[whichTask].getDescription());
+                break;
+            default:
+                System.out.println(cmd); // echo cmd
+                break;
             }
         }
-        System.out.println("Cheers.");
     }
 }
