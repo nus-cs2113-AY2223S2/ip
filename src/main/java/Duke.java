444b5c02fb08e98a5d42d122c2baff962943788e
DESKTOP-VAFN8PH\Gerald <geraldkoh4@gmail.com>
2023-01-31 20:28:05 +0800

    Finished level-4 and edited level 0, 1, 2 and 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,115 +1,70 @@
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n"
-                + "__________________________\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! Do you need anything from me?\n"
-                + "I have only been trained to greet, echo and list you so far.\n"
-                + "Once my owner is more proficient in what he does, he will give me more functions!\n"
-                + "Key in a number based on the function\n 1) Echo \n 2) List");
-        //To be added to make sure user input is read.
+        Methods.printGreetings();
+
         Task[] actions = new Task[100];
+        int actionCounter = 0;
+
         String line;
         Scanner in = new Scanner(System.in);
-        line = in.nextLine();
-        int action = Integer.parseInt(line);
-        switch (action) {
-            case 1:
-                line = in.nextLine();
-                while (!line.equals("Bye")) {
-                    System.out.println(line);
-                    line = in.nextLine();
-                }
-                break;
-            case 2:
-                int actionCounter = 0;
-                System.out.println("You may type anything to add to a list of a 100 things to do.\n"
-                        + "Type List to list down everything you have added so far.\n"
-                        + "Type Bye to exit.");
-                line = in.nextLine();
-                while (!line.equals("Bye")) {
-                    if (line.equals("List")) {
-                        if (actionCounter == 0) {
-                            System.out.println("There is nothing to list!");
-                            line = in.nextLine();
-                            continue;
-                        }
-                        for (int iterator = 0; iterator < actionCounter; iterator++) {
-                            System.out.println(iterator + 1
-                                    + ")[" + actions[iterator].getStatusIcon()
-                                    + "]"
-                                    + actions[iterator].getDescription()
-                                    );
+        int taskNumber;
+        String[] decisions;
+        Task toBeAdded;
+        String[] dates;
+        do {
+            line = in.nextLine();
+            decisions = line.split(" ");
+            dates = line.split("/");
+            switch (decisions[0]) {
+                case "echo":
+                    System.out.println(Methods.findTaskDetails(line));
+                    break;
 
-                        }
-                        System.out.println("You may now mark or unmark your tasks by typing Action followed by the task number! E.g. unmark 1 or mark 2\n"
-                                + "To go back to editing your list, type edit");
-                        line = in.nextLine();
-                        while (!line.equals("edit")) {
-                            String[] words = line.split(" ");
-                            if (!(words[0].equals("mark") || words[0].equals("unmark") || words[0].equals("edit"))) {
-                                System.out.println("Invalid Input!");
-                                line = in.nextLine();
-                                continue;
-                            }
-                            int taskNumber = Integer.parseInt(words[1]) - 1;
-                            if (words[0].equals("mark")) {
-                                actions[taskNumber].mark();
-                            } else {
-                                // must be unmarked or some weird other mistype
-                                actions[taskNumber].unmark();
-                            }
-                            System.out.println("Done!\n"
-                                    + "["
-                                    + actions[taskNumber].getStatusIcon()
-                                    + "]"
-                                    + actions[taskNumber].getDescription()
-                            );
-                            line = in.nextLine();
-                        }
-                        // have to edit before exit, might change this in the future
-                        if (line.equals("edit")) {
-                            System.out.println("Input to add to your list!");
-                        }
-                        line = in.nextLine();
-                    } else {
-                        System.out.println("Added: " + line);
-                        Task toBeAdded = new Task(line);
-                        actions[actionCounter] = toBeAdded;
-                        actionCounter++;
-                        line = in.nextLine();
+                case "todo":
+                    toBeAdded = new Todo(Methods.findTaskDetails(line));
+                    actions[actionCounter] = toBeAdded;
+                    actionCounter++;
+                    Methods.printAcknowledgement(toBeAdded,actionCounter);
+                    break;
+
+                case "event":
+                    toBeAdded = new Event(Methods.findTaskDetails(dates[0]), Methods.findTaskDetails(dates[1]), Methods.findTaskDetails(dates[2]));
+                    actions[actionCounter] = toBeAdded;
+                    actionCounter++;
+                    Methods.printAcknowledgement(toBeAdded,actionCounter);
+                    break;
+
+                case "deadline":
+                    toBeAdded = new Deadline(Methods.findTaskDetails(dates[0]), Methods.findTaskDetails(dates[1]));
+                    actions[actionCounter] = toBeAdded;
+                    actionCounter++;
+                    Methods.printAcknowledgement(toBeAdded,actionCounter);
+                    break;
+
+                case "mark":
+                    taskNumber = Integer.parseInt(decisions[1]) - 1;
+                    actions[taskNumber].mark();
+                    Methods.printDoneMarkingTasks(actions[taskNumber]);
+                    break;
+
+                case "unmark":
+                    taskNumber = Integer.parseInt(decisions[1]) - 1;
+                    actions[taskNumber].unmark();
+                    Methods.printDoneMarkingTasks(actions[taskNumber]);
+                    break;
+
+                case "list":
+                    if (actionCounter == 0) {
+                        Methods.printEmptyList();
+                        continue;
                     }
-                }
-                break;
-        }
+                    for (int iterator = 0; iterator < actionCounter; iterator++) {
+                        Methods.printListElement(iterator, actions[iterator]);
+                    }
+                    break;
+            }
+        } while (!decisions[0].equals("bye"));
         System.out.println("That's all from me! Goodbye!");
-    }
-
-    public static class Task {
-        protected String description;
-        protected boolean isDone;
-
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public String getStatusIcon() {
-            return (isDone ? "X" : " "); // mark done task with X
-        }
-        public void mark() {
-            this.isDone = true;
-        }
-        public void unmark() {
-            this.isDone = false;
-        }
-        public String getDescription() {
-            return(this.description);
-        }
     }
 }
---
+++ src/main/java/Methods.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Methods {
+    public static void printGreetings() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n"
+                + "__________________________\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! Do you need anything from me?\n"
+                + "I have only been trained to greet, echo and list you so far.\n"
+                + "Once my owner is more proficient in what he does, he will give me more functions!\n"
+                + "Key in a number based on the function\n 1)echo \n 2)todo\n 3)mark\n 4)unmark\n 5)deadline\n 6)event\n"
+                + "When you wish to exit, do tell me by typing : bye");
+    }
+    public static void print(String input) {
+        System.out.println(input);
+    }
+
+    public static void printEmptyList() {
+        System.out.println("There is nothing to list!");
+    }
+
+    public static void printListElement(int iterator, Task action) {
+        Methods.print(iterator + 1
+                + ")"
+                + action.toString());
+    }
+    public static void printDoneMarkingTasks(Task action) {
+        Methods.print("Done!\n"
+                + action.toString()
+        );
+    }
+    public static String findTaskDetails(String line) {
+        return line.substring(line.indexOf(" ") + 1);
+    }
+    public static void printAcknowledgement(Task action, int actionCounter) {
+        System.out.println("Got it. I've added this task:\n"
+                + action.toString()
+                + System.lineSeparator()
+                + "Now you have "
+                + actionCounter
+                + " tasks in the list.");
+    }
+}
