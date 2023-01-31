daafab5f16c71a0112439e64eacfb9ffea89408f
calebcjl <calebchan2000@gmail.com>
2023-01-31 14:24:55 +0800

    Add task class and feature to mark tasks

--- src/main/java/Nano.java
+++ src/main/java/Nano.java
@@ -1,30 +1,31 @@
 import java.util.Scanner;
+
 public class Nano {
-    //print horizontal line of length = len * 10
-    public static void print_hor_ln() {
-        int len = 10;
-        for (int i = 0; i < len; i += 1) {
+    //print horizontal line of length = length * 10
+    public static void printHorizontalLine() {
+        final int LENGTH = 10;
+        for (int i = 0; i < LENGTH; i += 1) {
             System.out.print("__________");
         }
         System.out.println();
     }
+
     public static void main(String[] args) {
         //chatbot startup
-        print_hor_ln();
-        String logo = "| \\  ||   / \\   | \\  |||  __  |\n"
-                    + "||\\\\ ||  / _ \\  ||\\\\ ||| |  | |\n"
-                    + "|| \\\\|| //   \\\\ || \\\\||| |__| |\n"
-                    + "||  \\_|//     \\\\||  \\_||______|\n";
-        System.out.println(logo);
+        printHorizontalLine();
+        final String NANO_LOGO = "| \\  ||   / \\   | \\  |||  __  |\n"
+                + "||\\\\ ||  / _ \\  ||\\\\ ||| |  | |\n"
+                + "|| \\\\|| //   \\\\ || \\\\||| |__| |\n"
+                + "||  \\_|//     \\\\||  \\_||______|\n";
+        System.out.println(NANO_LOGO);
         System.out.println("Serial number: 034-4532-5893.....");
         System.out.println("Activating the 7th generation Nano Machine of the Chan Corporation.....");
-        print_hor_ln();
+        printHorizontalLine();
         System.out.println("How may I assist you?");
-        print_hor_ln();
+        printHorizontalLine();
 
-        // initialise list of 100 items
-        String[] list = new String[100];
-        int listCount = 0;
+        // initialise list of 100 tasks 1-index
+        Task[] tasks = new Task[101];
 
         //receive commands
         String text;
@@ -32,21 +33,59 @@ public class Nano {
         while (!exit) {
             Scanner in = new Scanner(System.in);
             text = in.nextLine();
+
             if (text.equals("bye")) {
                 //chatbot exit when user types "bye"
                 exit = true;
-            }
-            else if (text.equals("list")) {
-                // display list
-                for (int i = 0; i < listCount; i += 1) {
-                    System.out.println((i + 1) + ". " + list[i]);
+            } else if (text.startsWith("/")) {
+                // input is a command
+                text = text.replace("/", "").trim();
+                int dividerIndex = text.indexOf(" ");
+                if (dividerIndex == -1 && text.equals("list")) {
+                    // display list
+                    for (int i = 1; i <= Task.getTaskCount(); i += 1) {
+                        System.out.print(i + ". ");
+                        if (tasks[i].getTaskStatus()) {
+                            System.out.print("[x] ");
+                        } else {
+                            System.out.print("[] ");
+                        }
+                        System.out.println(tasks[i].getTaskName());
+                    }
+                } else {
+                    // split command and taskName into 2 strings
+                    String command = text.substring(0, dividerIndex);
+                    String taskName = text.substring(dividerIndex + 1);
+                    System.out.println(taskName);
+
+                    switch (command) {
+                    case "mark":
+                        // mark task as completed
+                        for (int i = 1; i <= Task.getTaskCount(); i += 1) {
+                            if (tasks[i].getTaskName().equals(taskName) && !tasks[i].getTaskStatus()) {
+                                tasks[i].markTask();
+                                System.out.println("Marked " + tasks[i].getTaskName());
+                                break;
+                            }
+                        }
+                        break;
+                    case "unmark":
+                        // unmark task completed status
+                        for (int i = 1; i <= Task.getTaskCount(); i += 1) {
+                            if (tasks[i].getTaskName().equals(taskName) && tasks[i].getTaskStatus()) {
+                                tasks[i].unmarkTask();
+                                System.out.println("Unmarked " + tasks[i].getTaskName());
+                                break;
+                            }
+                        }
+                        break;
+                    }
                 }
-            }
-            else {
-                // add text to list
-                list[listCount] = text;
-                listCount += 1;
-                System.out.println("Added " + text + " to list.");
+            } else {
+                // add task to list
+                Task newTask = new Task(text);
+                tasks[Task.getTaskCount()] = newTask;
+                System.out.println("Added " + newTask.getTaskName() + " to list.");
             }
         }
 
---
+++ src/main/java/Task.java
@@ -0,0 +1,32 @@
+public class Task {
+    private String name;
+    private boolean isCompleted;
+    private static int taskCount = 0;
+    private static int completedTaskCount = 0;
+
+    public Task(String name) {
+        this.name = name;
+        this.isCompleted = false;
+        taskCount += 1;
+    }
+
+    // mark task as completed
+    public void markTask() {
+        this.isCompleted = true;
+        completedTaskCount += 1;
+    }
+    // unmark task to become uncompleted
+    public void unmarkTask() {
+        this.isCompleted = false;
+    }
+
+    public String getTaskName() {
+        return this.name;
+    }
+    public boolean getTaskStatus() {
+        return this.isCompleted;
+    }
+    public static int getTaskCount() {
+        return taskCount;
+    }
+}
