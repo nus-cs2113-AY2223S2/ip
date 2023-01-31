bc2fa218c1196692bb9714b69274c7ee7cb2d1b6
nichyjt <nicholas.yek@gmail.com>
2023-01-31 12:18:07 +0800

    Add TODO, EVENT and DEADLINE subclasses
    Signed-off-by: nichyjt <nicholas.yek@gmail.com>

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,14 @@
+public class Deadline extends Task{
+    private String dueDate;
+    public Deadline(String description, String dueDate){
+        super(description);
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String getDescription() {
+        String desc = "[D]" + super.getDescription();
+        desc = desc.concat(" (by: "+ this.dueDate + ")");
+        return desc;
+    }
+}
---
+++ src/main/java/Event.java
@@ -0,0 +1,18 @@
+ public class Event extends Task{
+    private String eventFrom;
+    private String eventTo;
+    public Event(String description, String from, String to){
+        super(description);
+        this.eventFrom = from;
+        this.eventTo = to;
+    }
+
+    @Override
+    public String getDescription() {
+        String desc =  "[E]" +super.getDescription();
+        String fromStr = " (FROM: " + eventFrom + ", ";
+        String toStr = "TO: " + eventTo + ")";
+        desc = desc.concat(fromStr).concat(toStr);
+        return desc;
+    }
+}
