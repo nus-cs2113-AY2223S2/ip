aeb3c02d13e753d7e107a4cd53a3b4baea8e5266
TJ\user <e0773494@u.nus.edu>
2023-01-31 17:21:02 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Hello! I'm Duke \nWhat can I do for you?");
+
+        boolean isFinished = false;
+
+        String userInput;
+        Scanner scan = new Scanner(System.in);
+
+        while (!isFinished) {
+            userInput = scan.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                isFinished = true;
+            } else {
+                System.out.println(userInput);
+            }
+        }
     }
 }
