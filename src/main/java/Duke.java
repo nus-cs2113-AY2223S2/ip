86daa30c1d200171010bbe7c7772f771e48d4869
yuanners <beetreeear@gmail.com>
2023-01-31 21:03:58 +0800

    Add A-CodeQuality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,6 +12,7 @@ public class Duke {
 		int count = 0;
 
 		while (!(input.equalsIgnoreCase (exit)) && !(input.isEmpty ())) {
+
 			if (input.equalsIgnoreCase (list)) {
 				listTasks (tasks, count);
 			} else if (input.startsWith ("mark") || input.startsWith ("unmark")) {
@@ -21,6 +22,7 @@ public class Duke {
 			} else {
 				break;
 			}
+
 			input = scan.nextLine ();
 		}
 		printEnd ();
@@ -34,6 +36,7 @@ public class Duke {
 		String[] arrInput = input.split (" ", 2);
 		printLine ();
 		System.out.println ("Got it. I've added this task:");
+
 		if (arrInput[0].equalsIgnoreCase (task)) {
 			tasks[count] = new Task (arrInput[1]);
 		} else if (arrInput[0].equalsIgnoreCase (deadline)) {
@@ -44,6 +47,7 @@ public class Duke {
 			String[] eventTask = arrTask[1].split ("/to");
 			tasks[count] = new Event (arrTask[0], eventTask[0], eventTask[1]);
 		}
+
 		System.out.println (tasks[count].getType () + tasks[count].toString ());
 		count++;
 		System.out.println ("Now you have " + count + " tasks in the list.");
@@ -55,11 +59,13 @@ public class Duke {
 		String[] arrInput = input.split (" ", 2);
 		int index = Integer.parseInt (arrInput[1]);
 		index -= 1;
+
 		if (input.startsWith ("mark")) {
 			tasks[index].isDone = true;
 		} else {
 			tasks[index].isDone = false;
 		}
+
 		printLine ();
 		System.out.println ("Nice! I've marked this task as done:");
 		System.out.println (tasks[index].getType () + tasks[index].toString ());
@@ -69,9 +75,11 @@ public class Duke {
 	public static void listTasks (Task[] tasks, int count) {
 		printLine ();
 		System.out.println ("Here are the tasks in your list:");
+
 		for (int i = 0; i < count; i++) {
 			System.out.println ((i + 1) + ". " + tasks[i].getType () + tasks[i].toString ());
 		}
+
 		printLine ();
 	}
 
@@ -82,7 +90,6 @@ public class Duke {
 				+ "| |_| | |_| |   <  __/\n"
 				+ "|____/ \\__,_|_|\\_\\___|\n";
 		System.out.println ("Hello from\n" + logo);
-
 		printLine ();
 		System.out.println ("Hello! I'm Duke");
 		System.out.println ("What can I do for you?");
