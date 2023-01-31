7e7af71bc9993c4288100e7e9e408dd040ef1088
Chick3nBoy <leonardo.ongdc@gmail.com>
2023-01-31 11:25:10 +0800

    Finish level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,28 +2,51 @@ import java.util.Scanner;
 public class Duke {
 
 
-    private static final String LINE = "____________________________________________________________\n";
+    private static final String LINE = "____________________________________________________________";
     private static final String BLANK = "    ";
 
     public static void main(String[] args) {
-        String logo = BLANK + LINE
+        String logo = BLANK + LINE + "\n"
                 + BLANK + "Hello! I'm Duke\n"
                 + BLANK + "What can I do for you?\n"
                 + BLANK + LINE;
         System.out.println("\n" + logo);
         Scanner in = new Scanner(System.in);
         String inputString;
-        inputString = in.nextLine();
-        boolean isExit = inputString.equals("bye");
-        while(!isExit) {
-            System.out.println(BLANK + LINE
-                               + BLANK + inputString + "\n"
-                               + BLANK + LINE);
+        String[] inputs = new String[100];
+        boolean isExit = false;
+        int numberOfInputs = 0;
+
+        while (!isExit) {
             inputString = in.nextLine();
-            isExit = inputString.equals("bye");
+            if (inputString.equals("list")) {
+                doList(inputs, numberOfInputs);
+            } else if (inputString.equals("bye")) {
+                doExit();
+                isExit = true;
+            } else {
+                inputs[numberOfInputs] = inputString;
+                printInput(inputString);
+                numberOfInputs += 1;
+            }
         }
-        System.out.println(BLANK + LINE +
-                           BLANK + "Bye. Hope to see you again soon!\n" +
-                           BLANK + LINE);
+    }
+
+    private static void doList(String[] inputs, int numberOfInputs) {
+        System.out.println(BLANK + LINE);
+        for (int i = 0; i < numberOfInputs; i += 1) {
+            System.out.println(BLANK + (i + 1) + "." + inputs[i]);
+        }
+        System.out.println(BLANK + LINE);
+    }
+    private static void doExit() {
+        System.out.println(BLANK + LINE);
+        System.out.println(BLANK + "Bye. Hope to see you again soon!");
+        System.out.println(BLANK + LINE);
+    }
+    private static void printInput(String input) {
+        System.out.println(BLANK + LINE);
+        System.out.println(BLANK + input);
+        System.out.println(BLANK + LINE);
     }
 }
