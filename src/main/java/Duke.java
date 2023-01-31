0b9bb0a713e1339f04972a3a2af5999ea9c9a3b1
ysl-28 <88529907+ysl-28@users.noreply.github.com>
2023-02-01 02:08:33 +0800

    Remove magic numbers

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,9 @@
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+        printWelcome();
 
         String command;
         Scanner in = new Scanner(System.in);
@@ -24,64 +15,122 @@ public class Duke {
             command = in.nextLine();
 
             if (command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                printBye();
                 isRunning = false;
 
             } else if (command.equals("list")) {
-                int count = 1;
-                System.out.println("Here are the tasks in your list:");
-                for (Task task: tasks) {
-                    System.out.println(count + "." + task);
-                    count += 1;
-                }
-            } else if (command.matches("mark \\d+")) {
-                int taskNumber = Character.getNumericValue(command.charAt(5)); //convert to char than get numeric value
-                Task task = tasks.get(taskNumber-1);
-                task.markAsDone();
+                printAllTasks(tasks);
 
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(task);
+            } else if (command.matches("mark \\d+")) {
+                markTaskDone(tasks, command);
 
             } else if (command.matches("unmark \\d+")) {
-                int taskNumber = Character.getNumericValue(command.charAt(7)); //convert to char than get numeric value
-                Task task = tasks.get(taskNumber-1);
-                task.unmarkAsDone();
-
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(task);
+                markTaskNotDone(tasks, command);
 
             } else if (command.matches("todo" + "(.*)")) {
-                System.out.println("Got it. I've added this task:");
-                String description = command.substring(5); //index 5 = beginning of task
-                Task task = new Todo(description);
-                tasks.add(task);
-
-                System.out.println(task);
-                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+               addTodo(tasks, command);
 
             } else if (command.matches("deadline" + "(.*)" + "/by" + "(.*)")) {
-                System.out.println("Got it. I've added this task:");
-                int endOfTask = command.indexOf("/");
-                String description = command.substring(9, endOfTask); //index 9 = beginning of task
-                String date = command.substring(endOfTask + 3);
-                Task task = new Deadline(description, date);
-                tasks.add(task);
-
-                System.out.println(task);
-                System.out.println("Now you have " + tasks.size() + " tasks in the list");
+                addDeadline(tasks, command);
 
             } else if (command.matches("event" + "(.*)" + "/from" + "(.*)" + "/to" + "(.*)")) {
-                System.out.println("Got it. I've added this task:");
-                command = command.replace("event ", ""); //remove "event" from string
-                String[] components = command.split(" /from | /to "); //split string using "/from" and "/to"
+               addEvent(tasks, command);
 
-                Task task = new Event(components[0], components[1], components[2]);
-                tasks.add(task);
-                System.out.println(task);
-                System.out.println("Now you have " + tasks.size() + " tasks in the list");
             } else {
-                System.out.println("I don't know what that means :-(");
+                printInvalidMessage();
             }
         }
+    }
+    public static void markTaskDone(ArrayList<Task> tasks, String command) {
+        command = command.replace("mark ", "");
+        int taskNumber = Integer.parseInt(command);
+        Task task = tasks.get(taskNumber-1);
+        task.markAsDone();
+        printMarkDone();
+        printTask(task);
+    }
+
+    public static void markTaskNotDone(ArrayList<Task> tasks, String command) {
+        command = command.replace("unmark ", "");
+        int taskNumber = Integer.parseInt(command);
+        Task task = tasks.get(taskNumber-1);
+        task.unmarkAsDone();
+
+        printMarkNotDone();
+        printTask(task);
+    }
+    public static void addTodo(ArrayList<Task> tasks, String command) {
+        printAddTask();
+
+        String description = command.replace("todo ", "");
+        Task task = new Todo(description);
+        tasks.add(task);
+
+        printTask(task);
+        printNoOfTasks(tasks.size());
+    }
+
+    public static void addDeadline(ArrayList<Task> tasks, String command) {
+        printAddTask();
+
+        command = command.replace("deadline ", "");
+        String[] components = command.split("/by");
+        Task task = new Deadline(components[0], components[1]);
+        tasks.add(task);
+
+        printTask(task);
+        printNoOfTasks(tasks.size());
+    }
+
+    public static void addEvent(ArrayList<Task> tasks, String command) {
+        printAddTask();
+
+        command = command.replace("event ", ""); //remove "event" from string
+        String[] components = command.split(" /from | /to "); //split string using "/from" and "/to"
+        Task task = new Event(components[0], components[1], components[2]);
+        tasks.add(task);
+
+        printTask(task);
+        printNoOfTasks(tasks.size());
+    }
+    public static void printAllTasks(ArrayList<Task> tasks) {
+        int count = 1;
+        System.out.println("Here are the tasks in your list:");
+        for (Task task: tasks) {
+            System.out.println(count + "." + task);
+            count += 1;
+        }
+    }
+    public static void printTask(Task task) {
+        System.out.println(task);
+    }
+    public static void printAddTask() {
+        System.out.println("Got it. I've added this task:");
+    }
+    public static void printMarkDone() {
+        System.out.println("Nice! I've marked this task as done:");
+    }
+    public static void printMarkNotDone() {
+        System.out.println("OK, I've marked this task as not done yet:");
+    }
+    public static void printNoOfTasks(int size) {
+        System.out.println("Now you have " + size + " tasks in the list");
+    }
+    public static void printInvalidMessage() {
+        System.out.println("I don't know what that means :-(");
+    }
+    public static void printBye() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+    public static void printWelcome() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
     }
 }
