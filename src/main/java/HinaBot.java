565a8e1d9f254770df775de2bdab9b0c2871768a
waiter-palypoo <gohjinghong2000@gmail.com>
2023-02-01 00:59:47 +0800

    Add code for adding items to a list and printing the list

--- src/main/java/HinaBot.java
+++ src/main/java/HinaBot.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class HinaBot {
+    protected static String[] taskList = new String[100];
+    protected static int taskCount = 0;
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
@@ -17,12 +19,28 @@ public class HinaBot {
     }
 
     public static void handleCommand(String command) {
-        if (!command.equalsIgnoreCase("bye")) {
-            System.out.println(command);
-        }
-        else {
+        if (command.equalsIgnoreCase("bye")) {
             System.out.println("Goodbye master, let's meet again soon...");
             System.exit(0);
+        }
+        else if (command.equalsIgnoreCase("list")) {
+            listTasks();
+        }
+        else {
+            addTask(command);
+        }
+    }
+
+    public static void addTask(String task) {
+        taskList[taskCount] = task;
+        taskCount++;
+        System.out.println("added: " + task);
+    }
+
+    public static void listTasks() {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.print(i + 1);
+            System.out.println(". " + taskList[i]);
         }
     }
 }
