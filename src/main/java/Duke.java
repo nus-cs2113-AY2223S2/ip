51a2b1d57a7ee377d09daef5b2e5d6c637ced8dc
saint thiri zaw <saintstz@gmail.com>
2023-01-31 17:32:28 +0800

    Level-4 updated

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,85 @@
 import java.util.Scanner;
 public class Duke {
-
     public static void printLine() {
         System.out.println("____________________________________________________________");
+    }
+
+    //list tasks
+    public static void listTasks(Task[] list, int count){
+        printLine();
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 1; i <= count; i++) {
+            System.out.println(i + "." + list[i - 1]);
+        }
+        printLine();
+    }
+
+    //add a new task
+    public static void addTask(Task[] list, int count, String command){
+        Task t = new Task(command);
+        list[count] = t;
+        //count++;
+        printLine();
+        System.out.println("added: " + t.description); //new item added
+        printLine();
+    }
+
+    //mark task as done
+    public static void markTask(Task[] list, String[] input, int i){
+        int index = Integer.parseInt(input[i]);
+        Task t = list[index-1];
+        t.markAsDone();
+        printLine();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println(t);
+        printLine();
+    }
+
+    //mark task as not done
+    public static void unmarkTask(Task[] list, String[] input, int i){
+        int index = Integer.parseInt(input[i]);
+        Task t = list[index-1];
+        t.markNotDone();
+        printLine();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println(t);
+        printLine();
+    }
+
+    //to-do
+    public static void addTodo(Task[] list, int count, String[] input, int i){
+        Task t = new Todo(input[i]);
+        list[count] = t;
+        count++;
+        printLine();
+        System.out.println("Got it. I've added this task:");
+        System.out.println(t);
+        System.out.println("Now you have " + count + " tasks in the list.");
+        printLine();
+    }
+
+    //deadline
+    public static void addDeadline(Task[] list, int count, String[] input, int i, int j){
+        Task t = new Deadline(input[i], input[j]);
+        list[count] = t;
+        count++;
+        printLine();
+        System.out.println("Got it. I've added this task:");
+        System.out.println(t);
+        System.out.println("Now you have " + count + " tasks in the list.");
+        printLine();
+    }
+
+    //event
+    public static void addEvent(Task[] list, int count, String[] start, String[] end, int i, int j, int k){
+        Task t = new Event(start[i], end[j], end[k] );
+        list[count] = t;
+        count++;
+        printLine();
+        System.out.println("Got it. I've added this task:");
+        System.out.println(t);
+        System.out.println("Now you have " + count + " tasks in the list.");
+        printLine();
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -27,88 +104,45 @@ public class Duke {
         while (!(command.equals("bye"))) {
             //list tasks
             if (command.equals("list")) {
-                printLine();
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 1; i <= count; i++) {
-                    System.out.println(i + "." + list[i - 1]);
-                }
-                printLine();
-            }
-            else {
+                listTasks(list, count);
+            } else {
                 String[] input = command.split(" ", 2); //only check the first word for "mark" or "unmark"
-
-                //mark task
-                if (input[0].equals("mark")) {
-                    int index = Integer.parseInt(input[1]);
-                    Task t = list[index-1];
-                    t.markAsDone();
-                    printLine();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(t);
-                    printLine();
-                }
-                //un-mark task
-                else if (input[0].equals("unmark")) {
-                    int index = Integer.parseInt(input[1]);
-                    Task t = list[index-1];
-                    t.markNotDone();
-                    printLine();
-                    System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println(t);
-                    printLine();
-                }
-                //todo
-                else if (input[0].equals("todo")) {
-                    Task t = new Todo(input[1]);
-                    list[count] = t;
-                    count++;
-                    printLine();
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(t);
-                    System.out.println("Now you have " + count + " tasks in the list.");
-                    printLine();
-
-                }
-                //deadline
-                else if (input[0].equals("deadline")) {
-                    String[] doBy = input[1].split("by ", 2);
-
-                    Task t = new Deadline(doBy[0], doBy[1]);
-                    list[count] = t;
-                    count++;
-                    printLine();
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(t);
-                    System.out.println("Now you have " + count + " tasks in the list.");
-                    printLine();
-                }
-                //event
-                else if (input[0].equals("event")) {
-                    String[] start = input[1].split("from ", 2);
-                    String[] end = start[1].split("to ", 2);
-
-                    Task t = new Event(start[0], end[0], end[1] );
-                    list[count] = t;
-                    count++;
-                    printLine();
-                    System.out.println("Got it. I've added this task:");
-                    System.out.println(t);
-                    System.out.println("Now you have " + count + " tasks in the list.");
-                    printLine();
-                }
-                //unlabelled
-                else {
-                    Task t = new Task(command);
-                    list[count] = t;
-                    count++;
-                    printLine();
-                    System.out.println("added: " + t.description); //new item added
-                    printLine();
+                switch (input[0]) {
+                    //mark task
+                    case "mark":
+                        markTask(list, input, 1);
+                        break;
+                    //un-mark task
+                    case "unmark":
+                        unmarkTask(list, input, 1);
+                        break;
+                    //to-do
+                    case "todo":
+                        addTodo(list, count, input, 1);
+                        count++;
+                        break;
+                    //deadline
+                    case "deadline":
+                        String[] doBy = input[1].split("by ", 2);
+                        addDeadline(list, count, doBy, 0, 1);
+                        count++;
+                        break;
+                    //event
+                    case "event":
+                        String[] start = input[1].split("from ", 2);
+                        String[] end = start[1].split("to ", 2);
+                        addEvent(list, count, start, end, 0, 0, 1);
+                        count++;
+                        break;
+                    //unlabelled
+                    default:
+                        addTask(list, count, command);
+                        count++;
+                        break;
                 }
             }
             command = in.nextLine(); //read next command
         }
-
         //exit loop when command received is "bye"
         printLine();
         System.out.println("Bye. Hope to see you again soon! :)");
