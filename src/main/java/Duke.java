7b3b00827fc960fb397b2c724768797bd883616b
victorouy <thevictors08@gmail.com>
2023-02-01 18:50:45 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,42 @@
 import java.util.Scanner;
 
 public class Duke {
+
     public static void main(String[] args) {
+        greetings();
+        echoText();
+        goodbye();
+    }
+
+    private static void echoText() {
+        Scanner scanner = new Scanner(System.in);
+        String echoInput = "";
+        String[] storedText = new String[100];
+        int counter = 0;
+
+        while (!echoInput.equals("bye")) {
+            echoInput = scanner.nextLine();
+            if (echoInput.equals("bye")) {
+                break;
+            }
+            if (echoInput.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < counter; i++) {
+                    System.out.println(i+1 + ". " + storedText[i]);
+                }
+                System.out.println("____________________________________________________________");
+            }
+            else {
+                storedText[counter] = echoInput;
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + echoInput);
+                System.out.println("____________________________________________________________");
+                counter = counter + 1;
+            }
+        }
+    }
+
+    private static void greetings() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -13,19 +48,9 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        String echoInput = "";
-
-        while (!echoInput.equals("bye")) {
-            echoInput = scanner.nextLine();
-
-            if (!echoInput.equals("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println(echoInput);
-                System.out.println("____________________________________________________________");
-            }
-        }
+    private static void goodbye() {
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
