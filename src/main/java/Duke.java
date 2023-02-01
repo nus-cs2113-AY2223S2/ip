8d7fd5db788f6bbfc172db3b271424a267383d98
RuiShengGit <seow.ruisheng@gmail.com>
2023-02-01 11:54:59 +0800

    Refactor code to improve code quality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,101 +2,128 @@ import java.util.Scanner;
 
 public class Duke {
 
-    public static String longLine = "---------------------------------------------";
+    public static String LONG_LINE = "--------------------------------------------";
+    public final int MAX_ARRAY_SIZE = 100;
 
-    private static void printList(Task[] s, int count) {
+    private static void printList(Task[] s, int listCount) {
         System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < count; ++i) {
+        for (int i = 0; i < listCount; ++i) {
             String index = Integer.toString(i + 1);
             System.out.println(index + '.' + s[i].toString());
         }
     }
 
-    private static void printCommand(Task[] taskList, int count) {
+    private static void printAddedTaskCommand(Task[] taskList, int listCount) {
         System.out.println("Got it. I've added this task:");
-        System.out.println("  " + taskList[count].toString());
-        System.out.println("Now you have " + Integer.toString(count + 1) + " tasks in the list.");
+        System.out.println("  " + taskList[listCount].toString());
+        System.out.println("Now you have " + Integer.toString(listCount + 1) + " tasks in the list.");
+    }
+
+    private static void printGreetings() {
+        System.out.println(LONG_LINE);
+        System.out.println("Hello I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(LONG_LINE);
+    }
+
+    private static void printExit() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LONG_LINE);
     }
 
     public static void main(String[] args) {
-        System.out.println(longLine);
-        System.out.println("Hello I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println(longLine);
-
-        boolean shouldRun = true;
+        printGreetings();
         Task[] taskList = new Task[100];
-        int count = 0;
-        int index = 0;
-        String line;
-        String task;
-        String input[];
-
-        while (shouldRun) {
-            Scanner in = new Scanner(System.in);
+        String line = "Start";
+        Scanner in = new Scanner(System.in);
+        int listCount = 0;
+        while (!line.equals("bye")) {
             line = in.nextLine();
-            String[] cases = line.split(" ", 2);
-            System.out.println(longLine);
-            switch (cases[0]) {
-            case "todo":
-                taskList[count] = new Todo(cases[1]);
-                printCommand(taskList, count);
-                count++;
-                break;
-            case "deadline":
-                input = cases[1].split("/", 2);
-                task = input[0].trim();
-                String by = input[1].substring(3);
-                taskList[count] = new Deadline(task, by);
-                printCommand(taskList, count);
-                count++;
-                break;
-            case "event":
-                input = cases[1].split("/", 3);
-                task = input[0].trim();
-                String from = input[1].substring(5).trim();
-                String to = input[2].substring(3);
-                taskList[count] = new Event(task, from, to);
-                printCommand(taskList, count);
-                count++;
-                break;
-            case "list":
-                printList(taskList, count);
-                break;
-            case "mark":
-                index = Integer.parseInt(cases[1]) - 1;
-                if (taskList[index] == null) {
-                    System.out.println("No such task! Try again.");
-                    break;
-                }
-                taskList[index].setDone(true);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(taskList[index].toString());
-                break;
-            case "unmark":
-                index = Integer.parseInt(cases[1]) - 1;
-                if (taskList[index] == null) {
-                    System.out.println("No such task! Try again.");
-                    break;
-                }
-                taskList[index].setDone(false);
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(taskList[index].toString());
-                break;
-            case "bye":
-                shouldRun = false;
-                break;
-            default:
-                taskList[count] = new Task(line);
-                count++;
-                System.out.println("added: " + line);
+            if (!line.equals("bye")) {
+                listCount = handleUserInputs(line, taskList, listCount);
             }
-            if (shouldRun) {
-                System.out.println(longLine);
-            }
+            System.out.println(LONG_LINE);
         }
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(longLine);
+        printExit();
         return;
+    }
+
+    private static void createTodo(Task[] taskList, int listCount, String input) {
+        taskList[listCount] = new Todo(input);
+        printAddedTaskCommand(taskList, listCount);
+    }
+
+    private static void createDeadline(Task[] taskList, int listCount, String input){
+        String[] splitInput = input.split("/", 2);
+        String task = splitInput[0].trim();
+        String by = splitInput[1].substring(3);
+        taskList[listCount] = new Deadline(task, by);
+        printAddedTaskCommand(taskList, listCount);
+    }
+
+    private static void createEvent(Task[] taskList, int listCount, String input){
+        String[] splitInput = input.split("/", 3);
+        String task = splitInput[0].trim();
+        String from = splitInput[1].substring(5).trim();
+        String to = splitInput[2].substring(3);
+        taskList[listCount] = new Event(task, from, to);
+        printAddedTaskCommand(taskList, listCount);
+    }
+
+    private static void markTask(Task[] taskList, String input){
+        int index = Integer.parseInt(input) - 1;
+        if (taskList[index] == null) {
+            System.out.println("No such task! Try again.");
+        }
+        else {
+            taskList[index].setDone(true);
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(taskList[index].toString());
+        }
+    }
+
+    private static void unmarkTask(Task[] taskList, String input){
+        int index = Integer.parseInt(input) - 1;
+        if (taskList[index] == null) {
+            System.out.println("No such task! Try again.");
+        }
+        else {
+            taskList[index].setDone(false);
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println(taskList[index].toString());
+        }
+    }
+
+    public static int handleUserInputs(String line, Task[] taskList, int listCount) {
+        String[] cases = line.split(" ", 2);
+        System.out.println(LONG_LINE);
+        switch (cases[0]) {
+        case "todo":
+            createTodo(taskList,listCount, cases[1]);
+            listCount++;
+            break;
+        case "deadline":
+            createDeadline(taskList, listCount, cases[1]);
+            listCount++;
+            break;
+        case "event":
+            createEvent(taskList, listCount, cases[1]);
+            listCount++;
+            break;
+        case "list":
+            printList(taskList, listCount);
+            break;
+        case "mark":
+            markTask(taskList, cases[1]);
+            break;
+        case "unmark":
+            unmarkTask(taskList, cases[1]);
+            break;
+        default:
+            taskList[listCount] = new Task(line);
+            listCount++;
+            System.out.println("added: " + line);
+        }
+        return listCount;
     }
 }
