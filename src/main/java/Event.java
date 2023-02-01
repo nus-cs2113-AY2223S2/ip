7e758c3a91d41d43006c379c502de1ceb83a4e1f
waiter-palypoo <gohjinghong2000@gmail.com>
2023-02-01 18:57:03 +0800

    Add support for todo, deadline and event task types.

---
+++ src/main/java/Event.java
@@ -0,0 +1,20 @@
+public class Event extends Task {
+    private String from;
+    private String to;
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    public String toString() {
+        String mark;
+        if (super.isDone()) {
+            mark = "X";
+        }
+        else {
+            mark = " ";
+        }
+        return String.format("[D][%s] %s(from: %s to: %s)", mark, super.getDescription(), from, to);
+    }
+}
