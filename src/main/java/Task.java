0fc7b01103d8626a9929e90fd51e20560544496f
EthanYidong <ethanyidong@gmail.com>
2023-02-01 23:38:55 +0800

    Refractor to move "done" functionality to the Task class.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,8 +1,10 @@
 public abstract class Task {
     private String name;
+    private boolean done;
 
     public Task(String name) {
         this.name = name;
+        this.done = false;
     }
 
     public String getName() {
@@ -13,6 +15,22 @@ public abstract class Task {
         this.name = name;
     }
 
+    public void markAsDone() {
+        this.done = true;
+    }
+
+    public void markAsNotDone() {
+        this.done = false;
+    }
+
+    protected abstract String label();
+
     @Override
-    public abstract String toString();
+    public String toString() {
+        char marker = ' ';
+        if (this.done) {
+            marker = 'X';
+        }
+        return "[" + this.label() + "][" + marker + "] " + this.getName();
+    }
 }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,25 +1,9 @@
 public class Todo extends Task {
-    private boolean done;
-
     public Todo(String name) {
         super(name);
-        this.done = false;
     }
 
-    public void markAsDone() {
-        this.done = true;
-    }
-
-    public void markAsNotDone() {
-        this.done = false;
-    }
-
-    @Override
-    public String toString() {
-        char marker = ' ';
-        if (this.done) {
-            marker = 'X';
-        }
-        return "[" + marker + "] " + this.getName();
+    protected String label() {
+        return "T";
     }
 }
