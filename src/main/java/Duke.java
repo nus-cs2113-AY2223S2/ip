7558b46825723cabfb327deef903da61b2f10fd7
TzeLoong <tzeloong12@gmail.com>
2023-02-01 13:37:43 +0800

    Level 0 Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,9 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke \n");
+        System.out.println("How can i help u? \n");
+        System.out.println("Goodbye. Hope to see u again :) \n");
+
     }
 }
