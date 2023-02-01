57d0c6a39a72a97984ff5c6392471ab2a7c9f259
Sebastian Soewanto <sebastiansoewanto@outlook.com>
2023-02-01 09:58:41 +0800

    Add mark to list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,10 +8,12 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
 
-        String line = "_________________________________\n";
+        String line = "____________________________________________________\n";
         System.out.println("Hello from\n" + logo);
         System.out.print(line + "Hello! I'm Duke\n" + "What can I do for you?\n" + line);
         String[] list = new String[100];
+        boolean[] isDone = new boolean[100];
+        String toFill;
         int listCount = 0;
         while(true) {
             Scanner userInput = new Scanner(System.in);
@@ -19,10 +21,26 @@ public class Duke {
             if (input.equals("bye")) {
                 System.out.print(line + "Bye. Hope to see you again soon!\n" + line);
                 break;
-            } else if(input.equals("list")) {
+            } else if (input.startsWith("mark")) {
+                int mark = Integer.parseInt(input.substring(5));
+                isDone[mark - 1] = true;
+                System.out.println(line + "Nice! I've marked this task as done:\n" + "  [X] " + list[mark - 1]);
                 System.out.print(line);
+            } else if (input.startsWith("unmark")) {
+                int unmark = Integer.parseInt(input.substring(7));
+                isDone[unmark - 1] = false;
+                System.out.println(line + "OK, I've marked this task as not done yet:\n" + "  [ ] "+ list[unmark - 1]);
+                System.out.print(line);
+            } else if(input.equals("list")) {
+                System.out.print(line + "Here are the tasks in your list:\n");
                 for (int i = 0; i < listCount; i++) {
-                    System.out.println(i + 1 + ". " + list[i]);
+                    if (isDone[i]) {
+                        toFill = "X";
+                    }
+                    else {
+                        toFill = " ";
+                    }
+                    System.out.println(i + 1 + ". [" + toFill + "] " + list[i]);
                 }
                 System.out.print(line);
             } else {
