08927aeb0610b62738511e2763b748a8e0700095
Ju-Can <ju.can.0860@gmail.com>
2023-02-02 00:32:36 +0800

    Improved code quality and minor implementation improvement
    error message added when markTask() and unmarkTask() user input index goes out of bound

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -3,13 +3,13 @@ public class Deadline extends Task{
         super(description);
     }
 
-    public String getDdl() {
+    public String getDeadline() {
         return description.substring(description.indexOf('/') + 4);
     }
 
     @Override
     public String printTask() {
-        return "[D]" + super.printTask().substring(0, super.printTask().indexOf('/')) + "(by: " + getDdl() + ") \n";
+        return "[D]" + super.printTask().substring(0, super.printTask().indexOf('/')) + "(by: " + getDeadline() + ") \n";
     }
 
 }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static String line = "____________________________________________________________\n";
+    public static int countTask = 0;
+    public static Task[] tasks = new Task[100];
+
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -8,7 +12,7 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
-        String line = "____________________________________________________________\n";
+
         String greet = line +
                 "Hello! I'm Duke.\n" +
                 "To add general tasks to the todo list, simply type in the task and press enter.\n" +
@@ -19,59 +23,96 @@ public class Duke {
                 "You can also add events to the list by typing 'event' " +
                 "followed by task description + '/from' + starting time of event + '/to' ending time of event.\n" +
                 "Type 'list' to view the current todo list.\n" +
-                "Type 'mark' and task event index to mark the task as done and type 'unmark'and index to undo the task.\n" +
+                "Type 'mark' and task event index to mark the task as done " +
+                "and type 'unmark' and index to undo the task.\n" +
                 line;
         System.out.println(greet);
 
         Scanner in = new Scanner(System.in);
-        int countTask = 0;
-        Task[] tasks = new Task[100];
+
         while (true) {
             String userInput = in.nextLine().trim();
             if (userInput.equals("bye")) {
                 System.out.println("Thank you for using Duke. Hope to see you soon!");
                 break;
             } else if (userInput.equals("list")) {
-                System.out.println(line + "Here are the tasks in your list:");
-                for (int i = 0; i < countTask; i++) {
-                    int taskIndex = i + 1;
-                    System.out.print(taskIndex + ". " + tasks[i].printTask());
-                }
-                System.out.println(line);
+                printTaskList();
             } else if (userInput.startsWith("mark")) {
-                int taskIndex = Integer.parseInt(userInput.substring(4).trim());
-                tasks[taskIndex - 1].markAsDone();
-                System.out.println(line + "Task " + taskIndex + " marked as done: \n" +
-                        tasks[taskIndex - 1].printTask() + line);
+                markTask(userInput);
             } else if (userInput.startsWith("unmark")) {
-                int taskIndex = Integer.parseInt(userInput.substring(6).trim());
-                tasks[taskIndex - 1].markAsUndone();
-                System.out.println(line + "Task " + taskIndex + " marked as not done yet: \n" +
-                        tasks[taskIndex - 1].printTask() + line);
+                unmarkTask(userInput);
             } else if (userInput.startsWith("todo")) {
-                ToDo todoTask = new ToDo(userInput.substring(5));
-                tasks[countTask] = todoTask;
-                countTask++;
-                System.out.println(line + "Great! I've added this task:\n" + "   " + todoTask.printTask());
-                System.out.println("Now you have " + countTask + " task(s) in the list.\n" + line);
+                createTodo(userInput);
             } else if (userInput.startsWith("deadline")) {
-                Deadline deadlineTask = new Deadline(userInput.substring(9));
-                tasks[countTask] = deadlineTask;
-                countTask++;
-                System.out.println(line + "Great! I've added this task:\n" + "   " + deadlineTask.printTask());
-                System.out.println("Now you have " + countTask + " task(s) in the list.\n" + line);
+                createDeadline(userInput);
             } else if (userInput.startsWith("event")) {
-                Event eventTask = new Event(userInput.substring(9));
-                tasks[countTask] = eventTask;
-                countTask++;
-                System.out.println(line + "Great! I've added this task:\n" + "   " + eventTask.printTask());
-                System.out.println("Now you have " + countTask + " task(s) in the list.\n" + line);
+                createEvent(userInput);
             } else {
-                Task t = new Task(userInput);
-                tasks[countTask] = t;
-                countTask++;
-                System.out.println(line + "added: " + t.getDescription() + '\n' + line);
+                createTask(userInput);
             }
         }
     }
+
+    public static void printTaskList() {
+        System.out.println(line + "Here are the tasks in your list:");
+        for (int i = 0; i < countTask; i++) {
+            int taskIndex = i + 1;
+            System.out.print(taskIndex + ". " + tasks[i].printTask());
+        }
+        System.out.println(line);
+    }
+
+    public static void markTask(String input) {
+        int taskIndex = Integer.parseInt(input.substring(4).trim());
+        if (taskIndex >= 1 && taskIndex <= countTask) {
+            tasks[taskIndex - 1].markAsDone();
+            System.out.println(line + "Task " + taskIndex + " marked as done: \n" +
+                    tasks[taskIndex - 1].printTask() + line);
+        } else {
+            System.out.println("Task " + taskIndex + " not found. Please try again.");
+        }
+    }
+
+    public static void unmarkTask(String input) {
+        int taskIndex = Integer.parseInt(input.substring(6).trim());
+        if (taskIndex >= 1 && taskIndex <= countTask) {
+            tasks[taskIndex - 1].markAsUndone();
+            System.out.println(line + "Task " + taskIndex + " marked as not done yet: \n" +
+                    tasks[taskIndex - 1].printTask() + line);
+        } else {
+            System.out.println("Task " + taskIndex + " not found. Please try again.");
+        }
+    }
+
+    public static void createTask(String input) {
+        Task t = new Task(input);
+        tasks[countTask] = t;
+        countTask++;
+        System.out.println(line + "added: " + t.getDescription() + '\n' + line);
+    }
+
+    public static void createTodo(String input) {
+        ToDo todoTask = new ToDo(input.substring(5));
+        tasks[countTask] = todoTask;
+        countTask++;
+        System.out.println(line + "Great! I've added this task:\n" + "   " + todoTask.printTask());
+        System.out.println("Now you have " + countTask + " task(s) in the list.\n" + line);
+    }
+
+    public static void createDeadline(String input) {
+        Deadline deadlineTask = new Deadline(input.substring(9));
+        tasks[countTask] = deadlineTask;
+        countTask++;
+        System.out.println(line + "Great! I've added this task:\n" + "   " + deadlineTask.printTask());
+        System.out.println("Now you have " + countTask + " task(s) in the list.\n" + line);
+    }
+
+    public static void createEvent(String input) {
+        Event eventTask = new Event(input.substring(9));
+        tasks[countTask] = eventTask;
+        countTask++;
+        System.out.println(line + "Great! I've added this task:\n" + "   " + eventTask.printTask());
+        System.out.println("Now you have " + countTask + " task(s) in the list.\n" + line);
+    }
 }
+
