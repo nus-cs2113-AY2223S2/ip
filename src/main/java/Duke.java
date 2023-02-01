fc5b4ed35581aa1d0031d372a9b64ccd9af1b040
aaronxujiachen <1943999635x@gmail.com>
2023-02-01 23:51:54 +0800

    My level 0 Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,11 @@
 public class Duke {
+
+    public static void drawLine() {
+        for (int i = 0; i < 20; i++) {
+            System.out.print("_");
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +13,16 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        drawLine();
+        System.out.println();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        drawLine();
+        System.out.println();
+        System.out.println("Bye. Hope to see you again soon!");
+        drawLine();
+
     }
+
 }
