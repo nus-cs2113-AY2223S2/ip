a96baa9bd23de0c6c703407290e41862b06534d3
NivethaUdayakumar <nivethaus4@gmail.com>
2023-02-01 19:25:20 +0800

    Implementing Automated Text UI Testing

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,9 +11,13 @@ public class Duke {
 
         while (true) {
             line = in.nextLine();
+            System.out.println("____________________________________________________________");
             if ("list".equalsIgnoreCase(line)) {
                 printAllTasks(tasks, taskCount);
-            } else if ((line.substring(0,4)).equalsIgnoreCase("mark")) {
+            }  else if ("bye".equalsIgnoreCase(line)) {
+                printByeMessage();
+                break;
+            }else if ((line.substring(0,4)).equalsIgnoreCase("mark")) {
                 markTaskAsComplete(line, tasks);
             } else if ((line.substring(0,6)).equalsIgnoreCase("unmark")) {
                 markTaskAsNotComplete(line, tasks);
@@ -37,10 +41,8 @@ public class Duke {
                 tasks[taskCount] = new Event(event, eventStart, eventEnd);
                 taskCount++;
                 printTaskAddedDescription(tasks, taskCount);
-            } else if ("bye".equalsIgnoreCase(line)) {
-                printByeMessage();
-                break;
             }
+            System.out.println("____________________________________________________________");
         }
     }
 
@@ -50,9 +52,7 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-
         System.out.println("Hello from\n" + logo);
-
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Duke, your personal task manager.");
         System.out.println(" What can I do for you?");
@@ -60,44 +60,35 @@ public class Duke {
     }
 
     private static void printAllTasks(Task[] tasks, int taskCount) {
-        System.out.println("____________________________________________________________");
         System.out.println(" Here are the tasks in your list:");
         int index = 1;
         for (int i = 0; i < taskCount; i++) {
             System.out.println("  " + index + "." + tasks[i].toString()); //print Task Description
             index++;
         }
-        System.out.println("____________________________________________________________");
     }
 
     private static void markTaskAsNotComplete(String line, Task[] tasks) {
-        System.out.println("____________________________________________________________");
         System.out.println(" Okay, I've marked this task as not done yet:");
         int index = Integer.parseInt(line.substring(7));
         tasks[index-1].markAsNotDone();
         System.out.println(tasks[index-1].toString()); //print Task Description
-        System.out.println("____________________________________________________________");
     }
 
     private static void markTaskAsComplete(String line, Task[] tasks) {
-        System.out.println("____________________________________________________________");
         System.out.println(" Nice! I've marked this task as done:");
         int index = Integer.parseInt(line.substring(5));
         tasks[index-1].markAsDone();
         System.out.println(tasks[index-1].toString()); //print Task Description
-        System.out.println("____________________________________________________________");
     }
 
     private static void printTaskAddedDescription(Task[] tasks, int taskCount) {
-        System.out.println("____________________________________________________________");
         System.out.println(" Got it. I've added this task:");
         System.out.println(tasks[taskCount-1].toString()); //print Task Description
         System.out.println(" Now you have " + taskCount + " tasks in your list.");
-        System.out.println("____________________________________________________________");
     }
 
     private static void printByeMessage() {
-        System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
