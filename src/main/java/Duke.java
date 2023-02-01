c1d6e71f6431989361594e5b3ccd215937396f2e
APANA49 <aungphonenaing57@gmail.com>
2023-02-01 14:43:03 +0800

    Add list and add functions along with spacings.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    public static void printList(List<String> l1) {
+        int index;
+        for (int i = 0; i < l1.size(); i += 1) {
+            index = i + 1;
+            System.out.println(index + ". " + l1.get(i));
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,13 +20,24 @@ public class Duke {
         System.out.println("Hello from\n" + logo);*/
         System.out.println("Hello! I'm a Robot");
         System.out.println("What can I do for you?\n");
+        List<String> list = new ArrayList<String>();
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
         while(!line.equals("bye")){
-            System.out.println(line);
+            if (line.equals("list")) {
+                if (!list.isEmpty()){
+                    System.out.println();
+                    printList(list);
+                } else {
+                    System.out.println('\n' + "List is empty!" + '\n');
+                }
+            } else {
+                System.out.println('\n' + "added: " + line + '\n');
+                list.add(line);
+            }
             line = in.nextLine();
         }
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println('\n' + "Bye. Hope to see you again soon!");
     }
 }
