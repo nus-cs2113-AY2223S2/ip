03e770e9bda521fd2748713fb1bdf608e2fded2b
NgYJ02 <91600220+NgYJ02@users.noreply.github.com>
2023-02-02 12:08:55 +0800

    A-CodeQuality

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -8,7 +8,7 @@ public class Deadline extends Task {
 
     @Override
     public String toString(){
-        return "[D]"+super.toString()+" (by: "+this.deadlineDay+")";
+        return "[D]"+super.toString()+" ("+this.deadlineDay+")";
     }
 
 }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,63 +12,62 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
         ArrayList<Task> tasks = new ArrayList<Task>();
-        Scanner scan = new Scanner(System.in);
-        String desc = scan.nextLine();
-        String output = new String();
-        while (!desc.equals("bye")) {
-            if (desc.equals("list")) {
-                int a = 1;
-                output="Here are the tasks in the list:"+System.lineSeparator()+Task.printTasksList(tasks);
-                System.out.println("Here are the tasks in the list:");
-                for (Task i : tasks) {
-                    System.out.println(a + ". " + i.toString());
-                    a++;
+        try (Scanner scan = new Scanner(System.in)) {
+            String input = scan.nextLine();
+            String output = new String();
+            while (!input.equals("bye")) {
+                if (input.equals("list")) {
+                    output = "Here are the tasks in the list:" + System.lineSeparator() + Task.printTasksList(tasks);
+
+                } else if (input.matches("mark [0-9]{1,2}")) {
+                    String[] marks = input.split(" ");
+                    tasks.get(Integer.parseInt(marks[1]) - 1).markAsDone();
+                    output = "Nice! I've marked this task as done:" + System.lineSeparator()
+                            + tasks.get(Integer.parseInt(marks[1]) - 1).toString();
+                } else if (input.matches("unmark [0-9]{1,2}")) {
+                    String[] marks = input.split(" ");
+                    tasks.get(Integer.parseInt(marks[1]) - 1).markAsNotDone();
+                    output = "OK, I've marked this task as not done yet:" + System.lineSeparator()
+                            + tasks.get(Integer.parseInt(marks[1]) - 1).toString();
+                } else if (input.startsWith("todo", 0)) {
+                    String toDoDesc = input.split("todo")[1].trim();
+                    Task toDo = new Todo(toDoDesc);
+                    tasks.add(toDo);
+                    output = "Got it. I've added this task:" + System.lineSeparator()
+                            + toDo.toString() + System.lineSeparator() + "Now you have " + Task.numberOfTasks
+                            + " in the list.";
+                } else if (input.startsWith("deadline", 0)) {
+                    String deadlineDesc = input.split("/")[0].split("deadline")[1].trim();
+                    String deadlineDay = input.split("/")[1].trim();
+                    Task deadline = new Deadline(deadlineDesc, deadlineDay);
+                    tasks.add(deadline);
+                    output = "Got it. I've added this task:" + System.lineSeparator()
+                            + deadline.toString() + System.lineSeparator() + "Now you have " + Task.numberOfTasks
+                            + " in the list.";
+                } else if (input.startsWith("event", 0)) {
+                    String eventDesc = input.split("/")[0].split("event")[1].trim();
+                    String start = input.split("/")[1].trim();
+                    String end = input.split("/")[2].trim();
+                    Task event = new Event(eventDesc, start, end);
+                    tasks.add(event);
+                    output = "Got it. I've added this task:" + System.lineSeparator()
+                            + event.toString() + System.lineSeparator() + "Now you have " + Task.numberOfTasks
+                            + " in the list.";
+                } else {
+                    Task task = new Task(input);
+                    tasks.add(task);
+                    output = "Got it. I've added this task:" + System.lineSeparator()
+                            + task.toString() + System.lineSeparator() + "Now you have " + Task.numberOfTasks
+                            + " in the list.";
                 }
-            } else if (desc.matches("mark [0-9]{1,2}")) {
-                String[] marks = desc.split(" ");
-                tasks.get(Integer.parseInt(marks[1]) - 1).markAsDone();
-                output = "Nice! I've marked this task as done:" + System.lineSeparator()
-                        + tasks.get(Integer.parseInt(marks[1]) - 1).toString();
-            } else if (desc.matches("unmark [0-9]{1,2}")) {
-                String[] marks = desc.split(" ");
-                tasks.get(Integer.parseInt(marks[1]) - 1).markAsNotDone();
-                output = "OK, I've marked this task as not done yet:" + System.lineSeparator()
-                        + tasks.get(Integer.parseInt(marks[1]) - 1).toString();
-            } else if (desc.startsWith("todo", 0)) {
-                String toDoDesc = desc.split("todo")[1].trim();
-                Task toDo = new Todo(toDoDesc);
-                tasks.add(toDo);
-                output = "Got it. I've added this task:" + System.lineSeparator()
-                        + toDo.toString() + System.lineSeparator() + "Now you have " + Task.numberOfTasks
-                        + " in the list.";
-            } else if (desc.startsWith("deadline", 0)) {
-                String deadlineDesc = desc.split("/")[0].split("deadline")[1].trim();
-                String deadlineDay = desc.split("/")[1].trim();
-                Task deadline = new Deadline(deadlineDesc, deadlineDay);
-                tasks.add(deadline);
-                output = "Got it. I've added this task:" + System.lineSeparator()
-                        + deadline.toString() + System.lineSeparator() + "Now you have " + Task.numberOfTasks
-                        + " in the list.";
-            } else if (desc.startsWith("event", 0)) {
-                String eventDesc = desc.split("/")[0].split("event")[1].trim();
-                String start = desc.split("/")[1].trim();
-                String end = desc.split("/")[2].trim();
-                Task event = new Event(eventDesc, start, end);
-                tasks.add(event);
-                output = "Got it. I've added this task:" + System.lineSeparator()
-                        + event.toString() + System.lineSeparator() + "Now you have " + Task.numberOfTasks
-                        + " in the list.";
-            } else {
-                Task task = new Task(desc);
-                tasks.add(task);
-                output = "Got it. I've added this task:" + System.lineSeparator()
-                        + task.toString() + System.lineSeparator() + "Now you have " + Task.numberOfTasks
-                        + " in the list.";
+                System.out.println("___________________________________________________");
+                System.out.println(output);
+                System.out.println("___________________________________________________");
+                input = scan.nextLine();
             }
-            System.out.println("___________________________________________________");
-            System.out.println(output);
-            System.out.println("___________________________________________________");
-            desc = scan.nextLine();
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
         System.out.println("Bye. Hope to see you again soon!");
 
