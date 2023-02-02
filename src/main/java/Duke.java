771d2a335f4bd64727f35fc1d521c4f2f837c1d3
Jin Xuan Ow Yong <owyongjinxuan@gmail.com>
2023-02-02 17:18:39 +0800

    Improve code readability for command and status handling
    Replaced if-else statements with switch-case statements.
    Refactored command and status handling into methods using the
    handleCommand<command> or handleStatus<status> rule.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -45,12 +45,7 @@ public class Duke {
 
     private static void greet() {
         printWithIndentation("Hello! I'm Duke\n"
-                + "What can I do for you?");
-        printLine();
-    }
-
-    private static void exit() {
-        printWithIndentation("Bye. How to see you again soon!");
+                                     + "What can I do for you?");
         printLine();
     }
 
@@ -59,14 +54,6 @@ public class Duke {
         ++numTasks;
         printWithIndentation("added: " + taskObj.getName());
         printWithIndentation("Now you have " + numTasks + " tasks in the list");
-        printLine();
-    }
-
-    private static void listTasks() {
-        for (int i = 0; i < numTasks; ++i) {
-            printWithIndentation((i + 1) + "." // number
-                    + tasks[i].describe());
-        }
         printLine();
     }
 
@@ -81,6 +68,85 @@ public class Duke {
         printLine();
     }
 
+    private static String getTaskDetails(Scanner input) {
+        // validate input
+        if (!input.hasNextLine()) {
+            return "";
+        }
+        String taskDetails = input.nextLine().trim();
+        if (!ToDo.isValidInput(taskDetails)) {
+            return "";
+        }
+        return taskDetails;
+    }
+
+    private static void handleStateExit() {
+        printWithIndentation("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    private static void handleStateInvalid() {
+        printWithIndentation("Invalid input, try again.");
+        printLine();
+    }
+
+    private static State handleCommandList() {
+        for (int i = 0; i < numTasks; ++i) {
+            printWithIndentation((i + 1) + "." // number
+                                         + tasks[i].describe());
+        }
+        printLine();
+        return State.SUCCESS;
+    }
+
+    private static State handleCommandMark(Scanner input) {
+        int number = input.nextInt();
+        setTaskStatus(number - 1, true);
+        return State.SUCCESS;
+    }
+
+    private static State handleCommandUnmark(Scanner input) {
+        int number = input.nextInt();
+        setTaskStatus(number - 1, false);
+        return State.SUCCESS;
+    }
+
+    private static State handleCommandTodo(Scanner input) {
+        String taskDetails = getTaskDetails(input);
+        if (taskDetails.equals("")) {
+            return State.INVALID;
+        }
+
+        // create task
+        ArrayList<String> detailsArr = ToDo.convertInputIntoDetails(taskDetails);
+        addTask(new ToDo(detailsArr));
+        return State.SUCCESS;
+    }
+
+    private static State handleCommandDeadline(Scanner input) {
+        String taskDetails = getTaskDetails(input);
+        if (taskDetails.equals("")) {
+            return State.INVALID;
+        }
+
+        // create task
+        ArrayList<String> detailsArr = Deadline.convertInputIntoDetails(taskDetails);
+        addTask(new Deadline(detailsArr));
+        return State.SUCCESS;
+    }
+
+    private static State handleCommandEvent(Scanner input) {
+        String taskDetails = getTaskDetails(input);
+        if (taskDetails.equals("")) {
+            return State.INVALID;
+        }
+
+        // create task
+        ArrayList<String> detailsArr = Event.convertInputIntoDetails(taskDetails);
+        addTask(new Event(detailsArr));
+        return State.SUCCESS;
+    }
+
     private static State processInput(String s) {
         Scanner input = new Scanner(s);
         String command = input.next().toLowerCase();
@@ -88,61 +154,35 @@ public class Duke {
             return State.EXIT;
         }
 
-        if (command.equals(COMMAND_LIST)) {
-            listTasks();
-        } else if (command.equals(COMMAND_MARK)) { // mark task
-            int number = input.nextInt();
-            setTaskStatus(number - 1, true);
-        } else if (command.equals(COMMAND_UNMARK)) { // unmark task
-            int number = input.nextInt();
-            setTaskStatus(number - 1, false);
-        } else if (command.equals(COMMAND_TODO)) { // add to do
-            // TODO: reduce code repetition
-            // validate input
-            if (!input.hasNextLine()) {
-                return State.INVALID;
-            }
-            String taskDetails = input.nextLine().trim();
-            if (!ToDo.isValidInput(taskDetails)) {
-                return State.INVALID;
-            }
-
-            // create task
-            ArrayList<String> detailsArr = ToDo.convertInputIntoDetails(taskDetails);
-            addTask(new ToDo(detailsArr));
-        } else if (command.equals(COMMAND_DEADLINE)) { // add deadline
-            // validate input
-            if (!input.hasNextLine()) {
-                return State.INVALID;
-            }
-            String taskDetails = input.nextLine().trim();
-            if (!Deadline.isValidInput(taskDetails)) {
-                return State.INVALID;
-            }
-
-            // create task
-            ArrayList<String> detailsArr = Deadline.convertInputIntoDetails(taskDetails);
-            addTask(new Deadline(detailsArr));
-        } else if (command.equals(COMMAND_EVENT)) { // add event
-            // validate input
-            if (!input.hasNextLine()) {
-                return State.INVALID;
-            }
-            String taskDetails = input.nextLine().trim();
-            if (!Event.isValidInput(taskDetails)) {
-                return State.INVALID;
-            }
-
-            // create task
-            ArrayList<String> detailsArr = Event.convertInputIntoDetails(taskDetails);
-            addTask(new Event(detailsArr));
-        } else if (ToDo.isValidInput(s)) {
-            addTask(new Task(s));
-        } else {
-            printWithIndentation("Unrecognised input, try again.");
+        State finalState;
+        switch (command) {
+        case COMMAND_EXIT:
+            finalState = State.EXIT;
+            break;
+        case COMMAND_LIST:
+            finalState = handleCommandList();
+            break;
+        case COMMAND_MARK:
+            finalState = handleCommandMark(input);
+            break;
+        case COMMAND_UNMARK:
+            finalState = handleCommandUnmark(input);
+            break;
+        case COMMAND_TODO:
+            finalState = handleCommandTodo(input);
+            break;
+        case COMMAND_DEADLINE:
+            finalState = handleCommandDeadline(input);
+            break;
+        case COMMAND_EVENT:
+            finalState = handleCommandEvent(input);
+            break;
+        default:
+            printWithIndentation("Unrecognised command, try again.");
+            finalState = State.INVALID;
         }
 
-        return State.SUCCESS;
+        return finalState;
     }
 
     public static void main(String[] args) {
@@ -151,19 +191,21 @@ public class Duke {
         greet();
 
         String input;
+        boolean programRunning = true;
         do {
             input = scan.nextLine();
             printLine();
             State result = processInput(input);
-            if (result == State.EXIT) {
-                exit();
+            switch (result) {
+            case EXIT:
+                handleStateExit();
+                programRunning = false;
+                break;
+            case INVALID:
+                handleStateInvalid();
                 break;
             }
-            if (result == State.INVALID) {
-                printWithIndentation("Invalid input, try again.");
-                printLine();
-            }
-        } while (true);
+        } while (programRunning);
 
         scan.close();
     }
