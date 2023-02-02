4dc9cbec1f926ae0cb28f4b039f1525aa7677e9b
0nandon <tommy9905@gmail.com>
2023-02-02 23:42:13 +0800

    iP : A-CodeQuality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,16 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static void printFormAbove(){
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t Got it. I've added this task:");
+    }
+
+    public static void printCnt(int cnt){
+        System.out.println("\t Now you have " + cnt + " tasks in the list.");
+        System.out.println("\t____________________________________________________________");
+    }
+
     public static void main(String[] args) {
         Task[] tasks = new Task[100];
 
@@ -18,31 +28,25 @@ public class Duke {
 
             switch(tgt[0]){
             case "todo":
-                System.out.println("\t____________________________________________________________");
-                System.out.println("\t Got it. I've added this task:");
+                printFormAbove();
                 tasks[cnt] = new Todo(tgt[1]);
                 System.out.println("\t\t" + tasks[cnt].toString());
                 cnt += 1;
-                System.out.println("\t Now you have " + cnt + " tasks in the list.");
-                System.out.println("\t____________________________________________________________");
+                printCnt(cnt);
                 break;
             case "deadline":
-                System.out.println("\t____________________________________________________________");
-                System.out.println("\t Got it. I've added this task:");
+                printFormAbove();
                 tasks[cnt] = new Deadline(tgt[1], tgt[2]);
                 System.out.println("\t\t" + tasks[cnt].toString());
                 cnt += 1;
-                System.out.println("\t Now you have " + cnt + " tasks in the list.");
-                System.out.println("\t____________________________________________________________");
+                printCnt(cnt);
                 break;
             case "event":
-                System.out.println("\t____________________________________________________________");
-                System.out.println("\t Got it. I've added this task:");
+                printFormAbove();
                 tasks[cnt] = new Event(tgt[1], tgt[2], tgt[3]);
                 System.out.println("\t\t" + tasks[cnt].toString());
                 cnt += 1;
-                System.out.println("\t Now you have " + cnt + " tasks in the list.");
-                System.out.println("\t____________________________________________________________");
+                printCnt(cnt);
                 break;
             case "list":
                 System.out.println("\t____________________________________________________________");
@@ -52,28 +56,6 @@ public class Duke {
                 System.out.println("\t____________________________________________________________\n");
                 break;
             }
-
-            /*
-            tgt = Integer.parseInt(input_text.replaceAll("[^0-9]",""));
-            if(input_text.contains("un")) {
-                t[tgt].markAsUnDone();
-                mark = false;
-            }else if(input_text.contains("mark")) {
-                t[tgt].markAsDone();
-                mark = true;
-            }else {
-                System.out.println("unsupported command!");
-                continue;
-            }
-
-            System.out.println("\t____________________________________________________________");
-            if(mark)
-                System.out.println("\tNice! I've marked this task as done:");
-            else
-                System.out.println("\tOK, I've marked this task as not done yet:");
-            System.out.println("\t\t[" + t[tgt].getStatusIcon() + "] " + t[tgt].getDescription());
-            System.out.println("\t____________________________________________________________\n");
-            */
         }
         in.close();
     }
