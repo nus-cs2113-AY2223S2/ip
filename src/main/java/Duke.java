0152dc9ef22256e513ac00bd6fb7c74230903fdf
JeraldChen <jeraldchenjunhan@gmail.com>
2023-02-02 21:23:33 +0800

    Add error checks for invalid inputs by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,16 @@ public class Duke {
     public static void printLine() {
         System.out.println("______________________________________________________________");
     }
+    private static void checkEmpty(String lowerCaseLine) throws DukeException {
+        if (lowerCaseLine.equals("todo") || lowerCaseLine.equals("deadline") || lowerCaseLine.equals("event")) {
+            throw new DukeException("☹ OOPS!!! The description of a " + lowerCaseLine + " cannot be empty.");
+        }
+    }
+    private static void checkCommand(String lowerCaseLine) throws DukeException {
+        if (!lowerCaseLine.equals("list") && !lowerCaseLine.equals("bye") && !lowerCaseLine.contains("mark") && !lowerCaseLine.contains("unmark") && !lowerCaseLine.contains("todo") && !lowerCaseLine.contains("deadline") && !lowerCaseLine.contains("event")) {
+            throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+        }
+    }
     public static void main(String[] args) {
         String line;
         String lowerCaseLine;
@@ -48,21 +58,60 @@ public class Duke {
             }
             else { //if input is any other text
                 if (lowerCaseLine.contains("todo")) {
-                    task = new Todo(line.substring(5));
+                    try {
+                        checkEmpty(lowerCaseLine);
+                        task = new Todo(line.substring(5));
+                    } catch (DukeException e) {
+                        System.out.println(e.getMessage());
+                        printLine();
+                        continue;
+                    }
                 } else if (lowerCaseLine.contains("deadline")) {
-                    int indexOfBy = line.indexOf("/by");
-                    task = new Deadline(line.substring(9, indexOfBy), line.substring(indexOfBy + 4));
+                    try {
+                        checkEmpty(lowerCaseLine);
+                        int indexOfBy = line.indexOf("/by");
+                        task = new Deadline(line.substring(9, indexOfBy), line.substring(indexOfBy + 4));
+                    } catch (DukeException e) {
+                        System.out.println(e.getMessage());
+                        printLine();
+                        continue;
+                    }
+//                    int indexOfBy = line.indexOf("/by");
+//                    task = new Deadline(line.substring(9, indexOfBy), line.substring(indexOfBy + 4));
                 } else if (lowerCaseLine.contains("event")) {
-                    int indexOfFrom = line.indexOf("/from");
-                    int indexOfTo = line.indexOf("/to");
-                    task = new Event(line.substring(6, indexOfFrom), line.substring(indexOfFrom + 6, indexOfTo), line.substring(indexOfTo + 4));
+                    try {
+                        checkEmpty(lowerCaseLine);
+                        int indexOfFrom = line.indexOf("/from");
+                        int indexOfTo = line.indexOf("/to");
+                        task = new Event(line.substring(6, indexOfFrom), line.substring(indexOfFrom + 6, indexOfTo), line.substring(indexOfTo + 4));
+                    } catch (DukeException e) {
+                        System.out.println(e.getMessage());
+                        printLine();
+                        continue;
+                    }
+//                    int indexOfFrom = line.indexOf("/from");
+//                    int indexOfTo = line.indexOf("/to");
+//                    task = new Event(line.substring(6, indexOfFrom), line.substring(indexOfFrom + 6, indexOfTo), line.substring(indexOfTo + 4));
                 }
-                textList[textListCount] = task; //add all input except for list, bye, mark and unmark to array of tasks
-                textListCount++; //counter to help input tasks into correct index in tasks array
-                System.out.println("Got it. I've added this task: ");
-                System.out.println(" " + task.toString());
-                System.out.println("Now you have " + textListCount + " tasks in the list.");
-                printLine();
+                try {
+                    checkCommand(lowerCaseLine);
+                    textList[textListCount] = task; //add all input except for list, bye, mark and unmark to array of tasks
+                    textListCount++; //counter to help input tasks into correct index in tasks array
+                    System.out.println("Got it. I've added this task: ");
+                    System.out.println(" " + task.toString());
+                    System.out.println("Now you have " + textListCount + " tasks in the list.");
+                    printLine();
+                } catch (DukeException e) {
+                    System.out.println(e.getMessage());
+                    printLine();
+                    continue;
+                }
+//                textList[textListCount] = task; //add all input except for list, bye, mark and unmark to array of tasks
+//                textListCount++; //counter to help input tasks into correct index in tasks array
+//                System.out.println("Got it. I've added this task: ");
+//                System.out.println(" " + task.toString());
+//                System.out.println("Now you have " + textListCount + " tasks in the list.");
+//                printLine();
             }
         }
     }
