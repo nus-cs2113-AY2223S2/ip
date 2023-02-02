6aa9d468a659759ebd82d28b0d0e55d952833294
aaronxujiachen <1943999635x@gmail.com>
2023-02-02 16:08:50 +0800

    My level 1 Duke

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,8 @@
+import java.util.Scanner;  // Import the Scanner class
 public class Duke {
 
-    public static void drawLine() {
-        for (int i = 0; i < 20; i++) {
+    public static void drawLine() {  // Draw horizontal lines
+        for (int i = 0; i < 30; i++) {
             System.out.print("_");
         }
     }
@@ -16,12 +17,31 @@ public class Duke {
 
         drawLine();
         System.out.println();
-        System.out.println("Hello! I'm Duke");
+
+        System.out.println("Hello! I'm Duke");  // Greetings
         System.out.println("What can I do for you?");
+
         drawLine();
         System.out.println();
+
+        Scanner scan = new Scanner(System.in);  // Create a Scanner object
+        String echo = "echo";
+
+        while(true) {  //Endless loop until "bye" is detected
+            echo = scan.nextLine();
+            drawLine();
+            System.out.println();
+            if(echo.equals("bye")) {  //Terminate echo and exit while loop
+                break;
+            }
+            System.out.println(echo);
+            drawLine();
+            System.out.println();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         drawLine();
+        System.out.println();
 
     }
 
