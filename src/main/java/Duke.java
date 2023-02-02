bd71f694b700bafba0eee0ff7308565ab166757c
Sebastian Soewanto <sebastiansoewanto@outlook.com>
2023-02-03 01:00:05 +0800

    Add todo, event and deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,10 +11,9 @@ public class Duke {
         String line = "____________________________________________________\n";
         System.out.println("Hello from\n" + logo);
         System.out.print(line + "Hello! I'm Duke\n" + "What can I do for you?\n" + line);
-        String[] list = new String[100];
-        boolean[] isDone = new boolean[100];
-        String toFill;
-        int listCount = 0;
+        Task[] task = new Task[100];
+        int taskCount = 0;
+        int errorCount = 0;
         while(true) {
             Scanner userInput = new Scanner(System.in);
             String input = userInput.nextLine();
@@ -23,30 +22,42 @@ public class Duke {
                 break;
             } else if (input.startsWith("mark")) {
                 int mark = Integer.parseInt(input.substring(5));
-                isDone[mark - 1] = true;
-                System.out.println(line + "Nice! I've marked this task as done:\n" + "  [X] " + list[mark - 1]);
+                task[mark - 1].markDone();
                 System.out.print(line);
             } else if (input.startsWith("unmark")) {
                 int unmark = Integer.parseInt(input.substring(7));
-                isDone[unmark - 1] = false;
-                System.out.println(line + "OK, I've marked this task as not done yet:\n" + "  [ ] "+ list[unmark - 1]);
+                task[unmark - 1].umarkDone();
                 System.out.print(line);
-            } else if(input.equals("list")) {
+            } else if (input.equals("list")) {
                 System.out.print(line + "Here are the tasks in your list:\n");
-                for (int i = 0; i < listCount; i++) {
-                    if (isDone[i]) {
-                        toFill = "X";
-                    }
-                    else {
-                        toFill = " ";
-                    }
-                    System.out.println(i + 1 + ". [" + toFill + "] " + list[i]);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(i + 1 + "." + task[i]);
                 }
                 System.out.print(line);
+            } else if (input.startsWith("todo")) {
+                task[taskCount] = new Todo(input.substring(5));
+                taskCount++;
+                System.out.println(line + "Got it. I've added this task:\n" + task[taskCount - 1]);
+                System.out.print("Now you have " + taskCount + " tasks in the list.\n" + line);
+            } else if (input.startsWith("deadline")) {
+                int byIndex = input.indexOf("/by");
+                task[taskCount] = new Deadline(input.substring(9, byIndex), input.substring(byIndex + 4));
+                taskCount++;
+                System.out.println(line + "Got it. I've added this task:\n" + task[taskCount - 1]);
+                System.out.print("Now you have " + taskCount + " tasks in the list.\n" + line);
+            } else if (input.startsWith("event")) {
+                int fromIndex = input.indexOf("/from");
+                int toIndex = input.indexOf("/to");
+                task[taskCount] = new Event(input.substring(6, fromIndex), input.substring(fromIndex + 6, toIndex), input.substring(toIndex + 4));
+                taskCount++;
+                System.out.println(line + "Got it. I've added this task:\n" + task[taskCount - 1]);
+                System.out.print("Now you have " + taskCount + " tasks in the list.\n" + line);
+            } else if (errorCount == 10) {
+                System.out.print(line +"Maybe you can try in another language. Goodbye!\n" + line);
+                break;
             } else {
-                list[listCount] = input;
-                listCount++;
-                System.out.print(line + "added: " + input + "\n" + line);
+                System.out.print(line + "Sorry, could you please repeat what you just said\n" + line);
+                errorCount++;
             }
         }
     }
