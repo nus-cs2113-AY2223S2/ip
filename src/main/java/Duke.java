17c49f2cec55d15d09976a93a78ff1b2970042c7
stevenantya <81511355+stevenantya@users.noreply.github.com>
2023-02-02 19:46:21 +0800

    Re-organized week 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,118 +1,72 @@
 // Java program to read data of various types using Scanner class.
 import java.util.Scanner;
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static final int TASKS_COUNT = 100;
+    private static Task[] tasks = new Task[TASKS_COUNT];
+    public static void printWelcomeMessage() {
         String welcomeMessage = "____________________________________________________________\n" +
-                " Hello! I'm Duke, ready to serve you\n" +
+                " Hello! I'm Elzi, your dog!\n" +
                 " What can I do for my master?\n" +
                 "____________________________________________________________\n";
         System.out.println(welcomeMessage);
-
-        // Declare the object and initialize with
-        // predefined standard input object
+    }
+    public static void printLine() {
+        System.out.print("___________________________******___________________________\n");
+    }
+    public static void printList(int taskCounter) {
+        System.out.println("Your current tasks are as follows: ");
+        for (int index = 0; index < taskCounter; index += 1) {
+            System.out.println((index + 1) + "." + "[" +
+                    tasks[index].getStatusIcon() + "]" + " " +
+                    tasks[index].getDescription());
+        }
+    }
+    private static void readInput() {
         Scanner sc = new Scanner(System.in);
-
-        // String input
         String input = "";
+        input = sc.nextLine();
 
-        // String data
-        Task[] tasks = new Task[100];
+        int taskCounter = 0;
 
-        int counter = 1;
+        while (!input.equals("bye")) {
 
-        while(true) {
-            // Scan the input
-            input = sc.nextLine();
+            String[] inputs = input.split(" ");
+            String command  = inputs[0];
 
-            // Create Text Line
-            String textLine = "\n___________________________******___________________________\n";
+            printLine();
 
-            // Print begin text line
-            System.out.println(textLine);
-
-            // Prints output
-            if (input.equals("bye")) {
-                String byeText = "Bye. Hope to see you again soon!";
-                System.out.println(byeText);
+            switch (command) {
+            case "list":
+                printList(taskCounter);
                 break;
-            }
-            else if (input.equals("list")){
-
-                for (int index = 1; index < counter; index += 1){
-                    System.out.println((index) + "." + "[" + tasks[index].getStatusIcon() + "]" + " " + tasks[index].getDescription());
-                }
-            }
-            else if (input.substring(0, 4).equals("mark")) {
-                if (input.length() <= 5) {
-                    System.out.println("Please enter a valid number after space, e.g. mark 5");
-                    continue;
-                }
-                String indexString = input.substring(5);
-                indexString = indexString.replaceAll("\\s", "");
-                int index = Integer.parseInt(indexString);
-                if (index < counter && index != 0) {
-                    if (tasks[index].isDone())  {
-                        System.out.println("  It is already done silly! Do something else...");
-                        continue;
-                    }
-                    else {
-                        tasks[index].markAsDone();
-                        System.out.println("  Nice! I've marked this task as done:\n"+"    [" +
-                                tasks[index].getStatusIcon() + "]" + " " + tasks[index].getDescription());
-                    }
-
-                }
-                else {
-                    System.out.println("Please enter a valid number");
-                    continue;
-                }
-
-            }
-            else if (input.substring(0, 6).equals("unmark")) {
-                if (input.length() <= 5) {
-                    System.out.println("Please enter a valid number after space, e.g. mark 5");
-                    continue;
-                }
-                String indexString = input.substring(7);
-                indexString = indexString.replaceAll("\\s", "");
-                int index = Integer.parseInt(indexString);
-                if (index < counter && index != 0) {
-                    if (!tasks[index].isDone())  {
-                        System.out.println("  It is not done silly! Do it quickly!");
-                        continue;
-                    }
-                    else {
-                        tasks[index].markAsNotDone();
-                        System.out.println("  Okay... whatever you say sir.... unmarked!\n"+"    [" +
-                                tasks[index].getStatusIcon() + "]" + " " + tasks[index].getDescription());
-                    }
-
-                }
-                else {
-                    System.out.println("Please enter a valid number");
-                    continue;
-                }
-            }
-            else {
+            case "mark":
+                int taskIndex = Integer.parseInt(inputs[1]);
+                tasks[taskIndex-1].markAsDone();
+                System.out.println("I have marked this task as done");
+                printList(taskCounter);
+                break;
+            case "unmark":
+                taskIndex = Integer.parseInt(inputs[1]);
+                tasks[taskIndex-1].markAsNotDone();
+                System.out.println("I have unmarked this task");
+                printList(taskCounter);
+                break;
+            default:
                 Task t = new Task(input);
-                tasks[counter] = t;
-                counter += 1;
-                System.out.println(input);
+                tasks[taskCounter] = t; taskCounter += 1;
+                System.out.println("added: " + input);
             }
 
-            // Print end text line
-            System.out.println(textLine);
+            printLine();
 
+            input = sc.nextLine();
         }
 
-
-
-
+    }
+    public static void main(String[] args) {
+        printWelcomeMessage();
+        readInput();
+        // Print end text line
+        printLine();
     }
 }
