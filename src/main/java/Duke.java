bb6a8d2383e0eb456ef81370496dbf4e5a715f20
notbingsu <bingyuan007@gmail.com>
2023-02-02 21:15:12 +0800

    level2, add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
@@ -13,9 +15,19 @@ public class Duke {
         System.out.println("What can I do for you?");
         Scanner userInput = new Scanner(System.in); //create Scanner object
         String inputCommand = userInput.nextLine(); //read user input
+        List<String> allInputs = new ArrayList();
         while (!inputCommand.equals("bye")){
-            System.out.println(inputCommand);
+            if (inputCommand.equals("list")){
+                for (int i = 0; i < allInputs.size(); i ++){
+                    System.out.println(i + ". " + allInputs.get(i));
+                }
+            }
+            else {
+                System.out.println("added: " + inputCommand);
+                allInputs.add(inputCommand);
+            }
             inputCommand = userInput.nextLine();
+
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
