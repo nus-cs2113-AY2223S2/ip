daf3c4d6f17de333d1fa3101b46ac372f881e600
0nandon <tommy9905@gmail.com>
2023-02-03 01:33:10 +0800

    iP : A-CodeQuality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,9 +6,19 @@ public class Duke {
         System.out.println("\t____________________________________________________________");
         System.out.println("\t Got it. I've added this task:");
     }
-
     public static void printCnt(int cnt){
         System.out.println("\t Now you have " + cnt + " tasks in the list.");
+        System.out.println("\t____________________________________________________________");
+    }
+    public static void printMark(){
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t Nice! I've marked this task as done:");
+    }
+    public static void printUnMark(){
+        System.out.println("\t____________________________________________________________");
+        System.out.println("\t OK, I've marked this task as not done yet:");
+    }
+    public static void printUnderBar(){
         System.out.println("\t____________________________________________________________");
     }
 
@@ -17,6 +27,7 @@ public class Duke {
 
         Scanner in = new Scanner(System.in);
         int cnt = 0;
+        int index = 0;
         String input_text;
         String[] tgt;
 
@@ -25,7 +36,7 @@ public class Duke {
             if(input_text.length() == 0)
                 break;
 
-            tgt = input_text.split(",");
+            tgt = input_text.split(", ");
 
             switch(tgt[0]){
             case "todo":
@@ -48,6 +59,22 @@ public class Duke {
                 System.out.println("\t\t" + tasks[cnt].toString());
                 cnt += 1;
                 printCnt(cnt);
+                break;
+            case "mark":
+                printMark();
+                index = Integer.parseInt(tgt[1])-1;
+                tasks[index].markAsDone();
+                System.out.println("\t\t[" + tasks[index].getStatusIcon() + "] " +
+                        tasks[index].getDescription());
+                printUnderBar();
+                break;
+            case "unmark":
+                printUnMark();
+                index = Integer.parseInt(tgt[1])-1;
+                tasks[index].markAsUnDone();
+                System.out.println("\t\t[" + tasks[index].getStatusIcon() + "] " +
+                        tasks[index].getDescription());
+                printUnderBar();
                 break;
             case "list":
                 System.out.println("\t____________________________________________________________");
