f02095e55066c960d23a23e4f696616102604319
ghzr0 <glenn262b@gmail.com>
2023-02-03 02:08:26 +0800

    Add Increments: Level-4, A-CodeQuality 1. Implemented ToDos, Events, Deadlines 2. Edited to fit coding quality and standards based on PRs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,30 +1,42 @@
-import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class Duke {
-
     //Definition lists //
-    public static final String margin = "*----------------------------*";
-    public static String updateStr = "added: ";          // for adding new tasks in taskList
+    public static void printMargin(){
+        String MARGIN = "*----------------------------*";
+        System.out.println(MARGIN);
+    }
+    public static void endProgram(){
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+    public static void welcomeMessage(){
+        String logo = "┊┊┊┊┊╭╭╭╮╮╮┊┊┊┊ \n" +
+                "┊┊┊┊┊╰╰╲╱╯╯┊┊┊┊ \n" +
+                "┊┏╮╭┓╭━━━━━━╮┊┊ \n" +
+                "┊╰╮╭╯┃┈┈┈┈┈┈┃┊┊ \n" +
+                "┊┊┃╰━╯┈┈╰╯┈┈┃┊┊ \n" +
+                "┊┊┃┈┈┈┈┈┈┈╰━┫┊┊ \n" +
+                "╲╱╲╱╲╱╲╱╲╱╲╱╲╱╲\n" ;
 
-    public static Task[] taskList = new Task[100];  // Create array to store the tasks
-    public static int taskCount = 0;                    // Counter to track how many tasks in taskList
-
-
-    // Run program here: //
-    public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
-
-
-        // Welcome Message //
-        System.out.println(margin);
-        System.out.println("Hello! I'm Duke");
+        System.out.println(logo);
+        System.out.println("Hello! I'm Duke!");
         System.out.println("What can I do for you?");
-        System.out.println(margin);
+    }
+
+    // Create array to store the tasks
+    public static ArrayList<Task> tasksArray = new ArrayList<>();
+
+    // Counter to track how many tasks in taskList
+    public static int taskCount = 0;
+
+    // Run program here:
+    public static void main(String[] args) {
+
+        printMargin();
+        welcomeMessage();
+        printMargin();
 
         Scanner input = new Scanner(System.in);
         String userCommand;
@@ -38,54 +50,117 @@ public class Duke {
 
         }
 
-    // How the program runs based on user's input commands //
+    // How the program runs based on user's input commands
     public static void enter(String userCommand) {
+        // Split user's input into individual words for easier index access
+        String[] wordSeparator = userCommand.split(" ");
+        // Identify first word in the user's input and execute corresponding command
+        String KEYWORD = wordSeparator[0];
 
-        String[] strNumSeparator = userCommand.split(" ");  // split string & number components
+        switch (KEYWORD) {
+        case "bye":        //Exit the program
+            printMargin();
+            endProgram();
+            printMargin();
+            break;
 
-        switch (strNumSeparator[0]) {
-            case "bye":        //Exit the program
-                System.out.println(margin);
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(margin);
-                break;
+        case "list":       // View taskList
+            printMargin();
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < tasksArray.size(); i++) {
+                System.out.println(Integer.toString(i + 1) + ". " + tasksArray.get(i).toString() );
+            }
+            printMargin();
+            break;
 
-            case "list":       // View taskList
-                System.out.println(margin);
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    System.out.println(Integer.toString(i + 1) + ". " + "["+ taskList[i].getStatusIcon() + "]" + taskList[i] );
-                }
-                System.out.println(margin);
-                break;
-
-            case "mark":       // Mark task as done
-                int taskToMark = Integer.parseInt(strNumSeparator[1]) - 1;      // identify which task to mark
-                System.out.println(margin);
-                System.out.println("Nice! I've marked this task as done:");
-                taskList[taskToMark].setDone(true);
-                System.out.println("  [" +  taskList[taskToMark].getStatusIcon() + "] " + taskList[taskToMark]);
-                System.out.println(margin);
-                break;
+        case "mark":       // Mark task as done
+            int taskToMark = Integer.parseInt(wordSeparator[1]) - 1;
+            printMargin();
+            System.out.println("Nice! I've marked this task as done:");
+            tasksArray.get(taskToMark).setDone(true);
+            System.out.println("  " +  tasksArray.get(taskToMark).getStatusIcon()  + tasksArray.get(taskToMark).description);
+            printMargin();
+            break;
 
 
-            case "unmark":      // unmark task
-                int taskToUnmark = Integer.parseInt(strNumSeparator[1]) - 1;      // identify which task to un
-                System.out.println(margin);
-                System.out.println("OK, I've marked this task as not done yet:");
-                taskList[taskToUnmark].setDone(false);
-                System.out.println("  [" +  taskList[taskToUnmark].getStatusIcon() + "] " + taskList[taskToUnmark]);
-                System.out.println(margin);
-                break;
+        case "unmark":      // unmark task
+            int taskToUnmark = Integer.parseInt(wordSeparator[1]) - 1;
+            printMargin();
+            System.out.println("OK, I've marked this task as not done yet:");
+            tasksArray.get(taskToUnmark).setDone(false);
+            System.out.println("  " +  tasksArray.get(taskToUnmark).getStatusIcon()  + tasksArray.get(taskToUnmark).description);
+            printMargin();
+            break;
 
-            default:            // adding new tasks
-                Task t = new Task(userCommand) ;
-                taskList[taskCount] = t;
-                taskCount++;
-                System.out.println(margin);
-                System.out.println(updateStr + t.description);
-                System.out.println(margin);
-                break;
+
+
+        case "todo":        // add todo task
+            // Get the task todo
+            String taskToDo = userCommand.substring(4,userCommand.length());
+            Todo todoTask = new Todo(taskToDo);
+
+            printMargin();
+            System.out.println("Got it. I've added this task:");
+            System.out.println("  "+ todoTask);
+            taskCount++ ;
+            System.out.println("Now you have "+ taskCount + " tasks in the list.");
+            printMargin();
+
+            tasksArray.add(todoTask);
+            break;
+
+
+        case "deadline":    // add deadline task
+            // Filter task and due date from user' input
+            int filterIdx = userCommand.indexOf('/');
+            // Get task from user's input
+            String deadlineTask = userCommand.substring(8,filterIdx-1);
+            // Get due date from user's input
+            String dueBy = userCommand.substring(filterIdx+3,userCommand.length());
+            Deadline d = new Deadline(deadlineTask, dueBy);
+
+            printMargin();
+            System.out.println("Got it. I've added this task:");
+            System.out.println("  " + d);
+            taskCount++ ;
+            System.out.println("Now you have "+ taskCount + " tasks in the list.");
+            printMargin();
+
+            tasksArray.add(d);
+            break;
+
+        case "event":       // add event task
+            // Filter task and due date from user' input
+            int filterEventIdx = userCommand.indexOf('/');
+            int filterEventIdx2 = userCommand.lastIndexOf('/');
+            // Get task from user's input
+            String eventTask = userCommand.substring(5,filterEventIdx-1);
+            // Get event start time
+            String startTime = userCommand.substring(filterEventIdx+5,filterEventIdx2-1);
+            // Get event end time
+            String endTime = userCommand.substring(filterEventIdx2+3,userCommand.length());
+            // Get duration of event
+            String eventDuration = startTime + " " + endTime;
+            Event e = new Event(eventTask,startTime, endTime);
+
+            printMargin();
+            System.out.println("Got it. I've added this task:");
+            System.out.println("  "+ e);
+            taskCount++ ;
+            System.out.println("Now you have "+ taskCount + " tasks in the list.");
+            printMargin();
+
+            tasksArray.add(e);
+            break;
+
+        default:            // adding generic tasks
+            Task t = new Task(userCommand) ;
+            tasksArray.add(t);
+            taskCount++;
+            printMargin();
+            System.out.println("added: " + t.description);
+            printMargin();
+            break;
 
         }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,25 +2,30 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    // mark done task with X on 2nd []
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        return (isDone ? "[X]" : "[ ]");
     }
 
+    // For marking and unmarking tasks
     public void setDone(boolean done) {
         isDone = done;
     }
+    // Return status of task
     public boolean isDone() {
         return isDone;
     }
 
     @Override
     public String toString(){
-        return description;
+
+        return  getStatusIcon()  + description;
     }
 
 
