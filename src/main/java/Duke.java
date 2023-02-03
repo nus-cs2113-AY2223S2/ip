57320b1f0c81b4906868febadc4ede5182cb5fcd
denzelcjy <denzel_cjy@hotmail.com>
2023-02-03 12:07:00 +0800

    Add Duke Level 5: Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,6 +15,12 @@ public class Duke {
         System.out.println("    ____________________________________________________________");
     }
 
+    public static void printErrorMessage(String errorMessage) {
+        System.out.println("    ____________________________________________________________");
+        System.out.println(errorMessage);
+        System.out.println("    ____________________________________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -27,40 +33,48 @@ public class Duke {
         ArrayList<Task> taskList = new ArrayList<>();
         while (input.hasNextLine()) {
             String[] nextInput = input.nextLine().split(" ", 2);
-            if (nextInput[0].equals("bye")) {
-                break;
+            try {
+                if (nextInput[0].equals("bye")) {
+                    break;
+                }
+                if (nextInput[0].isEmpty()) { //settle for the case of empty inputs
+                    continue;
+                }
+                if (nextInput[0].equals("list")) { //want to print out the task list
+                    Task.printTaskList(taskList);
+                    continue;
+                }
+                if (nextInput[0].equals("mark")) {
+                    int taskNumber = Integer.parseInt(nextInput[1]);
+                    taskList.get(taskNumber - 1).markAsDone();
+                    Task.printMarkedTask(taskNumber - 1, taskList);
+                    continue;
+                }
+                if (nextInput[0].equals("unmark")) {
+                    int taskNumber = Integer.parseInt(nextInput[1]);
+                    taskList.get(taskNumber - 1).markAsNotDone();
+                    Task.printUnmarkedTask(taskNumber - 1, taskList);
+                    continue;
+                }
+                if (nextInput[0].equals("todo")) {
+                    Task.addTodoTask(nextInput[1], taskList);
+                    continue;
+                }
+                if (nextInput[0].equals("deadline")) {
+                    Task.addDeadlineTask(nextInput[1], taskList);
+                    continue;
+                }
+                if (nextInput[0].equals("event")) {
+                    Task.addEventTask(nextInput[1], taskList);
+                    continue;
+                }
+                throw new DukeException("     ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+            } catch (DukeException exception) {
+                printErrorMessage(exception.getMessage());
+            } catch (ArrayIndexOutOfBoundsException exception) {
+                //for the case of no space after calling a command
+                printErrorMessage("     ☹ OOPS!!! The description of a " + nextInput[0] + " cannot be empty.");
             }
-            if (nextInput[0].isEmpty()) { //settle for the case of empty inputs
-                continue;
-            }
-            if (nextInput[0].equals("list")) { //want to print out the task list
-                Task.printTaskList(taskList);
-                continue;
-            }
-            if (nextInput[0].equals("mark")) {
-                int taskNumber = Integer.parseInt(nextInput[1]);
-                taskList.get(taskNumber - 1).markAsDone();
-                Task.printMarkedTask(taskNumber - 1, taskList);
-                continue;
-            }
-            if (nextInput[0].equals("unmark")) {
-                int taskNumber = Integer.parseInt(nextInput[1]);
-                taskList.get(taskNumber - 1).markAsNotDone();
-                Task.printUnmarkedTask(taskNumber - 1, taskList);
-                continue;
-            }
-            if (nextInput[0].equals("todo")) {
-                Task.addTodoTask(nextInput[1], taskList);
-                continue;
-            }
-            if (nextInput[0].equals("deadline")) {
-                Task.addDeadlineTask(nextInput[1], taskList);
-                continue;
-            }
-            if (nextInput[0].equals("event")) {
-                Task.addEventTask(nextInput[1], taskList);
-            }
-
         }
         endDuke();
     }
