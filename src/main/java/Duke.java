2cc6948891fdd0421113e8103d3eedbed53d7554
Wilson Lee Jun Wei <leejunwei2000@gmail.com>
2023-02-03 21:15:15 +0800

    Add Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,47 @@
 import java.util.Scanner;
 
+
 public class Duke {
     public static void main(String arguments[]) {
         int counter = 0;
         System.out.println("Hi there! My name is Coffee");
         System.out.println("How can I help you today?");
-
         Scanner command = new Scanner(System.in); // reading in inputs
         String input_Command = command.nextLine();
+        Task task_Array[];
+        task_Array = new Task[110];
 
-        String string_Array[];
-        string_Array = new String[110];
+
         while (!"bye".equals(input_Command)) { // means if bye as input, immediately go to I look forward to seeing you again
-                if ("list".equals(input_Command)) { // if list is an input
-                    for (int i = 0; i < counter; i = i + 1) {
-                        System.out.println((i + 1) + ". " + string_Array[i]);
-                    }
-                    command = new Scanner(System.in);
-                    input_Command = command.nextLine();
-                } else {
-                    string_Array[counter] = input_Command; // read book
-                    System.out.println("added: " + input_Command); // added: read book
-                    command = new Scanner(System.in); // re-read inputs again
-                    input_Command = command.nextLine(); // return book
-                    counter = counter + 1;
+            if ("list".equals(input_Command)) { // if list is an input
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < counter; i = i + 1) {
+                    System.out.println((i + 1) + ". " + task_Array[i]);
                 }
+                command = new Scanner(System.in);
+                input_Command = command.nextLine();
+            } else if (input_Command.contains("mark")) {
+                String[] tokens = input_Command.split(" ");
+                int index = Integer.parseInt(tokens[1]) - 1;
+                if (tokens[0].equals("mark")) {
+                    task_Array[index].markAsDone();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(task_Array[index]);
+                } else {
+                    task_Array[index].markAsNotDone();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(task_Array[index]);
+                }
+                command = new Scanner(System.in); // re-read inputs again
+                input_Command = command.nextLine();
+            } else {
+                task_Array[counter] = new Task(input_Command);
+                System.out.println("added: " + input_Command); // added: read book
+                command = new Scanner(System.in); // re-read inputs again
+                input_Command = command.nextLine(); // return book
+                counter = counter + 1;
             }
-        System.out.println("I look forward to seeing you again! Goodbye!");
         }
+        System.out.println("I look forward to seeing you again! Goodbye!");
     }
+}
---
+++ src/main/java/Task.java
@@ -0,0 +1,26 @@
+public class Task {
+    protected String description; // instance variable
+    protected boolean isDone;
+
+    public Task(String description) { // constructor
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // methods mark done task with X
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "]" + " " + description;
+    }
+}
