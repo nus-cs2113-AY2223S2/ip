6368709c6afed9f9d896c95570a2703900feab7e
saint thiri zaw <saintstz@gmail.com>
2023-02-03 20:16:01 +0800

    Level-4 Updated: separated code in main method into smaller methods and made minor changes to existing methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,26 @@
 import java.util.Scanner;
+
 public class Duke {
+    public static void greetUser() {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+
+        System.out.println("Hello from\n" + logo);
+        printLine();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        printLine();
+    }
+
+    public static void sayBye() {
+        printLine();
+        System.out.println("Bye. Hope to see you again soon! :)");
+        printLine();
+    }
+
     public static void printLine() {
         System.out.println("____________________________________________________________");
     }
@@ -25,9 +46,9 @@ public class Duke {
     }
 
     //mark task as done
-    public static void markTask(Task[] list, String[] input, int i){
-        int index = Integer.parseInt(input[i]);
-        Task t = list[index-1];
+    public static void markTask(Task[] tasks, String[] input){
+        int index = Integer.parseInt(input[1]);
+        Task t = tasks[index-1];
         t.markAsDone();
         printLine();
         System.out.println("Nice! I've marked this task as done:");
@@ -36,9 +57,9 @@ public class Duke {
     }
 
     //mark task as not done
-    public static void unmarkTask(Task[] list, String[] input, int i){
-        int index = Integer.parseInt(input[i]);
-        Task t = list[index-1];
+    public static void unmarkTask(Task[] tasks, String[] input){
+        int index = Integer.parseInt(input[1]);
+        Task t = tasks[index-1];
         t.markNotDone();
         printLine();
         System.out.println("OK, I've marked this task as not done yet:");
@@ -47,9 +68,9 @@ public class Duke {
     }
 
     //to-do
-    public static void addTodo(Task[] list, int count, String[] input, int i){
-        Task t = new Todo(input[i]);
-        list[count] = t;
+    public static void addTodo(Task[] tasks, int count, String[] input){
+        Task t = new Todo(input[1]);
+        tasks[count] = t;
         count++;
         printLine();
         System.out.println("Got it. I've added this task:");
@@ -59,9 +80,10 @@ public class Duke {
     }
 
     //deadline
-    public static void addDeadline(Task[] list, int count, String[] input, int i, int j){
-        Task t = new Deadline(input[i], input[j]);
-        list[count] = t;
+    public static void addDeadline(Task[] tasks, int count, String[] input){
+        String[] doBy = input[1].split("by ", 2);
+        Task t = new Deadline(doBy[0], doBy[1]);
+        tasks[count] = t;
         count++;
         printLine();
         System.out.println("Got it. I've added this task:");
@@ -71,9 +93,11 @@ public class Duke {
     }
 
     //event
-    public static void addEvent(Task[] list, int count, String[] start, String[] end, int i, int j, int k){
-        Task t = new Event(start[i], end[j], end[k] );
-        list[count] = t;
+    public static void addEvent(Task[] tasks, int count, String[] input){
+        String[] start = input[1].split("from ", 2);
+        String[] end = start[1].split("to ", 2);
+        Task t = new Event(start[0], end[0], end[1] );
+        tasks[count] = t;
         count++;
         printLine();
         System.out.println("Got it. I've added this task:");
@@ -81,72 +105,61 @@ public class Duke {
         System.out.println("Now you have " + count + " tasks in the list.");
         printLine();
     }
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
 
-        System.out.println("Hello from\n" + logo);
-        printLine();
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        printLine();
-
-        String command;
-        Scanner in = new Scanner(System.in);
-        command = in.nextLine();
-
-        Task[] list = new Task[100]; //store tasks in an array
-        int count = 0; //keep track of number of tasks in list
-
+    public static void handleCommands(Scanner in, String command, Task[] tasks) {
+        int count = 0; //keep track of number of tasks
+        
         while (!(command.equals("bye"))) {
             //list tasks
             if (command.equals("list")) {
-                listTasks(list, count);
+                listTasks(tasks, count);
             } else {
                 String[] input = command.split(" ", 2); //only check the first word for "mark" or "unmark"
                 switch (input[0]) {
                     //mark task
                     case "mark":
-                        markTask(list, input, 1);
+                        markTask(tasks, input);
                         break;
                     //un-mark task
                     case "unmark":
-                        unmarkTask(list, input, 1);
+                        unmarkTask(tasks, input);
                         break;
                     //to-do
                     case "todo":
-                        addTodo(list, count, input, 1);
+                        addTodo(tasks, count, input);
                         count++;
                         break;
                     //deadline
                     case "deadline":
-                        String[] doBy = input[1].split("by ", 2);
-                        addDeadline(list, count, doBy, 0, 1);
+                        addDeadline(tasks, count, input);
                         count++;
                         break;
                     //event
                     case "event":
-                        String[] start = input[1].split("from ", 2);
-                        String[] end = start[1].split("to ", 2);
-                        addEvent(list, count, start, end, 0, 0, 1);
+                        addEvent(tasks, count, input);
                         count++;
                         break;
                     //unlabelled
                     default:
-                        addTask(list, count, command);
+                        addTask(tasks, count, command);
                         count++;
                         break;
                 }
             }
             command = in.nextLine(); //read next command
         }
-        //exit loop when command received is "bye"
-        printLine();
-        System.out.println("Bye. Hope to see you again soon! :)");
-        printLine();
+    }
+
+    public static void main(String[] args) {
+        greetUser();
+
+        Scanner in = new Scanner(System.in);
+        String command = in.nextLine();
+        Task[] tasks = new Task[100]; //store tasks in an array
+
+        handleCommands(in, command, tasks);
+
+        sayBye(); //exit loop when command received is "bye"
     }
 }
 
