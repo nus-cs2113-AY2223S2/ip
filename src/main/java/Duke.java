7c733cacda975492e27c3ba57cd9bdded1ae655c
javienneyeo <javienneyeo@gmail.com>
2023-02-04 03:41:03 +0800

    added list to store items

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,32 +2,30 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         Scanner input = new Scanner(System.in);
         String text = input.nextLine();
         String[] list = new String[100];
         int j = 0;
-        while (!text.equals("bye"))
-        {
-            if (text.equals("list"))
-            {
-                for (int i = 0; i < list.length; i += 1)
-                {
-                    if (list[i] == null)
-                    {
+        while (!text.equals("bye")) {
+            if (text.equals("list")) {
+                for (int i = 0; i < list.length; i += 1) {
+                    if (list[i] == null) {
                         break;
-                    }
-                    else
-                    {
-                        System.out.println(i+1 + ". " + list[i]);
+                    } else {
+                        System.out.println(i + 1 + ". " + list[i]);
                     }
                 }
-            }
-            else
-            {
+            } else {
                 list[j] = text;
-                System.out.println("added:" + text);
+                System.out.println("added: " + text);
                 j += 1;
             }
             text = input.nextLine();
