23689e22c879dee6794082662822bae85b1e1d20
wenxin-c <chenwenxin0825@gmail.com>
2023-02-04 01:49:00 +0800

    catch empty task description for todo event deadline

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,7 +5,7 @@ public class Duke {
     private final static UI READ_COMMAND = new UI();
     private final static CommandManager COMMAND_MANAGER = new CommandManager();
     private final static String UNKNOWN_COMMAND_MESSAGE = "☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n";
-    private final static String EMPTY_CONTENT = "☹ OOPS!!! The description of a todo cannot be empty.\n";
+    private final static String EMPTY_CONTENT = "☹ OOPS!!! The description of a task cannot be empty.\n";
 
     public static void startDuke(){
         COMMAND_MANAGER.sayHello();
