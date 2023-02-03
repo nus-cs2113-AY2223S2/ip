6f5b823f54d0c34df7166a6ee346c68e82cdc280
L0Z1K <qortmddbs731@naver.com>
2023-02-03 16:54:22 +0800

    Feat: Add OrcaException

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,10 +1,14 @@
 public class Deadline extends Task {
     protected String by;
 
-    Deadline(String userInput, int startIdx) {
+    Deadline(String userInput, int startIdx) throws OrcaException {
         String[] deadline = userInput.substring(startIdx).split(" /by ");
-        this.description = deadline[0];
-        this.by = deadline[1];
+        try {
+            this.description = deadline[0];
+            this.by = deadline[1];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new OrcaException("The description/by of a deadline cannot be empty.");
+        }
         this.type = "D";
     }
 
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,6 +1,10 @@
 public class Todo extends Task {
-    public Todo(String userInput, int startIdx) {
-        this.description = userInput.substring(startIdx);
-        this.type = "T";
+    public Todo(String userInput, int startIdx) throws OrcaException {
+        try {
+            this.description = userInput.substring(startIdx);
+            this.type = "T";
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new OrcaException("The description of a todo cannot be empty.");
+        }
     }
 }
