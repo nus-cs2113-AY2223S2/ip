b217951566fa47be44e9751db5b7c9f5943e5516
iantenkai <iantenkai@gmail.com>
2023-02-03 12:53:51 +0800

    add todo add deadline
    seperated class and main file

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,35 +1,6 @@
 
 import java.util.Scanner;
 
-class Task {
-    String taskName;
-    boolean status;
-
-    Task() {
-        this.status = false;
-    }
-
-    public String getTaskStatus() {
-        String status = " ";
-
-        if (this.status == true) {
-            status = "X";
-        }
-        return status;
-    }
-
-    public void mark() {
-        this.status = true;
-        System.out.println("目標達成！- [" + this.getTaskStatus() + "] " + this.taskName);
-
-    }
-
-    public void unMark() {
-        this.status = false;
-        System.out.println("えええ？噓つき。- [" + this.getTaskStatus() + "] " + this.taskName);
-    }
-}
-
 public class Duke {
     public static void main(String[] args) {
         Task[] taskList = new Task[100];
@@ -50,17 +21,24 @@ public class Duke {
                     System.out.println("何もいない。。。頭のように。。。");
                 } else {
                     for (int i = 0; i < index; i++) {
-                        System.out.println(i + 1 + ". [" + taskList[i].getTaskStatus() + "] " + taskList[i].taskName);
+                        System.out.println(i + 1 + ". [" + taskList[i].getTaskStatus() + "] " + taskList[i].description);
                     }
                 }
             } else if (firstWord.equals("mark")) {
                 taskList[Integer.parseInt(words[1]) - 1].mark(); // shld break if i type mark 0
             } else if (firstWord.equals("unmark")) {
                 taskList[Integer.parseInt(words[1]) - 1].unMark(); // shld break if i type mark 0
+            } else if (firstWord.equals("deadline") && index < 100) {
+                String by = scanner.nextLine();
+                Deadline newDeadline = new Deadline(input, by);
+                taskList[index] = newDeadline;
+
+                System.out.println("覚えましたよ～ " + input);
+                index++;
+
 
             } else if (index < 100) {
-                Task newTask = new Task();
-                newTask.taskName = input;
+                Task newTask = new Task(input);
                 taskList[index] = newTask;
 
                 System.out.println("覚えましたよ～ " + input);
