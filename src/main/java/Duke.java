eb6accb4f1b5572b2f7a6448e5e3863d9b47e482
PoobalanAatmikaLakshmi <88182404+PoobalanAatmikaLakshmi@users.noreply.github.com>
2023-02-04 21:41:05 +0800

    Learn from comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,82 +3,95 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Duke {
-    static ArrayList<Task> list = new ArrayList<Task>();
+    static ArrayList<Task> tasksList = new ArrayList<Task>();
 
     private static void addTasks(Task a) {
-        list.add(a);
+        tasksList.add(a);
     }
-    private static void printMarking(int i){
+
+    private static void printMarking(int i) {
         System.out.println(String.format(" [%s] [%s] %s",
-                list.get(i).getTypeIcon(), list.get(i).getStatusIcon(), list.get(i).getDescription()));
+                tasksList.get(i).getTypeIcon(), tasksList.get(i).getStatusIcon(), tasksList.get(i).getDescription()));
     }
-    public static void main(String[] args) {
+
+    private static void addMark(String remainingWords) {
+        System.out.println("You are crushing it,1 task down!");
+        Task.markAsDone(tasksList.get(Integer.parseInt(remainingWords) - 1));
+        int i = Integer.parseInt(remainingWords) - 1;
+        printMarking(i);
+    }
+
+    private static void unMark(String remainingWords) {
+        System.out.println("I have unchecked it for you");
+        Task.markAsNotDone(tasksList.get(Integer.parseInt(remainingWords) - 1));
+        int i = Integer.parseInt(remainingWords) - 1;
+        printMarking(i);
+    }
+
+    private static void printIntro() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello I am Baymax your personal life assistant \n" + logo);
+        System.out.println("Hello I am Duke your personal life assistant \n" + logo);
         System.out.println("What can I do for you today?");
+    }
+
+    private static void processList() {
+        if (tasksList.size() == 0) {
+            System.out.println("Nothing to do for now!Take a break!");
+        } else {
+            System.out.println("Here's your plan for a productive day!");
+        }
+        for (int i = 0; i < tasksList.size(); i++) {
+            System.out.println(String.format("%d.[%s] [%s] %s", i + 1,
+                    tasksList.get(i).getTypeIcon(), tasksList.get(i).getStatusIcon(), tasksList.get(i).getDescription()));
+        }
+    }
+
+    public static void main(String[] args) {
+        printIntro();
         String line;
         Scanner in = new Scanner(System.in);
-
 
         while (true) {
             line = in.nextLine();
             String arr[] = line.split(" ", 2);
+
             if (line.equals("bye")) {
                 System.out.println("Bye see you again!");
                 break;
             } else if (line.equals("list")) {
-                if (list.size() == 0) {
-                    System.out.println("Nothing to do for now!Take a break!");
-                } else {
-                    System.out.println("Here's your plan for a productive day!");
-                }
-                for (int i = 0; i < list.size(); i++) {
-                    System.out.println(String.format("%d.[%s] [%s] %s", i + 1,
-                            list.get(i).getTypeIcon(), list.get(i).getStatusIcon(), list.get(i).getDescription()));
-                }
+                processList();
+
             } else if (arr[0].equals("mark")) {
-                System.out.println("You are crushing it,1 task down!");
-                Task.markAsDone(list.get(Integer.parseInt(arr[1]) - 1));
-                int i = Integer.parseInt(arr[1]) - 1;
-                printMarking(i);
+                addMark(arr[1]);
 
             } else if (arr[0].equals("unmark")) {
-                System.out.println("I have unchecked it for you");
-                Task.markAsNotDone(list.get(Integer.parseInt(arr[1]) - 1));
-                int i = Integer.parseInt(arr[1]) - 1;
-                printMarking(i);
+                unMark(arr[1]);
 
             } else if (arr[0].equals("todo")) {
                 addTasks(new Todo(arr[1]));
-                System.out.println("Added!");
-                System.out.println(String.format(" [%s] [%s] %s", 'T', " ", arr[1]));
-                System.out.println("Now you have " + list.size() + " tasks in your list");
+                System.out.println(arr[1]);
+                System.out.println("Now you have " + tasksList.size() + " tasks in your list");
 
             } else if (arr[0].equals("deadline")) {
                 String[] parts = arr[1].split("/");
                 addTasks(new Deadline(parts[0], parts[1]));
                 System.out.println("Added!");
                 System.out.println(String.format(" [%s] [%s] %s (%s)", 'D', " ", parts[0], parts[1]));
-                System.out.println("Now you have " + list.size() + " tasks in your list");
+                System.out.println("Now you have " + tasksList.size() + " tasks in your list");
 
             } else if (arr[0].equals("event")) {
                 String[] parts = arr[1].split("/");
                 addTasks(new Event(parts[0], parts[1], parts[2]));
                 System.out.println("Added!");
                 System.out.println(String.format(" [%s] [%s] %s (%s %s)", 'E', " ", parts[0], parts[1], parts[2]));
-                System.out.println("Now you have " + list.size() + " tasks in your list");
+                System.out.println("Now you have " + tasksList.size() + " tasks in your list");
 
             }
-
-
         }
-
-
     }
-
 
 }
