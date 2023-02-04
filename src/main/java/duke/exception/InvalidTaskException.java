51354267804689d145223e7deed46968098e8216
Gan868611 <y.zganz.y@gmail.com>
2023-02-04 15:24:47 +0800

    Edit Error Message

--- src/main/java/duke/exception/InvalidTaskException.java
+++ src/main/java/duke/exception/InvalidTaskException.java
@@ -2,6 +2,6 @@ package duke.exception;
 
 public class InvalidTaskException extends Exception {
     public String getMessage() {
-        return "Invalid Task Command, PLease enter Valid Command and Format";
+        return "Invalid Task Command, PLease enter valid command and format";
     }
 }
