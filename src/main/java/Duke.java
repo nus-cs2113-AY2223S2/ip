5a7e00d940ac9cb4c494706745766ef72b142854
Varnika Srivastava <>
2023-02-05 22:26:04 +0800

    Handle errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Duke {
 
     public static Task[] tasks = new Task[100];
-    public static int counter = 0;
+    public static int taskCounter = 0;
 
     public static void printLine() {
         System.out.println("____________________________________________________________");
@@ -24,77 +24,98 @@ public class Duke {
 
     private static void markAsUndone(String input) {
         printLine();
-        String taskNum = input.substring(input.indexOf(" ") + 1);
-        int taskNumber = Integer.parseInt(taskNum);
-        if (taskNumber > counter) {
-            System.out.println("Sorry, this task does not exist.");
-            printLine();
-        } else {
+        try {
+            String taskNum = input.substring(input.indexOf(" ") + 1);
+            int taskNumber = Integer.parseInt(taskNum);
             tasks[taskNumber - 1].markUndone();
             System.out.println("OK, I've marked this task as not done yet:");
             System.out.println(tasks[taskNumber - 1]);
+            printLine();
+        } catch (NullPointerException e) {
+            System.out.println("This task does not exist. Please provide a valid task index");
             printLine();
         }
     }
 
     private static void markAsDone(String input) {
         printLine();
-        String taskNum = input.substring(input.indexOf(" ") + 1);
-        int taskNumber = Integer.parseInt(taskNum);
-        if (taskNumber > counter) {
-            System.out.println("Sorry, this task does not exist.");
-            printLine();
-        } else {
+        try {
+            String taskNum = input.substring(input.indexOf(" ") + 1);
+            int taskNumber = Integer.parseInt(taskNum);
             tasks[taskNumber - 1].markDone();
             System.out.println("Great! I have marked this task as done:");
             System.out.println(tasks[taskNumber - 1]);
             printLine();
+        } catch (NullPointerException e) {
+            System.out.println("This task does not exist. Please provide a valid task index");
+            printLine();
         }
     }
 
-    private static void printList() {
-        printLine();
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < counter; i++) {
-            System.out.print("\t" + (i + 1) + ".");
-            System.out.println(tasks[i]);
+    private static void printList() throws DukeException {
+        if (taskCounter == 0) {
+            throw new DukeException("Sorry, there are no tasks in the list currently.");
+        } else {
+            printLine();
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < taskCounter; i++) {
+                System.out.print("\t" + (i + 1) + ".");
+                System.out.println(tasks[i]);
+            }
         }
         printLine();
     }
 
-    private static void addTodo(String input) {
-        Todo todo = new Todo(input.substring(5));
-        tasks[counter++] = todo;
-        printLine();
-        System.out.println("Got it. I've added this task:");
-        System.out.print("\t  ");
-        System.out.println(todo);
-        System.out.println("Now you have " + counter + " tasks in the list.");
-        printLine();
+    private static void addTodo(String input) throws DukeException {
+        String[] words = input.split(" ");
+        if (words.length < 2) {
+            throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.");
+        } else {
+            Todo todo = new Todo(input.substring(5));
+            tasks[taskCounter++] = todo;
+            printLine();
+            System.out.println("Got it. I've added this task:");
+            System.out.print("\t  ");
+            System.out.println(todo);
+            System.out.println("Now you have " + taskCounter + " tasks in the list.");
+            printLine();
+        }
     }
 
-    private static void addDeadline(String input) {
-        String[] wordDeadline = input.substring(9).split("/");
-        Deadline deadline = new Deadline(wordDeadline[0].trim(), wordDeadline[1].substring(3));
-        tasks[counter++] = deadline;
-        printLine();
-        System.out.println("Got it. I've added this task:");
-        System.out.print("\t  ");
-        System.out.println(deadline);
-        System.out.println("Now you have " + counter + " tasks in the list.");
-        printLine();
+    private static void addDeadline(String input) throws DukeException {
+        String[] words = input.split(" ");
+        int index = input.indexOf("/");
+        if (words.length < 2 || index == -1) {
+            throw new DukeException("☹ OOPS!!! The description or date of a deadline cannot be empty.");
+        } else {
+            String[] wordDeadline = input.substring(9).split("/");
+            Deadline deadline = new Deadline(wordDeadline[0].trim(), wordDeadline[1].substring(3));
+            tasks[taskCounter++] = deadline;
+            printLine();
+            System.out.println("Got it. I've added this task:");
+            System.out.print("\t  ");
+            System.out.println(deadline);
+            System.out.println("Now you have " + taskCounter + " tasks in the list.");
+            printLine();
+        }
     }
 
-    private static void addEvent(String input) {
-        String[] wordEvent = input.substring(5).split("/", 3);
-        Event event = new Event(wordEvent[0].trim(), wordEvent[1].substring(5), wordEvent[2].substring(3));
-        tasks[counter++] = event;
-        printLine();
-        System.out.println("Got it. I've added this task:");
-        System.out.print("\t  ");
-        System.out.println(event);
-        System.out.println("Now you have " + counter + " tasks in the list.");
-        printLine();
+    private static void addEvent(String input) throws DukeException {
+        String[] words = input.split(" ");
+        int index = input.indexOf("/");
+        if (words.length < 2 || index == -1) {
+            throw new DukeException("☹ OOPS!!! The description or time of an event cannot be empty.");
+        } else {
+            String[] wordEvent = input.substring(5).split("/", 3);
+            Event event = new Event(wordEvent[0].trim(), wordEvent[1].substring(5), wordEvent[2].substring(3));
+            tasks[taskCounter++] = event;
+            printLine();
+            System.out.println("Got it. I've added this task:");
+            System.out.print("\t  ");
+            System.out.println(event);
+            System.out.println("Now you have " + taskCounter + " tasks in the list.");
+            printLine();
+        }
     }
 
     private static void runDuke() {
@@ -102,18 +123,40 @@ public class Duke {
         String input;
         input = in.nextLine();
         while (!input.equals("bye")) {
-            if (input.equals("list")) {
-                printList();
-            } else if (input.startsWith("mark")) {
-                markAsDone(input);
-            } else if (input.startsWith("unmark")) {
-                markAsUndone(input);
-            } else if (input.startsWith("todo")) {
-                addTodo(input);
-            } else if (input.startsWith("deadline")) {
-                addDeadline(input);
-            } else if (input.startsWith("event")) {
-                addEvent(input);
+            try {
+                String[] words = input.split(" ");
+                switch (words[0]) {
+                case "list":
+                    printList();
+                    break;
+
+                case "mark":
+                    markAsDone(input);
+                    break;
+
+                case "unmark":
+                    markAsUndone(input);
+                    break;
+
+                case "todo":
+                    addTodo(input);
+                    break;
+
+                case "deadline":
+                    addDeadline(input);
+                    break;
+
+                case "event":
+                    addEvent(input);
+                    break;
+
+                default:
+                    throw new DukeException(" ☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
+            } catch (DukeException e) {
+                printLine();
+                System.out.println(e.getMessage());
+                printLine();
             }
             input = in.nextLine();
         }
