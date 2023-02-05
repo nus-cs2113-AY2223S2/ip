ac29bbf65857853bc969e5cf82fdc68c7c6901f0
sistine-yu <uusissi@connect.hku.hk>
2023-02-05 12:07:48 +0800

    Add Packages

--- src/main/java/duke/command/Duke.java
+++ src/main/java/duke/command/Duke.java
@@ -1,3 +1,10 @@
+package duke.command;
+
+import duke.task.Deadline;
+import duke.task.Event;
+import duke.task.Task;
+import duke.task.Todo;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
