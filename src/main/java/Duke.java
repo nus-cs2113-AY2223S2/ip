9a514c8fdbcfdb75ca2c94c4778a76da5a151921
sistine-yu <uusissi@connect.hku.hk>
2023-02-05 11:53:00 +0800

    Add Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,52 +6,77 @@ public class Duke {
     static int size = tasks.size();
 
     public static void add_todo(String line){
-        String command = line.substring(5);
-        Todo t = new Todo(command);
-        tasks.add(t);
-        System.out.println("Got it. I've added this task: ");
-        System.out.println(t);
-        size = tasks.size();
-        print_list_count();
+        try {
+            String command = line.substring(5);
+            Todo t = new Todo(command);
+            tasks.add(t);
+            System.out.println("Got it. I've added this task: ");
+            System.out.println(t);
+            size = tasks.size();
+            print_list_count();
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println(":(OOPS! Please pay attention to the input format.");
+        }
     }
 
     public static void add_deadline(String line){
         String command = line.substring(9);
-        String task_content = command.split("/")[0];
-        String by_time = command.split("/")[1].substring(3);
-        Deadline d = new Deadline(task_content, by_time);
-        tasks.add(d);
-        System.out.println("Got it. I've added this task: ");
-        System.out.println(d);
-        size = tasks.size();
-        print_list_count();
+        try {
+            String task_content = command.split("/")[0];
+            String by_time = command.split("/")[1].substring(3);
+            Deadline d = new Deadline(task_content, by_time);
+            tasks.add(d);
+            System.out.println("Got it. I've added this task: ");
+            System.out.println(d);
+            size = tasks.size();
+            print_list_count();
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println(":(OOPS! Please pay attention to the input format.");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(":(OOPS! Please pay attention to the input format.");
+        }
     }
 
     public static void add_event(String line){
-        String command = line.substring(6);
-        String task_content = command.split("/")[0];
-        String from_time = command.split("/")[1].substring(5);
-        String to_time = command.split("/")[2].substring(3);
-        Event e = new Event(task_content, from_time, to_time);
-        tasks.add(e);
-        System.out.println("Got it. I've added this task: ");
-        System.out.println(e);
-        size = tasks.size();
-        print_list_count();
+        try {
+            String command = line.substring(6);
+            String task_content = command.split("/")[0];
+            String from_time = command.split("/")[1].substring(5);
+            String to_time = command.split("/")[2].substring(3);
+            Event e = new Event(task_content, from_time, to_time);
+            tasks.add(e);
+            System.out.println("Got it. I've added this task: ");
+            System.out.println(e);
+            size = tasks.size();
+            print_list_count();
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println(":(OOPS! Please pay attention to the input format.");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(":(OOPS! Please pay attention to the input format.");
+        }
     }
 
     public static void mark_done(String line){
-        int index = Integer.parseInt(line.split(" ")[1])-1;
-        tasks.get(index).setDone(true);
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println(tasks.get(index).toString());
+        try {
+            int index = Integer.parseInt(line.split(" ")[1]) - 1;
+            tasks.get(index).setDone(true);
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(tasks.get(index).toString());
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("OOPS! The maximum number of task index is " + tasks.size());
+        }
+
     }
 
     public static void unmark(String line){
-        int index = Integer.parseInt(line.split(" ")[1])-1;
-        tasks.get(index).setDone(false);
-        System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println(tasks.get(index).toString());
+        try {
+            int index = Integer.parseInt(line.split(" ")[1]) - 1;
+            tasks.get(index).setDone(false);
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println(tasks.get(index).toString());
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("OOPS! The maximum number of task index is " + tasks.size());
+        }
     }
 
     public static void list_tasks(String line){
@@ -61,7 +86,7 @@ public class Duke {
         }
     }
 
-    public static void print_list_count(){
+    public static void print_list_count() {
         if(size == 1) {
             System.out.println("Now you have 1 task in the list.");
         }
@@ -70,7 +95,7 @@ public class Duke {
         }
     }
 
-    public static void main(String[] args) {
+    public static void greeting() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -82,13 +107,15 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+    }
 
+    public static void main(String[] args) {
 
+        greeting();
 
         while(true){
-            String line = "";
             Scanner in = new Scanner(System.in);
-            line = in.nextLine();
+            String line = in.nextLine();
             if(line.equals("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
@@ -110,6 +137,9 @@ public class Duke {
             }
             else if(line.startsWith("event")){
                 add_event(line);
+            }
+            else {
+                System.out.println(":( OOPS!!! Please choose from the command: todo, deadline, event, list, mark, unmark, bye.");
             }
         }
     }
