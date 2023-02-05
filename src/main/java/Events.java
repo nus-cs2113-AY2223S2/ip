6756d81b41ba8303ecf4a6162a5ed47f72e7fbf0
kishore-a00 <skippyepic@gmail.com>
2023-02-05 20:39:03 +0800

    Add Events class

---
+++ src/main/java/Events.java
@@ -0,0 +1,35 @@
+public class Events extends Task {
+    private String eventDate;
+    private String startTIme;
+    private String endTime;
+    public Events(String description, String eventDate, String startTime, String endTime) {
+        super(description, "E");
+        setEventDate(eventDate);
+        setStartTIme(startTime);
+        setEndTime(endTime);
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public String getStartTIme() {
+        return startTIme;
+    }
+
+    public void setStartTIme(String startTIme) {
+        this.startTIme = startTIme;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+}
