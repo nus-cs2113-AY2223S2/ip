5d93ec80abc07ead0c43153f07c5a5734af7b2a0
kishore-a00 <skippyepic@gmail.com>
2023-02-05 20:34:04 +0800

    Add Deadline class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,18 @@
+public class Deadline extends Task {
+
+    private String dueInfo;
+
+    public Deadline(String description, String dueInfo) {
+        super(description, "D");
+        setDueInfo(dueInfo);
+    }
+
+    public String getDueInfo() {
+        return dueInfo;
+    }
+
+    public void setDueInfo(String dueInfo) {
+        this.dueInfo = dueInfo;
+    }
+
+}
