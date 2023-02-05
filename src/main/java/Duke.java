b85467231e0748445fec22e699cef3493d65f174
Chao Yi-Ju <sketch.2048@gmail.com>
2023-02-05 17:27:48 +0800

    Add exceptions for event

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -80,7 +80,17 @@ public class Duke {
             }
             break;
         case "event":
-            addEvent(commandArgs);
+            try {
+                addEvent(commandArgs);
+            } catch (NoDescriptionException e) {
+                System.out.println("WOOFS!!! The description of a event cannot be empty.");
+                System.out.println("Please try to add event again υ´• ﻌ •`υ");
+                printLine();
+            } catch (FormatException e) {
+                System.out.println("WOOFS!!! The format of entering event is incorrect.");
+                System.out.println("Please try to add event again υ´• ﻌ •`υ");
+                printLine();
+            }
             break;
         case "mark":
             markTask(commandArgs);
@@ -119,12 +129,18 @@ public class Duke {
         printLine();
     }
 
-    private static void addEvent(String commandArgs) {
+    private static void addEvent(String commandArgs) throws NoDescriptionException, FormatException{
         final int indexOfFrom = commandArgs.indexOf("/from");
         final int indexOfTo = commandArgs.indexOf("/to");
+        if (indexOfTo == -1 || indexOfFrom == -1) {
+            throw new FormatException();
+        }
         String eventDescription = commandArgs.substring(0, indexOfFrom).trim();
         String from = commandArgs.substring(indexOfFrom, indexOfTo).trim().replace("/from", "");
         String to = commandArgs.substring(indexOfTo, commandArgs.length()).trim().replace("/to", "");
+        if (eventDescription.trim().length() == 0 || from.trim().length() == 0 || to.trim().length() == 0) {
+            throw new NoDescriptionException();
+        }
         addTask(new Event(eventDescription, from, to));
         printLine();
         System.out.println("Got it. I've added this task: \n" + tasks[taskCount - 1]);
