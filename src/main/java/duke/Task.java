25efc8e6ff684d7f9dc3e060f0bac97468f9b18e
denzelcjy <denzel_cjy@hotmail.com>
2023-02-05 16:49:40 +0800

    Update Duke Level 5 - removed unneccessary try-catch

--- src/main/java/duke/Task.java
+++ src/main/java/duke/Task.java
@@ -1,4 +1,5 @@
 package duke;
+
 import java.util.ArrayList;
 
 public class Task {
@@ -46,7 +47,7 @@ public class Task {
         System.out.println("    ____________________________________________________________");
         System.out.println("     Here are the tasks in your list:");
         for (int i = 0; i < taskList.size(); i++) {
-            System.out.println("     " + (i+1) + "." + taskList.get(i));
+            System.out.println("     " + (i + 1) + "." + taskList.get(i));
         }
         System.out.println("    ____________________________________________________________");
     }
@@ -71,60 +72,47 @@ public class Task {
         System.out.println("    ____________________________________________________________");
     }
 
-    public static void addTodoTask(String description, ArrayList<Task> taskList) {
-        try {
-            if (description.isEmpty()) {
-                //for the case where user keys in a space after the command
-                throw new DukeException("     ☹ OOPS!!! The description of a todo cannot be empty.");
-            }
-            Todo newTodoTask = new Todo(description);
-            taskList.add(newTodoTask);
-            Task.printAddedTask(newTodoTask, taskList.size());
-
-        } catch (DukeException exception) {
-            printErrorMessage(exception.getMessage());
+    public static void addTodoTask(String description, ArrayList<Task> taskList) throws DukeException {
+        if (description.isEmpty()) {
+            //for the case where user keys in a space after the command
+            throw new DukeException("     ☹ OOPS!!! The description of a todo cannot be empty.");
         }
+        Todo newTodoTask = new Todo(description);
+        taskList.add(newTodoTask);
+        Task.printAddedTask(newTodoTask, taskList.size());
     }
 
-    public static void addDeadlineTask(String description, ArrayList<Task> taskList) {
-        try {
-            if (description.isEmpty()) {
-                //for the case where user keys in a space after the command
-                throw new DukeException("     ☹ OOPS!!! The description of a deadline cannot be empty.");
-            }
-            int firstSlashIndex = description.indexOf("/");
-            int detailsEndIndex = firstSlashIndex - 1;
-            String details = description.substring(0, detailsEndIndex);
-            int byStartIndex = firstSlashIndex + 4;
-            String by = description.substring(byStartIndex);
-            Deadline newDeadlineTask = new Deadline(details, by);
-            taskList.add(newDeadlineTask);
-            Task.printAddedTask(newDeadlineTask, taskList.size());
-        } catch (DukeException exception) {
-            printErrorMessage(exception.getMessage());
+    public static void addDeadlineTask(String description, ArrayList<Task> taskList) throws DukeException {
+        if (description.isEmpty()) {
+            //for the case where user keys in a space after the command
+            throw new DukeException("     ☹ OOPS!!! The description of a deadline cannot be empty.");
         }
+        int firstSlashIndex = description.indexOf("/");
+        int detailsEndIndex = firstSlashIndex - 1;
+        String details = description.substring(0, detailsEndIndex);
+        int byStartIndex = firstSlashIndex + 4;
+        String by = description.substring(byStartIndex);
+        Deadline newDeadlineTask = new Deadline(details, by);
+        taskList.add(newDeadlineTask);
+        Task.printAddedTask(newDeadlineTask, taskList.size());
     }
 
-    public static void addEventTask(String description, ArrayList<Task> taskList) {
-        try {
-            if (description.isEmpty()) {
-                //for the case where user keys in a space after the command
-                throw new DukeException("     ☹ OOPS!!! The description of a event cannot be empty.");
-            }
-            int firstSlashIndex = description.indexOf("/");
-            int secondSlashIndex = description.indexOf("/", firstSlashIndex + 1);
-            int detailsEndIndex = firstSlashIndex - 1;
-            String details = description.substring(0, detailsEndIndex);
-            int fromStartIndex = firstSlashIndex + 6;
-            int fromEndIndex = secondSlashIndex - 1;
-            String from = description.substring(fromStartIndex, fromEndIndex);
-            int toStartIndex = secondSlashIndex + 4;
-            String to = description.substring(toStartIndex);
-            Event newEventTask = new Event(details, from, to);
-            taskList.add(newEventTask);
-            Task.printAddedTask(newEventTask, taskList.size());
-        } catch (DukeException exception) {
-            printErrorMessage(exception.getMessage());
+    public static void addEventTask(String description, ArrayList<Task> taskList) throws DukeException {
+        if (description.isEmpty()) {
+            //for the case where user keys in a space after the command
+            throw new DukeException("     ☹ OOPS!!! The description of a event cannot be empty.");
         }
+        int firstSlashIndex = description.indexOf("/");
+        int secondSlashIndex = description.indexOf("/", firstSlashIndex + 1);
+        int detailsEndIndex = firstSlashIndex - 1;
+        String details = description.substring(0, detailsEndIndex);
+        int fromStartIndex = firstSlashIndex + 6;
+        int fromEndIndex = secondSlashIndex - 1;
+        String from = description.substring(fromStartIndex, fromEndIndex);
+        int toStartIndex = secondSlashIndex + 4;
+        String to = description.substring(toStartIndex);
+        Event newEventTask = new Event(details, from, to);
+        taskList.add(newEventTask);
+        Task.printAddedTask(newEventTask, taskList.size());
     }
 }
