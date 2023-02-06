c60fa722b744400b803d8f05e784bb37ef7e6c01
kishore-a00 <skippyepic@gmail.com>
2023-02-06 16:21:12 +0800

    Remove Duke logo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,12 +3,6 @@ import java.util.Arrays;
 
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(logo);
         greeting();
         Task[] taskStorage = new Task[100];
 
