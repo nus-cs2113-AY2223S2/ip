681dd8c8fd9ab10784aee225b1393d7647802c5f
chongyongrui <yrchong2000@gmail.com>
2023-02-06 21:32:57 +0800

    add packages to package constants and input checking methods

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -1,3 +1,5 @@
+package Constants;
+
 public final class Constants {
     public static final int MAX_NUMBER_OF_ENTRIES = 100;
     public static final String[] listOfKeywords = {"event","todo","deadline","mark","unmark","list", "bye"};
