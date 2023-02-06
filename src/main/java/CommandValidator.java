34aff77e52dce183fac24930338f2f17f4725890
nichyjt <nicholas.yek@gmail.com>
2023-02-06 09:14:05 +0800

    Update documentation for correctness
    Signed-off-by: nichyjt <nicholas.yek@gmail.com>

--- src/main/java/CommandValidator.java
+++ src/main/java/CommandValidator.java
@@ -7,7 +7,7 @@ public class CommandValidator {
 
     // Takes in a command type to check the commandPayload map against
     // Checks for 2 things:
-    // 1. Required commands are present
+    // 1. All required commands are present
     // 2. Only the required commands are present
     public void validateCommandPayloadMap(Command command, HashMap commandPayload) throws InvalidCommandException {
         String[] requiredSubcommands = command.getSubcommandNames();
