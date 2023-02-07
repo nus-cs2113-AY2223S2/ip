9f5321473a643b0ad903b13f71001aed1f3c3ae8
Rayleigh47 <yanming8870@outlook.com>
2023-02-07 22:58:57 +0800

    changed packages branch

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 public class Duke {
 
     public static void main(String[] args) throws DukeException {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
         Conversation.line();
         Conversation.greet();
         Conversation.question();
