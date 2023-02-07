d37c2b18beea8dfe3e15acd5823797b9fb2cae09
11-Navya <e0913130@u.nus.edu>
2023-02-08 00:20:50 +0800

    Error handling

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,3 @@
-
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -20,14 +19,13 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
 
-        while(!line.equals("bye")) {
+        while (!line.equals("bye")) {
             System.out.println("____________________________________________________________\n");
-            if (line.equals("list")) {
+
+            if (line.equals("list") && !tasks.isEmpty()) {
                 System.out.println("Here are the tasks in your list: \n ");
                 for (int i = 0; i < tasks.size(); i++) {
                     Task todoList = tasks.get(i);
-                    //System.out.println("\t" + (i+1) + "." + "[" + todoList.getStatusIcon() + "]" +
-                    //        " " + todoList.description);
                     System.out.println("\t" + (i + 1) + ". " + todoList.toString());
                 }
                 System.out.println("____________________________________________________________\n");
@@ -35,69 +33,102 @@ public class Duke {
 
             else if (line.contains("unmark")) {
                 int index = line.indexOf(" ");
-                String str= line.substring(index + 1);
-                int pos = Integer.parseInt(str);
-                Task taskUnmarked = tasks.get(pos - 1);
-                taskUnmarked.setStatusIcon(false);
-                System.out.printf("" + "Okay, I've marked this task as not done yet:\n" + "\t" + " " + "[" +
-                        taskUnmarked.getStatusIcon() + "]" + " "+ taskUnmarked.description + "\n");
-                System.out.println("____________________________________________________________\n");
+                if (line.length() > 7 && (tasks.size() >= index)){
+                    String str = line.substring(index + 1);
+                    int pos = Integer.parseInt(str);
+                    Task taskUnmarked = tasks.get(pos - 1);
+                    taskUnmarked.setStatusIcon(false);
+                    System.out.printf("" + "Okay, I've marked this task as not done yet:\n" + "\t" + " " + "[" +
+                            taskUnmarked.getStatusIcon() + "]" + " " + taskUnmarked.description + "\n");
+                    System.out.println("____________________________________________________________\n");
+                }
+                else{
+                    System.out.println("🤪Sorry please drink some coffee and enter valid unmark command");
+                    System.out.println("____________________________________________________________\n");
+                }
             }
 
             else if (line.contains("mark")) {
                 int index = line.indexOf(" ");
-                String str= line.substring(index + 1);
-                int pos = Integer.parseInt(str);
-                Task taskMarked = tasks.get(pos - 1);
-                taskMarked.setStatusIcon(true);
-                System.out.printf(" " + "Nice! I've marked this task as done:\n" + "\t" + " " + "[" +
-                        taskMarked.getStatusIcon() + "]" + " "+ taskMarked.description + "\n");
-                System.out.println("____________________________________________________________\n");
+                if (line.length() > 5 && (tasks.size() >= index)){
+                    String str = line.substring(index + 1);
+                    int pos = Integer.parseInt(str);
+                    Task taskMarked = tasks.get(pos - 1);
+                    taskMarked.setStatusIcon(true);
+                    System.out.printf(" " + "Nice! I've marked this task as done:\n" + "\t" + " " + "[" +
+                            taskMarked.getStatusIcon() + "]" + " " + taskMarked.description + "\n");
+                    System.out.println("____________________________________________________________\n");
+                }
+                else{
+                    System.out.println("🤪Sorry please drink some coffee and enter valid mark command");
+                    System.out.println("____________________________________________________________\n");
+                }
             }
 
-            else if (line.contains("todo")){
-                int index = line.indexOf(" ");
-                String activity = line.substring(index, line.length());
-                Todo toDo = new Todo(activity);
-                tasks.add(toDo);
-                System.out.println("Added: " + "\n" + toDo.toString() + "\n");
-                System.out.println("You have " + tasks.size() + " number of tasks in you list.");
-                System.out.println("____________________________________________________________\n");
+            else if (line.contains("todo")) {
+                if (line.length() > 5){
+                    int index = line.indexOf(" ");
+                    String activity = line.substring(index, line.length());
+                    Todo toDo = new Todo(activity);
+                    tasks.add(toDo);
+                    System.out.println("Added: " + "\n" + toDo.toString() + "\n");
+                    System.out.println("You have " + tasks.size() + " number of tasks in you list.");
+                    System.out.println("____________________________________________________________\n");
+                }
+                else {
+                    System.out.println("🤪Drink coffee and enter a valid todo command");
+                    System.out.println("____________________________________________________________\n");
+                }
             }
 
-            else if (line.contains("deadline")){
-                int index = line.indexOf(" ");
-                int pos = line.indexOf("/");
-                String activity = line.substring(index, pos - 1);
-                String dueDate = line.substring(pos + 3, line.length());
-                Deadline deadline = new Deadline(activity,dueDate);
-                tasks.add(deadline);
-                System.out.println("Added: " + "\n" + deadline.toString() + "\n");
-                System.out.println("You have " + tasks.size() + " number of tasks in you list.");
-                System.out.println("____________________________________________________________\n");
+            else if (line.contains("deadline")) {
+                if (line.length() > 9 && line.contains("/")){
+                    int index = line.indexOf(" ");
+                    int pos = line.indexOf("/");
+                    String activity = line.substring(index, pos - 1);
+                    String dueDate = line.substring(pos + 3, line.length());
+                    Deadline deadline = new Deadline(activity, dueDate);
+                    tasks.add(deadline);
+                    System.out.println("Added: " + "\n" + deadline.toString() + "\n");
+                    System.out.println("You have " + tasks.size() + " number of tasks in you list.");
+                    System.out.println("____________________________________________________________\n");
+                }
+                else{
+                    System.out.println("🤪Drink coffee and enter a valid deadline command");
+                    System.out.println("____________________________________________________________\n");
+                }
             }
 
-            else if (line.contains("event")){
-                int index = line.indexOf(" ");
-                int startDate = line.indexOf("/");
-                String activity = line.substring(index, startDate);
-                String timeFrame = line.substring(startDate + 3, line.length());
-                Event event = new Event(activity,timeFrame);
-                tasks.add(event);
-                System.out.println("Added: " + "\n" + event.toString() + "\n");
-                System.out.println("You have " + tasks.size() + " number of tasks in you list.");
+            else if (line.contains("event")) {
+                if (line.length() > 6 && line.contains("/")){
+                    int index = line.indexOf(" ");
+                    int startDate = line.indexOf("/");
+                    String activity = line.substring(index, startDate);
+                    String timeFrame = line.substring(startDate + 3, line.length());
+                    Event event = new Event(activity, timeFrame);
+                    tasks.add(event);
+                    System.out.println("Added: " + "\n" + event.toString() + "\n");
+                    System.out.println("You have " + tasks.size() + " number of tasks in you list.");
+                    System.out.println("____________________________________________________________\n");
+                }
+                else{
+                    System.out.println("🤪Drink coffee and enter a valid event command");
+                    System.out.println("____________________________________________________________\n");
+                }
+            }
+
+            else if(tasks.isEmpty()){
+                System.out.println("🤪The list is empty. Do something in order to have tasks in your list");
                 System.out.println("____________________________________________________________\n");
             }
 
             else {
-                System.out.println("\t" + "added: " + line);
+                System.out.println("🤪Sorry enter a valid Duke command");
                 System.out.println("____________________________________________________________\n");
-                Task newTask = new Task(line);
-                tasks.add(newTask);
-
             }
             line = in.nextLine();
         }
+        System.out.println("____________________________________________________________\n");
         System.out.println(" Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n");
     }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -8,6 +8,6 @@ public class Event extends Task{
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (on: " + at + ")";
+        return "[E]" + super.toString() + " (from: " + at + ")";
     }
 }
