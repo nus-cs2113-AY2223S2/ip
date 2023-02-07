9876a3366620db9c1a2bdf5bff4715303260a8cd
vishnu4157 <e0727211@u.nus.edu>
2023-02-07 18:26:00 +0800

    added a class for the error handler

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 
 public class Duke {
-
     public static String parseEventString(String input) {
         int startIndex = input.indexOf("from") + 5;
         int endStartIndex = input.indexOf("to");
@@ -14,31 +13,6 @@ public class Duke {
         return input;
     }
 
-    public static Boolean isInputValid(String input) {
-        if (input.startsWith("event")) {
-            if (input.length() < 7) {
-                System.out.println("event cannot be empty, please try again!");
-                return false;
-            } else if (!input.contains("/from") || !input.contains("/to")) {
-                System.out.println("Please provide a BOTH a valid from and to \nex: event project meeting /from Mon 2pm /to 4pm");
-                return false;
-            }
-        } else if (input.startsWith("deadline")) {
-            if (input.length() < 10) {
-                System.out.println("Deadline cannot be empty, please try again!");
-                return false;
-            } else if (!input.contains("/by")) {
-                System.out.println("Please provide a valid deadline \nex: deadline return book /by Sunday");
-                return false;
-            }
-        } else if (input.startsWith("todo")) {
-            if (input.length() < 6) {
-                System.out.println("Todo cannot be empty, please try again!");
-                return false;
-            }
-        }
-        return true;
-    }
 
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
@@ -54,7 +28,7 @@ public class Duke {
         Todos[] todo = new Todos[100];
         int new_tasks = 0;
         while (!input.equalsIgnoreCase("bye")) {
-            if (!isInputValid(input)) {
+            if (!ErrorHandler.isInputValid(input)) {
                 input = myObj.nextLine();
                 continue;
             }
