136a8463c701d6611ebc461b0154afe9a321e3ec
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2023-02-07 22:39:47 +0800

    Level 5. Handle Errors

--- src/main/java/Jonathan.java
+++ src/main/java/Jonathan.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -10,7 +11,7 @@ public class Jonathan {
                 "    What can I do for you?\n" + line + "\n";
         String goodbye = "    Bye. Hope to see you again soon!\n" + line;
 
-        Task[] list = new Task[100];
+        ArrayList<Task> list = new ArrayList<>();
         int taskCounter = 0;
 
         System.out.println(welcome);
@@ -19,48 +20,73 @@ public class Jonathan {
         while (true) {
             command = input.nextLine(); // Prompt input from user
 
-            if (Objects.equals(command, "bye")) {
-                System.out.println(goodbye);
-                break; // quit the program
+            try {
+                if (Objects.equals(command, "bye")) {
+                    System.out.println(goodbye);
+                    break; // quit the program
 
-            } else if (Objects.equals(command, "list")) {
-                lookupTask(list, taskCounter);
+                } else if (Objects.equals(command, "list")) {
+                    lookupTask(list, taskCounter);
 
-            } else if (command.split(" ")[0].equals("mark") || command.split(" ")[0].equals("unmark")) {
-                boolean isDone = command.split(" ")[0].equals("mark");
-                int taskNum = Integer.parseInt(command.split(" ")[1]);
-                Task task = list[taskNum-1];
-                changeStatus(task, isDone);
+                } else if (command.split(" ")[0].equals("mark") || command.split(" ")[0].equals("unmark")) {
+                    boolean isDone = command.split(" ")[0].equals("mark");
+                    int taskNum = Integer.parseInt(command.split(" ")[1]);
+                    Task task = list.get(taskNum-1);
+                    changeStatus(task, isDone);
 
-            } else if (command.split(" ")[0].equals("todo")) {
-                String description = command.substring(command.indexOf(" ") + 1);
-                Todo todo = new Todo(description);
-                list[taskCounter] = todo;
-                taskCounter += 1;
-                addedMessage(todo, taskCounter);
+                } else if (command.split(" ")[0].equals("todo")) {
 
-            } else if (command.split(" ")[0].equals("deadline")) {
-                String description = command.substring(command.indexOf(" ") + 1, command.indexOf(" /by"));
-                String by = command.substring(command.indexOf("/by") + 4);
-                Deadline deadline = new Deadline(description, by);
-                list[taskCounter] = deadline;
-                taskCounter += 1;
-                addedMessage(deadline, taskCounter);
+                    if (command.split(" ").length == 1) {
+                        throw new StringIndexOutOfBoundsException();
+                    }
 
-            } else if (command.split(" ")[0].equals("event")) {
-                String description = command.substring(command.indexOf(" ") + 1, command.indexOf(" /from"));
-                String start = command.substring(command.indexOf("/from") + 6, command.indexOf(" /to"));
-                String end = command.substring(command.indexOf("/to") + 4);
-                Event event = new Event(description, start, end);
-                list[taskCounter] = event;
-                taskCounter += 1;
-                addedMessage(event, taskCounter);
+                    String description = command.substring(command.indexOf(" ") + 1);
+                    Todo todo = new Todo(description);
+                    list.add(todo);
+                    taskCounter += 1;
+                    addedMessage(todo, taskCounter);
 
-            } else { // add Task
+                } else if (command.split(" ")[0].equals("deadline")) {
+                    String description = command.substring(command.indexOf(" ") + 1, command.indexOf(" /by"));
+                    String by = command.substring(command.indexOf("/by") + 4);
+                    Deadline deadline = new Deadline(description, by);
+                    list.add(deadline);
+                    taskCounter += 1;
+                    addedMessage(deadline, taskCounter);
+
+                } else if (command.split(" ")[0].equals("event")) {
+                    String description = command.substring(command.indexOf(" ") + 1, command.indexOf(" /from"));
+                    String start = command.substring(command.indexOf("/from") + 6, command.indexOf(" /to"));
+                    String end = command.substring(command.indexOf("/to") + 4);
+                    Event event = new Event(description, start, end);
+                    list.add(event);
+                    taskCounter += 1;
+                    addedMessage(event, taskCounter);
+
+                } else {
+                    throw new JonathanException();
+                }
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("    ____________________________________________________________\n" +
+                        "    The task doesn't exist, please type it correctly!\n" +
+                        "    ____________________________________________________________\n");
+
+            } catch (StringIndexOutOfBoundsException e) {
+                System.out.println("    ____________________________________________________________\n" +
+                        "    Please type with the following format:\n\n" +
+                        "    - todo <description>\n" +
+                        "    - deadline <description> /by <time>\n" +
+                        "    - event <descripton> /from <time> /to <time>\n\n" +
+                        "    note: without the angle bracket\n" +
+                        "    ____________________________________________________________\n");
+
+            } catch (JonathanException e) {
                 System.out.println("    ____________________________________________________________\n" +
                         "    Wrong input, please type it correctly!\n" +
                         "    ____________________________________________________________\n");
             }
+
         }
     }
 
@@ -78,11 +104,11 @@ public class Jonathan {
     /*
     Method to print all the Task, including the Task's status
      */
-    public static void lookupTask(Task[] list, int counter) {
+    public static void lookupTask(ArrayList<Task> list, int counter) {
         System.out.println("    ____________________________________________________________\n" +
                 "    Here are the tasks in your list:");
         for (int i = 0; i < counter; i++) {
-            System.out.println("    " + (i+1) + ". "  + list[i]);
+            System.out.println("    " + (i+1) + ". "  + list.get(i));
         }
         System.out.println("    ____________________________________________________________\n");
     }
---
+++ src/main/java/JonathanException.java
@@ -0,0 +1,2 @@
+public class JonathanException extends Exception {
+}
