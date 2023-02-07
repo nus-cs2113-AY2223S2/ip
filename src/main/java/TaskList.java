6d31a1c86efa54a6e6d11799744720eb1c1fc4df
Rayleigh47 <yanming8870@outlook.com>
2023-02-07 22:42:50 +0800

    updated exception handling

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -9,17 +9,26 @@ public class TaskList {
             System.out.println((i + 1) + ". " + tasks.get(i).fullDescription());
         }
     }
-    public static void addToList(String command) {
+    public static void addToList(String command) throws DukeException {
         String[] arrayOfWords = command.split(" ", 2);
         switch(arrayOfWords[0]) {
         case "todo":
+            if(arrayOfWords.length < 2) {
+                throw new DukeException("OOPS! Use case: todo TASK");
+            }
             tasks.add(new Todo(arrayOfWords[1]));
             break;
         case "deadline":
+            if(!command.contains("/by")) {
+                throw new DukeException("OOPS! Use case: deadline X /by Y");
+            }
             String[] arrayOfDeadline = arrayOfWords[1].split("/by");
             tasks.add(new Deadline(arrayOfDeadline[0].trim(), arrayOfDeadline[1].trim()));
             break;
         case "event":
+            if(!command.contains("/from") || !command.contains("/to")) {
+                throw new DukeException("OOPS! Use case: event X /from Y /to Z");
+            }
             String[] arrayOfEvent = arrayOfWords[1].split("/from");
             String eventDescription = arrayOfEvent[0];
             String eventStart = arrayOfEvent[1].split("/to", 2)[0].trim();
@@ -27,13 +36,13 @@ public class TaskList {
             tasks.add(new Event(eventDescription, eventStart, eventEnd));
             break;
         default:
-            tasks.add(new Task(command));
+            throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
         }
         System.out.print("added: ");
         Conversation.copy(command);
         System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
     }
-    public static void mark(String command) {
+    public static void mark(String command) throws DukeException {
         String[] arrOfCommand = command.split(" ");
         try {
             Integer.parseInt(arrOfCommand[1]);
@@ -46,7 +55,7 @@ public class TaskList {
         System.out.println("Nice! I've marked this task as done:");
         System.out.println(tasks.get(taskNumber).fullDescription());
     }
-    public static void unmark(String command) {
+    public static void unmark(String command) throws DukeException {
         String[] arrOfCommand = command.split(" ");
         try {
             Integer.parseInt(arrOfCommand[1]);
