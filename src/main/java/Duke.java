5b5737d997619183895e2ef2c7c802ae482b304a
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-02-08 18:11:37 +0000

    Shifted write method to class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,5 @@
 import java.util.Scanner;  // Import the Scanner class
 import java.io.File; // Import the File class
-import java.io.FileWriter; // Import the FileWriter class
-import java.io.BufferedWriter; // Import the BufferedWriter class
-import java.io.IOException; // Import the IOException class to handle errors
 
 public class Duke {
 
@@ -21,17 +18,26 @@ public class Duke {
                 String[] lineSplit = line.split(" \\| ");
                 if(lineSplit[0].equals("T")){
                     tasks[taskCount] = new Todo(lineSplit[2]);
+                    if(lineSplit.length > 3){
+                        throw new DukeException("Your data file is corrupted");
+                    }
                     if(lineSplit[1].equals("true")){
                         tasks[taskCount].markAsDone();
                     }
                     taskCount++;
                 }else if(lineSplit[0].equals("D")){
+                    if(lineSplit.length > 4){
+                        throw new DukeException("Your data file is corrupted");
+                    }
                     tasks[taskCount] = new Deadline(lineSplit[2], lineSplit[3]);
                     if(lineSplit[1].equals("true")){
                         tasks[taskCount].markAsDone();
                     }
                     taskCount++;
                 }else if(lineSplit[0].equals("E")){
+                    if(lineSplit.length > 4){
+                        throw new DukeException("Your data file is corrupted");
+                    }
                     tasks[taskCount] = new Event(lineSplit[2], lineSplit[3]);
                     if(lineSplit[1].equals("true")){
                         tasks[taskCount].markAsDone();
@@ -39,20 +45,21 @@ public class Duke {
                     taskCount++;
                 }
             }
-            //print out the tasks
-            System.out.println("Here are the tasks in your old list:");
-            for(int i=0;i<taskCount;i++){
-                System.out.println((i+1) + ". " + tasks[i]);
-            }
+            //print the tasks in the file
+            
             fileScan.close();
         //otherwise create a new file
         } catch(Exception e){
-            System.out.println("Creating new file...");
+            //Create a new file and directory
+            try{
+                File file = new File("data/duke.txt");
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }catch(Exception e1){
+                e1.printStackTrace();
+            }
         }
-        //Print a new line
-        System.out.println("____________________________________________________________");
 
-    
         //Print out the logo
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -240,45 +247,8 @@ public class Duke {
         //Close the scanner
         userScan.close();
 
-        //Save the tasks to the file
-        try{
-        //Create a file writer
-        FileWriter fw = new FileWriter("data/duke.txt");
-        //Erase the file
-        fw.write("");
-        //Create a buffered writer
-        BufferedWriter bw = new BufferedWriter(fw);
         //Write the tasks to the file
-        for(int i=0;i<taskCount;i++){
-            //if the task is a todo
-            if(tasks[i] instanceof Todo){
-                //write the task to the file
-                bw.write("T | " + tasks[i].isDone + " | " + tasks[i].taskName);
-                bw.newLine();
-            }
-            //if the task is a deadline
-            else if(tasks[i] instanceof Deadline){
-                //write the task to the file
-                bw.write("D | " + tasks[i].isDone + " | " + tasks[i].taskName + " | " + ((Deadline) tasks[i]).deadline);
-                bw.newLine();
-            }
-            //if the task is an event
-            else if(tasks[i] instanceof Event){
-                //write the task to the file
-                bw.write("E | " + tasks[i].isDone + " | " + tasks[i].taskName + " | " + ((Event) tasks[i]).eventTime);
-                bw.newLine();
-            }
-
-
-        } 
-        //Close the buffered writer
-        bw.close();
-
-        //Close the file writer
-        fw.close();
-        } catch(IOException e){
-            System.out.println("Error writing to file.");
-        }
+        FileRW.writeToFile(tasks, taskCount);
         
     }
 
