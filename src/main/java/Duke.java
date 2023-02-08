30de721553d945d38bdd4b9c07ec94e6b7d7380b
Rayleigh47 <yanming8870@outlook.com>
2023-02-08 17:47:23 +0800

    update main

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,8 @@
-import Exceptions.DukeException;
 import Tasks.TaskList;
 import UI.Conversation;
 
 public class Duke {
+    //run duke
     public static void main(String[] args) {
         Conversation.intro();
         boolean isContinue = true;
