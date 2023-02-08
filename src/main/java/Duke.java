7bfdaeee30022dc19aa81ecab1e8256201f558a1
ChongQiRong <e0727229@u.nus.edu>
2023-02-08 15:40:24 +0800

    Add exceptions for event, deadline, mark and list command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -27,22 +27,8 @@ public class Duke {
         horizontalLine();
     }
 
-//    public static void echo(String userInput) {
-//        horizontalLine();
-//        System.out.println("added: " + userInput);
-//        horizontalLine();
-//    }
-
-    private static void listCommand(Task[] tasks) {
-        horizontalLine();
-        for (int i = 0; i < Task.getNumberOfTasks(); i += 1) {
-            System.out.println(tasks[i].getTaskNumber() + "." + tasks[i]);
-        }
-        horizontalLine();
-    }
-
     public static String[] processLine(String userInput) {
-       return userInput.split(" ");
+        return userInput.split(" ");
     }
 
     public static void taskAdded(Task t) {
@@ -55,27 +41,35 @@ public class Duke {
 
     public static void processCommands(Task[] tasks, String userInput) throws DukeException {
         String[] words = processLine(userInput);
-        try {
-            if (words[0].equals("list")) {
-                listCommand(tasks);
-            } else if (words[0].equals("todo")) {
-                todoCommand(tasks, userInput);
-            } else if (words[0].equals("event")) {
-                eventCommand(tasks, userInput);
-            } else if (words[0].equals("deadline")) {
-                deadlineCommand(tasks, userInput);
-            } else if (words[0].contains("mark")) {
-                markUnmarkCommand(tasks, words);
-            } else {
-                throw new DukeException(ErrorMessage.INVALID_COMMAND.toString());
-            }
-        } finally {
 
+        if (words[0].equals("list")) {
+            listCommand(tasks);
+        } else if (words[0].equals("todo")) {
+            todoCommand(tasks, userInput);
+        } else if (words[0].equals("event")) {
+            eventCommand(tasks, userInput);
+        } else if (words[0].equals("deadline")) {
+            deadlineCommand(tasks, userInput);
+        } else if (words[0].contains("mark")) {
+            markUnmarkCommand(tasks, words);
+        } else {
+            throw new DukeException(ErrorMessage.INVALID_COMMAND.toString());
         }
-
     }
 
-    private static void todoCommand(Task[] tasks, String userInput) throws DukeException{
+    private static void listCommand(Task[] tasks) throws DukeException {
+        if (Task.getNumberOfTasks() > 0) {
+            horizontalLine();
+            for (int i = 0; i < Task.getNumberOfTasks(); i += 1) {
+                System.out.println(tasks[i].getTaskNumber() + "." + tasks[i]);
+            }
+            horizontalLine();
+        } else {
+            throw new DukeException(ErrorMessage.EMPTY_LIST.toString());
+        }
+    }
+
+    private static void todoCommand(Task[] tasks, String userInput) throws DukeException {
         try {
             userInput = userInput.substring(5);
             tasks[Task.getNumberOfTasks()] = new Todo(userInput);
@@ -85,33 +79,60 @@ public class Duke {
         }
     }
 
-    private static void eventCommand(Task[] tasks, String userInput) {
-        int positionOfSlash = userInput.indexOf("/");
-        String startEnd = userInput.substring(positionOfSlash + 1);
-        String description = userInput.substring(6, positionOfSlash - 1);
-        positionOfSlash = startEnd.indexOf("/");
-        String start = startEnd.substring(5, positionOfSlash - 1);
-        String end = startEnd.substring(positionOfSlash + 4);
-        tasks[Task.getNumberOfTasks()] = new Event(description, start, end);
-        taskAdded(tasks[Task.getNumberOfTasks() - 1]);
+    private static void eventCommand(Task[] tasks, String userInput) throws DukeException { //event project meeting /from Mon 2pm /to 4pm
+        try {
+            int positionOfSlash = userInput.indexOf("/");
+            if (positionOfSlash == -1) {
+                throw new DukeException(ErrorMessage.MISSING_TWO_EVENT_PARAMETER.toString());
+            }
+            String startEnd = userInput.substring(positionOfSlash + 1);
+            positionOfSlash = startEnd.indexOf("/");
+            if (positionOfSlash == -1) {
+                throw new DukeException(ErrorMessage.MISSING_ONE_EVENT_PARAMETER.toString());
+            }
+            String start = startEnd.substring(5, positionOfSlash - 1);
+            String end = startEnd.substring(positionOfSlash + 4);
+
+            String description = userInput.substring(6, positionOfSlash - 1);
+            tasks[Task.getNumberOfTasks()] = new Event(description, start, end);
+            taskAdded(tasks[Task.getNumberOfTasks() - 1]);
+        } catch (IndexOutOfBoundsException error) {
+            throw new DukeException(ErrorMessage.MISSING_EVENT_PARAMETER.toString());
+        }
     }
 
-    private static void deadlineCommand(Task[] tasks, String userInput) {
-        int positionOfSlash = userInput.indexOf("/");
-        String by = userInput.substring(positionOfSlash + 4);
-        String description = userInput.substring(9, positionOfSlash - 1);
-        tasks[Task.getNumberOfTasks()] = new Deadline(description, by);
-        taskAdded(tasks[Task.getNumberOfTasks() - 1]);
+    private static void deadlineCommand(Task[] tasks, String userInput) throws DukeException {
+        int positionOfBy = userInput.indexOf("/by");
+        if (positionOfBy == -1) {
+            throw new DukeException(ErrorMessage.MISSING_DEADLINE_BY_PARAMETER.toString());
+        }
+
+        try {
+            String by = userInput.substring(positionOfBy + 4);
+            try {
+                String description = userInput.substring(9, positionOfBy - 1);
+                tasks[Task.getNumberOfTasks()] = new Deadline(description, by);
+                taskAdded(tasks[Task.getNumberOfTasks() - 1]);
+            } catch (IndexOutOfBoundsException error) {
+                throw new DukeException(ErrorMessage.MISSING_DEADLINE_PARAMETER.toString());
+            }
+        } catch (IndexOutOfBoundsException error) {
+            throw new DukeException(ErrorMessage.EMPTY_DEADLINE_BY_PARAMETER.toString());
+        }
     }
 
-    private static void markUnmarkCommand(Task[] tasks, String[] words) {
-        int taskNumber = Integer.parseInt(words[1]) - 1;
+    private static void markUnmarkCommand(Task[] tasks, String[] words) throws DukeException {
         horizontalLine();
+        int taskNumber = Integer.parseInt(words[1]);
+        if （taskNumber < Task.getNumberOfTasks()）{
+            throw new DukeException(ErrorMessage.INVALID_TASK.toString());
+        }
+
         if (words[0].equals("mark")) {
-            tasks[taskNumber].markDone();
+            tasks[taskNumber - 1].markDone();
             System.out.println("Nice! I've marked this task as done:");
         } else {
-            tasks[taskNumber].markUndone();
+            tasks[taskNumber - 1].markUndone();
             System.out.println("OK, I've marked this task as not done yet:");
         }
         System.out.println(tasks[taskNumber]);
@@ -134,12 +155,10 @@ public class Duke {
                 processCommands(tasks, userInput);
             } catch (Exception error) {
                 horizontalLine();
-                System.out.println(error.getMessage());
+                System.out.println("Error message: " + error.getMessage());
                 horizontalLine();
             }
         }
         exit();
     }
-
-
 }
