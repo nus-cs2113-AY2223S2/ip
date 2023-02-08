e68ef1472b61ca9532672075b73905f3434e67bf
haoyangw <haoyangw@gmail.com>
2023-02-09 00:59:21 +0800

    Add our own RuntimeException class
    Have RicaException to represent all of Rica's internal exceptions and RicaTaskException(subclass of RicaException)
      to represent Rica's task-related exceptions

---
+++ src/main/java/RicaTaskException.java
@@ -0,0 +1,10 @@
+public class RicaTaskException extends RicaException {
+
+    public RicaTaskException() {
+        super();
+    }
+
+    public RicaTaskException(String msg) {
+        super(msg);
+    }
+}
