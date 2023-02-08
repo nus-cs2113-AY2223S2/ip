dccfb91acfb956535dfc2fc6fe551a29dfbaf354
moseslee9012 <mlee9012@gmail.com>
2023-02-08 16:23:30 +0800

    level 1: greet, echo, exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
 public class Duke {
+
+    public static void echo(String line) {
+        String exitCommand = "bye";
+        while (!line.equals(exitCommand)) {
+            System.out.println(line);
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+        }
+        System.out.println("Bye! Hope to see you again");
+    }
+    
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,5 +20,10 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hi! My name is Duke. \n Nice to meet you!");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        echo(line);
     }
 }
