b96bd1ac9a3a2583d09f6025e71dc9255c4102eb
Manoj364 <theerazor@outlook.com>
2023-02-09 20:28:14 +0800

    Move all classes into a package

--- src/main/java/duke/Todo.java
+++ src/main/java/duke/Todo.java
@@ -1,5 +1,7 @@
+package duke;
+
 public class Todo extends Task {
-    protected boolean isDone;
+    public boolean isDone;
     public Todo(String description) {
         super(description);
         this.isDone = false;
