1bf5e36b9a69a67b93847300bdba31f74fd77162
TJ\user <e0773494@u.nus.edu>
2023-02-09 21:43:34 +0800

    A-CodingStandard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,33 +1,34 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.security.InvalidAlgorithmParameterException;
+import java.util.MissingFormatArgumentException;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
         Scanner in = new Scanner(System.in);
-        ArrayList<Task> taskList = new ArrayList<Task>();
+        ArrayList<Task> tasks = new ArrayList<Task>();
 
         String userInput;
         while(in.hasNext()){
-
             userInput = in.nextLine();
             if(userInput.equals("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
                 break;
-            }
-            if(userInput.equals("list")) {
-                for (int i = 0; i < taskList.size(); i++) {
+            } else if(userInput.contains("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
                     System.out.print(i + 1 + ".");
-                    System.out.println(taskList.get(i).toString());
+                    System.out.println(tasks.get(i).toString());
                 }
             } else if (userInput.contains("todo")) {
                 String info = userInput.substring(5).trim();
-                taskList.add(new Todo(info));
-                int finalTask = taskList.size() - 1;
+                tasks.add(new Todo(info));
+                int finalTask = tasks.size() - 1;
                 System.out.println("Got it. I've added this task:");
-                System.out.println(taskList.get(finalTask).toString());
-                if (taskList.size() == 1){
-                    System.out.println("Now you have " +taskList.size()+" task in the list.");
+                System.out.println(tasks.get(finalTask).toString());
+                if (tasks.size() == 1) {
+                    System.out.println("Now you have " +tasks.size()+" task in the list.");
                 } else {
-                    System.out.println("Now you have " + taskList.size() + " tasks in the list.");
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                 }
             } else if (userInput.contains("deadline")) {
                 String [] listArray = userInput.split("/",2);
@@ -35,14 +36,14 @@ public class Duke {
                 String dueDate = listArray[1];
                 String info = description.substring(8).trim();
                 String due = dueDate.substring(3).trim();
-                taskList.add(new Deadline(info, due));
-                int finalTask = taskList.size() - 1;
+                tasks.add(new Deadline(info, due));
+                int finalTask = tasks.size() - 1;
                 System.out.println("Got it. I've added this task:");
-                System.out.println(taskList.get(finalTask).toString());
-                if (taskList.size() == 1){
-                    System.out.println("Now you have " +taskList.size()+" task in the list.");
+                System.out.println(tasks.get(finalTask).toString());
+                if (tasks.size() == 1) {
+                    System.out.println("Now you have " + tasks.size() + " task in the list.");
                 } else {
-                    System.out.println("Now you have " + taskList.size() + " tasks in the list.");
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                 }
             } else if (userInput.contains("event")) {
                 String [] listArray = userInput.split("/",3);
@@ -52,34 +53,33 @@ public class Duke {
                 String info = description.substring(6).trim();
                 String start = startTime.substring(5).trim();
                 String end = endTime.substring(3).trim();
-                taskList.add(new Event(info, start, end));
-                int finalTask = taskList.size() - 1;
+                tasks.add(new Event(info, start, end));
+                int finalTask = tasks.size() - 1;
                 System.out.println("Got it. I've added this task:");
-                System.out.println(taskList.get(finalTask).toString());
-                if (taskList.size() == 1){
-                    System.out.println("Now you have " +taskList.size()+" task in the list.");
+                System.out.println(tasks.get(finalTask).toString());
+                if (tasks.size() == 1){
+                    System.out.println("Now you have " + tasks.size() + " task in the list.");
                 } else {
-                    System.out.println("Now you have " + taskList.size() + " tasks in the list.");
+                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
                 }
-            } else if(userInput.contains("unmark") || userInput.contains("mark")){
+            } else if(userInput.contains("mark")){
                 Integer itemNumber;
-                String [] IndexArr = userInput.split(" ",2);
+                String [] commandIndex = userInput.split(" ",2);
                 itemNumber = Integer.parseInt(IndexArr[1])-1;
-                if (userInput.contains("unmark")) {
+                if (userInput.equals("unmark")) {
                     System.out.println("OK, I've marked this task as not done yet:");
-                    taskList.get((int) itemNumber).isCompleted = false;
-                    System.out.println(taskList.get(itemNumber).toString());
+                    tasks.get((int) itemNumber).isCompleted = false;
+                    System.out.println(tasks.get(itemNumber).toString());
                 }
                 else {
                     System.out.println("Nice! I've marked this task as done:");
-                    taskList.get(itemNumber).isCompleted = true;
-                    System.out.println(taskList.get(itemNumber).toString());
+                    tasks.get(itemNumber).isCompleted = true;
+                    System.out.println(tasks.get(itemNumber).toString());
                 }
             } else {
-                taskList.add(new Task(userInput,false));
-                System.out.println("added: "+userInput);
+                tasks.add(new Task(userInput, false));
+                System.out.println("added: " + userInput);
             }
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
