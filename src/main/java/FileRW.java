0236b6fecf00ae420763a94874084b0c20d43b26
DaDevChia <88506363+DaDevChia@users.noreply.github.com>
2023-02-09 05:12:11 +0000

    Separated fileRW methods and fixed arraylist

--- src/main/java/FileRW.java
+++ src/main/java/FileRW.java
@@ -3,11 +3,16 @@ import java.io.File; // Import the File class
 import java.io.FileWriter; // Import the FileWriter class
 import java.io.BufferedWriter; // Import the BufferedWriter class
 import java.io.IOException; // Import the IOException class to handle errors
+import java.util.ArrayList; // Import the ArrayList class
 
 public class FileRW {
    
-    //method that takes in input tasks and taskCount and reads the file and writes the tasks into the array
-    public static void readFromFile(Task[] tasks, int taskCount)  {
+    //method that tand reads the file and returns an arraylist of tasks 
+    public static ArrayList<Task> readFile(){
+        //new arraylist of tasks
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        //new taskCount
+        int taskCount = 0;
         //check if there is a file to read from
         try{
             File file = new File("data/duke.txt");
@@ -16,35 +21,34 @@ public class FileRW {
                 String line = fileScan.nextLine();
                 String[] lineSplit = line.split(" \\| ");
                 if(lineSplit[0].equals("T")){
-                    tasks[taskCount] = new Todo(lineSplit[2]);
+                    tasks.add(new Todo(lineSplit[2]));
                     if(lineSplit.length > 3){
                         throw new DukeException("Your data file is corrupted");
                     }
                     if(lineSplit[1].equals("true")){
-                        tasks[taskCount].markAsDone();
+                        tasks.get(taskCount).markAsDone();
                     }
                     taskCount++;
                 }else if(lineSplit[0].equals("D")){
                     if(lineSplit.length > 4){
                         throw new DukeException("Your data file is corrupted");
                     }
-                    tasks[taskCount] = new Deadline(lineSplit[2], lineSplit[3]);
+                    tasks.add(new Deadline(lineSplit[2], lineSplit[3]));
                     if(lineSplit[1].equals("true")){
-                        tasks[taskCount].markAsDone();
+                        tasks.get(taskCount).markAsDone();
                     }
                     taskCount++;
                 }else if(lineSplit[0].equals("E")){
                     if(lineSplit.length > 4){
                         throw new DukeException("Your data file is corrupted");
                     }
-                    tasks[taskCount] = new Event(lineSplit[2], lineSplit[3]);
+                    tasks.add(new Event(lineSplit[2], lineSplit[3]));
                     if(lineSplit[1].equals("true")){
-                        tasks[taskCount].markAsDone();
+                        tasks.get(taskCount).markAsDone();
                     }
                     taskCount++;
                 }
             }
-            //print the tasks in the file
             
             fileScan.close();
         //otherwise create a new file
@@ -58,13 +62,15 @@ public class FileRW {
                 e1.printStackTrace();
             }
         }
+
+        return tasks;
         
     }
 
     //method that takes in input tasks and taskCount and writes the tasks into the file
-    public static void writeToFile(Task[] tasks, int taskCount)  {
-        //Save the tasks to the file
-        try{
+    public static void writeFile(ArrayList<Task> tasks, int taskCount){
+        //Write the tasks to the file
+         try{
             //Create a file writer
             FileWriter fw = new FileWriter("data/duke.txt");
             //Erase the file
@@ -74,21 +80,21 @@ public class FileRW {
             //Write the tasks to the file
             for(int i=0;i<taskCount;i++){
                 //if the task is a todo
-                if(tasks[i] instanceof Todo){
+                if(tasks.get(i) instanceof Todo){
                     //write the task to the file
-                    bw.write("T | " + tasks[i].isDone + " | " + tasks[i].taskName);
+                    bw.write("T | " + tasks.get(i).isDone + " | " + tasks.get(i).taskName);
                     bw.newLine();
                 }
                 //if the task is a deadline
-                else if(tasks[i] instanceof Deadline){
+                else if(tasks.get(i) instanceof Deadline){
                     //write the task to the file
-                    bw.write("D | " + tasks[i].isDone + " | " + tasks[i].taskName + " | " + ((Deadline) tasks[i]).deadline);
+                    bw.write("D | " + tasks.get(i).isDone + " | " + tasks.get(i).taskName + " | " + ((Deadline) tasks.get(i)).deadline);
                     bw.newLine();
                 }
                 //if the task is an event
-                else if(tasks[i] instanceof Event){
+                else if(tasks.get(i) instanceof Event){
                     //write the task to the file
-                    bw.write("E | " + tasks[i].isDone + " | " + tasks[i].taskName + " | " + ((Event) tasks[i]).eventTime);
+                    bw.write("E | " + tasks.get(i).isDone + " | " + tasks.get(i).taskName + " | " + ((Event) tasks.get(i)).eventTime);
                     bw.newLine();
                 }
             } 
@@ -102,6 +108,7 @@ public class FileRW {
         } catch(IOException e){
             System.out.println("Error writing to file.");
         }
+
         
     }
 
