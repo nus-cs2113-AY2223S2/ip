9e21f2bb6a9c2a7be40103b6a359bafd1bf41319
LAPTOP-DQSB43T9\deepa <deepanjali.dhawan@gmail.com>
2023-02-09 11:52:50 +0800

    Commit Level-2: Add, List

--- src/main/java/IPChat.java
+++ src/main/java/IPChat.java
@@ -2,17 +2,33 @@ import java.util.Scanner;
 
 public class IPChat {
     public static void main(String[] args) {
-        // Level 1: Greet Echo exit
-        Scanner input =  new Scanner(System.in);
+
+        // Level 2: Add, List
         System.out.println("Hello I'm IPChat, What can I do for you");
         System.out.println("------------------------------------------");
 
-        String statement = input.next();
+        String[] items = new String [100]; // array to store the items in the list
+        Scanner input =  new Scanner(System.in);
+        String statement = input.nextLine();
+        int count = 0;
+
         while(!statement.equals("bye")) {
-            System.out.println(statement);
             System.out.println("------------------------------------------");
-            statement = input.next();
+            if (statement.equals("list")) {
+                for (int i = 0; i < count; i += 1) {
+                    System.out.println((i + 1) + ". " + items[i]);
+                }
+                System.out.println("------------------------------------------");
+            } else {
+                items[count] = statement;
+                count += 1;
+                System.out.println("added: " + statement);
+                System.out.println("------------------------------------------");
+            }
+            statement = input.nextLine();
+
         }
+        System.out.println("------------------------------------------");
         System.out.println("Bye, Hope to see you soon");
         System.out.println("------------------------------------------");
     }
