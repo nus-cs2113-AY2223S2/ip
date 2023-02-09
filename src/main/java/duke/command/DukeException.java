c13ba82ee62ca5eba94661f146157cd117e3b690
ngkaiwen123 <e0726144@u.nus.edu>
2023-02-09 15:32:16 +0800

    -Changed comments

--- src/main/java/duke/command/DukeException.java
+++ src/main/java/duke/command/DukeException.java
@@ -1,7 +1,7 @@
 package duke.command;
 
 /**
- * Custom exception used to process exceptions in the 'Duke' program.
+ * Custom exception made, used for processing exceptions in the 'Duke' program.
  */
 public class DukeException extends Exception {
 
