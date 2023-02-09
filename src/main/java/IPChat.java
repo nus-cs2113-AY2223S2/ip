ad81f69edcecc007ff7b2b8d208c45e013bed41e
LAPTOP-DQSB43T9\deepa <deepanjali.dhawan@gmail.com>
2023-02-09 12:46:02 +0800

    Commit A-CodingStandard

--- src/main/java/IPChat.java
+++ src/main/java/IPChat.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class IPChat {
     public static void main(String[] args) {
 
@@ -8,31 +7,31 @@ public class IPChat {
         System.out.println("------------------------------------------");
 
         Scanner input =  new Scanner(System.in);
-        String statement = input.nextLine();
+        String statements = input.nextLine(); // changed statement to statements
         Task[] tasks = new Task [100];
-        int tasks_count = 0;
+        int tasksCount = 0; // used camelCase
 
-        while(!statement.equals("bye")) {
+        while(!statements.equals("bye")) {
             System.out.println("------------------------------------------");
-            if (statement.equals("list")) {
+            if (statements.equals("list")) {
                 System.out.println("Here is the list of tasks for the day! All the best :) \n");
-                for (int i = 0; i < tasks_count; i += 1) {
+                for (int i = 0; i < tasksCount; i += 1) {
                     System.out.println((i + 1) + "." +"[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
                 }
                 System.out.println("------------------------------------------");
-            } else if (statement.contains("mark")) {
+            } else if (statements.contains("mark")) {
                 System.out.println("I have marked the task as done");
-                int index = Integer.parseInt(statement.substring(statement.length()-1)) - 1;
-                tasks[index].markAsDone();
+                int taskIndex = Integer.parseInt(statements.substring(statements.length()-1)) - 1; // changed index to taskIndex
+                tasks[taskIndex].markAsDone();
                 System.out.println("------------------------------------------");
             } else {
-                tasks[tasks_count] = new Task(statement);
-                tasks_count += 1;
+                tasks[tasksCount] = new Task(statements);
+                tasksCount += 1;
                 System.out.println("Adding new tasks to the list");
-                System.out.println("added: " + statement);
+                System.out.println("added: " + statements);
                 System.out.println("------------------------------------------");
             }
-            statement = input.nextLine();
+            statements = input.nextLine();
 
         }
         System.out.println("------------------------------------------");
