f45a8dab2d2c952ef203bbe4abc936e3a8ee2106
ghzr0 <glenn262b@gmail.com>
2023-02-10 02:24:04 +0800

    1. Added increment Level 5 to handle errors inputted by user 2. Created DukeException class to store and output error scenarios and messages 3. Repackaged and reorganised previous code with methods to facilitate readability

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,22 @@
+public class DukeException extends Exception{
+    private static final String MARGIN = "*----------------------------*";
+
+    public void todoError(){
+        System.out.println(MARGIN);
+        System.out.println("☹ OOPS!!! The description of a todo cannot be empty.");
+        System.out.println(MARGIN);
+    }
+
+    public void eventError(){
+        System.out.println(MARGIN);
+        System.out.println("☹ OOPS!!! The description of a event cannot be empty.");
+        System.out.println(MARGIN);
+    }
+
+    public void deadlineError(){
+        System.out.println(MARGIN);
+        System.out.println("☹ OOPS!!! The description of a deadline cannot be empty.");
+        System.out.println(MARGIN);
+    }
+
+}
