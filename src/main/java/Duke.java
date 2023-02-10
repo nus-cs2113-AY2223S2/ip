e4db506d8867fb764215f2fab091efa34739e86b
Koh_Jing_Jie_Marcus <88085589+Koh-Jing-Jie-Marcus@users.noreply.github.com>
2023-02-10 10:18:48 +0800

    Add code to handle errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,16 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Duke {
+    private static void checkError(String input) throws DukeException{
+        if (Objects.equals(input, "event") || Objects.equals(input, "todo") || Objects.equals(input, "deadline")) {
+            throw new DukeException("The description of the body cannot be empty! Please enter a proper input.");
+        } else if (Objects.equals(input, "")) {
+            throw new DukeException("You did not enter any input! Please enter a proper input.");
+        } else {
+            throw new DukeException("I'm sorry, but i don't know what that means. Please enter a proper input.");
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,6 +25,7 @@ public class Duke {
 
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
+
         boolean isBye = false;
         Task[] inputs = new Task[100];
         for(int k = 0; k < 100; k++) {
@@ -48,25 +59,37 @@ public class Duke {
                     inputs[order - 1].markUndone();
                     System.out.println("OK, I've marked this task as not done yet:\n" + inputs[order - 1]);
 
+            } else if (Objects.equals(input, "event") || Objects.equals(input, "todo") || Objects.equals(input, "deadline")) {
+                try {
+                    checkError(input);
+                } catch(DukeException e) {
+                    System.out.println("Error: " + e);
+                }
             } else {
-                if (input.substring(0,4).equals("todo")) {
+                if (input.length() > 3 && input.substring(0,4).equals("todo")) {
                     String info = input.substring(5,input.length());
                     inputs[count] = new Todo(info);
                     inputs[count].isDone = false;
                     System.out.println("Got it. I've added this task: \n" + inputs[count] + "\nNow you have " + (count + 1) + " tasks in your list." );
-                } else if (input.substring(0,8).equals("deadline")) {
+                } else if (input.length() > 7 && input.substring(0,8).equals("deadline")) {
                     String info = input.substring(9,input.indexOf("/"));
                     String timeBy = input.substring(input.indexOf("/")+1, input.length());
                     inputs[count] = new Deadline(info, timeBy);
                     inputs[count].isDone = false;
                     System.out.println("Got it. I've added this task: \n" + inputs[count] + "\nNow you have " + (count + 1) + " tasks in your list." );
-                } else {
+                } else if (input.length() > 4 && input.substring(0,5).equals("event")) {
                     String info = input.substring(6,input.indexOf("/"));
                     String timeFrom = input.substring(input.indexOf("/")+1, input.lastIndexOf("/") - 1);
                     String timeBy = input.substring(input.lastIndexOf("/")+1, input.length());
                     inputs[count] = new Event(info, timeFrom, timeBy);
                     inputs[count].isDone = false;
                     System.out.println("Got it. I've added this task: \n" + inputs[count] + "\nNow you have " + (count + 1) + " tasks in your list." );
+                } else {
+                    try {
+                        checkError(input);
+                    } catch(DukeException e) {
+                        System.out.println("Error: " + e);
+                    }
                 }
                 count++;
             }
