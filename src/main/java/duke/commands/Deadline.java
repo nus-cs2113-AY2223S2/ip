3dd67b6f0210e26a4aad240af7de676fbce8c8b7
kyrixn <keyrixn@gmail.com>
2023-02-10 16:08:47 +0800

    created exceptions and hendled erros

---
+++ src/main/java/duke/commands/Deadline.java
@@ -0,0 +1,32 @@
+package commands;
+
+import exceptions.LackOfTaskDetail;
+
+public class Deadline extends Task {
+    String due;
+
+    private static boolean containTime(String s) {
+        boolean haveContent = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                haveContent = true;
+                break;
+            }
+        }
+
+        return haveContent;
+    }
+
+    public Deadline(String discription, String due) throws LackOfTaskDetail {
+        super(discription);
+        if (due.equals("") || !containTime(due)) {
+            throw new LackOfTaskDetail("No time information!");
+        }
+        this.due = due;
+    }
+
+    public String toString() {
+        return "[D]" + super.toString() + " (" + due + ")";
+    }
+}
