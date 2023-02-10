c0e20e4fa8522ed8e1e90d8ab08ef39abc182af1
Nigel Chen Chin Hao <nigel_ncch@hotmail.com>
2023-02-11 04:04:44 +0800

    Add support to load tasks from disk

--- src/main/java/genesis/Genesis.java
+++ src/main/java/genesis/Genesis.java
@@ -15,9 +15,24 @@ import exception.GenesisException;
 import exception.UnknownCommandException;
 
 public class Genesis {
-    private static ArrayList<Task> tasks = new ArrayList<Task>();
+    private ArrayList<Task> tasks;
 
-    private static void handleListTasks() throws GenesisException {
+    public Genesis() {
+        try {
+            this.tasks = FileHandler.loadFromFile();
+            System.out.println("Task list loaded successfully.");
+        } catch (IOException e) {
+            this.tasks = new ArrayList<>();
+            System.out.println("There is no existing task list found. Initializing new task list.");
+        } catch (IndexOutOfBoundsException | GenesisException e) {
+            this.tasks = new ArrayList<>();
+            System.out.println("Task list is corrupted. Initializing new task list.");
+        } finally {
+            ConsolePrinter.breakLine();
+        }
+    }
+
+    private void handleListTasks() throws GenesisException {
         if (tasks.size() < 1) {
             throw new GenesisException("List is empty, nothing to show");
         }
@@ -25,7 +40,7 @@ public class Genesis {
         ConsolePrinter.listTasks(tasks);
     }
 
-    private static void handleMarkTask(String content) {
+    private void handleMarkTask(String content) {
         int index = Integer.parseInt(content) - 1;
         Task task = tasks.get(index);
         task.setIsDone(true);
@@ -33,7 +48,7 @@ public class Genesis {
         ConsolePrinter.onTaskMarked(task.getListDescription());
     }
 
-    private static void handleUnmarkTask(String content) {
+    private void handleUnmarkTask(String content) {
         int index = Integer.parseInt(content) - 1;
         Task task = tasks.get(index);
         task.setIsDone(false);
@@ -41,14 +56,14 @@ public class Genesis {
         ConsolePrinter.onTaskUnmarked(task.getListDescription());
     }
 
-    private static void handleTodo(String description) throws GenesisException {
+    private void handleTodo(String description) throws GenesisException {
         Todo todo = new Todo(description);
         tasks.add(todo);
 
         ConsolePrinter.onTaskAdded(todo.getListDescription(), tasks.size());
     }
 
-    private static void handleDeadline(String content) throws GenesisException {
+    private void handleDeadline(String content) throws GenesisException {
         String[] parts = content.split(" /by ", 2);
         if (parts.length < 2) {
             throw new GenesisException("Description for deadline is invalid");
@@ -63,7 +78,7 @@ public class Genesis {
         ConsolePrinter.onTaskAdded(deadline.getListDescription(), tasks.size());
     }
 
-    private static void handleEvent(String content) throws GenesisException {
+    private void handleEvent(String content) throws GenesisException {
         String[] parts = content.split(" /from | /to ", 3);
         if (parts.length < 3) {
             throw new GenesisException("Description for event is invalid");
@@ -79,19 +94,19 @@ public class Genesis {
         ConsolePrinter.onTaskAdded(event.getListDescription(), tasks.size());
     }
 
-    private static void validateIndex(String[] contentArr) throws GenesisException {
+    private void validateIndex(String[] contentArr) throws GenesisException {
         if (contentArr.length < 2) {
             throw new GenesisException("Index cannot be empty");
         }
     }
 
-    private static void validateDescription(String[] contentArr) throws GenesisException {
+    private void validateDescription(String[] contentArr) throws GenesisException {
         if (contentArr.length < 2 || contentArr[1].isBlank()) {
             throw new GenesisException("The description cannot be empty.");
         }
     }
 
-    public static void askGenesis(String userInput) {
+    public void askGenesis(String userInput) {
         try {
             ConsolePrinter.breakLine();
 
@@ -106,7 +121,6 @@ public class Genesis {
             case "mark": {
                 validateIndex(contentArr);
                 handleMarkTask(contentArr[1]);
-                FileHandler.saveToFile(tasks);
                 break;
             }
             case "unmark": {
@@ -145,7 +159,7 @@ public class Genesis {
         } catch (NumberFormatException e) {
             System.out.println("☹ OOPS!!! Index is not a number");
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("☹ OOPS!!! task.Task index does not exist");
+            System.out.println("☹ OOPS!!! Task index does not exist");
         } catch (IOException e) {
             System.out.println("☹ OOPS!!! Failed to update to local file storage");
         } finally {
--- src/main/java/utility/FileHandler.java
+++ src/main/java/utility/FileHandler.java
@@ -1,6 +1,7 @@
 package utility;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -8,10 +9,15 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import task.Task;
+import task.Todo;
+import task.Deadline;
+import task.Event;
+
+import exception.GenesisException;
 
 public class FileHandler {
-    final static String DIR_PATH = "./storage";
-    final static String FILE_PATH = "./storage/genesis_data.txt";
+    final static String DIR_PATH = "./store";
+    final static String FILE_PATH = "./store/genesis.txt";
 
     public static void saveToFile(ArrayList<Task> tasks) throws IOException {
         Files.createDirectories(Paths.get(DIR_PATH));
@@ -24,5 +30,39 @@ public class FileHandler {
 
         fw.flush();
         fw.close();
+    }
+
+    public static ArrayList<Task> loadFromFile() throws IOException, IndexOutOfBoundsException, GenesisException {
+
+        ArrayList<Task> tasks = new ArrayList<>();
+
+        File file = new File(FILE_PATH);
+        Scanner sc = new Scanner(file);
+        sc.useDelimiter("\n");
+
+        while (sc.hasNext()) {
+            String entry = sc.next();
+            String[] contentArr = entry.split(" \\| ");
+            switch (contentArr[0]) {
+            case "T":
+                Todo todo = new Todo(contentArr[2], Boolean.parseBoolean(contentArr[1]));
+                tasks.add(todo);
+                break;
+            case "D":
+                Deadline deadline = new Deadline(contentArr[2], contentArr[3], Boolean.parseBoolean(contentArr[1]));
+                tasks.add(deadline);
+                break;
+            case "E":
+                Event event = new Event(contentArr[2], contentArr[3], contentArr[4], Boolean.parseBoolean(contentArr[1]));
+                tasks.add(event);
+                break;
+            default:
+                throw new GenesisException("Type does not exist");
+            }
+        }
+
+        sc.close();
+
+        return tasks;
     }
 }
