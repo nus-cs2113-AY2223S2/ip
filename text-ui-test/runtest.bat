b0682c038b244ee66d037eb75380184dd80fe4cf
denzelcjy <denzel_cjy@hotmail.com>
2023-02-11 12:29:31 +0800

    Updated TextUiTesting with new inputs and runtest.bat, ignore duke.txt in TextUiTesting

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -6,6 +6,9 @@ if not exist ..\bin mkdir ..\bin
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
+REM delete duke.txt from previous run
+if exist data\duke.txt del data\duke.txt
+
 REM compile the code into the bin folder
 javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java ..\src\main\java\duke\*.java
 IF ERRORLEVEL 1 (
