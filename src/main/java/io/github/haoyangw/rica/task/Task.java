36c94f86a61712502381aab060d10ed5ce579cf4
haoyangw <haoyangw@gmail.com>
2023-02-11 18:50:00 +0800

    Task.class: Implement getType() as well to facilitate serializing

--- src/main/java/io/github/haoyangw/rica/task/Task.java
+++ src/main/java/io/github/haoyangw/rica/task/Task.java
@@ -1,10 +1,15 @@
 package io.github.haoyangw.rica.task;
 
 public class Task {
+    private static final String TYPE = "T";
     protected final String description;
 
     public Task(String description) {
         this.description = description;
+    }
+
+    protected String getType() {
+        return Task.TYPE;
     }
 
     public String getDescription() {
