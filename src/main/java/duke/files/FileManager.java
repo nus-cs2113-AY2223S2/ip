c7a60ca036ab188c5a82684d5daf88487f2e7fdf
jacob-stein1 <jmstein@bu.edu>
2023-02-11 16:24:56 +0800

    Read from file to task list

--- src/main/java/duke/files/FileManager.java
+++ src/main/java/duke/files/FileManager.java
@@ -3,17 +3,18 @@ package duke.files;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import duke.tasks.Task;
+import duke.tasks.*;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class FileManager {
 
+    private static final String filePath = "duke/files/data.txt";
     private static boolean isFile = false;
 
     public static boolean checkFile() {
-        File f = new File("duke/files/data.txt");
+        File f = new File(filePath);
         return (f.exists() && !f.isDirectory());
     }
 
@@ -39,14 +40,45 @@ public class FileManager {
         return taskStrings;
     }
 
-    public static void main(String[] args) {
-        System.out.println(checkFile());
-        try {
-            writeToFile("duke/files/data.txt", "hello");
-        } catch (IOException e) {
-            System.out.println("Something went wrong: " + e.getMessage());
+    private static void addTaskFromFile(String parsedTask) {
+        char type = parsedTask.charAt(1);
+        char status = parsedTask.charAt(4);
+        String description = parsedTask.substring(7);
+        Task temp;
+        if (type == 'T') {
+            temp = new Todo(description);
+        } else if (type == 'D') {
+            String dueDate = description.substring(description.indexOf(":")+2, description.indexOf(")"));
+            temp = new Deadline(description, dueDate);
+        } else {
+            String startTime = description.substring(description.indexOf("from"), description.indexOf("to"));
+            String endTime = description.substring(description.indexOf("to"));
+            endTime = endTime.substring(0, endTime.length()-1);
+            temp = new Event(description, startTime, endTime);
         }
-        Task.printAllTasks();
+        if (status == 'X'){
+            temp.isComplete = true;
+        }
+    }
+
+    private static void populateTaskList() throws FileNotFoundException {
+        try {
+            ArrayList<String> fileContents = retrieveFileContents(filePath);
+            for(int i = 0; i<fileContents.size(); i++) {
+                addTaskFromFile(fileContents.get(i));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error fetching file contents");
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            populateTaskList();
+            Task.printAllTasks();
+        } catch (FileNotFoundException e) {
+            System.out.print("Oops!");
+        }
     }
 
 }
