3262052c4571e7b1e9c005cd7fe7f83e9af2ca9e
moseslee9012 <mlee9012@gmail.com>
2023-02-11 10:15:23 +0800

    Level 3(revised)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Duke {
 
     private static Scanner in = new Scanner(System.in);
+
     public static String getStatus(int itemNumber, Boolean[] isDone) {
         if (isDone[itemNumber] == true) {
             return "[X]";
@@ -14,13 +15,17 @@ public class Duke {
     public static void command(String line) {
         String exitCommand = "bye";
         String toDo = "list";
-        String checkDone = "mark";
+        String checkOff = "mark";
+
         String[] toDoList = new String[100];
         Boolean[] isDone = new Boolean[100];
         int numberOfItems = 0;
 
-        while (!line.equals(exitCommand)) {
-            if (line.equals(toDo)) {
+        String[] tokens = line.split(" ", 0);
+        String command = tokens[0];
+
+        while (!command.equals(exitCommand)) {
+            if (tokens[0].equals(toDo)) {
                 System.out.println("Here are your list of tasks to complete!");
                 for (int i = 0; i < numberOfItems; ++i) {
                     System.out.format("%d. ", (i + 1));
@@ -28,9 +33,22 @@ public class Duke {
                     System.out.println(status + " " + toDoList[i]);
                 }
                 line = in.nextLine();
-            } else if (line.equals(checkDone)){
-                System.out.println("check done");
+                tokens = line.split(" ", 0);
+                command = tokens[0];
+            } else if (command.equals(checkOff)){
+                int checkDone = Integer.parseInt(tokens[1]) - 1;
+                isDone[checkDone] = true;
+                System.out.println("Alright! I have marked the task as complete!");
+
+                for (int i = 0; i < numberOfItems; ++i) {
+                    System.out.format("%d. ", (i + 1));
+                    String status = getStatus(i, isDone);
+                    System.out.println(status + " " + toDoList[i]);
+                }
+
                 line = in.nextLine();
+                tokens = line.split(" ", 0);
+                command = tokens[0];
             } else {
                 toDoList[numberOfItems] = line;
                 isDone[numberOfItems] = false;
@@ -38,6 +56,8 @@ public class Duke {
 
                 System.out.println("added: " + line);
                 line = in.nextLine();
+                tokens = line.split(" ", 0);
+                command = tokens[0];
             }
         }
         System.out.println("Bye! Hope to see you again");
