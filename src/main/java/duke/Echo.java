1891192ab2534f2081d1024b220a5118d8d9df55
nicholas132000 <beefnlemon@gmail.com>
2023-02-11 12:13:10 +0800

    Changed fixed sized array to Arraylist

--- src/main/java/duke/Echo.java
+++ src/main/java/duke/Echo.java
@@ -1,36 +1,12 @@
 package duke;//import java.util.Scanner;
-//
-//public class Echo {
-//    public static void Echo() {
-//        String line;
-//        Scanner in = new Scanner(System.in);
-//        line = in.nextLine();
-//        String[] tasks = new String[100];
-//        int count = 0;
-//        while (!(line.equals("Bye") || line.equals("bye"))) {
-//            if (!line.equals("list")) {
-//                System.out.println("added: " + line);
-//                tasks[count] = line;
-//                ++count;
-//                line = in.nextLine();
-//            } else {
-//                int index = 1;
-//                for (int i = 0; i < count; ++i) {
-//                    System.out.println(index + ": " + tasks[i]);
-//                    ++index;
-//                }
-//                line = in.nextLine();
-//            }
-//        }
-//        System.out.println("Bye. Hope to see you again!\n");
-//    }
-//}
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Echo {
     public static void Echo() {
-        Task[] tasks = new Task[100];
+        //Task[] tasks = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<>();
         int count = 0;
 
         Scanner in = new Scanner(System.in);
@@ -91,69 +67,69 @@ public class Echo {
         System.out.println("Bye. Hope to see you again!");
     }
 
-    private static int event_Input(Task[] tasks, int count, String task) throws DukeException {
+    private static int event_Input(ArrayList<Task> tasks, int count, String task) throws DukeException {
         if (task == " ") {
             throw new DukeException();
         }
         System.out.println("Got it. I've added this task:");
         String taskAsArray[] = task.split("/");
-        tasks[count] = new Event(taskAsArray[0], taskAsArray[1], taskAsArray[2]);
-        System.out.println(tasks[count].toString());
+        tasks.add(new Event(taskAsArray[0], taskAsArray[1], taskAsArray[2]));
+        System.out.println(tasks.get(count).toString());
         ++count;
         System.out.println("Now you have " + count + " tasks in the list.");
         return count;
     }
 
-    private static int deadline_Input(Task[] tasks, int count, String task) throws DukeException {
+    private static int deadline_Input(ArrayList<Task> tasks, int count, String task) throws DukeException {
         if (task == " ") {
             throw new DukeException();
         }
         System.out.println("Got it. I've added this task:");
         String taskAsArray[] = task.split("/");
-        tasks[count] = new Deadline(taskAsArray[0], taskAsArray[1]);
-        System.out.println(tasks[count].toString());
+        tasks.add(new Deadline(taskAsArray[0], taskAsArray[1]));
+        System.out.println(tasks.get(count).toString());
         ++count;
         System.out.println("Now you have " + count + " tasks in the list.");
         return count;
     }
 
-    private static int todo_Input(Task[] tasks, int count, String task) throws DukeException {
+    private static int todo_Input(ArrayList<Task> tasks, int count, String task) throws DukeException {
         if (task == " ") {
             throw new DukeException();
         }
         System.out.println("Got it. I've added this task:");
-        tasks[count] = new Todo(task);
-        System.out.println(tasks[count].toString());
+        tasks.add(new Todo(task));
+        System.out.println(tasks.get(count).toString());
         ++count;
         System.out.println("Now you have " + count + " tasks in the list.");
         return count;
     }
 
-    private static void mark_Input(Task[] tasks, String task) throws DukeException {
+    private static void mark_Input(ArrayList<Task> tasks, String task) throws DukeException {
         if (task == " ") {
             throw new DukeException();
         }
         int taskNoInt = Integer.parseInt(task);
-        tasks[taskNoInt - 1].markAsDone();
+        tasks.get(taskNoInt - 1).markAsDone();
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println(tasks[taskNoInt - 1].toString());
+        System.out.println(tasks.get(taskNoInt - 1).toString());
     }
 
-    private static void unmark_Input(Task[] tasks, String task) throws DukeException {
+    private static void unmark_Input(ArrayList<Task> tasks, String task) throws DukeException {
         if (task == " ") {
             throw new DukeException();
         }
         int taskNoInt = Integer.parseInt(task);
-        tasks[taskNoInt - 1].markAsUndone();
+        tasks.get(taskNoInt - 1).markAsUndone();
         System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println(tasks[taskNoInt - 1].toString());
+        System.out.println(tasks.get(taskNoInt - 1).toString());
     }
 
-    private static void list_Input(Task[] tasks, int count) {
+    private static void list_Input(ArrayList<Task> tasks, int count) {
         System.out.println("Here are the tasks in your list:");
         int index = 1;
-        for (int i = 0; i < count; ++i) {
-            System.out.println(index + "." + tasks[i].toString());
+        for (Task task : tasks) {
+            System.out.println(index + "." + task.toString());
             ++index;
         }
     }
