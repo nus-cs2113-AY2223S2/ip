465ea263e889f9582440238b8b65b45758a21d75
SSzeWen <seetszewen@gmail.com>
2023-02-12 14:29:17 +0800

    Add Database for saving of tasks

--- src/main/java/duke/Database.java
+++ src/main/java/duke/Database.java
@@ -6,12 +6,8 @@ import duke.task.Task;
 import duke.task.Todo;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -19,10 +15,13 @@ import java.util.Scanner;
 public class Database {
 
     protected ArrayList<Task> tasks;
+
+    protected ArrayList<String> databaseString;
     public static final String FILE_PATH = "data/duke.txt";
 
     public Database() {
         this.tasks = new ArrayList<>();
+        this.databaseString = new ArrayList<>();
         try {
             initialise();
         } catch (IOException e) {
@@ -30,42 +29,45 @@ public class Database {
         }
     }
 
-    public void saveAddTask(String[] information) throws IOException{
-        FileWriter fw = new FileWriter(FILE_PATH,true);
+    public void saveAddTask(String information) throws IOException {
+        FileWriter fw = new FileWriter(FILE_PATH, true);
         fw.write(writeTask(information));
         fw.close();
     }
 
-    public static String writeTask(String[] information) {
-        String output = "";
-        for (String string : information) {
-            output += string + " , ";
+    public void updateDatabaseTask() throws IOException {
+        FileWriter fw = new FileWriter(FILE_PATH);
+        for (int i = 0; i < tasks.size(); ++i) {
+            fw.write(writeTask(tasks.get(i).getTaskString()));
         }
-        output += System.lineSeparator();
-        return output;
+        fw.close();
     }
+
+    public static String writeTask(String information) {
+        information += System.lineSeparator();
+        return information;
+    }
+
     private void initialise() throws IOException {
         File savedData = new File(FILE_PATH);
         if (savedData.exists()) {
             Scanner contents = new Scanner(savedData);
-            ArrayList<String> databaseString = new ArrayList<>();
             while (contents.hasNext()) {
                 databaseString.add(contents.nextLine());
             }
-            dataConversion(databaseString);
+            dataConversion();
         } else {
             File directory = new File("data");
             directory.mkdirs();
             savedData.createNewFile();
         }
-
     }
 
-    private void dataConversion(ArrayList<String> databaseString) {
+    private void dataConversion() {
         for (String data : databaseString) {
             String[] information = data.split(" , ");
-            Task task = new Task("");
-            boolean corrupted = false;
+            Task task = null;
+            boolean isCorrupted = false;
             switch (information[0]) {
             case "T":
                 task = new Todo(information[2]);
@@ -77,21 +79,16 @@ public class Database {
                 task = new Event(information[2], information[3], information[4]);
                 break;
             default:
-                corrupted = true;
-                System.out.println("file is corrupted");
+                isCorrupted = true;
             }
-            if (!corrupted) {
-                if (information[1].equals("1")) {
+            if (!isCorrupted) {
+                if (information[1].equals("true")) {
                     task.setDone("mark");
-                } else if (information[1].equals("0")) {
+                } else if (information[1].equals("false")) {
                     task.setDone("unmark");
-                } else {
-                    System.out.println("file is corrupted");
                 }
                 tasks.add(task);
             }
         }
     }
-
-
 }
