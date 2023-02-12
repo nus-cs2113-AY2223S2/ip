aae4ff656c17133b606e89794139d0654c7fe0c7
Khulon <e0725788@u.nus.edu>
2023-02-12 16:27:52 +0800

    changed to array list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,42 +1,44 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
-    public static Task[] tasks = new Task[100];
-    public static int tasksLength = 0;
+    public static ArrayList<Task> tasks = new ArrayList<>();
+
+    //public static int tasksLength = 0;
 
     public static void printTasks() {
         Greeting.printSeperator();
         System.out.println("\tHere are the tasks in your list:");
-        for (int i = 0; i < tasksLength; i++) {
-            System.out.println("\t" + (i+1) + "." + tasks[i].printTask());
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println("\t" + (i+1) + "." + tasks.get(i).printTask());
         }
         Greeting.printSeperator();
     }
 
     public static void printNewTask(int taskNumber) {
         Greeting.printSeperator();
-        System.out.println("\tGot it. I've added this task: \n"+ "\t\t" + tasks[tasksLength].printTask());
-        tasksLength ++;
-        System.out.println("\tNow you have " + tasksLength + " tasks in the list.");
+        System.out.println("\tGot it. I've added this task: \n"+ "\t\t" + tasks.get(tasks.size()-1).printTask());
+        //tasksLength ++;
+        System.out.println("\tNow you have " + tasks.size() + " tasks in the list.");
         Greeting.printSeperator();
     }
 
     public static void mark(String userInput) {
         int taskNumber = Integer.parseInt(userInput);
-        tasks[taskNumber - 1].MarkStatusDone();
+        tasks.get(taskNumber - 1).MarkStatusDone();
         Greeting.printSeperator();
         System.out.println("\tNice! I've marked this task as done:\n" +
-                "\t\t[X] " + tasks[taskNumber - 1].getTask());
+                "\t\t[X] " + tasks.get(taskNumber - 1).getTask());
         Greeting.printSeperator();
     }
 
     public static void unmark(String userInput) {
         int taskNumber = Integer.parseInt(userInput);
-        tasks[taskNumber - 1].MarkStatusUndone();
+        tasks.get(taskNumber - 1).MarkStatusUndone();
         Greeting.printSeperator();
         System.out.println("\tOK, I've marked this task as not done yet:\n" +
-                "\t\t[ ] " + tasks[taskNumber - 1].getTask());
+                "\t\t[ ] " + tasks.get(taskNumber - 1).getTask());
         Greeting.printSeperator();
     }
 
@@ -66,7 +68,7 @@ public class Duke {
             case "todo":
                 try {
                     addTodo(userInput);
-                    printNewTask(tasksLength);
+                    printNewTask(tasks.size());
                 } catch (IllegalInputException e){
                     Greeting.printEmptyTask();
                 }
@@ -75,7 +77,7 @@ public class Duke {
             case "deadline":
                 try {
                     addDeadline(userInput);
-                    printNewTask(tasksLength);
+                    printNewTask(tasks.size());
                 } catch (IllegalInputException e){
                     Greeting.printEmptyTask();
                 } catch (MissingCommandException e) {
@@ -88,7 +90,7 @@ public class Duke {
             case "event":
                 try {
                     addEvent(userInput);
-                    printNewTask(tasksLength);
+                    printNewTask(tasks.size());
                 } catch (IllegalInputException e){
                     Greeting.printEmptyTask();
                 } catch (MissingCommandException e) {
@@ -113,7 +115,7 @@ public class Duke {
         if (todoTask == "") {
             throw new IllegalInputException();
         }
-        tasks[tasksLength] = new Todo (todoTask);
+        tasks.add(new Todo (todoTask));
         return;
     }
 
@@ -130,7 +132,7 @@ public class Duke {
         if (deadlineDay == "") {
             throw new IllegalDayException();
         }
-        tasks[tasksLength] = new Deadline(deadlineTask, deadlineDay);
+        tasks.add(new Deadline(deadlineTask, deadlineDay));
         return;
     }
 
@@ -148,7 +150,7 @@ public class Duke {
         if (eventFrom == "" || eventTo == "") {
             throw new IllegalDayException();
         }
-        tasks[tasksLength] = new Event(eventTask, eventFrom, eventTo);
+        tasks.add(new Event(eventTask, eventFrom, eventTo));
         return;
     }
 
