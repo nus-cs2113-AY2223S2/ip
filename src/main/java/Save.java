3f93d1a21a8a77307d991f03f0541757c106822f
T-Wan-Lin <tanwanlin.c2021>
2023-02-12 16:04:22 +0800

    Construct Save Class

--- src/main/java/Save.java
+++ src/main/java/Save.java
@@ -4,8 +4,18 @@ import java.io.*;
 
 public class Save {
     //rough implementation: in this class the task to be saved will be broken down according to it's tags [T], [E] or [D]
-    private static final String TO_DO = "[T]";
-    private static final String EVENT = "[E]";
-
-    private static final String DEADLINE = "[D]";
+    private String taskType;
+    private String description;
+    private String dueDate;
+    private String start;
+    private String end;
+    private boolean isDone;
+    public Save(String taskType, boolean isDone, String description, String dueDate, String start, String end) {
+        this.taskType = taskType;
+        this.isDone = isDone;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.start = start;
+        this.end = end;
+    }
 }
