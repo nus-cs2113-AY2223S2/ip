cd8db8d85ecd93c3d2201cc0a76e400dc2532967
SSzeWen <seetszewen@gmail.com>
2023-02-12 13:58:37 +0800

    Added saveTaskString to all tasks for easier saving

--- src/main/java/duke/task/Task.java
+++ src/main/java/duke/task/Task.java
@@ -1,12 +1,16 @@
 package duke.task;
 
-public class Task {
+public abstract class Task {
     protected String description;
     protected boolean isDone;
+
+    protected String taskString;
+    public static final String COMMA = " , ";
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        this.taskString = saveTaskString();
     }
 
     public String toString() {
@@ -24,9 +28,12 @@ public class Task {
         }
     }
 
+    public abstract String saveTaskString();
+
     public String addTaskMessage() {
         return "Got it. I've added this task:\n  " + this.toString() + System.lineSeparator();
     }
-
-
+    public String getTaskString() {
+        return taskString;
+    }
 }
--- src/main/java/duke/task/Todo.java
+++ src/main/java/duke/task/Todo.java
@@ -9,4 +9,11 @@ public class Todo extends Task {
     public String toString() {
         return "[T]" + super.toString();
     }
+
+    @Override
+    public String saveTaskString() {
+        String saveString = new String();
+        saveString += "T" + COMMA + isDone + COMMA + description;
+        return saveString;
+    }
 }
