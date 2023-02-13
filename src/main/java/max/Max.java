0d935ed359268e33c6e81575cc258c86b3c72016
nichyjt <nicholas.yek@gmail.com>
2023-02-13 16:44:23 +0800

    Refactor task handling into a TaskManager Controller
    Signed-off-by: nichyjt <nicholas.yek@gmail.com>

--- src/main/java/max/Max.java
+++ src/main/java/max/Max.java
@@ -4,20 +4,16 @@ import max.command.Command;
 import max.command.CommandParser;
 import max.command.CommandValidator;
 import max.command.InvalidCommandException;
-import max.task.Deadline;
-import max.task.Event;
-import max.task.Task;
-import max.task.Todo;
+import max.task.*;
 
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Max {
     private static boolean isListening;
-    private static final int TASK_NUMBER_LIMIT = 100;
-    private static Task[] tasks = new Task[TASK_NUMBER_LIMIT];
-    private static int numTasks;
+    private static boolean isDebugMode = false;
 
+    private static TaskManager taskManager;
 
     public static void setIsListening(boolean keepListening) {
         Max.isListening = keepListening;
@@ -32,79 +28,7 @@ public class Max {
         System.out.print("Goodbye! Thank you for using MAX.\n");
     }
 
-    public static Task createTask(HashMap<String, String> commandMap, Command command) throws InvalidCommandException {
-        // Assertion: commandMap has the correct subcommands & length
-        Task newTask = null;
-        if (command.equals(Command.TASK_TODO)) {
-            // To-do task
-            String description = commandMap.get("todo");
-            newTask = new Todo(description);
-        } else if (command.equals(Command.TASK_DEADLINE)) {
-            // Deadline task
-            String description = commandMap.get("deadline");
-            String deadline = commandMap.get("by");
-            newTask = new Deadline(description, deadline);
-        } else if (command.equals(Command.TASK_EVENT)) {
-            // Event task
-            String description = commandMap.get("event");
-            String from = commandMap.get("from");
-            String to = commandMap.get("to");
-            newTask = new Event(description, from, to);
-        }
-        if (newTask == null) {
-            // Safety check in case the assertion fails
-            throw new InvalidCommandException("Throw me a bone here, I couldn't create a task!");
-        }
-        return newTask;
-    }
 
-    public static void addTaskToList(Task newTask) throws IndexOutOfBoundsException {
-        if (numTasks == TASK_NUMBER_LIMIT) {
-            throw new IndexOutOfBoundsException("MAX is a dog, and has limited memory. Too many tasks for MAX to remember!");
-        }
-        tasks[numTasks] = newTask;
-        numTasks++;
-    }
-
-    public static void printTasklist() {
-        if (numTasks <= 0) {
-            System.out.println("There's nothing in your list. I'm gonna bite you.");
-            return;
-        }
-        System.out.println("Here's what's in your list:");
-        for (int i = 0; i < numTasks; ++i) {
-            // Print number, box, description in that order
-            Task curr = tasks[i];
-            System.out.print(i + 1 + ". " + curr.getDescription() + '\n');
-        }
-    }
-
-    public static void markTask(String taskNumString, boolean isDone) {
-        int taskNum;
-
-        try {
-            taskNum = Integer.parseInt(taskNumString) - 1; // Convert to 0-idx
-        } catch (NumberFormatException exception) {
-            System.out.println("I'm a dog, but even I know that you didn't enter a number.");
-            return;
-        }
-
-        // Guard clause to check for taskNum validity
-        if (taskNum < 0 || taskNum >= numTasks) {
-            System.out.println("Invalid task number!");
-            return;
-        }
-
-        // Update the task's done status
-        if (isDone) {
-            tasks[taskNum].markAsDone();
-            System.out.println("Okay, marking this task as done: ");
-        } else {
-            tasks[taskNum].markAsUndone();
-            System.out.println("Okay, setting this task as undone: ");
-        }
-        System.out.println(tasks[taskNum].getDescription());
-    }
 
 
     public static void handleCommand(String command) {
@@ -136,30 +60,28 @@ public class Max {
             exit();
             break;
         case LIST:
-            printTasklist();
+            taskManager.printTasklist();
             break;
         case MARK:
             String taskNumString = commandPayload.get("mark");
-            markTask(taskNumString, true);
+            taskManager.markTask(taskNumString, true);
             break;
         case UNMARK:
             taskNumString = commandPayload.get("unmark");
-            markTask(taskNumString, false);
+            taskManager.markTask(taskNumString, false);
             break;
         case TASK_EVENT:
         case TASK_DEADLINE:
         case TASK_TODO:
-            // TODO: Consider refactoring Task logic to a TaskHandler class
             try {
-                Task newTask = createTask(commandPayload, mainCommand);
-                addTaskToList(newTask);
-                System.out.println("Got it. Task added:");
-                System.out.println(newTask.getDescription());
-                System.out.println("You now have " + numTasks + " tasks in your list.");
-
-            } catch (InvalidCommandException | IndexOutOfBoundsException exception) {
+                taskManager.createTask(commandPayload, mainCommand);
+            }catch (TaskException exception){
                 System.out.println(exception.getMessage());
             }
+            break;
+        case DELETE:
+            taskNumString = commandPayload.get("delete");
+            taskManager.deleteTask(taskNumString);
             break;
         default:
             // { Command.UNKNOWN_COMMAND }
@@ -186,12 +108,11 @@ public class Max {
                 "|__/     |__/|__/  |__/|__/  |__/     V__)  ||";
         System.out.println(logo);
 
+        // Init task subsystem (Controller)
+        taskManager = new TaskManager();
+
         greet();
         setIsListening(true);
-
-        // Init tasks subsystem
-        numTasks = 0;
-
         // Init IO
         Scanner input = new Scanner(System.in);
 
