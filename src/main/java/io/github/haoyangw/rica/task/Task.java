39cd5b129ff850fdc2727c948ac0fa20d0a7f28c
haoyangw <haoyangw@gmail.com>
2023-02-13 15:49:20 +0800

    Task.class: Augment its deserializeObject() method
    As the superclass, let Task.deserializeObject() handle(and
      delegate) the deserialization of Task and its subclasses
      from local storage.
    
    Throw RicaStorageException if an unrecognised Task-related
      object data is given.

--- src/main/java/io/github/haoyangw/rica/task/Task.java
+++ src/main/java/io/github/haoyangw/rica/task/Task.java
@@ -10,6 +10,7 @@ public class Task implements Serializable {
     protected static final String INCOMPLETE_SERIALIZED_OBJECT_STRING = " Incomplete data found in my past memory... Am I getting memory loss?";
     protected static final String DATA_STRING_SEPARATOR = " | ";
     protected static final String DATA_STRING_SEPARATOR_REGEX = "\\s\\|\\s";
+    protected static final String UNKNOWN_SERIALIZED_OBJECT_TYPE = " What type of task is this... You didn't teach me how to handle it LOL";
     protected static final String WRONG_SERIALIZED_OBJECT_TYPE = " Hmm this task from my past memory is... Not my type xD";
     protected final String description;
 
@@ -31,8 +32,18 @@ public class Task implements Serializable {
             throw new RicaSerializationException(Task.INCOMPLETE_SERIALIZED_OBJECT_STRING);
         }
         String taskType = variables[0];
-        if (!taskType.equals(Task.TYPE) || variables.length > Task.NUM_OF_SERIALIZED_DATA) {
-            throw new RicaSerializationException(Task.WRONG_SERIALIZED_OBJECT_TYPE);
+        switch (taskType) {
+        case Deadline.TYPE:
+            return Deadline.deserializeObject(objectData);
+        case Event.TYPE:
+            return Event.deserializeObject(objectData);
+        case Task.TYPE:
+            if (variables.length != Task.NUM_OF_SERIALIZED_DATA) {
+                return Todo.deserializeObject(objectData);
+            }
+            break;
+        default:
+            throw new RicaStorageException(Task.UNKNOWN_SERIALIZED_OBJECT_TYPE);
         }
         // We only reach this point when the number of variables match Task's, so
         //   we're dealing with a serialized Task String
