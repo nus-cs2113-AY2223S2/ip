71359e7ee1f82cc58c1e38bf0808fa17b983d763
ngyida <e0726193@u.nus.edu>
2023-02-14 01:34:02 +0800

    Add DeadlineCommand

---
+++ src/main/java/DeadlineCommand.java
@@ -0,0 +1,22 @@
+import task.Deadline;
+import task.Task;
+
+public class DeadlineCommand extends Command{
+
+    public DeadlineCommand(String[] commands) {
+        super(commands);
+    }
+
+    @Override
+    public void doCommand(int taskCount, Task[] tasks) {
+        String description = getCommands()[1];
+        String due = getCommands()[2];
+        Deadline deadlineTask = new Deadline(description, due);
+        tasks[taskCount] = deadlineTask;
+        System.out.println("____________________________________________________________");
+        System.out.println("Got it. I've added this task:");
+        System.out.println(deadlineTask.getSummary());
+        System.out.printf("Now you have %d tasks in the list.\n", taskCount);
+        System.out.println("____________________________________________________________");
+    }
+}
