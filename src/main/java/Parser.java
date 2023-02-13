cf9ea3855e616f4d0f4129d9e543a41bf0ce99b7
ngyida <e0726193@u.nus.edu>
2023-02-13 23:17:59 +0800

    Add Parser

---
+++ src/main/java/Parser.java
@@ -0,0 +1,46 @@
+import exception.DukeException;
+import task.Task;
+
+public class Parser {
+    public static String[] parse(String input) throws DukeException {
+        String[] commands = new String[10];
+        input = input.trim();
+        String[] inputs = input.split(" ");
+        commands[0] = inputs[0];
+
+        switch (commands[0]) {
+        case "list":
+            break;
+        case "mark":
+        case "unmark":
+            commands[1] = inputs[1];
+            break;
+        case "todo":
+            String description = input.split("todo ")[1];
+            commands[1] = description;
+            break;
+        case "deadline":
+            String removedKeyword = input.split("deadline ")[1];
+            String[] splitString = removedKeyword.split(" /by ");
+            description = splitString[0];
+            String due = splitString[1];
+            commands[1] = description;
+            commands[2] = due;
+            break;
+        case "event":
+            removedKeyword = input.split("event ")[1];
+            splitString = removedKeyword.split(" /from ");
+            description = splitString[0];
+            splitString = splitString[1].split(" /to ");
+            String start = splitString[0];
+            String end = splitString[1];
+            commands[1] = description;
+            commands[2] = start;
+            commands[3] = end;
+            break;
+        default:
+            commands[0] = null;
+        }
+        return commands;
+    }
+}
