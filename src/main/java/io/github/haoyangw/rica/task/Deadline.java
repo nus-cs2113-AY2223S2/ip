1d2f9a118d120f0a9d6d154ac9e22c8ecfc1f433
haoyangw <haoyangw@gmail.com>
2023-02-13 11:36:22 +0800

    Deadline.class: Implement Serializable interface
    Like Event.class, add a private constructor for
      instantiating all instance variables with given
      values to support deserializing.
    
    Then we implement serializeObject() and provide a
      factory method for instantiating an instance of
      Deadline from a serialized String representation.

--- src/main/java/io/github/haoyangw/rica/task/Deadline.java
+++ src/main/java/io/github/haoyangw/rica/task/Deadline.java
@@ -1,5 +1,6 @@
 package io.github.haoyangw.rica.task;
 
+import io.github.haoyangw.rica.exception.RicaSerializationException;
 import io.github.haoyangw.rica.exception.RicaTaskException;
 
 public class Deadline extends Todo {
@@ -7,12 +8,17 @@ public class Deadline extends Todo {
     private static final String DEADLINE_KEYWORD = "/by";
     private static final String MISSING_DEADLINE_ERROR = " Forget something? (Hint: Where's the deadline for this deadline task?)";
     private static final String MISSING_DESCRIPTION_ERROR = " Did you give a description for this deadline task xD";
-    private static final String TYPE = "D";
+    private static final int NUM_OF_SERIALIZED_DATA = 4;
     private static final String WRONG_CREATE_CMD_ERROR = " Hmm I'm being told to set a deadline with the wrong command. Help xP";
+    protected static final String TYPE = "D";
     protected final String deadline;
 
     public Deadline(String description, String deadline) {
-        super(description);
+        this(description, false, deadline);
+    }
+
+    private Deadline(String description, boolean isDone, String deadline) {
+        super(description, isDone);
         this.deadline = deadline;
     }
 
@@ -68,9 +74,29 @@ public class Deadline extends Todo {
         return this.deadline;
     }
 
+    public static Deadline deserializeObject(String objectData) {
+        String[] variables = objectData.split(Task.DATA_STRING_SEPARATOR_REGEX);
+        if (variables.length < Deadline.NUM_OF_SERIALIZED_DATA) {
+            throw new RicaSerializationException(Task.INCOMPLETE_SERIALIZED_OBJECT_STRING);
+        }
+        if (!variables[0].equals(Deadline.TYPE)) {
+            throw new RicaSerializationException(Task.WRONG_SERIALIZED_OBJECT_TYPE);
+        }
+        String description = variables[1];
+        boolean isDone = Boolean.parseBoolean(variables[2]);
+        String deadline = variables[3];
+        return new Deadline(description, isDone, deadline);
+    }
+
     @Override
     protected String getType() {
         return Deadline.TYPE;
+    }
+
+    @Override
+    public String serializeObject() {
+        String todoData = super.serializeObject();
+        return todoData + Task.DATA_STRING_SEPARATOR + this.getDeadline();
     }
 
     @Override
