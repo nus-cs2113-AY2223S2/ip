e40dfd028f52eaf0caf260edeb9ae74d7d91d409
waiter-palypoo <gohjinghong2000@gmail.com>
2023-02-14 22:25:44 +0800

    Add code to handle unknown command, insufficient detail exceptions.

--- src/main/java/HinaBot.java
+++ src/main/java/HinaBot.java
@@ -3,13 +3,20 @@ import java.util.Scanner;
 public class HinaBot {
     protected static Task[] taskList = new Task[100];
     protected static int taskCount = 0;
+
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
         showGreeting();
         while (true) {
-            line = in.nextLine();
-            handleCommand(line);
+            try {
+                line = in.nextLine();
+                handleCommand(line);
+            } catch (HinaException cmdException) {
+                System.out.println(">.< Hina does not recognise this command!");
+            } catch (StringIndexOutOfBoundsException argException) {
+                System.out.println("@_@ Please give Hina more details!");
+            }
         }
     }
 
@@ -18,30 +25,26 @@ public class HinaBot {
         System.out.println("What are your orders?");
     }
 
-    public static void handleCommand(String command) {
+    public static void handleCommand(String command) throws HinaException {
         if (command.equalsIgnoreCase("bye")) {
             System.out.println("Goodbye master, let's meet again soon...");
             System.exit(0);
-        }
-        else if (command.equalsIgnoreCase("list")) {
+        } else if (command.equalsIgnoreCase("list")) {
             listTasks();
-        }
-        else if (command.split(" ")[0].equalsIgnoreCase("mark")) {
+        } else if (command.split(" ")[0].equalsIgnoreCase("mark")) {
             int taskIndex = Integer.parseInt(command.split(" ")[1]);
             markTask(taskIndex);
-        }
-        else if (command.split(" ")[0].equalsIgnoreCase("unmark")) {
+        } else if (command.split(" ")[0].equalsIgnoreCase("unmark")) {
             int taskIndex = Integer.parseInt(command.split(" ")[1]);
             unmarkTask(taskIndex);
-        }
-        else if (command.split(" ")[0].equalsIgnoreCase("todo")){
+        } else if (command.split(" ")[0].equalsIgnoreCase("todo")) {
             addTask(command.substring(5));
-        }
-        else if (command.split(" ")[0].equalsIgnoreCase("deadline")) {
+        } else if (command.split(" ")[0].equalsIgnoreCase("deadline")) {
             addDeadline(command.substring(9));
-        }
-        else if (command.split(" ")[0].equalsIgnoreCase("event")) {
+        } else if (command.split(" ")[0].equalsIgnoreCase("event")) {
             addEvent(command.substring(6));
+        } else {
+            throw new HinaException();
         }
     }
 
@@ -76,22 +79,30 @@ public class HinaBot {
 
     public static void addDeadline(String deadline) {
         String[] details = deadline.split("/");
-        Deadline newDeadline = new Deadline(details[0], details[1].substring(3));
-        taskList[taskCount] = newDeadline;
-        taskCount++;
-        System.out.println("Noted! This task has been added:");
-        System.out.println(newDeadline.toString());
-        getTaskCount();
+        if (details.length < 2) {
+            System.out.println("Hina needs to know the deadline for this task!");
+        } else {
+            Deadline newDeadline = new Deadline(details[0], details[1].substring(3));
+            taskList[taskCount] = newDeadline;
+            taskCount++;
+            System.out.println("Noted! This task has been added:");
+            System.out.println(newDeadline.toString());
+            getTaskCount();
+        }
     }
 
     public static void addEvent(String event) {
         String[] details = event.split("/");
-        Event newEvent = new Event(details[0], details[1].substring(5).trim(), details[2].substring(3));
-        taskList[taskCount] = newEvent;
-        taskCount++;
-        System.out.println("Noted! This task has been added:");
-        System.out.println(newEvent.toString());
-        getTaskCount();
+        if (details.length < 3) {
+            System.out.println("Please tell Hina when this event starts and ends!");
+        } else {
+            Event newEvent = new Event(details[0], details[1].substring(5).trim(), details[2].substring(3));
+            taskList[taskCount] = newEvent;
+            taskCount++;
+            System.out.println("Noted! This task has been added:");
+            System.out.println(newEvent.toString());
+            getTaskCount();
+        }
     }
 
     public static void getTaskCount() {
