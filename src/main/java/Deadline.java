f86c49ec73cdccbe22df108396e502a25c75701e
Sophie <sophieyxtang@gmail.com>
2023-02-14 17:18:32 +0800

    Add comments

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,6 +1,7 @@
 public class Deadline extends Task {
 
-    // Code from partial solution
+    // Solution below adapted from https://nus-cs2113-ay2223s2.github.io/website/admin/ip-w4.html
+    // Week 4 A-Inheritance partial solution with modifications
     protected String by;
     public Deadline(String description, String by) {
         super(description);
