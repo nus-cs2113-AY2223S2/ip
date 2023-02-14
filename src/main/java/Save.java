8bb7ee00932a78869613facb54e989e824ca6b0a
cleoncheng2000 <e0725796@u.nus.edu>
2023-02-15 07:47:43 +0800

    Update updateSaveFile and loadSaveFile functions

--- src/main/java/Save.java
+++ src/main/java/Save.java
@@ -8,45 +8,62 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+import java.sql.SQLOutput;
 
 public class Save {
-    private static String filePath;
 
-    public void createSaveFile() throws IOException {
+    public static String filePath;
+
+    public static void createSaveFile() throws IOException {
         File file = new File(filePath);
-        file.createNewFile();
-        file.mkdirs();
+        if (!file.exists()) {
+            file.createNewFile();
+            file.mkdirs();
+        }
     }
 
     public static void loadSaveFile(Task[] tasks) throws IOException {
-        File file = new File(filePath);
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        String line = reader.readLine();
-        String[] args = line.split("\\|", -1);
-        int currentIndex = 0;
-        while(line != null) {
-            if (line.startsWith("T")) {
-                ToDo todo = new ToDo(currentIndex, args[1]);
-                tasks[currentIndex] = todo;
-            } else if (line.startsWith("D")){
-                Deadline deadline = new Deadline(currentIndex, args[1], args[2]);
-                tasks[currentIndex] = deadline;
-            } else {
-                Event event = new Event(currentIndex, args[1], args[2], args[3]);
-                tasks[currentIndex] = event;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] args = line.split("\\|", -1);
+                int currentIndex = 0;
+                if (line.startsWith("T")) {
+                    ToDo todo = new ToDo(currentIndex, args[2]);
+                    if (args[2].equals("X")) {
+                        todo.markAsDone();
+                    }
+                    tasks[currentIndex] = todo;
+                } else if (line.startsWith("D")) {
+                    Deadline deadline = new Deadline(currentIndex, args[2], args[3]);
+                    if (args[2].equals("X")) {
+                        deadline.markAsDone();
+                    }
+                    tasks[currentIndex] = deadline;
+                } else {
+                    Event event = new Event(currentIndex, args[2], args[3], args[4]);
+                    if (args[2].equals("X")) {
+                        event.markAsDone();
+                    }
+                    tasks[currentIndex] = event;
+                }
+                currentIndex++;
             }
-            currentIndex++;
+        } catch (IOException exception) {
+            System.out.println("☹ OOPS!!! An error occurred");
+        } catch (Exception exception) {
+            System.out.println("☹ OOPS!!! An error occurred");
         }
-
     }
-    public static void updateSaveFile(Task[] tasks) throws IOException{
+
+    public void updateSaveFile(Task[] tasks) throws IOException {
         FileWriter fw = new FileWriter(filePath);
         for (int i = 0; i < tasks.length - 1; i++) {
             Task currentTask = tasks[i];
             String type;
-            String done = currentTask.getStatusIcon();
             String description = currentTask.getDescription();
+            String done = currentTask.isDone() ? "X" : " ";
             if (currentTask instanceof ToDo) {
                 type = "T";
                 fw.write(type + "|" + done + "|" + description);
