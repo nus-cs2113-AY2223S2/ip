89f6b314cc44188d9a90e7fee961d4776ebe8881
Stella1585 <88623728+Stella1585@users.noreply.github.com>
2023-02-15 05:02:25 +0800

    delete

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -7,11 +7,11 @@ import duke.task.Event;
 import duke.task.Task;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-
     public static final String LINE = "    ____________________________________________________________\n";
-    public static final String GREET = "     past.Hello! I'm Duke\n" +
+    public static final String GREET = "     Hello! I'm Duke\n" +
             "     Welcome to Your To-do List!\n" +
             "     Enter \"todo 'task-name'\" to add a task.\n" +
             "     Enter \"deadline 'task-name' /by 'deadline'\" to add a task with a deadline.\n" +
@@ -24,13 +24,7 @@ public class Duke {
     public static final String DONE = "    Nice! I've marked this task as done: \n    ";
     public static final String ERROR = "    Invalid command! :( Check your input and try again! \n";
 
-    public static void printAddedTask(Task task, int total) {
-        System.out.print(LINE + "    Got it. I've added this task:\n      " +
-                task.toString() + "\n    Now you have " + total + " tasks in the list.\n" +
-                LINE);
-    }
-
-    private static void addEvent(int count, Task[] tasks, String[] words) throws MissingDescriptionException {
+    private static void addEvent(ArrayList<Task> tasks, String[] words) throws MissingDescriptionException {
         if (words.length != 2) {
             throw new MissingDescriptionException();
         }
@@ -42,10 +36,11 @@ public class Duke {
         if (dates.length != 2) {
             throw new MissingDescriptionException();
         }
-        tasks[count] = new Event(description[0], "E", dates[0], dates[1]);
+        Event event = new Event(description[0], "E", dates[0], dates[1]);
+        tasks.add(event);
     }
 
-    private static void addDeadline(int count, Task[] tasks, String[] words) throws MissingDescriptionException {
+    private static void addDeadline(ArrayList<Task> tasks, String[] words) throws MissingDescriptionException {
         if (words.length != 2) {
             throw new MissingDescriptionException();
         }
@@ -53,48 +48,59 @@ public class Duke {
         if (description.length != 2) {
             throw new MissingDescriptionException();
         }
-        tasks[count] = new Deadline(description[0], "D", description[1]);
+        Deadline deadline = new Deadline(description[0], "D", description[1]);
+        tasks.add(deadline);
     }
 
-    private static void addTodo(int count, Task[] tasks, String[] words) throws MissingDescriptionException {
+    private static void addTodo(ArrayList<Task> tasks, String[] words) throws MissingDescriptionException {
         if (words.length != 2) {
             throw new MissingDescriptionException();
         }
         String description = words[1];
-        tasks[count] = new Task(description, "T");
+        Task todo = new Task(description, "T");
+        tasks.add(todo);
     }
 
-    private static void editMarkStatus(Task[] tasks, String[] words, String command) {
+    private static void deleteTask(ArrayList<Task> tasks, String[] words) {
+        int index = Integer.parseInt(words[1]) - 1;
+        System.out.print(LINE + "    Noted. I've removed this task:\n      " +
+                tasks.get(index).toString());
+        tasks.remove(index);
+        System.out.print("\n    Now you have " + tasks.size() + " tasks in the list.\n" +
+                LINE);
+    }
+
+    private static void editMarkStatus(ArrayList<Task> tasks, String[] words, String command) {
         int index = Integer.parseInt(words[1]) - 1;
         if (command.equals("unmark")) {
-            tasks[index].setDone(false);
+            tasks.get(index).setDone(false);
             System.out.print(LINE + NOT_DONE);
         } else {
-            tasks[index].setDone(true);
+            tasks.get(index).setDone(true);
             System.out.print(LINE + DONE);
         }
-        System.out.print(tasks[index].toString() + "\n" + LINE);
+        System.out.print(tasks.get(index).toString() + "\n" + LINE);
     }
 
-    private static void printList(int count, Task[] tasks, String[] words) throws ExcessInputsException {
+    private static void printList(ArrayList<Task> tasks, String[] words) throws ExcessInputsException {
         if (words.length > 1) {
             throw new ExcessInputsException();
         }
         System.out.println(LINE + "Here are the tasks in your list:");
-        for (int i = 1; i <= count; i++) {
-            System.out.println("    " + i + "." + tasks[i-1].toString());
+        for (int i = 1; i <= tasks.size(); i++) {
+            System.out.println("    " + i + "." + tasks.get(i-1).toString());
         }
         System.out.print(LINE);
     }
 
-    private static void processCommands(String line, int count, Scanner in) {
-        Task[] tasks = new Task[100];
+    private static void processCommands(String line, Scanner in) {
+        ArrayList<Task> tasks = new ArrayList<>();
         while (!line.equals("bye")) {
             String[] words = line.split(" ", 2);
             String command = words[0];
             if (command.equals("list")) {
                 try {
-                    printList(count, tasks, words);
+                    printList(tasks, words);
                 } catch (ExcessInputsException e) {
                     System.out.println(LINE + "Too many inputs! Please only enter \"list\"\n" + LINE);
                 }
@@ -105,27 +111,41 @@ public class Duke {
                     System.out.println(LINE + "Mark/Unmark must only be followed by the index number!\n" +
                             "Enter \"list\" to check the index.\n" + LINE);
                 } catch (IndexOutOfBoundsException e) {
-                    System.out.println(LINE + "Missing task index number!\n" +
+                    System.out.println(LINE + "Missing/Invalid task index number!\n" +
                             "Enter \"list\" to check the index.\n" + LINE);
                 } catch (NullPointerException e) {
-                    System.out.println(LINE + "Invalid task index number!\n" +
+                    System.out.println(LINE + "Missing/Invalid task index number!\n" +
+                            "Enter \"list\" to check the index.\n" + LINE);
+                }
+            } else if (command.equals("delete")) {
+                try {
+                    deleteTask(tasks, words);
+                } catch (NumberFormatException e){
+                    System.out.println(LINE + "Delete must only be followed by the index number!\n" +
+                            "Enter \"list\" to check the index.\n" + LINE);
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println(LINE + "Missing/Invalid task index number!\n" +
+                            "Enter \"list\" to check the index.\n" + LINE);
+                } catch (NullPointerException e) {
+                    System.out.println(LINE + "Missing/Invalid task index number!\n" +
                             "Enter \"list\" to check the index.\n" + LINE);
                 }
             } else {
                 try {
                     if (command.equals("todo")) {
-                        addTodo(count, tasks, words);
+                        addTodo(tasks, words);
                     } else if (command.equals("deadline")) {
-                        addDeadline(count, tasks, words);
+                        addDeadline(tasks, words);
                     } else if (command.equals("event")) {
-                        addEvent(count, tasks, words);
+                        addEvent(tasks, words);
                     } else {
                         System.out.print(LINE + ERROR + LINE);
                         line = in.nextLine();
                         continue;
                     }
-                    count++;
-                    printAddedTask(tasks[count -1], count);
+                    System.out.print(LINE + "    Got it. I've added this task:\n      " +
+                            tasks.get(tasks.size()-1).toString() + "\n    Now you have " + tasks.size() + " tasks in the list.\n" +
+                            LINE);
                 } catch (MissingDescriptionException e){
                     System.out.println(LINE + "Missing task description details.\n" +
                             "Please check the input format for adding events\n" + LINE);
@@ -137,10 +157,9 @@ public class Duke {
 
     private static void readInputs() {
         String line;
-        int count = 0;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
-        processCommands(line, count, in);
+        processCommands(line, in);
     }
 
     public static void main(String[] args) {
