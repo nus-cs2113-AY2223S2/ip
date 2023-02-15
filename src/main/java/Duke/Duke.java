afe7c03340c3c46008b73a514bb0f0d0643c4ede
LAPTOP-6GKQP4JJ\Douph <pdou@connect.ust.hk>
2023-02-15 15:34:03 +0800

    add save

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -1,11 +1,16 @@
 package Duke;
 
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Duke {
     public static final int LENGTH_DEADLINE = 8;
     public static final int LENGTH_TODO = 4;
     public static final int LENGTH_EVENT = 5;
+    public static String relativePath = ".\\lists.txt";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -14,7 +19,14 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println("--------------------------------");
         Task[] lists = new Task[100];
-        int index = 0;
+        int index=0;
+//        try{
+//            initializeList(lists,relativePath);
+//            index = getIndexOfList(lists,relativePath);
+//        } catch (FileNotFoundException e) {
+//            System.out.println("File not found");
+//        }
+
         while (true) {
             String line = in.nextLine();
             if (line.equals("bye")) {
@@ -23,80 +35,99 @@ public class Duke {
                 System.out.println("--------------------------------");
                 break;
             } else if (line.substring(0, 4).equals("mark")) {
-                try{
+                try {
                     String number = line.substring(5, 6);
                     int markIndex = Integer.parseInt(number);
                     lists[markIndex - 1].isDone = true;
-                }catch(NumberFormatException e){
+                    writeToFile(relativePath, lists, index);
+                } catch (NumberFormatException e) {
                     System.out.println("Oops! Mark should be followed by a number. " +
                             "(A valid index number should be separated by a space after the mark)");
-                }catch(IndexOutOfBoundsException e){
+                } catch (IndexOutOfBoundsException e) {
                     System.out.println("Oops! Mark index out of bound! " +
                             "(A valid index number should be separated by a space after the mark)");
-                }catch(NullPointerException e){
+                } catch (NullPointerException e) {
                     System.out.println("Oops! Mark index out of bound! " +
                             "(A valid index number should be separated by a space after the mark)");
+                } catch (IOException e) {
+                    System.out.println("Something went wrong: " + e.getMessage());
                 }
             } else if (line.contains("unmark")) {
-                try{
+                try {
                     String number = line.substring(7, 8);
                     int unMarkIndex = Integer.parseInt(number);
                     lists[unMarkIndex - 1].isDone = false;
-                }catch(NumberFormatException e){
+                    writeToFile(relativePath, lists, index);
+                } catch (NumberFormatException e) {
                     System.out.println("Oops! Unmark should be followed by a number. " +
                             "(A valid index number should be separated by a space after the unmark)");
-                }catch(IndexOutOfBoundsException e){
+                } catch (IndexOutOfBoundsException e) {
                     System.out.println("Oops! Unmark index out of bound! " +
                             "(A valid index number should be separated by a space after the unmark)");
-                }catch(NullPointerException e){
+                } catch (NullPointerException e) {
                     System.out.println("Oops! Unmark index out of bound! " +
                             "(A valid index number should be separated by a space after the unmark)");
+                } catch (IOException e) {
+                    System.out.println("Something went wrong: " + e.getMessage());
                 }
             } else if (line.equals("list")) {
                 System.out.println("list:");
                 for (int i = 0; i < index; i++) {
                     System.out.println("\t" + (i + 1) + "." + lists[i].toString());
                 }
-            } else if(line.startsWith("delete")){
-                try{
+            } else if (line.startsWith("delete")) {
+                try {
                     String number = line.substring(7, 8);
                     int deleteIndex = Integer.parseInt(number);
-                    if(deleteIndex==index){
-                        printDeleteMessage(lists,deleteIndex-1,index);
-                        index=index-1;
-                    }
-                    else if(deleteIndex>=1 && deleteIndex<=index-1){
-                        printDeleteMessage(lists,deleteIndex-1,index);
-                        for(int i=deleteIndex-1;i<index-1;i++){
+                    if (deleteIndex == index) {
+                        printDeleteMessage(lists, deleteIndex - 1, index);
+                        index = index - 1;
+                    } else if (deleteIndex >= 1 && deleteIndex <= index - 1) {
+                        printDeleteMessage(lists, deleteIndex - 1, index);
+                        for (int i = deleteIndex - 1; i < index - 1; i++) {
                             lists[i] = lists[i + 1];
                         }
-                        index=index-1;
-                    }else{
+                        index = index - 1;
+                    } else {
                         System.out.println("Oops! Delete should be followed by a valid number. " +
                                 "(A valid index number should be separated by a space after the delete)");
                     }
-                }catch(NumberFormatException e){
+                    writeToFile(relativePath, lists, index);
+                } catch (NumberFormatException e) {
                     System.out.println("Oops! Unmark should be followed by a number. " +
                             "(A valid index number should be separated by a space after the delete)");
-                }catch(IndexOutOfBoundsException e){
+                } catch (IndexOutOfBoundsException e) {
                     System.out.println("Oops! Unmark index out of bound! " +
                             "(A valid index number should be separated by a space after the delete)");
-                }catch(NullPointerException e){
+                } catch (NullPointerException e) {
                     System.out.println("Oops! Unmark index out of bound! " +
                             "(A valid index number should be separated by a space after the delete)");
+                } catch (IOException e) {
+                    System.out.println("Something went wrong: " + e.getMessage());
                 }
-            }else {
+            } else {
                 if (line.substring(0, LENGTH_TODO).equals("todo")) {
                     lists[index] = new Todo(line.substring(LENGTH_TODO + 1));
-                    printMessage(lists,index);
+                    printMessage(lists, index);
                     index++;
+                    try {
+                        writeToFile(relativePath, lists, index);
+                    } catch (IOException e) {
+                        System.out.println("Something went wrong: " + e.getMessage());
+                    }
+
                 } else if (line.substring(0, LENGTH_DEADLINE).equals("deadline")) {
                     int breakingPoint = line.indexOf("/by");
                     String description = line.substring(LENGTH_DEADLINE + 1, breakingPoint);
                     String by = line.substring(breakingPoint + 4);
                     lists[index] = new Deadline(description, by);
-                    printMessage(lists,index);
+                    printMessage(lists, index);
                     index++;
+                    try {
+                        writeToFile(relativePath, lists, index);
+                    } catch (IOException e) {
+                        System.out.println("Something went wrong: " + e.getMessage());
+                    }
                 } else if (line.substring(0, LENGTH_EVENT).equals("event")) {
                     int breakingPoint_1 = line.indexOf("/from");
                     int breakingPoint_2 = line.indexOf("/to");
@@ -104,9 +135,14 @@ public class Duke {
                     String start = line.substring(breakingPoint_1 + 6, breakingPoint_2);
                     String end = line.substring(breakingPoint_2 + 4);
                     lists[index] = new Event(description, start, end);
-                    printMessage(lists,index);
+                    printMessage(lists, index);
                     index++;
-                }else{
+                    try {
+                        writeToFile(relativePath, lists, index);
+                    } catch (IOException e) {
+                        System.out.println("Something went wrong: " + e.getMessage());
+                    }
+                } else {
                     System.out.println("Sorry I can't get your message! Please try again.");
                 }
 
@@ -114,18 +150,103 @@ public class Duke {
 
         }
     }
-    public static void printMessage(Task[] Tasks, int taskIndex){
+
+    public static void printMessage(Task[] Tasks, int taskIndex) {
         System.out.println("--------------------------------");
         System.out.println("Got it. I've added this task:");
         System.out.println(Tasks[taskIndex].toString());
-        System.out.println("Now you have "+(taskIndex+1)+" task(s) in the list");
+        System.out.println("Now you have " + (taskIndex + 1) + " task(s) in the list");
         System.out.println("--------------------------------");
     }
-    public static void printDeleteMessage(Task[] Tasks, int deleteIndex,int totalIndex){
+
+    public static void printDeleteMessage(Task[] Tasks, int deleteIndex, int totalIndex) {
         System.out.println("--------------------------------");
         System.out.println("Noted. I've removed this task:");
         System.out.println(Tasks[deleteIndex].toString());
-        System.out.println("Now you have "+(totalIndex-1)+" task(s) in the list");
+        System.out.println("Now you have " + (totalIndex - 1) + " task(s) in the list");
         System.out.println("--------------------------------");
+    }
+
+    public static void writeToFile(String filePath, Task[] Tasks, int index) throws IOException {
+        File f = new File(filePath);
+        if (!f.exists()) {
+            f.createNewFile();
+            System.out.println("File created: " + f.getName());
+        }
+        FileWriter fw = new FileWriter(filePath);
+        for (int i = 0; i < index; i++) {
+            fw.write((i + 1) + "." + Tasks[i].toString() + '\n');
+        }
+        fw.close();
+
+
+    }
+
+    public static void initializeList(Task[] lists, String filePath) throws FileNotFoundException {
+        File f=new File(filePath);
+        int index=0;
+        if(f.exists()){
+            Scanner s=new Scanner(f);
+            while(true){
+                String line=s.toString();
+                if (line.substring(3,4).equals("T")) {
+                    lists[index] = new Todo(line.substring(9));
+                    printMessage(lists, index);
+                    index++;
+                    try {
+                        writeToFile(relativePath, lists, index);
+                    } catch (IOException e) {
+                        System.out.println("Something went wrong: " + e.getMessage());
+                    }
+
+                } else if (line.substring(3,4).equals("D")) {
+                    int breakingPoint = line.indexOf("/by");
+                    String description = line.substring(9, breakingPoint);
+                    String by = line.substring(breakingPoint + 4);
+                    lists[index] = new Deadline(description, by);
+                    printMessage(lists, index);
+                    index++;
+                    try {
+                        writeToFile(relativePath, lists, index);
+                    } catch (IOException e) {
+                        System.out.println("Something went wrong: " + e.getMessage());
+                    }
+                } else if (line.substring(3,4).equals("E")) {
+                    int breakingPoint_1 = line.indexOf("/from");
+                    int breakingPoint_2 = line.indexOf("/to");
+                    String description = line.substring(9, breakingPoint_1);
+                    String start = line.substring(breakingPoint_1 + 6, breakingPoint_2);
+                    String end = line.substring(breakingPoint_2 + 4);
+                    lists[index] = new Event(description, start, end);
+                    printMessage(lists, index);
+                    index++;
+                    try {
+                        writeToFile(relativePath, lists, index);
+                    } catch (IOException e) {
+                        System.out.println("Something went wrong: " + e.getMessage());
+                    }
+                }
+                s.nextLine();
+                if(!s.hasNext()){
+                    break;
+                }
+            }
+        }
+    }
+    public static int getIndexOfList(Task[] lists,String filePath) throws FileNotFoundException{
+        File f=new File(filePath);
+        int index=0;
+        if(f.exists()){
+            Scanner s=new Scanner(f);
+            while(s.hasNext()){
+                if(s.toString().equals(" ")){
+                    break;
+                }
+                index=index+1;
+                s.nextLine();
+            }
+        }
+        System.out.println(index);
+        return index;
     }
 }
