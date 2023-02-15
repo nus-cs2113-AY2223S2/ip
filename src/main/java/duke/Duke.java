9d5b104447b251115417981a316cf8c31db92911
khooyourun <khooyourun@hotmail.com>
2023-02-15 17:24:00 +0800

    Changed to arrayList and added delete feature

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -5,6 +5,7 @@ import duke.tasks.Event;
 import duke.tasks.ToDo;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
@@ -13,7 +14,7 @@ public class Duke {
         System.out.println("Good day. YodaBot, I am.");
         System.out.println("Assistance, you need?");
         Scanner scanner = new Scanner(System.in);
-        Task[] taskArray = new Task[100];
+        ArrayList<Task> taskArray = new ArrayList<Task>();
         int taskIndex = 0;
         while (true) {
             String command = scanner.nextLine(); //reads in input
@@ -40,14 +41,14 @@ public class Duke {
                 } else {
                     System.out.println("As shown, list is:");
                     for (int j = 0; j < taskIndex; ++j) {
-                        if (taskArray[j] instanceof ToDo) {
-                            System.out.println(j + ". " + ((ToDo) taskArray[j]).getToDo() + " " + taskArray[j].getDoneStatus() + " " + taskArray[j].getDescription());
+                        if (taskArray.get(j) instanceof ToDo) {
+                            System.out.println(j + ". " + ((ToDo) taskArray.get(j)).getToDo() + " " + taskArray.get(j).getDoneStatus() + " " + taskArray.get(j).getDescription());
                         }
-                        if (taskArray[j] instanceof Deadline) {
-                            System.out.println(j + ". " + ((Deadline) taskArray[j]).getDeadline() + " " + taskArray[j].getDoneStatus() + " " + taskArray[j].getDescription() + " (" + ((Deadline) taskArray[j]).getDate() + ")");
+                        if (taskArray.get(j) instanceof Deadline) {
+                            System.out.println(j + ". " + ((Deadline) taskArray.get(j)).getDeadline() + " " + taskArray.get(j).getDoneStatus() + " " + taskArray.get(j).getDescription() + " (" + ((Deadline) taskArray.get(j)).getDate() + ")");
                         }
-                        if (taskArray[j] instanceof Event) {
-                            System.out.println(j + ". " + ((Event) taskArray[j]).getEvent() + " " + taskArray[j].getDoneStatus() + " " + taskArray[j].getDescription() + " (" + ((Event) taskArray[j]).getStartAndEnd() + ")");
+                        if (taskArray.get(j) instanceof Event) {
+                            System.out.println(j + ". " + ((Event) taskArray.get(j)).getEvent() + " " + taskArray.get(j).getDoneStatus() + " " + taskArray.get(j).getDescription() + " (" + ((Event) taskArray.get(j)).getStartAndEnd() + ")");
                         }
                     }
                 }
@@ -58,16 +59,16 @@ public class Duke {
                 if (taskIndex == 0 || indexToMark > taskIndex) {
                     System.out.println("In list, it is not.");
                 } else {
-                    taskArray[indexToMark].isDone = true;
+                    taskArray.get(indexToMark).isDone = true;
                     System.out.println("Marked it, I have:");
-                    if (taskArray[indexToMark] instanceof ToDo) {
-                        System.out.println(indexToMark + ". " + ((ToDo) taskArray[indexToMark]).getToDo() + " " + taskArray[indexToMark].getDoneStatus() + " " + taskArray[indexToMark].getDescription());
+                    if (taskArray.get(indexToMark) instanceof ToDo) {
+                        System.out.println(indexToMark + ". " + ((ToDo) taskArray.get(indexToMark)).getToDo() + " " + taskArray.get(indexToMark).getDoneStatus() + " " + taskArray.get(indexToMark).getDescription());
                     }
-                    if (taskArray[indexToMark] instanceof Deadline) {
-                        System.out.println(indexToMark + ". " + ((Deadline) taskArray[indexToMark]).getDeadline() + " " + taskArray[indexToMark].getDoneStatus() + " " + taskArray[indexToMark].getDescription() + "(" + ((Deadline) taskArray[indexToMark]).getDate() + ")");
+                    if (taskArray.get(indexToMark) instanceof Deadline) {
+                        System.out.println(indexToMark + ". " + ((Deadline) taskArray.get(indexToMark)).getDeadline() + " " + taskArray.get(indexToMark).getDoneStatus() + " " + taskArray.get(indexToMark).getDescription() + "(" + ((Deadline) taskArray.get(indexToMark)).getDate() + ")");
                     }
-                    if (taskArray[indexToMark] instanceof Event) {
-                        System.out.println(indexToMark + ". " + ((Event) taskArray[indexToMark]).getEvent() + " " + taskArray[indexToMark].getDoneStatus() + " " + taskArray[indexToMark].getDescription() + " (" + ((Event) taskArray[indexToMark]).getStartAndEnd() + ")");
+                    if (taskArray.get(indexToMark) instanceof Event) {
+                        System.out.println(indexToMark + ". " + ((Event) taskArray.get(indexToMark)).getEvent() + " " + taskArray.get(indexToMark).getDoneStatus() + " " + taskArray.get(indexToMark).getDescription() + " (" + ((Event) taskArray.get(indexToMark)).getStartAndEnd() + ")");
                     }
                 }
                 break;
@@ -78,16 +79,16 @@ public class Duke {
                     System.out.println("In list, it is not.");
                     break;
                 } else {
-                    taskArray[indexToUnmark].isDone = false;
+                    taskArray.get(indexToUnmark).isDone = false;
                     System.out.println("Unmarked it, I have:");
-                    if (taskArray[indexToUnmark] instanceof ToDo) {
-                        System.out.println(indexToUnmark + ". " + ((ToDo) taskArray[indexToUnmark]).getToDo() + " " + taskArray[indexToUnmark].getDoneStatus() + " " + taskArray[indexToUnmark].getDescription());
+                    if (taskArray.get(indexToUnmark) instanceof ToDo) {
+                        System.out.println(indexToUnmark + ". " + ((ToDo) taskArray.get(indexToUnmark)).getToDo() + " " + taskArray.get(indexToUnmark).getDoneStatus() + " " + taskArray.get(indexToUnmark).getDescription());
                     }
-                    if (taskArray[indexToUnmark] instanceof Deadline) {
-                        System.out.println(indexToUnmark + ". " + ((Deadline) taskArray[indexToUnmark]).getDeadline() + " " + taskArray[indexToUnmark].getDoneStatus() + " " + taskArray[indexToUnmark].getDescription() + " (" + ((Deadline) taskArray[indexToUnmark]).getDate() + ")");
+                    if (taskArray.get(indexToUnmark) instanceof Deadline) {
+                        System.out.println(indexToUnmark + ". " + ((Deadline) taskArray.get(indexToUnmark)).getDeadline() + " " + taskArray.get(indexToUnmark).getDoneStatus() + " " + taskArray.get(indexToUnmark).getDescription() + " (" + ((Deadline) taskArray.get(indexToUnmark)).getDate() + ")");
                     }
-                    if (taskArray[indexToUnmark] instanceof Event) {
-                        System.out.println(indexToUnmark + ". " + ((Event) taskArray[indexToUnmark]).getEvent() + " " + taskArray[indexToUnmark].getDoneStatus() + " " + taskArray[indexToUnmark].getDescription() + " (" + ((Event) taskArray[indexToUnmark]).getStartAndEnd() + ")");
+                    if (taskArray.get(indexToUnmark) instanceof Event) {
+                        System.out.println(indexToUnmark + ". " + ((Event) taskArray.get(indexToUnmark)).getEvent() + " " + taskArray.get(indexToUnmark).getDoneStatus() + " " + taskArray.get(indexToUnmark).getDescription() + " (" + ((Event) taskArray.get(indexToUnmark)).getStartAndEnd() + ")");
                     }
                 }
                 break;
@@ -97,8 +98,9 @@ public class Duke {
                     if (remainingWords == null) {
                         throw new IllegalCommandException();
                     }
-                    taskArray[taskIndex] = new ToDo(remainingWords, false, "[T]");
-                    System.out.println(taskIndex + ". " + ((ToDo) taskArray[taskIndex]).getToDo() + " " + taskArray[taskIndex].getDoneStatus() + " " + taskArray[taskIndex].getDescription() + " - Added, I have");
+                    Task newTask = new ToDo(remainingWords, false, "[T]");
+                    taskArray.add(newTask);
+                    System.out.println(taskIndex + ". " + ((ToDo) taskArray.get(taskIndex)).getToDo() + " " + taskArray.get(taskIndex).getDoneStatus() + " " + taskArray.get(taskIndex).getDescription() + " - Added, I have");
                     ++taskIndex;
                 } catch (IllegalCommandException e) {
                     System.out.println("Error: To do what, I ask?");
@@ -110,8 +112,9 @@ public class Duke {
                 int demarcator = remainingWords.indexOf("/");
                 String work = remainingWords.substring(0, demarcator - 1);
                 String date = remainingWords.substring(demarcator + 1, remainingWords.length());
-                taskArray[taskIndex] = new Deadline(work, false, "[D]", date);
-                System.out.println(taskIndex + ". " + ((Deadline) taskArray[taskIndex]).getDeadline() + " " + taskArray[taskIndex].getDoneStatus() + " " + taskArray[taskIndex].getDescription() + " " + ((Deadline) taskArray[taskIndex]).getDate() + " - Added, I have");
+                Task newTask = new Deadline(work, false, "[D]", date);
+                taskArray.add(newTask);
+                System.out.println(taskIndex + ". " + ((Deadline) taskArray.get(taskIndex)).getDeadline() + " " + taskArray.get(taskIndex).getDoneStatus() + " " + taskArray.get(taskIndex).getDescription() + " " + ((Deadline) taskArray.get(taskIndex)).getDate() + " - Added, I have");
                 ++taskIndex;
                 break;
             //if command is Event, prompt for event, then prompt for start and end date
@@ -119,9 +122,20 @@ public class Duke {
                 demarcator = remainingWords.indexOf("/");
                 work = remainingWords.substring(0, demarcator - 1);
                 date = remainingWords.substring(demarcator + 1, remainingWords.length());
-                taskArray[taskIndex] = new Event(work, false, "[E]", date);
-                System.out.println(taskIndex + ". " + ((Event) taskArray[taskIndex]).getEvent() + " " + taskArray[taskIndex].getDoneStatus() + " " + taskArray[taskIndex].getDescription() + " " + ((Event) taskArray[taskIndex]).getStartAndEnd() + " - Added, I have");
+                Task newTask1 = new Event(work, false, "[E]", date);
+                taskArray.add(newTask1);
+                System.out.println(taskIndex + ". " + ((Event) taskArray.get(taskIndex)).getEvent() + " " + taskArray.get(taskIndex).getDoneStatus() + " " + taskArray.get(taskIndex).getDescription() + " " + ((Event) taskArray.get(taskIndex)).getStartAndEnd() + " - Added, I have");
                 ++taskIndex;
+                break;
+            case "Delete":
+                int indexToDelete = Integer.parseInt(remainingWords);
+                if (taskIndex == 0 || indexToDelete > taskIndex) {
+                    System.out.println("In list, it is not.");
+                } else {
+                    taskArray.remove(indexToDelete);
+                    taskIndex -= 1;
+                    System.out.println("Deleted, I have");
+                }
                 break;
             //throws IllegalCommandException if the input is not one of the above cases
             default:
