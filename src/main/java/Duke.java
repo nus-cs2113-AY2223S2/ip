025872e7177313968fbd0eab178a64fbb7407db7
clement559 <clementsiut@gmail.com>
2023-02-15 18:19:10 +0800

    **Duke.java** Imported ArrayList package. Changed array of tasks to ArrayList of tasks. Removed use of constant MAX_NUM_OF_TASKS. Renamed printTask to printAddTask for less confusion between deletion and addition. Added exception handler for main, IndexOutOfBounds exception when mark/unmark/delete are not given index numbers of elements currently in the ArrayList. Added printError for the exception handler. Removed use of int index in main code. Checks for size of ArrayList in the individual functions when needed instead. Added Level-6 increment to support deletion of tasks.
    **Task.java**
    Removed additional square brackets in intermediary printing as it has been added to getStatusIcon function instead.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,36 +1,38 @@
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-    public static final int MAX_NUM_OF_TASKS = 100;
     public static final String LINE_BREAK = "---------------------------------------------";
 
-    public static void registerTodo (Task[] lists, String line, int index) throws IndexOutOfBoundsException {
+    public static void registerTodo (ArrayList<Task> lists, String line) throws IndexOutOfBoundsException {
         try {
             String[] inputLine = line.split(" ", 2);
             Task item = new Todo(inputLine[1]);
-            lists[index] = item;
-            printTask(item, index);
+            lists.add(item);
+            int index = lists.size();
+            printAddTask(item, index);
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Your task must be of the following format: task (task name)");
         }
     }
 
-    public static void registerDeadline (Task[] lists, String line, int index) throws IndexOutOfBoundsException {
+    public static void registerDeadline (ArrayList<Task> lists, String line) throws IndexOutOfBoundsException {
         try {
             String[] inputLines = line.split(" ", 2);
             inputLines = inputLines[1].split("/by ");
             String description = inputLines[0];
             String deadline = inputLines[1];
             Task item = new Deadline(description, deadline);
-            lists[index] = item;
-            printTask(item, index);
+            lists.add(item);
+            int index = lists.size();
+            printAddTask(item, index);
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Your deadline must be of the following format: deadline (deadline name) /by (date)");
         }
     }
 
-    public static void registerEvent (Task[] lists, String line, int index) throws IndexOutOfBoundsException {
+    public static void registerEvent (ArrayList<Task> lists, String line) throws IndexOutOfBoundsException {
         try {
             String[] inputLines = line.split(" ", 2);
             inputLines = inputLines[1].split("/from ");
@@ -39,14 +41,25 @@ public class Duke {
             String start = inputLines[0];
             String end = inputLines[1];
             Task item = new Event(description, start, end);
-            lists[index] = item;
-            printTask(item, index);
+            lists.add(item);
+            int index = lists.size();
+            printAddTask(item, index);
         } catch (IndexOutOfBoundsException e) {
         System.out.println("Your event must be of the following format: event (event name) /from (date) /to (date)");
         }
     }
 
-    public static void printTask (Task item, int index) {
+    public static void DeleteTask (ArrayList<Task> lists, int itemNumber) {
+        int index = itemNumber - 1;
+        Task item = lists.get(index);
+        System.out.println("Got it. I've removed this task: " + item.getTypeIcon()
+                + item.getStatusIcon() + item.description);
+        lists.remove(index);
+        System.out.println("Now you have " + lists.size() + " tasks in the list.");
+        System.out.println(LINE_BREAK);
+    }
+
+    public static void printAddTask (Task item, int index) {
         System.out.println("Got it. I've added this task: " + item.getTypeIcon()
                 + item.getStatusIcon() + item.description);
         System.out.println("Now you have " + index + " tasks in the list.");
@@ -65,6 +78,11 @@ public class Duke {
         System.out.println(LINE_BREAK);
     }
 
+    public static void printError() {
+        System.out.println("You chose a task that does not exist!");
+        System.out.println(LINE_BREAK);
+    }
+
     public static String getInput() throws NoSuchElementException {
         Scanner in = new Scanner(System.in);
         String line = "";
@@ -76,27 +94,24 @@ public class Duke {
         return line;
     }
 
-    public static int identifyInput(String line, Task[] lists, int index) throws DukeException {
+    public static void identifyInput(String line, ArrayList<Task> lists) throws DukeException {
         try {
             if (line.startsWith("todo")) {
-                registerTodo(lists, line, index);
+                registerTodo(lists, line);
             } else if (line.startsWith("deadline")) {
-                registerDeadline(lists, line, index);
+                registerDeadline(lists, line);
             } else if (line.startsWith("event")) {
-                registerEvent(lists, line, index);
+                registerEvent(lists, line);
             } else {
                 throw new DukeException();
             }
         } catch (DukeException e){
             System.out.println("I am not a chatbot, please do not chat to me.");
         }
-
-        return (index + 1);
     }
 
-    public static void main(String[] args) throws DukeException {
-        Task[] lists = new Task[MAX_NUM_OF_TASKS];
-        int index = 0;
+    public static void main(String[] args) throws DukeException, IndexOutOfBoundsException {
+        ArrayList<Task> lists = new ArrayList<>();
         printIntro();
         while (true) {
             String line = getInput();
@@ -106,26 +121,29 @@ public class Duke {
                 System.out.println("Here are the tasks in your list:");
                 int itemNumber = 1;
                 for (Task item : lists) {
-                    if ((itemNumber - 1) == index) {
-                        break;
-                    }
                     item.printTask(itemNumber);
                     itemNumber++;
                 }
             }
-            else if (line.startsWith("mark")) {
-                int itemNumber = Integer.parseInt(words[1]);
-                lists[itemNumber - 1].markAsDone();
-            }
-            else if (line.startsWith("unmark")) {
-                int itemNumber = Integer.parseInt(words[1]);
-                lists[itemNumber - 1].markAsUndone();
+            else if (line.startsWith("mark") || line.startsWith("unmark") || line.startsWith("delete")) {
+                try {
+                    int itemNumber = Integer.parseInt(words[1]);
+                    if (line.startsWith("mark")) {
+                        lists.get(itemNumber - 1).markAsDone();
+                    } else if (line.startsWith("unmark")){
+                        lists.get(itemNumber - 1).markAsUndone();
+                    } else {
+                        DeleteTask(lists, itemNumber);
+                    }
+                } catch (IndexOutOfBoundsException e){
+                    printError();
+                }
             }
             else if (line.equals("bye")) {
                 break;
             }
             else {
-                index = identifyInput(line, lists, index);
+                identifyInput(line, lists);
             }
         }
         printExiting();
