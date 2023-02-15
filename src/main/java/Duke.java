87fb9119c34880d77d2410a0d7ae25f4cb4f8da3
saint thiri zaw <saintstz@gmail.com>
2023-02-15 23:53:25 +0800

    Added Save

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
-import duke.Task;
-import duke.Todo;
-import duke.Deadline;
-import duke.Event;
-import duke.DukeException;
+import duke.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
-import java.lang.Character;
+
+import static duke.FileOperations.*;
 
 public class Duke {
     public static void greetUser() {
@@ -21,7 +21,6 @@ public class Duke {
         printLine();
     }
 
-    //exit loop when command received is "bye"
     public static void sayBye() {
         printLine();
         System.out.println("Bye. Hope to see you again soon! :)");
@@ -30,6 +29,113 @@ public class Duke {
 
     public static void printLine() {
         System.out.println("____________________________________________________________");
+    }
+
+    //load the data from the hard disk when Duke starts up
+    //write the existing data into the current list of tasks
+    public static void loadData() throws FileNotFoundException {
+        String filePath = "data/duke.txt";
+        File f = new File(filePath);
+        Scanner s = new Scanner(f);
+        while (s.hasNext()) {
+            String str = s.nextLine();
+            convertFromData(str);
+        }
+    }
+
+    //save the tasks in the hard disk automatically whenever the task list changes
+    public static void saveData() {
+        String filePath = "data/duke.txt";
+        try {
+            writeToFile(filePath, convertToData(Task.tasks.get(0)) + System.lineSeparator());
+            for (int i = 1; i < Task.tasks.size(); i++) {
+                appendToFile(filePath, convertToData(Task.tasks.get(i)) + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
+    }
+
+    //convert data from text file into Task list format to Load Data
+    public static void convertFromData(String str) {
+        String[] task = str.split("\\|");
+        //for (String s : task) { System.out.println(s); }
+        Character c = task[0].charAt(0); //check if task is a To-do, Deadline or Event
+        Character marked = task[1].charAt(1); //check if task is marked done or not
+        if (c.equals('T')) {
+            String description = task[2].substring(1);
+            //System.out.println(description);
+            Task t = new Todo(description);
+            Task.tasks.add(Task.taskCount, t);
+            Task.taskCount++;
+            if (marked.equals('1')) {
+                t.markAsDone();
+            }
+        } else if (c.equals('D')) {
+            String description = task[2].substring(1,(task[2].length()-1));
+            //System.out.println(description);
+            String deadline = task[3].substring(1);
+            //System.out.println(deadline);
+            Task t = new Deadline(description, deadline);
+            Task.tasks.add(Task.taskCount, t);
+            Task.taskCount++;
+            if (marked.equals('1')) {
+                t.markAsDone();
+            }
+        } else {
+            String description = task[2].substring(1,(task[2].length()-1));
+            //System.out.println(description);
+            String event = task[3].substring(1);
+            String[] duration = event.split("-");
+            //System.out.println(event);
+            Task t = new Event(description, duration[0], duration[1]);
+            Task.tasks.add(Task.taskCount, t);
+            Task.taskCount++;
+            if (marked.equals('1')) {
+                t.markAsDone();
+            }
+        }
+    }
+
+    //convert tasks in arraylist to save in text file for Save Data
+    public static String convertToData(Task t) {
+        //String str = String.valueOf(t);
+        String str = ""+t;
+        //System.out.println(str);
+        String data;
+        Character c = str.charAt(1); //check if task is a To-do, Deadline or Event
+        Character marked = str.charAt(4); //check if task is marked done or not
+        String task =  str.substring(6);
+        if (c.equals('T')) {
+            if (marked.equals('X')) {
+                data = ("T | 1 |" + task);
+            } else {
+                data = ("T | 0 |" + task);
+            }
+        } else if (c.equals('D')) {
+            int openBracket = task.indexOf("(");
+            int closeBracket = task.indexOf(")");
+            String description = task.substring(0,openBracket);
+            String by = task.substring(openBracket+1, closeBracket);
+            String[] deadline = by.split("by: ");
+            if (marked.equals('X')) {
+                data = ("D | 1 |" + description + " | " + deadline[1]);
+            } else {
+                data = ("D | 0 |" + description + " | " + deadline[1]);
+            }
+        } else {
+            int openBracket = task.indexOf("(");
+            int closeBracket = task.indexOf(")");
+            String description = task.substring(0,openBracket);
+            String duration = task.substring(openBracket+6, closeBracket);
+            String[] eventTime = duration.split("to: ");
+            if (marked.equals('X')) {
+                data = ("E | 1 |" + description + " | " + eventTime[0].substring(1) + "-" + eventTime[1]);
+            } else {
+                data = ("E | 0 |" + description + " | " + eventTime[0].substring(1) + "-" + eventTime[1]);
+            }
+        }
+        return data;
     }
 
     //check if task description is blank (for to-do)
@@ -46,8 +152,10 @@ public class Duke {
     public static void listTasks(){
         printLine();
         System.out.println("Here are the tasks in your list:");
-        for (int i = 1; i <= Task.taskCount; i++) {
-            System.out.println(i + "." + Task.tasks[i - 1]);
+        int i = 1;
+        for (Task t : Task.tasks) {
+            System.out.println(i + "." + t);
+            i++;
         }
         printLine();
     }
@@ -56,20 +164,25 @@ public class Duke {
     public static void markTask(String[] input){
         try {
             int index = Integer.parseInt(input[1]);
-            Task t = duke.Task.tasks[index-1];
+            if (index < 1 || index > Task.taskCount) {
+                throw new DukeException();
+            }
+            Task t = duke.Task.tasks.get(index - 1);
             t.markAsDone();
+            saveData();
             printLine();
             System.out.println("Nice! I've marked this task as done:");
             System.out.println(t);
             printLine();
-        } catch (NullPointerException e) {
-            System.out.println("☹ OOPS!!! There's not that many tasks in here. Please try again.");
-            printLine();
         } catch (NumberFormatException e) {
-            System.out.println("☹ OOPS!!! You entered an invalid task number. Please try again.");
+            System.out.println("☹ OOPS!!! Task number is blank. Please try again.");
             printLine();
         } catch (IndexOutOfBoundsException e) {
             System.out.println("☹ OOPS!!! Task number is missing. Please try again. ");
+            printLine();
+        } catch (DukeException e) {
+            System.out.println("☹ OOPS!!! You entered an invalid task number. Please try again.");
+            printLine();
         }
     }
 
@@ -77,20 +190,53 @@ public class Duke {
     public static void unmarkTask(String[] input){
         try {
             int index = Integer.parseInt(input[1]);
-            Task t = Task.tasks[index-1];
+            if (index < 1 || index > Task.taskCount) {
+                throw new DukeException();
+            }
+            Task t = duke.Task.tasks.get(index - 1);
             t.markNotDone();
+            saveData();
             printLine();
             System.out.println("OK, I've marked this task as not done yet:");
             System.out.println(t);
             printLine();
-        } catch (NullPointerException e) {
-            System.out.println("☹ OOPS!!! There's not that many tasks in here. Please try again.");
-            printLine();
         } catch (NumberFormatException e) {
-            System.out.println("☹ OOPS!!! You entered an invalid task number. Please try again.");
+            System.out.println("☹ OOPS!!! Task number is blank. Please try again.");
             printLine();
         } catch (IndexOutOfBoundsException e) {
             System.out.println("☹ OOPS!!! Task number is missing. Please try again. ");
+            printLine();
+        } catch (DukeException e) {
+            System.out.println("☹ OOPS!!! You entered an invalid task number. Please try again.");
+            printLine();
+        }
+    }
+
+    //delete task
+    public static void deleteTask(String[] input){
+        try {
+            int index = Integer.parseInt(input[1]);
+            if (index < 1 || index > Task.taskCount) {
+                throw new DukeException();
+            }
+            Task t = duke.Task.tasks.get(index - 1);
+            Task.tasks.remove(index - 1);
+            Task.taskCount--;
+            saveData();
+            printLine();
+            System.out.println("Noted. I've removed this task:");
+            System.out.println(t);
+            System.out.println("Now you have " + Task.taskCount + " tasks in the list.");
+            printLine();
+        } catch (NumberFormatException e) {
+            System.out.println("☹ OOPS!!! Task number is blank. Please try again.");
+            printLine();
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("☹ OOPS!!! Task number is missing. Please try again. ");
+            printLine();
+        } catch (DukeException e) {
+            System.out.println("☹ OOPS!!! You entered an invalid task number. Please try again.");
+            printLine();
         }
     }
 
@@ -101,8 +247,9 @@ public class Duke {
                 throw new DukeException();
             }
             Task t = new Todo(input[1]);
-            Task.tasks[Task.taskCount] = t;
+            Task.tasks.add(Task.taskCount, t);
             Task.taskCount++;
+            saveData();
             printLine();
             System.out.println("Got it. I've added this task:");
             System.out.println(t);
@@ -129,8 +276,9 @@ public class Duke {
             }
             String[] doBy = input[1].split("/by ", 2);
             Task t = new Deadline(doBy[0], doBy[1]);
-            Task.tasks[Task.taskCount] = t;
+            Task.tasks.add(Task.taskCount, t);
             Task.taskCount++;
+            saveData();
             printLine();
             System.out.println("Got it. I've added this task:");
             System.out.println(t);
@@ -158,8 +306,9 @@ public class Duke {
             String[] start = input[1].split("/from ", 2);
             String[] end = start[1].split("/to ", 2);
             Task t = new Event(start[0], end[0], end[1] );
-            Task.tasks[Task.taskCount] = t;
+            Task.tasks.add(Task.taskCount, t);
             Task.taskCount++;
+            saveData();
             printLine();
             System.out.println("Got it. I've added this task:");
             System.out.println(t);
@@ -199,6 +348,10 @@ public class Duke {
                     case "unmark":
                         unmarkTask(input);
                         break;
+                    //delete task
+                    case "delete":
+                        deleteTask(input);
+                        break;
                     //to-do
                     case "todo":
                         addTodo(input);
@@ -224,9 +377,18 @@ public class Duke {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         greetUser();
+        try {
+            loadData();
+        } catch (IOException e) {
+            System.out.println("☹ OOPS!!! The file does not exist. Creating a new file.");
+            String pathName = "data";
+            String filePath = "data/duke.txt";
+            createFile(filePath, pathName);
+        }
         handleCommands();
+        saveData();
         sayBye();
     }
 }
