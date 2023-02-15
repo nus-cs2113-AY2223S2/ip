709e0ab161085c57d119c81ea113b4765d37502a
jared-ee <eejared01@gmail.com>
2023-02-16 01:18:27 +0800

    level0

--- src/main/java/level0.java
+++ src/main/java/level0.java
@@ -3,6 +3,5 @@ public class level0 {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("change");
     }
 }
