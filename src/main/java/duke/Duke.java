c08ffeaa6b58cb2544f8617cc599eb5eb6f143e1
Mitch Malvin <e0773911@u.nus.edu>
2023-02-15 14:59:48 +0800

    add delete

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,16 +1,20 @@
 package duke;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
-    private static Task[] taskList = new Task[100];
+    //private static Task[] taskList = new Task[100];
+    private static ArrayList<Task> taskList = new ArrayList<>() ;
     private static int counter = 0;
     private static final String LINE = "___________________________________________";
     private static final String EXIT_STRING = "bye";
     private static final String LIST_STRING = "list";
     private static final String MARK_STRING = "mark";
     private static final String UNMARK_STRING = "unmark";
+
+    private static final String DELETE_STRING = "delete";
 
     private static boolean isExecuting;
 
@@ -25,7 +29,7 @@ public class Duke {
         System.out.println("\t" + LINE);
         System.out.println("\t Here are the tasks for your list: ");
         for (int i = 0; i < counter; i++) {
-            System.out.println("\t " + (i + 1) + "." + taskList[i]);
+            System.out.println("\t " + (i + 1) + "." + taskList.get(i));
         }
         System.out.println("\t" + LINE);
     }
@@ -45,28 +49,43 @@ public class Duke {
     }
 
     public static void markTaskAndPrint(int taskIndex) throws DukeException {
-        if (taskIndex < 0 || taskList[taskIndex] == null) {
+        if (taskIndex < 0 || taskList.get(taskIndex) == null) {
             throw new DukeException("Please ensure that you enter the correct task number");
         }
         //only gets executed if no exception is thrown
-        taskList[taskIndex].isDone = true;
+        taskList.get(taskIndex).isDone = true;
         System.out.println("\t" + LINE);
         System.out.println("\t Nice, I have marked this task as done: ");
-        System.out.println("\t [" + taskList[taskIndex].getStatusIcon() + "] " + taskList[taskIndex].description);
+        System.out.println("\t [" + taskList.get(taskIndex).getStatusIcon() + "] " + taskList.get(taskIndex).description);
         System.out.println("\t" + LINE);
     }
 
     public static void unmarkTaskAndPrint(int taskIndex) throws DukeException {
-        if (taskIndex < 0 || taskList[taskIndex] == null) {
+        if (taskIndex < 0 || taskList.get(taskIndex) == null) {
             throw new DukeException("Please ensure that you enter the correct task number");
         }
 
-        taskList[taskIndex].isDone = false;
+        taskList.get(taskIndex).isDone = false;
         System.out.println("\t" + LINE);
         System.out.println("\t Ouch, I have unmarked this task: ");
-        System.out.println("\t [" + taskList[taskIndex].getStatusIcon() + "] " + taskList[taskIndex].description);
+        System.out.println("\t [" + taskList.get(taskIndex).getStatusIcon() + "] " + taskList.get(taskIndex).description);
         System.out.println("\t" + LINE);
     }
+
+    public static void deleteTaskAndPrint(int indexToDelete) throws DukeException {
+        if (indexToDelete < 0 || taskList.get(indexToDelete) == null) {
+            throw new DukeException("Please ensure that you enter the correct task number to remove");
+        }
+
+        System.out.println("\t" + LINE);
+        System.out.println("\t Noted. I've removed this task: ");
+        System.out.println("\t " + taskList.get(indexToDelete));
+        System.out.println("\t Now you have " + Integer.toString(taskList.size() -1 )+ " tasks in your list");
+        System.out.println("\t" + LINE);
+        taskList.remove(indexToDelete);
+
+    }
+
 
     public static void handleAddTask(String taskType, String commandInfo) {
         Task newTask = null;
@@ -82,7 +101,7 @@ public class Duke {
             newTask = new Event(infoArr[0].trim(), infoArr[1].trim(), infoArr[2].trim());
         }
 
-        taskList[counter] = newTask;
+        taskList.add(newTask) ;
         counter++;
         printTask(newTask);
     }
@@ -116,6 +135,14 @@ public class Duke {
             } else {
                 int indexToUnmark = Integer.parseInt(inputWords[1]) - 1;
                 unmarkTaskAndPrint(indexToUnmark);
+            }
+        }
+        else if(command.equals(DELETE_STRING)) {
+            if(inputWords.length <2) {
+                throw new DukeException("Please specify which task you wish to delete");
+            } else {
+                int indexToDelete = Integer.parseInt(inputWords[1])-1;
+                deleteTaskAndPrint(indexToDelete);
             }
         }
         //check if command string matches either of the string
