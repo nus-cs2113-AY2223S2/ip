2e50cf5238288bcef3660ed2fc4bbd33a898f927
woowenjun99 <woowenjun99@gmail.com>
2023-02-16 18:45:55 +0800

    Remove update file

--- src/main/java/parser/FileParser.java
+++ src/main/java/parser/FileParser.java
@@ -74,45 +74,6 @@ public class FileParser {
         }
     }
 
-    public void updateFile(String messageToChange, String newMessage) {
-        try {
-            // Step 1: Create a temporary file and a writer to it.
-            String TEMP_FILE_NAME = "temp.txt";
-            File temp = new File(TEMP_FILE_NAME);
-            temp.createNewFile();
-            FileWriter writer = new FileWriter(TEMP_FILE_NAME);
-
-            // Step 2: Create a scanner to the current file            
-            String CURRENT_FILE_NAME = "mongo.txt";
-            File db = new File(CURRENT_FILE_NAME);
-            Scanner reader = new Scanner(db);
-
-            // Step 3: If the current line is not the information to be
-            // changed, copy it over to the temp file. If the current 
-            // line is to be changed, add in the newMessage instead.
-            while (reader.hasNextLine()) {
-                String information = reader.nextLine().trim();
-                if (information != messageToChange) {
-                    writer.write(information);
-                } else {
-                    writer.write(newMessage);
-                }
-            }
-
-            // Step 4: Delete the old file and rename the temp file to
-            // be the old file
-            db.delete();
-            temp.renameTo(db);
-
-            // Step 5: Close the reader and the writer
-            reader.close();
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("An error occured during the update process");
-            e.printStackTrace();
-        }
-    }
-
     protected FileParser() {
     }
 
