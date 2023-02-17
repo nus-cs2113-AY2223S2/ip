15bc022c703c39055d67ad7cc211fc5048a85c07
0nandon <tommy9905@gmail.com>
2023-02-17 11:38:27 +0800

    iP : A-collections

--- src/main/java/Onandon/checkpoint/Checkpoint.java
+++ src/main/java/Onandon/checkpoint/Checkpoint.java
@@ -3,25 +3,26 @@ package Onandon.checkpoint;
 import java.io.*;
 import Onandon.module.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Checkpoint {
-    protected static final String PATH = "src/main/java/";
-    protected static final String CHECKPOINT = "checkpoint.txt";
+    protected static final String PATH = System.getProperty("user.dir");
+    protected static final String CHECKPOINT = "/checkpoint.txt";
 
-    public static void storeCheckpoint(Task[] tasks, int cnt){
+    public static void storeCheckpoint(ArrayList<Task> tasks, int cnt){
         String store = "";
 
         for(int i=0; i<cnt; i++){
-            if(tasks[i] instanceof Todo){
-                store += ("T/" + tasks[i].getStatusIcon() + "/" + tasks[i].getDescription() + "\n");
-            } else if (tasks[i] instanceof Deadline) {
-                store += ("D/" + tasks[i].getStatusIcon() + "/" + tasks[i].getDescription());
-                store += "/" + tasks[i].getBy() + "\n";
-            } else if (tasks[i] instanceof Event) {
-                store += "E/" + tasks[i].getStatusIcon() + "/" + tasks[i].getDescription();
-                store += "/" + tasks[i].getFrom() + "/" + tasks[i].getTo() + "\n";
+            if(tasks.get(i) instanceof Todo){
+                store += ("T/" + tasks.get(i).getStatusIcon() + "/" + tasks.get(i).getDescription() + "\n");
+            } else if (tasks.get(i) instanceof Deadline) {
+                store += ("D/" + tasks.get(i).getStatusIcon() + "/" + tasks.get(i).getDescription());
+                store += "/" + tasks.get(i).getBy() + "\n";
+            } else if (tasks.get(i) instanceof Event) {
+                store += "E/" + tasks.get(i).getStatusIcon() + "/" + tasks.get(i).getDescription();
+                store += "/" + tasks.get(i).getFrom() + "/" + tasks.get(i).getTo() + "\n";
             }
         }
 
@@ -36,7 +37,7 @@ public class Checkpoint {
     }
 
     public static List<Object> recallCheckpoint() {
-        Task[] tasks = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<Task>(100);
         String checkpoint = "";
         String[] checkpointArray;
         String[] sen;
@@ -62,26 +63,29 @@ public class Checkpoint {
 
                 switch(sen[0]){
                 case "T":
-                    tasks[cnt] = new Todo(sen[2]);
+                    tasks.add(new Todo(sen[2]));
                     if(sen[1].equals("X"))
-                        tasks[cnt].markAsDone();
+                        tasks.get(cnt).markAsDone();
                     else if(sen[1].equals(" "))
-                        tasks[cnt].markAsUnDone();
+                        tasks.get(cnt).markAsUnDone();
                     cnt += 1;
+                    break;
                 case "D":
-                    tasks[cnt] = new Deadline(sen[2], sen[3]);
+                    tasks.add(new Deadline(sen[2], sen[3]));
                     if(sen[1].equals("X"))
-                        tasks[cnt].markAsDone();
+                        tasks.get(cnt).markAsDone();
                     else if(sen[1].equals(" "))
-                        tasks[cnt].markAsUnDone();
+                        tasks.get(cnt).markAsUnDone();
                     cnt += 1;
+                    break;
                 case "E":
-                    tasks[cnt] = new Event(sen[2], sen[3], sen[4]);
+                    tasks.add(new Event(sen[2], sen[3], sen[4]));
                     if(sen[1].equals("X"))
-                        tasks[cnt].markAsDone();
+                        tasks.get(cnt).markAsDone();
                     else if(sen[1].equals(" "))
-                        tasks[cnt].markAsUnDone();
+                        tasks.get(cnt).markAsUnDone();
                     cnt += 1;
+                    break;
                 }
             }
 
