117e8a10c4aa97d95db1ede256e66f13249477a7
NicholasChungJunJie <84615041+NicholasChungJunJie@users.noreply.github.com>
2023-02-18 15:51:29 +0800

    Refactor Storage.java

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,9 +1,9 @@
+import java.util.Scanner;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Storage {
 
