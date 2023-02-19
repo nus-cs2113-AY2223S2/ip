05eb4c510b43b251172ebecdde53c3d4563df446
Vignesh-30 <30vignesh@gmail.com>
2023-02-19 11:49:02 +0800

    changes in taskList to accesss ArrayList of tasks

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -8,16 +8,15 @@ import duke.task.Todo;
 import java.util.ArrayList;
 
 public class TaskList {
-    public static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void doCommandMark(int taskNum) {
         Duke.isFileEdited = true;
         try {
-            tasks.get(--taskNum).setStatus(true);
+            Duke.tasks.get(--taskNum).setStatus(true);
             if (Duke.toPrint) {
                 System.out.println(Ui.LINE);
                 System.out.println("\tNoted. Task " + (taskNum + 1) + " has been marked as \"complete\":");
-                System.out.println("\t  " + tasks.get(taskNum).getTaskNameAndStatus());
+                System.out.println("\t  " + Duke.tasks.get(taskNum).getTaskNameAndStatus());
                 System.out.println(Ui.LINE);
             }
         } catch (IndexOutOfBoundsException | NullPointerException out_mark_b) {
@@ -30,11 +29,11 @@ public class TaskList {
     public static void doCommandUnmark(int taskNum) {
         Duke.isFileEdited = true;
         try {
-            tasks.get(--taskNum).setStatus(false);
+            Duke.tasks.get(--taskNum).setStatus(false);
             if (Duke.toPrint) {
                 System.out.println(Ui.LINE);
                 System.out.println("\tOh, ok. Task " + (taskNum + 1) + " has been marked as \"incomplete\":");
-                System.out.println("\t  " + tasks.get(taskNum).getTaskNameAndStatus());
+                System.out.println("\t  " + Duke.tasks.get(taskNum).getTaskNameAndStatus());
                 System.out.println(Ui.LINE);
             }
         } catch (IndexOutOfBoundsException | NullPointerException out_unmark_b) {
@@ -53,7 +52,7 @@ public class TaskList {
             System.out.println("\tHere are your tasks:");
             for (int index = 0; index < Duke.taskCount; index++) {
                 System.out.print("\t" + count + ".");
-                System.out.println(tasks.get(index));
+                System.out.println(Duke.tasks.get(index));
                 count++;
             }
         }
@@ -62,12 +61,12 @@ public class TaskList {
 
     public static void doCommandTodo(String taskName) {
         Duke.isFileEdited = true;
-        tasks.add(new Todo(taskName));
+        Duke.tasks.add(new Todo(taskName));
         Duke.taskCount++;
         if (Duke.toPrint) {
             System.out.println(Ui.LINE);
             System.out.println("\t" + "Task added!");
-            System.out.println("\t  " + tasks.get(Duke.taskCount - 1));
+            System.out.println("\t  " + Duke.tasks.get(Duke.taskCount - 1));
             System.out.println("\t" + "Now you have " + Duke.taskCount + " pending tasks.");
             System.out.println(Ui.LINE);
         }
@@ -75,12 +74,12 @@ public class TaskList {
 
     public static void doCommandDeadline(String taskName, String taskDeadline) {
         Duke.isFileEdited = true;
-        tasks.add(new Deadline(taskName, taskDeadline));
+        Duke.tasks.add(new Deadline(taskName, taskDeadline));
         Duke.taskCount++;
         if (Duke.toPrint) {
             System.out.println(Ui.LINE);
             System.out.println("\t" + "Task added!");
-            System.out.println("\t  " + tasks.get(Duke.taskCount - 1));
+            System.out.println("\t  " + Duke.tasks.get(Duke.taskCount - 1));
             System.out.println("\t" + "Now you have " + Duke.taskCount + " pending tasks.");
             System.out.println(Ui.LINE);
         }
@@ -88,12 +87,12 @@ public class TaskList {
 
     public static void doCommandEvent(String eventName, String eventDetailsPartOne, String eventDetailsPartTwo) {
         Duke.isFileEdited = true;
-        tasks.add(new Event(eventName, eventDetailsPartOne, eventDetailsPartTwo));
+        Duke.tasks.add(new Event(eventName, eventDetailsPartOne, eventDetailsPartTwo));
         Duke.taskCount++;
         if (Duke.toPrint) {
             System.out.println(Ui.LINE);
             System.out.println("\t" + "Task added!");
-            System.out.println("\t  " + tasks.get(Duke.taskCount - 1));
+            System.out.println("\t  " + Duke.tasks.get(Duke.taskCount - 1));
             System.out.println("\t" + "Now you have " + Duke.taskCount + " pending tasks.");
             System.out.println(Ui.LINE);
         }
@@ -102,10 +101,10 @@ public class TaskList {
     public static void doCommandDelete(int taskNum) {
         Duke.isFileEdited = true;
         System.out.println(Ui.LINE);
-        System.out.println("\t  " + tasks.get(taskNum - 1));
+        System.out.println("\t  " + Duke.tasks.get(taskNum - 1));
         System.out.println("\t" + "Task removed!");
         System.out.println("\t" + "Now you have " + (Duke.taskCount - 1) + " pending tasks.");
-        tasks.remove(taskNum - 1);
+        Duke.tasks.remove(taskNum - 1);
         System.out.println(Ui.LINE);
         Duke.taskCount--;
     }
