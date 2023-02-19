359d481cd968d95c1bcba322d7977e0b5fed3c2a
JeraldChen <jeraldchenjunhan@gmail.com>
2023-02-19 15:07:02 +0800

    Completed Level-8 Dates and Times for deadline and event

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,13 @@
 import duke.Task;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
 public class Deadline extends Task{
     public String by;
     public Deadline (String description, String by) {
         super(description);
-        this.by = by;
+        LocalDateTime date = LocalDateTime.parse(by);
+        this.by = date.format(DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm"));
     }
     @Override
     public String toString() {
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,15 @@
 import duke.Task;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 public class Event extends Task{
     protected String from;
     protected String to;
     public Event (String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        LocalDateTime startDate = LocalDateTime.parse(from);
+        LocalDateTime endDate = LocalDateTime.parse(to);
+        this.from = startDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm"));
+        this.to = endDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm"));
     }
 
     @Override
