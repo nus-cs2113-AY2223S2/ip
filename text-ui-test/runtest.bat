67033c5d520d99e8c87b9faf6b134bce82b1bd12
HiIAmTzeKean <ngtzekean600@gmail.com>
2023-02-19 16:28:13 +0800

    test: text-ui-testing update

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,7 @@ REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac -cp ../src/main/java;../build/install/ip/lib/* -Xlint:none -d ../bin ../src/main/java/duke/*.java ../src/main/java/parser/*.java ../src/main/java/task/*.java ../src/main/java/ui/*.java
+javac -cp ../src/main/java;../build/install/ip/lib/* -Xlint:none -d ../bin ../src/main/java/duke/*.java ../src/main/java/parser/*.java ../src/main/java/task/*.java ../src/main/java/ui/*.java ../src/main/java/serialiser/*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
@@ -16,6 +16,9 @@ REM no error here, errorlevel == 0
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -cp ../bin/;../build/install/ip/lib/* duke/Duke < input.txt > ACTUAL.TXT
+
+REM removes the data and file directory to ensure consistent testing
+rmdir /s /q data
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,7 +13,7 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp "../src/main/java:../build/install/ip/lib/*" -Xlint:none -d ../bin ../src/main/java/duke/*.java ../src/main/java/parser/*.java ../src/main/java/task/*.java ../src/main/java/ui/*.java
+if ! javac -cp "../src/main/java:../build/install/ip/lib/*" -Xlint:none -d ../bin ../src/main/java/duke/*.java ../src/main/java/parser/*.java ../src/main/java/task/*.java ../src/main/java/ui/*.java ../src/main/java/serialiser/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
@@ -22,6 +22,8 @@ fi
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -cp '../build/install/ip/lib/*:../bin' duke/Duke < input.txt > ACTUAL.TXT
 
+rm data -R
+echo "Clearing data directory"
 
 # convert to UNIX format
 cp EXPECTED-LINUX.TXT EXPECTED-UNIX.TXT
