7853c51414a987c62e1e2af9e1e205d33eb8fbf0
Vignesh-30 <30vignesh@gmail.com>
2023-02-19 10:55:22 +0800

    Extracted out Ui class to deal with printing messages to user

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -14,40 +14,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    static final String LINE = "\t____________________________________________________________";
-    static final String COMMAND_BYE = "bye";
-    static final String COMMAND_LIST = "list";
-    static final String COMMAND_MARK = "mark";
-    static final String COMMAND_UNMARK = "unmark";
-    static final String COMMAND_EVENT = "event";
-    static final String COMMAND_TODO = "todo";
-    static final String COMMAND_DELETE = "delete";
-    static final String COMMAND_DEADLINE = "deadline";
     static boolean isFileEdited = false;
     static boolean toPrint = true;
     static ArrayList<Task> tasks = new ArrayList<>();
     static int taskCount = 0;
-
-    public static void printInvalidNumber(String taskType) {
-        System.out.println(LINE);
-        System.out.println("\t☹ Error! Invalid input.");
-        System.out.println("\tPlease provide an integer number for \"" + taskType + "\" command.");
-        System.out.println("\tPlease use \"list\" command to see your task numbers.");
-        System.out.println(LINE);
-    }
-
-    public static void printEmptyCommand(String taskType) {
-        System.out.println(LINE);
-        System.out.println("\t☹ Error! \"" + taskType + "\" command cannot be empty.");
-        System.out.println("\tPlease provide more details");
-        System.out.println(LINE);
-    }
-
-    public static void printInvalidFormat(String taskType) {
-        System.out.println(LINE);
-        System.out.println("\t☹ Error! Invalid format for \"" + taskType + "\" command.");
-        System.out.println(LINE);
-    }
 
     public static void extractData(File fileName) throws FileNotFoundException {
         Scanner s = new Scanner(fileName);
@@ -57,11 +27,12 @@ public class Duke {
             String currentLine = s.nextLine();
             String userCommand = currentLine.substring(4);
             handleUserCommand(userCommand);
-            String markStatus = currentLine.substring(0,4);
+            String markStatus = currentLine.substring(0, 4);
             if (markStatus.equals("[X] ")) {
                 doCommandMark(count);
             } else if (!markStatus.equals("[ ] ")) {
-                System.out.println("\t☹ Error! File saved in wrong format.");
+                // print task wrong format
+                Ui.printIncorrectTaskFormat();
             }
             count++;
         }
@@ -83,17 +54,16 @@ public class Duke {
         try {
             File fileName = new java.io.File(path.toUri());
             if (fileName.createNewFile()) {
-                System.out.println(LINE);
-                System.out.println("\tFile \"duke.txt\" created!");
-                System.out.println(LINE);
+                // print file created
+                Ui.printFileCreated(true);
             } else {
                 Scanner s = new Scanner(fileName);
                 if (!s.hasNext()) {
-                    System.out.println(LINE);
-                    System.out.println("\tSaved file is empty.");
-                    System.out.println(LINE);
+                    //print empty file
+                    Ui.printEmptyFile();
                 } else {
-                    System.out.println(LINE);
+                    //print here are your tasks
+                    System.out.println(Ui.LINE);
                     System.out.println("\tHere are your stored tasks!");
 
                     int index = 1;
@@ -101,28 +71,30 @@ public class Duke {
                         System.out.println("\t" + index + ". " + s.nextLine());
                         index++;
                     }
-                    System.out.println(LINE);
+                    System.out.println(Ui.LINE);
                 }
                 extractData(fileName);
             }
         } catch (IOException e) {
-            System.out.println("☹ Error! Failed to create file.");
+            // print file creation error
+            Ui.printFileCreated(false);
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("☹ Error! Tasks saved in incorrect format.");
+            // print task saved incorrect format
+            Ui.printIncorrectTaskFormat();
         }
     }
 
     public static void doCommandGreet() {
-        System.out.println(LINE);
+        System.out.println(Ui.LINE);
         System.out.println("\tHello! I'm Duke.");
         System.out.println("\tHow can I help you today?\n");
-        System.out.println(LINE);
+        System.out.println(Ui.LINE);
     }
 
     public static void doCommandBye() {
-        System.out.println(LINE);
+        System.out.println(Ui.LINE);
         System.out.println("\tBye! Remember to finish your tasks.\n");
-        System.out.println(LINE);
+        System.out.println(Ui.LINE);
     }
 
     public static void doCommandMark(int taskNum) {
@@ -130,14 +102,14 @@ public class Duke {
         try {
             tasks.get(--taskNum).setStatus(true);
             if (toPrint) {
-                System.out.println(LINE);
+                System.out.println(Ui.LINE);
                 System.out.println("\tNoted. Task " + (taskNum + 1) + " has been marked as \"complete\":");
                 System.out.println("\t  " + tasks.get(taskNum).getTaskNameAndStatus());
-                System.out.println(LINE);
+                System.out.println(Ui.LINE);
             }
         } catch (IndexOutOfBoundsException | NullPointerException out_mark_b) {
             if (toPrint) {
-                printInvalidNumber("mark");
+                Ui.printInvalidNumber("mark");
             }
         }
     }
@@ -147,20 +119,20 @@ public class Duke {
         try {
             tasks.get(--taskNum).setStatus(false);
             if (toPrint) {
-                System.out.println(LINE);
+                System.out.println(Ui.LINE);
                 System.out.println("\tOh, ok. Task " + (taskNum + 1) + " has been marked as \"incomplete\":");
                 System.out.println("\t  " + tasks.get(taskNum).getTaskNameAndStatus());
-                System.out.println(LINE);
+                System.out.println(Ui.LINE);
             }
         } catch (IndexOutOfBoundsException | NullPointerException out_unmark_b) {
             if (toPrint) {
-                printInvalidNumber("unmark");
+                Ui.printInvalidNumber("unmark");
             }
         }
     }
 
     public static void doCommandList() {
-        System.out.println(LINE);
+        System.out.println(Ui.LINE);
         int count = 1;
         if (taskCount == 0) {
             System.out.println("\tYou have no pending tasks! ☺");
@@ -172,7 +144,7 @@ public class Duke {
                 count++;
             }
         }
-        System.out.println(LINE);
+        System.out.println(Ui.LINE);
     }
 
     public static void doCommandTodo(String taskName) {
@@ -180,11 +152,11 @@ public class Duke {
         tasks.add(new Todo(taskName));
         taskCount++;
         if (toPrint) {
-            System.out.println(LINE);
+            System.out.println(Ui.LINE);
             System.out.println("\t" + "Task added!");
             System.out.println("\t  " + tasks.get(taskCount - 1));
             System.out.println("\t" + "Now you have " + taskCount + " pending tasks.");
-            System.out.println(LINE);
+            System.out.println(Ui.LINE);
         }
     }
 
@@ -193,11 +165,11 @@ public class Duke {
         tasks.add(new Deadline(taskName, taskDeadline));
         taskCount++;
         if (toPrint) {
-            System.out.println(LINE);
+            System.out.println(Ui.LINE);
             System.out.println("\t" + "Task added!");
             System.out.println("\t  " + tasks.get(taskCount - 1));
             System.out.println("\t" + "Now you have " + taskCount + " pending tasks.");
-            System.out.println(LINE);
+            System.out.println(Ui.LINE);
         }
     }
 
@@ -206,22 +178,22 @@ public class Duke {
         tasks.add(new Event(eventName, eventDetailsPartOne, eventDetailsPartTwo));
         taskCount++;
         if (toPrint) {
-            System.out.println(LINE);
+            System.out.println(Ui.LINE);
             System.out.println("\t" + "Task added!");
             System.out.println("\t  " + tasks.get(taskCount - 1));
             System.out.println("\t" + "Now you have " + taskCount + " pending tasks.");
-            System.out.println(LINE);
+            System.out.println(Ui.LINE);
         }
     }
 
     public static void doCommandDelete(int taskNum) {
         isFileEdited = true;
-        System.out.println(LINE);
+        System.out.println(Ui.LINE);
         System.out.println("\t  " + tasks.get(taskNum - 1));
         System.out.println("\t" + "Task removed!");
         System.out.println("\t" + "Now you have " + (taskCount - 1) + " pending tasks.");
         tasks.remove(taskNum - 1);
-        System.out.println(LINE);
+        System.out.println(Ui.LINE);
         taskCount--;
     }
 
@@ -229,57 +201,57 @@ public class Duke {
         String[] extractFirstWord = userCommand.split(" ", 2);
         String firstWord = extractFirstWord[0];
         switch (firstWord) {
-        case COMMAND_MARK:
+        case Ui.COMMAND_MARK:
             try {
                 int taskNum = Integer.parseInt(extractFirstWord[1]);
                 doCommandMark(taskNum);
             } catch (IndexOutOfBoundsException | NumberFormatException out_mark_a) {
-                printInvalidNumber("mark");
+                Ui.printInvalidNumber("mark");
             }
             break;
-        case COMMAND_UNMARK:
+        case Ui.COMMAND_UNMARK:
             try {
                 int taskNum = Integer.parseInt(extractFirstWord[1]);
                 doCommandUnmark(taskNum);
             } catch (IndexOutOfBoundsException | NumberFormatException out_unmark_a) {
-                printInvalidNumber("unmark");
+                Ui.printInvalidNumber("unmark");
             }
             break;
-        case COMMAND_LIST:
+        case Ui.COMMAND_LIST:
             doCommandList();
             break;
-        case COMMAND_BYE:
+        case Ui.COMMAND_BYE:
             doCommandBye();
             break;
-        case COMMAND_DELETE:
+        case Ui.COMMAND_DELETE:
             try {
                 int taskNum = Integer.parseInt(extractFirstWord[1]);
                 doCommandDelete(taskNum);
             } catch (IndexOutOfBoundsException | NumberFormatException out_delete_a) {
-                printInvalidNumber("delete");
+                Ui.printInvalidNumber("delete");
             }
             break;
-        case COMMAND_TODO:
+        case Ui.COMMAND_TODO:
             try {
                 String taskName = (extractFirstWord[1]);
                 doCommandTodo(taskName);
             } catch (IndexOutOfBoundsException out_todo_a) {
-                printEmptyCommand("todo");
+                Ui.printEmptyCommand("todo");
             }
             break;
-        case COMMAND_DEADLINE:
+        case Ui.COMMAND_DEADLINE:
             try {
                 int index = extractFirstWord[1].indexOf("/by");
                 String taskName = extractFirstWord[1].substring(0, index);
                 String taskDeadline = extractFirstWord[1].substring(index + 4);
                 doCommandDeadline(taskName, taskDeadline);
             } catch (StringIndexOutOfBoundsException out_deadline_a) {
-                printInvalidFormat("deadline");
+                Ui.printInvalidFormat("deadline");
             } catch (IndexOutOfBoundsException out_deadline_a) {
-                printEmptyCommand("deadline");
+                Ui.printEmptyCommand("deadline");
             }
             break;
-        case COMMAND_EVENT:
+        case Ui.COMMAND_EVENT:
             try {
                 int indexOfEventDetailsPartOne = extractFirstWord[1].indexOf("/from");
                 int indexOfEventDetailsPartTwo = extractFirstWord[1].indexOf("/to");
@@ -288,16 +260,16 @@ public class Duke {
                 String eventDetailsPartTwo = extractFirstWord[1].substring(indexOfEventDetailsPartTwo + 4);
                 doCommandEvent(eventName, eventDetailsPartOne, eventDetailsPartTwo);
             } catch (StringIndexOutOfBoundsException out_event_a) {
-                printInvalidFormat("event");
+                Ui.printInvalidFormat("event");
             } catch (IndexOutOfBoundsException out_event_a) {
-                printEmptyCommand("event");
+                Ui.printEmptyCommand("event");
             }
             break;
         default:
             if (toPrint) {
-                System.out.println(LINE);
+                System.out.println(Ui.LINE);
                 System.out.println("\t☹ Error! Please input a valid command!");
-                System.out.println(LINE);
+                System.out.println(Ui.LINE);
             }
         }
     }
@@ -312,7 +284,7 @@ public class Duke {
         do {
             userCommand = in.nextLine();
             handleUserCommand(userCommand);
-        } while (!userCommand.equals(COMMAND_BYE));
+        } while (!userCommand.equals(Ui.COMMAND_BYE));
         try {
             if (isFileEdited) {
                 doEditFile(path);
---
+++ src/main/java/duke/Ui.java
@@ -0,0 +1,59 @@
+package duke;
+
+public class Ui {
+
+    static final String LINE = "\t____________________________________________________________";
+    static final String COMMAND_BYE = "bye";
+    static final String COMMAND_LIST = "list";
+    static final String COMMAND_MARK = "mark";
+    static final String COMMAND_UNMARK = "unmark";
+    static final String COMMAND_EVENT = "event";
+    static final String COMMAND_TODO = "todo";
+    static final String COMMAND_DELETE = "delete";
+    static final String COMMAND_DEADLINE = "deadline";
+
+    public static void printInvalidNumber(String taskType) {
+        System.out.println(LINE);
+        System.out.println("\t☹ Error! Invalid input.");
+        System.out.println("\tPlease provide an integer number for \"" + taskType + "\" command.");
+        System.out.println("\tPlease use \"list\" command to see your task numbers.");
+        System.out.println(LINE);
+    }
+
+    public static void printEmptyCommand(String taskType) {
+        System.out.println(LINE);
+        System.out.println("\t☹ Error! \"" + taskType + "\" command cannot be empty.");
+        System.out.println("\tPlease provide more details");
+        System.out.println(LINE);
+    }
+
+    public static void printInvalidFormat(String taskType) {
+        System.out.println(LINE);
+        System.out.println("\t☹ Error! Invalid format for \"" + taskType + "\" command.");
+        System.out.println(LINE);
+    }
+
+    public static void printEmptyFile() {
+        System.out.println(LINE);
+        System.out.println("\tSaved file is empty.");
+        System.out.println(LINE);
+    }
+
+    public static void printIncorrectTaskFormat() {
+        System.out.println(LINE);
+        System.out.println("\t☹ Error! Tasks saved in incorrect format.");
+        System.out.println(LINE);
+    }
+
+    public static void printFileCreated(boolean fileCreated) {
+        if (fileCreated) {
+            System.out.println(LINE);
+            System.out.println("\tFile \"duke.txt\" created!");
+            System.out.println(LINE);
+        } else {
+            System.out.println(LINE);
+            System.out.println("\t☹ Error! Failed to create file.");
+            System.out.println(LINE);
+        }
+    }
+}
