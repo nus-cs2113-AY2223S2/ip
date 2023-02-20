7f8b7454625377c8f84df5a5389554955ded5a6f
SSzeWen <seetszewen@gmail.com>
2023-02-21 00:22:40 +0800

    documentation for main Duke program

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,13 +2,28 @@ package duke;
 
 import duke.command.Command;
 
+/**
+ * Duke Object that runs the entire program
+ * It works with other Objects such as Storage, TaskList, Ui, Parser that handles the different
+ * commands from the user
+ */
 public class Duke {
-
+    /**
+     * Storage object that deals with loading tasks from the file and saving tasks in the file
+     */
     private Storage database;
-
+    /**
+     * TaskList object that contains the task list e.g., it has operations to add/delete tasks in the list
+     */
     public TaskList tasks;
+    /**
+     * Ui object that deals with interactions with the user
+     */
     private Ui ui;
 
+    /**
+     * Initialises the objects of the Duke program and gives a greeting
+     */
     public Duke() {
         ui = new Ui();
         database = new Storage();
@@ -16,6 +31,9 @@ public class Duke {
         Ui.greeting();
     }
 
+    /**
+     * Runs the program by reading inputs from user line by line until user types bye to exit
+     */
     public void run() {
         boolean isProgramRunning = true;
         while (isProgramRunning) {
