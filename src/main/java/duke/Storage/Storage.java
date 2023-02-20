707d8d114299834c24ce3f37810fe5e917709b0d
Matthew Liu <matthew.liu.zhenjie@gmail.com>
2023-02-20 20:09:28 +0800

    Refactor SaveState and LoadState to Storage class.

--- src/main/java/duke/Storage/Storage.java
+++ src/main/java/duke/Storage/Storage.java
@@ -1,36 +1,51 @@
-package duke.SaveManager;
+package duke.Storage;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
-
 import duke.task.Deadline;
 import duke.task.Event;
 import duke.task.Task;
-import duke.SaveManager.RuntimeTypeAdapterFactory;
 import duke.task.ToDo;
+import duke.ui.Ui;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.lang.reflect.Type;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
-public class LoadState {
-
+public class Storage {
+    private final Ui ui = new Ui();
     private static final String saveLocation = System.getProperty("user.dir")+"/save.json";
-
     final TypeToken<ArrayList<Task>> listOfTasks = new TypeToken<ArrayList<Task>>(){};
-    RuntimeTypeAdapterFactory<Task> adapter = RuntimeTypeAdapterFactory.of(Task.class,"type", true)
+    RuntimeTypeAdapterFactory<Task> adapter = RuntimeTypeAdapterFactory.of(Task.class,"type")
             .registerSubtype(ToDo.class, "ToDo")
             .registerSubtype(Deadline.class, "Deadline")
             .registerSubtype(Event.class, "Event");
-
-    GsonBuilder builder = new GsonBuilder().registerTypeAdapterFactory(adapter);
+    GsonBuilder builder = new GsonBuilder().setPrettyPrinting().registerTypeAdapterFactory(adapter);
     Gson gson = builder.create();
+
+    public void saveToFile(ArrayList<Task> taskList){
+        File saveFile = new File(saveLocation);
+        String gsonData = gson.toJson(taskList);
+        if (!saveFile.exists()){
+            try {
+                saveFile.createNewFile();
+            }
+            catch (IOException e){
+                ui.showException("IOException");
+            }
+
+        }
+        try{
+            FileWriter taskWriter;
+            taskWriter = new FileWriter(saveFile.getAbsoluteFile(),false);
+            taskWriter.write(gsonData);
+            taskWriter.close();
+        }catch (IOException e){
+            System.out.println("IOException");
+        }
+    }
     public ArrayList<Task> loadTasks(){
         File saveFile = new File(saveLocation);
         ArrayList<Task> savedList = new ArrayList<>();
@@ -48,24 +63,3 @@ public class LoadState {
         return savedList;
     }
 }
-
-/*
-    public void readFromFile(){
-
-        File saveFile = new File(saveLocation);
-        if(!saveFile.exists()){
-            return;
-        }
-        InputStreamReader fileReader;
-        try{
-            fileReader = new InputStreamReader(new FileInputStream(saveFile), StandardCharsets.UTF_8);
-            System.out.println("entered");
-            JsonReader gsonInterpreter = new JsonReader(fileReader);
-            System.out.println("Entered too");
-            Task[] savedList = gson.fromJson(fileReader, Task[].class);
-        }catch (Exception e){
-            System.out.println("weird error");
-        }
-    }
-
- */
