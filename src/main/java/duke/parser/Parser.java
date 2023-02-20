ffeb64ecc2ff60f82c364937032507178df7e73c
Matthew Liu <matthew.liu.zhenjie@gmail.com>
2023-02-20 20:10:36 +0800

    Add Parser. Update Ui class.

---
+++ src/main/java/duke/parser/Parser.java
@@ -0,0 +1,114 @@
+package duke.parser;
+
+import duke.task.TaskList;
+import duke.exception.EmptyTaskException;
+import duke.ui.Ui;
+
+public class Parser {
+    private static Ui ui;
+    private static final int OFFSET = -1;
+    public Parser(){
+        ui = new Ui();
+    }
+
+    private static boolean isMarkCommand(String userInput, String Command){
+        return userInput.length()>5 && Command.equalsIgnoreCase("mark");
+    }
+
+    private static boolean isUnmarkCommand(String userInput, String Command){
+        return userInput.length()>7 && Command.equalsIgnoreCase("unmark");
+    }
+
+    private static boolean isDeleteCommand(String userInput, String Command){
+        return userInput.length()>7 && Command.equalsIgnoreCase("delete");
+    }
+    private static boolean isToDoCommand(String userInput, String Command){
+        return userInput.length()>5 && Command.equalsIgnoreCase("todo");
+    }
+    private static boolean isDeadlineCommand(String userInput, String Command){
+        return userInput.length()>9 && Command.equalsIgnoreCase("deadline");
+    }
+    private static boolean isEventCommand(String userInput, String Command){
+        return userInput.length()>6 && Command.equalsIgnoreCase("event");
+    }
+
+    public static void parseInput(String userInput, TaskList taskList){
+        userInput = userInput.trim(); //removes excess whitespace in front and back of command
+        String[] commandLine = userInput.split(" "); //split substrings by whitespaces
+        String remainder = userInput.substring(userInput.indexOf(" ")+1);
+        if (userInput.equalsIgnoreCase("list")){
+            taskList.listTasks();
+        } else if (userInput.equalsIgnoreCase("helo")){
+            ui.printHelp();
+        }else if (isMarkCommand(userInput, commandLine[0])){
+            parseMarkCommand(remainder, taskList);
+        } else if (isUnmarkCommand(userInput, commandLine[0])){
+            parseUnmarkCommand(remainder, taskList);
+        } else if (isDeleteCommand(userInput, commandLine[0])){
+            parseDeleteCommand(remainder, taskList);
+        } else if (isToDoCommand(userInput,commandLine[0])){
+            parseToDoCommand(remainder, taskList);
+        } else if (isDeadlineCommand(userInput, commandLine[0])){
+            parseDeadlineCommand(remainder, taskList);
+        } else if (isEventCommand(userInput, commandLine[0])) {
+            parseEventCommand(remainder, taskList);
+        } else if (userInput.equalsIgnoreCase("bye")){
+            ui.handleExit();
+        } else{
+            ui.showException("IllegalCommandException");
+        }
+    }
+
+    private static void parseEventCommand(String remainder, TaskList taskList) {
+        try{
+            taskList.generateEvent(remainder);
+        } catch(EmptyTaskException e){
+            ui.showException("EmptyTaskException");
+        }
+    }
+
+    private static void parseDeadlineCommand(String remainder, TaskList taskList) {
+        try{
+            taskList.generateDeadline(remainder);
+        }catch(EmptyTaskException e){
+            ui.showException("EmptyTaskException");
+        }
+    }
+
+    private static void parseToDoCommand(String remainder, TaskList taskList) {
+        try{
+            taskList.generateToDo(remainder);
+        } catch(EmptyTaskException e){
+            ui.showException("EmptyTaskException");
+        }
+    }
+
+
+    private static void parseDeleteCommand(String taskIndex, TaskList taskList) {
+        try{
+            int targetIndex = Integer.parseInt(taskIndex) + OFFSET;
+            taskList.deleteTask(targetIndex);
+        }catch(NumberFormatException e){
+            ui.showException("NumberFormatException");
+        }
+    }
+
+    private static void parseUnmarkCommand(String taskIndex, TaskList taskList) {
+        try{
+            int targetIndex = Integer.parseInt(taskIndex) + OFFSET;
+            taskList.markTaskAsUndone(targetIndex);
+        } catch(NumberFormatException e){
+            ui.showException("NumberFormatException");
+        }
+    }
+
+    private static void parseMarkCommand(String taskIndex, TaskList taskList) {
+        try{
+            int targetIndex = Integer.parseInt(taskIndex) + OFFSET;
+            taskList.markTaskAsDone(targetIndex); //fix in task manager
+        } catch (NumberFormatException e){
+            ui.showException("NumberFormatException");
+        }
+    }
+
+}
--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -1,19 +1,42 @@
 package duke.ui;
 
+import duke.task.Task;
+
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ui {
-    public static final String LOGO = " ____        _        \n"
-            + "|  _ \\ _   _| | _____ \n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|";
+    public static final String LOGO = " _______             __                 \n" +
+            "/       \\           /  |                \n" +
+            "$$$$$$$  | __    __ $$ |   __   ______  \n" +
+            "$$ |  $$ |/  |  /  |$$ |  /  | /      \\ \n" +
+            "$$ |  $$ |$$ |  $$ |$$ |_/$$/ /$$$$$$  |\n" +
+            "$$ |  $$ |$$ |  $$ |$$   $$<  $$    $$ |\n" +
+            "$$ |__$$ |$$ \\__$$ |$$$$$$  \\ $$$$$$$$/ \n" +
+            "$$    $$/ $$    $$/ $$ | $$  |$$       |\n" +
+            "$$$$$$$/   $$$$$$/  $$/   $$/  $$$$$$$/ ";
 
-    public static final String DIVIDER = "_______________________________________________________________";
+    private static final String GOODBYE = "" +
+            "   ______                     __    __                 \n" +
+            "  / ____/  ____   ____   ____/ /   / /_    __  __  ___ \n" +
+            " / / __   / __ \\ / __ \\ / __  /   / __ \\  / / / / / _ \\\n" +
+            "/ /_/ /  / /_/ // /_/ // /_/ /   / /_/ / / /_/ / /  __/\n" +
+            "\\____/   \\____/ \\____/ \\__,_/   /_.___/  \\__, /  \\___/ \n" +
+            "                                        /____/         ";
+    private static final String DIVIDER = "_______________________________________________________________";
 
-    public static final String WELCOME = "\"Hello! I'm Duke\\nWhat can I do for you?\"";
+    private static final String WELCOME = "\"Hello! I'm Duke!\" \n\"What can I do for you?\"";
+    private static final String FAREWELL = "Bye! Hope to see you again!";
+    private static final String EMPTYTASKEXCEPTION_MSG = "Task description cannot be empty!";
+    private static final String ILLEGALCOMMANDEXCEPTION_MSG = "Illegal Command entered. Type 'help' for assistance.";
+    private static final String NUMBERFORMATEXCEPTION_MSG = "You can only use this command with a valid integer";
+
+    private static final String INVALID_DEADLINE_EVENT_MSG = "Invalid Deadline or Event Command. Type \'help\' " +
+            "for more assistance";
+    private static final String INVALID_ID = "Not a valid ID";
+
     private final Scanner in;
     private final PrintStream out;
 
@@ -29,6 +52,25 @@ public class Ui {
         return inputLine.trim().isEmpty();
     }
 
+    public void showException(String message){
+        switch(message) {
+        case "EmptyTaskException":
+            out.println(EMPTYTASKEXCEPTION_MSG);
+            break;
+        case "NumberFormatException":
+            out.println(NUMBERFORMATEXCEPTION_MSG);
+            break;
+        case "IllegalCommandException":
+            out.println(ILLEGALCOMMANDEXCEPTION_MSG);
+            break;
+        case "Invalid ID":
+            out.println(INVALID_ID);
+        case "Invalid Deadline or Event":
+            out.println(INVALID_DEADLINE_EVENT_MSG);
+        default:
+            break;
+        }
+    }
     public String getUserCommand(){
         out.print("Enter Command: ");
         String fullInputLine = in.nextLine();
@@ -48,5 +90,57 @@ public class Ui {
 
     public void showWelcome(){
         showToUser(DIVIDER, DIVIDER, LOGO, DIVIDER, DIVIDER, WELCOME, DIVIDER);
+    }
+
+    public void handleExit(){
+        showToUser(FAREWELL, DIVIDER, DIVIDER, GOODBYE, DIVIDER);
+    }
+
+    public void showSuccessfulAdd(Task task){
+        out.println("Got it. I've added this to duke: ");
+        out.println("    " + task.toString());
+        out.println(DIVIDER);
+    }
+
+    public void showSuccessfulMark(Task task){
+        out.println("Nice! I've marked this task as done: ");
+        out.println(task.toString());
+        out.println(DIVIDER);
+    }
+    public void showSuccessfulUnmark(Task task){
+        out.println("Nice! I've marked this task as not done yet: ");
+        out.println(task.toString());
+        out.println(DIVIDER);
+    }
+
+    public void printHelp(){ //duke.Duke prints this if none of the duke.commands below are used
+        System.out.println("Looks like you did not enter a valid duke.Duke.command.\n");
+        System.out.println("Command list:");
+        System.out.println("    1. todo: Adds a duke.Duke.task to be done");
+        System.out.println("        Usage example: todo eat dinner");
+        System.out.println("    2. deadline: Adds a duke.Duke.task to be done, use /by to specify when it is due");
+        System.out.println("        Usage example: deadline submit homework /by 3pm tonight");
+        System.out.println("    3. event: Adds an upcoming event. Use /from and /to to specify when it starts " +
+                "and ends respectively.");
+        System.out.println("        Usage example: event lecture this evening /from 4pm /to 6pm");
+        System.out.println("    4. list: lists all tasks recorded by duke. Only enter the keyword.");
+        System.out.println("    5. mark: Marks a specific duke.Duke.task as done. Command requires a specific " +
+                "index starting from 1");
+        System.out.println("        Usage example: mark 2 (assuming that there are more than two tasks in the list");
+        System.out.println("    6. bye: Exits duke");
+        out.println(DIVIDER);
+    }
+    public void showSuccessfulDelete(Task task, int size){
+        System.out.println("Noted. I've removed this task: ");
+        System.out.println("    " + task.toString());
+        System.out.println("Now you have " +  + size + " tasks in the list");
+        out.println(DIVIDER);
+    }
+    public void listTasks(ArrayList<Task> tasks){
+        for (int i = 0; i < tasks.size(); i++){
+            System.out.print((i+1)+ ".");
+            System.out.println(tasks.get(i).toString());
+        }
+        out.println(DIVIDER);
     }
 }
