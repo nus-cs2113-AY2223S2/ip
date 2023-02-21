692165f9e8a80c99c289dec192059953d4279257
SSzeWen <seetszewen@gmail.com>
2023-02-21 23:47:50 +0800

    Add JavaDocs for all Command type classes

--- src/main/java/duke/command/AddCommand.java
+++ src/main/java/duke/command/AddCommand.java
@@ -7,8 +7,17 @@ import duke.task.Task;
 
 import java.io.IOException;
 
+/**
+ * AddCommand class that inherits from the Command class
+ * Refers to the add task type of Command
+ */
 public abstract class AddCommand extends Command {
-
+    /**
+     * Saves newly added task to database
+     *
+     * @param currTask refers to the Task that is being added
+     * @param database to be used to store the newly added task
+     */
     public void addTaskToDatabase(Task currTask, Storage database) {
         try {
             database.saveAddTask(currTask.getTaskString());
@@ -17,6 +26,12 @@ public abstract class AddCommand extends Command {
         }
     }
 
+    /**
+     * Adds the new task to the TaskList
+     *
+     * @param tasks    the TaskList for the task to be added to
+     * @param currTask the newly created Task Object
+     */
     public void addTaskToTaskList(TaskList tasks, Task currTask) {
         tasks.addTaskToTaskList(currTask);
         Ui.addSpecialTaskMessage(tasks);
--- src/main/java/duke/command/AddDeadlineCommand.java
+++ src/main/java/duke/command/AddDeadlineCommand.java
@@ -5,18 +5,41 @@ import duke.TaskList;
 import duke.task.Deadline;
 import duke.task.Task;
 
-
+/**
+ * AddDeadlineCommand class that inherits from the AddCommand class
+ * Refers to the adding of deadline task type of Command
+ */
 public class AddDeadlineCommand extends AddCommand {
 
-    protected String[] stringSplit;
+    /**
+     * describes the deadline task that the user is referring to
+     */
+    protected String description;
+    /**
+     * when the task should be completed by
+     */
+    protected String deadline;
 
-    public AddDeadlineCommand(String[] stringSplit) {
-        this.stringSplit = stringSplit;
+    /**
+     * Constructor to initialise the new AddDeadlineCommand object with a description and deadline
+     *
+     * @param description
+     * @param deadline
+     */
+    public AddDeadlineCommand(String description, String deadline) {
+        this.description = description;
+        this.deadline = deadline;
     }
 
+    /**
+     * Executes the Deadline Command by creating a new Deadline Task and saving it to TaskList and Database
+     *
+     * @param tasks    the TaskList the new deadline task is being added to
+     * @param database the Storage the new deadline task is being saved to
+     */
     @Override
     public void execute(TaskList tasks, Storage database) {
-        Task currTask = new Deadline(stringSplit[0], stringSplit[1]);
+        Task currTask = new Deadline(description, deadline);
         addTaskToTaskList(tasks, currTask);
         addTaskToDatabase(currTask, database);
     }
