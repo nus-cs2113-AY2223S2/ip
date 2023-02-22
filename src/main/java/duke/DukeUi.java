5da442f82d8d2f4f5e395787cc61b237455c6e7b
tyuyang <tayyuyang@gmail.com>
2023-02-22 15:10:41 +0800

    Added relevant commands to DukeUi

--- src/main/java/duke/DukeUi.java
+++ src/main/java/duke/DukeUi.java
@@ -50,6 +50,35 @@ public class DukeUi {
         System.out.println("delete <task number>: Deletes the task from the list.");
         System.out.println("find <keyword>: Finds and lists the tasks that contains the keyword " + 
                 "in the name of the task.");
+        System.out.println("save: Saves all the tasks to the savefile");
+
+        // Planned feature
+        // System.out.println("autosave: Enables file to be saved after every command. " + 
+        //         "May slow down the program.");
         System.out.println("bye: Exits the program.");
+    }
+
+    public void printDirCreated() {
+        System.out.println("data directory successfully created.");
+    }
+
+    public void printDirNotFound() {
+        System.out.println("data directory not found, creating directory.");
+    }
+
+    public void printSavefileNotFound() {
+        System.out.println("savefile not found, creating new savefile.");
+    }
+
+    public void printSavefileCreated() {
+        System.out.println("savefile successfully created.");
+    }
+
+    public void printSavefileFound() {
+        System.out.println("savefile found. Parsing savefile.");
+    }
+
+    public void printSavingToFile() {
+        System.out.println("Saving tasks to savefile, please do not close the application.");
     }
 }
