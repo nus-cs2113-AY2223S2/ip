4f71cee38da21d67867f7934cde3eee9af9595a0
tyuyang <tayyuyang@gmail.com>
2023-02-23 09:10:20 +0800

    Modified duke so that static errors do not occur

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -6,12 +6,19 @@ import parser.DukeParser;
 import tasklist.TaskList;
 
 public class Duke {
-    public boolean hasEnteredBye = false;
+    public boolean hasEnteredBye;
     private final boolean WITH_SCANNER = true;
-    public DukeUi ui = new DukeUi(WITH_SCANNER);
+    public DukeUi ui;
     public TaskList taskList;
-    public SavefileManager savefileManager = new SavefileManager();
+    public SavefileManager savefileManager;
     public DukeParser parser;
+
+    public Duke() {
+        hasEnteredBye = false;
+        ui = new DukeUi(WITH_SCANNER);
+        savefileManager = new SavefileManager();
+        parser = new DukeParser(this);
+    }
 
     public void exit() {
         ui.printLine();
@@ -19,71 +26,14 @@ public class Duke {
         ui.closeScanner();
         System.out.println("Bye. Hope to see you again soon!");
     }
-    
-    // public void parseUserInput(String input) {
-    //     String[] split = input.trim().split("\\s+", 2);
-    //     String command = split[0];
-    //     String args = split.length == 2 ? split[1] : "";
-    //     switch (command) {
-    //     case "bye":
-    //         hasEnteredBye = true;
-    //         exit();
-    //         break;
-    //     case "list":
-    //         taskList.listTasks();
-    //         break;
-    //     case "help":
-    //         ui.printHelp();
-    //         break;
-    //     case "mark":
-    //         try {
-    //             taskList.markDone(Integer.parseInt(args));
-    //         } catch (Exception e) {
-    //             System.out.println("Please input an integer after mark.");
-    //         }
-    //         break;
-    //     case "unmark":
-    //         try {
-    //             taskList.unmarkDone(Integer.parseInt(args));
-    //         } catch (Exception e) {
-    //             System.out.println("Please input an integer after unmark.");
-    //         }
-    //         break;
-    //     case "todo":
-    //         taskList.addToDo(args);
-    //         break;
-    //     case "deadline":
-    //         taskList.addDeadline(args);
-    //         break;
-    //     case "event":
-    //         taskList.addEvent(args);
-    //         break;
-    //     case "delete":
-    //         try {
-    //             taskList.deleteTask(Integer.parseInt(args));
-    //         } catch (Exception e) {
-    //             System.out.println("Please input an integer after delete.");
-    //         }
-    //         break;
-    //     case "find":
-    //         taskList.findTask(args);
-    //         break;
-    //     case "save":
-    //         savefileManager.save(taskList);
-    //         break;
-    //     default:
-    //         System.out.println("Invalid command entered, please enter 'help' to see " +
-    //         "the list of commands.");
-    //     }
-    // }
-    
     public static void main(String[] args) {
-        ui.greetUser();
+        Duke duke = new Duke();
+        duke.ui.greetUser();
         try {
-            savefileManager.checkSaveDir();
-            savefileManager.checkSavefile();
-            taskList = savefileManager.parseSavefile();
-            ui.printLine();
+            duke.savefileManager.checkSaveDir();
+            duke.savefileManager.checkSavefile();
+            duke.taskList = duke.savefileManager.parseSavefile();
+            duke.ui.printLine();
         } catch (IOException e) {
             System.out.println("Savefile cannot be found, please delete the savefile yourself");
             System.out.println("cos I am too lazy to delete it for you for now.");
@@ -92,11 +42,11 @@ public class Duke {
             return;
         }
         
-        while (!hasEnteredBye) {
-            String line = ui.getNextLine();
-            parser.parseUserInput(line);
-            ui.printLine();
+        while (!duke.hasEnteredBye) {
+            String line = duke.ui.getNextLine();
+            duke.parser.parseUserInput(line);
+            duke.ui.printLine();
         }
-        ui.closeScanner();
+        duke.ui.closeScanner();
     }
 }
