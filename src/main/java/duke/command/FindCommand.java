6cf373b2706a93946ec60b6924e8aa987bb7f02c
Hong Lin Shang <e0774890@u.nus.edu>
2023-02-23 15:32:01 +0800

    Create class for Find command

---
+++ src/main/java/duke/command/FindCommand.java
@@ -0,0 +1,18 @@
+package duke.command;
+
+import duke.Storage;
+import duke.Ui;
+import duke.task.TaskList;
+
+public class FindCommand extends Command {
+    protected String keyword;
+
+    public FindCommand(String keyword) {
+        this.keyword = keyword;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) {
+        ui.printList(tasks.getAllTasks());
+    }
+}
