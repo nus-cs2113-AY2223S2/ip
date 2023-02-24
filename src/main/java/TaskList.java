540458f3aeafc9be7d95612127a90461363ff716
cleoncheng2000 <e0725796@u.nus.edu>
2023-02-24 22:34:42 +0800

    Add taskList class

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,46 @@
+import duke.task.Task;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
+public class TaskList {
+    private ArrayList<Task> taskList;
+
+    public TaskList() {
+        this.taskList = new ArrayList<>();
+    }
+
+    public int sizeOfTaskList() {
+        return taskList.size();
+    }
+
+    public void addTask(Task task) {
+        taskList.add(task);
+    }
+
+    public void deleteTask(int index) {
+        try {
+            taskList.remove(index);
+        } catch (IndexOutOfBoundsException exception) {
+            Messages.invalidTaskMessage();
+        }
+    }
+
+    public void markAsDone(int index) {
+        try {
+            Task curTask = taskList.get(index);
+            curTask.setAsDone();
+        } catch (IndexOutOfBoundsException exception) {
+            Messages.invalidTaskMessage();
+        }
+    }
+
+    public void markAsUnDone(int index) {
+        try {
+            Task curTask = taskList.get(index);
+            curTask.setAsUndone();
+        } catch (IndexOutOfBoundsException exception) {
+            Messages.invalidTaskMessage();
+        }
+    }
+}
