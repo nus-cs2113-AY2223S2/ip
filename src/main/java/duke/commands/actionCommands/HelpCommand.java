7e22e65ffa2a1dbd6d74f27f858062ec1dffc500
irving11119 <irving.deboer@gmail.com>
2023-02-24 10:55:19 +0800

    Update help command and message

--- src/main/java/duke/commands/actionCommands/HelpCommand.java
+++ src/main/java/duke/commands/actionCommands/HelpCommand.java
@@ -5,21 +5,9 @@ import duke.exceptions.InvalidArgsException;
 import duke.save.Storage;
 import duke.tasks.TaskList;
 
-import static duke.constants.Constants.LINEBREAK;
+import static duke.constants.Constants.MESSAGE_HELP;
 
 public class HelpCommand extends Command {
-
-    public static final String MESSAGE_HELP = "Here are the commands you can use:\n"
-            + "1. todo <task name> - Adds a todo task to the list.\n"
-            + "2. deadline <task name> /by <YYYY-MM-DD> - Adds a deadline task to the list.\n"
-            + "3. event <task name> /from <YYYY-MM-DD HHmm> /to <YYYY-MM-DD HHmm> - Adds an event task to the list.\n"
-            + "4. list - Lists all tasks in the list.\n"
-            + "5. mark <task number> - Marks a task as done.\n"
-            + "6. unmark <task number> - Marks a task as not done.\n"
-            + "7. help - Shows program usage instructions.\n"
-            + "8. find <keyword> - Finds tasks with the keyword.\n"
-            + "9. bye - Exits the program.\n"
-            + LINEBREAK;
 
     @Override
     public void handleCommand(String line, TaskList tasks, Storage storage) {
--- src/main/java/duke/constants/Constants.java
+++ src/main/java/duke/constants/Constants.java
@@ -1,5 +1,19 @@
 package duke.constants;
 
 public class Constants {
-    public static String LINEBREAK = "====================================================";
+
+    public static final String LINEBREAK = "====================================================";
+
+    public static final String MESSAGE_HELP = "Here are the commands you can use:\n"
+            + "1. todo <task name> - Adds a todo task to the list.\n"
+            + "2. deadline <task name> /by <YYYY-MM-DD> - Adds a deadline task to the list.\n"
+            + "3. event <task name> /from <YYYY-MM-DD HHmm> /to <YYYY-MM-DD HHmm> - Adds an event task to the list.\n"
+            + "4. list - Lists all tasks in the list.\n"
+            + "5. mark <task number> - Marks a task as done.\n"
+            + "6. unmark <task number> - Marks a task as not done.\n"
+            + "7. find <keyword> - Finds tasks with the keyword.\n"
+            + "8. delete <task number> - Deletes a task from the list.\n"
+            + "9. help - Shows program usage instructions.\n"
+            + "10. bye - Exits the program.\n"
+            + LINEBREAK;
 }
