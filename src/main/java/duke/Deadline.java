b694e374177787e9b2880ef363a31de52be674cf
nicholas132000 <beefnlemon@gmail.com>
2023-02-24 16:52:54 +0800

    Add javadoc comments for Todo, Deadline and Event

--- src/main/java/duke/Deadline.java
+++ src/main/java/duke/Deadline.java
@@ -1,18 +1,34 @@
 package duke;
-
+/**
+ * This object represents a task object of Deadline type.
+ * Deadline type contains all the attributes and methods from template Task and also contains when this task should be completed by
+ */
 public class Deadline extends Task{
     protected String by;
 
+    /**
+     * Initialises a Deadline object that contains the description and when the task should be completed by
+     * @param description The description of the task
+     * @param by The date/time that this task should be completed by
+     */
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
     }
 
+    /**
+     * Overrides the template Task toString with an additional [D] string which symbolizes that it is a Deadline task
+     * @return Returns the symbol [D], the status icon and the description and when to complete by
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + by + ")";
     }
 
+    /**
+     * Overrides the template Task getSymbol with "[D]" string which symbolizes that it is a Deadline task
+     * @return Returns the character "D" symbolizes that it is a Deadline task
+     */
     @Override
     public String getSymbol() {
         return "D";
--- src/main/java/duke/Event.java
+++ src/main/java/duke/Event.java
@@ -1,19 +1,38 @@
 package duke;
 
+/**
+ * This object represents a task object of Event type.
+ * Deadline type contains all the attributes and methods from template Task and also contains when this task starts and ends
+ */
 public class Event extends Task{
     protected String start;
     protected String end;
+
+    /**
+     * Initialises a Event object that contains the description and start and time of the task
+     * @param description The description of the task
+     * @param start The date/time that this task should start
+     * @param end The date/time that this task should end
+     */
     public Event(String description, String start, String end) {
         super(description);
         this.start = start;
         this.end = end;
     }
 
+    /**
+     * Overrides the template Task toString with an additional [E] string which symbolizes that it is an Event task
+     * @return Returns the symbol [E], the status icon and the description and the start and end time of the task
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() + " (from: " + start + " to: " + end + ")";
     }
 
+    /**
+     * Overrides the template Task getSymbol with "[E]" string which symbolizes that it is an Event task
+     * @return Returns the character "E" symbolizes that it is an Event task
+     */
     @Override
     public String getSymbol() {
         return "E";
