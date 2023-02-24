7c445025506a366de2ae59ae15cd5f1851e42f57
cleoncheng2000 <e0725796@u.nus.edu>
2023-02-24 22:34:23 +0800

    Add Messages class

---
+++ src/main/java/Messages.java
@@ -0,0 +1,10 @@
+public class Messages {
+    public static final String MESSAGE_DOTTED_LINE = "____________________________________________________________";
+    public static final String MESSAGE_INVALID_TASK = "The task index provided is invalid";
+    public static void invalidTaskMessage() {
+        System.out.println(MESSAGE_DOTTED_LINE);
+        System.out.println(MESSAGE_INVALID_TASK);
+        System.out.println(MESSAGE_DOTTED_LINE);
+    }
+
+}
