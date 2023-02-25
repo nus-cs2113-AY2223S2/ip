45b888e774b04d86b0a998b0278384b1485ac065
cleoncheng2000 <e0725796@u.nus.edu>
2023-02-26 06:54:04 +0800

    Add javadoc for task, todo, deadline, event class

--- src/main/java/duke/task/ToDo.java
+++ src/main/java/duke/task/ToDo.java
@@ -1,11 +1,24 @@
 package duke.task;
 
+/**
+ * Represents todo object that it a type of task
+ */
 public class ToDo extends Task {
+    /**
+     * Constructor for todo task object, with specified type "T"
+     *
+     * @param description the description of the current todo
+     */
     public ToDo(String description) {
         super(description);
         this.type = "[T]";
     }
 
+    /**
+     * Prints the todo object elements
+     *
+     * @return the type, status and description of current todo object
+     */
     @Override
     public String toString() {
         return super.toString() + getType() + getStatusIcon() + getDescription();
