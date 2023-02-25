7b76aabaa4b88a10bc974b601651a3755063ba29
cleoncheng2000 <e0725796@u.nus.edu>
2023-02-26 06:54:50 +0800

    Add javadoc for duke main class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,22 +7,38 @@ import java.util.ArrayList;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * Main class to manage all operations
+ */
 public class Duke {
     public static boolean isEnd = false;
     private Storage storage;
     private TaskList tasks;
     private UI ui;
 
+    /**
+     * Start the duke programme
+     * @param args
+     * @throws IOException if an error occurred while loading save file
+     */
     public static void main(String[] args) throws IOException {
         new Duke("./taskSave.txt").run();
     }
 
+    /**
+     * Constructor to create new UI, storage and task list objects
+     * @param filepath path of the previous save file
+     */
     public Duke(String filepath) {
         ui = new UI();
         storage = new Storage(filepath);
         tasks = new TaskList();
     }
 
+    /**
+     * Loads the save file and executes duke programme
+     * @throws IOException if an error occurred while loading the save file
+     */
     public void run() throws IOException {
         try {
             storage.createSaveFile();
