da151307c703ae78aaf5e5f6734d5c81b67d57fb
cleoncheng2000 <e0725796@u.nus.edu>
2023-02-26 06:53:11 +0800

    Add javadoc for todo, deadline and event command

--- src/main/java/EventCommand.java
+++ src/main/java/EventCommand.java
@@ -1,8 +1,16 @@
 import duke.task.Event;
 
+/**
+ * Command to execute when adding new event object to task list
+ */
 public class EventCommand extends Command {
     private Event event;
 
+    /**
+     * creates a new event object
+     *
+     * @param input data of given command and description
+     */
     public void setEvent(String input) {
         String[] temp = input.split(("event | /from | /to ")); //separates event description and times
         String description = temp[1];
@@ -11,6 +19,13 @@ public class EventCommand extends Command {
         this.event = new Event(description, from, to);
     }
 
+    /**
+     * Adds a new event object into the task list
+     *
+     * @param input data of given command and description
+     * @param tasks task list containing all current tasks
+     * @param ui    UI object to print task successfully added statement
+     */
     @Override
     public void runCommand(String input, TaskList tasks, UI ui) {
         try {
