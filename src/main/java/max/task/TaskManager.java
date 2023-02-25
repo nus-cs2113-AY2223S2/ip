18f17783c5ce79a3b64e90b2468314f00a31fcec
nichyjt <nicholas.yek@gmail.com>
2023-02-25 22:46:17 +0800

    Add find function and reduce magic literals
    Signed-off-by: nichyjt <nicholas.yek@gmail.com>

--- src/main/java/max/task/TaskManager.java
+++ src/main/java/max/task/TaskManager.java
@@ -11,6 +11,19 @@ public class TaskManager {
     private ArrayList<Task> tasks;
     private Ui ui;
 
+    private static String MESSAGE_MAX_CANNOT_FIND = "Max dug around everywhere but can't find anything!";
+    private static String MESSAGE_MAX_HAS_FOUND = "Max sniffed out these matching tasks:";
+    private static String MESSAGE_EMPTY_QUERY = "I'm gonna bite you. Your query is empty!";
+    private static String MESSAGE_REMOVE_TASK = "Woof woof this task will be rem-woofed:";
+    private static String MESSAGE_MARK_DONE = "Okay, marking this task as done: ";
+    private static String MESSAGE_UNMARK_DONE = "Okay, setting this task as undone: ";
+    private static String MESSAGE_SHOW_LIST = "Here's what's in your list:";
+    private static String MESSAGE_LIST_EMPTY = "There's nothing in your list. I'm gonna bite you.";
+    private static String MESSAGE_ADD_TASK = "Got it. Task added:";
+    private static String EXCEPTION_INVALID_NUMBER = "Invalid task number!";
+    private static String EXCEPTION_NOT_A_NUMBER = "I'm a dog, but even I know that you didn't enter a number.";
+    private static String EXCEPTION_ADD_TASK_FAILED = "Throw me a bone here, I couldn't create a task!";
+
     public void createTask(HashMap<String, String> commandMap, Command command) throws TaskException {
         // Assertion: commandMap has the correct subcommands & length
         Task newTask = null;
@@ -32,20 +45,20 @@ public class TaskManager {
         }
         if (newTask == null) {
             // Safety check in case the assertion fails
-            throw new TaskException("Throw me a bone here, I couldn't create a task!");
+            throw new TaskException(EXCEPTION_ADD_TASK_FAILED);
         }
         tasks.add(newTask);
-        ui.printMessage("Got it. Task added:");
+        ui.printMessage(MESSAGE_ADD_TASK);
         ui.printMessage(newTask.getDescription());
         ui.printMessage("You now have " + tasks.size() + " tasks in your list.");
     }
 
     public void printTasklist() {
         if (tasks.size() == 0) {
-            ui.printMessage("There's nothing in your list. I'm gonna bite you.");
+            ui.printMessage(MESSAGE_LIST_EMPTY);
             return;
         }
-        ui.printMessage("Here's what's in your list:");
+        ui.printMessage(MESSAGE_SHOW_LIST);
         for (int i = 0; i < tasks.size(); ++i) {
             // Print number, box, description in that order
             Task curr = tasks.get(i);
@@ -61,10 +74,10 @@ public class TaskManager {
         try {
             taskNum = Integer.parseInt(taskNumString) - 1; // Convert to 0-idx
         } catch (NumberFormatException exception) {
-            throw new TaskException("I'm a dog, but even I know that you didn't enter a number.");
+            throw new TaskException(EXCEPTION_NOT_A_NUMBER);
         }
         if (taskNum < 0 || taskNum >= tasks.size()) {
-            throw new TaskException("Invalid task number!");
+            throw new TaskException(EXCEPTION_INVALID_NUMBER);
         }
         return taskNum;
     }
@@ -81,10 +94,10 @@ public class TaskManager {
         // Update the task's done status
         if (isDone) {
             tasks.get(taskNum).markAsDone();
-            ui.printMessage("Okay, marking this task as done: ");
+            ui.printMessage(MESSAGE_MARK_DONE);
         } else {
             tasks.get(taskNum).markAsUndone();
-            ui.printMessage("Okay, setting this task as undone: ");
+            ui.printMessage(MESSAGE_UNMARK_DONE);
         }
         ui.printMessage(tasks.get(taskNum).getDescription());
     }
@@ -98,7 +111,7 @@ public class TaskManager {
             return;
         }
         // Assertion: taskNum is in tasklist range, guaranteed by convertTaskNumStr()
-        ui.printMessage("Woof woof this task will be rem-woofed:");
+        ui.printMessage(MESSAGE_REMOVE_TASK);
         ui.printMessage(tasks.get(taskNum).getDescription());
         tasks.remove(taskNum);
         ui.printMessage("You have have " + tasks.size() + " tasks left.");
@@ -116,6 +129,39 @@ public class TaskManager {
 
     public void resetTaskList() {
         tasks.clear();
+    }
+
+    public void findTasks(HashMap<String, String> commandPayload) {
+        // Naiively search for tasks that have exact matches with the query string
+        String query;
+        if (commandPayload.containsKey("find")) {
+            query = commandPayload.get("find");
+        } else {
+            query = commandPayload.get("fetch");
+        }
+        if (query.trim().length() == 0) {
+            ui.printMessage(MESSAGE_EMPTY_QUERY);
+            return;
+        }
+
+        ArrayList<Task> matchedTasks = new ArrayList<>();
+        for (Task task : tasks) {
+            String taskDescription = task.getDescription();
+            if (taskDescription.contains(query)) {
+                matchedTasks.add(task);
+            }
+        }
+        // Print all related items
+        if (matchedTasks.size() == 0) {
+            ui.printMessage(MESSAGE_MAX_CANNOT_FIND);
+        } else {
+            ui.printMessage(MESSAGE_MAX_HAS_FOUND);
+            for (int i = 0; i < matchedTasks.size(); ++i) {
+                Task curr = matchedTasks.get(i);
+                ui.printMessage(i + 1 + ". " + curr.getDescription());
+            }
+        }
+
     }
 
     public TaskManager() {
