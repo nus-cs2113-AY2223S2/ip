173a793e07d67d08e2e0f442e52b66729ddfb2e7
dxZhang <dxzhang49@gmail.com>
2023-02-25 23:46:43 +0800

    Add common messages

---
+++ src/main/java/duke/common/Messages.java
@@ -0,0 +1,16 @@
+package duke.common;
+
+public class Messages {
+    private static final String LOGO = """
+             ____        _       \s
+            |  _ \\ _   _| | _____\s
+            | | | | | | | |/ / _ \\
+            | |_| | |_| |   <  __/
+            |____/ \\__,_|_|\\_\\___|
+            """;
+    public static final String SPACER = "\t ";
+    public static final String DIVIDER = "____________________________________________________________";
+    public static final String MSG_GREETINGS = "Hello from\n" + LOGO;
+    public static final String MSG_INTRODUCTION = "Hello! I'm Tohru.\nWhat can I do for you?";
+    public static final String MSG_GOODBYE = "Bye. Hope to see you again soon!";
+}
