4e4dadfd47376cb989948335935976a31af11b8b
nichyjt <nicholas.yek@gmail.com>
2023-02-26 12:20:11 +0800

    Add JavaDoc
    Explain clearly how the user input format works
    
    Signed-off-by: nichyjt <nicholas.yek@gmail.com>

--- src/main/java/max/command/CommandParser.java
+++ src/main/java/max/command/CommandParser.java
@@ -4,48 +4,91 @@ import java.util.HashMap;
 
 public class CommandParser {
 
+    /**
+     * Constructs a CommandParser.
+     * <br>
+     * A CommandParser is used to process user input from a defined format <p>
+     * We define the user input format via console to consist of: <br>
+     * 1. COMMANDS - user input substring consisting of an argument and payload <br>
+     * 2. ARGUMENTS - command substring representing the action/parameters of the command <br>
+     * 3. PAYLOADS - value of the action/parameters <br>
+     * <br>
+     * For example, a given user input: <i>"deadline work on CS2113 --by Sunday"</i>
+     * <li>Has commands ["deadline work on CS2113", "by Sunday"]</li>
+     * <li>Has arguments ["deadline", "by"]</li>
+     * <li>Has payloads ["work on CS2113", ["Sunday"]</li>
+     */
     public CommandParser() {
 
     }
 
+    /**
+     * Takes in a user input (string) in its entirety and partitions it into its commands in an array. <br>
+     * A command is defined as either: <br>
+     * 1. The first word in the user input and its payload <br>
+     * 2. OR the word immediately following " --" and its payload <br>
+     * For example: <i>deadline my_task --by -xyz sunday-monday</i>
+     * Will render as: ["deadline my_task", "by -xyz sunday-monday"]
+     * <p>
+     *
+     * @param rawCommand user console input in its entirety
+     * @return String array of commands
+     */
     public String[] splitIntoCommands(String rawCommand) {
-        // " -" splits the string into substrings
-        // if the substring starts with two dashes '--' and is preceded by whitespace
-        // For example: deadline my_task --by -xyz sunday-monday
-        // Will output: ["deadline my_task", "by -xyz sunday-monday"]
-        // For now, this does not account for accidental or adversarial '--' input at the start of words
-        // but this is an unlikely edge case that can be dealt with later
         return rawCommand.split(" --");
     }
 
+    /**
+     * Processes a list of commands and maps each command's argument to its payload <br>
+     * <p>
+     * Each command consists of an argument and payload <br>
+     * <p>
+     * For example, for user input: "deadline work on CS2113 --by Sunday"
+     * <li>    Has commands ["deadline work on CS2113", "by Sunday"]    </li>
+     * <li>    Has arguments ["deadline", "by"]                         </li>
+     * <li>    Has payloads ["work on CS2113", ["Sunday"]               </li>
+     * Thus,  as: <"deadline", "work on CS2113">, <by, Sunday>
+     *
+     * @param commandList list of commands
+     * @return HashMap
+     */
     public HashMap<String, String> getCommandPayloadMap(String[] commandList) {
-        // Each command can be processed into a subcommand and its corresponding text value
-        // For example: "deadline work on CS2113 --by Sunday"
-        // Should map to: <"deadline", "work on CS2113">, <by, Sunday>
         HashMap<String, String> commandMap = new HashMap<>();
         for (String cmdStr : commandList) {
-            String cmd = extractCommandFromSubcommand(cmdStr);
-            String text = extractTextFromSubcommand(cmdStr);
+            String cmd = extractArgumentFromCommand(cmdStr);
+            String text = extractPayloadFromCommand(cmdStr);
             commandMap.put(cmd, text);
         }
         return commandMap;
     }
 
-    public static String extractCommandFromSubcommand(String subcommand) {
-        // Subcommands have format <subcommand> <text_to_extract>
-        // Return the <subcommand>
-        // For example, the deadline subcommand: "by Friday, 13th"
-        // Should return "by"
-        String[] words = subcommand.split(" ");
+
+    /**
+     * Return the argument from a command <br>
+     * <p>
+     * For example, the command: "by Friday, 13th" <br>
+     * Should return ["by"]
+     *
+     * @param command the command to be processed
+     * @return command's payload rendered as a string
+     */
+    private static String extractArgumentFromCommand(String command) {
+        String[] words = command.split(" ");
         return words[0];
     }
 
-    public static String extractTextFromSubcommand(String subcommand) {
-        // Subcommands have format <subcommand> <text_to_extract>
-        // Return the <subcommand> and <text_to_extract> in a string array
-        // For example, the deadline subcommand: "by Friday, 13th"
-        // Should return ["by" , "Friday, 13th"]
-        String[] words = subcommand.split(" ");
+    /**
+     * Return the payload from a command text <br>
+     * <p>
+     * For example, the command: "by Friday, 13th" <br>
+     * Should return ["by" , "Friday, 13th"]
+     *
+     * @param command the command to be processed
+     * @return command's payload rendered as a string
+     */
+    private static String extractPayloadFromCommand(String command) {
+
+        String[] words = command.split(" ");
         String subcommandText = "";
         for (int i = 1; i < words.length; ++i) {
             subcommandText = subcommandText.concat(words[i]);
@@ -56,14 +99,19 @@ public class CommandParser {
         return subcommandText;
     }
 
-    // Takes in the Main command and its payload, if any
+
+    /**
+     * Returns what valid main command the input string represents.
+     * A main command is a valid command defined under the Command enum
+     * The input string should be the FIRST argument input of any given command.
+     * For example, the input <i>"mark 10"</i> has first argument <i>`mark`</i>
+     *
+     * @param mainCommand the first argument input, i.e. the command that the user wants to execute (e.g. list)
+     * @return Command type of the detected command. Returns UNKNOWN_COMMAND for unmatchable inputs.
+     */
     public Command getCommandType(String mainCommand) {
-        /* TODO:
-         *  Consider possible optimisation by hashing the commands
-         *   This may be premature optimisation, might not be necessary
-         */
-        // Extract the real command from its payload
-        String commandName = extractCommandFromSubcommand(mainCommand);
+        // Extract the main command from its payload
+        String commandName = extractArgumentFromCommand(mainCommand);
 
         // Find out which Command commandName is
         Command[] commands = Command.values();
