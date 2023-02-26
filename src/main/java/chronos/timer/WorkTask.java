d9499ba9fad491bcba5f7a8426782b54ee5ca285
T-Wan-Lin <tanwanlin.c2021>
2023-02-26 22:34:02 +0800

    Documentation of Timer Packages

--- src/main/java/chronos/timer/WorkTask.java
+++ src/main/java/chronos/timer/WorkTask.java
@@ -4,11 +4,11 @@ import java.util.TimerTask;
 
 
 /**
- * 
+ * Represents a WorkTask type of clock, subclass of TimerTask
  */
 public class WorkTask extends TimerTask{
     /**
-     *
+     * Prints out a string to inform users that the Work timer is running
      */
     @Override
     public void run() {
