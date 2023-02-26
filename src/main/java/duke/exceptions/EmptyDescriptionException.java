d919bc17d70dd70d608b09ba003ead967d47a24f
javienneyeo <javienneyeo@gmail.com>
2023-02-26 11:23:11 +0800

    added delete task functionality

--- src/main/java/duke/exceptions/EmptyDescriptionException.java
+++ src/main/java/duke/exceptions/EmptyDescriptionException.java
@@ -7,7 +7,7 @@ public class EmptyDescriptionException extends Exception {
         this.typeOfTask = typeOfTask;
     }
     public void printErrorMessage() {
-        if (typeOfTask.equals("marked") || typeOfTask.equals("unmarked")) {
+        if (typeOfTask.equals("marked") || typeOfTask.equals("unmarked") || typeOfTask.equals("deleted")) {
             System.out.println("OOPS!! Task to be " + typeOfTask + " was not specified!");
         } else {
             System.out.println("OOPS!! The description of " + typeOfTask + " cannot be empty!");
