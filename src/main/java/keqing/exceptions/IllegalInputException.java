57542279dda3ef69e1662072156d9a714198c537
alexgoexercise <alexzhaolixiuqi@gmail.com>
2023-02-26 16:35:50 +0800

    Add some comments to the code

--- src/main/java/keqing/exceptions/IllegalInputException.java
+++ src/main/java/keqing/exceptions/IllegalInputException.java
@@ -1,6 +1,10 @@
 package keqing.exceptions;
 
 public class IllegalInputException extends Exception {
+    /**
+     * Constructor for IllegalInputException
+     * @param message the message to be printed
+     */
     public IllegalInputException(String message){
         super(message);
     }
