91f4ae41d1296bb170ad72f8f7893c9bd1c9d761
Benjamin Toh <toh.benjamin123@gmail.com>
2023-02-26 15:58:28 +0800

    Fix exception error formatting bug

--- src/main/java/duke/exceptions/InvalidFormatException.java
+++ src/main/java/duke/exceptions/InvalidFormatException.java
@@ -3,7 +3,7 @@ public class InvalidFormatException extends DukeException {
     private static final String exceptionMessage = "Wrong format! Please try again.";
 
     public InvalidFormatException(String... missing) {
-        super("Wrong format! Please include " + missing);
+        super("Wrong format! Please include " + String.join(" and ", missing));
     }
 
     public InvalidFormatException() {
