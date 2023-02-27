77a84442f79a3eac9e9c089bb7a2433520f2b4ae
ong-ck <e0774208@u.nus.edu>
2023-02-27 15:40:13 +0800

    Add: Level-8 (Dates and Times)

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -1,19 +1,37 @@
 package duke.task;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
+
 public class Deadline extends Task {
+
+    protected LocalDate date;
+
     public Deadline(String description) {
         super(description);
         this.taskType = TaskType.DEADLINE;
+        this.date = null;
     }
 
     @Override
     public String toString() {
         String[] descriptionParts = this.description.split("/by");
         String descriptionDetails = descriptionParts[0];
-        String descriptionDeadline = descriptionParts[1];
+        String descriptionDeadline = descriptionParts[1].trim();
+        String descriptionOutput;
+
+        try {
+            this.date = LocalDate.parse(descriptionDeadline);
+            descriptionOutput = date.format(DateTimeFormatter.ofPattern("MMM dd yyyy"));
+        } catch (DateTimeParseException e) {
+            descriptionOutput = descriptionDeadline;
+        }
 
         return "[" + this.getType() + "][" + this.getStatusIcon() + "] " +
-                descriptionDetails + "(by:" + descriptionDeadline + ")";
+                descriptionDetails + "(by: " + descriptionOutput + " )";
     }
 
 }
