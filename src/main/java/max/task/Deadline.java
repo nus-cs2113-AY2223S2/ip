7251368566948e298e1274babad50535a2fb8d69
nichyjt <nicholas.yek@gmail.com>
2023-02-27 17:12:39 +0800

    Add JavaDoc and refactor magic literals
    Signed-off-by: nichyjt <nicholas.yek@gmail.com>

--- src/main/java/max/task/Deadline.java
+++ src/main/java/max/task/Deadline.java
@@ -1,25 +1,49 @@
 package max.task;
 
+import max.Ui.Ui;
+
+/**
+ * Deadline is a subclass of Task
+ * <p>
+ * It additionally stores the due date of the Task.
+ */
 public class Deadline extends Task {
+    private static final String TASK_LABEL = "D";
+    private static final String WRAPPER_BY = " (by: ";
+    private static final String WRAPPER_END = ")";
+    private Ui ui;
     private String dueDate;
 
+
+    /**
+     * Create a Deadline object, a subclass of Task.
+     * Used to keep track of a Task's due date.
+     * @param description String describing the Deadline
+     * @param dueDate Date that the deadline is due
+     */
     public Deadline(String description, String dueDate) {
         super(description);
         this.dueDate = dueDate;
+        ui = new Ui();
     }
 
     @Override
     public String getDescription() {
-        String description = "[" + getTaskLabel() + "]" + super.getDescription();
-        description = description.concat(" (by: " + this.dueDate + ")");
+        String description = ui.wrapMessage(getTaskLabel()) + super.getDescription();
+        description = description.concat( WRAPPER_BY+ this.dueDate + WRAPPER_END);
         return description;
     }
 
     @Override
     public String getTaskLabel() {
-        return "D";
+        return TASK_LABEL;
     }
 
+    /**
+     * Helper method to retrieve the due date of a Deadline
+     *
+     * @return Deadline's due date
+     */
     public String getDueDate() {
         return dueDate;
     }
--- src/main/java/max/task/Event.java
+++ src/main/java/max/task/Event.java
@@ -1,33 +1,64 @@
 package max.task;
 
+import max.Ui.Ui;
+
+/**
+ * Event is a subclass of Task
+ * <p>
+ * It additionally stores the from and to dates of the event.
+ */
 public class Event extends Task {
+    private static final String TASK_LABEL = "E";
+    private static final String WRAPPER_FROM = " (FROM: ";
+    private static final String WRAPPER_COMMA = ", ";
+    private static final String WRAPPER_TO = "TO: ";
+    private static final String WRAPPER_END = ")";
+    private Ui ui;
     private String eventFrom;
     private String eventTo;
 
+    /**
+     * Create an Event object, a subclass of Task.
+     * Used to keep track of a Task's start and end times.
+     * @param description String describing the Event
+     * @param from Datetime of the event's start
+     * @param to Datetime of the event's end
+     */
     public Event(String description, String from, String to) {
         super(description);
         this.eventFrom = from;
         this.eventTo = to;
+        ui = new Ui();
     }
 
     @Override
     public String getDescription() {
-        String desc = "[" + getTaskLabel() + "]" + super.getDescription();
-        String fromStr = " (FROM: " + eventFrom + ", ";
-        String toStr = "TO: " + eventTo + ")";
+        String desc = ui.wrapMessage(getTaskLabel()) + super.getDescription();
+        String fromStr = WRAPPER_FROM + eventFrom + WRAPPER_COMMA;
+        String toStr = WRAPPER_TO + eventTo + WRAPPER_END;
         desc = desc.concat(fromStr).concat(toStr);
         return desc;
     }
 
     @Override
     public String getTaskLabel() {
-        return "E";
+        return TASK_LABEL;
     }
 
+    /**
+     * Helper method to retrieve the 'from' date of an Event
+     *
+     * @return Event 'from' date
+     */
     public String getEventFrom() {
         return eventFrom;
     }
 
+    /**
+     * Helper method to retrieve the 'to' date of an Event
+     *
+     * @return Event 'to' date
+     */
     public String getEventTo() {
         return eventTo;
     }
