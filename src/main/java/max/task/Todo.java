3158a5a404493c158638e5b5ccaf0b524202e805
nichyjt <nicholas.yek@gmail.com>
2023-02-27 17:11:52 +0800

    Add JavaDoc
    Signed-off-by: nichyjt <nicholas.yek@gmail.com>

--- src/main/java/max/task/Todo.java
+++ src/main/java/max/task/Todo.java
@@ -1,17 +1,34 @@
 package max.task;
 
+import max.Ui.Ui;
+
+/**
+ * Todo is a subclass of Task
+ * <p>
+ * It stores the description of the task.
+ */
 public class Todo extends Task {
+    private static final String TASK_LABEL = "T";
+    private Ui ui;
+    /**
+     * Create an Todo object, a subclass of Task.
+     *
+     * @param description String describing the Task
+     */
     public Todo(String description) {
         super(description);
+        ui = new Ui();
     }
 
     @Override
     public String getDescription() {
-        return "[" + getTaskLabel() + "]" + super.getDescription();
+        String wrappedTask = ui.wrapMessage(getTaskLabel());
+        wrappedTask += ui.getSingleSpace();
+        return wrappedTask + super.getDescription();
     }
 
     @Override
     public String getTaskLabel() {
-        return "T";
+        return TASK_LABEL;
     }
 }
