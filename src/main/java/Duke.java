4f68deba541a05c9e4761d82476e6867cb5992b1
Wilson Lee Jun Wei <leejunwei2000@gmail.com>
2023-02-27 12:47:25 +0800

    Add Level-5: Handle Errors (Exceptions)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class Duke {
@@ -14,19 +15,33 @@ public class Duke {
         while (isRunning) {
             Scanner command = new Scanner(System.in);
             String input_Command = command.nextLine();
+            // e.g. "todo borrow book"
+
             String[] first_Word_Array;
             first_Word_Array = input_Command.split(" ", 2);
+            // first_Word_Array[0,1] is now ["todo", "borrow book"]
             String first_Word = first_Word_Array[0];
             String[] tokens;
 
             switch (first_Word) {
+
             case "todo":
-                Todo todo_Word = new Todo(first_Word_Array[1]);
-                counter = counter + 1;
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + todo_Word);
-                System.out.println("Now you have " + counter + " tasks in the list.");
-                task_Array[counter] = todo_Word;
+                try {
+                    if (first_Word_Array.length == 1) { // means only have TODO
+                        throw new todoMissingException();
+                    }
+                    Todo todo_Word = new Todo(first_Word_Array[1]);
+                    counter = counter + 1;
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + todo_Word);
+                    System.out.println("Now you have " + counter + " tasks in the list.");
+                    task_Array[counter] = todo_Word;
+                    task_Array[counter] = new Task(input_Command);
+                    System.out.println("added: " + input_Command);
+                    counter = counter + 1;
+                    } catch (todoMissingException exception){
+                     System.out.println("OOPS!!! The description of a todo cannot be empty.");
+                }
                 break;
 
             case "deadline":
@@ -38,6 +53,9 @@ public class Duke {
                 System.out.println(" " + deadline_Word + " (by:" + get_Weekday[1] + ")");
                 System.out.println("Now you have " + counter + " tasks in the list.");
                 task_Array[counter] = deadline_Word;
+                task_Array[counter] = new Task(input_Command);
+                System.out.println("added: " + input_Command);
+                counter = counter + 1;
                 break;
 
             case "event":
@@ -53,6 +71,9 @@ public class Duke {
                 System.out.println(" " + meeting_Type + "(from: " + meeting_From[1] + "to: " + meeting_To[1] + ")");
                 System.out.println("Now you have " + counter + " tasks in the list.");
                 task_Array[counter] = meeting_Type;
+                task_Array[counter] = new Task(input_Command);
+                System.out.println("added: " + input_Command);
+                counter = counter + 1;
                 break;
 
             case "mark":
@@ -84,9 +105,11 @@ public class Duke {
                 return;
 
             default:
-                task_Array[counter] = new Task(input_Command);
-                System.out.println("added: " + input_Command);
-                counter = counter + 1;
+                try {
+                    throw new wrongCommandException();
+                } catch (wrongCommandException exception) {
+                    System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                }
             }
         }
     }
