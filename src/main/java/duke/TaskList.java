95aed88b67f0b5bd4b25166aa1527d6a0c743531
nicholas132000 <beefnlemon@gmail.com>
2023-02-27 16:27:32 +0800

    Add JavaDoc comments for FileHandling and TaskList classes

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -7,6 +7,13 @@ import static java.util.stream.Collectors.toList;
 
 public class TaskList {
 
+    /**
+     * Finds all tasks that contains the string task and returns it to the user
+     * @param tasks The ArrayList that contains all the tasks currently stored
+     * @param task The keyword of the task that the user is finding
+     * @return All the tasks that contains the keyword that the user is finding
+     * @throws DukeException Exception thrown when the task param is a white space
+     */
     public static ArrayList<Task> find_Input(ArrayList<Task> tasks, String task) throws DukeException {
         if (task == " ") {
             throw new DukeException();
@@ -16,6 +23,13 @@ public class TaskList {
                 .collect(toList());
     }
 
+    /**
+     * Adds a new Event object into the tasks ArrayList and stores it locally
+     * @param tasks The ArrayList that contains all the tasks currently stored
+     * @param task The keyword of the task that the user is finding
+     * @throws DukeException Exception thrown when the task param is a white space
+     * @throws IOException Exception thrown if an error occurs in the .saveContents() method
+     */
     public static void event_Input(ArrayList<Task> tasks, String task) throws DukeException, IOException {
         if (task == " ") {
             throw new DukeException();
@@ -28,6 +42,13 @@ public class TaskList {
         FileHandling.saveContents();
     }
 
+    /**
+     * Adds a new Deadline object into the tasks ArrayList and stores it locally
+     * @param tasks The ArrayList that contains all the tasks currently stored
+     * @param task The keyword of the task that the user is finding
+     * @throws DukeException Exception thrown when the task param is a white space
+     * @throws IOException Exception thrown if an error occurs in the .saveContents() method
+     */
     public static void deadline_Input(ArrayList<Task> tasks, String task) throws DukeException, IOException {
         if (task == " ") {
             throw new DukeException();
@@ -42,6 +63,13 @@ public class TaskList {
 
     }
 
+    /**
+     * Adds a new Todo object into the tasks ArrayList and stores it locally
+     * @param tasks The ArrayList that contains all the tasks currently stored
+     * @param task The keyword of the task that the user is finding
+     * @throws DukeException Exception thrown when the task param is a white space
+     * @throws IOException Exception thrown if an error occurs in the .saveContents() method
+     */
     public static void todo_Input(ArrayList<Task> tasks, String task) throws DukeException, IOException {
         if (task == " ") {
             throw new DukeException();
@@ -55,6 +83,13 @@ public class TaskList {
 
     }
 
+    /**
+     * Marks a task as done and stores this change locally
+     * @param tasks The ArrayList that contains all the tasks currently stored
+     * @param task The keyword of the task that the user is finding
+     * @throws DukeException Exception thrown when the task param is a white space
+     * @throws IOException Exception thrown if an error occurs in the .saveContents() method
+     */
     public static void mark_Input(ArrayList<Task> tasks, String task) throws DukeException, IOException {
         if (task == " ") {
             throw new DukeException();
@@ -66,6 +101,13 @@ public class TaskList {
         FileHandling.saveContents();
     }
 
+    /**
+     * Marks a task as undone and stores this change locally
+     * @param tasks The ArrayList that contains all the tasks currently stored
+     * @param task The keyword of the task that the user is finding
+     * @throws DukeException Exception thrown when the task param is a white space
+     * @throws IOException Exception thrown if an error occurs in the .saveContents() method
+     */
     public static void unmark_Input(ArrayList<Task> tasks, String task) throws DukeException, IOException {
         if (task == " ") {
             throw new DukeException();
@@ -77,6 +119,13 @@ public class TaskList {
         FileHandling.saveContents();
     }
 
+    /**
+     * Deletes a task from the tasks ArrayList and stores this change locally
+     * @param tasks The ArrayList that contains all the tasks currently stored
+     * @param task The keyword of the task that the user is finding
+     * @throws DukeException Exception thrown when the task param is a white space
+     * @throws IOException Exception thrown if an error occurs in the .saveContents() method
+     */
     public static void delete(ArrayList<Task> tasks, String task) throws DukeException, IOException {
         if (task == " ") {
             throw new DukeException();
@@ -89,6 +138,10 @@ public class TaskList {
         FileHandling.saveContents();
     }
 
+    /**
+     * List out all the elements currently in the tasks ArrayList
+     * @param tasks The ArrayList that contains all the tasks currently stored
+     */
     public static void list_Input(ArrayList<Task> tasks) {
         System.out.println("Here are the tasks in your list:");
         int index = 1;
