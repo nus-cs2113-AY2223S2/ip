8f9aee54ab4c9fed5ae858f198d6391bebba3f80
briantjs00 <briantjs00@gmail.com>
2023-03-01 02:21:57 +0800

    Fixed bug for DataParser

--- src/main/java/storage/DataParser.java
+++ src/main/java/storage/DataParser.java
@@ -4,20 +4,20 @@ import static task.TaskList.*;
 
 public class DataParser {
     public static void parseTodo(String data) {
-        String task = data.split("|",4)[3];
-        String status = data.split("|",4)[2];
+        String task = data.split("|",5)[4];
+        String status = data.split("|",5)[2];
         addTodoData(task, status);
     }
 
     public static void parseDeadline(String data) {
-        String task = data.split("|",4)[3];
-        String status = data.split("|",4)[2];
+        String task = data.split("|",5)[4];
+        String status = data.split("|",5)[2];
         addDeadlineData(task, status);
     }
 
     public static void parseEvent(String data) {
-        String task = data.split("|", 4)[3];
-        String status = data.split("|", 4)[2];
+        String task = data.split("|", 5)[4];
+        String status = data.split("|", 5)[2];
         addEventData(task, status);
     }
 }
