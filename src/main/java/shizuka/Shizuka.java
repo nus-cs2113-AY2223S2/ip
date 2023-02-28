28396c07c57ab9c73764efe83b7e325f11ffde99
SpawnageLoong <richardloongcj@gmail.com>
2023-02-28 20:58:43 +0800

    Add JavaDocs comments

--- src/main/java/shizuka/Shizuka.java
+++ src/main/java/shizuka/Shizuka.java
@@ -6,10 +6,20 @@ import java.util.Scanner;
 public class Shizuka {
     static final String FILE_PATH = "shizuka.txt";
 
+    /**
+     * Splits the input string into command keyword and arguments.
+     * @param args User input string
+     * @return Array of size 2, with command keyword at index 0 and arguments at index 1
+     */
     public static String[] parseCommand(String args) {
         return args.split(" ", 2);
     }
 
+    /**
+     * Parses the task number from the input string.
+     * @param args User input string
+     * @return Task number
+     */
     public static int parseNumber(String args) {
         int endIndex = args.indexOf(' ') + 1;
         return Integer.parseInt(args.substring(endIndex));
--- src/main/java/shizuka/TodoList.java
+++ src/main/java/shizuka/TodoList.java
@@ -22,6 +22,13 @@ public class TodoList {
         this.todos = new ArrayList<>();
     }
 
+    /**
+     * Parses the arguments for the deadline command.
+     *
+     * @param args The arguments String for the deadline command.
+     * @return An array of the arguments.
+     * @throws InvalidCommandFormatException If the arguments are not in the correct format.
+     */
     private static String[] parseDeadline(String args) throws InvalidCommandFormatException {
         String[] vars = args.split(BY_COMMAND, MAX_DEADLINE_ARGS);
         if (vars.length != 2) {
@@ -30,6 +37,13 @@ public class TodoList {
         return vars;
     }
 
+    /**
+     * Parses the arguments for the event command.
+     *
+     * @param args The arguments String for the event command.
+     * @return An array of the arguments.
+     * @throws InvalidCommandFormatException If the arguments are not in the correct format.
+     */
     private static String[] parseEvent(String args) throws InvalidCommandFormatException {
         String[] vars = new String[MAX_EVENT_ARGS];
         String[] tempArray0 = args.split(FROM_COMMAND, 2);
@@ -43,12 +57,22 @@ public class TodoList {
         return vars;
     }
 
+    /**
+     * Adds a Todo item to the list.
+     *
+     * @param args The description String for the item.
+     */
     public void addTodo(String args) {
         this.todos.add(new Todo(args));
         numberOfTasks += 1;
         Printer.addToList(args);
     }
 
+    /**
+     * Adds a Deadline item to the list.
+     *
+     * @param args The arguments String for the item.
+     */
     public void addDeadline(String args) {
         String[] vars;
         try {
@@ -62,6 +86,11 @@ public class TodoList {
         Printer.addToList(vars[0]);
     }
 
+    /**
+     * Adds an Event item to the list.
+     *
+     * @param args The arguments String for the item.
+     */
     public void addEvent(String args) {
         String[] vars;
         try {
@@ -75,6 +104,9 @@ public class TodoList {
         Printer.addToList(vars[0]);
     }
 
+    /**
+     * Prints the list of items.
+     */
     public void list() {
         System.out.print(LINE_BREAK + YOUR_TODO_LIST + LINE_BREAK);
         for (int i = 0; i < numberOfTasks; i += 1) {
@@ -83,24 +115,44 @@ public class TodoList {
         System.out.print(LINE_BREAK);
     }
 
+    /**
+     * Marks an item as done.
+     *
+     * @param num The number of the item to be marked.
+     */
     public void mark(int num) {
         this.todos.get(num - 1).isDone = true;
         System.out.println(LINE_BREAK + MARKED_TASK + num + IN_LIST + this.todos.get(num - 1).getStatusIcon()
                 + ' ' + this.todos.get(num - 1).description + "\n" + LINE_BREAK);
     }
 
+    /**
+     * Marks an item as not done.
+     *
+     * @param num The number of the item to be unmarked.
+     */
     public void unmark(int num) {
         this.todos.get(num - 1).isDone = false;
         System.out.println(LINE_BREAK + UNMARKED_TASK + num + IN_LIST + this.todos.get(num - 1).getStatusIcon()
                 + ' ' + this.todos.get(num - 1).description + "\n" + LINE_BREAK);
     }
 
+    /**
+     * Removes an item from the list.
+     *
+     * @param num The number of the item to be removed.
+     */
     public void deleteTask(int num) {
         this.todos.remove(num - 1);
         numberOfTasks -= 1;
         System.out.println(LINE_BREAK + "I have deleted Task " + num + IN_LIST + LINE_BREAK);
     }
 
+    /**
+     * Reformats the list of items into a String array for saving.
+     *
+     * @return The String array of items.
+     */
     public String[] listWriter() {
         String[] list = new String[numberOfTasks];
         for (int i = 0; i < numberOfTasks; i += 1) {
@@ -109,21 +161,41 @@ public class TodoList {
         return list;
     }
 
+    /**
+     * Adds a Todo item to the list from a file.
+     *
+     * @param args The arguments String for the item.
+     */
     public void addTodoFromFile(String[] args) {
         this.todos.add(new Todo(args));
         numberOfTasks += 1;
     }
 
+    /**
+     * Adds a Deadline item to the list from a file.
+     *
+     * @param args The arguments String for the item.
+     */
     public void addDeadlineFromFile(String[] args) {
         this.todos.add(new Deadline(args));
         numberOfTasks += 1;
     }
 
+    /**
+     * Adds an Event item to the list from a file.
+     *
+     * @param args The arguments String for the item.
+     */
     public void addEventFromFile(String[] args) {
         this.todos.add(new Event(args));
         numberOfTasks += 1;
     }
 
+    /**
+     * Finds and prints items in the list that contain a keyword.
+     *
+     * @param keyword The keyword to be searched for.
+     */
     public void find(String keyword) {
         ArrayList<Integer> foundItems = new ArrayList<Integer>();
         for (int i = 0; i < numberOfTasks; i += 1) {
