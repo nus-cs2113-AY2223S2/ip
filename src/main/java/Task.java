ecad58b2ca76568beaabf86a569ef098c17d4870
GraceZhuXY <grace.zhu.xing.yu@gmail.com>
2023-02-28 21:31:37 +0800

    Implement Find, add JavaDoc comments

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -3,28 +3,34 @@ public abstract class Task {
     protected boolean isDone;
     protected String type;
 
+    /** Creates a task, which must contain a description */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /** Returns whether a task is complete or incomplete */
     public String getStatusIcon() {
         // mark done task with X
         return (isDone ? "X" : " ");
     }
 
+    /** Marks a task as done */
     public void markAsDone(){
         isDone = true;
     }
 
+    /** Marks a task as not done */
     public void unmarkAsDone(){
         isDone = false;
     }
 
+    /** Returns type of task */
     public String getType() {
         return null;
     }
 
+    /** Returns relevant timings of task, if any */
     public String getTimings() {
         return null;
     }
