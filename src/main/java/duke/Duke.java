8cb0ed6b9946d9f2c4f0fe5a748bcc419dbdd941
yizt73 <e0773674@u.nus.edu>
2023-03-01 01:13:48 +0800

    added find function.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -4,88 +4,72 @@ package duke;
 import java.util.ArrayList;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 public class Duke {
-    private static int taskNum = 0;
-    private static final String filePath = "duke.txt";
-
-
-
+    private static Parser parser;
+    private static UI ui;
+    private static Storage storage;
+    private static TaskList taskList;
     public static void main(String[] args) throws IOException{
+        storage = new Storage();
+        ui = new UI();
+        parser = new Parser();
+        taskList = new TaskList();
         String command = "";
         Scanner in = new Scanner(System.in);
-        ArrayList<Task> storedTasks = new ArrayList<>();
-        File file = new File(filePath);
-        if (!file.exists()) {
-            file.createNewFile();
+        loadFile();
+        ui.showGreetings();
+        while (!command.equals("bye")) {
+            ui.printLinebreak();
+            command = in.nextLine();
+            String[] commandLine = parser.parseCommand(command);
+            ui.printLinebreak();
+            doCommand(commandLine);
         }
+    }
+    private static void loadFile() throws IOException {
+        File file = storage.createFile();
         Scanner fileReader = new Scanner(file);
-
         while (fileReader.hasNext()) {
             String[] savedDataLine;
             savedDataLine = fileReader.nextLine().split("/");
             if (savedDataLine[0].equals("[T]")) {
-                Todo todo = new Todo(savedDataLine[2]);
-                if (savedDataLine[1].equals("[X]")) {
-                    todo.setDone(true);
-                }
-                storedTasks.add(todo);
+                taskList.setStoredTasks(parser.parseTodo(savedDataLine, taskList.getStoredTasks()));
             } else if (savedDataLine[0].equals("[D]")) {
-                Deadline deadline = new Deadline(savedDataLine[2], savedDataLine[3]);
-                if (savedDataLine[1].equals("[X]")) {
-                    deadline.setDone(true);
-                }
-                storedTasks.add(deadline);
+                taskList.setStoredTasks(parser.parseDeadline(savedDataLine, taskList.getStoredTasks()));
             } else if (savedDataLine[0].equals("[E]")) {
-                String[] period = savedDataLine[3].split(",");
-                Event event = new Event(savedDataLine[2], period[0], period[1]);
-                if (savedDataLine[1].equals("[X]")) {
-                    event.setDone(true);
-                }
-                storedTasks.add(event);
+                taskList.setStoredTasks(parser.parseEvent(savedDataLine, taskList.getStoredTasks()));
             }
-            taskNum++;
+            taskList.incrementTaskNum();
         }
-
-        showGreetings();
-
-        while (!command.equals("bye")) {
-            printLineBreak();
-            command = in.nextLine();
-            String[] commandLine = command.split(" ", 2);
-            printLineBreak();
-            doCommand(storedTasks, commandLine);
-
-        }
-
-
-
     }
 
-    private static void doCommand(ArrayList<Task> storedTasks, String[] commandLine) {
+    private static void doCommand(String[] commandLine) {
         switch (commandLine[0]) {
         case "todo":
-            addTodoTask(storedTasks, commandLine);
+            addTodoTask(commandLine);
             break;
         case "deadline":
-            addDeadlineTask(storedTasks, commandLine);
+            addDeadlineTask(commandLine);
             break;
         case "event":
-            addEventTask(storedTasks, commandLine);
+            addEventTask(commandLine);
             break;
         case "mark":
-            markTask(storedTasks, commandLine);
+            markTask(commandLine);
             break;
         case "unmark":
-            unmarkTask(storedTasks, commandLine);
+            unmarkTask(commandLine);
             break;
         case "list":
-            listTasks(storedTasks);
+            listTasks();
             break;
         case "delete":
-            deleteTask(storedTasks, commandLine);
+            deleteTask(commandLine);
+            break;
+        case "find":
+            findItem(commandLine);
             break;
         case "bye":
             System.out.println("Bye. Hope to see you again soon!");
@@ -103,84 +87,76 @@ public class Duke {
         throw new IllegalDukeArgumentException();
     }
 
-    private static void listTasks(ArrayList<Task> storedTasks) {
+    private static void listTasks() {
         listMessage();
         int count = 0;
-        for (Task i : storedTasks) {
+        for (Task i : taskList.getStoredTasks()) {
             System.out.println((count + 1) + ". " + i.getTypeIcon() +
                     i.getStatusIcon() + " " + i.getDescription());
             count++;
         }
     }
 
-    private static void deleteTask(ArrayList<Task> storedTasks, String[] commandLine) {
+    private static void deleteTask(String[] commandLine) {
         int delIndex = Integer.parseInt((commandLine[1])) - 1;
         System.out.println("Noted. I've removed this task:");
-        System.out.println(storedTasks.get(delIndex).getTypeIcon() + storedTasks.get(delIndex).getStatusIcon()
-                + " " + storedTasks.get(delIndex).getDescription());
-        storedTasks.remove(delIndex);
+        System.out.println(taskList.getStoredTasks().get(delIndex).getTypeIcon()
+                    + taskList.getStoredTasks().get(delIndex).getStatusIcon()
+                    + " " + taskList.getStoredTasks().get(delIndex).getDescription());
+        taskList.removeItem(delIndex);
         try{
-            updateFile(storedTasks);
+            storage.updateFile(taskList.getStoredTasks());
         } catch (IOException e) {
             System.out.println("Oops! Something broke: " + e);
         }
     }
 
-    private static void unmarkTask(ArrayList<Task> storedTasks, String[] commandLine) {
+    private static void unmarkTask(String[] commandLine) {
         int unmarkIndex = Integer.parseInt(commandLine[1]) - 1;
-        storedTasks.get(unmarkIndex).setDone(false);
+        taskList.getStoredTasks().get(unmarkIndex).setDone(false);
         System.out.println("OK, I've marked this task as not done yet.");
-        System.out.println(storedTasks.get(unmarkIndex).getTypeIcon() +
-                storedTasks.get(unmarkIndex).getStatusIcon() + " " + storedTasks.get(unmarkIndex).getDescription());
+        System.out.println(taskList.getStoredTasks().get(unmarkIndex).getTypeIcon() +
+                    taskList.getStoredTasks().get(unmarkIndex).getStatusIcon()
+                    + " " + taskList.getStoredTasks().get(unmarkIndex).getDescription());
         try{
-            updateFile(storedTasks);
+            storage.updateFile(taskList.getStoredTasks());
         } catch (IOException e) {
             System.out.println("Oops! Something broke: " + e);
         }
     }
 
-    private static void markTask(ArrayList<Task> storedTasks, String[] commandLine) {
+    private static void markTask(String[] commandLine) {
         int taskIndex = Integer.parseInt(commandLine[1]) - 1;
-        storedTasks.get(taskIndex).setDone(true);
+        taskList.getStoredTasks().get(taskIndex).setDone(true);
         System.out.println("Nice! I've marked this task as done.");
-        System.out.println(storedTasks.get(taskIndex).getTypeIcon() +
-                storedTasks.get(taskIndex).getStatusIcon() + " " + storedTasks.get(taskIndex).getDescription());
+        System.out.println(taskList.getStoredTasks().get(taskIndex).getTypeIcon() +
+                taskList.getStoredTasks().get(taskIndex).getStatusIcon() + " " + taskList.getStoredTasks().get(taskIndex).getDescription());
 
         try{
-            updateFile(storedTasks);
+            storage.updateFile(taskList.getStoredTasks());
         } catch (IOException e) {
             System.out.println("Oops! Something broke: " + e);
         }
 
     }
 
-    private static void addEventTask(ArrayList<Task> storedTasks, String[] commandLine) {
+    private static void addEventTask(String[] commandLine) {
         String[] eventString = commandLine[1].split("/from");
         String[] eventStartEnd = eventString[1].split("/to");
         Event ev = new Event(eventString[0], eventStartEnd[0], eventStartEnd[1]);
-        storedTasks.add(ev);
+        taskList.addItem(ev);
         addTaskMessage();
         System.out.println("  " + ev.getTypeIcon() +
                 ev.getStatusIcon() + " " + ev.getDescription() + "(from: " + ev.getStart() + " to: " + ev.getEnd() +")");
-        taskNum++;
+        taskList.incrementTaskNum();
         displayTasksNum();
         try{
-            updateFile(storedTasks);
+            storage.updateFile(taskList.getStoredTasks());
         } catch (IOException e) {
             System.out.println("Oops! Something broke: " + e);
         }
     }
-    private static void updateFile(ArrayList<Task> storedTasks) throws IOException {
-        FileWriter fileWriter = new FileWriter(filePath);
-        for (Task i : storedTasks) {
-            String textToAdd = i.getTypeIcon() + "/" + i.getStatusIcon() + "/" + i.getDescription() + "/" +
-                    i.getInfo() + "\n";
-            fileWriter.write(textToAdd);
-        }
-        fileWriter.close();
-    }
-
-    private static void addTodoTask(ArrayList<Task> storedTasks, String[] commandLine) {
+    private static void addTodoTask(String[] commandLine) {
 
         try {
             validateTodo(commandLine);
@@ -189,13 +165,13 @@ public class Duke {
             return;
         }
         Todo td = new Todo(commandLine[1]);
-        storedTasks.add(td);
+        taskList.addItem(td);
         addTaskMessage();
         System.out.println("  " + td.getTypeIcon() + td.getStatusIcon() + " " + td.getDescription());
-        taskNum++;
+        taskList.incrementTaskNum();
         displayTasksNum();
         try {
-            updateFile(storedTasks);
+            storage.updateFile(taskList.getStoredTasks());
         } catch (IOException e) {
             System.out.println("Oops! Something broke: " + e);
         }
@@ -207,36 +183,38 @@ public class Duke {
         }
     }
 
-    private static void addDeadlineTask(ArrayList<Task> storedTasks, String[] commandLine) {
+    private static void addDeadlineTask(String[] commandLine) {
         String[] deadlineString = commandLine[1].split("/by");
         Deadline dl = new Deadline(deadlineString[0], deadlineString[1]);
-        storedTasks.add(dl);
+        taskList.addItem(dl);
         addTaskMessage();
         System.out.println("  " + dl.getTypeIcon() +
                 dl.getStatusIcon() + " " + dl.getDescription() + "(by: " + dl.getBy() + ")");
-        taskNum++;
+        taskList.incrementTaskNum();
         displayTasksNum();
         try{
-            updateFile(storedTasks);
+            storage.updateFile(taskList.getStoredTasks());
         } catch (IOException e) {
             System.out.println("Oops! Something broke: " + e);
         }
     }
-
-    private static void showGreetings() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
+    private static void findItem(String[] commandLine) {
+        String keyword = commandLine[1];
+        int count = 1;
+        System.out.println("Here are the matching tasks in your list: ");
+        for (Task i : taskList.getStoredTasks()) {
+            if (i.getDescription().contains(keyword)) {
+                if (i instanceof Deadline) {
+                    System.out.println(count + "." + i.getTypeIcon() + i.getStatusIcon() + i.getDescription() + "/by: " + i.getInfo());
+                } else if (i instanceof Event) {
+                    System.out.println(count + "." + i.getTypeIcon() + i.getStatusIcon() + i.getDescription() + "/start, end: " + i.getInfo());
+                } else {
+                    System.out.println(count + "." + i.getTypeIcon() + i.getStatusIcon() + i.getDescription() + i.getInfo());
+                }
+                count++;
+            }
+        }
     }
-    private static void printLineBreak() {
-        System.out.println("--------------------------------------------------------------");
-    }
-
     private static void addTaskMessage() {
         System.out.println("Got it. I've added this task.");
     }
@@ -244,6 +222,6 @@ public class Duke {
         System.out.println("Here are the tasks in your list:");
     }
     private static void displayTasksNum() {
-        System.out.println("Now you have " + taskNum + " task(s) in the list.");
+        System.out.println("Now you have " + taskList.getTaskNum() + " task(s) in the list.");
     }
 }
