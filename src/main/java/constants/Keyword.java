7d9ebc430210dc75abf1e36482db27f254181cc6
woowenjun99 <woowenjun99@gmail.com>
2023-03-02 20:35:31 +0800

    Add in new keywords

--- src/main/java/constants/Keyword.java
+++ src/main/java/constants/Keyword.java
@@ -6,7 +6,11 @@ public interface Keyword {
     String DEADLINE = "deadline";
     String DESCRIPTION = "description";
     String END = "end";
+    String FROM = "from ";
     String INDEX = "index";
     String KEYWORD = "keyword";
+    String SLASH = "/";
     String START = "start";
+    String SPACE = " ";
+    String TO = "to ";
 }
