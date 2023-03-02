f4e64e55895bbd366345264b09a3938f96da7255
moseslee9012 <mlee9012@gmail.com>
2023-03-02 20:25:01 +0800

    Level 9: "Find" function implemented

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -4,6 +4,7 @@ public class Duke {
 
     private static Scanner in = new Scanner(System.in);
     private static final Task[] tasks = new Task[100];
+    private static final Task[] foundTasks = new Task[100];
 
     public static void whatToDo(String line) {
         try {
@@ -21,11 +22,23 @@ public class Duke {
         }
     }
 
+    public static boolean isWordPresent(Task task, String keyword) {
+        String[] words = (task.description).split(" ");
+
+        for (String temp : words) {
+            if (temp.compareToIgnoreCase(keyword) == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void command(String line) throws InvalidCommand {
         String exitCommand = "bye";
         String taskList = "list";
         String checkAsDone = "mark";
         String del = "delete";
+        String search = "find";
         int numberOfTasks = 0;
 
         String[] tokens = line.split(" ", 2);
@@ -69,46 +82,65 @@ public class Duke {
                 line = in.nextLine();
                 tokens = line.split(" ", 0);
                 command = tokens[0];
-            } else {
-                String task = tokens[1];
-                switch(command) {
-                case "todo":
-                    Task t = new ToDo(task);
-                    tasks[numberOfTasks] = t;
-                    tasks[numberOfTasks].isDone = false;
-                    ++numberOfTasks;
-                    break;
-                case "deadline":
-                    String[] taskDeadline = task.split("/", 2);
-                    String taskDescription = taskDeadline[0];
-                    String dueBy = taskDeadline[1];
+            } else if (command.equals(search)) {
+                String itemToFind = tokens[1];
+                int searchNumber = 0;
 
-                    Task d = new Deadline(taskDescription, dueBy);
-                    tasks[numberOfTasks] = d;
-                    tasks[numberOfTasks].isDone = false;
-                    ++numberOfTasks;
-                    break;
-                case "event":
-                    String[] taskEvent = task.split("/", 3);
-                    String eventDescription = taskEvent[0];
-                    String start = taskEvent[1];
-                    String end = taskEvent[2];
-
-                    Task e = new Event(eventDescription, start, end);
-                    tasks[numberOfTasks] = e;
-                    tasks[numberOfTasks].isDone = false;
-                    ++numberOfTasks;
-                    break;
-                default:
-                    System.out.println("Sorry! I couldn't understand your command:( Please try again.");
-                    throw new InvalidCommand();
+                for (int i = 0; i < numberOfTasks; ++i) {
+                    if (isWordPresent(tasks[i], itemToFind)) {
+                        foundTasks[searchNumber] = tasks[i];
+                        searchNumber++;
+                    }
                 }
-                System.out.println("added: " + line);
+                System.out.println("Tasks matching with " + itemToFind);
+                for (int i = 0; i < searchNumber; ++i) {
+                    System.out.format("%d. ", (i + 1));
+                    System.out.println(foundTasks[i]);
+                }
+
                 line = in.nextLine();
                 tokens = line.split(" ", 0);
                 command = tokens[0];
+            } else {
+                    String task = tokens[1];
+                    switch(command) {
+                    case "todo":
+                        Task t = new ToDo(task);
+                        tasks[numberOfTasks] = t;
+                        tasks[numberOfTasks].isDone = false;
+                        ++numberOfTasks;
+                        break;
+                    case "deadline":
+                        String[] taskDeadline = task.split("/", 2);
+                        String taskDescription = taskDeadline[0];
+                        String dueBy = taskDeadline[1];
+
+                        Task d = new Deadline(taskDescription, dueBy);
+                        tasks[numberOfTasks] = d;
+                        tasks[numberOfTasks].isDone = false;
+                        ++numberOfTasks;
+                        break;
+                    case "event":
+                        String[] taskEvent = task.split("/", 3);
+                        String eventDescription = taskEvent[0];
+                        String start = taskEvent[1];
+                        String end = taskEvent[2];
+
+                        Task e = new Event(eventDescription, start, end);
+                        tasks[numberOfTasks] = e;
+                        tasks[numberOfTasks].isDone = false;
+                        ++numberOfTasks;
+                        break;
+                    default:
+                        System.out.println("Sorry! I couldn't understand your command:( Please try again.");
+                        throw new InvalidCommand();
+                    }
+                    System.out.println("added: " + line);
+                    line = in.nextLine();
+                    tokens = line.split(" ", 0);
+                    command = tokens[0];
+                }
             }
-        }
         System.out.println("Bye! Hope to see you again");
     }
 
