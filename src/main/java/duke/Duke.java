87078b25962cb26ac05c952cdb713c92596fabbb
woowenjun99 <woowenjun99@gmail.com>
2023-03-02 12:49:04 +0800

    Extract out the switch case

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,6 +2,7 @@ package duke;
 
 import constants.Command;
 import constants.ErrorMessage;
+import constants.Keyword;
 import constants.Message;
 import controller.TaskController;
 import exception.DukeException;
@@ -41,53 +42,8 @@ public class Duke {
   private static void process(String words) {
     try {
       HashMap<String, String> dictionary = parser.parse(words);
-      String command = dictionary.get("command");
-      String description;
-      String index;
-      int position;
-
-      switch (command) {
-      case Command.DELETE:
-        index = dictionary.get("index");
-        position = Integer.parseInt(index) - 1;
-        controller.deleteTask(position);
-        ui.printMessage(Message.TASK_DELETED);
-        break;
-      case Command.LIST:
-        controller.listTasks();
-        break;
-      case Command.BYE:
-        terminate();
-        break;
-      case Command.TODO:
-        description = dictionary.get("description");
-        controller.addTodoTask(description);
-        break;
-      case Command.MARK:
-      case Command.UNMARK:
-        index = dictionary.get("index");
-        position = Integer.parseInt(index) - 1;
-        boolean isMark = command.equals(Command.MARK);
-        controller.toggleMark(isMark, position);
-        break;
-      case Command.DEADLINE:
-        description = dictionary.get("description");
-        String deadline = dictionary.get("deadline");
-        controller.addDeadlineTask(description, deadline);
-        break;
-      case Command.EVENT:
-        description = dictionary.get("description");
-        String start = dictionary.get("start");
-        String end = dictionary.get("end");
-        controller.addEventTask(description, start, end);
-        break;
-      case Command.FIND:
-        String keyword = dictionary.get("keyword");
-        controller.findTask(keyword);
-        break;
-      default:
-        throw new DukeException(ErrorMessage.INVALID_COMMAND);
-      }
+      String command = dictionary.get(Keyword.COMMAND);
+      processDictionary(dictionary, command);
     } catch (DukeException e) {
       ui.printMessage(e.getDescription());
     } catch (NumberFormatException e) {
@@ -95,6 +51,60 @@ public class Duke {
     } catch (Exception e) {
       ui.printMessage(e.getMessage());
     }
+  }
+
+  private static void processDictionary(HashMap<String, String> dictionary, String command) throws Exception {
+    int position;
+    String index;
+    String description;
+    switch (command) {
+    case Command.DELETE:
+      handleDelete(dictionary);
+      break;
+    case Command.LIST:
+      controller.listTasks();
+      break;
+    case Command.BYE:
+      terminate();
+      break;
+    case Command.TODO:
+      description = dictionary.get(Keyword.DESCRIPTION);
+      controller.addTodoTask(description);
+      break;
+    case Command.MARK:
+    case Command.UNMARK:
+      index = dictionary.get(Keyword.INDEX);
+      position = Integer.parseInt(index) - 1;
+      boolean isMark = command.equals(Command.MARK);
+      controller.toggleMark(isMark, position);
+      break;
+    case Command.DEADLINE:
+      description = dictionary.get(Keyword.DESCRIPTION);
+      String deadline = dictionary.get(Keyword.DEADLINE);
+      controller.addDeadlineTask(description, deadline);
+      break;
+    case Command.EVENT:
+      description = dictionary.get(Keyword.DESCRIPTION);
+      String start = dictionary.get("start");
+      String end = dictionary.get("end");
+      controller.addEventTask(description, start, end);
+      break;
+    case Command.FIND:
+      String keyword = dictionary.get(Keyword.KEYWORD);
+      controller.findTask(keyword);
+      break;
+    default:
+      throw new DukeException(ErrorMessage.INVALID_COMMAND);
+    }
+  }
+
+  private static void handleDelete(HashMap<String, String> dictionary) throws Exception {
+    String index;
+    int position;
+    index = dictionary.get(Keyword.INDEX);
+    position = Integer.parseInt(index) - 1;
+    controller.deleteTask(position);
+    ui.printMessage(Message.TASK_DELETED);
   }
 
   /**
