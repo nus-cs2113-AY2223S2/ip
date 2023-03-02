b3873f5e0f46f15b2d41985dbf4a79c8259805ad
haoyangw <haoyangw@gmail.com>
2023-03-02 20:45:06 +0800

    Improve data file creation logic
    Replace my own String parsing code for
      the data file path String with Java's
      own get parent path method Path.getParent()
      to cut down unnecessary coding(duplicate).
    
    Also, use RicaStorageException instead of
      returning an error message String
      from StorageManager.createDataFile().
      More conventional.
    
    Lastly, make the happy path of
      StorageManager.getSavedTasks() clearer
      and include more specific exception
      handling and corresponding user-
      friendly messages to print on the
      user's screen.

--- src/main/java/io/github/haoyangw/rica/storage/StorageManager.java
+++ src/main/java/io/github/haoyangw/rica/storage/StorageManager.java
@@ -2,6 +2,7 @@ package io.github.haoyangw.rica.storage;
 
 import io.github.haoyangw.rica.exception.RicaStorageException;
 import io.github.haoyangw.rica.task.Task;
+import io.github.haoyangw.rica.ui.TextUi;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -21,10 +22,13 @@ import java.util.Scanner;
  */
 public class StorageManager {
     private static final String DEFAULT_DATA_PATH = "data/tasks.txt";
-    private static final String INVALID_DATA_PATH_GIVEN_ERROR = " Empty data path given, what am I supposed to read xD";
+    private static final String DATA_FILE_HAS_NO_PARENT_ERROR = " My data file is placed in the void? Please don't give me amnesia :O (contact Mr Stark!!)";
     private static final String DATA_FILE_NOT_FOUND_ERROR = " Cannot find the data file, you're gonna have to deal with my amnesia today my friend xP";
+    private static final String INVALID_DATA_PATH_GIVEN_ERROR = " Empty data path given, what am I supposed to read xD";
     private static final String FAILED_TO_CREATE_DATA_FILE_ERROR = " Darn, can't even create a data file to store my memory. Not my day! xP";
+    private static final String STILL_NO_DATA_FILE_ERROR = " Mayday mayday... I tried creating my data file and it disappeared Dx";
     private final String dataPath;
+    private final TextUi textUi;
 
     public StorageManager() {
         this(StorageManager.DEFAULT_DATA_PATH);
@@ -32,33 +36,36 @@ public class StorageManager {
 
     public StorageManager(String dataPath) {
         this.dataPath = dataPath;
+        this.textUi = new TextUi();
     }
 
     /**
-     * Creates all directories along the data storage path if they don't already exist.
+     * Creates all directories along the data file path if they don't already exist.
      *
-     * @param dataPath Desired data storage path
+     * @param dataPathString Path to desired data file
      * @return String specifying any errors encountered during the process
      */
-    private static String createDataFile(String dataPath) {
-        String[] pathComponents = dataPath.split("/");
-        int numOfComponents = pathComponents.length;
-        String fileName = pathComponents[numOfComponents - 1];
-        // beforeLastClash: Basically remove the filename and final '/'
-        int beforeLastSlash = dataPath.length() - fileName.length() - 1;
-        String parentDir = dataPath.substring(0, beforeLastSlash);
-        Path parentPath = Paths.get(parentDir);
-        try {
-            Files.createDirectories(parentPath);
-        } catch (IOException exception) {
-            return StorageManager.FAILED_TO_CREATE_DATA_FILE_ERROR;
+    private static void createDataFile(String dataPathString) {
+        Path dataPath = Paths.get(dataPathString);
+        Path parentDirPath = dataPath.getParent();
+        if (parentDirPath == null) {
+            throw new RicaStorageException(StorageManager.DATA_FILE_HAS_NO_PARENT_ERROR);
         }
-        String NO_ERRORS_RESULT = "";
-        return NO_ERRORS_RESULT;
+        try {
+            // Create all directories along the data file path if any don't exist
+            Files.createDirectories(parentDirPath);
+            dataPath.toFile().createNewFile();
+        } catch (IOException exception) {
+            throw new RicaStorageException(StorageManager.FAILED_TO_CREATE_DATA_FILE_ERROR);
+        }
     }
 
     private String getDataPath() {
         return this.dataPath;
+    }
+
+    private TextUi getTextUi() {
+        return this.textUi;
     }
 
     /**
@@ -72,10 +79,22 @@ public class StorageManager {
      */
     public ArrayList<Task> getSavedTasks() throws RicaStorageException {
         File dataFile;
+        try {
+            dataFile = new File(this.getDataPath());
+        } catch (NullPointerException exception) {
+            throw new RicaStorageException(StorageManager.INVALID_DATA_PATH_GIVEN_ERROR);
+        }
+        if (!dataFile.exists()) {
+            this.getTextUi().printlnWithIndent(StorageManager.DATA_FILE_NOT_FOUND_ERROR);
+            /*
+             * createDataFile() will throw RicaStorageException if data file cannot
+             *   be created
+             */
+            StorageManager.createDataFile(this.getDataPath());
+        }
         Scanner dataReader;
         ArrayList<Task> savedTasks = new ArrayList<>();
         try {
-            dataFile = new File(this.getDataPath());
             dataReader = new Scanner(dataFile);
             while (dataReader.hasNext()) {
                 String objectData = dataReader.nextLine();
@@ -85,14 +104,7 @@ public class StorageManager {
         } catch (NullPointerException exception) {
             throw new RicaStorageException(StorageManager.INVALID_DATA_PATH_GIVEN_ERROR);
         } catch (FileNotFoundException exception) {
-            String result = StorageManager.createDataFile(this.getDataPath());
-            String errorMessage = StorageManager.DATA_FILE_NOT_FOUND_ERROR;
-            if (!result.isBlank()) {
-                errorMessage += System.lineSeparator() + result;
-            }
-            errorMessage += System.lineSeparator()
-                    + "    ____________________________________________________________";
-            throw new RicaStorageException(errorMessage);
+            throw new RicaStorageException(StorageManager.STILL_NO_DATA_FILE_ERROR);
         }
         return savedTasks;
     }
