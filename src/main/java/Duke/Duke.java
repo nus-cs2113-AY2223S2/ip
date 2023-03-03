d457e9eeaa8b7e307a4fea56c1fff0a317631d7e
aaronxujiachen <1943999635x@gmail.com>
2023-03-03 20:41:30 +0800

    Duke Level 6

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -1,4 +1,7 @@
+package Duke;
+
 import java.util.Scanner;  // Import the Scanner class
+import java.util.ArrayList;
 public class Duke {
 
     public static void drawLine() {  // Draw horizontal lines
@@ -16,7 +19,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         drawLine();
-        System.out.println("Hello! I'm Duke");
+        System.out.println("Hello! I'm Duke.Duke");
         System.out.println("What can I do for you?");
         drawLine();
     }
@@ -26,77 +29,71 @@ public class Duke {
         drawLine();
     }
 
-    public static void listTasks(Task[] tasks, int curPos) {  // List out all the tasks
+    public static void listTasks(ArrayList<Task> tasks) {  // List out all the tasks
         System.out.println("Here are the tasks in your list:");
-        for(int i = 0; i < curPos; i++) {
-            System.out.println((i+1) + ". " + tasks[i].getType() + tasks[i].toString());
+        for(int i = 0; i < tasks.size(); i++) {
+            System.out.println((i+1) + ". " + tasks.get(i).getType() + tasks.get(i).toString());
         }
         drawLine();
     }
 
-    public static void markStatus(String input, Task[] tasks) {  // Mark tasks as done/undone
+    public static void markStatus(String input, ArrayList<Task> tasks) {  // Mark tasks as done/undone
         String[] arrOfInput = input.split(" ", 2);
-        int index = Integer.parseInt(arrOfInput[1]) - 1;  //index = curPos + 1
+        int index = Integer.parseInt(arrOfInput[1]);
+        index = index - 1;
 
         if(input.startsWith("mark")){
-            tasks[index].isDone = true;
+            tasks.get(index).isDone = true;
             System.out.println("Nice! I've marked this task as done:");
         }
         else {
-            tasks[index].isDone = false;
+            tasks.get(index).isDone = false;
             System.out.println("OK, I've marked this task as not done yet:");
         }
 
-        System.out.println("   " + tasks[index].getType() + tasks[index].getStatusIcon() + tasks[index].description);
+        System.out.println("   " + tasks.get(index).getType() + tasks.get(index).toString());
         drawLine();
     }
 
-    public static int updateTasks(String input, Task[] tasks, int curPos) {  // Include remarks
+    public static int updateTasks(String input, ArrayList<Task> tasks, int curPos) {  // Include remarks
         String[] arrOfInput = input.split(" ", 2);
-        String todo = "todo";
-        String deadline = "deadline";
-        String event = "event";
         System.out.println("Got it. I've added this task:");
 
-        if (arrOfInput[0].equals(todo)) {
-            tasks[curPos] = new Task(arrOfInput[1]);
+        if (arrOfInput[0].equals("todo")) {
+            tasks.add(new Task(arrOfInput[1]));
         }
-        else if (arrOfInput[0].equals(deadline)) {
+        else if (arrOfInput[0].equals("deadline")) {
             String[] arrOfTask = arrOfInput[1].split("/by");
-            tasks[curPos] = new Deadline(arrOfTask[0], arrOfTask[1]);
+            tasks.add(new Deadline(arrOfTask[0], arrOfTask[1]));
         }
         else {
             String[] arrOfTask = arrOfInput[1].split("/from");
             String[] arrOfEvent = arrOfTask[1].split("/to");
-            tasks[curPos] = new Event(arrOfTask[0], arrOfEvent[0], arrOfEvent[1]);
+            tasks.add(new Event(arrOfTask[0], arrOfEvent[0], arrOfEvent[1]));
         }
 
-        System.out.println("   " + tasks[curPos].getType() + tasks[curPos].toString());
+        System.out.println("   " + tasks.get(curPos).getType() + tasks.get(curPos).toString());
         curPos++;
-
-        if(curPos == 1) {
-            System.out.println("Now you have " + curPos + " task in the list.");
-        }
-        else {
-            System.out.println("Now you have " + curPos + " tasks in the list.");
-        }
-
+        System.out.println("Now you have " + curPos + (curPos > 1 ? " tasks " : " task ") + "in the list.");
         drawLine();
         return curPos;
     }
 
     // @@[yuanners] [aaronxujiachen]-reused
     // With minor modification
-    public static void checkInput(String input, Task[] tasks, int curPos) throws DukeException {
+    public static int checkInput(String input, ArrayList<Task> tasks, int curPos) throws DukeException {
 
         if(input.length () > 4) {
-            if(input.startsWith ("mark") || input.startsWith ("unmark")) {
+            if(input.startsWith("mark") || input.startsWith("unmark")) {
                 markStatus(input, tasks);
             }
-            else if(input.startsWith ("todo") || input.startsWith ("deadline") || input.startsWith ("event")) {
+            else if(input.startsWith("todo") || input.startsWith("deadline") || input.startsWith("event")) {
                 curPos = updateTasks(input, tasks, curPos);
             }
-            else if(!((input.startsWith("mark")) || (input.startsWith ("unmark")) || (input.startsWith ("todo")) || (input.startsWith ("deadline")) || (input.startsWith ("event")))) {
+            else if(input.startsWith("delete")) {
+                curPos = deleteTask (input, tasks, curPos);
+            }
+            else if(!((input.startsWith("mark")) || (input.startsWith("unmark")) || (input.startsWith("todo")) || (input.startsWith("deadline")) || (input.startsWith("event")) || (input.startsWith("delete")))) {
                 throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
             else{
@@ -105,7 +102,7 @@ public class Duke {
         }
         else {
             if(input.equals("list")) {
-                listTasks(tasks, curPos);
+                listTasks(tasks);
             }
             else if(input.equals("mark") || input.equals("todo")) {
                 throw new DukeException("☹ OOPS!!! The description of a " + input + " cannot be empty.");
@@ -114,16 +111,30 @@ public class Duke {
                 throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
         }
+
+        return curPos;
     }
-
-
     // @@[yuanners]
+
+    public static int deleteTask (String input, ArrayList<Task> tasks, int curPos) {
+        String[] arrOfInput = input.split (" ", 2);
+        int index = Integer.parseInt(arrOfInput[1]);
+        index = index - 1;
+        System.out.println("Noted. I've removed this task:");
+        System.out.println("	" + tasks.get(index).getType() + tasks.get(index).toString());
+        tasks.remove(index);
+        curPos--;
+        System.out.println("Now you have " + curPos + " tasks in the list.");
+        drawLine();
+        return curPos;
+    }
 
     public static void main(String[] args) {
         hiDuke();  //Print Logo
+
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
-        Task tasks[] = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<Task>();
         int curPos = 0;
 
         while(!(input.isEmpty())) {
@@ -133,7 +144,7 @@ public class Duke {
             }
             else {
                 try {
-                    checkInput(input, tasks, curPos);
+                    curPos = checkInput(input, tasks, curPos);
                 }
                 catch(DukeException duEx) {
                     System.out.println(duEx.getMessage());
