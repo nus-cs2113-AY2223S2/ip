e311f6b594e990054d53ef94a4aa6d7f20a31a24
kishore-a00 <skippyepic@gmail.com>
2023-03-03 16:09:27 +0800

    Add documentation for Deadline class

--- src/main/java/duke/tasks/Deadline.java
+++ src/main/java/duke/tasks/Deadline.java
@@ -1,9 +1,18 @@
 package duke.tasks;
 
+/**
+ * This class inherits from Task class.
+ * Deadlines contain a description as well as a due date.
+ */
 public class Deadline extends Task {
 
     private String dueInfo;
 
+    /**
+     * Creates a deadline object.
+     * @param description
+     * @param dueInfo
+     */
     public Deadline(String description, String dueInfo) {
         super(description, "D");
         setDueInfo(dueInfo);
