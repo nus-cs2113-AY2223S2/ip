7e0462af0dcbebf96ad06e48881e216e1070b31f
pyongjie <pyjavatar@gmail.com>
2023-03-03 22:01:52 +0800

    Implement Duke A-JavaDoc: JavaDoc - Add comments to code under Duke

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -17,10 +17,20 @@ public class Duke {
     private Storage storage;
     private TaskList tasks;
 
+    /**
+     * Creates a new instance of Duke chatbot and calls run().
+     * @param args
+     */
     public static void main(String[] args) {
         new Duke().run();
     }
 
+    /**
+     * Initialises an instance of Duke chatbot.
+     * Creates a new instance of ui, storage and task list in Duke.
+     * Tries to load the task list from the given file path.
+     * Creates a new empty task list if loading from file path fails.
+     */
     public Duke() {
         ui = new Ui();
         storage = new Storage("./data/duke.txt");
@@ -32,6 +42,11 @@ public class Duke {
         }
     }
 
+    /**
+     * Runs the Duke chatbot by displaying a welcome message, getting user input and executing commands based
+     * on input.
+     * Displays an exit message when the Duke exits.
+     */
     public void run() {
         ui.showWelcomeMessage();
         boolean isExit = false;
@@ -39,7 +54,7 @@ public class Duke {
         while (!isExit) {
             try {
                 String input = ui.getInput();
-                Command c = Parser.parse(input, ui);
+                Command c = Parser.parse(input);
                 c.execute(tasks, ui, storage);
                 isExit = c.isExit();
             } catch (UnknownCommandException e) {
