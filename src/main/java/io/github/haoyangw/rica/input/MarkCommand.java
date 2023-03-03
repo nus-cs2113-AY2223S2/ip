a2e043bb9672e8b217e2e5dbe17b8dccf8eaa621
haoyangw <haoyangw@gmail.com>
2023-03-03 16:04:22 +0800

    Handle NumberFormatException
    mark and unmark commands are supposed to be
      passed integer arguments. Currently, if
      non-integers are given, NumberFormatException
      is thrown but not caught.
    
    Handle NumberFormatException by throwing our
      very own RicaCommandException with a
      user-friendly error message that Rica will
      catch(and tell the user about).

--- src/main/java/io/github/haoyangw/rica/input/MarkCommand.java
+++ src/main/java/io/github/haoyangw/rica/input/MarkCommand.java
@@ -1,8 +1,10 @@
 package io.github.haoyangw.rica.input;
 
+import io.github.haoyangw.rica.exception.RicaCommandException;
 import io.github.haoyangw.rica.task.TaskManager;
 
 public class MarkCommand extends Command {
+    private static final String NOT_A_NUMBER_ERROR = " That's not a Task number is it? Or are you from another dimension hmm";
 
     public MarkCommand(String command, TaskManager taskManager) {
         super(command, taskManager);
@@ -15,7 +17,12 @@ public class MarkCommand extends Command {
     public void run() {
         String[] params = super.getCommand().split(Command.CMD_SEPARATOR);
         int SECOND_PARAM = 1;
-        int indexOfTodo = Integer.parseInt(params[SECOND_PARAM]) - 1;
+        int indexOfTodo;
+        try {
+            indexOfTodo = Integer.parseInt(params[SECOND_PARAM]) - 1;
+        } catch (NumberFormatException exception) {
+            throw new RicaCommandException(MarkCommand.NOT_A_NUMBER_ERROR);
+        }
         super.getTaskManager().markDone(indexOfTodo);
     }
 
--- src/main/java/io/github/haoyangw/rica/input/UnmarkCommand.java
+++ src/main/java/io/github/haoyangw/rica/input/UnmarkCommand.java
@@ -1,8 +1,10 @@
 package io.github.haoyangw.rica.input;
 
+import io.github.haoyangw.rica.exception.RicaCommandException;
 import io.github.haoyangw.rica.task.TaskManager;
 
 public class UnmarkCommand extends Command {
+    private static final String NOT_A_NUMBER_ERROR = " Hmm I don't think that's a Task number? Hope my CPU is working xP";
 
     public UnmarkCommand(String command, TaskManager taskManager) {
         super(command, taskManager);
@@ -16,7 +18,12 @@ public class UnmarkCommand extends Command {
     public void run() {
         String[] params = super.getCommand().split(Command.CMD_SEPARATOR);
         int SECOND_PARAM = 1;
-        int indexOfTodo = Integer.parseInt(params[SECOND_PARAM]) - 1;
+        int indexOfTodo;
+        try {
+            indexOfTodo = Integer.parseInt(params[SECOND_PARAM]) - 1;
+        } catch (NumberFormatException exception) {
+            throw new RicaCommandException(UnmarkCommand.NOT_A_NUMBER_ERROR);
+        }
         super.getTaskManager().unmarkDone(indexOfTodo);
     }
 
