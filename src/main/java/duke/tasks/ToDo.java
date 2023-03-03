4b71b8879e23b262c3566e3661e3ff269001e4d9
kishore-a00 <skippyepic@gmail.com>
2023-03-03 16:08:17 +0800

    Add comments for ToDo class

--- src/main/java/duke/tasks/ToDo.java
+++ src/main/java/duke/tasks/ToDo.java
@@ -1,7 +1,14 @@
 package duke.tasks;
 
+/**
+ * This class inherits from Task class.
+ * ToDos only contain a description.
+ */
 public class ToDo extends Task {
-
+    /**
+     * Creates a todo object.
+     * @param description
+     */
     public ToDo(String description) {
         super(description, "T");
     }
