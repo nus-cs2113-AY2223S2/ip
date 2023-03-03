cef26a2ee1b85a75a452e8c827fd5ea30fbce7df
Liam Van <74201538+SpeciLiam@users.noreply.github.com>
2023-03-03 16:46:17 +0800

    Code Cleanup

--- src/main/java/duke/Commands/FindCommand.java
+++ src/main/java/duke/Commands/FindCommand.java
@@ -1,2 +1,27 @@
-package duke.Commands;public class FindCommand {
+package duke.Commands;
+
+import duke.Ui;
+
+public class FindCommand extends Command {
+    public static final String COMMAND_WORD = "find";
+    private String term;
+    public FindCommand(String term) {
+        this.term = term;
+    }
+    public void cmd() {
+        Ui ui = new Ui();
+        ui.showLine();
+        if (tasks.getSize() == 0) {
+            System.out.println("There are no tasks to search.");
+        } else {
+            System.out.println("\t Here are the matching tasks in your list:\n");
+            for (int i = 0; i < tasks.getSize(); i++) {
+                if (tasks.getTask(i).getDescription().contains(this.term)) {
+                    System.out.println(tasks.getTask(i));
+                }
+            }
+        }
+
+    }
+
 }
--- src/main/java/duke/Parser.java
+++ src/main/java/duke/Parser.java
@@ -13,33 +13,37 @@ public class Parser {
         String cmd = cmds[0];
         Command tmpCommand = null;
         boolean isParse = true;
-
+        System.out.println("Here " + cmd);
         switch (cmd) {
         case DeadLineCommand.COMMAND_WORD:
-                tmpCommand = parseDeadline(cmds[1]);
-                break;
+            tmpCommand = parseDeadline(cmds[1]);
+            break;
         case DeleteCommand.COMMAND_WORD:
             tmpCommand = parseDelete(cmds[1]);
             break;
         case EventCommand.COMMAND_WORD:
-             tmpCommand = parseEvent(cmds[1]);
-             break;
+            tmpCommand = parseEvent(cmds[1]);
+            break;
         case ExitCommand.COMMAND_WORD:
-             tmpCommand = parseExit();
-             break;
+            tmpCommand = parseExit();
+            break;
         case ListCommand.COMMAND_WORD:
-             tmpCommand = parseList();
-             break;
+            tmpCommand = parseList();
+            break;
         case MarkCommand.COMMAND_WORD:
-             tmpCommand = parseMark(cmds[1]);
-             break;
+            tmpCommand = parseMark(cmds[1]);
+            break;
         case UnMarkCommand.COMMAND_WORD:
-             tmpCommand = parseUnMark(cmds[1]);
-             break;
+            tmpCommand = parseUnMark(cmds[1]);
+            break;
         case ToDoCommand.COMMAND_WORD:
-            System.out.println(cmds[1]);
-             tmpCommand = parseToDo(cmds[1]);
-             break;
+            tmpCommand = parseToDo(cmds[1]);
+            break;
+        case FindCommand.COMMAND_WORD:
+            System.out.println("Here in caseFind");
+
+            tmpCommand = parseFind(cmds[1]);
+            break;
         default:
             return new Command();
         }
@@ -47,9 +51,16 @@ public class Parser {
         return tmpCommand;
 
     }
+
+    public static Command parseFind(String input) {
+        System.out.println("Here in parseFind");
+        return new FindCommand(input);
+    }
+
     public static Command parseToDo(String input) {
         return new ToDoCommand(input, false);
     }
+
     public static Command parseDeadline(String input) {
         int idx = input.indexOf("/by");
         System.out.println(input);
@@ -61,8 +72,9 @@ public class Parser {
 
     public static Command parseDelete(String input) {
         int dIdx = Integer.parseInt(input);
-        return new DeleteCommand(dIdx-1);
+        return new DeleteCommand(dIdx - 1);
     }
+
     public static Command parseEvent(String input) {
         int idx = input.indexOf("/from");
         int idx1 = input.indexOf("/to");
@@ -71,21 +83,24 @@ public class Parser {
         String end = input.substring(idx1 + 3);
         return new EventCommand(desc, false, start, end);
     }
+
     public static Command parseExit() {
         return new ExitCommand();
     }
+
     public static Command parseList() {
         return new ListCommand();
     }
+
     public static Command parseMark(String input) {
         int dIdx = Integer.parseInt(input);
-        return new MarkCommand(dIdx-1);
-    }
-    public static Command parseUnMark(String input) {
-        int dIdx = Integer.parseInt(input);
-        return new UnMarkCommand(dIdx-1);
+        return new MarkCommand(dIdx - 1);
     }
 
+    public static Command parseUnMark(String input) {
+        int dIdx = Integer.parseInt(input);
+        return new UnMarkCommand(dIdx - 1);
+    }
 
 
 }
