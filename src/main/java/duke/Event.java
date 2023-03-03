31538d3a46115b97fbd186a1e8fbec82c3c75c0e
Li Mingyuan <e0773670@u.nus.edu>
2023-03-03 14:19:19 +0800

    Add JavaDocs

--- src/main/java/duke/Event.java
+++ src/main/java/duke/Event.java
@@ -1,14 +1,27 @@
 package duke;
 
+/**
+ * Event subclass of Task
+ */
 public class Event extends Deadline {
     public String from;
 
+    /**
+     * Constructor for an Event
+     * @param description description of the Event
+     * @param from starting time of the Event
+     * @param to ending time of the Event
+     */
     public Event(String description, String from, String to) {
         super(description, to);
         this.from = from;
         this.type = TaskType.EVENT;
     }
 
+    /**
+     * Gets a formatted string of the Event with properties
+     * @return the formatted string
+     */
     @Override
     public String getLabel() {
         String typeIndicator = "[E]";
--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -2,37 +2,71 @@ package duke;
 
 import java.util.ArrayList;
 
+/**
+ * Class representing a list of tasks and its associated functions
+ */
 public class TaskList {
     private final ArrayList<Task> tasks;
 
+    /**
+     * Constructor for a task list if there is already a list of tasks.
+     * @param tasks ArrayList of tasks
+     */
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Constructor for a new task list, will be empty.
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Getter for all tasks in the task list
+     * @return ArrayList containing all tasks
+     */
     public ArrayList<Task> getAllTasks() {
         return tasks;
     }
 
+    /**
+     * Adds a new task to the task list
+     * @param task the task to be added
+     * @return the task that was just added
+     */
     public Task addTask(Task task) {
         tasks.add(task);
         return task;
     }
 
+    /**
+     * Remove a task by its displayed index
+     * @param displayedIndex the displayed index of the task
+     * @return the task that was just removed
+     */
     public Task removeTaskByDisplayedIndex(int displayedIndex) {
         Task task = tasks.get(displayedIndex-1);
         tasks.remove(displayedIndex-1);
         return task;
     }
 
+    /**
+     * Marks a task as done by its displayed index
+     * @param displayedIndex the displayed index of the task
+     * @return the task that was just marked
+     */
     public Task markTaskByDisplayedIndex(int displayedIndex) {
         tasks.get(displayedIndex-1).setIsDone(true);
         return tasks.get(displayedIndex-1);
     }
 
+    /**
+     * Marks a task as not done by its displayed index
+     * @param displayedIndex the displayed index of the task
+     * @return the task that was just unmarked
+     */
     public Task unmarkTaskByDisplayedIndex(int displayedIndex) {
         tasks.get(displayedIndex-1).setIsDone(false);
         return tasks.get(displayedIndex-1);
