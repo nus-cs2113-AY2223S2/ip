399c136985d2b8d73ea86354feb1f9caa39e0f88
Gujimugger <wangsilangCHANGE@gmail.com>
2023-03-05 23:53:01 +0800

    Published UG JAR released

--- src/main/java/Duke/Ui.java
+++ src/main/java/Duke/Ui.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Ui {
+public class Ui { 
     private Scanner scanner;
 
     public Ui() {
