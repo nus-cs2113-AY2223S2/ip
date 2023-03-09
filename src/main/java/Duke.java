47468b6bd9c490ac58a7077671bd7b8cdc35e373
Wilson Lee Jun Wei <leejunwei2000@gmail.com>
2023-03-09 10:46:58 +0800

    Add Merge-PRs

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
  */
 public class Duke {
     // testing the merge-pr branch
+    // testing the merge pull request
     public static void main(String[] arguments) {
         ArrayList<Task> listItems = new ArrayList<>();
         int index = 0;
