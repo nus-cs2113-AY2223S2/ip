2064f44e06d4ea24a4da0c55f3737054626da8c9
Wilson Lee Jun Wei <leejunwei2000@gmail.com>
2023-03-12 15:26:32 +0800

    Edit reuse statement

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,4 +1,4 @@
-//Solution below adapted and reused from Student Oh Yi Xiu Wilson (Only for A-More OOP)
+//Solution below adapted and reused from Student Oh Yi Xiu Wilson
 // with modifications made by Wilson Lee Jun Wei
 
 /**
