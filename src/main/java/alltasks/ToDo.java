123ea6602741e1a29f218e7b96a40181ba4e29af
Wilson Lee Jun Wei <leejunwei2000@gmail.com>
2023-03-14 14:27:16 +0800

    Rectify Storage.java

--- src/main/java/alltasks/ToDo.java
+++ src/main/java/alltasks/ToDo.java
@@ -29,7 +29,7 @@ public class ToDo extends Task{
     //Solution below adapted from Student Oh Yi Xiu Wilson
     @Override
     public String getInfo() {
-        return String.format("%s|%s|%s", "Todo", this.isDone ? 1 : 0, this.description);
+        return String.format("%s|%s|%s", "T", this.isDone ? 1 : 0, this.description);
     }
     //End of adapted solution from Student Oh Yi Xiu Wilson
 }
