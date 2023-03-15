2e8ce4e5ff8b9777cad206def4aa4d60df7830ae
nichyjt <nicholas.yek@gmail.com>
2023-03-15 13:10:36 +0800

    Update formatting nit
    Signed-off-by: nichyjt <nicholas.yek@gmail.com>

--- src/main/java/max/command/Command.java
+++ src/main/java/max/command/Command.java
@@ -58,6 +58,7 @@ public enum Command {
     /**
      * Get the string list for arguments needed by the main command
      * e.g. to, from for
+     *
      * @return String list for the subcommands
      */
     public String[] getSubcommandNames() {
