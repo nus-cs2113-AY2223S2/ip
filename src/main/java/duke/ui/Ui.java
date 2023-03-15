6cf7c8c7ecd0fd5aed95c6b841fe35b2fcc9675e
tsx0314 <tian.shixing2001@gmail.com>
2023-03-15 14:43:58 +0800

    Add find and exit command in help page

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -36,7 +36,8 @@ public class Ui {
 			+ "    mark {task index number} --mark the task as done" + "\n"
 			+ "    unmark {task index number} --mark the task as undone" + "\n"
 			+ "    delete {task index number} --delete the task" + "\n"
-			+ "    list --to show the current task list"
+			+ "    find {keywords}" + "\n"
+			+ "    list --to show the current task list" + "\n"
 			+ "    exit --to exit the application";
 
 	/**
