3e30fc7c335ffbe05f83176ba982df009ba3ed5e
Toh-HongFeng <hf.toh2@gmail.com>
2023-03-18 14:41:25 +0800

    update jar file

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -38,7 +38,6 @@ public class Duke {
         }
     }
     public static void main(String[] args) {
-        new Duke("taskData.txt").run(); // TODO: create jar file
-        // TODO: update User Guide and release
+        new Duke("taskData.txt").run();
     }
 }
