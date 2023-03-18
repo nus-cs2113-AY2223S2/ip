77557053191a1bd12b3be41c481dec426d41bc83
hyperbola-bear <hung.hin.wang@gmail.com>
2023-03-19 00:40:17 +0800

    oops left out something

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -21,33 +21,37 @@ public class Storage {
     // stores the task type, status, description, date and time into the respective
     // arraylists
     // constructor cannot be used due to the way the constructor is written
-    public static void readFile(String filePath) throws FileNotFoundException { // reads file
-        File file = new File(filePath);
-        Scanner reader = new Scanner(file);
-        while (reader.hasNextLine()) {
-            String[] strArray = reader.nextLine().split(" \\| ");
-            if (strArray[0].trim().equals("TODO")) // store task type
-                Task.setAddTypeArray(Task.TaskType.TODO);
-            else if (strArray[0].trim().equals("DEADLINE"))
-                Task.setAddTypeArray(Task.TaskType.DEADLINE);
-            else if (strArray[0].trim().equals("EVENT"))
-                Task.setAddTypeArray(Task.TaskType.EVENT);
-            if (strArray[1].trim().equals("1")) // store task status
-                Task.setAddMarkArray(true);
-            else
-                Task.setAddMarkArray(false);
-            if (strArray[3].trim().equals("null")) // store date and time
-                Task.setAddDateFromArray(null);
-            else
-                Task.setAddDateFromArray(LocalDateTime.parse(strArray[3].trim()));
-            if (strArray[4].trim().equals("null"))
-                Task.setAddDateToArray(null);
-            else
-                Task.setAddDateToArray(LocalDateTime.parse(strArray[4].trim()));
-            Task.setAddDescArray(strArray[2].trim()); // store task description
+    public static void readFile(String filePath) { // reads file
+        try {
+            File file = new File(filePath);
+            Scanner reader = new Scanner(file);
+            while (reader.hasNextLine()) {
+                String[] strArray = reader.nextLine().split(" \\| ");
+                if (strArray[0].trim().equals("TODO")) // store task type
+                    Task.setAddTypeArray(Task.TaskType.TODO);
+                else if (strArray[0].trim().equals("DEADLINE"))
+                    Task.setAddTypeArray(Task.TaskType.DEADLINE);
+                else if (strArray[0].trim().equals("EVENT"))
+                    Task.setAddTypeArray(Task.TaskType.EVENT);
+                if (strArray[1].trim().equals("1")) // store task status
+                    Task.setAddMarkArray(true);
+                else
+                    Task.setAddMarkArray(false);
+                if (strArray[3].trim().equals("null")) // store date and time
+                    Task.setAddDateFromArray(null);
+                else
+                    Task.setAddDateFromArray(LocalDateTime.parse(strArray[3].trim()));
+                if (strArray[4].trim().equals("null"))
+                    Task.setAddDateToArray(null);
+                else
+                    Task.setAddDateToArray(LocalDateTime.parse(strArray[4].trim()));
+                Task.setAddDescArray(strArray[2].trim()); // store task description
+            }
+            reader.close();
+            System.out.println("File read successfully");
+        } catch (FileNotFoundException e) {
+            System.out.println("Save file not found! use command 'save' to create a save file");
         }
-        reader.close();
-        System.out.println("File read successfully");
     }
 
     // saves file to data/duke.txt
