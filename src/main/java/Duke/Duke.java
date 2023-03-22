84b5533f1cbe59619cf45334e4979304ab7f4c95
aaronxujiachen <1943999635x@gmail.com>
2023-03-22 10:11:29 +0800

    Duke final level

--- src/main/java/Duke/Duke.java
+++ src/main/java/Duke/Duke.java
@@ -1,7 +1,18 @@
 package Duke;
 
+import java.io.*;
 import java.util.Scanner;  // Import the Scanner class
 import java.util.ArrayList;
+
+
+/*
+ * Duke is a chatbot program.
+ * It can interact with users to perform various tasks such as adding, deleting, and listing tasks.
+ * Event details and deadlines can also be stored.
+ * Invalid input can be identified as well.
+ */
+
+
 public class Duke {
 
     public static void drawLine() {  // Draw horizontal lines
@@ -19,7 +30,7 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         drawLine();
-        System.out.println("Hello! I'm Duke.Duke");
+        System.out.println("Hello! I'm Duke Bot");
         System.out.println("What can I do for you?");
         drawLine();
     }
@@ -55,7 +66,7 @@ public class Duke {
         drawLine();
     }
 
-    public static int updateTasks(String input, ArrayList<Task> tasks, int curPos) {  // Include remarks
+    public static int updateTasks(String input, ArrayList<Task> tasks, int curPos) throws IOException {  // Include remarks
         String[] arrOfInput = input.split(" ", 2);
         System.out.println("Got it. I've added this task:");
 
@@ -75,13 +86,14 @@ public class Duke {
         System.out.println("   " + tasks.get(curPos).getType() + tasks.get(curPos).toString());
         curPos++;
         System.out.println("Now you have " + curPos + (curPos > 1 ? " tasks " : " task ") + "in the list.");
+        saveList(tasks);
         drawLine();
         return curPos;
     }
 
     // @@[yuanners] [aaronxujiachen]-reused
     // With minor modification
-    public static int checkInput(String input, ArrayList<Task> tasks, int curPos) throws DukeException {
+    public static int checkInput(String input, ArrayList<Task> tasks, int curPos) throws DukeException, IOException {
 
         if(input.length () > 4) {
             if(input.startsWith("mark") || input.startsWith("unmark")) {
@@ -93,17 +105,26 @@ public class Duke {
             else if(input.startsWith("delete")) {
                 curPos = deleteTask (input, tasks, curPos);
             }
+            else if(input.startsWith("find")) {
+                findTask(input, tasks);
+            }
             else if(!((input.startsWith("mark")) || (input.startsWith("unmark")) || (input.startsWith("todo")) || (input.startsWith("deadline")) || (input.startsWith("event")) || (input.startsWith("delete")))) {
                 throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means :-(");
             }
             else{
                 throw new DukeException("☹ OOPS!!! The description of a " + input + " cannot be empty.");
             }
+
+            saveList(tasks);
+
         }
         else {
             if(input.equals("list")) {
                 listTasks(tasks);
             }
+            /*else if (input.equals("save")) {
+            	saveList(tasks);
+            }*/
             else if(input.equals("mark") || input.equals("todo")) {
                 throw new DukeException("☹ OOPS!!! The description of a " + input + " cannot be empty.");
             }
@@ -129,13 +150,80 @@ public class Duke {
         return curPos;
     }
 
-    public static void main(String[] args) {
-        hiDuke();  //Print Logo
+    public static void saveList (ArrayList<Task> tasks) throws IOException {
+        FileWriter writer = new FileWriter("duke.txt");
+        for(int i = 0; i < tasks.size(); i++) {
+            //System.out.println((i+1) + ". " + tasks.get(i).getType() + tasks.get(i).toString());
+            writer.write((i+1) + ". " + tasks.get(i).getType() + tasks.get(i).toString() + System.lineSeparator());
+        }
+        writer.close();
+    }
 
+    public static ArrayList<Task> loadList () throws IOException {
+        BufferedReader bufReader = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/duke.txt"));
+        //String filePath = System.getProperty("user.dir") + "duke.txt";
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        ArrayList<String> list = new ArrayList<>();
+
+        String line = bufReader.readLine();
+        while(line != null) {
+            list.add(line);
+            line = bufReader.readLine();
+        }
+
+        bufReader.close();
+        //System.out.println(list.get(0));
+
+        for(int i = 0; i < list.size(); i++) {
+
+            if (list.get(i).charAt(4) == 'T') {
+                tasks.add(new Task(list.get(i).substring(9, list.get(i).length())));
+            }
+            else if (list.get(i).charAt(4) == 'D') {
+                String[] arrOfTask = list.get(i).split("by:");
+                tasks.add(new Deadline(list.get(i).substring(9,arrOfTask[0].length()-1), arrOfTask[1].substring(0, arrOfTask[1].length()-1)));
+            }
+            else if (list.get(i).charAt(4) == 'E') {
+                String[] arrOfTask = list.get(i).split("from:");
+                String[] arrOfEvent = arrOfTask[1].split("to:");
+                tasks.add(new Event(list.get(i).substring(9,arrOfTask[0].length()-1), arrOfEvent[0], arrOfEvent[1].substring(0, arrOfEvent[1].length()-1)));
+            }
+            else {
+                System.out.println("duke.txt has invalid format");
+                System.exit(0);
+            }
+        }
+
+        return tasks;
+    }
+
+    public static void findTask (String input, ArrayList<Task> tasks) {
+        String[] arrOfInput = input.split (" ", 2);
+        System.out.println("Here are the matching tasks in your list:");
+        int count = 1;
+        for(int i = 0; i < tasks.size(); i++) {
+            if (tasks.get(i).toString().contains(arrOfInput[1])) {
+                System.out.println((count) + ". " + tasks.get(i).getType() + tasks.get(i).toString());
+                count++;
+            }
+        }
+        drawLine();
+    }
+
+    public static void main(String[] args) throws IOException {
+        hiDuke();  //Print Logo
+        //loadList();
+        ArrayList<Task> tasks;
+        File f = new File(System.getProperty("user.dir") + "/duke.txt");
+        if(f.exists() && !f.isDirectory()) {
+            tasks = loadList();
+        }
+        else{
+            tasks = new ArrayList<Task>();
+        }
+        int curPos = tasks.size();
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
-        ArrayList<Task> tasks = new ArrayList<Task>();
-        int curPos = 0;
 
         while(!(input.isEmpty())) {
             drawLine();
@@ -160,3 +248,4 @@ public class Duke {
         byeDuke();
     }
 }
+
